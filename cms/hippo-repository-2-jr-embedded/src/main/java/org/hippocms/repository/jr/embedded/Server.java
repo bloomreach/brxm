1f9e751ea31c0a06833ed81d23cf574f732d7b79
Walter Grevink <w.grevink@onehippo.com>
2007-04-12 16:02:41 +0000

    HREPTWO-6 Trivial server test
    svn path=/hippo-repository-2/trunk/; revision=5965

--- cms/hippo-repository-2-jr-embedded/src/main/java/org/hippocms/repository/jr/embedded/Server.java
+++ cms/hippo-repository-2-jr-embedded/src/main/java/org/hippocms/repository/jr/embedded/Server.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2006 Hippo
+ * Copyright 2007 Hippo
  *
  * Licensed under the Apache License, Version 2.0 (the  "License");
  * you may not use this file except in compliance with the License.
@@ -18,26 +18,14 @@ package org.hippocms.repository.jr.embedded;
 import java.io.File;
 import java.io.InputStream;
 
-import javax.jcr.Node;
-import javax.jcr.NodeIterator;
-import javax.jcr.Property;
-import javax.jcr.PropertyIterator;
 import javax.jcr.Repository;
-import javax.jcr.Session;
-import javax.jcr.Value;
-import javax.jcr.SimpleCredentials;
 import javax.jcr.RepositoryException;
-import javax.jcr.Workspace;
-// import javax.jcr.NodeTypeManager; Not yet in JCR1
+import javax.jcr.Session;
+import javax.jcr.SimpleCredentials;
 
 import org.apache.jackrabbit.api.JackrabbitRepository;
 import org.apache.jackrabbit.core.RepositoryImpl;
 import org.apache.jackrabbit.core.config.RepositoryConfig;
-import org.apache.jackrabbit.rmi.server.ServerAdapterFactory;
-import org.apache.jackrabbit.core.nodetype.NodeTypeDef;
-import org.apache.jackrabbit.core.nodetype.NodeTypeManagerImpl;
-import org.apache.jackrabbit.core.nodetype.NodeTypeRegistry;
-import org.apache.jackrabbit.core.nodetype.compact.CompactNodeTypeDefReader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -46,72 +34,50 @@ import org.slf4j.LoggerFactory;
  *
  */
 public class Server {
+    
     private static final Logger log = LoggerFactory.getLogger(Server.class);
-    private JackrabbitRepository repository;
-    private Session session;
+    private String workingDir;
 
-    Server(String workingDirectory) throws RepositoryException {
-        String workingDir = new File(workingDirectory).getAbsolutePath();
+    
+    public Server(String workingDirectory) throws RepositoryException {
+       workingDir = new File(workingDirectory).getAbsolutePath();
+    }
+    
+    
+    public JackrabbitRepository startUp() throws RepositoryException {
         InputStream config = getClass().getResourceAsStream("repository.xml");
-        repository = RepositoryImpl.create(RepositoryConfig.create(config, workingDir));
+        JackrabbitRepository repository = RepositoryImpl.create(RepositoryConfig.create(config, workingDir));
         String result = repository.getDescriptor("OPTION_NODE_TYPE_REG_SUPPORTED");
-        log.info("Node type registration support: " + (result!=null?result:"no"));
-        session = repository.login(new SimpleCredentials("username", "password".toCharArray()));
-        log.info("Logged in as " + session.getUserID() + " to a " + repository.getDescriptor(Repository.REP_NAME_DESC)
+        log.info("Node type registration support: " + (result != null ? result : "no"));
+        
+        return repository;
+    }
+    
+    
+    public Session login(JackrabbitRepository repository) throws RepositoryException {
+        Session result = repository.login(new SimpleCredentials("username", "password".toCharArray()));
+        log.info("Logged in as " + result.getUserID() + " to a " + repository.getDescriptor(Repository.REP_NAME_DESC)
                 + " repository.");
+        
+        return result;
     }
-
-    void traverse(Node parent) throws RepositoryException {
-        System.out.println();
-        traverse(parent, 0);
-        System.out.println();
-    }
-
-    void traverse(Node parent, int level) throws RepositoryException {
-        String prefix = "";
-        for (int i = 0; i < level; i++)
-            prefix += "  ";
-        System.out.println(prefix + parent.getPath() + " [name=" + parent.getName() + ",depth=" + parent.getDepth()
-                + "]");
-        for (PropertyIterator iter = parent.getProperties(); iter.hasNext();) {
-            Property prop = iter.nextProperty();
-            System.out.print(prefix + "| " + prop.getPath() + " [name=" + prop.getName() + "] = ");
-            if (prop.getDefinition().isMultiple()) {
-                Value[] values = prop.getValues();
-                System.out.print("[ ");
-                for (int i = 0; i < values.length; i++)
-                    System.out.println((i > 0 ? ", " : "") + values[i].getString());
-                System.out.println(" ]");
-            } else
-                System.out.println(prop.getString());
-        }
-        for (NodeIterator iter = parent.getNodes(); iter.hasNext();) {
-            Node node = iter.nextNode();
-            if (!node.getPath().equals("/jcr:system"))
-                traverse(node, level + 1);
-        }
-    }
-
-    void test() throws RepositoryException {
-        Node root = session.getRootNode();
-        Workspace workspace = session.getWorkspace();
-        NodeTypeManagerImpl ntmgr = (NodeTypeManagerImpl) workspace.getNodeTypeManager();
-        NodeTypeRegistry ntreg = ntmgr.getNodeTypeRegistry();
-
-        root.addNode("x");
-        root.addNode("y");
-        root.addNode("z");
-
-        traverse(session.getRootNode());
-        session.save();
-        session.logout();
+    
+    
+    public void shutDown(JackrabbitRepository repository) {
         repository.shutdown();
     }
+    
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         try {
-            Server server = new Server(args.length>0 ? args[0] : ".");
-            server.test();
+            String defaultWorkdir = System.getProperty("user.dir") + System.getProperty("file.separator") + "work";
+            
+            Server server = new Server(args.length > 0 ? args[0] : defaultWorkdir);
+            JackrabbitRepository repository  = server.startUp();
+            Session session = server.login(repository);
+            // do something
+            server.shutDown(repository);
+
         } catch (RepositoryException ex) {
             System.err.println(ex.getMessage());
             ex.printStackTrace(System.err);
