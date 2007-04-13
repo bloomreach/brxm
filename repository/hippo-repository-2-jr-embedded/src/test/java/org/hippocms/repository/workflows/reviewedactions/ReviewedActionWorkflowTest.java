416a4971b313c87ceca7c2eeeebec229624abb88
Johan Stuyts <j.stuyts@onehippo.com>
2007-04-13 13:58:28 +0000

    HREPTWO-15: added test to check that the document can be changed when there are no pending requests.
    svn path=/hippo-repository-2/trunk/; revision=6004

---
+++ repository/hippo-repository-2-jr-embedded/src/test/java/org/hippocms/repository/workflows/reviewedactions/ReviewedActionWorkflowTest.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2007 Hippo.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.hippocms.repository.workflows.reviewedactions;
+
+import junit.framework.TestCase;
+import org.hippocms.repository.model.CurrentUsernameSource;
+import org.hippocms.repository.model.Document;
+import org.hippocms.repository.model.DocumentTemplate;
+
+public class ReviewedActionWorkflowTest extends TestCase {
+    public ReviewedActionWorkflowTest() {
+        super();
+    }
+
+    public ReviewedActionWorkflowTest(String name) {
+        super(name);
+    }
+
+    public void testCanChangeContentWithNoPendingRequests() {
+        DocumentTemplate docTemplate = new DocumentTemplate();
+
+        CurrentUsernameSource currentUsernameSource = new CurrentUsernameSource();
+        currentUsernameSource.setCurrentUsername("John Doe");
+        docTemplate.setCurrentUsernameSource(currentUsernameSource);
+        docTemplate.setWorkflowFactory(new ReviewedActionsWorkflowFactory());
+        Document doc = docTemplate.create("Lorem ipsum");
+
+        try {
+            doc.setContent("Quux qux baz bar foo.");
+        } catch (Exception e) {
+            fail("It must be allowed to set the content of documents when there are no pending request");
+        }
+    }
+}
