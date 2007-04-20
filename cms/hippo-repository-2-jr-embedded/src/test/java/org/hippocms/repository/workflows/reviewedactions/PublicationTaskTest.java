a35043a83c9ba9ee9fefe0fefc7d28e194fb456f
Johan Stuyts <j.stuyts@onehippo.com>
2007-04-20 15:27:08 +0000

    HREPTWO-15: moved tests to other classes.
    svn path=/hippo-repository-2/trunk/; revision=6208

--- cms/hippo-repository-2-jr-embedded/src/test/java/org/hippocms/repository/workflows/reviewedactions/PublicationTaskTest.java
+++ cms/hippo-repository-2-jr-embedded/src/test/java/org/hippocms/repository/workflows/reviewedactions/PublicationTaskTest.java
@@ -20,6 +20,7 @@ import org.hippocms.repository.model.CurrentUsernameSource;
 import org.hippocms.repository.model.Document;
 import org.hippocms.repository.model.DocumentTemplate;
 import org.hippocms.repository.model.PublicationServiceProvider;
+import org.hippocms.repository.model.Scheduler;
 import org.hippocms.repository.model.mock.MockScheduler;
 import org.jmock.Mock;
 import org.jmock.MockObjectTestCase;
@@ -61,5 +62,32 @@ public class PublicationTaskTest extends MockObjectTestCase {
         workflow.publish(publicationDate, null);
 
         scheduler.runAllTasks();
+    }
+
+    public void testCancellingPublicationTaskRemovesTaskFromScheduler() {
+        DocumentTemplate docTemplate = new DocumentTemplate();
+
+        CurrentUsernameSource currentUsernameSource = new CurrentUsernameSource();
+        currentUsernameSource.setCurrentUsername("John Doe");
+        docTemplate.setCurrentUsernameSource(currentUsernameSource);
+        ReviewedActionsWorkflowFactory workflowFactory = new ReviewedActionsWorkflowFactory();
+        workflowFactory.setCurrentUsernameSource(currentUsernameSource);
+
+        Mock schedulerMock = mock(Scheduler.class);
+        Scheduler scheduler = (Scheduler) schedulerMock.proxy();
+        Date publicationDate = new Date(System.currentTimeMillis() + SECONDS_IN_A_DAY);
+        String taskId = "1";
+        schedulerMock.expects(once()).method("schedule").with(eq(publicationDate), NOT_NULL).will(returnValue(taskId));
+        schedulerMock.expects(once()).method("cancel").with(eq(taskId));
+        workflowFactory.setScheduler(scheduler);
+
+        docTemplate.setWorkflowFactory(workflowFactory);
+
+        Document doc = docTemplate.create("Lorem ipsum");
+        doc.setContent("Foo bar baz qux quux.");
+        ReviewedActionsWorkflow workflow = (ReviewedActionsWorkflow) doc.getWorkflow();
+        workflow.publish(publicationDate, null);
+
+        workflow.cancelPublicationTask();
     }
 }
