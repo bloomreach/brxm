25a6246bbc367cc8c7794977c856f5ae58e06fd3
Bart van der Schans <b.vanderschans@onehippo.com>
2007-04-24 15:42:33 +0000

    HREPTWO-6: Export Webdav to JCR over RMI
    svn path=/hippo-repository-2/trunk/; revision=6253

--- repository/hippo-repository-2-jr-testdata/src/main/java/org/hippocms/repository/jr/Webdav2JCRDumper.java
+++ repository/hippo-repository-2-jr-testdata/src/main/java/org/hippocms/repository/jr/Webdav2JCRDumper.java
@@ -18,12 +18,16 @@ package org.hippocms.repository.jr;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.util.Iterator;
+import java.util.StringTokenizer;
 
-import javax.jcr.Node;
+import javax.jcr.PathNotFoundException;
+import javax.jcr.Repository;
 import javax.jcr.RepositoryException;
 import javax.jcr.Session;
+import javax.jcr.SimpleCredentials;
 import javax.jcr.Value;
 import javax.jcr.ValueFactory;
+import javax.jcr.Workspace;
 
 import nl.hippo.webdav.batchprocessor.Configuration;
 import nl.hippo.webdav.batchprocessor.Plugin;
@@ -31,43 +35,122 @@ import nl.hippo.webdav.batchprocessor.PluginConfiguration;
 import nl.hippo.webdav.batchprocessor.WebdavBatchProcessor;
 import nl.hippo.webdav.batchprocessor.WebdavBatchProcessorException;
 
+import org.apache.jackrabbit.rmi.client.ClientRepositoryFactory;
 import org.apache.webdav.lib.Property;
 import org.apache.webdav.lib.PropertyName;
-import org.hippocms.repository.jr.embedded.Server;
 
 public class Webdav2JCRDumper implements Plugin {
 
-    private String webdavRootUri;
-    private String dumpFile;
-    private String workingDir;
+    // ---------------------------------------------------- Constants
+    private static final String JCR_RMI_HOST = "jcr.rmi.host";
+    private static final String JCR_RMI_PORT = "jcr.rmi.port";
 
-    private Server server;
+    private static final String JCR_REPOSITORY = "jcr.repository";
+    private static final String JCR_WORKSPACE = "jcr.workspace";
+    private static final String JCR_USER = "jcr.rmi.user";
+    private static final String JCR_PASS = "jcr.rmi.pass";
+    private static final String JCR_PATH = "jcr.path";
+
+    
+    // ---------------------------------------------------- Instance variables
+    // rmi
+    private String rmiHost;
+    private String rmiPort;
+
+    // jcr
+    private String jcrRepository;
+    private String jcrWorkspace;
+    private String jcrUser;
+    private String jcrPass;
+    private String jcrPath;
+
     private Session session;
     private ValueFactory valueFactory;
+    private String webdavRootUri;
+
+    // backup & embedded server
+    //private String dumpFile;
+    //private String workingDir;
+    //private Server server;
 
     public void configure(WebdavBatchProcessor processor, Configuration config, PluginConfiguration pluginConfig) {
+
+        // Fetch properties
         webdavRootUri = config.getRootUri();
-        workingDir = pluginConfig.getValue("jcr.workingdir");
-        dumpFile = pluginConfig.getValue("dumpfile");
-        try {
-            server = new Server(workingDir);
-            session = server.login();
-            valueFactory = session.getValueFactory();
-        } catch (RepositoryException e) {
-            throw new IllegalStateException("Failed to start JCR server.", e);
+        //workingDir = pluginConfig.getValue("jcr.workingdir");
+        //dumpFile = pluginConfig.getValue("dumpfile");
+
+        // rmi connection
+        rmiHost = pluginConfig.getValue(JCR_RMI_HOST);
+        rmiPort = pluginConfig.getValue(JCR_RMI_PORT);
+
+        // jcr settings
+        jcrRepository = pluginConfig.getValue(JCR_REPOSITORY);
+        jcrWorkspace = pluginConfig.getValue(JCR_WORKSPACE);
+        jcrUser = pluginConfig.getValue(JCR_USER);
+        jcrPass = pluginConfig.getValue(JCR_PASS);
+        jcrPath = pluginConfig.getValue(JCR_PATH);
+
+        // remove trainling slash
+        if (jcrPath.endsWith("/")) {
+            jcrPath = jcrPath.substring(0, jcrPath.length() - 1);
         }
+
+        // show config
+        printConfig(config);
+
+        // test and setup connection and login       
+        ClientRepositoryFactory factory = new ClientRepositoryFactory();
+        Repository repository;
+        try {
+            // get the repository
+            repository = factory.getRepository(getRmiUrl());
+
+            // login and get session
+            session = repository.login(new SimpleCredentials(jcrUser, jcrPass.toCharArray()));
+
+            // todo, add option to set and create workspace
+            Workspace ws = session.getWorkspace();
+            if (!ws.getName().equals(jcrWorkspace)) {
+                throw new RuntimeException("Workspaces not equal. Found: " + ws.getName() + " requested: "
+                        + jcrWorkspace);
+            }
+
+            // test and create target path
+            checkPath();
+
+            valueFactory = session.getValueFactory();
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
     }
 
     public void process(nl.hippo.webdav.batchprocessor.Node webdavNode) throws WebdavBatchProcessorException {
-        String parentPath = parentPath(webdavNode.getUri());
+        String jcrParentPath = jcrPath + parentPath(webdavNode.getUri());
         String nodeName = nodeName(webdavNode.getUri());
 
+        if (webdavNode.getUri().equals(webdavRootUri)) {
+            return;
+        }
+        
         try {
-            javax.jcr.Node parent = (javax.jcr.Node) session.getItem(parentPath);
-            javax.jcr.Node current = parent.addNode(nodeName);
+            //System.out.println("Converting WebDAV node: " + webdavNode.getUri() + " => " + jcrParentPath + "/" + nodeName);
+            System.out.append('.');
+            
+            javax.jcr.Node parent = (javax.jcr.Node) session.getItem(jcrParentPath);
 
             // Add content property
             if (!webdavNode.isCollection()) {
+
+                // Overwrite existing nodes
+                if (parent.hasNode(nodeName)) {
+                    parent.getNode(nodeName).remove();
+                }
+
+                javax.jcr.Node current = parent.addNode(nodeName);
+
                 String contentLengthAsString = webdavNode.getProperty("DAV:", "getcontentlength").getPropertyAsString();
                 int contentLength = Integer.parseInt(contentLengthAsString);
 
@@ -76,22 +159,32 @@ public class Webdav2JCRDumper implements Plugin {
                     Value value = valueFactory.createValue(new ByteArrayInputStream(content));
                     current.setProperty("content", value);
                 }
-            }
 
-            // Add metadata properties
-            Iterator webdavPropertyNames = webdavNode.propertyNamesIterator();
-            while (webdavPropertyNames.hasNext()) {
-                PropertyName webdavPropertyName = (PropertyName) webdavPropertyNames.next();
-                String webdavPropertyNamespace = webdavPropertyName.getNamespaceURI();
-                if (!webdavPropertyNamespace.equals("DAV:")) {
-                    String name = webdavPropertyName.getLocalName();
-                    Property webdavProperty = webdavNode.getProperty(webdavPropertyNamespace, name);
-                    Value value = valueFactory.createValue(webdavProperty.getPropertyAsString());
-                    current.setProperty(name, value);
+                // Add metadata properties
+                Iterator webdavPropertyNames = webdavNode.propertyNamesIterator();
+                while (webdavPropertyNames.hasNext()) {
+                    PropertyName webdavPropertyName = (PropertyName) webdavPropertyNames.next();
+                    String webdavPropertyNamespace = webdavPropertyName.getNamespaceURI();
+                    if (!webdavPropertyNamespace.equals("DAV:")) {
+                        String name = webdavPropertyName.getLocalName();
+                        Property webdavProperty = webdavNode.getProperty(webdavPropertyNamespace, name);
+                        Value value = valueFactory.createValue(webdavProperty.getPropertyAsString());
+                        current.setProperty(name, value);
+                    }
+                }
+            } else {
+                // create 'collection' nodes if they don't exist
+                if (!parent.hasNode(nodeName)) {
+                    parent.addNode(nodeName);
                 }
             }
             session.save();
 
+        } catch (PathNotFoundException e) {
+            System.err.println("");
+            System.err.println("JCR Path does not exist: " + e.getMessage());
+            System.err.println("Unable to create: " + nodeName);
+            throw new RuntimeException("Path does not exist: " + e.getMessage());
         } catch (RepositoryException e) {
             e.printStackTrace();
         } catch (IOException e) {
@@ -106,32 +199,86 @@ public class Webdav2JCRDumper implements Plugin {
     }
 
     public void postprocess() {
-        try {
-            //TODO: Implement createBackup(file) and restoreBackup(file) in Hippo repo2 server.
-            //server.createBackup(dumpFile);
-            
-            //For the time being just dump the repo contents to the console to verify that it works.
-            Node root = session.getRootNode();
-            server.dump(root);
-            
-            
-            session.logout();
-            server.close();
-        } catch (RepositoryException e) {
-            e.printStackTrace();
-        }
+        session.logout();
+        //        try {
+        //            //TODO: Implement createBackup(file) and restoreBackup(file) in Hippo repo2 server.
+        //            //server.createBackup(dumpFile);
+        //            
+        //            //For the time being just dump the repo contents to the console to verify that it works.
+        //            //Node root = session.getRootNode();
+        //            //server.dump(root);
+        //            
+        //            
+        //            //server.close();
+        //        } catch (RepositoryException e) {
+        //            e.printStackTrace();
+        //        }
     }
 
     private String parentPath(String uri) {
         String result = uri.substring(webdavRootUri.length());
+
         int i = result.lastIndexOf("/");
         result = i == -1 ? result : result.substring(0, i);
 
-        return "".equals(result) ? "/" : result;
+        return result;
     }
 
     private String nodeName(String uri) {
         return uri.substring(uri.lastIndexOf("/") + 1);
     }
 
+    private String getRmiUrl() {
+        return "rmi://" + rmiHost + ":" + rmiPort + "/" + jcrRepository;
+    }
+
+    private void checkPath() {
+        try {
+            javax.jcr.Node node = (javax.jcr.Node) session.getRootNode();
+            String currentPath = "";
+
+            StringTokenizer st = new StringTokenizer(jcrPath, "/"); 
+            
+            while (st.hasMoreTokens()) {
+                
+                String nodeName = st.nextToken();
+                
+                if (nodeName == null || "".endsWith(nodeName)) {
+                    continue;
+                }
+                System.out.println("Checking for: " + currentPath + "/" + nodeName);
+
+                // add node if it doesn't exist
+                if (!node.hasNode(nodeName)) {
+                    node.addNode(nodeName);
+                    System.out.println("Added node for jcrPath: " + currentPath);
+                }
+                currentPath += "/" + nodeName;
+                
+                // shift to child node 
+                node = node.getNode(nodeName);
+            }
+            session.save();
+
+        } catch (RepositoryException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void printConfig(Configuration config) {
+        System.out.println("******************************************************");
+        System.out.println("******************************************************");
+        System.out.println("Exporting from WebDAV: ");
+        System.out.println("  Url         : http://" + config.getLocationHost() + ":" + config.getLocationPort());
+        System.out.println("  Repository  : " + config.getLocationRootPath());
+        System.out.println("  Path        : " + config.getFullPath());
+        System.out.println("Importing in JCR:");
+        System.out.println("  RMI Url     : " + getRmiUrl());
+        System.out.println("  Repository  : " + jcrRepository);
+        System.out.println("  Workspace   : " + jcrWorkspace);
+        System.out.println("  Path        : " + jcrPath);
+        System.out.println("******************************************************");
+        System.out.println("******************************************************");
+    }
 }
