d7cbcc3ef9dbf183f811e871ba4eb1a374c59b42
Walter Grevink <w.grevink@onehippo.com>
2007-06-15 08:00:58 +0000

    HREPTWO-22 TreeView now get's it's data from JCR repository
    svn path=/hippo-repository-2/trunk/; revision=6782

--- cms/hippo-repository-2-webapp/src/main/java/org/hippocms/repository/webapp/TreeView.java
+++ cms/hippo-repository-2-webapp/src/main/java/org/hippocms/repository/webapp/TreeView.java
@@ -1,77 +1,153 @@
 package org.hippocms.repository.webapp;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
+import java.net.MalformedURLException;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.util.Enumeration;
 
+import javax.jcr.LoginException;
+import javax.jcr.Node;
+import javax.jcr.NodeIterator;
+import javax.jcr.Property;
+import javax.jcr.PropertyIterator;
+import javax.jcr.Repository;
+import javax.jcr.RepositoryException;
+import javax.jcr.Session;
+import javax.jcr.SimpleCredentials;
+import javax.jcr.Value;
+import javax.jcr.ValueFormatException;
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.DefaultTreeModel;
 import javax.swing.tree.TreeModel;
 import javax.swing.tree.TreeNode;
 
-import org.apache.wicket.PageParameters;
+import org.apache.jackrabbit.rmi.client.ClientRepositoryFactory;
 import org.apache.wicket.extensions.markup.html.tree.Tree;
 import org.apache.wicket.extensions.markup.html.tree.DefaultAbstractTree.LinkType;
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.tree.ITreeStateListener;
 
-public class TreeView extends WebPage {
+public class TreeView extends WebPage implements ITreeStateListener {
 
     private static final long serialVersionUID = 1L;
 
-    public TreeView(final PageParameters parameters) {
-        // create a list with sublists
-        List l1 = new ArrayList();
-        l1.add("test 1.1");
-        l1.add("test 1.2");
-        List l2 = new ArrayList();
-        l2.add("test 2.1");
-        l2.add("test 2.2");
-        l2.add("test 2.3");
-        List l3 = new ArrayList();
-        l3.add("test 3.1");
-        l2.add(l3);
-        l2.add("test 2.4");
-        l1.add(l2);
-        l1.add("test 1.3");
+    public TreeView() throws MalformedURLException, ClassCastException, RemoteException, NotBoundException,
+            LoginException, RepositoryException {
 
-        // create a tree
-        TreeModel treeModel = convertToTreeModel(l1);
+        ClientRepositoryFactory repositoryFactory = new org.apache.jackrabbit.rmi.client.ClientRepositoryFactory();
+        Repository repositoryConnection = repositoryFactory.getRepository("rmi://localhost:1099/jackrabbit.repository");
+        Session repositorySession = repositoryConnection.login(new SimpleCredentials("username", "password"
+                .toCharArray()));
+
+        //Create the root node and add it's children
+        DefaultMutableTreeNode root = new DefaultMutableTreeNode(repositorySession.getRootNode());
+        addChildren(root);
+
+        //Create the treeModel based on the root node
+        TreeModel treeModel = new DefaultTreeModel(root);
+
+        //Create the treeComponent based on the treeModel
         final Tree tree = new Tree("tree", treeModel) {
             private static final long serialVersionUID = 1L;
 
             protected String renderNode(TreeNode node) {
                 DefaultMutableTreeNode treeNode = (DefaultMutableTreeNode) node;
-                Object userObject = treeNode.getUserObject();
-                return (userObject instanceof List) ? "<subtree>" : String.valueOf(treeNode.getUserObject());
+                Node jcrNode = (Node) treeNode.getUserObject();
+                String result;
+                try {
+                    result = jcrNode.getName();
+                } catch (RepositoryException e) {
+                    result = e.getMessage();
+                }
+                return result;
             }
         };
+        //Collapse the treeComponent
+        tree.getTreeState().collapseAll();
 
-        // disable ajax links in this example
+        //Add behaviour to the treeComponent
+        tree.getTreeState().addTreeStateListener(this);
+
+        //Disable ajax links for the time being
         tree.setLinkType(LinkType.REGULAR);
 
+        //Add the treeComponent to the webPage
         add(tree);
     }
 
-    private TreeModel convertToTreeModel(List list) {
-        TreeModel model = null;
-        DefaultMutableTreeNode rootNode = new DefaultMutableTreeNode("<root>");
-        add(rootNode, list);
-        model = new DefaultTreeModel(rootNode);
-        return model;
-    }
+    private void addChildren(DefaultMutableTreeNode treeNode) throws RepositoryException {
+        Node jcrNode = (Node) treeNode.getUserObject();
 
-    private void add(DefaultMutableTreeNode parent, List sub) {
-        for (Iterator i = sub.iterator(); i.hasNext();) {
-            Object o = i.next();
-            if (o instanceof List) {
-                DefaultMutableTreeNode child = new DefaultMutableTreeNode(o);
-                parent.add(child);
-                add(child, (List) o);
-            } else {
-                DefaultMutableTreeNode child = new DefaultMutableTreeNode(o);
-                parent.add(child);
+        for (NodeIterator iter = jcrNode.getNodes(); iter.hasNext();) {
+            Node node = iter.nextNode();
+            if (!node.getPath().equals("/jcr:system")) {
+                treeNode.add(new DefaultMutableTreeNode(node));
             }
         }
+    }
+
+    // Behaviour of the treeComponent
+
+    public void nodeExpanded(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+        Enumeration children = node.children();
+        while (children.hasMoreElements()) {
+            DefaultMutableTreeNode child = (DefaultMutableTreeNode) children.nextElement();
+            try {
+                addChildren(child);
+            } catch (RepositoryException e) {
+                System.out.println("Exception while expanding node " + e.getMessage());
+            }
+        }
+    }
+
+    public void nodeCollapsed(TreeNode node) {
+    }
+
+    public void nodeSelected(TreeNode node) {
+        if (node != null) {
+            DefaultMutableTreeNode treeNode = (DefaultMutableTreeNode) node;
+            Node jcrNode = (Node) treeNode.getUserObject();
+
+            StringBuffer text = new StringBuffer();
+            try {
+                for (PropertyIterator iter = jcrNode.getProperties(); iter.hasNext();) {
+                    Property prop = iter.nextProperty();
+
+                    text.append(prop.getPath() + " [name=" + prop.getName() + "] = ");
+                    if (prop.getDefinition().isMultiple()) {
+                        Value[] values = prop.getValues();
+                        text.append("[ ");
+                        for (int i = 0; i < values.length; i++) {
+                            text.append((i > 0 ? ", " : "") + values[i].getString());
+                        }
+                        text.append(" ]");
+                    } else {
+                        text.append(prop.getString());
+                    }
+                }
+            } catch (ValueFormatException e) {
+                text.append(e.getMessage());
+            } catch (IllegalStateException e) {
+                text.append(e.getMessage());
+            } catch (RepositoryException e) {
+                text.append(e.getMessage());
+            }
+//            System.out.println(text.toString());
+//            add(new Label("properties", text.toString()));
+        }
+    }
+
+    public void nodeUnselected(TreeNode node) {
+    }
+
+    public void allNodesExpanded() {
+    }
+
+    public void allNodesCollapsed() {
     }
 
 }
