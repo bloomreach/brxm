72fd75de0a753909be64de6fcc4b621d06d8fbdd
Bart van der Schans <b.vanderschans@onehippo.com>
2007-07-26 13:33:11 +0000

    HREPTWO-35: Make the repository.xml file location configurable - for the embedded webapp it's possible to set the repostiory-config in web.xml or set the repo.config System propery - for the standalone it's possible to define the repo.config System propery
    svn path=/hippo-repository-2/trunk/; revision=7396

--- cms/hippo-repository-2-core/src/main/java/org/hippocms/repository/jr/embedded/LocalHippoRepository.java
+++ cms/hippo-repository-2-core/src/main/java/org/hippocms/repository/jr/embedded/LocalHippoRepository.java
@@ -15,7 +15,11 @@
  */
 package org.hippocms.repository.jr.embedded;
 
+import java.io.BufferedInputStream;
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
