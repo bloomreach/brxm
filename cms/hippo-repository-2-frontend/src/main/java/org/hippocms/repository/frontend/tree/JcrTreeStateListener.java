5dba22483d2b9256e7e12a8e4fa6682ecdc462dc
Niels van Kampenhout <n.vankampenhout@onehippo.com>
2007-07-26 13:16:18 +0000

    HREPTWO-39: refactored ITreeStateListener implementation into separate class JcrTreeStateListener to make it reusable
    svn path=/hippo-repository-2/trunk/; revision=7388

---
+++ cms/hippo-repository-2-frontend/src/main/java/org/hippocms/repository/frontend/tree/JcrTreeStateListener.java
@@ -0,0 +1,60 @@
+package org.hippocms.repository.frontend.tree;
+
+import javax.jcr.Node;
+import javax.jcr.NodeIterator;
+import javax.jcr.RepositoryException;
+import javax.swing.tree.TreeNode;
+
+import org.apache.wicket.IClusterable;
+import org.apache.wicket.markup.html.tree.ITreeStateListener;
+import org.hippocms.repository.frontend.model.JcrNodeModel;
+
+public class JcrTreeStateListener implements ITreeStateListener, IClusterable {
+	private static final long serialVersionUID = 1L;
+
+	public void nodeExpanded(TreeNode treeNodeModel) {
+        JcrNodeModel nodeModel = (JcrNodeModel) treeNodeModel;
+        Node node = nodeModel.getNode();
+        if (node != null) {
+            try {
+                for (NodeIterator iter = node.getNodes(); iter.hasNext();) {
+                    Node childNode = iter.nextNode();
+                    JcrNodeModel childNodeModel = new JcrNodeModel(childNode);
+                    nodeModel.add(childNodeModel);
+                }
+            } catch (RepositoryException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void nodeCollapsed(TreeNode treeNodeModel) {
+        if (treeNodeModel == null) {
+            return;
+        }
+        JcrNodeModel nodeModel = (JcrNodeModel) treeNodeModel;
+        nodeModel.removeAllChildren();
+    }
+
+	public void allNodesCollapsed() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void allNodesExpanded() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void nodeSelected(TreeNode node) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void nodeUnselected(TreeNode node) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
