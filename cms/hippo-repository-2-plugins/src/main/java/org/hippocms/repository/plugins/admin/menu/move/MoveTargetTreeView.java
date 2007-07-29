35c1c570ad40ea73c3c129b1463c9d45d3d4c7f2
Walter Grevink <w.grevink@onehippo.com>
2007-07-29 14:14:52 +0000

    HREPTWO-47 Got rid of IUpdatable interface. It got in the way
    svn path=/hippo-repository-2/trunk/; revision=7425

--- cms/hippo-repository-2-plugins/src/main/java/org/hippocms/repository/plugins/admin/menu/move/MoveTargetTreeView.java
+++ cms/hippo-repository-2-plugins/src/main/java/org/hippocms/repository/plugins/admin/menu/move/MoveTargetTreeView.java
@@ -4,23 +4,22 @@ import javax.swing.tree.TreeModel;
 import javax.swing.tree.TreeNode;
 
 import org.apache.wicket.ajax.AjaxRequestTarget;
-import org.hippocms.repository.frontend.IUpdatable;
 import org.hippocms.repository.frontend.model.JcrNodeModel;
 import org.hippocms.repository.frontend.tree.JcrTree;
 
 public class MoveTargetTreeView extends JcrTree {
     private static final long serialVersionUID = 1L;
     private TreeNode selectedNode;
-    private IUpdatable updatable;
+    private MoveDialog dialog;
 
-    public MoveTargetTreeView(String id, TreeModel treeModel, IUpdatable updatable) {
+    public MoveTargetTreeView(String id, TreeModel treeModel, MoveDialog dialog) {
         super(id, treeModel);
-        this.updatable = updatable;
+        this.dialog = dialog;
     }
 
     protected void onNodeLinkClicked(AjaxRequestTarget target, TreeNode treeNode) {
         selectedNode = treeNode;
-        updatable.update(target, (JcrNodeModel) treeNode);
+        dialog.update(target, (JcrNodeModel) treeNode);
     }
 
     public TreeNode getSelectedNode() {
