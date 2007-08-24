cb80a4185480449a45b904af9ff9b17a24f030fe
Walter Grevink <w.grevink@onehippo.com>
2007-08-24 12:25:15 +0000

    HREPTWO-58 (almost) finished
    1> Introducing 'HistoryWriter', a junit testdecorator that enables tests to write performance metrics to a historypoints file.
    2> Introducing the history:report maven plugin that generates a nicely formatted report based on these historypoint files
    
    Both need the system property 'history.points' set to the same directory (1) for writing historypoints and (2) for reading them.
    
    svn path=/hippo-repository-2/trunk/; revision=7999

--- repository/hippo-repository-2-testutils/historytest-utils/src/main/java/org/hippoecm/repository/testutils/history/HistoryWriter.java
+++ repository/hippo-repository-2-testutils/historytest-utils/src/main/java/org/hippoecm/repository/testutils/history/HistoryWriter.java
@@ -20,158 +20,152 @@ import junit.framework.TestResult;
 
 import org.hippoecm.repository.testutils.history.myXML.myXMLException;
 
-
 public class HistoryWriter extends TestSetup implements TestListener, XmlConstants {
 
-  private File historyFile;
-  
-  private myXML testSuite;
-  private myXML testCase;  
-  
-  private Set loggedTo;
-  private String name;
-  private long timeStamp;
-  
-  private Configuration config;
+    private File historyFile;
 
- 
-  public HistoryWriter(Test test) {
-    super(test);
-    this.name = getName(test);
-    this.timeStamp = System.currentTimeMillis();
-    this.config = Configuration.getInstance();
-  }
-  
-  
-  public String getName() {
-    return name;
-  }
+    private myXML testSuite;
+    private myXML testCase;
 
+    private Set loggedTo;
+    private String name;
+    private long timeStamp;
 
-  public void run(final TestResult result) {
-    result.addListener(this);
-    super.run(result);
-  }
+    private String history;
 
-
-  public void setUp() {
-    try {
-      historyFile = new File(config.getHistoryDir(), getName() + "-history.xml");
-      
-      if (historyFile.exists()) {
-        testSuite = new myXML(new BufferedReader(new FileReader(historyFile)));
-      } else {
-        testSuite = new myXML(TESTSUITE);
-      }
-      testSuite.Attribute.add(ATTR_CLASSNAME, getName());
-    } catch (Exception e) {
-      System.err.println("HistoryWriter.setUp failed: " + e.getMessage());
+    public HistoryWriter(Test test) {
+        super(test);
+        this.name = getName(test);
+        this.timeStamp = System.currentTimeMillis();
+        this.history = System.getProperty("history.points");
+        System.out.println("history: " + history);
     }
-  }
 
-
-  public void startTest(Test test) {
-    try {
-      testCase = testSuite.findElement(TESTCASE, ATTR_NAME, getName(test));
-      if (testCase == null) {
-        testCase = testSuite.addElement(TESTCASE);
-      }
-      testCase.Attribute.add(ATTR_NAME, getName(test));
-    } catch (myXMLException e) {
-      System.err.println("Failed to add testcase data to testsuite: " + e.getMessage());
+    public String getName() {
+        return name;
     }
-    loggedTo = new HashSet();
-  }
 
-
-  public void endTest(Test test) {    
-    if (testCase.isEmpty()) {
-      try {
-        testSuite.removeElement(testCase);
-      } catch (myXMLException e) {
-        System.err.println("Unable to remove empty testcase " + getName(test) + " from suite " + getName());
-      }
-    }    
-  }
-
-
-  public void tearDown() {
-    writeToHistoryFile();
-    new ChartsCreator(testSuite).execute();
-  }
-
-
-  public void write(String name, String value, String unit) {
-    write(name, value, unit, true);
-  }
-
-
-  public void write(String name, String value, String unit, boolean fuzzy) {
-    if (loggedTo != null) {
-      if (!loggedTo.add(name)) {
-        System.out.println("Duplicate log name: " + name
-            + ", it is not allowed to log to the same name more than once in one testmethod.");
-        return;
-      }
-      
-      try {
-        myXML metric = testCase.findElement(METRIC, ATTR_NAME, name);
-        if (metric == null) {
-          metric = testCase.addElement(METRIC);
+    public void run(final TestResult result) {
+        if (history != null) {
+            result.addListener(this);
         }
-        metric.Attribute.add(ATTR_NAME, name);
-        metric.Attribute.add(ATTR_UNIT, unit);
-        metric.Attribute.add(ATTR_FUZZY, String.valueOf(fuzzy));
-        
-        myXML measurePoint = metric.addElement(MEASUREPOINT);
-        measurePoint.Attribute.add(ATTR_TIMESTAMP, String.valueOf(timeStamp));
-        measurePoint.Attribute.add(ATTR_VALUE, value);
-      } catch (myXMLException e) {
-        System.err.println("Failed to add testcase data to testsuite: " + e.getMessage());
-      }
+        super.run(result);
     }
-  }
-  
-  
-  public void addError(Test test, Throwable t) {
-  }
 
-  public void addFailure(Test test, AssertionFailedError t) {
-  }
-
-  private void writeToHistoryFile() {
-    OutputStream out = null;
-    try {
-      historyFile.getParentFile().mkdirs();
-      out = new FileOutputStream(historyFile);
-    } catch (IOException e) {
-      System.err.println("Cannot open file " + historyFile.getAbsolutePath());
-    }
-    if (out != null) {
-      PrintWriter writer = null;
-      try {
-        writer = new PrintWriter(new OutputStreamWriter(out, "UTF8"));
-        testSuite.serialize(writer);
-      } catch (IOException exc) {
-        System.err.println("Unable to history file" + exc.getMessage());
-      } finally {
-        if (out != System.out && out != System.err && writer != null) {
-          writer.close();
+    public void setUp() {
+        if (history != null) {
+            try {
+                historyFile = new File(history, getName() + "-history.xml");
+                if (historyFile.exists()) {
+                    testSuite = new myXML(new BufferedReader(new FileReader(historyFile)));
+                } else {
+                    testSuite = new myXML(TESTSUITE);
+                }
+                testSuite.Attribute.add(ATTR_CLASSNAME, getName());
+            } catch (Exception e) {
+                System.err.println("HistoryWriter.setUp failed: " + e.getMessage());
+            }
         }
-      }
     }
-  }
 
-  private String getName(Test t) {
-    try {
-       Method getNameMethod = t.getClass().getMethod("getName", new Class [0]);
-        if (getNameMethod != null && getNameMethod.getReturnType() == String.class) {
-            return (String) getNameMethod.invoke(t, new Object[0]);
+    public void tearDown() {
+        if (history != null) {
+            OutputStream out = null;
+            try {
+                historyFile.getParentFile().mkdirs();
+                out = new FileOutputStream(historyFile);
+            } catch (IOException e) {
+                System.err.println("Cannot open file " + historyFile.getAbsolutePath());
+            }
+            if (out != null) {
+                PrintWriter writer = null;
+                try {
+                    writer = new PrintWriter(new OutputStreamWriter(out, "UTF8"));
+                    testSuite.serialize(writer);
+                } catch (IOException exc) {
+                    System.err.println("Unable to history file" + exc.getMessage());
+                } finally {
+                    if (out != System.out && out != System.err && writer != null) {
+                        writer.close();
+                    }
+                }
+            }
         }
-    } catch (Throwable e) {
-        // ignore
     }
-    return "unknown";
-  }
+
+    public void write(String name, String value, String unit) {
+        write(name, value, unit, true);
+    }
+
+    public void write(String name, String value, String unit, boolean fuzzy) {
+        if (loggedTo != null) {
+            if (!loggedTo.add(name)) {
+                System.out.println("Duplicate log name: " + name
+                        + ", it is not allowed to log to the same name more than once in one testmethod.");
+                return;
+            }
+
+            try {
+                myXML metric = testCase.findElement(METRIC, ATTR_NAME, name);
+                if (metric == null) {
+                    metric = testCase.addElement(METRIC);
+                }
+                metric.Attribute.add(ATTR_NAME, name);
+                metric.Attribute.add(ATTR_UNIT, unit);
+                metric.Attribute.add(ATTR_FUZZY, String.valueOf(fuzzy));
+
+                myXML measurePoint = metric.addElement(MEASUREPOINT);
+                measurePoint.Attribute.add(ATTR_TIMESTAMP, String.valueOf(timeStamp));
+                measurePoint.Attribute.add(ATTR_VALUE, value);
+            } catch (myXMLException e) {
+                System.err.println("Failed to add testcase data to testsuite: " + e.getMessage());
+            }
+        }
+    }
+    
+    // interface TestListener
+
+    public void startTest(Test test) {
+        try {
+            testCase = testSuite.findElement(TESTCASE, ATTR_NAME, getName(test));
+            if (testCase == null) {
+                testCase = testSuite.addElement(TESTCASE);
+            }
+            testCase.Attribute.add(ATTR_NAME, getName(test));
+        } catch (myXMLException e) {
+            System.err.println("Failed to add testcase data to testsuite: " + e.getMessage());
+        }
+        loggedTo = new HashSet();
+    }
+
+    public void endTest(Test test) {
+        if (testCase.isEmpty()) {
+            try {
+                testSuite.removeElement(testCase);
+            } catch (myXMLException e) {
+                System.err.println("Unable to remove empty testcase " + getName(test) + " from suite " + getName());
+            }
+        }
+    }
+
+    public void addError(Test test, Throwable t) {
+    }
+
+    public void addFailure(Test test, AssertionFailedError t) {
+    }
+    
+    // privates
+
+    private String getName(Test t) {
+        try {
+            Method getNameMethod = t.getClass().getMethod("getName", new Class[0]);
+            if (getNameMethod != null && getNameMethod.getReturnType() == String.class) {
+                return (String) getNameMethod.invoke(t, new Object[0]);
+            }
+        } catch (Exception e) {
+            // ignore
+        }
+        return "unknown";
+    }
 
 }
---
+++ repository/hippo-repository-2-testutils/historytest-utils/src/main/java/org/hippoecm/repository/testutils/history/PrepareReport.java
@@ -0,0 +1,48 @@
+package org.hippoecm.repository.testutils.history;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import org.apache.tools.ant.Task;
+
+//Hack to copy the junit-frames.xsl to the current project,
+//so that the junit-report task can use it.
+public class PrepareReport extends Task {
+
+    public void execute() {
+        InputStream from = getClass().getResourceAsStream("/junit-frames.xsl");
+        File toFile = new File("target/junit-frames.xsl");
+        
+        OutputStream to = null;
+        try {
+            to = new FileOutputStream(toFile);
+            byte[] buffer = new byte[4096];
+            int bytesRead;
+
+            while ((bytesRead = from.read(buffer)) != -1) {
+                to.write(buffer, 0, bytesRead);
+            }
+        } catch (IOException e) {
+            System.err.println(e);
+        } finally {
+            if (from != null) {
+                try {
+                    from.close();
+                } catch (IOException e) {
+                    System.err.println(e);
+                }
+            }
+            if (to != null) {
+                try {
+                    to.close();
+                } catch (IOException e) {
+                    System.err.println(e);
+                }
+            }
+        }
+    }
+
+}
