cd92c4da1b02a02fc40233cd5326a8cdfdd2e2a8
Berry van Halderen <b.vanhalderen@onehippo.com>
2007-09-20 10:11:35 +0000

    HREPTWO-16: not my code
    svn path=/hippo-repository-2/trunk/; revision=8240

--- cms/core/src/main/java/org/hippoecm/repository/FacetedNavigationEngineThirdImpl.java
+++ cms/core/src/main/java/org/hippoecm/repository/FacetedNavigationEngineThirdImpl.java
@@ -1,10 +1,4 @@
 /*
-  THIS CODE IS UNDER CONSTRUCTION, please leave as is until
-  work has proceeded to a stable level, at which time this comment
-  will be removed.  -- Berry
-*/ 
-
-/*
  * Copyright 2007 Hippo.
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
