97c4ecc58c06d32ea2e636a0bddeb62618f81bb4
Berry van Halderen <b.vanhalderen@onehippo.com>
2007-09-20 10:03:29 +0000

    HREPTWO-86: implemented remote version of the FacetedNavigationTest
    svn path=/hippo-repository-2/trunk/; revision=8237

---
+++ repository/core/src/test/java/org/hippoecm/repository/FacetedNavigationRemoteTest.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright 2007 Hippo
+ *
+ * Licensed under the Apache License, Version 2.0 (the  "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.hippoecm.repository;
+
+import java.io.IOException;
+
+import javax.jcr.Node;
+import javax.jcr.RepositoryException;
+import javax.jcr.Session;
+
+public class FacetedNavigationRemoteTest extends FacetedNavigationAbstractTest {
+
+    private static final String SYSTEMUSER_ID = "systemuser";
+    private static final char[] SYSTEMUSER_PASSWORD = "systempass".toCharArray();
+
+    private HippoRepositoryServer backgroundServer;
+    private HippoRepository server;
+
+    public void setUp() throws Exception {
+        backgroundServer = new HippoRepositoryServer();
+        backgroundServer.run(true);
+        Thread.sleep(3000);
+        server = HippoRepositoryFactory.getHippoRepository("rmi://localhost:1099/jackrabbit.repository");
+        session = server.login(SYSTEMUSER_ID, SYSTEMUSER_PASSWORD);
+        session.getRootNode().addNode("navigation");
+    }
+
+    public void tearDown() throws Exception {
+        if(session.getRootNode().hasNode("navigation")) {
+            session.getRootNode().getNode("navigation").remove();
+        }
+        if(session.getRootNode().hasNode("documents")) {
+            session.getRootNode().getNode("documents").remove();
+        }
+        session.save();
+        if(session != null) {
+            session.logout();
+        }
+        if (server != null) {
+            server.close();
+        }
+        backgroundServer.close();
+        Thread.sleep(3000);
+    }
+
+    public void testTraversal() throws RepositoryException, IOException {
+        Node node = commonStart();
+        traverse(node); // for a full verbose dump use: Utilities.dump(root);
+        commonEnd();
+    }
+
+    public void testCounts() throws RepositoryException, IOException {
+        numDocs = 500;
+        Node node = commonStart();
+        check("/navigation/xyz/x1", 1, 0, 0);
+        check("/navigation/xyz/x2", 2, 0, 0);
+        check("/navigation/xyz/x1/y1", 1, 1, 0);
+        check("/navigation/xyz/x1/y2", 1, 2, 0);
+        check("/navigation/xyz/x2/y1", 2, 1, 0);
+        check("/navigation/xyz/x2/y2", 2, 2, 0);
+        check("/navigation/xyz/x1/y1/z1", 1, 1, 1);
+        check("/navigation/xyz/x1/y1/z2", 1, 1, 2);
+        check("/navigation/xyz/x1/y2/z1", 1, 2, 1);
+        check("/navigation/xyz/x1/y2/z2", 1, 2, 2);
+        check("/navigation/xyz/x2/y1/z1", 2, 1, 1);
+        check("/navigation/xyz/x2/y1/z2", 2, 1, 2);
+        check("/navigation/xyz/x2/y2/z1", 2, 2, 1);
+        check("/navigation/xyz/x2/y2/z2", 2, 2, 2);
+        commonEnd();
+    }
+}
