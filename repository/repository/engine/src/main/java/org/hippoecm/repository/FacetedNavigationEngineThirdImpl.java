30e655ad526397cbd8cca12bd7e189b4fa2760ac
Ard Schrijvers <a.schrijvers@onehippo.com>
2007-09-25 20:43:34 +0000

    HREPTWO-81 fixes for  HIPPO_RESULTSET
    svn path=/hippo-ecm/trunk/; revision=8317

--- repository/repository/engine/src/main/java/org/hippoecm/repository/FacetedNavigationEngineThirdImpl.java
+++ repository/repository/engine/src/main/java/org/hippoecm/repository/FacetedNavigationEngineThirdImpl.java
@@ -33,6 +33,7 @@ import org.apache.lucene.search.BooleanQuery;
 import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.TermQuery;
 import org.apache.lucene.search.BooleanClause.Occur;
+import org.hippoecm.repository.api.HippoNodeType;
 import org.hippoecm.repository.query.lucene.AuthorizationQuery;
 import org.hippoecm.repository.query.lucene.FacetPropExistsQuery;
 import org.hippoecm.repository.query.lucene.FacetResultCollector;
@@ -41,8 +42,8 @@ import org.hippoecm.repository.query.lucene.ServicingFieldNames;
 import org.hippoecm.repository.query.lucene.ServicingIndexingConfiguration;
 import org.hippoecm.repository.query.lucene.ServicingSearchIndex;
 import org.hippoecm.repository.servicing.RepositoryDecorator;
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class FacetedNavigationEngineThirdImpl
   implements FacetedNavigationEngine<FacetedNavigationEngineThirdImpl.QueryImpl, FacetedNavigationEngineThirdImpl.ContextImpl>
@@ -91,7 +92,7 @@ public class FacetedNavigationEngineThirdImpl
   private Map<IndexReader, Map<String,Map<Integer, String[]>>> tfvCache ;
   
   /** The logger instance for this class */
-  //private static final Logger log = LoggerFactory.getLogger(FacetedNavigationEngineThirdImpl.class);
+  private static final Logger log = LoggerFactory.getLogger(FacetedNavigationEngineThirdImpl.class);
 
   
   public FacetedNavigationEngineThirdImpl() {
@@ -155,6 +156,21 @@ public class FacetedNavigationEngineThirdImpl
                                                                      nsMappings, 
                                                                      (ServicingIndexingConfiguration)index.getIndexingConfig(),
                                                                      true); 
+      
+      BooleanQuery searchQuery = new BooleanQuery();
+      
+      
+      if(facetsQuery.getQuery().clauses().size() > 0){
+          searchQuery.add(facetsQuery.getQuery(), Occur.MUST);
+      }
+      // TODO perhaps create cached user specific filter for authorisation to gain speed
+      if(authorizationQuery.getQuery().clauses().size() > 0){
+          searchQuery.add(authorizationQuery.getQuery(), Occur.MUST);
+      }
+      
+      if(initialLuceneQuery != null){
+          searchQuery.add(initialLuceneQuery, Occur.MUST);
+      }
  
       FacetResultCollector collector = null; 
       IndexReader indexReader = null;
@@ -164,35 +180,26 @@ public class FacetedNavigationEngineThirdImpl
           searcher = new IndexSearcher(indexReader);
       
           // In principle, below, there is always one facet 
-          for(String facet : resultset.keySet()) {
-              /*
-               * facetPropExists: the document must have the property as facet
-               */
-              FacetPropExistsQuery facetPropExists = new FacetPropExistsQuery(facet, nsMappings, (ServicingIndexingConfiguration)index.getIndexingConfig());
-              
-              BooleanQuery searchQuery = new BooleanQuery();
-              searchQuery.add(facetPropExists.getQuery(), Occur.MUST);
-              
-              if(facetsQuery.getQuery().clauses().size() > 0){
-                  searchQuery.add(facetsQuery.getQuery(), Occur.MUST);
-              }
-              // TODO perhaps create cached user specific filter for authorisation to gain speed
-              if(authorizationQuery.getQuery().clauses().size() > 0){
-                  searchQuery.add(authorizationQuery.getQuery(), Occur.MUST);
-              }
-              
-              if(initialLuceneQuery != null){
-                  searchQuery.add(initialLuceneQuery, Occur.MUST);
-              }
-              
+          if(resultset != null){
+              for(String facet : resultset.keySet()) {
+                  /*
+                   * facetPropExists: the document must have the property as facet
+                   */
+                  FacetPropExistsQuery facetPropExists = new FacetPropExistsQuery(facet, nsMappings, (ServicingIndexingConfiguration)index.getIndexingConfig());
+                  searchQuery.add(facetPropExists.getQuery(), Occur.MUST);
+                 
+                  long start = System.currentTimeMillis();
+                  collector = new FacetResultCollector(indexReader, facet, resultset, hitsRequested, nsMappings);        
+                  searcher.search(searchQuery, collector);
+                  log.debug("lucene query: " + searchQuery.toString() + " took " +(System.currentTimeMillis() - start) + " ms for " + collector.getNumhits() +" results"); 
+              } 
+          } else {
+              // resultset is null, so search for HippoNodeType.HIPPO_RESULTSET
               long start = System.currentTimeMillis();
-              collector = new FacetResultCollector(indexReader, facet, resultset, hitsRequested, nsMappings);        
+              collector = new FacetResultCollector(indexReader, null, null, hitsRequested, nsMappings);        
               searcher.search(searchQuery, collector);
-              //System.out.println("lucene query: " + searchQuery.toString() + " took " +(System.currentTimeMillis() - start) + " ms for " + collector.getNumhits() +" results");
-             // log.debug("lucene query: " + searchQuery.toString() + " took " +(System.currentTimeMillis() - start) + " ms for " + collector.getNumhits() +" results"); 
-              //System.out.println("lucene query: " + searchQuery.toString() + " took " +(System.currentTimeMillis() - start) ); 
-              
-          } 
+              log.debug("lucene query: " + searchQuery.toString() + " took " +(System.currentTimeMillis() - start) + " ms for " + collector.getNumhits() +" results"); 
+          }
           
       } catch (IOException e) {
           e.printStackTrace();
