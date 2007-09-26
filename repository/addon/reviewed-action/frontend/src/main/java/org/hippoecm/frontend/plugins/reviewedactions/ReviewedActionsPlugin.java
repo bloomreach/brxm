9457b32b662ecc2eb0e1ebeca1f4f45e675d6775
Walter Grevink <w.grevink@onehippo.com>
2007-09-26 13:54:43 +0000

    HREPTWO-89
    Moved plugin update- and reconfigure logic from RootPlugin up to Home
    
    Removed the panel with wicketId 'workflowPlugin' from the minimal and default frontend apps since they haven't any configured workflows
    Created Rootplugin java+html file in addon-reviewed-action-frontend that does define a panel with wicketId 'workflowPlugin'
    
    This means that the 'reviewed action' application now comes with it's own RootPlugin defining an extra workflowPlugin panel
    
    Tuned logging in WorkflowManager
    
    svn path=/hippo-ecm/trunk/; revision=8322

--- repository/addon/reviewed-action/frontend/src/main/java/org/hippoecm/frontend/plugins/reviewedactions/ReviewedActionsPlugin.java
+++ repository/addon/reviewed-action/frontend/src/main/java/org/hippoecm/frontend/plugins/reviewedactions/ReviewedActionsPlugin.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.hippoecm.frontend.plugins.reviewedaction;
+package org.hippoecm.frontend.plugins.reviewedactions;
 
 import javax.jcr.RepositoryException;
 
