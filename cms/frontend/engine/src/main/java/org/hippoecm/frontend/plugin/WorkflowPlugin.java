5feda95e3e35efe3515dedb29c5804856893e971
Walter Grevink <w.grevink@onehippo.com>
2007-09-27 11:18:02 +0000

    HREPTWO-89 Added ReviewedActionsWorkflowPlugin and RequestWorkflowPlugin (Frontend implementations of the Reviewed actions workflow)
    svn path=/hippo-ecm/trunk/; revision=8326

--- cms/frontend/engine/src/main/java/org/hippoecm/frontend/plugin/WorkflowPlugin.java
+++ cms/frontend/engine/src/main/java/org/hippoecm/frontend/plugin/WorkflowPlugin.java
@@ -15,15 +15,30 @@
  */
 package org.hippoecm.frontend.plugin;
 
+import java.rmi.RemoteException;
+
+import javax.jcr.RepositoryException;
+
+import org.apache.wicket.Component;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.markup.html.AjaxLink;
+import org.apache.wicket.markup.html.basic.Label;
 import org.hippoecm.frontend.model.JcrNodeModel;
+import org.hippoecm.repository.api.Workflow;
 import org.hippoecm.repository.api.WorkflowDescriptor;
+import org.hippoecm.repository.api.WorkflowException;
 import org.hippoecm.repository.api.WorkflowManager;
+import org.hippoecm.repository.api.WorkflowMappingException;
 
 public abstract class WorkflowPlugin extends Plugin {
 
     //TODO:  wrap these in detachable models instead of making them transient
     private transient WorkflowManager workflowManager;
     private transient WorkflowDescriptor workflowDescriptor;
+    
+    protected interface Callback {
+        void execute(Workflow workflow) throws WorkflowMappingException, RemoteException, WorkflowException, RepositoryException;
+    }
 
     public WorkflowPlugin(String id, JcrNodeModel model, WorkflowManager workflowManager, WorkflowDescriptor workflowDescriptor) {
         super(id, model);
@@ -37,5 +52,49 @@ public abstract class WorkflowPlugin extends Plugin {
 
     public WorkflowDescriptor getWorkflowDescriptor() {
         return workflowDescriptor;
+    }
+    
+    protected Workflow getWorkflow() {
+        Workflow workflow = null;
+        try {
+            workflow = getWorkflowManager().getWorkflow(workflowDescriptor);
+        } catch (WorkflowMappingException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (RepositoryException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return workflow;
+    }
+    
+    protected Component workflowMethodCaller(String id, final JcrNodeModel model, String label, final Callback callback) {
+        Component result = null;
+        if (model == null || model.getNode() == null) {
+            //TODO: ask permission to execute this method, if not allowed show this label 
+            result = new Label(id, label);
+        } else {
+            result = new AjaxLink(id, model) {
+                private static final long serialVersionUID = 1L;
+                public void onClick(AjaxRequestTarget target) {
+                    try {
+                        callback.execute(getWorkflow());
+                    } catch (WorkflowMappingException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    } catch (RemoteException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    } catch (WorkflowException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    } catch (RepositoryException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                }
+            };
+        }
+        return result;   
     }
 }
