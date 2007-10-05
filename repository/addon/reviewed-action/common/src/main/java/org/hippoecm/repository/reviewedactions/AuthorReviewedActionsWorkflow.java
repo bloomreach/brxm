8424c99f01d17cc0cad22eaf7d528421546739bd
Berry van Halderen <b.vanhalderen@onehippo.com>
2007-10-05 12:30:23 +0000

    HREPTWO-129: duplication of reviewed actions workflow for author and editor perspective
    svn path=/hippo-ecm/trunk/; revision=8467

---
+++ repository/addon/reviewed-action/common/src/main/java/org/hippoecm/repository/reviewedactions/AuthorReviewedActionsWorkflow.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright 2007 Hippo
+ *
+ * Licensed under the Apache License, Version 2.0 (the  "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.hippoecm.repository.reviewedactions;
+
+import java.util.Date;
+import java.rmi.RemoteException;
+
+import javax.jcr.Node;
+import javax.jcr.RepositoryException;
+
+import org.hippoecm.repository.api.Workflow;
+import org.hippoecm.repository.api.WorkflowException;
+import org.hippoecm.repository.api.WorkflowMappingException;
+
+public interface AuthorReviewedActionsWorkflow extends Workflow {
+    /**
+     * Request this editable copy of the document.
+     */
+    public void obtainEditableInstance()
+        throws WorkflowException, WorkflowMappingException, RepositoryException, RemoteException;
+
+    /**
+     * Do away with the editable copy of the document which was previously
+     * obtained.
+     */
+    public void disposeEditableInstance()
+        throws WorkflowException, WorkflowMappingException, RepositoryException, RemoteException;
+
+    /**
+     * Request unpublication and deletion of document.
+     */
+    public void requestDeletion()
+        throws WorkflowException, WorkflowMappingException, RepositoryException, RemoteException;
+
+    /**
+     * Request unpublication.
+     */
+    public void requestDepublication()
+        throws WorkflowException, WorkflowMappingException, RepositoryException, RemoteException;
+
+    /**
+     * Request unpublication at given date.
+     */
+    public void requestDepublication(Date publicationDate)
+        throws WorkflowException, WorkflowMappingException, RepositoryException, RemoteException;
+
+    /**
+     * Request for this instance of the document to be published.
+     */
+    public void requestPublication()
+        throws WorkflowException, WorkflowMappingException, RepositoryException, RemoteException;
+
+    /**
+     * Request for this instance of the document to be published at the given
+     * date.
+     */
+    public void requestPublication(Date publicationDate)
+        throws WorkflowException, WorkflowMappingException, RepositoryException, RemoteException;
+
+    /**
+     * Request for this instance of the document to be published at the given
+     * date and to be scheduled for unpublication.
+     */
+    public void requestPublication(Date publicationDate, Date unpublicationDate)
+        throws WorkflowException, WorkflowMappingException, RepositoryException, RemoteException;
+}
---
+++ repository/addon/reviewed-action/frontend/src/main/java/org/hippoecm/frontend/plugins/reviewedactions/AuthorReviewedActionsWorkflowPlugin.java
@@ -0,0 +1,99 @@
+/*
+ * Copyright 2007 Hippo
+ *
+ * Licensed under the Apache License, Version 2.0 (the  "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.hippoecm.frontend.plugins.reviewedactions;
+
+import org.apache.wicket.Page;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.extensions.ajax.markup.html.modal.ModalWindow;
+import org.hippoecm.frontend.dialog.DialogWindow;
+import org.hippoecm.frontend.model.JcrNodeModel;
+import org.hippoecm.frontend.plugin.WorkflowPlugin;
+import org.hippoecm.frontend.plugins.reviewedactions.authordialogs.delete.DeleteDialog;
+import org.hippoecm.frontend.plugins.reviewedactions.authordialogs.depublish.DePublishDialog;
+import org.hippoecm.frontend.plugins.reviewedactions.authordialogs.disposeeditableinstance.DisposeEditableInstanceDialog;
+import org.hippoecm.frontend.plugins.reviewedactions.authordialogs.obtaineditableinstance.ObtainEditableInstanceDialog;
+import org.hippoecm.frontend.plugins.reviewedactions.authordialogs.publish.PublishDialog;
+import org.hippoecm.frontend.plugins.reviewedactions.authordialogs.requestdeletion.RequestDeletionDialog;
+import org.hippoecm.frontend.plugins.reviewedactions.authordialogs.requestdepublication.RequestDePublicationDialog;
+import org.hippoecm.frontend.plugins.reviewedactions.authordialogs.requestpublication.RequestPublicationDialog;
+import org.hippoecm.repository.api.WorkflowDescriptor;
+import org.hippoecm.repository.api.WorkflowManager;
+import org.hippoecm.repository.reviewedactions.ReviewedActionsWorkflow;
+import org.hippoecm.repository.reviewedactions.AuthorReviewedActionsWorkflow;
+
+public class AuthorReviewedActionsWorkflowPlugin extends WorkflowPlugin {
+    private static final long serialVersionUID = 1L;
+
+    public AuthorReviewedActionsWorkflowPlugin(String id, final JcrNodeModel model, WorkflowManager workflowManager,
+            WorkflowDescriptor workflowDescriptor) {
+        super(id, model, workflowManager, workflowDescriptor);
+
+        final DialogWindow obtainEditableInstanceDialog = new DialogWindow("obtainEditableInstance-dialog", model, false);
+        obtainEditableInstanceDialog.setPageCreator(new ModalWindow.PageCreator() {
+            private static final long serialVersionUID = 1L;
+            public Page createPage() {
+                return new ObtainEditableInstanceDialog(obtainEditableInstanceDialog, model, (AuthorReviewedActionsWorkflow) getWorkflow());
+            }
+        });
+        add(obtainEditableInstanceDialog);
+        add(obtainEditableInstanceDialog.dialogLink("obtainEditableInstance"));
+        
+        final DialogWindow disposeEditableInstanceDialog = new DialogWindow("disposeEditableInstance-dialog", model, false);
+        disposeEditableInstanceDialog.setPageCreator(new ModalWindow.PageCreator() {
+            private static final long serialVersionUID = 1L;
+            public Page createPage() {
+                return new DisposeEditableInstanceDialog(disposeEditableInstanceDialog, model, (AuthorReviewedActionsWorkflow) getWorkflow());
+            }
+        });
+        add(disposeEditableInstanceDialog);
+        add(disposeEditableInstanceDialog.dialogLink("disposeEditableInstance"));
+
+        final DialogWindow requestPublicationDialog = new DialogWindow("requestPublication-dialog", model, false);
+        requestPublicationDialog.setPageCreator(new ModalWindow.PageCreator() {
+            private static final long serialVersionUID = 1L;
+            public Page createPage() {
+                return new RequestPublicationDialog(requestPublicationDialog, model, (AuthorReviewedActionsWorkflow) getWorkflow());
+            }
+        });
+        add(requestPublicationDialog);
+        add(requestPublicationDialog.dialogLink("requestPublication"));
+
+        final DialogWindow requestDePublicationDialog = new DialogWindow("requestDePublication-dialog", model, false);
+        requestDePublicationDialog.setPageCreator(new ModalWindow.PageCreator() {
+            private static final long serialVersionUID = 1L;
+            public Page createPage() {
+                return new RequestDePublicationDialog(requestDePublicationDialog, model, (AuthorReviewedActionsWorkflow) getWorkflow());
+            }
+        });
+        add(requestDePublicationDialog);
+        add(requestDePublicationDialog.dialogLink("requestDePublication"));
+
+        final DialogWindow requestDeletionDialog = new DialogWindow("requestDeletion-dialog", model, false);
+        requestDeletionDialog.setPageCreator(new ModalWindow.PageCreator() {
+            private static final long serialVersionUID = 1L;
+            public Page createPage() {
+                return new RequestDeletionDialog(requestDeletionDialog, model, (AuthorReviewedActionsWorkflow) getWorkflow());
+            }
+        });
+        add(requestDeletionDialog);
+        add(requestDeletionDialog.dialogLink("requestDeletion"));
+    }
+
+    public void update(final AjaxRequestTarget target, final JcrNodeModel model) {
+        //Nothing much to do
+    }
+
+}
