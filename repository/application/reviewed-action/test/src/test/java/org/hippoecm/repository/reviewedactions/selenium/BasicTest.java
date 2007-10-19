7d004b5000238e711e4594b660674ee8b57af2ba
Niels van Kampenhout <n.vankampenhout@onehippo.com>
2007-10-19 08:48:13 +0000

    HREPTWO-145: Initial functional GUI test project for reviewed actions application.
    The test subproject is currently commented out in the parent POM to prevent the tests from being run on a "mvn install", as the test environment (a running repository and frontend) is not initialized automatically yet.
    
    svn path=/hippo-ecm/trunk/; revision=8580

---
+++ repository/application/reviewed-action/test/src/test/java/org/hippoecm/repository/reviewedactions/selenium/BasicTest.java
@@ -0,0 +1,149 @@
+/*
+ * Copyright 2007 Hippo
+ *
+ * Licensed under the Apache License, Version 2.0 (the  "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.hippoecm.repository.reviewedactions.selenium;
+
+//import org.hippoecm.testutils.integration.HippoEcmSetup;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+import com.thoughtworks.selenium.DefaultSelenium;
+import com.thoughtworks.selenium.SeleniumException;
+
+public class BasicTest extends TestCase {
+    private DefaultSelenium selenium;
+
+    // TODO implement suite() method returning a TestSetup which starts a repository and frontend
+//    public static Test suite() throws Exception {
+//        Test suite = new TestSuite(BasicTest.class);
+//        HippoEcmSetup setup = new HippoEcmSetup(suite);
+//        return setup;
+//    }
+    
+    @Override
+    public void setUp() throws Exception {
+        selenium = createSeleniumClient("http://localhost:8082/");
+        selenium.start();
+    }
+
+    @Override
+    public void tearDown() throws Exception {
+        selenium.stop();
+    }
+
+    protected DefaultSelenium createSeleniumClient(String url) throws Exception {
+        return new DefaultSelenium("localhost", 4444, "*firefox", url);
+    }
+
+    /**
+     * Checks if the "Add Node" menu action is present on the page.
+     */
+    public void testDummy() throws Exception {
+        try {
+            selenium.open("/");
+            assertEquals("Add Node", selenium.getText("//a[@id='node-dialog-link4']"));
+        } 
+        catch (SeleniumException ex) {
+            fail(ex.getMessage());
+            throw ex;
+        }
+    }
+    
+    /**
+     * Browses through the tree to a document 'my article' and checks if it has the
+     * workflow action "obtain editable instance".
+     */
+    public void testMyArticleHasWorkflow() throws Exception {
+        try {
+            selenium.open("/");
+            selenium.click("//span/span");
+            for (int second = 0;; second++) {
+                if (second >= 60) fail("timeout");
+                try { if (selenium.isElementPresent("//div[6]/div/a[2]/span[2]")) break; } catch (Exception e) {}
+                Thread.sleep(1000);
+            }
+
+            selenium.click("//div[6]/div/a[1]/span/span");
+            for (int second = 0;; second++) {
+                if (second >= 60) fail("timeout");
+                try { if (selenium.isElementPresent("//div[7]/div/a[2]/span[2]")) break; } catch (Exception e) {}
+                Thread.sleep(1000);
+            }
+
+            selenium.click("//div[7]/div/a[1]/span/span");
+            for (int second = 0;; second++) {
+                if (second >= 60) fail("timeout");
+                try { if (selenium.isElementPresent("//div[8]/div/a[2]/span[2]")) break; } catch (Exception e) {}
+                Thread.sleep(1000);
+            }
+
+            selenium.click("//div[8]/div/a[2]/span[2]");
+            for (int second = 0;; second++) {
+                if (second >= 60) fail("timeout");
+                try { if (selenium.isElementPresent("obtainEditableInstance61")) break; } catch (Exception e) {}
+                Thread.sleep(1000);
+            }
+
+            assert(selenium.isTextPresent("Obtain editable copy"));
+        assert(selenium.isTextPresent("Obtain editable copy"));
+        } 
+        catch (SeleniumException ex) {
+            fail(ex.getMessage());
+            throw ex;
+        }
+    }
+    
+    
+    /**
+     * Opens the frontend in a browser and logs in as user "johndoe".
+     */
+    public void testLogin() throws Exception {
+        selenium.open("/");
+        assert(selenium.isTextPresent("Logged in as anonymous"));
+        selenium.click("login-dialog-link21");
+        for (int second = 0;; second++) {
+            if (second >= 60) fail("timeout");
+            try { if (selenium.isElementPresent("ok8")) break; } catch (Exception e) {}
+            Thread.sleep(1000);
+        }
+
+        selenium.click("label4");
+        for (int second = 0;; second++) {
+            if (second >= 60) fail("timeout");
+            try { if (selenium.isElementPresent("editor10")) break; } catch (Exception e) {}
+            Thread.sleep(1000);
+        }
+
+        selenium.type("editor10", "johndoe");
+        selenium.keyPress("editor10", "\\13");
+        selenium.click("label6");
+        for (int second = 0;; second++) {
+            if (second >= 60) fail("timeout");
+            try { if (selenium.isElementPresent("editor11")) break; } catch (Exception e) {}
+            Thread.sleep(1000);
+        }
+
+        selenium.type("editor11", "secret");
+        selenium.keyPress("editor11", "\\13");
+        selenium.click("ok8");
+        for (int second = 0;; second++) {
+            if (second >= 60) fail("timeout");
+            try { if (selenium.isTextPresent("Logged in as johndoe")) break; } catch (Exception e) {}
+            Thread.sleep(1000);
+        }
+    }
+}
