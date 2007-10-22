5ae80d2de6ba5f3f80cddf89e0ae46a91823c285
Niels van Kampenhout <n.vankampenhout@onehippo.com>
2007-10-22 12:30:10 +0000

    HREPTWO-146: logout
    svn path=/hippo-ecm/trunk/; revision=8586

--- repository/frontend/plugins/src/main/java/org/hippoecm/frontend/plugins/admin/login/LoginPlugin.java
+++ repository/frontend/plugins/src/main/java/org/hippoecm/frontend/plugins/admin/login/LoginPlugin.java
@@ -1,13 +1,28 @@
+/*
+ * Copyright 2007 Hippo
+ *
+ * Licensed under the Apache License, Version 2.0 (the  "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.hippoecm.frontend.plugins.admin.login;
 
-import org.apache.wicket.Page;
 import org.apache.wicket.ajax.AjaxRequestTarget;
-import org.apache.wicket.extensions.ajax.markup.html.modal.ModalWindow;
+import org.apache.wicket.ajax.markup.html.AjaxLink;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.model.PropertyModel;
 import org.apache.wicket.util.value.ValueMap;
 import org.hippoecm.frontend.UserSession;
 import org.hippoecm.frontend.dialog.DialogWindow;
+import org.hippoecm.frontend.dialog.DynamicDialogFactory;
 import org.hippoecm.frontend.model.JcrEvent;
 import org.hippoecm.frontend.model.JcrNodeModel;
 import org.hippoecm.frontend.plugin.Plugin;
@@ -15,7 +30,15 @@ import org.hippoecm.frontend.plugin.Plugin;
 public class LoginPlugin extends Plugin {
     private static final long serialVersionUID = 1L;
 
+    private static final String LOGIN_DIALOG_LABEL = "Login";
+    private static final String LOGOUT_DIALOG_LABEL = "Logout";
+    
+    private static final String LOGIN_DIALOG_CLASSNAME = "org.hippoecm.frontend.plugins.admin.login.LoginDialog";
+    private static final String LOGOUT_DIALOG_CLASSNAME = "org.hippoecm.frontend.plugins.admin.login.LogoutDialog";
+    
+    private DynamicDialogFactory loginDialogFactory;
     private String username;
+    private Label loginDialogLinkLabel;
 
     public LoginPlugin(String id, JcrNodeModel model) {
         super(id, model);
@@ -23,25 +46,51 @@ public class LoginPlugin extends Plugin {
         UserSession session = (UserSession) getSession();
         ValueMap credentials = session.getCredentials();
         username = credentials.getString("username");
-        username = (username == null || username.equals("")) ? "anonymous" : username;
+        
+        String label;
+        String className;
+        
+        if (username == null || username.equals("")) {
+            username = "anonymous";
+            label = LOGIN_DIALOG_LABEL;
+            className = LOGIN_DIALOG_CLASSNAME;
+        }
+        else {
+            label = LOGOUT_DIALOG_LABEL;
+            className = LOGOUT_DIALOG_CLASSNAME;
+        }
+
         add(new Label("username", new PropertyModel(this, "username")));
 
         final DialogWindow loginDialog = new DialogWindow("login-dialog", model, true);
-        loginDialog.setPageCreator(new ModalWindow.PageCreator() {
-            private static final long serialVersionUID = 1L;
-            public Page createPage() {
-                return new LoginDialog(loginDialog);
-            }
-        });
         add(loginDialog);
-        add(loginDialog.dialogLink("login-dialog-link"));
+        
+        // the login/logout dialog link
+        AjaxLink dialogLink = loginDialog.dialogLink("login-dialog-link");
+        loginDialogLinkLabel = new Label("login-dialog-link-label", label); 
+        dialogLink.add(loginDialogLinkLabel);
+        add(dialogLink);
+
+        // the factory providing the login or logout dialog based on the logged in/out status
+        loginDialogFactory = new DynamicDialogFactory(loginDialog, className);
+        loginDialog.setPageCreator( loginDialogFactory );
     }
 
     public void update(AjaxRequestTarget target, JcrEvent jcrEvent) {
         UserSession session = (UserSession) getSession();
         ValueMap credentials = session.getCredentials();
         username = credentials.getString("username");
-        username = (username == null || username.equals("")) ? "anonymous" : username;
+        
+        if (username == null || username.equals("")) {
+            username = "anonymous";
+            loginDialogFactory.setClassName(LOGIN_DIALOG_CLASSNAME);
+            loginDialogLinkLabel.setModelObject(LOGIN_DIALOG_LABEL);
+        }
+        else {
+            loginDialogFactory.setClassName(LOGOUT_DIALOG_CLASSNAME);
+            loginDialogLinkLabel.setModelObject(LOGOUT_DIALOG_LABEL);
+        }
+        
         if (target != null) {
             target.addComponent(this);
         }
