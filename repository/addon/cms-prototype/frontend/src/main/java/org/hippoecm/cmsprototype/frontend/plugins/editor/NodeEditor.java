a31adda85f4e22ce7a397d08c83bf8e3c37aefe5
Arje Cahn <a.cahn@onehippo.com>
2007-10-25 15:06:12 +0000

    HREPTWO-181: Added editor to edit tab
    svn path=/hippo-ecm/trunk/; revision=8621

---
+++ repository/addon/cms-prototype/frontend/src/main/java/org/hippoecm/cmsprototype/frontend/plugins/editor/NodeEditor.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2007 Hippo
+ *
+ * Licensed under the Apache License, Version 2.0 (the  "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.hippoecm.cmsprototype.frontend.plugins.editor;
+
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.markup.html.form.Form;
+import org.hippoecm.frontend.model.JcrEvent;
+import org.hippoecm.frontend.model.JcrNodeModel;
+
+public class NodeEditor extends Form {
+    private static final long serialVersionUID = 1L;
+
+    public NodeEditor(String id, final JcrNodeModel model) {
+        super(id, model);
+        setOutputMarkupId(true);
+        add(new PropertiesEditor("properties", model));
+    }
+
+    public void update(AjaxRequestTarget target, JcrEvent jcrEvent) {
+        JcrNodeModel model = jcrEvent.getModel();
+        if (model != null) {
+            JcrNodeModel editorNodeModel = (JcrNodeModel) getModel();
+            editorNodeModel.impersonate(model);
+        }
+        if (target != null) {
+            target.addComponent(this);
+        }
+    }
+
+}
