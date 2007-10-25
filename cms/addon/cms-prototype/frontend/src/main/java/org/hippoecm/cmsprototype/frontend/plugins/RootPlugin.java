cecf355d3e13f0fe016e4f64104abd3bdb76e187
Niels van Kampenhout <n.vankampenhout@onehippo.com>
2007-10-25 14:23:05 +0000

    HREPTWO-181: tabs!!
    svn path=/hippo-ecm/trunk/; revision=8617

--- cms/addon/cms-prototype/frontend/src/main/java/org/hippoecm/cmsprototype/frontend/plugins/RootPlugin.java
+++ cms/addon/cms-prototype/frontend/src/main/java/org/hippoecm/cmsprototype/frontend/plugins/RootPlugin.java
@@ -15,19 +15,85 @@
  */
 package org.hippoecm.cmsprototype.frontend.plugins;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.extensions.ajax.markup.html.tabs.AjaxTabbedPanel;
+import org.apache.wicket.extensions.markup.html.tabs.AbstractTab;
+import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.model.Model;
 import org.hippoecm.frontend.model.JcrEvent;
 import org.hippoecm.frontend.model.JcrNodeModel;
 import org.hippoecm.frontend.plugin.Plugin;
+import org.hippoecm.frontend.plugins.admin.browser.BrowserPlugin;
 
 public class RootPlugin extends Plugin {
     private static final long serialVersionUID = 1L;
 
-    public RootPlugin(String id, JcrNodeModel model) {
+    public RootPlugin(String id, final JcrNodeModel model) {
         super(id, model);
+
+        // create a list of ITab objects used to feed the tabbed panel
+        List tabs = new ArrayList();
+        tabs.add(new AbstractTab(new Model("first tab"))
+        {
+            public Panel getPanel(String panelId)
+            {
+                return new TabPanel1(panelId, model);
+            }
+        });
+
+        tabs.add(new AbstractTab(new Model("second tab"))
+        {
+            public Panel getPanel(String panelId)
+            {
+                return new TabPanel2(panelId, model);
+            }
+        });
+
+        add(new AjaxTabbedPanel("tabs", tabs));
+    
+    
     }
 
     public void update(final AjaxRequestTarget target, JcrEvent jcrEvent) {
     }
 
+    /**
+     * Panel representing the content panel for the first tab.
+     */
+    private static class TabPanel1 extends Panel
+    {
+        /**
+         * Constructor
+         * 
+         * @param id
+         *            component id
+         */
+        public TabPanel1(String id, JcrNodeModel model)
+        {
+            super(id);
+            
+            add(new BrowserPlugin("tree", model));
+        }
+    };
+
+    /**
+     * Panel representing the content panel for the second tab.
+     */
+    private static class TabPanel2 extends Panel
+    {
+        /**
+         * Constructor
+         * 
+         * @param id
+         *            component id
+         */
+        public TabPanel2(String id, JcrNodeModel model)
+        {
+            super(id);
+        }
+    };
+    
 }
