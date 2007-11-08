029010c3295e70f9c9de29a37eca79918e9a421e
Berry van Halderen <b.vanhalderen@onehippo.com>
2007-11-08 15:43:28 +0000

    HREPTWO-191: Use Name iso QName class in (unfortunately) necessary cast
    svn path=/hippo-ecm/trunk/; revision=8873

--- cms/repository/engine/src/main/java/org/hippoecm/repository/security/SimpleAccessManager.java
+++ cms/repository/engine/src/main/java/org/hippoecm/repository/security/SimpleAccessManager.java
@@ -32,7 +32,7 @@ import org.apache.jackrabbit.core.state.ItemState;
 import org.apache.jackrabbit.core.state.ItemStateException;
 import org.apache.jackrabbit.core.state.NoSuchItemStateException;
 import org.apache.jackrabbit.core.state.NodeState;
-import org.apache.jackrabbit.name.QName;
+import org.apache.jackrabbit.spi.Name;
 
 import org.hippoecm.repository.jackrabbit.HippoHierarchyManager;
 
@@ -105,8 +105,8 @@ public class SimpleAccessManager extends org.apache.jackrabbit.core.security.Sim
             if(itemState.isNode()) {
                 NodeState nodeState = (NodeState) itemState;
                 for(Object mixin : nodeState.getMixinTypeNames()) {
-                    QName mixinName = (QName) mixin;
-                    if(mixinName.toString().equals("{http://www.hippoecm.org/nt/1.0}restricted"))
+                    Name mixinName = (Name) mixin;
+		    if((mixinName.getNamespaceURI() + ":" + mixinName.getLocalName()).equals("{http://www.hippoecm.org/nt/1.0}restricted"))
                         throw new AccessDeniedException();
                 }
             }
@@ -138,14 +138,14 @@ public class SimpleAccessManager extends org.apache.jackrabbit.core.security.Sim
             // and allow or deny access
         }
 
-        // FIXME Current simple implementation to be removed for M2.
+        // FIXME Current simple implementation to be removed for M2 or M3 at the latest.
         try {
             ItemState itemState = ((HippoHierarchyManager)hierMgr).getItemState(id);
             if(itemState.isNode()) {
                 NodeState nodeState = (NodeState) itemState;
                 for(Object mixin : nodeState.getMixinTypeNames()) {
-                    QName mixinName = (QName) mixin;
-                    if(mixinName.toString().equals("{http://www.hippoecm.org/nt/1.0}restricted"))
+                    Name mixinName = (Name) mixin;
+		    if((mixinName.getNamespaceURI() + ":" + mixinName.getLocalName()).equals("{http://www.hippoecm.org/nt/1.0}restricted"))
                         return false;
                 }
             }
