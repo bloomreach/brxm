73a530646d505e5a8c88627ea8f28240b7dacdaf
Frank van Lankvelt <f.vanlankvelt@onehippo.com>
2007-11-29 13:50:22 +0000

    HREPTWO-298: upload files to properties, used by the directory class path entry
    svn path=/hippo-ecm/trunk/; revision=9141

---
+++ repository/frontend/plugins/src/main/java/org/hippoecm/frontend/plugins/admin/upload/UploadPlugin.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright 2007 Hippo
+ *
+ * Licensed under the Apache License, Version 2.0 (the  "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.hippoecm.frontend.plugins.admin.upload;
+
+import javax.jcr.Node;
+
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.extensions.ajax.markup.html.form.upload.UploadProgressBar;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.upload.FileUpload;
+import org.apache.wicket.markup.html.form.upload.FileUploadField;
+import org.apache.wicket.util.lang.Bytes;
+
+import org.hippoecm.frontend.model.JcrNodeModel;
+import org.hippoecm.frontend.plugin.JcrEvent;
+import org.hippoecm.frontend.plugin.Plugin;
+import org.hippoecm.frontend.plugin.PluginDescriptor;
+
+public class UploadPlugin extends Plugin {
+
+    private static final long serialVersionUID = 1L;
+
+    private class FileUploadForm extends Form {
+
+        private static final long serialVersionUID = 1L;
+
+        private FileUploadField fileUploadField;
+
+        public FileUploadForm(String name, JcrNodeModel model) {
+            super(name, model);
+            setMultiPart(true);
+            setMaxSize(Bytes.megabytes(5));
+            add(fileUploadField = new FileUploadField("fileInput"));
+        }
+
+        protected void onSubmit() {
+            final FileUpload upload = fileUploadField.getFileUpload();
+            if (upload != null) {
+                Node node = ((JcrNodeModel) getModel()).getNode();
+                if (node != null) {
+                    try {
+                        node.setProperty(upload.getClientFileName(), upload.getInputStream());
+                    } catch (Exception ex) {
+                        // FIXME: report back to user
+                        ex.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+
+    private FileUploadForm form;
+
+    public UploadPlugin(PluginDescriptor pluginDescriptor, JcrNodeModel model, Plugin parentPlugin) {
+        super(pluginDescriptor, model, parentPlugin);
+
+        // Add upload form with ajax progress bar
+        form = new FileUploadForm("form", model);
+        form.add(new UploadProgressBar("progress", form));
+        add(form);
+    }
+
+    public void update(AjaxRequestTarget target, JcrEvent event) {
+        if (event.getModel() != null) {
+            form.setModel(event.getModel());
+        }
+    }
+}
