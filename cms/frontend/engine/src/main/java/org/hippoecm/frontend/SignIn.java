62fd84e8493e054632fe53b0ce0a82d7814b0038
Niels van Kampenhout <n.vankampenhout@onehippo.com>
2007-12-04 15:14:03 +0000

    HREPTWO-297: clean up code
    svn path=/hippo-ecm/trunk/; revision=9172

--- cms/frontend/engine/src/main/java/org/hippoecm/frontend/SignIn.java
+++ cms/frontend/engine/src/main/java/org/hippoecm/frontend/SignIn.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2007 Hippo
+ *
+ * Licensed under the Apache License, Version 2.0 (the  "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.hippoecm.frontend;
 
 import javax.jcr.LoginException;
@@ -24,74 +39,43 @@ public final class SignIn extends WebPage
 {
     private static final long serialVersionUID = 1L;
 
-    /**
-	 * Constructor
-	 */
-	public SignIn()
-	{
-		this(null);
-	}
+    public SignIn()
+    {
+        this(null);
+    }
 
-	/**
-	 * Constructor
-	 * 
-	 * @param parameters
-	 *            The page parameters
-	 */
-	public SignIn(final PageParameters parameters)
-	{
-		// Create feedback panel and add to page
-		final FeedbackPanel feedback = new FeedbackPanel("feedback");
+    public SignIn(final PageParameters parameters)
+    {
+        final FeedbackPanel feedback = new FeedbackPanel("feedback");
+        add(feedback);
+        add(new SignInForm("signInForm"));
+    }
 
-		add(feedback);
-
-		// Add sign-in form to page, passing feedback panel as validation error
-		// handler
-		add(new SignInForm("signInForm"));
-	}
-
-	/**
-	 * Sign in form
-	 * 
-	 * @author Jonathan Locke
-	 */
-	public final class SignInForm extends Form
-	{
+    public final class SignInForm extends Form
+    {
         private static final long serialVersionUID = 1L;
 
-        // El-cheapo model for form
-		private final ValueMap properties = new ValueMap();
+        private final ValueMap properties = new ValueMap();
 
-		/**
-		 * Constructor
-		 * 
-		 * @param id
-		 *            id of the form component
-		 */
-		public SignInForm(final String id)
-		{
-			super(id);
+        public SignInForm(final String id)
+        {
+            super(id);
+            add(new TextField("username", new PropertyModel(properties, "username")));
+            add(new PasswordTextField("password", new PropertyModel(properties, "password")));
+        }
 
-			// Attach textfield components that edit properties map model
-			add(new TextField("username", new PropertyModel(properties, "username")));
-			add(new PasswordTextField("password", new PropertyModel(properties, "password")));
-		}
+        public final void onSubmit()
+        {
+            Main main = (Main) Application.get();
+            HippoRepository repository = main.getRepository();
+            Session jcrSession = null;
+            
+            String username = properties.getString("username");
+            String password = properties.getString("password");
+            
+            String message = "Unable to sign in";
 
-		/**
-		 * @see org.apache.wicket.markup.html.form.Form#onSubmit()
-		 */
-		public final void onSubmit()
-		{
-	        Main main = (Main) Application.get();
-	        HippoRepository repository = main.getRepository();
-	        Session jcrSession = null;
-	        
-	        String username = properties.getString("username");
-	        String password = properties.getString("password");
-	        
-	        String message = "Unable to sign in";
-
-	        try {
+            try {
                 jcrSession = repository.login(username, password.toCharArray());
             } catch (LoginException e) {
                 message += ": " + e;
@@ -101,31 +85,26 @@ public final class SignIn extends WebPage
                 e.printStackTrace();
             }
 
-	        if (jcrSession != null) {
-	        
-    	        UserSession userSession = (UserSession) getSession();
+            if (jcrSession != null) {
+            
+                UserSession userSession = (UserSession) getSession();
                 ValueMap credentials = new ValueMap();
                 credentials.add("username", username);
                 credentials.add("password", password);
-    	        
-    	        userSession.setJcrSession(jcrSession, credentials);
+                
+                userSession.setJcrSession(jcrSession, credentials);
 
                 if (!continueToOriginalDestination())
                 {
                     setResponsePage(getApplication().getHomePage());
                 }
-    	        
-	        }
+                
+            }
             else
             {
-                // Form method that will notify feedback panel
-                // Try the component based localizer first. If not found try the
-                // application localizer. Else use the default
-                final String errmsg = getLocalizer().getString("loginError", this,
-                        message);
-                error(errmsg);
+                error(message);
             }
-		    
-		}
-	}
+            
+        }
+    }
 }
