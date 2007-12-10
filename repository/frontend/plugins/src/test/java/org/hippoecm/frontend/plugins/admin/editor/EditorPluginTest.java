846d9ae31ce78c2d531f7409c4b313354e98028d
Niels van Kampenhout <n.vankampenhout@onehippo.com>
2007-12-10 14:50:02 +0000

    HREPTWO-123: update test
    svn path=/hippo-ecm/trunk/; revision=9211

--- repository/frontend/plugins/src/test/java/org/hippoecm/frontend/plugins/admin/editor/EditorPluginTest.java
+++ repository/frontend/plugins/src/test/java/org/hippoecm/frontend/plugins/admin/editor/EditorPluginTest.java
@@ -2,13 +2,9 @@ package org.hippoecm.frontend.plugins.admin.editor;
 
 import junit.framework.TestCase;
 
-import org.apache.wicket.extensions.ajax.markup.html.AjaxEditableLabel;
 import org.apache.wicket.markup.html.form.Check;
 import org.apache.wicket.util.tester.WicketTester;
-import org.hippoecm.frontend.plugins.admin.editor.EditorPlugin;
-import org.hippoecm.frontend.plugins.admin.editor.NodeEditor;
-import org.hippoecm.frontend.plugins.admin.editor.PropertiesEditor;
-import org.hippoecm.frontend.plugins.admin.editor.PropertyValueEditor;
+import org.hippoecm.frontend.widgets.TextFieldWidget;
 
 public class EditorPluginTest extends TestCase {
 
@@ -36,7 +32,7 @@ public class EditorPluginTest extends TestCase {
         tester.assertComponent("editorPlugin:editor", NodeEditor.class);
         tester.assertComponent("editorPlugin:editor:properties", PropertiesEditor.class);
         tester.assertComponent("editorPlugin:editor:properties:1:values", PropertyValueEditor.class);
-        tester.assertComponent("editorPlugin:editor:properties:1:values:1:value", AjaxEditableLabel.class);
+        tester.assertComponent("editorPlugin:editor:properties:1:values:1:value", TextFieldWidget.class);
 
         tester.assertComponent("editorPlugin:editor:types", NodeTypesEditor.class);
         tester.assertComponent("editorPlugin:editor:types:type:0:check", Check.class);
