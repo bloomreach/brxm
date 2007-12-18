7c66025626003b218321d6d57a88ef56ee9eedf0
Niels van Kampenhout <n.vankampenhout@onehippo.com>
2007-12-18 15:29:02 +0000

    HREPTWO-339: add new document wizard (it doesn't do anything yet)
    svn path=/hippo-ecm/trunk/; revision=9359

---
+++ cms/addon/cms-prototype/frontend/src/main/java/org/hippoecm/cmsprototype/frontend/plugins/addnew/AddNewWizard.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2007 Hippo
+ *
+ * Licensed under the Apache License, Version 2.0 (the  "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.hippoecm.cmsprototype.frontend.plugins.addnew;
+
+import org.apache.wicket.markup.html.form.Button;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.markup.html.panel.FeedbackPanel;
+import org.apache.wicket.model.Model;
+import org.apache.wicket.model.PropertyModel;
+import org.apache.wicket.util.value.ValueMap;
+import org.hippoecm.frontend.model.JcrNodeModel;
+import org.hippoecm.frontend.plugin.Plugin;
+import org.hippoecm.frontend.plugin.PluginDescriptor;
+
+public class AddNewWizard extends Plugin {
+    private static final long serialVersionUID = 1L;
+    
+    public AddNewWizard(PluginDescriptor pluginDescriptor, JcrNodeModel model, Plugin parentPlugin) {
+        super(pluginDescriptor, model, parentPlugin);
+        add(new AddNewForm("addNewForm"));
+        add(new FeedbackPanel("feedback"));
+    }
+    
+    private final class AddNewForm extends Form {
+        private static final long serialVersionUID = 1L;
+        
+        private ValueMap properties;
+        
+        public AddNewForm(String id) {
+            super(id);
+            properties = new ValueMap();
+            TextField name = new TextField("name", new PropertyModel(properties, "name"));
+            name.setRequired(true);
+            add(name);
+            add(new Button("submit", new Model("Add")));
+        }
+
+        @Override
+        protected void onSubmit() {
+            error("Not implemented...");
+        }
+        
+    }
+
+}
