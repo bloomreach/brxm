3a02bc75fa65d9335420bb6e3651b8c27ef72300
Arje Cahn <a.cahn@onehippo.com>
2007-12-18 11:30:34 +0000

    HREPTWO-330: Added Dashboard perspective and Todo plugin
    svn path=/hippo-ecm/trunk/; revision=9349

---
+++ repository/addon/cms-prototype/frontend/src/main/java/org/hippoecm/cmsprototype/frontend/plugins/perspectives/DashboardPerspective.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2007 Hippo
+ *
+ * Licensed under the Apache License, Version 2.0 (the  "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.hippoecm.cmsprototype.frontend.plugins.perspectives;
+
+import org.hippoecm.frontend.model.JcrNodeModel;
+import org.hippoecm.frontend.plugin.Plugin;
+import org.hippoecm.frontend.plugin.PluginDescriptor;
+
+/**
+ * Panel representing the content panel for the first tab.
+ */
+public class DashboardPerspective extends Plugin {
+    private static final long serialVersionUID = 1L;
+
+    public DashboardPerspective(PluginDescriptor pluginDescriptor, JcrNodeModel model, Plugin parentPlugin) {
+        super(pluginDescriptor, model, parentPlugin);
+    }
+
+}
