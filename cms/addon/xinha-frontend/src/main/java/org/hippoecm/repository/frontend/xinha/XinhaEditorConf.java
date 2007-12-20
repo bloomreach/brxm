e08ae67d5ec5946651edc2391a9328dec9e25ddc
Berry van Halderen <b.vanhalderen@onehippo.com>
2007-12-20 15:01:59 +0000

    HREPTWO-334: Fix package vs directory structure
    svn path=/hippo-ecm/trunk/; revision=9417

--- cms/addon/xinha-frontend/src/main/java/org/hippoecm/repository/frontend/xinha/XinhaEditorConf.java
+++ cms/addon/xinha-frontend/src/main/java/org/hippoecm/repository/frontend/xinha/XinhaEditorConf.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.hippoecm.repository.frontend.wysiwyg.xinha;
+package org.hippoecm.repository.frontend.xinha;
 
 import java.io.Serializable;
 import java.util.Map;
