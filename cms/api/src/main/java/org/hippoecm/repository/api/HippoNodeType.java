bf1b9df784802f8f5782f354e9e0d0f325f0f989
Bart van der Schans <b.vanderschans@onehippo.com>
2007-12-21 15:48:16 +0000

    HREPTWO-163,  HREPTWO-165, HREPTWO-166: Add initial implementation for facet based authorization, user, groups and roles. The current facetAuthorization is based on the (rule) OR (rule) OR.. with each rule (facet= valA OR valB OR..)
    svn path=/hippo-ecm/trunk/; revision=9466

--- cms/api/src/main/java/org/hippoecm/repository/api/HippoNodeType.java
+++ cms/api/src/main/java/org/hippoecm/repository/api/HippoNodeType.java
@@ -32,22 +32,40 @@ public interface HippoNodeType {
     final public static String WORKFLOWS_PATH = "hippo:workflows";
     final public static String DOCUMENTS_PATH = "hippo:documents";
     final public static String FRONTEND_PATH = "hippo:frontend";
+    final public static String USERS_PATH = "hippo:users";
+    final public static String GROUPS_PATH = "hippo:groups";
+    final public static String ROLES_PATH = "hippo:roles";
+    final public static String FACETAUTH_PATH = "hippo:facetauths";
     final public static String PLUGIN_PATH = "hippo:plugins";
 
+    //--- Hippo NodeTypes ---//
+    final public static String NT_APPLICATION = "hippo:application";
     final public static String NT_CONFIGURATION = "hippo:configuration";
     final public static String NT_DOCUMENT = "hippo:document";
+    final public static String NT_FACETAUTH = "hippo:facetauth";
+    final public static String NT_FACETAUTHFOLDER = "hippo:facetauthfolder";
     final public static String NT_FACETRESULT = "hippo:facetresult";
     final public static String NT_FACETSUBSEARCH = "hippo:facetsubsearch";
     final public static String NT_FACETSEARCH = "hippo:facetsearch";
     final public static String NT_FACETSELECT = "hippo:facetselect";
+    final public static String NT_GROUP = "hippo:group";
+    final public static String NT_GROUPFOLDER = "hippo:groupfolder";
     final public static String NT_HANDLE = "hippo:handle";
     final public static String NT_MIRROR = "hippo:mirror";
+    final public static String NT_FRONTEND = "hippo:frontend";
+    final public static String NT_PAGE = "hippo:page";
     final public static String NT_INITIALIZEFOLDER = "hippo:initializefolder";
     final public static String NT_INITIALIZEITEM = "hippo:initializeitem";
     final public static String NT_QUERY = "hippo:query";
     final public static String NT_QUERYFOLDER = "hippo:queryfolder";
     final public static String NT_REFERENCEABLE = "hippo:referenceable";
+    final public static String NT_REQUEST = "hippo:request";
+    final public static String NT_ROLE = "hippo:role";
+    final public static String NT_ROLEFOLDER = "hippo:rolefolder";
+    final public static String NT_TYPE = "hippo:type";
     final public static String NT_TYPES = "hippo:types";
+    final public static String NT_USER = "hippo:user";
+    final public static String NT_USERFOLDER = "hippo:userfolder";
     final public static String NT_WORKFLOW = "hippo:workflow";
     final public static String NT_WORKFLOWCATEGORY = "hippo:workflowcategory";
     final public static String NT_WORKFLOWFOLDER = "hippo:workflowfolder";
@@ -55,6 +73,7 @@ public interface HippoNodeType {
     final public static String NT_PLUGIN = "hippo:plugin";
     final public static String NT_FRONTENDPLUGIN = "hippo:frontendplugin";
 
+    //--- Hippo Item Names ---//
     final public static String HIPPO_CLASSNAME = "hippo:classname";
     final public static String HIPPO_CONTENT = "hippo:content";
     final public static String HIPPO_CONTENTROOT = "hippo:contentroot";
@@ -62,18 +81,23 @@ public interface HippoNodeType {
     final public static String HIPPO_DISPLAY = "hippo:display";
     final public static String HIPPO_DOCBASE = "hippo:docbase";
     final public static String HIPPO_DOCUMENTS = "hippo:documents";
+    final public static String HIPPO_FACET = "hippo:facet";
     final public static String HIPPO_FACETS = "hippo:facets";
     final public static String HIPPO_INITIALIZE = "hippo:initialize";
     final public static String HIPPO_LANGUAGE = "hippo:language";
+    final public static String HIPPO_MEMBERS = "hippo:members";
     final public static String HIPPO_MODES = "hippo:modes";
     final public static String HIPPO_NAMESPACE = "hippo:namespace";
     final public static String HIPPO_NODETYPE = "hippo:nodetype";
     final public static String HIPPO_NODETYPES = "hippo:nodetypes";
+    final public static String HIPPO_PASSWORD = "hippo:password";
     final public static String HIPPO_PATHS = "hippo:paths";
+    final public static String HIPPO_PERMISSIONS = "hippo:permissions";
     final public static String HIPPO_QUERY = "hippo:query";
     final public static String HIPPO_QUERYNAME = "hippo:queryname";
     final public static String HIPPO_RENDERER = "hippo:renderer";
     final public static String HIPPO_RESULTSET = "hippo:resultset";
+    final public static String HIPPO_ROLES = "hippo:roles";
     final public static String HIPPO_SEARCH = "hippo:search";
     final public static String HIPPO_SERVICE = "hippo:classname";
     final public static String HIPPO_TYPES = "hippo:types";
