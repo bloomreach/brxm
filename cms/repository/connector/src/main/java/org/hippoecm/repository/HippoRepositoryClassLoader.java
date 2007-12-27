177dec4c94452a9bda25e73406067d7323938c5b
Berry van Halderen <b.vanhalderen@onehippo.com>
2007-12-27 08:50:46 +0000

    HREPTWO-16: add missing copyright header, add fixme indicators
    svn path=/hippo-ecm/trunk/; revision=9537

--- cms/repository/connector/src/main/java/org/hippoecm/repository/HippoRepositoryClassLoader.java
+++ cms/repository/connector/src/main/java/org/hippoecm/repository/HippoRepositoryClassLoader.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2007 Hippo
+ *
+ * Licensed under the Apache License, Version 2.0 (the  "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.hippoecm.repository;
 
 import java.net.URL;
@@ -5,26 +20,34 @@ import java.net.URL;
 import javax.jcr.Session;
 
 import org.apache.jackrabbit.classloader.DynamicRepositoryClassLoader;
+
 import org.hippoecm.repository.api.HippoNodeType;
 
 public class HippoRepositoryClassLoader extends DynamicRepositoryClassLoader {
 
-    /** dynamic loading class path
+    /**
+     * Dynamic loading class path
      * Jars should be put into /hippo:plugins/&lt;plugin&gt;/&lt;jar&gt;
      */
     private static final String JARS = HippoNodeType.PLUGIN_PATH + "/*/*/jcr:data";
     private static final String[] handles = { JARS };
 
     public HippoRepositoryClassLoader(Session session) {
-        // This is the preferred method of instantiation, with automatic (on-demand)
-        // unpacking of newly inserted jars.  However, this fails because of JR bug #???.
+        /* FIXME:
+	 * This is the preferred method of instantiation, with automatic (on-demand)
+         * unpacking of newly inserted jars.  However, this fails because of JR bug #???.
+	 */
         super(session, handles, HippoRepositoryClassLoader.class.getClassLoader());
     }
 
-    // Used by RMI to construct codebase for the classes that have been loaded.
-    // The "jcr" protocol is not generally available and would have to be 
-    // registered globally in a J2EE environment.  Assume that the client is using
-    // a Hippo classloader, i.e. uses the same path in the repository.
+    /**
+     * Used by RMI to construct codebase for the classes that have been loaded.
+     * The "jcr" protocol is not generally available and would have to be 
+     * registered globally in a J2EE environment.  Assume that the client is using
+     * a Hippo classloader, i.e. uses the same path in the repository.
+     *
+     * @returns an empty array of URLs.
+     */
     @Override
     public URL[] getURLs() {
         return null;
