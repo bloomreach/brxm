d7065854b0e1cc5ebc7c10ff3548bf5358ec7344
Ard Schrijvers <a.schrijvers@onehippo.com>
2008-01-02 17:52:02 +0000

    HREPTWO-358  extract abstract default listing plugin, which can be used by all listings
    svn path=/hippo-ecm/trunk/; revision=9600

--- repository/addon/cms-prototype/frontend/src/main/java/org/hippoecm/cmsprototype/frontend/plugins/list/NodeCell.java
+++ repository/addon/cms-prototype/frontend/src/main/java/org/hippoecm/cmsprototype/frontend/plugins/list/NodeCell.java
@@ -15,20 +15,31 @@
  */
 package org.hippoecm.cmsprototype.frontend.plugins.list;
 
+import javax.jcr.PathNotFoundException;
+import javax.jcr.Property;
+import javax.jcr.PropertyType;
+import javax.jcr.RepositoryException;
+import javax.jcr.ValueFormatException;
+
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.ajax.markup.html.AjaxLink;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.panel.Panel;
-import org.apache.wicket.model.PropertyModel;
 import org.hippoecm.frontend.model.JcrNodeModel;
 import org.hippoecm.frontend.model.NodeModelWrapper;
 import org.hippoecm.frontend.plugin.channel.Channel;
 import org.hippoecm.frontend.plugin.channel.Request;
+import org.hippoecm.repository.api.HippoNode;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class NodeCell extends Panel {
     private static final long serialVersionUID = 1L;
 
-    public NodeCell(String id, NodeModelWrapper model, final Channel channel) {
+    static final Logger log = LoggerFactory.getLogger(AbstractListingPlugin.class);
+    
+    
+    public NodeCell(String id, NodeModelWrapper model, final Channel channel, String columnName) {
         super(id, model);
         AjaxLink link = new AjaxLink("link", model) {
             private static final long serialVersionUID = 1L;
@@ -36,14 +47,83 @@ public class NodeCell extends Panel {
             @Override
             public void onClick(AjaxRequestTarget target) {
                 // create a "select" request with the node path as a parameter
-            	JcrNodeModel nodeModel = ((NodeModelWrapper)this.getModel()).getNodeModel(); 
+                JcrNodeModel nodeModel = ((NodeModelWrapper) this.getModel()).getNodeModel();
                 Request request = channel.createRequest("select", nodeModel.getMapRepresentation());
                 channel.send(request);
                 request.getContext().apply(target);
             }
-        
+
         };
+        if (model.getObject() instanceof HippoNode) {
+            try {
+                HippoNode n = (HippoNode) model.getObject();
+                
+                if (columnName.equals("name")) {
+                    addLabel(link, n.getName());
+                } else if (columnName.equals("displayname")) {
+                    addLabel(link, n.getDisplayName());
+                } else if (columnName.equals("islocked")) {
+                    addLabel(link, String.valueOf(n.isLocked()));
+                } else if (columnName.equals("path")) {
+                    addLabel(link, n.getPath());
+                }
+                else {
+
+                    Property p = n.getProperty(columnName);
+                    switch (p.getType()) {
+                    case PropertyType.BINARY:
+                        // never show binary value
+                        break;
+                    case PropertyType.BOOLEAN:
+                        addLabel(link, String.valueOf(p.getBoolean()));
+                        break;
+                    case PropertyType.DATE:
+                        addLabel(link, String.valueOf(p.getDate()));
+                        break;
+                    case PropertyType.DOUBLE:
+                        addLabel(link, String.valueOf(p.getDouble()));
+                        break;
+                    case PropertyType.LONG:
+                        addLabel(link, String.valueOf(p.getLong()));
+                        break;
+                    case PropertyType.REFERENCE:
+                        // do not show references
+                        break;
+                    case PropertyType.PATH:
+                        addLabel(link, String.valueOf(p.getPath()));
+                        break;
+                    case PropertyType.STRING:
+                        addLabel(link, String.valueOf(p.getString()));
+                        break;
+                    case PropertyType.NAME:
+                        addLabel(link, String.valueOf(p.getName()));
+                        break;
+                    default:
+                        throw new IllegalArgumentException("illegal internal value type");
+                    }
+                } 
+            } catch (ValueFormatException e) {
+                log.debug("Unable to find property for culumn " + columnName + ". Creating empty label. Reason : " + e.getMessage());
+                emptyLabel(link);
+            } catch (PathNotFoundException e) {
+                log.debug("Unable to find property for culumn " + columnName + ". Creating empty label. Reason : " + e.getMessage());
+                emptyLabel(link);
+            } catch (RepositoryException e) {
+                log.debug("Unable to find property for culumn " + columnName + ". Creating empty label. Reason : " + e.getMessage());
+                emptyLabel(link);
+            }
+        } else {
+            emptyLabel(link);
+        }
+
         add(link);
-        link.add(new Label("label", new PropertyModel(model, "name")));
+    }
+
+    private void addLabel(AjaxLink link, String value) {
+        link.add(new Label("label", value));
+    }
+
+    private void emptyLabel(AjaxLink link) {
+        link.add(new Label("label"));
     }
 }
