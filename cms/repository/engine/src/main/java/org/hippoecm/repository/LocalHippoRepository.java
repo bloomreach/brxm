0a6cb687fa7badfae3d48277a52c31ac4aad9fa4
Berry van Halderen <b.vanhalderen@onehippo.com>
2008-01-02 15:32:03 +0000

    12345678901234567890123456789012345678901234567890123456789012345678901234567890 HREPTWO-197: EAR packaging complete and furthermore customizability of skin etc - auto start rmiregistry when not already running (always try to start it, when   connect address starts with rmi://). - always set the repository connect address to rmi://localhost:1099/... - move style.css file into seperate project and as the URL /skin/style.css (iso   /style.css). - move xinha editor files in xinha add-on one level deeper as URL, so in stead   of directly as /xinha, put them as /xinha/xinha.  This allowed for the usage   of this subproject as both overlay WAR as well as EAR composition.  Otherwise   the svn:external would become complicated.
    svn path=/hippo-ecm/trunk/; revision=9593

--- cms/repository/engine/src/main/java/org/hippoecm/repository/LocalHippoRepository.java
+++ cms/repository/engine/src/main/java/org/hippoecm/repository/LocalHippoRepository.java
@@ -25,6 +25,7 @@ import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.Enumeration;
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 
 import javax.jcr.AccessDeniedException;
@@ -267,9 +268,23 @@ class LocalHippoRepository extends HippoRepositoryImpl {
                     throw new RepositoryException("Could not initialize repository with configuration content", ex);
                 }
                 try {
+                    List extensions = new LinkedList();
                     for(Enumeration iter = getClass().getClassLoader().getResources("org/hippoecm/repository/extension.xml");
                         iter.hasMoreElements(); ) {
                         URL configurationURL = (URL) iter.nextElement();
+                        extensions.add(configurationURL);
+                    }
+                    /* FIXME: does not seem to be necessary, as long as in the
+                     * EAR the extensions are all placed in the default place,
+                     * not in for instance APP-INF/lib
+                     *     for(Enumeration iter = getClass().getClassLoader().getParent().
+                     *         getResources("org/hippoecm/repository/extension.xml"); iter.hasMoreElements(); ) {
+                     *         URL configurationURL = (URL) iter.nextElement();
+                     *         extensions.addpend(configurationURL);
+                     *     }
+                     */
+                    for(Iterator iter = extensions.iterator(); iter.hasNext(); ) {
+                        URL configurationURL = (URL) iter.next();
                         log.info("Initializing additional configuration content from "+configurationURL);
                         try {
                             InputStream configurationStream = configurationURL.openStream();
