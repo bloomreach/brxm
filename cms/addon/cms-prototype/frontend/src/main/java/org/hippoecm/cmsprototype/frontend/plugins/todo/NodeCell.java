3585e228a1af0e306e9adc97cbf6f235f051fbaa
Ard Schrijvers <a.schrijvers@onehippo.com>
2008-01-07 16:44:22 +0000

    HREPTWO-16 correct indenting and formatting
    svn path=/hippo-ecm/trunk/; revision=9768

--- cms/addon/cms-prototype/frontend/src/main/java/org/hippoecm/cmsprototype/frontend/plugins/todo/NodeCell.java
+++ cms/addon/cms-prototype/frontend/src/main/java/org/hippoecm/cmsprototype/frontend/plugins/todo/NodeCell.java
@@ -17,8 +17,6 @@ package org.hippoecm.cmsprototype.frontend.plugins.todo;
 
 import javax.jcr.Node;
 import javax.jcr.PathNotFoundException;
-import javax.jcr.Property;
-import javax.jcr.PropertyIterator;
 import javax.jcr.RepositoryException;
 import javax.jcr.ValueFormatException;
 
@@ -27,7 +25,6 @@ import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.ajax.markup.html.AjaxLink;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.panel.Panel;
-import org.apache.wicket.model.PropertyModel;
 import org.hippoecm.frontend.model.JcrNodeModel;
 import org.hippoecm.frontend.model.NodeModelWrapper;
 import org.hippoecm.frontend.plugin.channel.Channel;
@@ -45,7 +42,7 @@ public class NodeCell extends Panel {
             @Override
             public void onClick(AjaxRequestTarget target) {
                 // create a "select" request with the node path as a parameter
-                JcrNodeModel nodeModel = ((NodeModelWrapper)this.getModel()).getNodeModel();
+                JcrNodeModel nodeModel = ((NodeModelWrapper) this.getModel()).getNodeModel();
                 Request request = channel.createRequest("select", nodeModel.getMapRepresentation());
                 channel.send(request);
                 request.getContext().apply(target);
@@ -55,41 +52,41 @@ public class NodeCell extends Panel {
         add(link);
 
         String type = "";
-        String username ="";
+        String username = "";
         String doc = "";
         String reason = "";
-        String path ="";
+        String path = "";
 
         try {
 
-                type = model.getNodeModel().getNode().getProperty("type").getString();
-                        username = model.getNodeModel().getNode().getProperty("username").getString();
+            type = model.getNodeModel().getNode().getProperty("type").getString();
+            username = model.getNodeModel().getNode().getProperty("username").getString();
 
-                        UserSession session = (UserSession) Session.get();
-                Node node = session.getJcrSession().getNodeByUUID(model.getNodeModel().getNode().getProperty("document").getString());
+            UserSession session = (UserSession) Session.get();
+            Node node = session.getJcrSession().getNodeByUUID(
+                    model.getNodeModel().getNode().getProperty("document").getString());
 
-                if(node != null) {
-                        doc = node.getName();
-                        path = node.getPath();
-                }else {
-                        doc ="unknown";
-                }
+            if (node != null) {
+                doc = node.getName();
+                path = node.getPath();
+            } else {
+                doc = "unknown";
+            }
 
+            reason = model.getNodeModel().getNode().getProperty("reason").getString();
 
-                        reason = model.getNodeModel().getNode().getProperty("reason").getString();
+        } catch (ValueFormatException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (PathNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (RepositoryException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
 
-                } catch (ValueFormatException e) {
-                        // TODO Auto-generated catch block
-                        e.printStackTrace();
-                } catch (PathNotFoundException e) {
-                        // TODO Auto-generated catch block
-                        e.printStackTrace();
-                } catch (RepositoryException e) {
-                        // TODO Auto-generated catch block
-                        e.printStackTrace();
-                }
-
-                add(new Label("type", type));
+        add(new Label("type", type));
         add(new Label("username", username));
         add(new Label("path", path));
         add(new Label("reason", reason));
@@ -97,6 +94,5 @@ public class NodeCell extends Panel {
         link.add(new Label("label", doc));
 
     }
-
 
 }
