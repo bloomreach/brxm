213c4b002317ddbb4834ec96c4f1a57c1102a9d5
Arje Cahn <a.cahn@onehippo.com>
2008-01-10 12:00:18 +0000

    HREPTWO-331: Messaging between Tasklist and BrowsePerspective plugins (tree, documentlisting, workflow, variants)
    svn path=/hippo-ecm/trunk/; revision=9826

--- repository/frontend/engine/src/main/java/org/hippoecm/frontend/tree/AbstractTreePlugin.java
+++ repository/frontend/engine/src/main/java/org/hippoecm/frontend/tree/AbstractTreePlugin.java
@@ -15,8 +15,16 @@
  */
 package org.hippoecm.frontend.tree;
 
+import java.util.Map;
+
+import javax.jcr.AccessDeniedException;
+import javax.jcr.ItemNotFoundException;
+import javax.jcr.Node;
+import javax.jcr.RepositoryException;
+import javax.jcr.ValueFormatException;
 import javax.swing.tree.TreeNode;
 
+import org.apache.wicket.Session;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.hippoecm.frontend.model.JcrNodeModel;
 import org.hippoecm.frontend.model.tree.AbstractTreeNode;
@@ -24,8 +32,11 @@ import org.hippoecm.frontend.model.tree.JcrTreeModel;
 import org.hippoecm.frontend.plugin.Plugin;
 import org.hippoecm.frontend.plugin.PluginDescriptor;
 import org.hippoecm.frontend.plugin.channel.Channel;
+import org.hippoecm.frontend.plugin.channel.MessageContext;
 import org.hippoecm.frontend.plugin.channel.Notification;
 import org.hippoecm.frontend.plugin.channel.Request;
+import org.hippoecm.frontend.session.UserSession;
+import org.hippoecm.repository.api.HippoNodeType;
 
 public abstract class AbstractTreePlugin extends Plugin {
     private static final long serialVersionUID = 1L;
@@ -68,9 +79,29 @@ public abstract class AbstractTreePlugin extends Plugin {
     @Override
     public void receive(Notification notification) {
         Request request = notification.getRequest();
+        
+        String op = "";
+        Map data;
+        MessageContext context;
+        
         if (request != null) {
-            if ("select".equals(notification.getOperation())) {
-                JcrNodeModel model = new JcrNodeModel(request.getData());
+            op = request.getOperation();
+            data = request.getData();
+            context = request.getContext();
+        }
+        else {
+            op = notification.getOperation();
+            data = notification.getData();
+            context = notification.getContext();
+        }
+           
+        
+        if (notification != null) {
+            if ("select".equals(op)) {
+                JcrNodeModel model = new JcrNodeModel(data);
+                
+                model = findCollectionModel(model);
+               
                 AbstractTreeNode node = null;
                 while (model != null) {
                     node = rootNode.getTreeModel().lookup(model);
@@ -81,15 +112,83 @@ public abstract class AbstractTreePlugin extends Plugin {
                         model = model.getParentModel();
                     }
                 }
-            } else if ("flush".equals(notification.getOperation())) {
-                AbstractTreeNode node = rootNode.getTreeModel().lookup(new JcrNodeModel(request.getData()));
+            } else if ("flush".equals(op)) {
+                AbstractTreeNode node = rootNode.getTreeModel().lookup(new JcrNodeModel(data));
                 if (node != null) {
                     node.markReload();
                     node.getTreeModel().nodeStructureChanged(node);
-                    request.getContext().addRefresh(tree, "updateTree");
+                    context.addRefresh(tree, "updateTree");
                 }
             }
         }
         super.receive(notification);
     }
+
+    /**
+     * Finds the first parent node that is a collection of handles 
+     * @param nodeModel
+     * @return the JcrNodeModel of the matching parent node, or null
+     */
+    protected JcrNodeModel findCollectionModel(JcrNodeModel nodeModel) {
+        
+        JcrNodeModel result = nodeModel;
+        String docUUID = "";
+        
+        Node resultNode = result.getNode();
+            
+        try {
+            if (resultNode.hasProperty("document")) {
+                
+                try {
+                    docUUID = resultNode.getProperty("document").getString();
+                } catch (ValueFormatException e) {
+                    // docUUID property has incorrect formatting
+                    result = null;
+                    return result;
+                }
+                
+                if (!"".equals(docUUID)) {
+                    
+                    // This is a referencing Node. Maybe a request node? Find the document the nodeModel is referring at.
+    
+                    javax.jcr.Session session = (javax.jcr.Session)(((UserSession)Session.get()).getJcrSession());
+                    result = findCollectionModel(new JcrNodeModel(session.getNodeByUUID(docUUID)));
+                    return result;
+                }
+            }
+        } catch (RepositoryException e2) {
+            return null;
+        }
+        
+        try {
+            if (resultNode.getPrimaryNodeType().isNodeType(HippoNodeType.NT_DOCUMENT)) {
+                // This is a document. We need the parent (or maybe even the parent's parent).
+                result = findCollectionModel(new JcrNodeModel(resultNode.getParent()));
+                return result;
+            }
+        } catch (ItemNotFoundException e2) {
+            return null;
+        } catch (AccessDeniedException e2) {
+            return null;
+        } catch (RepositoryException e2) {
+            return null;
+        }
+        
+            try {
+                if (resultNode.getPrimaryNodeType().isNodeType(HippoNodeType.NT_HANDLE)){
+                    // This is a handle. We need the parent.
+                    result = findCollectionModel(new JcrNodeModel(nodeModel.getNode().getParent()));
+                    return result;
+                }
+            } catch (ItemNotFoundException e1) {
+                return null;
+            } catch (AccessDeniedException e1) {
+                return null;
+            } catch (RepositoryException e1) {
+                return null;
+            }
+        
+       return result;
+    }
+
 }
