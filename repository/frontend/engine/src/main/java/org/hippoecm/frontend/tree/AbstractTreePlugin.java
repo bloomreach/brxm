8f84403196e60c94e2c0f02d6747ada1dbb1a174
Arje Cahn <a.cahn@onehippo.com>
2008-01-10 15:03:49 +0000

    HREPTWO-331: Moved code from more generic AbstractTreePlugin into FolderTreePlugin
    svn path=/hippo-ecm/trunk/; revision=9835

--- repository/frontend/engine/src/main/java/org/hippoecm/frontend/tree/AbstractTreePlugin.java
+++ repository/frontend/engine/src/main/java/org/hippoecm/frontend/tree/AbstractTreePlugin.java
@@ -15,16 +15,8 @@
  */
 package org.hippoecm.frontend.tree;
 
-import java.util.Map;
-
-import javax.jcr.AccessDeniedException;
-import javax.jcr.ItemNotFoundException;
-import javax.jcr.Node;
-import javax.jcr.RepositoryException;
-import javax.jcr.ValueFormatException;
 import javax.swing.tree.TreeNode;
 
-import org.apache.wicket.Session;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.hippoecm.frontend.model.JcrNodeModel;
 import org.hippoecm.frontend.model.tree.AbstractTreeNode;
@@ -32,17 +24,14 @@ import org.hippoecm.frontend.model.tree.JcrTreeModel;
 import org.hippoecm.frontend.plugin.Plugin;
 import org.hippoecm.frontend.plugin.PluginDescriptor;
 import org.hippoecm.frontend.plugin.channel.Channel;
-import org.hippoecm.frontend.plugin.channel.MessageContext;
 import org.hippoecm.frontend.plugin.channel.Notification;
 import org.hippoecm.frontend.plugin.channel.Request;
-import org.hippoecm.frontend.session.UserSession;
-import org.hippoecm.repository.api.HippoNodeType;
 
 public abstract class AbstractTreePlugin extends Plugin {
     private static final long serialVersionUID = 1L;
 
-    private JcrTree tree;
-    private AbstractTreeNode rootNode;
+    protected JcrTree tree;
+    protected AbstractTreeNode rootNode;
 
     public AbstractTreePlugin(PluginDescriptor pluginDescriptor, AbstractTreeNode rootNode, Plugin parentPlugin) {
         super(pluginDescriptor, rootNode.getNodeModel(), parentPlugin);
@@ -76,32 +65,13 @@ public abstract class AbstractTreePlugin extends Plugin {
         }
     }
 
+
     @Override
     public void receive(Notification notification) {
         Request request = notification.getRequest();
-        
-        String op = "";
-        Map data;
-        MessageContext context;
-        
         if (request != null) {
-            op = request.getOperation();
-            data = request.getData();
-            context = request.getContext();
-        }
-        else {
-            op = notification.getOperation();
-            data = notification.getData();
-            context = notification.getContext();
-        }
-           
-        
-        if (notification != null) {
-            if ("select".equals(op)) {
-                JcrNodeModel model = new JcrNodeModel(data);
-                
-                model = findCollectionModel(model);
-               
+            if ("select".equals(notification.getOperation())) {
+                JcrNodeModel model = new JcrNodeModel(request.getData());
                 AbstractTreeNode node = null;
                 while (model != null) {
                     node = rootNode.getTreeModel().lookup(model);
@@ -112,83 +82,15 @@ public abstract class AbstractTreePlugin extends Plugin {
                         model = model.getParentModel();
                     }
                 }
-            } else if ("flush".equals(op)) {
-                AbstractTreeNode node = rootNode.getTreeModel().lookup(new JcrNodeModel(data));
+            } else if ("flush".equals(notification.getOperation())) {
+                AbstractTreeNode node = rootNode.getTreeModel().lookup(new JcrNodeModel(request.getData()));
                 if (node != null) {
                     node.markReload();
                     node.getTreeModel().nodeStructureChanged(node);
-                    context.addRefresh(tree, "updateTree");
+                    request.getContext().addRefresh(tree, "updateTree");
                 }
             }
         }
         super.receive(notification);
     }
-
-    /**
-     * Finds the first parent node that is a collection of handles 
-     * @param nodeModel
-     * @return the JcrNodeModel of the matching parent node, or null
-     */
-    protected JcrNodeModel findCollectionModel(JcrNodeModel nodeModel) {
-        
-        JcrNodeModel result = nodeModel;
-        String docUUID = "";
-        
-        Node resultNode = result.getNode();
-            
-        try {
-            if (resultNode.hasProperty("document")) {
-                
-                try {
-                    docUUID = resultNode.getProperty("document").getString();
-                } catch (ValueFormatException e) {
-                    // docUUID property has incorrect formatting
-                    result = null;
-                    return result;
-                }
-                
-                if (!"".equals(docUUID)) {
-                    
-                    // This is a referencing Node. Maybe a request node? Find the document the nodeModel is referring at.
-    
-                    javax.jcr.Session session = (javax.jcr.Session)(((UserSession)Session.get()).getJcrSession());
-                    result = findCollectionModel(new JcrNodeModel(session.getNodeByUUID(docUUID)));
-                    return result;
-                }
-            }
-        } catch (RepositoryException e2) {
-            return null;
-        }
-        
-        try {
-            if (resultNode.getPrimaryNodeType().isNodeType(HippoNodeType.NT_DOCUMENT)) {
-                // This is a document. We need the parent (or maybe even the parent's parent).
-                result = findCollectionModel(new JcrNodeModel(resultNode.getParent()));
-                return result;
-            }
-        } catch (ItemNotFoundException e2) {
-            return null;
-        } catch (AccessDeniedException e2) {
-            return null;
-        } catch (RepositoryException e2) {
-            return null;
-        }
-        
-            try {
-                if (resultNode.getPrimaryNodeType().isNodeType(HippoNodeType.NT_HANDLE)){
-                    // This is a handle. We need the parent.
-                    result = findCollectionModel(new JcrNodeModel(nodeModel.getNode().getParent()));
-                    return result;
-                }
-            } catch (ItemNotFoundException e1) {
-                return null;
-            } catch (AccessDeniedException e1) {
-                return null;
-            } catch (RepositoryException e1) {
-                return null;
-            }
-        
-       return result;
-    }
-
 }
