43ba55833314d1ace03aa459a3804c26ff7e03be
Ard Schrijvers <a.schrijvers@onehippo.com>
2008-02-06 15:01:41 +0000

    HREPTWO-457 changed comment implying bug, though it is programmatically correct
    svn path=/hippo-ecm/trunk/; revision=10310

--- repository/repository/engine/src/test/java/org/hippoecm/repository/HREPTWOIssueTest456.java
+++ repository/repository/engine/src/test/java/org/hippoecm/repository/HREPTWOIssueTest456.java
@@ -73,7 +73,7 @@ public class HREPTWOIssueTest456 extends TestCase {
          * session.getRootNode().getNode("test").getNode("n").getNode("n").getNodes().getSize() should equal 1
          * session.getRootNode().getNode("test").getNode("n").getNode("n").getNode("n").getNodes().getSize() should equal 1
          * etc...
-         * Currently, there is a bug, where each facet select in facet select adds the childs of the previous  the child nodes, see asserts:
+         * Currently, there is an undesired behavior (programmatically correct), where each facet select in facet select adds the childs of the previous  the child nodes, see asserts:
          */
         assertTrue( session.getRootNode().getNode("test").getNode("n").getNodes().getSize() == 1 );
         
