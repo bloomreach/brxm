82836706284cef75f214ba94c5a69540db0c2a96
Walter Grevink <w.grevink@onehippo.com>
2008-02-08 10:59:55 +0000

    HREPTWO-511 Other icon for virtual nodes TODO: better icon TODO: is same for console and cms
    svn path=/hippo-ecm/trunk/; revision=10340

--- cms/frontend/engine/src/main/java/org/hippoecm/frontend/tree/JcrTree.java
+++ cms/frontend/engine/src/main/java/org/hippoecm/frontend/tree/JcrTree.java
@@ -15,15 +15,23 @@
  */
 package org.hippoecm.frontend.tree;
 
+import javax.jcr.RepositoryException;
 import javax.swing.tree.TreeNode;
 
+import org.apache.wicket.ResourceReference;
+import org.apache.wicket.extensions.markup.html.tree.DefaultAbstractTree;
 import org.apache.wicket.extensions.markup.html.tree.Tree;
 import org.apache.wicket.markup.html.tree.ITreeState;
 import org.hippoecm.frontend.model.tree.AbstractTreeNode;
 import org.hippoecm.frontend.model.tree.JcrTreeModel;
+import org.hippoecm.repository.api.HippoNode;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public abstract class JcrTree extends Tree {
     private static final long serialVersionUID = 1L;
+
+    static final Logger log = LoggerFactory.getLogger(JcrTree.class);
 
     public JcrTree(String id, JcrTreeModel treeModel) {
         super(id, treeModel);
@@ -40,6 +48,25 @@ public abstract class JcrTree extends Tree {
     protected String renderNode(TreeNode treeNode) {
         AbstractTreeNode treeNodeModel = (AbstractTreeNode) treeNode;
         return treeNodeModel.renderNode();
+    }
+
+    @Override
+    protected ResourceReference getNodeIcon(TreeNode node) {
+        boolean isVirtual = false;
+        try {
+            if (node instanceof AbstractTreeNode) {
+                AbstractTreeNode treeNode = (AbstractTreeNode) node;
+                HippoNode jcrNode = treeNode.getNodeModel().getNode();
+                isVirtual = jcrNode.getCanonicalNode() == null || !jcrNode.getCanonicalNode().isSame(jcrNode);
+            }
+        } catch (RepositoryException e) {
+            log.error(e.getMessage(), e);
+        }
+        if (isVirtual) {
+            return new ResourceReference(JcrTree.class, "icons/virtual.gif");
+        }
+        return super.getNodeIcon(node);
+
     }
 
 }
