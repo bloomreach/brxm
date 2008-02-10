b457bda0ce8faa4ca01b17a9588a951ca04bead2
Berry van Halderen <b.vanhalderen@onehippo.com>
2008-02-10 14:51:23 +0000

    HREPTWO-509: provisions for showing not only variants, but also requests. (However the whole idea of a Document model in the front-end seriously needs to be refactored, as the current way is broken from the start and cannot be patched up.)
    svn path=/hippo-ecm/trunk/; revision=10362

--- cms/addon/cms-prototype/frontend/src/main/java/org/hippoecm/cmsprototype/frontend/plugins/variants/VariantsPlugin.java
+++ cms/addon/cms-prototype/frontend/src/main/java/org/hippoecm/cmsprototype/frontend/plugins/variants/VariantsPlugin.java
@@ -19,6 +19,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.jcr.RepositoryException;
+import javax.jcr.NodeIterator;
 
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.ajax.markup.html.AjaxLink;
@@ -28,6 +29,10 @@ import org.apache.wicket.markup.html.list.ListItem;
 import org.apache.wicket.markup.html.list.ListView;
 import org.apache.wicket.model.Model;
 import org.apache.wicket.model.PropertyModel;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import org.hippoecm.cmsprototype.frontend.model.content.Document;
 import org.hippoecm.cmsprototype.frontend.model.content.DocumentVariant;
 import org.hippoecm.cmsprototype.frontend.model.exception.ModelWrapException;
@@ -39,8 +44,8 @@ import org.hippoecm.frontend.plugin.PluginDescriptor;
 import org.hippoecm.frontend.plugin.channel.Channel;
 import org.hippoecm.frontend.plugin.channel.Notification;
 import org.hippoecm.frontend.plugin.channel.Request;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.hippoecm.repository.api.HippoNode;
+import org.hippoecm.repository.api.HippoNodeType;
 
 /**
  * Plugin for showing the available variants of a document
@@ -55,6 +60,9 @@ public class VariantsPlugin extends Plugin {
     private static final String VARIANTS_LIST = "variants";
     private static final String VARIANT_LABEL = "variant";
     private static final String VARIANT_LINK = "variantlink";
+    private static final String REQUESTS_LIST = "requests";
+    private static final String REQUEST_LABEL = "request";
+    private static final String REQUEST_LINK = "requestlink";
 
     // Default labels
     // TODO: needs i18m
@@ -62,7 +70,8 @@ public class VariantsPlugin extends Plugin {
 
     private Document document;
     private DocumentVariant selectedVariant;
-    
+    private JcrNodeModel selectedRequest;
+ 
     /**
      * The label displaying the node name
      */
@@ -72,6 +81,11 @@ public class VariantsPlugin extends Plugin {
      * The list containing the variants for a hippo:handle
      */
     protected List<DocumentVariant> variantsList = new ArrayList<DocumentVariant>();
+
+    /**
+     * The list containing other related documents for a hippo:handle
+     */
+    protected List<JcrNodeModel> requestList = new ArrayList<JcrNodeModel>();
 
     public VariantsPlugin(PluginDescriptor descriptor, IPluginModel model, Plugin parentPlugin) {
         super(descriptor, new JcrNodeModel(model), parentPlugin);
@@ -92,6 +106,10 @@ public class VariantsPlugin extends Plugin {
                     public void onClick(AjaxRequestTarget target) {
                         Channel channel = getDescriptor().getIncoming();
                         if (channel != null) {
+                            try {
+                            } catch(RepositoryException ex) {
+                                ex.printStackTrace();
+                            }
                             Request request = channel.createRequest("select", variant.getNodeModel());
                             channel.send(request);
                             request.getContext().apply(target);
@@ -109,10 +127,47 @@ public class VariantsPlugin extends Plugin {
 
             }
         };
-        add(new Label(NODE_NAME_LABEL, new PropertyModel(this, "nodeName")));
         add(listView);
+
+        ListView listView2 = new ListView(REQUESTS_LIST, requestList) {
+            private static final long serialVersionUID = 1L;
+
+            @Override
+            protected void populateItem(ListItem item) {
+                final JcrNodeModel model = (JcrNodeModel) item.getModelObject();
+
+                AjaxLink link = new AjaxLink(REQUEST_LINK, model) {
+                    private static final long serialVersionUID = 1L;
+
+                    @Override
+                    public void onClick(AjaxRequestTarget target) {
+                        Channel channel = getDescriptor().getIncoming();
+                        if (channel != null) {
+                            Request request = channel.createRequest("select", model);
+                            channel.send(request);
+                            request.getContext().apply(target);
+                        }
+                    }
+
+                };
+                
+                if (model.equals(selectedRequest)) {
+                    item.add(new AttributeAppender("class", new Model("selected"), " "));
+                }
+                
+                item.add(link);
+                try {
+                    link.add(new Label(REQUEST_LABEL, model.getNode().getProperty("type").getString() + " request"));
+                } catch(RepositoryException ex) {
+                    link.add(new Label(REQUEST_LABEL, "unknown request"));
+                }
+
+            }
+        };
+        add(listView2);
+
+        add(new Label(NODE_NAME_LABEL, new PropertyModel(this, "nodeName")));
     }
-    
 
     private void populateView(JcrNodeModel nodeModel) {
         
@@ -120,11 +175,13 @@ public class VariantsPlugin extends Plugin {
         try {
             WorkflowRequest request = new WorkflowRequest(nodeModel);
             selectedVariant = request.getDocumentVariant();
+            selectedRequest = nodeModel;
             if (selectedVariant != null) {
                 setDocument(selectedVariant.getDocument());
                 return;
             }
         } catch (ModelWrapException e) {
+            selectedRequest = null;
             selectedVariant = null;
         }
          
@@ -150,6 +207,7 @@ public class VariantsPlugin extends Plugin {
         try {
             setPluginModel(nodeModel);
             variantsList.clear();
+            requestList.clear();
             nodeName = nodeModel.getNode().getDisplayName();
         } catch (RepositoryException e) {
             log.error(e.getMessage());
@@ -157,13 +215,29 @@ public class VariantsPlugin extends Plugin {
         }
         
     }
-    
+
     private void setDocument(Document newDoc) {
         if (newDoc != null) {
             document = newDoc;
             setPluginModel(document.getNodeModel());
             variantsList.clear();
+            requestList.clear();
             variantsList.addAll(document.getVariants());
+
+            /* Duplication of code, showing that the usage of model knowledge
+             * in the model is plain wrong.
+             */
+            try {
+                for (NodeIterator iter = document.getNodeModel().getNode().getNodes(); iter.hasNext(); ) {
+                    HippoNode docNode = (HippoNode) iter.next();
+                    if (!docNode.isNodeType(HippoNodeType.NT_DOCUMENT)) {
+                        requestList.add(new JcrNodeModel(docNode));
+                    }
+                }
+            } catch(RepositoryException ex) {
+                log.error(ex.getMessage());
+            }
+
             nodeName = document.getName();
         }
         else {
