63f8f8a2f5bc7488bcaf07690b3018c6f9448a12
Berry van Halderen <b.vanhalderen@onehippo.com>
2008-02-11 12:53:23 +0000

    HREPTWO-523: enter workflow for demo
    svn path=/hippo-ecm/trunk/; revision=10377

--- cms/addon/standards/common/src/main/java/org/hippoecm/repository/standardworkflow/EditmodelWorkflow.java
+++ cms/addon/standards/common/src/main/java/org/hippoecm/repository/standardworkflow/EditmodelWorkflow.java
@@ -23,10 +23,20 @@ import org.hippoecm.repository.api.Workflow;
 import org.hippoecm.repository.api.WorkflowException;
 import org.hippoecm.repository.api.MappingException;
 
-public interface RemodelWorkflow extends Workflow {
+public interface EditmodelWorkflow extends Workflow {
     /**
-     * Instruct the repository to apply the new node definition overriding the
-     * earlier node definition.
+     * Returns the path of the JCR node containing the template definition to
+     * edit.  Because this function is invoked on the template definition
+     * itself this is at this time equivalent to returning the UUID of the
+     * same node.
      */
-    public String[] remodel(String cnd) throws WorkflowException, MappingException, RepositoryException, RemoteException;
+    public String edit() throws WorkflowException, MappingException, RepositoryException, RemoteException;
+
+    /**
+     * Return the path of the JCR node containing a new the template
+     * definition to edit.  This new template definition is created by first
+     * copying the node definition of the template definition indicated.
+     * The created template definition resides within the same namespace.
+     */
+    public String copy(String name) throws WorkflowException, MappingException, RepositoryException, RemoteException;
 }
