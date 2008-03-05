3a99b811114f53eeee0e61abdf4e6020d6b1a200
Frank van Lankvelt <f.vanlankvelt@onehippo.com>
2008-03-05 12:03:03 +0000

    HREPTWO-583: introducing the ItemRowPlugin; it groups template items in a row
    svn path=/hippo-ecm/trunk/; revision=10668

---
+++ cms/frontend/plugins/src/main/java/org/hippoecm/frontend/plugins/template/ItemRowPlugin.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2008 Hippo
+ *
+ * Licensed under the Apache License, Version 2.0 (the  "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.hippoecm.frontend.plugins.template;
+
+import org.hippoecm.frontend.model.IPluginModel;
+import org.hippoecm.frontend.plugin.Plugin;
+import org.hippoecm.frontend.plugin.PluginDescriptor;
+import org.hippoecm.frontend.template.ItemDescriptor;
+import org.hippoecm.frontend.template.model.ItemModel;
+import org.hippoecm.frontend.template.model.ItemProvider;
+import org.hippoecm.repository.api.HippoNodeType;
+
+public class ItemRowPlugin extends Plugin {
+    private static final long serialVersionUID = 1L;
+
+    public ItemRowPlugin(PluginDescriptor pluginDescriptor, IPluginModel pluginModel, Plugin parentPlugin) {
+        super(pluginDescriptor, new ItemModel(pluginModel), parentPlugin);
+
+        ItemModel model = (ItemModel) getPluginModel();
+        ItemDescriptor descriptor = model.getDescriptor();
+
+        ItemProvider provider = new ItemProvider(descriptor, model.getNodeModel());
+        add(new ItemView("fields", provider, this));
+
+        setOutputMarkupId(true);
+    }
+
+    @Override
+    public Plugin addChild(PluginDescriptor childDescriptor) {
+        if (!childDescriptor.getPluginId().equals(HippoNodeType.HIPPO_ITEM)) {
+            return super.addChild(childDescriptor);
+        }
+        return null;
+    }
+
+}
