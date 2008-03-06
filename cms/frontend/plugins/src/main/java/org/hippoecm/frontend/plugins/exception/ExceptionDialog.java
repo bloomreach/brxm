854457d8761b4bf87a2ce609bf6aa3b81fc8d00e
Ard Schrijvers <a.schrijvers@onehippo.com>
2008-03-06 15:36:44 +0000

    HREPTWO-590 enable a dialog which can display the message of an exception
    svn path=/hippo-ecm/trunk/; revision=10680

---
+++ cms/frontend/plugins/src/main/java/org/hippoecm/frontend/plugins/exception/ExceptionDialog.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2007 Hippo
+ *
+ * Licensed under the Apache License, Version 2.0 (the  "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.hippoecm.frontend.plugins.exception;
+
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.extensions.ajax.markup.html.modal.ModalWindow;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.model.PropertyModel;
+import org.hippoecm.frontend.dialog.AbstractDialog;
+import org.hippoecm.frontend.dialog.DialogWindow;
+import org.hippoecm.frontend.plugin.channel.Channel;
+
+public class ExceptionDialog extends AbstractDialog {
+    private static final long serialVersionUID = 1L;
+
+    private String exception_message = "";
+
+    public ExceptionDialog(DialogWindow dialogWindow, Channel channel) {
+        super(dialogWindow, channel);
+        dialogWindow.setTitle("Exception");
+
+        final Label exceptionMessageLabel = new Label("exception-message", new PropertyModel(this, "exception_message"));
+        exceptionMessageLabel.setOutputMarkupId(true);
+        add(exceptionMessageLabel);
+
+        cancel.setVisible(false);
+
+        dialogWindow.setWindowClosedCallback(new ModalWindow.WindowClosedCallback() {
+            private static final long serialVersionUID = 1L;
+
+            public void onClose(AjaxRequestTarget target) {
+            }
+        });
+    }
+
+    @Override
+    protected void ok() throws Exception {
+    }
+
+    @Override
+    protected void cancel() {
+    }
+
+    public void setExceptionMessage(Exception exception) {
+        this.exception_message = exception.getMessage();
+    }
+
+}
