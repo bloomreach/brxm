08a428394396bff511be3cdbc54e5b677edd3a2e
Arthur Bogaart <a.bogaart@onehippo.com>
2008-03-18 15:12:04 +0000

    HREPTWO-646  - added minimal system configuration for XinhaPlugin to system-templates  - refactored XinhaPlugin.Configuration classes and added logic for handling hippo:parameters  - XinhaEditorBehaviour will write specific configurations for every Xinha instance on the page
    svn path=/hippo-ecm/trunk/; revision=10789

--- repository/addon/xinha-frontend/src/main/java/org/hippoecm/repository/frontend/xinha/XinhaEditorBehavior.java
+++ repository/addon/xinha-frontend/src/main/java/org/hippoecm/repository/frontend/xinha/XinhaEditorBehavior.java
@@ -17,6 +17,7 @@ package org.hippoecm.repository.frontend.xinha;
 
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.Map;
 import java.util.Set;
 
 import org.apache.wicket.Page;
@@ -29,11 +30,13 @@ import org.apache.wicket.markup.html.IHeaderResponse;
 class XinhaEditorBehavior extends AbstractHeaderContributor {
     private static final long serialVersionUID = 1L;
 
+    protected static final String SINGLE_QUOTE = "'";
+
     private Page page;
-    private Set configurations;
+    private Set<XinhaPlugin.Configuration> configurations;
 
     XinhaEditorBehavior(Page page) {
-        configurations = new HashSet();
+        configurations = new HashSet<XinhaPlugin.Configuration>();
         this.page = page;
         page.add(this);
     }
@@ -42,8 +45,12 @@ class XinhaEditorBehavior extends AbstractHeaderContributor {
         configurations.add(conf);
     }
 
+    /**
+     * Remove configuration from behavior, if no more configurations left, remove behavior from page
+     * @param conf Configuration to be removed
+     */
     void unregister(XinhaPlugin.Configuration conf) {
-        if(configurations.contains(conf)) {
+        if (configurations.contains(conf)) {
             configurations.remove(conf);
             if (configurations.size() == 0) {
                 for (Iterator iter = page.getBehaviors().iterator(); iter.hasNext();) {
@@ -56,92 +63,126 @@ class XinhaEditorBehavior extends AbstractHeaderContributor {
         }
     }
 
+    private String serialize2JS(String value) {
+        if (value == null)
+            return "null";
+        else if (value.equalsIgnoreCase("true"))
+            return "true";
+        else if (value.equalsIgnoreCase("false"))
+            return "false";
+        else if (!value.matches("\\d*") && !value.matches("(\\w+)\\.(\\w+)"))
+            return SINGLE_QUOTE + value.replaceAll(SINGLE_QUOTE, "\\\\" + SINGLE_QUOTE) + SINGLE_QUOTE;
+        return value;
+    }
+
     public final IHeaderContributor[] getHeaderContributors() {
         return null;
     }
 
     IHeaderContributor[] getHeaderContributorsPartly() {
-         
+
         return new IHeaderContributor[] {
 
-            new IHeaderContributor() {
-                private static final long serialVersionUID = 1L;
+        new IHeaderContributor() {
+            private static final long serialVersionUID = 1L;
 
-                public void renderHead(IHeaderResponse response) {
-                    StringBuffer sb = new StringBuffer();
-                    sb.append("_editor_url = 'xinha/xinha/';\n");
-                    sb.append("_editor_lang = 'en';\n");
-                    response.renderJavascript(sb, null);
+            public void renderHead(IHeaderResponse response) {
+                StringBuffer sb = new StringBuffer();
+                sb.append("_editor_url = 'xinha/xinha/';\n");
+                sb.append("_editor_lang = 'en';\n");
+                response.renderJavascript(sb, null);
+            }
+        },
+
+        HeaderContributor.forJavaScript("xinha/xinha/XinhaLoader.js"),
+
+        new IHeaderContributor() {
+            private static final long serialVersionUID = 1L;
+
+            public void renderHead(IHeaderResponse response) {
+                Set<XinhaPlugin.PluginConfiguration> plugins = new HashSet<XinhaPlugin.PluginConfiguration>();
+                for (XinhaPlugin.Configuration config : configurations) {
+                    for (XinhaPlugin.PluginConfiguration pluginConfig : config.getPluginConfigurations()) {
+                        plugins.add(pluginConfig);
+                    }
                 }
-            },
-
-            HeaderContributor.forJavaScript("xinha/xinha/XinhaLoader.js"),
-
-            new IHeaderContributor() {
-                private static final long serialVersionUID = 1L;
-
-                public void renderHead(IHeaderResponse response) {
-                    StringBuffer sb = new StringBuffer();
-                    Set plugins = new HashSet();
-                    for (Iterator iter = configurations.iterator(); iter.hasNext();) {
-                        XinhaPlugin.Configuration conf = (XinhaPlugin.Configuration) iter.next();
-                        String[] plugin = conf.getPlugins();
-                        if (plugin != null) {
-                            for (int i = 0; i < plugin.length; i++)
-                                plugins.add(plugin[i]);
+                
+                System.out.println("NU BEN IK ER WEL EEN BEETJE KLAAR MEE!! -------------------------------");
+                
+                StringBuffer sb = new StringBuffer();
+                sb.append("xinha_editors = null;\n");
+                sb.append("xinha_init    = null;\n");
+                sb.append("xinha_config  = null;\n");
+                sb.append("xinha_plugins = null;\n");
+                sb.append("xinha_init = xinha_init ? xinha_init : function()\n");
+                sb.append("{\n");
+                sb.append("  xinha_editors = xinha_editors ? xinha_editors :\n");
+                appendAsJSArray(sb, configurations);
+                sb.append(";\n");
+                sb.append("  xinha_plugins = xinha_plugins ? xinha_plugins :\n");
+                appendAsJSArray(sb, plugins);
+                sb.append(";\n");
+                sb.append("  if(!Xinha.loadPlugins(xinha_plugins, xinha_init)) return;\n");
+                sb.append("  xinha_config = xinha_config ? xinha_config() : new Xinha.Config();\n");
+                sb.append("  xinha_config.pageStyleSheets = [ _editor_url + \"examples/full_example.css\" ];\n");
+                sb.append("  xinha_editors   = Xinha.makeEditors(xinha_editors, xinha_config);\n");
+                for (XinhaPlugin.Configuration config : configurations) {
+                    sb.append("  if(xinha_editors.").append(config.getName()).append(" != undefined) {\n");
+                    sb.append("    xinha_editors.").append(config.getName()).append(".registerPlugins(");
+                    appendAsJSArray(sb, config.getPluginConfigurations());
+                    sb.append(");\n");
+                    
+                    String prefix = "    xinha_editors." + config.getName() + ".config.";
+                    appendProperties(sb, prefix, config.getProperties());
+                    
+                    if(config.getToolbarItems() != null && config.getToolbarItems().size() > 0) {
+                        sb.append(prefix).append("toolbar = [[");
+                        for (Iterator<String> it = config.getToolbarItems().iterator(); it.hasNext();) {
+                            sb.append(serialize2JS(it.next()));
+                            if (it.hasNext())
+                                sb.append(",");
                         }
+                        sb.append("]];\n");
                     }
-
-                    sb.append("xinha_editors = null;\n");
-                    sb.append("xinha_init    = null;\n");
-                    sb.append("xinha_config  = null;\n");
-                    sb.append("xinha_plugins = null;\n");
-                    sb.append("xinha_init = xinha_init ? xinha_init : function()\n");
-                    sb.append("{\n");
-                    sb.append("  xinha_editors = xinha_editors ? xinha_editors :\n");
-                    sb.append("  [\n");
-                    for (Iterator iter = configurations.iterator(); iter.hasNext();) {
-                        sb.append("    '");
-                        sb.append(((XinhaPlugin.Configuration) iter.next()).getName());
-                        sb.append("'");
-                        if (iter.hasNext())
-                            sb.append(",");
-                        sb.append("\n");
+                    for (XinhaPlugin.PluginConfiguration pluginConfig : config.getPluginConfigurations()) {
+                        appendProperties(sb, prefix  + pluginConfig.getName() + ".", pluginConfig.getProperties());
                     }
-                    sb.append("  ];\n");
-                    sb.append("  xinha_plugins = xinha_plugins ? xinha_plugins :\n");
-                    sb.append("  [\n");
-                    for (Iterator iter = plugins.iterator(); iter.hasNext();) {
-                        sb.append("    '");
-                        sb.append((String) iter.next());
-                        sb.append("'");
-                        if (iter.hasNext())
-                            sb.append(",");
-                        sb.append("\n");
-                    }
-                    sb.append("  ];\n");
-                    sb.append("  if(!Xinha.loadPlugins(xinha_plugins, xinha_init)) return;\n");
-                    sb.append("  xinha_config = xinha_config ? xinha_config() : new Xinha.Config();\n");
-                    sb.append("  xinha_config.width = '500px';\n");
-                    sb.append("  xinha_config.height = '300px';\n");
-                    sb.append("  xinha_config.toolbar = [['popupeditor','formatblock', 'bold','italic','underline']];\n");
-                    sb.append("  xinha_config.pageStyleSheets = [ _editor_url + \"examples/full_example.css\" ];\n");
-                    sb.append("  xinha_editors   = Xinha.makeEditors(xinha_editors, xinha_config, xinha_plugins);\n");
-                    sb.append("  Xinha.startEditors(xinha_editors);\n");
-                    sb.append("}\n");
-                    sb.append("Xinha._addEvent(window,'load', xinha_init);\n");
-
-                    response.renderJavascript(sb, null);
+                    sb.append("  }\n");
                 }
-            },
+                sb.append("  Xinha.startEditors(xinha_editors);\n");
+                sb.append("}\n");
+                sb.append("Xinha._addEvent(window,'load', xinha_init);\n");
+                response.renderJavascript(sb, null);
+            }
 
-            new IHeaderContributor() {
-                private static final long serialVersionUID = 1L;
+            private void appendAsJSArray(StringBuffer sb, Set<? extends XinhaPlugin.BaseConfiguration> configs) {
+                sb.append("  [\n");
+                for (Iterator<? extends XinhaPlugin.BaseConfiguration> iter = configs.iterator(); iter.hasNext();) {
+                    sb.append("    ");
+                    sb.append(serialize2JS(iter.next().getName()));
+                    if (iter.hasNext())
+                        sb.append(",");
+                    sb.append("\n");
+                }
+                sb.append("  ]");
+            }
 
-                public void renderHead(IHeaderResponse response) {
-                    response.renderOnLoadJavascript("xinha_init();");
+            private void appendProperties(StringBuffer sb, String prefix, Map<String, String> properties) {
+                for (String key : properties.keySet()) {
+                    sb.append(prefix);
+                    sb.append(key).append(" = ").append(serialize2JS(properties.get(key)));
+                    sb.append(";\n");
                 }
             }
+        },
+
+        new IHeaderContributor() {
+            private static final long serialVersionUID = 1L;
+
+            public void renderHead(IHeaderResponse response) {
+                response.renderOnLoadJavascript("xinha_init();");
+            }
+        }
 
         };
     }
