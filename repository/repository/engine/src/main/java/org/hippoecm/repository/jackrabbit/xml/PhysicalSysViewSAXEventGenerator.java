d3748e4d3e0018a632914b68b1b4b2bb0a92351a
Ard Schrijvers <a.schrijvers@onehippo.com>
2008-03-20 16:40:30 +0000

    HREPTWO-603 from now on, only fysical nodes are exported with exportSystemView. For jcr compliance reasons, we might choose to give the method another name instead of overriding the one from jackrabbit
    svn path=/hippo-ecm/trunk/; revision=10832

---
+++ repository/repository/engine/src/main/java/org/hippoecm/repository/jackrabbit/xml/PhysicalSysViewSAXEventGenerator.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2007 Hippo
+ *
+ * Licensed under the Apache License, Version 2.0 (the  "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.hippoecm.repository.jackrabbit.xml;
+
+import javax.jcr.ItemNotFoundException;
+import javax.jcr.Node;
+import javax.jcr.Property;
+import javax.jcr.RepositoryException;
+
+import org.apache.jackrabbit.core.xml.SysViewSAXEventGenerator;
+import org.hippoecm.repository.api.HippoNode;
+import org.hippoecm.repository.api.HippoNodeType;
+import org.xml.sax.ContentHandler;
+import org.xml.sax.SAXException;
+
+public class PhysicalSysViewSAXEventGenerator extends SysViewSAXEventGenerator {
+
+    public PhysicalSysViewSAXEventGenerator(Node node, boolean noRecurse, boolean skipBinary,
+            ContentHandler contentHandler) throws RepositoryException {
+        super(node, noRecurse, skipBinary, contentHandler);
+    }
+
+    @Override
+    protected void process(Node node, int level) throws RepositoryException, SAXException {
+        if (node instanceof HippoNode) {
+            HippoNode hippoNode = (HippoNode) node;
+            try {
+                if (hippoNode.getCanonicalNode() == null) {
+                    // virtual node
+                    return;
+                }
+                if (!hippoNode.getCanonicalNode().isSame(hippoNode)) {
+                    // virtual node
+                    return;
+                }
+            } catch (ItemNotFoundException e) {
+                // can happen only for virtual nodes while HREPTWO-599
+                return;
+            }
+
+        }
+        // if here, we have a physical node: continue
+        super.process(node, level);
+    }
+
+    @Override
+    protected void process(Property prop, int level) throws RepositoryException, SAXException {
+
+        if (prop.getParent().getPrimaryNodeType().getName().equals(HippoNodeType.NT_FACETSEARCH)
+                && HippoNodeType.HIPPO_COUNT.equals(prop.getName())) {
+            // this is a virtual hippo:count property
+            return;
+        }
+        super.process(prop, level);
+    }
+
+}
