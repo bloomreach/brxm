c8ff1e05f2b0078f3c96ced2611b8510e02c249f
Walter Grevink <w.grevink@onehippo.com>
2008-04-01 23:00:09 +0000

    HREPTWO-725 quick fix
    svn path=/hippo-ecm/trunk/; revision=10975

--- cms/addon/cms/browse/src/main/java/org/hippoecm/frontend/plugins/cms/browse/FolderTreePlugin.java
+++ cms/addon/cms/browse/src/main/java/org/hippoecm/frontend/plugins/cms/browse/FolderTreePlugin.java
@@ -15,30 +15,43 @@
  */
 package org.hippoecm.frontend.plugins.cms.browse;
 
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import javax.jcr.Node;
 import javax.swing.tree.TreeNode;
 
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.extensions.markup.html.tree.Tree;
 import org.hippoecm.frontend.model.IPluginModel;
 import org.hippoecm.frontend.model.JcrNodeModel;
+import org.hippoecm.frontend.model.PluginModel;
 import org.hippoecm.frontend.model.tree.AbstractTreeNode;
 import org.hippoecm.frontend.model.tree.JcrTreeModel;
 import org.hippoecm.frontend.plugin.Plugin;
 import org.hippoecm.frontend.plugin.PluginDescriptor;
+import org.hippoecm.frontend.plugin.channel.Channel;
+import org.hippoecm.frontend.plugin.channel.Request;
 import org.hippoecm.frontend.tree.AbstractTreePlugin;
-import org.hippoecm.frontend.tree.JcrTree;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * A tree containing nodes of type FolderTreeNode
- *
  */
 public class FolderTreePlugin extends AbstractTreePlugin {
     private static final long serialVersionUID = 1L;
+    
+    static final Logger log = LoggerFactory.getLogger(FolderTreePlugin.class);
 
     public FolderTreePlugin(PluginDescriptor pluginDescriptor, IPluginModel model, Plugin parentPlugin) {
         super(pluginDescriptor, new FolderTreeNode(new JcrNodeModel(model)), parentPlugin);
     }
-    
+
     @Override
     protected Tree newTree(JcrTreeModel treeModel) {
         return new CmsJcrTree("tree", treeModel) {
@@ -50,5 +63,37 @@ public class FolderTreePlugin extends AbstractTreePlugin {
                 FolderTreePlugin.this.onSelect(treeNodeModel, target);
             }
         };
+    }
+
+    @Override
+    protected void onSelect(final AbstractTreeNode treeNodeModel, AjaxRequestTarget target) {
+        super.onSelect(treeNodeModel, target);
+        Channel channel = getTopChannel();
+        if (channel != null) {
+            // create and send a "list" request with the children 
+            // of the selected node as a parameters
+            IPluginModel listModel = new PluginModel() {
+                private static final long serialVersionUID = 1L;
+
+                public Map<String, Object> getMapRepresentation() {
+                    Map<String, Object> map = new HashMap<String, Object>();
+                    try {
+                        map.put("node", treeNodeModel.getNodeModel());
+                        List<String> entries = new ArrayList<String>();
+                        Iterator<Node> childNodes = treeNodeModel.getNodeModel().getNode().getNodes();
+                        while (childNodes.hasNext()) {
+                            entries.add(childNodes.next().getPath());
+                        }
+                        map.put("entries", entries);
+                    } catch (Exception e) {
+                        log.error(e.getMessage());
+                    }
+                    return map;
+                }
+            };
+            Request list = channel.createRequest("list", listModel);
+            channel.send(list);
+            list.getContext().apply(target);
+        }
     }
 }
