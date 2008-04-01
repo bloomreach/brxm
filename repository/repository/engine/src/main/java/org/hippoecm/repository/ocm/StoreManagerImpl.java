40fac9a8250ae1a82af646f6daa067bae7fc0346
Berry van Halderen <b.vanhalderen@onehippo.com>
2008-04-01 13:32:17 +0000

    HREPTWO-709: automatically set mix:referenceable on requests as well
    svn path=/hippo-ecm/trunk/; revision=10961

--- repository/repository/engine/src/main/java/org/hippoecm/repository/ocm/StoreManagerImpl.java
+++ repository/repository/engine/src/main/java/org/hippoecm/repository/ocm/StoreManagerImpl.java
@@ -186,6 +186,9 @@ public class StoreManagerImpl extends StoreManager {
             if(node.isNodeType(HippoNodeType.NT_DOCUMENT)) {
                 node.addMixin(HippoNodeType.NT_HARDDOCUMENT);
             }
+            if(node.isNodeType(HippoNodeType.NT_REQUEST)) {
+                node.addMixin("mix:referenceable");
+            }
             sm.provideFields(cmd.getAllFieldNumbers(), new FieldManagerImpl(sm, session, types, node));
         } catch (PathNotFoundException ex) {
             System.err.println("PathNotFoundException: " + ex.getMessage());
