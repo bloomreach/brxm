d3d2d95cecf7a25fbc84dd2a9abf52136eb691ff
Walter Grevink <w.grevink@onehippo.com>
2008-04-01 22:30:57 +0000

    HREPTWO-638, HREPTWO-639
    1)reviewed actions workflow now deletes request node after reject, accept and cancel
    2)show link to handle in dashboard view in case no variant can be found
    3)set dashboard views max length to 15
    4)added some if != null checks to ActionsPlugin to avoid stacktraces
    
    svn path=/hippo-ecm/trunk/; revision=10974

--- repository/addon/cms/edit/src/main/java/org/hippoecm/frontend/plugins/cms/action/ActionPlugin.java
+++ repository/addon/cms/edit/src/main/java/org/hippoecm/frontend/plugins/cms/action/ActionPlugin.java
@@ -16,36 +16,22 @@
 
 package org.hippoecm.frontend.plugins.cms.action;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import javax.jcr.Node;
 import javax.jcr.NodeIterator;
 import javax.jcr.RepositoryException;
 
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.ajax.markup.html.AjaxLink;
-import org.apache.wicket.behavior.AttributeAppender;
-import org.apache.wicket.markup.html.basic.Label;
-import org.apache.wicket.markup.html.list.ListItem;
-import org.apache.wicket.markup.html.list.ListView;
-import org.apache.wicket.model.Model;
-import org.apache.wicket.model.PropertyModel;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import org.hippoecm.frontend.dialog.DialogLink;
 import org.hippoecm.frontend.model.IPluginModel;
 import org.hippoecm.frontend.model.JcrNodeModel;
 import org.hippoecm.frontend.plugin.Plugin;
 import org.hippoecm.frontend.plugin.PluginDescriptor;
 import org.hippoecm.frontend.plugin.channel.Channel;
-import org.hippoecm.frontend.plugin.channel.ChannelFactory;
 import org.hippoecm.frontend.plugin.channel.Notification;
 import org.hippoecm.frontend.plugin.channel.Request;
 import org.hippoecm.repository.api.HippoNode;
-import org.hippoecm.repository.api.HippoNodeType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ActionPlugin extends Plugin {
     private static final long serialVersionUID = 1L;
@@ -72,13 +58,6 @@ public class ActionPlugin extends Plugin {
 
         };
         add(edit);
-
-        JcrNodeModel jcrModel = (JcrNodeModel) getModel();
-        Channel channel = getTopChannel();
-        ChannelFactory factory = getPluginManager().getChannelFactory();
-
-        // setVisibilities();
-        edit.setVisible(true);
     }
 
     @Override
@@ -88,34 +67,39 @@ public class ActionPlugin extends Plugin {
 
             try {
                 HippoNode n = model.getNode();
-                System.err.println("received "+n.getPath());
-                if(n.isNodeType("hippo:handle")) {
-                    Node best = null;
-                    for(NodeIterator iter = n.getNodes(); iter.hasNext(); ) {
-                        Node sub = iter.nextNode();
-                        if(sub.isNodeType("hippo:document")) {
-                            if(sub.hasProperty("hippostd:state")) {
-                                if(sub.getProperty("hippostd:state").getString().equals("draft")) {
-                                    best = sub;
-                                } else if(sub.getProperty("hippostd:state").getString().equals("unpublished") && (best == null || !best.getProperty("hippostd:state").getString().equals("draft"))) {
-                                    best = sub;
-                                } else if(sub.getProperty("hippostd:state").getString().equals("unpublished") && best == null) {
-                                    best = sub;
+                if (n != null) {
+                    System.err.println("received " + n.getPath());
+                    if (n.isNodeType("hippo:handle")) {
+                        Node best = null;
+                        for (NodeIterator iter = n.getNodes(); iter.hasNext();) {
+                            Node sub = iter.nextNode();
+                            if (sub.isNodeType("hippo:document")) {
+                                if (sub.hasProperty("hippostd:state")) {
+                                    if (sub.getProperty("hippostd:state").getString().equals("draft")) {
+                                        best = sub;
+                                    } else if (sub.getProperty("hippostd:state").getString().equals("unpublished")
+                                            && (best == null || !best.getProperty("hippostd:state").getString().equals(
+                                                    "draft"))) {
+                                        best = sub;
+                                    } else if (sub.getProperty("hippostd:state").getString().equals("unpublished")
+                                            && best == null) {
+                                        best = sub;
+                                    }
                                 }
                             }
                         }
-                    }
-                    if(best != null) {
-                        model = new JcrNodeModel(best);
-                        Channel channel = getTopChannel();
-                        if (channel != null) {
-                            Request request = channel.createRequest("select", model);
-                            channel.send(request);
-                            // request.getContext().apply(model);
+                        if (best != null) {
+                            model = new JcrNodeModel(best);
+                            Channel channel = getTopChannel();
+                            if (channel != null) {
+                                Request request = channel.createRequest("select", model);
+                                channel.send(request);
+                                // request.getContext().apply(model);
+                            }
                         }
                     }
                 }
-            } catch(RepositoryException ex) {
+            } catch (RepositoryException ex) {
                 log.error(ex.getMessage());
             }
             // populateView(new JcrNodeModel(notification.getModel()));
@@ -123,16 +107,18 @@ public class ActionPlugin extends Plugin {
 
             try {
                 Node node = new JcrNodeModel(getPluginModel()).getNode();
-                Node canonical = ((HippoNode)node).getCanonicalNode();
-                boolean isRoot = node.isNodeType("rep:root");            
-                boolean isVirtual = canonical == null || !canonical.isSame(node);
-                if(isRoot || isVirtual) {
-                    edit.setVisible(false);
-                } else if(canonical != null && canonical.hasProperty("hippostd:state")) {
-                    System.err.println("BERRY "+canonical.getProperty("hippostd:state").getString());
-                    edit.setVisible(canonical.getProperty("hippostd:state").getString().equals("draft"));
+                if (node != null) {
+                    Node canonical = ((HippoNode) node).getCanonicalNode();
+                    boolean isRoot = node.isNodeType("rep:root");
+                    boolean isVirtual = canonical == null || !canonical.isSame(node);
+                    if (isRoot || isVirtual) {
+                        edit.setVisible(false);
+                    } else if (canonical != null && canonical.hasProperty("hippostd:state")) {
+                        System.err.println("BERRY " + canonical.getProperty("hippostd:state").getString());
+                        edit.setVisible(canonical.getProperty("hippostd:state").getString().equals("draft"));
+                    }
                 }
-            } catch(RepositoryException ex) {
+            } catch (RepositoryException ex) {
                 log.error(ex.getMessage());
                 edit.setVisible(false);
             }
