7c64d171df3f5f8891db3d04560d77f69068cffd
Jeroen Hoffman <j.hoffman@onehippo.com>
2008-04-03 14:29:45 +0000

    HREPTWO-573: make the DirectAccessServlet also use urlBasePath and repositoryBaseLocation, enabling (again?) to view images, also from browser location bar
    svn path=/hippo-ecm/trunk/; revision=11018

--- cms/hst/taglib/src/main/java/org/hippoecm/hst/ContextFilter.java
+++ cms/hst/taglib/src/main/java/org/hippoecm/hst/ContextFilter.java
@@ -32,26 +32,29 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.slf4j.LoggerFactory;
-import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.slf4j.Logger;
 
 /**
  * Filter that creates a context available for expression language.  
  */
 public class ContextFilter implements Filter {
-    private static final Logger logger = LoggerFactory.getLogger(ContextFilter.class);
+
+    //    private static final Logger logger = LoggerFactory.getLogger(ContextFilter.class);
     public static final String ENCODING_SCHEME = "UTF-8";
 
     public static final String ATTRIBUTE_NAME = ContextFilter.class.getName() + ".ATTRIBUTE_NAME";
+    public static final String URL_BASE_PATH = ContextFilter.class.getName() + ".URL_BASE_PATH";
+    public static final String REPOSITORY_BASE_LOCATION = ContextFilter.class.getName() + ".REPOSITORY_BASE_LOCATION";
     public static final String URL_MAPPING_LOCATION = ContextFilter.class.getName() + ".URL_MAPPING_LOCATION";
 
     private String attributeName = "context";
     private String urlBasePath = "";
-    private String repositoryBaseLocation; // [BvH] I'm not fond of this repositoryBaseLocation as the AdditionalContextFilter is a far more elegant solution
+    private String repositoryBaseLocation;
     private String urlMappingLocation = "/urlMapping";
     private String[] skippedExtensions = 
-        new String[] {"ico", "gif", "jpg", "jpeg", "svg", "png", "css", "js"};
-    private final List<String> skippedExtensionsList = new ArrayList<String>(); 
+        new String[] { "ico", "gif", "jpg", "jpeg", "svg", "png", "css", "js" };
+    private final List<String> skippedExtensionsList = new ArrayList<String>();
     boolean urlMappingActive = false;
 
     public ContextFilter() {
@@ -61,41 +64,45 @@ public class ContextFilter implements Filter {
     // from interface
     public void init(FilterConfig filterConfig) throws ServletException {
 
+        // get attributeName, save in context for use by tags
         String param = filterConfig.getInitParameter("attributeName");
         if (param != null && !param.trim().equals("")) {
             this.attributeName = param.trim();
         }
-
-        // save in context for use by tags
         filterConfig.getServletContext().setAttribute(ATTRIBUTE_NAME, this.attributeName);
 
+        // get urlBasePath, save in context for use by DirectAccessServlet
         param = filterConfig.getInitParameter("urlBasePath");
         if (param != null && !param.trim().equals("")) {
             this.urlBasePath = param;
         }
-        
+        filterConfig.getServletContext().setAttribute(URL_BASE_PATH, this.urlBasePath);
+
+        // get repositoryBaseLocation, save in context for use by DirectAccessServlet
         param = filterConfig.getInitParameter("repositoryBaseLocation");
         if (param != null && !param.trim().equals("")) {
             this.repositoryBaseLocation = param;
         } else {
             this.repositoryBaseLocation = urlBasePath;
         }
+        filterConfig.getServletContext().setAttribute(REPOSITORY_BASE_LOCATION, this.repositoryBaseLocation);
+
         
+        // get urlMappingLocation, save in context for use by IncludeTag
         param = filterConfig.getInitParameter("urlMappingLocation");
         if (param != null && !param.trim().equals("")) {
             this.urlMappingLocation = param;
 
-        } 
-
-        // save in context for use by IncludeTag
+        }
         filterConfig.getServletContext().setAttribute(URL_MAPPING_LOCATION, this.attributeName);
 
+        // get skippedExtensions
         param = filterConfig.getInitParameter("skippedExtensions");
         if (param != null && !param.trim().equals("")) {
             this.skippedExtensions = param.split(",");
-        } 
+        }
 
-        // convert to list for easy 'contains' access
+        // convert extensions to list for easy 'contains' access
         for (int i = 0; i < skippedExtensions.length; i++) {
             String extension = skippedExtensions[i];
             if (!extension.startsWith(".")) {
@@ -111,23 +118,23 @@ public class ContextFilter implements Filter {
 
     // from interface
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) 
-                        throws IOException, ServletException {
-        
+		    			throws IOException, ServletException {
+
         HttpServletRequest req = (HttpServletRequest) request;
         HttpServletResponse res = (HttpServletResponse) response;
 
         String servletPath = req.getServletPath();
 
-        if (servletPath.lastIndexOf(".") >= 0) { 
-            // [BvH] We have lost here the essential ability to generate pictures, nice...
+        if (servletPath.lastIndexOf(".") >= 0) {
+			// images are to be retrieved with the DirectAccessServlet        
             String extension = servletPath.substring(servletPath.lastIndexOf("."));
 
             if (skippedExtensionsList.contains(extension)) {
                 filterChain.doFilter(req, res);
                 return;
-            }   
+            }
         }
-        
+
         String relativePath = req.getRequestURI();
         /* This next part is better resolved by using filterConfig.getServletContext().getContextPath(), but
          * this is only available after Servlet API 2.5.
@@ -138,13 +145,13 @@ public class ContextFilter implements Filter {
         if (relativePath.startsWith(req.getContextPath())) {
             relativePath = relativePath.substring(req.getContextPath().length());
         }
-        
+
         // remove session-related part of url
         int semicolonIdx = relativePath.indexOf(';');
         if (semicolonIdx != -1) {
             relativePath = relativePath.substring(0, semicolonIdx);
         }
-        
+
         // remove end /
         if (relativePath.endsWith("/")) {
             relativePath = relativePath.substring(0, relativePath.length() - 1);
@@ -154,9 +161,9 @@ public class ContextFilter implements Filter {
         if (relativePath.startsWith(urlBasePath)) {
             relativePath = relativePath.substring(urlBasePath.length());
         }
-        
+
         // decode
-        relativePath = URLDecoder.decode(relativePath, ENCODING_SCHEME);   
+        relativePath = URLDecoder.decode(relativePath, ENCODING_SCHEME);
 
         Session jcrSession = JCRConnector.getJCRSession(req.getSession());
         Context context = new Context(jcrSession, urlBasePath, repositoryBaseLocation);
@@ -166,25 +173,24 @@ public class ContextFilter implements Filter {
 
         // check url mapping
         if (urlMappingActive) {
-            
+
             URLMappingResponseWrapper responseWrapper = new URLMappingResponseWrapper(context, req, res);
 
             try {
                 String mappedPage = responseWrapper.mapRepositoryDocument(urlMappingLocation, context.getLocation());
-                
-                if (mappedPage == null) {
-                    logger.warn("No mapped page could be found for path " + context.getLocation());
-                    throw new ServletException("No mapped page could be found for path " + context.getLocation());
-                } else {
+
+                // mapping allowed to fail, use-case is a /images url with this filter in root
+                if (mappedPage != null) {
+
                     // forward the request to that page
                     RequestDispatcher dispatcher = request.getRequestDispatcher(mappedPage);
-                    
+
                     if (dispatcher == null) {
                         throw new ServletException("No dispatcher could be obtained for mapped page " + mappedPage);
                     }
-                    
+
                     dispatcher.forward(request, responseWrapper);
-    
+
                     // no further filter chaining when forwarding
                     return;
                 }   
