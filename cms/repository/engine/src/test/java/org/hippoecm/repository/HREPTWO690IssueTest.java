f8e9e0a53b7433fedc69a0e9ff310958b39093dd
Berry van Halderen <b.vanhalderen@onehippo.com>
2008-04-16 07:59:25 +0000

    HREPTWO-690: Added unit issue test
    svn path=/hippo-ecm/trunk/; revision=11116

---
+++ cms/repository/engine/src/test/java/org/hippoecm/repository/HREPTWO690IssueTest.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2007 Hippo
+ *
+ * Licensed under the Apache License, Version 2.0 (the  "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.hippoecm.repository;
+
+import javax.jcr.Node;
+import javax.jcr.RepositoryException;
+
+import org.hippoecm.repository.TestCase;
+
+public class HREPTWO690IssueTest extends TestCase {
+    private String[] content1 = {
+        "/test", "nt:unstructured",
+        "/test/docs", "nt:unstructured",
+        "jcr:mixinTypes", "mix:referenceable",
+        "/test/docs/funny", "hippo:document",
+        "jcr:mixinTypes", "hippo:harddocument",
+        "jcr:mixinTypes", "hst:page",
+        "hst:pageType", "jsp",
+        "hst:pageFile", "index.jsp",
+    };
+    private String[] content2 = {
+        "/test/nav", "hippo:facetselect",
+        "hippo:docbase", "/test/docs",
+        "hippo:facets", "lang",
+        "hippo:values", "en",
+        "hippo:modes", "select"
+    };
+
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    public void testIssue() throws RepositoryException {
+        Node result;
+        build(session, content1);
+        build(session, content2);
+        session.save();
+        session.refresh(false);
+        //Utilities.dump(session.getRootNode());
+
+        result = traverse(session, "/test/docs/funny");
+        assertNotNull(result);
+        assertTrue(result.isNodeType("hst:page"));
+
+        result = traverse(session, "/test/nav/funny");
+        assertNotNull(result);
+
+        // actual issue: (ought to be assertTrue)
+        assertFalse(result.isNodeType("hst:page"));
+    }
+}
---
+++ cms/repository/engine/src/test/java/org/hippoecm/repository/TestCase.java
@@ -0,0 +1,145 @@
+/*
+ * Copyright 2007 Hippo
+ *
+ * Licensed under the Apache License, Version 2.0 (the  "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.hippoecm.repository;
+
+import javax.jcr.Node;
+import javax.jcr.PropertyType;
+import javax.jcr.RepositoryException;
+import javax.jcr.Session;
+import javax.jcr.Value;
+import javax.jcr.nodetype.PropertyDefinition;
+
+public abstract class TestCase extends junit.framework.TestCase {
+    private final static String SVN_ID = "$Id";
+
+    private static final String SYSTEMUSER_ID = "admin";
+    private static final char[] SYSTEMUSER_PASSWORD = "admin".toCharArray();
+
+    private HippoRepositoryServer backgroundServer = null;;
+    private enum Mode { STANDALONE, REMOTED, APPSERVER };
+    private Mode mode = Mode.STANDALONE;
+
+    protected HippoRepository server = null;
+    protected Session session = null;
+
+    public TestCase() {
+        if(isRemote() && mode != mode.STANDALONE) {
+            mode = mode.REMOTED;
+        }
+    }
+
+    protected boolean isRemote() {
+        return false;
+    }
+
+    protected void setUp() throws Exception {
+        switch(mode) {
+        case APPSERVER:
+            throw new UnsupportedOperationException("not yet supported");
+        case REMOTED:
+            backgroundServer = new HippoRepositoryServer();
+            backgroundServer.run(true);
+            Thread.sleep(3000);
+            server = HippoRepositoryFactory.getHippoRepository("rmi://localhost:1099/hipporepository"); 
+            break;
+        case STANDALONE: // deliberate fall through
+        default:
+            backgroundServer = null;
+            server = HippoRepositoryFactory.getHippoRepository();
+            session = server.login(SYSTEMUSER_ID, SYSTEMUSER_PASSWORD);
+        }
+        if(session.getRootNode().hasNode("test")) {
+            session.getRootNode().getNode("test").remove();
+        }
+        session.save();
+    }
+
+    protected void tearDown() throws Exception {
+        session.refresh(false);
+        if(session.getRootNode().hasNode("test")) {
+            session.getRootNode().getNode("test").remove();
+        }
+        if(session != null) {
+            session.logout();
+        }
+        if (server != null) {
+            server.close();
+        }
+    }
+
+    protected void build(Session session, String[] contents) throws RepositoryException {
+        Node node = null;
+        for (int i=0; i<contents.length; i+=2) {
+            if (contents[i].startsWith("/")) {
+                String path = contents[i].substring(1);
+                node = session.getRootNode();
+                if (path.contains("/")) {
+                    node = node.getNode(path.substring(0,path.lastIndexOf("/")));
+                    path = path.substring(path.lastIndexOf("/")+1);
+                }
+                node = node.addNode(path, contents[i+1]);
+            } else {
+                PropertyDefinition propDef = null;
+                PropertyDefinition[] propDefs = node.getPrimaryNodeType().getPropertyDefinitions();
+                for (int propidx=0; propidx<propDefs.length; propidx++)
+                    if(propDefs[propidx].getName().equals(contents[i])) {
+                        propDef = propDefs[propidx];
+                        break;
+                    }
+                if("jcr:mixinTypes".equals(contents[i])) {
+                    node.addMixin(contents[i+1]);
+                } else {
+                    if (propDef != null && propDef.isMultiple()) {
+                        Value[] values;
+                        if (node.hasProperty(contents[i])) {
+                            values = node.getProperty(contents[i]).getValues();
+                            Value[] newValues = new Value[values.length+1];
+                            System.arraycopy(values,0,newValues,0,values.length);
+                            values = newValues;
+                        } else {
+                            if(contents[i+1] != null)
+                                values = new Value[1];
+                            else
+                                values = new Value[0];
+                        }
+                        if(values.length > 0) {
+                            if(propDef.getRequiredType() == PropertyType.REFERENCE) {
+                                values[values.length-1] = session.getValueFactory().createValue(session.getRootNode().getNode(contents[i+1]).getUUID(), PropertyType.REFERENCE);
+                            } else {
+                                values[values.length-1] = session.getValueFactory().createValue(contents[i+1]);
+                            }
+                        }
+                        node.setProperty(contents[i], values);
+                    } else {
+                        if(propDef != null && propDef.getRequiredType() == PropertyType.REFERENCE) {
+                            node.setProperty(contents[i], session.getValueFactory().createValue(session.getRootNode().getNode(contents[i+1].substring(1)).getUUID(), PropertyType.REFERENCE));
+                        } else if("hippo:docbase".equals(contents[i])) {
+                            node.setProperty(contents[i], session.getValueFactory().createValue(session.getRootNode().getNode(contents[i+1].substring(1)).getUUID(), PropertyType.REFERENCE));
+                        } else {
+                            node.setProperty(contents[i], contents[i+1]);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    protected Node traverse(Session session, String path) throws RepositoryException {
+        if(path.startsWith("/"))
+            path = path.substring(1);
+        return HierarchyResolver.getNode(session.getRootNode(), path);
+    }
+}
