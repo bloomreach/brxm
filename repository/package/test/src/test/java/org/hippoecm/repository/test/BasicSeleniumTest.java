32e5768e6020bd6f4e8bf5db046fcc147e473c46
Berry van Halderen <b.vanhalderen@onehippo.com>
2008-05-14 09:17:30 +0000

    HREPTWO-804, HREPTWO-805, HREPTWO-764, HREPTWO-16: - Included maven 2.0.8 prerequisited in documentation - correcting name of filter/servlet in mappingin   frontend/application/src/main/webapp/WEB-INF/web.xml, usage of   repository-address, etcetera - fixing PersistanceInheritanceTest to create query using querymanager - Fix in derived data engine to avoid class cast exception. - run non-remote tests also remotely when referenced in RemoteTest.java - run application based tests in AppserverTest.java.  This is like RemoteTest,   but instead of running the repository in the background and accessing it   over RMI, it runs the repository in an application server (only performed   when integration tests are run, see next point).  The tests will only   succeed correctly when the rmiregistry is not run by the application server   at this time, but is started seperately using the same JVM as used to run   the tests. - Integration testing by use of selenium, tests should be referenced in   IntegrationTest and tests themselves should be defined in source files   named *SeleniumTest.java   Integration tests are run by the use of     mvn -Dselenium.firefox=<path to firefox-bin> \         -Dappserver.home=<path to glassfish v2> integration-tests   the goal integration-tests is also part of the "install" end-goal, but the   integration tests are skipped when the environment variable appserver.home   is not set. - Simple login-logout selenium test included, removed out-of-date BasicTest - TabsPlugin should allow maximum number of characters in tab without showing   "..", also increased the maximum such that "Dashboard" is not truncated. - Using wicket application as servlet iso filter in s/a and ear deployments - Included in EAR the same set of dependencies as in the WAR packaging.
    svn path=/hippo-ecm/trunk/; revision=11338

---
+++ repository/package/test/src/test/java/org/hippoecm/repository/test/BasicSeleniumTest.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright 2007 Hippo
+ *
+ * Licensed under the Apache License, Version 2.0 (the  "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.hippoecm.repository.test;
+
+import org.junit.*;
+import org.junit.internal.runners.InitializationError;
+import org.junit.runner.Description;
+import org.junit.runner.RunWith;
+import org.junit.runner.notification.Failure;
+import org.junit.runner.notification.RunNotifier;
+import org.junit.runners.Suite;
+import static org.junit.Assert.*;
+
+import com.thoughtworks.selenium.DefaultSelenium;
+import com.thoughtworks.selenium.SeleniumException;
+
+public class BasicSeleniumTest {
+    private DefaultSelenium selenium;
+
+    @Before
+    public void setUp() throws Exception {
+        selenium = new DefaultSelenium("localhost", 4444, "*firefox", "http://localhost:8080/");
+        selenium.start();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        selenium.stop();
+    }
+
+    @Test
+    public void testLoginLogout() throws Exception {
+        doLogin();
+        doLogout();
+    }
+
+    public void doLogin() throws Exception {
+        try {
+            selenium.open("http://localhost:8080/");
+            assert(selenium.isTextPresent("Username"));
+            assert(selenium.isTextPresent("Password"));
+
+            // enter username
+            selenium.type("username3", "demo");
+
+            // enter password
+            selenium.type("password4", "demo");
+
+            // click ok
+            selenium.click("submit5");
+            for (int second = 0;; second++) {
+                if (second >= 60) fail("timeout");
+                try { if (selenium.isTextPresent("Dashboard")) break; } catch (Exception e) {}
+                Thread.sleep(1000);
+            }
+        } catch (SeleniumException ex) {
+            fail(ex.getMessage());
+            throw ex;
+        }
+    }
+
+    public void doLogout() throws Exception {
+        try {
+            selenium.click("logout_link9");
+            for (int second = 0;; second++) {
+                if (second >= 60) fail("timeout");
+                try { if (selenium.isTextPresent("Password")) break; } catch (Exception e) {}
+                Thread.sleep(1000);
+            }
+        } catch (SeleniumException ex) {
+            fail(ex.getMessage());
+            throw ex;
+        }
+    }
+}
