57b28d5f279ef53e8b86e5c496ec91b92e1c3675
Arthur Bogaart <a.bogaart@onehippo.com>
2008-05-26 15:07:10 +0000

    HREPTWO-814: svn:ignore failed
    svn path=/hippo-ecm/trunk/; revision=11479

--- cms/addon/yui/frontend/target/classes/org/hippoecm/frontend/yui/dragdrop/node/DragNode.js
+++
@@ -1,8 +0,0 @@
-function fn_initDrag${id}(){
-  drag${id} = new YAHOO.hippo.DDNode('${id}', '${group}', {label: '${label}', centerFrame: true, resizeFrame: false});
-  drag${id}.onDragDrop = function(ev, id) {
-    var callbackParameters = [{key: 'targetId', value: id}];
-    ${callbackScript}
-  };
-}
-Wicket.Event.add(window,"domready", fn_initDrag${id});
--- cms/addon/yui/frontend/target/classes/org/hippoecm/frontend/yui/logger/YuiLogBehavior.js
+++
@@ -1,7 +0,0 @@
-var myLogReader = null;
-function initLogger() {
-  if(myLogReader == null)
-    myLogReader = new YAHOO.widget.LogReader();
-}
-//YAHOO.util.Event.addListener(window, "load", initLogger);
-Wicket.Event.add(window,"domready", initLogger);
