6815730befab4cfd69731f0145f618b58ee73f47
Arthur Bogaart <a.bogaart@onehippo.com>
2008-05-26 15:07:10 +0000

    HREPTWO-814: svn:ignore failed
    svn path=/hippo-ecm/trunk/; revision=11479

--- repository/addon/yui/frontend/target/classes/org/hippoecm/frontend/yui/dragdrop/node/DropNode.js
+++
@@ -1,7 +0,0 @@
-function fn_initDropNode${id}(){
-    if('${group}' == '')
-      drop${id} = new YAHOO.util.DDTarget('${id}');
-    else 
-      drop${id} = new YAHOO.util.DDTarget('${id}', '${group}');
-}
-Wicket.Event.add(window,"domready", fn_initDropNode${id});
