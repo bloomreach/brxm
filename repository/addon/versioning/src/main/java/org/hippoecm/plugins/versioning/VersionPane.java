10ba6f686364c5d4db634e2441480475666de138
Berry van Halderen <b.vanhalderen@onehippo.com>
2008-05-29 14:22:48 +0000

    HREPTWO-844: simple history viewing complete
    svn path=/hippo-ecm/trunk/; revision=11618

--- repository/addon/versioning/src/main/java/org/hippoecm/plugins/versioning/VersionPane.java
+++ repository/addon/versioning/src/main/java/org/hippoecm/plugins/versioning/VersionPane.java
@@ -19,15 +19,20 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.rmi.RemoteException;
+import java.text.DateFormat;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.Iterator;
+import java.util.Locale;
 import java.util.Map;
 import java.util.Set;
-
 import java.util.SortedMap;
+
 import javax.jcr.Node;
+import javax.jcr.Property;
 import javax.jcr.RepositoryException;
 
+import javax.jcr.Session;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -36,6 +41,7 @@ import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.ajax.markup.html.AjaxLink;
 import org.apache.wicket.markup.html.basic.Label;
 
+import org.apache.wicket.model.Model;
 import org.hippoecm.frontend.model.JcrNodeModel;
 import org.hippoecm.frontend.sa.plugin.IPlugin;
 import org.hippoecm.frontend.sa.plugin.IPluginContext;
@@ -47,24 +53,28 @@ import org.hippoecm.frontend.sa.service.render.ModelReference;
 import org.hippoecm.frontend.sa.service.render.ModelReference.ModelMessage;
 import org.hippoecm.frontend.sa.service.topic.TopicService;
 import org.hippoecm.repository.api.Document;
-import org.hippoecm.repository.api.Document;
+import org.hippoecm.repository.api.HippoNode;
 import org.hippoecm.repository.api.HippoWorkspace;
+import org.hippoecm.repository.api.MappingException;
+import org.hippoecm.repository.api.Workflow;
 import org.hippoecm.repository.api.WorkflowException;
 import org.hippoecm.repository.api.WorkflowManager;
+import org.hippoecm.repository.reviewedactions.FullReviewedActionsWorkflow;
 import org.hippoecm.repository.standardworkflow.VersionWorkflow;
 
 public class VersionPane extends RenderPlugin
         implements IPlugin, IMessageListener, IClusterable {
-
     static Logger log = LoggerFactory.getLogger(VersionPane.class);
     private IPluginContext context;
     private IPluginConfig config;
     private TopicService topic;
     private TopicService subtopic;
-    DisplayDocument documentComponent;
+    Label documentComponent;
+    Label versionComponent;
     Label createdComponent;
     Label expiredComponent;
     Label labeledComponent;
+    AjaxLink testComponent;
     AjaxLink restoreComponent;
     AjaxLink compareComponent;
     AjaxLink olderComponent;
@@ -75,7 +85,7 @@ public class VersionPane extends RenderPlugin
         this.context = context;
         this.config = config;
 
-        if (config.get(RenderPlugin.MODEL_ID) != null) {
+        if (config.get(RenderPlugin.MODEL_ID)!=null) {
             topic = new TopicService(config.getString(RenderPlugin.MODEL_ID));
             topic.addListener(this);
             topic.init(context);
@@ -85,27 +95,26 @@ public class VersionPane extends RenderPlugin
             log.warn("");
         }
 
-        add(documentComponent = new DisplayDocument(context, config));
+        addExtensionPoint("display");
+        add(documentComponent = new Label("document"));
+        add(versionComponent = new Label("version"));
         add(createdComponent = new Label("created"));
         add(expiredComponent = new Label("expired"));
         add(labeledComponent = new Label("labeled"));
         add(restoreComponent = new AjaxLink("restore") {
-
             @Override
             public void onClick(AjaxRequestTarget target) {
                 restoreVersion();
             }
         });
 
-        add(compareComponent = new AjaxLink("compare") {
-
-            @Override
-            public void onClick(AjaxRequestTarget target) {
-            }
-        });
+        /*add(compareComponent = new AjaxLink("compare") {
+        @Override
+        public void onClick(AjaxRequestTarget target) {
+        }
+        });*/
 
         add(olderComponent = new AjaxLink("older") {
-
             @Override
             public void onClick(AjaxRequestTarget target) {
                 browseVersion(-1);
@@ -113,10 +122,16 @@ public class VersionPane extends RenderPlugin
         });
 
         add(newerComponent = new AjaxLink("newer") {
-
             @Override
             public void onClick(AjaxRequestTarget target) {
                 browseVersion(+1);
+            }
+        });
+
+        add(newerComponent = new AjaxLink("test") {
+            @Override
+            public void onClick(AjaxRequestTarget target) {
+                test();
             }
         });
     }
@@ -145,45 +160,145 @@ public class VersionPane extends RenderPlugin
 
     public void onModelChanged() {
         super.onModelChanged();
-        JcrNodeModel model = (JcrNodeModel) getModel();
-        if (model != null) {
+        JcrNodeModel model = (JcrNodeModel)getModel();
+        if (model!=null) {
             try {
                 if (model.getNode().isNodeType("hippostd:document")) {
-                    currentVersion = -1;
+                    documentComponent.setModel(new Model(model.getNode().getName()));
+                    versionComponent.setModel(new Model("current"));
                     subtopic.publish(new ModelMessage(ModelReference.SET_MODEL, new JcrNodeModel(model.getNode())));
+                    createdComponent.setModel(new Model(""));
+                    expiredComponent.setModel(new Model(""));
+                    labeledComponent.setModel(new Model(""));
                 }
             } catch (RepositoryException ex) {
             }
         }
     }
-    private int currentVersion;
 
     private void restoreVersion() {
     }
 
     private void browseVersion(int direction) {
-        JcrNodeModel model = (JcrNodeModel) VersionPane.this.getModel();
-        if (model != null) {
+        DateFormat dateFormat = DateFormat.getDateInstance(DateFormat.FULL, Locale.getDefault());
+        JcrNodeModel model = (JcrNodeModel)VersionPane.this.getModel();
+        Object currentVersionObject = versionComponent.getModel().getObject();
+        int currentVersion = (currentVersionObject instanceof Integer ? ((Integer)currentVersionObject).intValue() : -1);
+        if (model!=null) {
             Node document = model.getNode();
             try {
-                WorkflowManager workflowManager = ((HippoWorkspace) document.getSession().getWorkspace()).getWorkflowManager();
-                VersionWorkflow workflow = (VersionWorkflow) workflowManager.getWorkflow("versioning", document);
-                if (workflow != null) {
+                WorkflowManager workflowManager = ((HippoWorkspace)document.getSession().getWorkspace()).getWorkflowManager();
+                VersionWorkflow workflow = (VersionWorkflow)workflowManager.getWorkflow("versioning", document);
+                if (workflow!=null) {
                     SortedMap<Calendar, Set<String>> versions = workflow.list();
-                    Iterator<Calendar> iter = versions.keySet().iterator();
-                    for(int i=0; i<currentVersion+direction; i++)
-                            iter.next();
-                    Document historicDocument = workflow.retrieve(iter.next());
+                    if (versions.size()==0)
+                        return;
+                    Iterator iter = versions.entrySet().iterator();
+                    if (currentVersion<0)
+                        currentVersion = versions.size();
+                    currentVersion += direction;
+                    if (currentVersion>=versions.size())
+                        currentVersion = versions.size()-1;
+                    if (currentVersion<0)
+                        currentVersion = 0;
+                    versionComponent.setModel(new Model(new Integer(currentVersion)));
+                    for (int i = 0; i<currentVersion; i++)
+                        iter.next();
+                    Map.Entry<Calendar, Set<String>> entry = (Map.Entry<Calendar, Set<String>>)iter.next();
+                    Date date = entry.getKey().getTime();
+                    createdComponent.setModel(new Model(dateFormat.format(date)));
+                    if (iter.hasNext()) {
+                        date = ((Map.Entry<Calendar, Set<String>>)iter.next()).getKey().getTime();
+                        expiredComponent.setModel(new Model((dateFormat.format(date))));
+                    } else
+                        expiredComponent.setModel(new Model("present"));
+                    StringBuffer labels = new StringBuffer();
+                    for (String label : entry.getValue()) {
+                        if (labels.length()>0)
+                            labels.append(", ");
+                        labels.append(label);
+                    }
+                    labeledComponent.setModel(new Model(new String(labels)));
+                    Document historicDocument = workflow.retrieve(entry.getKey());
                     subtopic.publish(new ModelMessage(ModelReference.SET_MODEL, new JcrNodeModel(document.getSession().getNodeByUUID(historicDocument.getIdentity()))));
+                    redraw();
                 }
             } catch (WorkflowException ex) {
-                log.error(ex.getClass().getName() + ": " + ex.getMessage());
+                log.error(ex.getClass().getName()+": "+ex.getMessage());
+                ex.printStackTrace(System.err);
             } catch (RemoteException ex) {
-                log.error(ex.getClass().getName() + ": " + ex.getMessage());
+                log.error(ex.getClass().getName()+": "+ex.getMessage());
+                ex.printStackTrace(System.err);
             } catch (RepositoryException ex) {
-                log.error(ex.getClass().getName() + ": " + ex.getMessage());
+                log.error(ex.getClass().getName()+": "+ex.getMessage());
+                ex.printStackTrace(System.err);
             }
         }
+    }
+    // FIXME: to be removed after testing
+    private void test() {
+        JcrNodeModel model = (JcrNodeModel)VersionPane.this.getModel();
+        if (model!=null) {
+            Node document = model.getNode();
+            try {
+                publish(edit(document));
+            } catch (WorkflowException ex) {
+                log.error(ex.getClass().getName()+": "+ex.getMessage());
+                ex.printStackTrace(System.err);
+            } catch (RemoteException ex) {
+                log.error(ex.getClass().getName()+": "+ex.getMessage());
+                ex.printStackTrace(System.err);
+            } catch (RepositoryException ex) {
+                log.error(ex.getClass().getName()+": "+ex.getMessage());
+                ex.printStackTrace(System.err);
+            }
+        }
+    }
+
+    private Node edit(Node node) throws WorkflowException, MappingException, RepositoryException, RemoteException {
+        FullReviewedActionsWorkflow publishwf;
+        Document document;
+        Property prop;
+
+        Session session = node.getSession();
+        String path = node.getPath();
+
+        // start edit
+        publishwf = (FullReviewedActionsWorkflow)getWorkflow(node, "reviewed-action");
+        document = publishwf.obtainEditableInstance();
+        session.save();
+        session.refresh(false);
+
+        // edit
+        node = session.getNodeByUUID(document.getIdentity());
+        node.setProperty("hippostd:content", node.getProperty("hippostd:content").getString()+"!");
+        session.save();
+        session.refresh(false);
+
+        // commit edit
+        node = session.getNodeByUUID(document.getIdentity());
+        publishwf = (FullReviewedActionsWorkflow)getWorkflow(node, "reviewed-action");
+        publishwf.commitEditableInstance();
+        session.save();
+        session.refresh(false);
+
+        return session.getRootNode().getNode(path.substring(1));
+    }
+
+    private void publish(Node node) throws WorkflowException, MappingException, RepositoryException, RemoteException {
+        Session session = node.getSession();
+        session.save();
+        FullReviewedActionsWorkflow publishwf = (FullReviewedActionsWorkflow)getWorkflow(node, "reviewed-action");
+        publishwf.publish();
+        session.save();
+        session.refresh(false);
+    }
+
+    protected Workflow getWorkflow(Node node, String category) throws RepositoryException {
+        HippoWorkspace wsp = (HippoWorkspace)node.getSession().getWorkspace();
+        WorkflowManager workflowMgr = wsp.getWorkflowManager();
+        Node canonicalNode = ((HippoNode)node).getCanonicalNode();
+        return workflowMgr.getWorkflow(category, canonicalNode);
     }
 }
 
