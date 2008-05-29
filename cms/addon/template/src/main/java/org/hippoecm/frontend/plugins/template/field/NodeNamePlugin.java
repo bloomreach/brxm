6457f7cc048a24c58f8ff7c073f52db271fb5739
Arthur Bogaart <a.bogaart@onehippo.com>
2008-05-29 12:40:46 +0000

    HREPTWO-853: added TemplateField Plugin that prints out nodename
    svn path=/hippo-ecm/trunk/; revision=11595

---
+++ cms/addon/template/src/main/java/org/hippoecm/frontend/plugins/template/field/NodeNamePlugin.java
@@ -0,0 +1,38 @@
+package org.hippoecm.frontend.plugins.template.field;
+
+import java.util.List;
+
+import javax.jcr.RepositoryException;
+
+import org.apache.wicket.markup.html.basic.Label;
+import org.hippoecm.frontend.model.IPluginModel;
+import org.hippoecm.frontend.model.JcrNodeModel;
+import org.hippoecm.frontend.plugin.Plugin;
+import org.hippoecm.frontend.plugin.PluginDescriptor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class NodeNamePlugin extends Plugin {
+    private static final long serialVersionUID = 1L;
+    
+    static final Logger log = LoggerFactory.getLogger(NodeNamePlugin.class);
+
+    public NodeNamePlugin(PluginDescriptor pluginDescriptor, IPluginModel model, Plugin parentPlugin) {
+        super(pluginDescriptor, model, parentPlugin);
+        
+        List<String> captions = pluginDescriptor.getParameter("caption").getStrings();
+        if(captions != null && captions.size() > 0) {
+            add(new Label("name", captions.get(0)));
+        } else {
+            add(new Label("name", ""));
+        }
+        
+        JcrNodeModel nodeModel = new JcrNodeModel(model);
+        try {
+            add(new Label("field", nodeModel.getNode().getName()));
+        } catch (RepositoryException e) {
+            log.error("Could not retrieve name of node (" + nodeModel.getItemModel().getPath() + ")", e);
+        }
+    }
+
+}
