9064358911596fb2ec8abcf2ff93f90750a26e07
Arthur Bogaart <a.bogaart@onehippo.com>
2008-06-06 14:17:20 +0000

    HREPTWO-962: added validator that makes the Widget act as required only if the model value is null. Also added custom ChainedModel that only allowes non-null, non-empty string to be set as model value.
    svn path=/hippo-ecm/trunk/; revision=11877

--- cms/frontend/engine/src/main/java/org/hippoecm/frontend/widgets/PasswordTextFieldWidget.java
+++ cms/frontend/engine/src/main/java/org/hippoecm/frontend/widgets/PasswordTextFieldWidget.java
@@ -15,15 +15,96 @@
  */
 package org.hippoecm.frontend.widgets;
 
+import javax.jcr.RepositoryException;
+
+import org.apache.jackrabbit.value.StringValue;
 import org.apache.wicket.markup.html.form.PasswordTextField;
+import org.apache.wicket.model.IChainingModel;
 import org.apache.wicket.model.IModel;
+import org.apache.wicket.validation.IValidatable;
+import org.apache.wicket.validation.validator.AbstractValidator;
+import org.hippoecm.frontend.model.properties.JcrPropertyValueModel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class PasswordTextFieldWidget extends AjaxUpdatingWidget {
     private static final long serialVersionUID = 1L;
 
-    public PasswordTextFieldWidget(String id, IModel model) {
+    private static final Logger log = LoggerFactory.getLogger(PasswordTextFieldWidget.class);
+
+    private MyModel myModel;
+
+    public PasswordTextFieldWidget(final String id, final IModel model) {
         super(id, model);
-        addFormField(new PasswordTextField("widget", this.getModel()).setResetPassword(false));
+
+        myModel = new MyModel(model);
+
+        final PasswordTextField pwd = new PasswordTextField("widget", myModel);
+        pwd.setRequired(false);
+        pwd.add(new AbstractValidator() {
+            @Override
+            protected void onValidate(IValidatable validatable) {
+                String modelValue = myModel.getObject() != null ? (String) myModel.getObject() : "";
+                String formValue = validatable.getValue() != null ? (String) validatable.getValue() : "";
+                if (modelValue.length() == 0 && formValue.length() == 0)
+                    PasswordTextFieldWidget.this.error("Password is required");
+            }
+
+            @Override
+            public boolean validateOnNullValue() {
+                return true;
+            }
+        });
+        addFormField(pwd);
+    }
+
+    class MyModel implements IChainingModel {
+        private static final long serialVersionUID = 1L;
+
+        private JcrPropertyValueModel model;
+
+        public MyModel(IModel model) {
+            setChainedModel(model);
+        }
+
+        public IModel getChainedModel() {
+            return model;
+        }
+
+        public void setChainedModel(IModel model) {
+            if (model instanceof JcrPropertyValueModel)
+                this.model = (JcrPropertyValueModel) model;
+        }
+
+        public void detach() {
+            model.detach();
+        }
+
+        public Object getObject() {
+            if (model != null) {
+                try {
+                    return model.getValue().getString();
+                } catch (RepositoryException e) {
+                    log.error("An error occurred while trying to get password value", e);
+                }
+            }
+            return null;
+        }
+
+        /**
+         * Special purpose override, makes sure the JcrPropertyValueModel will never be null or ""
+         * (non-Javadoc)
+         * @see org.apache.wicket.model.IModel#setObject(java.lang.Object)
+         */
+        public void setObject(Object object) {
+            if (object == null)
+                return;
+
+            String value = (String) object;
+            if (value.length() > 0) {
+                model.setValue(new StringValue(value));
+            }
+        }
     }
 
 }
