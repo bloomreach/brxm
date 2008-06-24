49710c04daaceac0a454f8e3da2d056972ac61d0
Ard Schrijvers <a.schrijvers@onehippo.com>
2008-06-24 15:49:00 +0000

    HREPTWO-1058 trivial: moved the onDetach to end of class instead of above constructor
    svn path=/hippo-ecm/trunk/; revision=12165

--- repository/addon/standards/frontend/src/main/java/org/hippoecm/frontend/plugins/standards/perspective/Perspective.java
+++ repository/addon/standards/frontend/src/main/java/org/hippoecm/frontend/plugins/standards/perspective/Perspective.java
@@ -40,14 +40,6 @@ public abstract class Perspective extends RenderPlugin implements ITitleDecorato
     private String title = "title";
     private ModelService modelService;
 
-    @Override
-    protected void onDetach() {
-        if(modelService != null) {
-            modelService.detach();
-        }
-        super.onDetach();
-    }
-
     public Perspective(IPluginContext context, IPluginConfig config) {
         super(context, config);
 
@@ -85,6 +77,14 @@ public abstract class Perspective extends RenderPlugin implements ITitleDecorato
         if (modelService != null) {
             modelService.setModel(model);
         }
+    }
+    
+    @Override
+    protected void onDetach() {
+        if(modelService != null) {
+            modelService.detach();
+        }
+        super.onDetach();
     }
 
 }
