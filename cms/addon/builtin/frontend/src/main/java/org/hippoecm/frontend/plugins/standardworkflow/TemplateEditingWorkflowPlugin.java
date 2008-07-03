a0e4efaaeba4ab9fa7dc9ae5bbe9acd2d2ad31cb
Berry van Halderen <b.vanhalderen@onehippo.com>
2008-07-03 08:35:30 +0000

    HREPTWO-18: add SVN_ID
    svn path=/hippo-ecm/trunk/; revision=12294

--- cms/addon/builtin/frontend/src/main/java/org/hippoecm/frontend/plugins/standardworkflow/TemplateEditingWorkflowPlugin.java
+++ cms/addon/builtin/frontend/src/main/java/org/hippoecm/frontend/plugins/standardworkflow/TemplateEditingWorkflowPlugin.java
@@ -28,6 +28,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class TemplateEditingWorkflowPlugin extends AbstractWorkflowPlugin {
+    @SuppressWarnings("unused")
+    private final static String SVN_ID = "$Id$";
+
     private static final long serialVersionUID = 1L;
 
     static protected Logger log = LoggerFactory.getLogger(TemplateEditingWorkflowPlugin.class);
