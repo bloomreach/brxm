a7728004fab7b6822d3f27efebba04004c832815
Arthur Bogaart <a.bogaart@onehippo.com>
2008-07-14 10:50:45 +0000

    HREPTWO-1208: now replaces all occurences
    svn path=/hippo-ecm/trunk/; revision=12508

--- cms/addon/xinha/webResources/xinha/plugins/ImageFilter/image-filter.js
+++ cms/addon/xinha/webResources/xinha/plugins/ImageFilter/image-filter.js
@@ -7,52 +7,48 @@ Add a prefix to images with a relative @src value
 function ImageFilter(editor) {
     this.editor = editor;
     this.prefix = _editor_jcrnode_url;
-    if(this.prefix.charAt(this.prefix.length-1) != '/') {
+    if (this.prefix.charAt(this.prefix.length - 1) != '/') {
         this.prefix += '/';
     }
+    this.imgRE = new RegExp('<img[^>]+>', 'gi');
+    this.srcRE = new RegExp('src="[^"]+"', 'i');
 }
 
 ImageFilter._pluginInfo = {
-  name          : "ImageFilter",
-  version       : "1.0",
-  developer     : "Arthur Bogaart",
-  developer_url : "http://www.onehippo.org",
-  c_owner       : "Arthur Bogaart",
-  sponsor       : "",
-  sponsor_url   : "",
-  license       : ""
+    name :"ImageFilter",
+    version :"1.0",
+    developer :"Arthur Bogaart",
+    developer_url :"http://www.onehippo.org",
+    c_owner :"Arthur Bogaart",
+    sponsor :"",
+    sponsor_url :"",
+    license :""
 }
 
-ImageFilter.prototype.inwardHtml = function(html)
-{
-    //add node prefix to all images
-    var imgRE = new RegExp('<img[^>]+>');
-    var srcRE = new RegExp('src="[^"]+"');
+ImageFilter.prototype.inwardHtml = function(html) {
+    // add node prefix to all images
     var _this = this;
-    html = html.replace(imgRE, function(m) {
-        m = m.replace(srcRE, function(n) {
-            var val = n.substring(5, n.length-1);
-            if(val.indexOf('/') == 0 || val.indexOf('http:') == 0)
+    html = html.replace(this.imgRE, function(m) {
+        m = m.replace(_this.srcRE, function(n) {
+            var val = n.substring(5, n.length - 1);
+            if (val.indexOf('/') == 0 || val.indexOf('http:') == 0)
                 return n;
             else {
                 return 'src="' + _this.prefix + val + '"';
-            }    
+            }
         });
         return m;
     });
     return html;
 }
 
-ImageFilter.prototype.outwardHtml = function(html)
-{
-    var imgRE = new RegExp('<img[^>]+>');
-    var srcRE = new RegExp('src="[^"]+"');
+ImageFilter.prototype.outwardHtml = function(html) {
     var _this = this;
-    html = html.replace(imgRE, function(m) {
-        m = m.replace(srcRE, function(n) {
+    html = html.replace(this.imgRE, function(m) {
+        m = m.replace(_this.srcRE, function(n) {
             var idx = n.indexOf(_this.prefix);
-            if(idx > -1) {
-                return 'src="' + n.substr(_this.prefix.length+idx);            
+            if (idx > -1) {
+                return 'src="' + n.substr(_this.prefix.length + idx);
             }
             return n;
         });
