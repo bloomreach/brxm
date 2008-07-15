afb2607d303250eff863f8a398e94f01e7128421
Berry van Halderen <b.vanhalderen@onehippo.com>
2008-07-15 02:25:04 +0000

    HREPTWO-1204: dashboard shortcut; work in progress
    svn path=/hippo-ecm/trunk/; revision=12526

---
+++ repository/frontend/engine/src/main/java/org/hippoecm/frontend/plugin/workflow/Gallery.java
@@ -0,0 +1,65 @@
+// FIXME [BvH] work in progress, this needs to be moved into seperate package
+
+/*
+ *  Copyright 2008 Hippo.
+ * 
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.hippoecm.frontend.plugin.workflow;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.jcr.RepositoryException;
+
+import org.apache.wicket.Component;
+import org.apache.wicket.Session;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.markup.html.AjaxLink;
+import org.apache.wicket.markup.html.panel.EmptyPanel;
+import org.apache.wicket.model.Model;
+
+import org.hippoecm.frontend.dialog.AbstractDialog;
+import org.hippoecm.frontend.dialog.AbstractWorkflowDialog;
+import org.hippoecm.frontend.dialog.DialogLink;
+import org.hippoecm.frontend.dialog.ExceptionDialog;
+import org.hippoecm.frontend.dialog.IDialogFactory;
+import org.hippoecm.frontend.dialog.IDialogService;
+import org.hippoecm.frontend.model.JcrNodeModel;
+import org.hippoecm.frontend.model.WorkflowsModel;
+import org.hippoecm.frontend.plugin.IPluginContext;
+import org.hippoecm.frontend.plugin.config.IPluginConfig;
+import org.hippoecm.frontend.service.IJcrService;
+import org.hippoecm.frontend.service.render.RenderPlugin;
+import org.hippoecm.frontend.session.UserSession;
+import org.hippoecm.repository.api.HippoNodeType;
+import org.hippoecm.repository.api.MappingException;
+import org.hippoecm.repository.api.Workflow;
+import org.hippoecm.repository.api.WorkflowManager;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Gallery {
+    @SuppressWarnings("unused")
+    private final static String SVN_ID = "$Id$";
+
+    static final Logger log = LoggerFactory.getLogger(Gallery.class);
+
+    private static final long serialVersionUID = 1L;
+
+    public Gallery() {
+        super();
+    }
+}
---
+++ repository/frontend/engine/src/main/java/org/hippoecm/frontend/plugin/workflow/ShortcutsPlugin.java
@@ -0,0 +1,167 @@
+/*
+ *  Copyright 2008 Hippo.
+ * 
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.hippoecm.frontend.plugin.workflow;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import javax.jcr.Node;
+import javax.jcr.NodeIterator;
+import javax.jcr.RepositoryException;
+import javax.jcr.query.Query;
+import javax.jcr.query.QueryManager;
+import javax.jcr.query.QueryResult;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.apache.wicket.Session;
+import org.apache.wicket.model.IDetachable;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.markup.html.panel.Panel;
+
+import org.hippoecm.frontend.model.IModelListener;
+import org.hippoecm.frontend.model.JcrNodeModel;
+import org.hippoecm.frontend.model.ModelService;
+import org.hippoecm.frontend.plugin.IPlugin;
+import org.hippoecm.frontend.plugin.IPluginContext;
+import org.hippoecm.frontend.plugin.IPluginControl;
+import org.hippoecm.frontend.plugin.config.IPluginConfig;
+import org.hippoecm.frontend.plugin.config.impl.JavaClusterConfig;
+import org.hippoecm.frontend.plugin.config.impl.JavaPluginConfig;
+import org.hippoecm.frontend.plugin.config.impl.JcrPluginConfig;
+import org.hippoecm.frontend.service.IBrowseService;
+import org.hippoecm.frontend.service.IEditService;
+import org.hippoecm.frontend.service.IJcrService;
+import org.hippoecm.frontend.service.IRenderService;
+import org.hippoecm.frontend.service.render.RenderService;
+import org.hippoecm.frontend.session.UserSession;
+import org.hippoecm.frontend.service.render.RenderPlugin;
+
+public class ShortcutsPlugin extends Panel implements IPlugin, IModelListener, IDetachable {
+    @SuppressWarnings("unused")
+    private final static String SVN_ID = "$Id$";
+
+    private static final long serialVersionUID = 1L;
+
+    private static final Logger log = LoggerFactory.getLogger(ShortcutsPlugin.class);
+
+    private static final String PLUGINSQUERY = "shortcuts.query";
+    public static final String SHORTCUTS_ID = "shortcuts.id";
+
+    private IPluginContext context;
+    private IPluginConfig config;
+    private String factoryId;
+    private Map<String, IPluginControl> plugins;
+    private Map<String, ModelService> models;
+    private int pluginCount;
+    private String pluginsQuery = "/jcr:root//element(*,frontend:plugin)[@wicket.id='shortcut']";
+
+    public ShortcutsPlugin(IPluginContext context, IPluginConfig config) {
+        super("id"); 
+
+        this.context = context;
+        this.config = config;
+
+        IModel model = new JcrNodeModel("/");
+        setModel(model);
+
+        plugins = new HashMap<String, IPluginControl>();
+        models = new HashMap<String, ModelService>();
+        pluginCount = 0;
+
+        if (config.get(PLUGINSQUERY) != null) {
+            pluginsQuery = config.getString(PLUGINSQUERY);
+        } else {
+            log.warn("No query defined for {}", factoryId);
+        }
+
+        if (config.getString(RenderService.MODEL_ID) != null) {
+            context.registerService(this, config.getString(RenderService.MODEL_ID));
+        } else {
+            log.warn("");
+        }
+
+        context.registerService(this, IJcrService.class.getName());
+        System.err.println("BERRY done");
+    }
+
+    @Override
+    public void updateModel(IModel imodel) {
+        System.err.println("BERRY updateModel");
+        closePlugins();
+        try {
+        System.err.println("BERRY query "+pluginsQuery);
+            QueryManager qmgr = ((UserSession) getSession()).getJcrSession().getWorkspace().getQueryManager();
+            Query query = qmgr.createQuery(pluginsQuery, Query.XPATH);
+            QueryResult result = query.execute();
+
+            for (NodeIterator iter = result.getNodes(); iter.hasNext(); ) {
+                Node pluginNode = iter.nextNode();
+
+                String pluginId = config.getString(SHORTCUTS_ID) + (pluginCount++);
+                String modelId = pluginId + ".model";
+
+                IPluginConfig pluginConfig = new JcrPluginConfig(new JcrNodeModel(pluginNode));
+                pluginConfig.put(RenderService.WICKET_ID, config.get(RenderService.WICKET_ID));
+                pluginConfig.put(RenderService.MODEL_ID, modelId);
+
+                JavaClusterConfig clusterConfig = new JavaClusterConfig();
+                clusterConfig.addPlugin(pluginConfig);
+
+                IPluginControl plugin = context.start(clusterConfig);
+
+                // look up render service
+                String controlId = context.getReference(plugin).getServiceId();
+                IRenderService renderer = context.getService(controlId, IRenderService.class);
+
+                // register as the factory for the render service
+                context.registerService(this, context.getReference(renderer).getServiceId());
+
+                plugins.put(controlId, plugin);
+            }
+        } catch (RepositoryException ex) {
+            log.error("could not setup plugin", ex);
+        }
+    }
+
+    private void closePlugins() {
+        for (Map.Entry<String, IPluginControl> entry : plugins.entrySet()) {
+            String controlId = entry.getKey();
+
+            // unregister as the factory for the render service
+            IRenderService renderer = context.getService(controlId, IRenderService.class);
+            context.registerService(this, context.getReference(renderer).getServiceId());
+
+            entry.getValue().stopPlugin();
+        }
+        plugins = new HashMap<String, IPluginControl>();
+
+        for (Map.Entry<String, ModelService> entry : models.entrySet()) {
+            ModelService modelService = entry.getValue();
+            modelService.destroy();
+        }
+        models = new HashMap<String, ModelService>();
+    }
+
+    public void onDetach() {
+        config.detach();
+        super.onDetach();
+    }
+}
