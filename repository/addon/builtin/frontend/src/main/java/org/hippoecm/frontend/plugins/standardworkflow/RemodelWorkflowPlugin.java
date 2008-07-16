9844a80557888521160825a9366a68660323db46
Frank van Lankvelt <f.vanlankvelt@onehippo.com>
2008-07-16 13:31:38 +0000

    HREPTWO-1197: rename actions
    svn path=/hippo-ecm/trunk/; revision=12577

--- repository/addon/builtin/frontend/src/main/java/org/hippoecm/frontend/plugins/standardworkflow/RemodelWorkflowPlugin.java
+++ repository/addon/builtin/frontend/src/main/java/org/hippoecm/frontend/plugins/standardworkflow/RemodelWorkflowPlugin.java
@@ -15,11 +15,15 @@
  */
 package org.hippoecm.frontend.plugins.standardworkflow;
 
+import javax.jcr.RepositoryException;
+
+import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.model.Model;
 import org.hippoecm.frontend.dialog.AbstractDialog;
 import org.hippoecm.frontend.dialog.DialogLink;
 import org.hippoecm.frontend.dialog.IDialogFactory;
 import org.hippoecm.frontend.dialog.IDialogService;
+import org.hippoecm.frontend.model.JcrNodeModel;
 import org.hippoecm.frontend.plugin.IPluginContext;
 import org.hippoecm.frontend.plugin.IServiceReference;
 import org.hippoecm.frontend.plugin.config.IPluginConfig;
@@ -27,33 +31,47 @@ import org.hippoecm.frontend.plugin.workflow.AbstractWorkflowPlugin;
 import org.hippoecm.frontend.plugins.standardworkflow.dialogs.CreateTypeDialog;
 import org.hippoecm.frontend.plugins.standardworkflow.dialogs.RemodelDialog;
 import org.hippoecm.frontend.service.IJcrService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class RemodelWorkflowPlugin extends AbstractWorkflowPlugin {
     @SuppressWarnings("unused")
     private final static String SVN_ID = "$Id$";
 
+    private static final Logger log = LoggerFactory.getLogger(RemodelWorkflowPlugin.class);
+
     private static final long serialVersionUID = 1L;
 
     public RemodelWorkflowPlugin(IPluginContext context, IPluginConfig config) {
         super(context, config);
-        
+
         IJcrService jcrService = context.getService(IJcrService.class.getName(), IJcrService.class);
         final IServiceReference<IJcrService> jcrRef = context.getReference(jcrService);
 
-        add(new DialogLink("remodelRequest-dialog", new Model("Update content"), new IDialogFactory() {
+        JcrNodeModel model = (JcrNodeModel) getModel();
+        String ns = "types";
+        try {
+            ns = model.getNode().getName() + " types";
+        } catch (RepositoryException ex) {
+            log.error(ex.getMessage());
+        }
+
+        add(new Label("title", ns));
+        add(new DialogLink("remodelRequest-dialog", new Model("Update all content"), new IDialogFactory() {
             private static final long serialVersionUID = 1L;
+
             public AbstractDialog createDialog(IDialogService dialogService) {
                 return new RemodelDialog(RemodelWorkflowPlugin.this, getDialogService(), jcrRef);
             }
         }, getDialogService()));
-        
+
         add(new DialogLink("createTypeRequest-dialog", new Model("Create new type"), new IDialogFactory() {
             private static final long serialVersionUID = 1L;
 
             public AbstractDialog createDialog(IDialogService dialogService) {
                 return new CreateTypeDialog(RemodelWorkflowPlugin.this, getDialogService(), jcrRef);
             }
-            
+
         }, getDialogService()));
     }
 }
