e107063ebc86f3fc27489e8ca00958253b4dd227
Arthur Bogaart <a.bogaart@onehippo.com>
2008-07-17 15:30:18 +0000

    HREPTWO-1258: added component that stores functions in a queue and executes them in one batch
    svn path=/hippo-ecm/trunk/; revision=12627

---
+++ cms/addon/yui/src/main/java/org/hippoecm/frontend/plugins/yui/inc/hippo/2.5.2/functionqueue/functionqueue-debug.js
@@ -0,0 +1,77 @@
+/**
+ * @description
+ * <p>
+ * Class that queues an array of init functions that get executed upon page load
+ * or in Wicket's post ajax phase
+ * </p>
+ * @namespace YAHOO.hippo
+ * @requires yahoo
+ * @module hippoutil
+ */
+( function() {
+    var Lang = YAHOO.lang;
+
+    /**
+     * @description
+     * <p>
+     * Class that queues an array of init functions that get executed upon page
+     * load or in Wicket's post ajax phase
+     * </p>
+     * @namespace YAHOO.hippo
+     * @requires yahoo
+     * @module hippoutil
+     */
+
+    YAHOO.namespace('hippo');
+
+    YAHOO.hippo.FunctionQueue = function(_id) {
+        this.id = _id;
+        this.queue = new Array();
+        this.uniques = new Array();
+        this.preQueueHandler = null;
+        this.postQueueHandler = null;
+
+    };
+
+    YAHOO.hippo.FunctionQueue.prototype = {
+
+        handleQueue : function() {
+            YAHOO.log('Handle function  queue[' + this.id + '] with size: '
+                    + this.queue.length, 'info', 'FunctionQueue');
+            if (Lang.isFunction(this.preQueueHandler)) {
+                this.preQueueHandler.apply();
+            }
+            while (this.queue.length > 0) {
+                this.queue.shift().apply();
+            }
+            this.uniques = [];
+
+            if (Lang.isFunction(this.postQueueHandler)) {
+                this.postQueueHandler.apply();
+            }
+        },
+
+        registerFunction : function(func, uniqueId) {
+            YAHOO.log('Register function[' + uniqueId + '] in queue[' + this.id
+                    + '] -> func=' + func, 'info', 'FunctionQueue');
+            if (!Lang.isFunction(func))
+                return;
+            if (!Lang.isUndefined(uniqueId) && !Lang.isNull(uniqueId)) {
+                for ( var i = 0; i < this.uniques.length; i++) {
+                    if (this.uniques[i] == uniqueId)
+                        return;
+                }
+                this.uniques.push(uniqueId);
+            }
+            this.queue.push(func);
+        },
+
+        toString : function() {
+            return 'Function queue [' + this.id + ']';
+        }
+    };
+})();
+YAHOO.register("functionqueue", YAHOO.hippo.FunctionQueue, {
+    version :"2.5.2",
+    build :"1076"
+});
