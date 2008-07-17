33c4bcbeec9d0c4d57d51bac65e2fae25f48383e
Arthur Bogaart <a.bogaart@onehippo.com>
2008-07-17 15:31:14 +0000

    HREPTWO-1258: refactored to work with new FunctionQueue
    svn path=/hippo-ecm/trunk/; revision=12629

--- repository/addon/yui/src/main/java/org/hippoecm/frontend/plugins/yui/inc/hippo/2.5.2/dragdropmanager/dragdropmanager.js
+++ repository/addon/yui/src/main/java/org/hippoecm/frontend/plugins/yui/inc/hippo/2.5.2/dragdropmanager/dragdropmanager.js
@@ -3,57 +3,73 @@ YAHOO.namespace('hippo');
 if (!YAHOO.hippo.DragDropManager) {
 
     /**
-     * @description <p>TODO</p>
+     * @description <p>Todo</p>
      * @namespace YAHOO.hippo
-     * @requires dragdropmodel, wicketloader
+     * @requires dragdropmodel, functionqueue
      * @module drapdropmanager
      */
     YAHOO.hippo.DragDropManager = {
-        wicketLoader: new YAHOO.hippo.WicketLoader(),
-        
-        addDraggable: function(id, label, groups, callbackFunc, callbackUrl, callbackParameters) {
+        loader :new YAHOO.hippo.FunctionQueue('DragDropQueue'),
+
+        onLoad : function() {
+            this.loader.handleQueue();
+        },
+
+        addDraggable : function(id, label, groups, callbackFunc, callbackUrl,
+                callbackParameters) {
             var js = YAHOO.lang;
             var func = function() {
-          
-                var config = {centerFrame: true, resizeFrame: false}
-                if(!js.isUndefined(label) && !js.isNull(label)) {
+
+                var config = {
+                    centerFrame :true,
+                    resizeFrame :false
+                }
+                if (!js.isUndefined(label) && !js.isNull(label)) {
                     config.label = label;
                 }
-          
+
                 var drag = null;
-                //maybe isArray checks for null/undef but code is obfuscated
-                if(!js.isUndefined(groups) && !js.isNull(groups) && js.isArray(groups)) {
+                // maybe isArray checks for null/undef but code is obfuscated
+                if (!js.isUndefined(groups) && !js.isNull(groups)
+                        && js.isArray(groups)) {
                     drag = new YAHOO.hippo.DDModel(id, groups.shift(), config);
-                    while(groups.length > 0) {
+                    while (groups.length > 0) {
                         drag.addToGroup(groups.shift());
                     }
                 } else {
                     drag = new YAHOO.util.DDModel(id, null, config);
                 }
-                if(js.isUndefined(callbackParameters) || js.isNull(callbackParameters)) {
-                    callbackParameters = new Array();        
+                if (js.isUndefined(callbackParameters)
+                        || js.isNull(callbackParameters)) {
+                    callbackParameters = new Array();
                 }
-          
+
                 drag.onDragDrop = function(ev, dropId) {
-                    var targetId = {key: 'targetId', value: dropId};
+                    var targetId = {
+                        key :'targetId',
+                        value :dropId
+                    };
                     callbackParameters.push(targetId);
-                    for(i=0;i<callbackParameters.length;i++) {
-                        var paramKey=callbackParameters[i].key, paramValue=Wicket.Form.encode(callbackParameters[i].value);
-                        callbackUrl += (callbackUrl.indexOf('?') > -1) ? '&' : '?';
+                    for (i = 0; i < callbackParameters.length; i++) {
+                        var paramKey = callbackParameters[i].key, paramValue = Wicket.Form
+                                .encode(callbackParameters[i].value);
+                        callbackUrl += (callbackUrl.indexOf('?') > -1) ? '&'
+                                : '?';
                         callbackUrl += (paramKey + '=' + paramValue);
                     }
                     callbackFunc(callbackUrl);
                 }
             };
-            this.wicketLoader.registerFunction(func);
+            this.loader.registerFunction(func);
         },
-        
-        addDroppable: function(id, groups) {
+
+        addDroppable : function(id, groups) {
             var js = YAHOO.lang;
             var drop = null;
-            if(!js.isUndefined(groups) && !js.isNull(groups) && js.isArray(groups)) {
+            if (!js.isUndefined(groups) && !js.isNull(groups)
+                    && js.isArray(groups)) {
                 drop = new YAHOO.util.DDTarget(id, groups.shift());
-                while(groups.length > 0) {
+                while (groups.length > 0) {
                     drop.addToGroup(groups.shift());
                 }
             } else {
@@ -61,6 +77,9 @@ if (!YAHOO.hippo.DragDropManager) {
             }
         }
     };
-    
-    YAHOO.register("dragdropmanager", YAHOO.hippo.DragDropManager, {version: "2.5.2", build: "1076"});
+
+    YAHOO.register("dragdropmanager", YAHOO.hippo.DragDropManager, {
+        version :"2.5.2",
+        build :"1076"
+    });
 }
