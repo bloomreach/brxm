1c70474e0e9d94232f2bf2c869f995848db1359d
Arthur Bogaart <a.bogaart@onehippo.com>
2008-07-18 16:20:20 +0000

    HREPTWO-1174: added temporary fix for template editor
    svn path=/hippo-ecm/trunk/; revision=12675

--- cms/addon/xinha/webResources/xinha/plugins/AutoResize/auto-resize.js
+++ cms/addon/xinha/webResources/xinha/plugins/AutoResize/auto-resize.js
@@ -1,12 +1,28 @@
 
+function doFindDisplayTable(el) {
+    while (el != null && el != document.body) {
+        if (YAHOO.util.Dom.hasClass(el, 'displaytable')) {
+            return true;
+        }
+        el = el.parentNode;
+    }
+    return false;
+}
+
 function AutoResize(editor) {
     this.editor = editor;
+    this.templateStyle = false;
+    
+    var isTemplate = doFindDisplayTable(this.editor._textArea);
     
     YAHOO.hippo.LayoutManager.registerResizeListener(this.editor._textArea, this.editor, function(type, args, me) {
         var w= args[0].body.w ? parseInt(args[0].body.w) : null;
         var h= args[0].body.h ? parseInt(args[0].body.h) : null;
         if(YAHOO.lang.isNumber(w)) {
-            w -= 15;
+            if(isTemplate) {
+                w -= (parseInt(w*0.1) + 43);
+            }
+            w -= 20;
         } 
         if(YAHOO.lang.isNumber(h)) {
             h =parseInt(h/3);
