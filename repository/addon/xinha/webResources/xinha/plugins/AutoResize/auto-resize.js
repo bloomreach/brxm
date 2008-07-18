f94194b76564fbe84dc2de4781379b3d7ee78f75
Arthur Bogaart <a.bogaart@onehippo.com>
2008-07-18 14:29:36 +0000

    HREPTWO-1174: resize Xinha onload/onresize of parent wireframes; take as much width as possible, takes about 1/3 of available height.
    svn path=/hippo-ecm/trunk/; revision=12665

---
+++ repository/addon/xinha/webResources/xinha/plugins/AutoResize/auto-resize.js
@@ -0,0 +1,39 @@
+
+function AutoResize(editor) {
+    this.editor = editor;
+    
+    YAHOO.hippo.LayoutManager.registerResizeListener(this.editor._textArea, this.editor, function(type, args, me) {
+        var w= args[0].body.w ? parseInt(args[0].body.w) : null;
+        var h= args[0].body.h ? parseInt(args[0].body.h) : null;
+        if(YAHOO.lang.isNumber(w)) {
+            w -= 15;
+        } 
+        if(YAHOO.lang.isNumber(h)) {
+            h =parseInt(h/3);
+        }
+        doAutoResize(me, w, h);
+    });
+    
+}
+
+AutoResize._pluginInfo = {
+  name          : "AutoResize",
+  version       : "1.0",
+  developer     : "Arthur Bogaart",
+  developer_url : "http://www.onehippo.org",
+  c_owner       : "Arthur Bogaart",
+  sponsor       : "",
+  sponsor_url   : "",
+  license       : ""
+}
+
+function doAutoResize(editor, w, h) {
+    if (editor._iframe == null) {
+        var again  = function() {
+            doAutoResize(editor, w, h);
+        }
+        window.setTimeout(again, 100);
+    } else {
+        editor.sizeEditor(w + 'px', h + 'px', true, true);
+    }
+};
