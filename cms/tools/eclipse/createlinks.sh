696ac5507aeb2ecee73e78e492c50619b3c8a0e9
Walter Grevink <w.grevink@onehippo.com>
2008-07-22 08:59:17 +0000

    HREPTWO-1281 Eclipse documentation revamp Added scripts to help automate some tasks
    svn path=/hippo-ecm/trunk/; revision=12704

---
+++ cms/tools/eclipse/createlinks.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# This script assumes that it's located in the hippo-ecm/tools/eclipse directory. 
+ 
+cd $(dirname $0) 
+ 
+cd ../../package/war/src/main/webapp
+ln -s -v ../../../../skin/src/main/webapp skin
+ln -s -v ../../../../../addon/xinha/src/main/webapp xinha
+cd ../../../../../addon/xinha/src/main/webapp/xinha/plugins
+ln -s -v ../../../../../webResources/xinha/plugins/AutoSave AutoSave
+ln -s -v ../../../../../webResources/xinha/plugins/AutoResize AutoResize
+ln -s -v ../../../../../webResources/xinha/plugins/CustomLinker CustomLinker
+ln -s -v ../../../../../webResources/xinha/plugins/ImagePicker ImagePicker
+cd ../skins
+ln -s -v ../../../../../webResources/xinha/skins/hippo-gray hippo-gray
