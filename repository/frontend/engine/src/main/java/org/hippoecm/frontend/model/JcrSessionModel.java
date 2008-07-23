83ef97bb42d35d2215380a88fdbd2295a0cdbea9
Arthur Bogaart <a.bogaart@onehippo.com>
2008-07-23 11:24:56 +0000

    HREPTWO-1151: implemented invalid-login page
    svn path=/hippo-ecm/trunk/; revision=12725

--- repository/frontend/engine/src/main/java/org/hippoecm/frontend/model/JcrSessionModel.java
+++ repository/frontend/engine/src/main/java/org/hippoecm/frontend/model/JcrSessionModel.java
@@ -25,6 +25,7 @@ import org.apache.wicket.RestartResponseException;
 import org.apache.wicket.model.LoadableDetachableModel;
 import org.apache.wicket.protocol.http.WebRequestCycle;
 import org.apache.wicket.util.value.ValueMap;
+import org.hippoecm.frontend.InvalidLoginPage;
 import org.hippoecm.frontend.Main;
 import org.hippoecm.frontend.session.SessionClassLoader;
 import org.hippoecm.frontend.session.WorkflowManagerDecorator;
@@ -138,7 +139,7 @@ public class JcrSessionModel extends LoadableDetachableModel {
         if (result == null) {
             Main main = (Main) Application.get();
             main.resetConnection();
-            throw new RestartResponseException(main.getHomePage());
+            throw new RestartResponseException(InvalidLoginPage.class);
         }
         return result;
     }
