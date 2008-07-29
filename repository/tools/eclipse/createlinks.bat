82241c337402368ef20ab231d1ba6b91d643933e
Walter Grevink <w.grevink@onehippo.com>
2008-07-29 10:47:28 +0000

    HREPTWO-1281 Eclipse scripts now also fix quickstart projects
    svn path=/hippo-ecm/trunk/; revision=12784

--- repository/tools/eclipse/createlinks.bat
+++ repository/tools/eclipse/createlinks.bat
@@ -3,14 +3,20 @@ REM This script assumes that it's located in and executed from the
 REM hippo-ecm/tools/eclipse directory.
 
 cd ..\..\
+
 cd package\war\src\main\webapp
 junction skin ..\..\..\..\skin\src\main\webapp
 junction xinha ..\..\..\..\..\addon\xinha\src\main\webapp
-cd ..\..\..\..\..
-cd addon\xinha\src\main\webapp\xinha\plugins
+
+cd ..\..\..\..\..\quickstart\war\src\main\webapp
+junction skin ..\..\..\..\..\package\skin\src\main\webapp
+junction xinha ..\..\..\..\..\addon\xinha\src\main\webapp
+
+cd ..\..\..\..\..\addon\xinha\src\main\webapp\xinha\plugins
 junction AutoSave ..\..\..\..\..\webResources\xinha\plugins\AutoSave
 junction AutoResize ..\..\..\..\..\webResources\xinha\plugins\AutoResize
 junction CustomLinker ..\..\..\..\..\webResources\xinha\plugins\CustomLinker
 junction ImagePicker ..\..\..\..\..\webResources\xinha\plugins\ImagePicker
+
 cd ..\skins
 junction hippo-gray ..\..\..\..\..\webResources\xinha\skins\hippo-gray
--- repository/tools/eclipse/createlinks.sh
+++ repository/tools/eclipse/createlinks.sh
@@ -3,13 +3,21 @@
  
 cd $(dirname $0) 
  
-cd ../../package/war/src/main/webapp
+cd ../..
+
+cd package/war/src/main/webapp
 ln -s -v ../../../../skin/src/main/webapp skin
 ln -s -v ../../../../../addon/xinha/src/main/webapp xinha
+
+cd ../../../../../quickstart/war/src/main/webapp
+ln -s -v ../../../../../package/skin/src/main/webapp skin
+ln -s -v ../../../../../addon/xinha/src/main/webapp xinha
+
 cd ../../../../../addon/xinha/src/main/webapp/xinha/plugins
 ln -s -v ../../../../../webResources/xinha/plugins/AutoSave AutoSave
 ln -s -v ../../../../../webResources/xinha/plugins/AutoResize AutoResize
 ln -s -v ../../../../../webResources/xinha/plugins/CustomLinker CustomLinker
 ln -s -v ../../../../../webResources/xinha/plugins/ImagePicker ImagePicker
+
 cd ../skins
 ln -s -v ../../../../../webResources/xinha/skins/hippo-gray hippo-gray
