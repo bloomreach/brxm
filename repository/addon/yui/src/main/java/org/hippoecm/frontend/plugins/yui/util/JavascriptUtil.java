70f875f0863b3ddfc65a5c9b33a6fc37047b1432
Arthur Bogaart <a.bogaart@onehippo.com>
2008-07-29 11:44:21 +0000

    HREPTWO-16: turned of js-function call matching since it interferes with url's
    svn path=/hippo-ecm/trunk/; revision=12789

--- repository/addon/yui/src/main/java/org/hippoecm/frontend/plugins/yui/util/JavascriptUtil.java
+++ repository/addon/yui/src/main/java/org/hippoecm/frontend/plugins/yui/util/JavascriptUtil.java
@@ -25,7 +25,7 @@ public class JavascriptUtil {
     public static final String SINGLE_QUOTE = "'";
     
     private static Pattern numbers = Pattern.compile("\\d*");
-    private static Pattern functions = Pattern.compile("(\\w+)\\.(\\w+)");
+    //private static Pattern functions = Pattern.compile("(\\w+)\\.(\\w+)");
     
     public static String serialize2JS(String value) {
         if (value == null)
@@ -36,7 +36,7 @@ public class JavascriptUtil {
             return "true";
         else if (value.equalsIgnoreCase("false"))
             return "false";
-        else if (numbers.matcher(value).matches() || functions.matcher(value).find())
+        else if (numbers.matcher(value).matches()) // || functions.matcher(value).find())
             return value;
 
         return SINGLE_QUOTE + value.replaceAll(SINGLE_QUOTE, "\\\\" + SINGLE_QUOTE) + SINGLE_QUOTE;
