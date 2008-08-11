7ca1fea333e7b0397b786fae1b5622d120a5c6ab
Ard Schrijvers <a.schrijvers@onehippo.com>
2008-08-11 16:56:25 +0000

    HREPTWO-1386 get a template node through reflection instead of if statements.
    svn path=/experimental/hst2/trunk/; revision=12975

--- site-toolkit/core/src/main/java/org/hippoecm/hst/core/template/node/NodeList.java
+++ site-toolkit/core/src/main/java/org/hippoecm/hst/core/template/node/NodeList.java
@@ -1,5 +1,7 @@
 package org.hippoecm.hst.core.template.node;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,81 +10,85 @@ import javax.jcr.NodeIterator;
 import javax.jcr.RepositoryException;
 
 import org.hippoecm.hst.core.template.ContextBase;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.sun.corba.se.spi.activation.Repository;
 
 public class NodeList<E extends TemplateNode> {
+    
+    private Logger log = LoggerFactory.getLogger(NodeList.class);
+    
     private Node mapNode;
     private Class listItemsClass;
     private List<E> listItems;
     private ContextBase contextBase;
-    
+
     public NodeList() {
-    	listItems = new ArrayList<E>();
+        listItems = new ArrayList<E>();
     }
-    
-	public NodeList(ContextBase contextBase, String relativePath, Class itemsClass) throws RepositoryException {
-		this.contextBase = contextBase;
-		this.mapNode = contextBase.getRelativeNode(relativePath);		
-		this.listItemsClass = itemsClass;	
-		listItems = createItemList();
-	}
-	
-	public NodeList(ContextBase contextBase, Node mapNode, Class itemsClass) throws RepositoryException {
-		this.contextBase = contextBase;
-		this.mapNode = mapNode;
-		this.listItemsClass = itemsClass;	
-		listItems = createItemList();
-	}
-	
-	public List<E> getItems() {
-		return listItems;
-	}
-	
-	public E getItemByNodeName(String nodeName) throws RepositoryException {
-		for (E item : listItems) {
-			if (item.getJcrNode().getName().equals(nodeName)) {
-				return item;
-			}
-		}
-		return null;
-	}
-	
-	private List<E> createItemList() {
-		List<E> items = new ArrayList<E> ();
-		try {
-			NodeIterator iter = mapNode.getNodes();
-			while (iter.hasNext()) {
-				Node node = (Node) iter.next();
-				items.add(getNode(node));
-			}
-		} catch (RepositoryException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return items;
-	}
-	
-	private E getNode(Node n) {
-		//a PageNode has childnode PageContainerNod
-	
-		if (listItemsClass.equals(PageContainerNode.class)) {
-		    return (E) new PageContainerNode(contextBase, n);		  
-		}
-		if (listItemsClass.equals(TemplatePartNode.class)) {
-			return (E) new TemplatePartNode(contextBase, n);
-		}
-		if (listItemsClass.equals(PageContainerModuleNode.class)) {
-			return (E) new PageContainerModuleNode(contextBase, n);
-		}
-		if (listItemsClass.equals(LayoutAttributeNode.class)) {
-			return (E) new LayoutAttributeNode(contextBase, n);
-		}
-		if (listItemsClass.equals(NavigationItemNode.class)) {
-			return (E) new NavigationItemNode(contextBase, n);
-		}
-		return (E) new TemplateNode(contextBase, n);		
-	}
-	
-	
+
+    public NodeList(ContextBase contextBase, String relativePath, Class itemsClass) throws RepositoryException {
+        this.contextBase = contextBase;
+        this.mapNode = contextBase.getRelativeNode(relativePath);
+        this.listItemsClass = itemsClass;
+        listItems = createItemList();
+    }
+
+    public NodeList(ContextBase contextBase, Node mapNode, Class itemsClass) throws RepositoryException {
+        this.contextBase = contextBase;
+        this.mapNode = mapNode;
+        this.listItemsClass = itemsClass;
+        listItems = createItemList();
+    }
+
+    public List<E> getItems() {
+        return listItems;
+    }
+
+    public E getItemByNodeName(String nodeName) throws RepositoryException {
+        for (E item : listItems) {
+            if (item.getJcrNode().getName().equals(nodeName)) {
+                return item;
+            }
+        }
+        return null;
+    }
+
+    private List<E> createItemList() {
+        List<E> items = new ArrayList<E>();
+        try {
+            NodeIterator iter = mapNode.getNodes();
+            while (iter.hasNext()) {
+                Node node = (Node) iter.next();
+                items.add(getNode(node));
+            }
+        } catch (RepositoryException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return items;
+    }
+
+    private E getNode(Node n) {
+        //a PageNode has childnode PageContainerNode
+        String message;
+        try {
+            Class[] formalArgs = new Class[] { ContextBase.class, Node.class };
+            Constructor constructor = listItemsClass.getConstructor(formalArgs);
+            Object[] actualArgs = new Object[] { contextBase, n };
+            E e = (E) constructor.newInstance(actualArgs);
+            return e;
+
+        } catch (InvocationTargetException e) {
+            message = e.getTargetException().getClass().getName() + ": " + e.getTargetException().getMessage();
+        } catch (Exception e) {
+            message = e.getClass().getName() + ": " + e.getMessage();
+        }
+        log.error("failed to instantiate class " + listItemsClass.getName() + "\n " + message + "\n load default TemplateNode");
+        // if error happened, return default TemplateNode
+        return (E) new TemplateNode(contextBase, n);
+
+    }
+
 }
