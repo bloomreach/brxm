03c66d28edff090c4475d7eda7213a0e51019b15
Ard Schrijvers <a.schrijvers@onehippo.com>
2008-08-12 11:30:39 +0000

    HST-106 getvariable name default impl in abstractr modulebase
    svn path=/experimental/hst2/trunk/; revision=12986

--- site-toolkit/core/src/main/java/org/hippoecm/hst/core/template/module/ModuleBase.java
+++ site-toolkit/core/src/main/java/org/hippoecm/hst/core/template/module/ModuleBase.java
@@ -1,13 +1,5 @@
 package org.hippoecm.hst.core.template.module;
 
-import javax.jcr.PathNotFoundException;
-import javax.jcr.Property;
-import javax.jcr.RepositoryException;
-import javax.servlet.RequestDispatcher;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.jsp.PageContext;
-
 import org.hippoecm.hst.core.template.TemplateException;
 import org.hippoecm.hst.core.template.node.ModuleNode;
 
@@ -19,6 +11,10 @@ public abstract class ModuleBase implements Module {
 	
 	public void setVariableName(String name) {
 		this.variableName = name;
+	}
+	
+	public String getVariableName() {
+	    return variableName;
 	}
 	
 	public void setModuleNode(ModuleNode node) {
