6f3cbeb5e93af10be7d4fd8564ce906ea860784b
Jeroen Reijn <j.reijn@onehippo.com>
2008-08-13 10:34:46 +0000

    HST-101: Create default RepositoryBasedNavigationModule
    svn path=/experimental/hst2/trunk/; revision=13004

---
+++ site-toolkit/core/src/main/java/org/hippoecm/hst/core/template/module/navigation/NavigationItem.java
@@ -0,0 +1,66 @@
+package org.hippoecm.hst.core.template.module.navigation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.jcr.Node;
+import javax.jcr.NodeIterator;
+import javax.jcr.RepositoryException;
+
+import org.hippoecm.hst.core.template.node.el.AbstractELNode;
+import org.hippoecm.repository.api.HippoNodeType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author jreijn
+ * 
+ *
+ */
+public class NavigationItem extends AbstractELNode {
+    private static final Logger log = LoggerFactory.getLogger(NavigationItem.class);
+
+    Boolean selected = false;
+
+    public NavigationItem(Node node) {
+        super(node);
+    }
+
+    public NavigationItem(Node node,Boolean selected) {
+        super(node);
+        this.selected = selected;
+    }
+
+    public List<NavigationItem> getChildren() {
+        List<NavigationItem> wrappedChildNodes = new ArrayList<NavigationItem>();
+        NodeIterator subNodes;
+        try {
+            subNodes = this.getJcrNode().getNodes();
+            while (subNodes.hasNext()) {
+                Node subNode = subNodes.nextNode();
+                // always check a node iterator node for null
+                if (subNode == null) {
+                    continue;
+                }
+                if (subNode.isNodeType(HippoNodeType.NT_HANDLE)) {
+                    // if there is no document present with the same name as the handle
+                    // skip this one (ie, there is a unpublished version, but not a published one)
+                    if (!subNode.hasNode(subNode.getName())) {
+                        continue;
+                    }
+                    subNode = subNode.getNode(subNode.getName());
+                }
+                wrappedChildNodes.add(new NavigationItem(subNode));
+            }
+
+        } catch (RepositoryException e) {
+            log.error("RepositoryException while fetching children " + e.getMessage());
+        }
+        return wrappedChildNodes;
+    }
+
+    public Boolean getSelected() {
+        return this.selected;
+    }
+
+}
---
+++ site-toolkit/core/src/main/java/org/hippoecm/hst/core/template/module/navigation/RepositoryBasedNavigationModule.java
@@ -0,0 +1,68 @@
+package org.hippoecm.hst.core.template.module.navigation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.jcr.Node;
+import javax.jcr.NodeIterator;
+import javax.jcr.RepositoryException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.jsp.PageContext;
+
+import org.hippoecm.hst.core.template.ContextBase;
+import org.hippoecm.hst.core.template.ContextBaseFilter;
+import org.hippoecm.hst.core.template.HstFilterBase;
+import org.hippoecm.hst.core.template.TemplateException;
+import org.hippoecm.hst.core.template.module.ModuleBase;
+import org.hippoecm.hst.core.template.node.ModuleNode;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class RepositoryBasedNavigationModule extends ModuleBase {
+    private static final Logger log = LoggerFactory.getLogger(RepositoryBasedNavigationModule.class);
+
+    public String execute(HttpServletRequest request, HttpServletResponse response) throws TemplateException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public void init(HttpServletRequest request) {
+        // TODO Auto-generated method stub
+    }
+    
+    /**
+     * Puts an List of wrapped JCR Nodes on the pageContext that can be used by the corresponding JSP.
+     * The name of the object can be set with an attribute named "var" on the corresponding module tag.
+     *    
+     * @see    PageContext
+     */    
+    public void render(PageContext pageContext) {
+        HttpServletRequest request = (HttpServletRequest) pageContext.getRequest();
+        ModuleNode currNode = (ModuleNode) request.getAttribute("currentModuleNode");
+
+        String path = currNode.getPropertyValue("hst:contentlocation");
+        if(path == null) {
+            pageContext.setAttribute(getVar(),new ArrayList<NavigationItem>());
+            return;
+        }
+        ContextBase ctxBase = (ContextBase) request.getAttribute(HstFilterBase.CONTENT_CONTEXT_REQUEST_ATTRIBUTE);
+        List<NavigationItem> wrappedNodes = new ArrayList<NavigationItem>();
+        try {
+            Node n = ctxBase.getRelativeNode(path);
+            NodeIterator subNodes = n.getNodes();
+            while (subNodes.hasNext()) {
+                Node subNode = subNodes.nextNode();
+                if(subNode == null) {continue;}
+                wrappedNodes.add(new NavigationItem(subNode));
+            }
+        } catch (RepositoryException e) {
+            log.error(e.getMessage(), e);
+            wrappedNodes = new ArrayList();
+        }
+        
+        pageContext.setAttribute(getVar(), wrappedNodes);
+
+    }
+
+}
