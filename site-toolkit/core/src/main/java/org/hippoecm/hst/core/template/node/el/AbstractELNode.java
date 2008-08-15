18b7e4734ce085d429e5200368982bae1ff658ca
Ard Schrijvers <a.schrijvers@onehippo.com>
2008-08-15 12:09:15 +0000

    HST-105 started implementation for correct display of properties other then type String. Needed is that a date prop can have a pattern, and how to display some props like a reference, and how to do multivalued props
    svn path=/experimental/hst2/trunk/; revision=13050

--- site-toolkit/core/src/main/java/org/hippoecm/hst/core/template/node/el/AbstractELNode.java
+++ site-toolkit/core/src/main/java/org/hippoecm/hst/core/template/node/el/AbstractELNode.java
@@ -20,7 +20,9 @@ import java.util.Map;
 
 import javax.jcr.Node;
 import javax.jcr.PathNotFoundException;
+import javax.jcr.PropertyType;
 import javax.jcr.RepositoryException;
+import javax.jcr.Value;
 import javax.jcr.ValueFormatException;
 
 import org.hippoecm.hst.core.template.ContextBase;
@@ -30,105 +32,159 @@ import org.hippoecm.repository.api.ISO9075Helper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public abstract class AbstractELNode implements ELNode{
-    
-      private Logger log = LoggerFactory.getLogger(AbstractELNode.class);
-      protected Node jcrNode; 
-      
-      public AbstractELNode(Node node) {
-          this.jcrNode = node;
-      }
-    
-      public AbstractELNode(ContextBase contextBase, String relativePath) throws RepositoryException  {
-          this.jcrNode = contextBase.getRelativeNode(relativePath);
+public abstract class AbstractELNode implements ELNode {
+
+    private Logger log = LoggerFactory.getLogger(AbstractELNode.class);
+    protected Node jcrNode;
+
+    public AbstractELNode(Node node) {
+        this.jcrNode = node;
+    }
+
+    public AbstractELNode(ContextBase contextBase, String relativePath) throws RepositoryException {
+        this.jcrNode = contextBase.getRelativeNode(relativePath);
     }
 
     public Node getJcrNode() {
-          return jcrNode;
-      }
-      
+        return jcrNode;
+    }
+
     public Map getProperty() {
-        if(jcrNode == null) {
+        if (jcrNode == null) {
             log.error("jcrNode is null. Return empty map");
             return Collections.EMPTY_MAP;
         }
         return new ELPseudoMap() {
             public Object get(Object propertyName) {
-                try { 
-                    return jcrNode.getProperty((String) propertyName).getValue().getString();
-                } catch (ValueFormatException e) {                  
+                String prop = (String) propertyName;
+                try {
+                    if (!jcrNode.hasProperty(prop)) {
+                        log.debug("Property " + prop + " not found. Return empty string");
+                        return "";
+                    }
+                    if (jcrNode.getProperty(prop).getDefinition().isMultiple()) {
+                        StringBuffer sb = new StringBuffer("");
+                        for (Value val : jcrNode.getProperty(prop).getValues()) {
+                            sb.append(value2String(val));
+                            sb.append(" ");
+                        }
+                        return sb;
+                    } else {
+                        return value2String(jcrNode.getProperty(prop).getValue());
+                    }
+
+                } catch (PathNotFoundException e) {
                     e.printStackTrace();
-                } catch (IllegalStateException e) {                 
-                    e.printStackTrace();
-                } catch (PathNotFoundException e) {             
-                    e.printStackTrace();
-                } catch (RepositoryException e) {               
+                } catch (RepositoryException e) {
                     e.printStackTrace();
                 }
                 return "";
             }
         };
-     }
-    
+    }
+
+    private String value2String(Value val) {
+        try {
+            switch (val.getType()) {
+            case PropertyType.BINARY:
+                break;
+            case PropertyType.BOOLEAN:
+                return String.valueOf(val.getBoolean());
+            case PropertyType.DATE:
+                // todo : format date
+                return val.getDate().toString();
+            case PropertyType.DOUBLE:
+                return String.valueOf(val.getDouble());
+            case PropertyType.LONG:
+                return String.valueOf(val.getLong());
+            case PropertyType.REFERENCE:
+                // TODO return path of referenced node?
+                break;
+            case PropertyType.PATH:
+                // TODO return what?
+                break;
+            case PropertyType.STRING:
+                return val.getString();
+            case PropertyType.NAME:
+                // TODO what to return
+                break;
+            default:
+                log.error("Illegal type for Value");
+                return "";
+            }
+        } catch (ValueFormatException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (RepositoryException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return "";
+    }
+
     public Map getResourceUrl() {
-        if(jcrNode == null) {
+        if (jcrNode == null) {
             log.error("jcrNode is null. Return empty map");
             return Collections.EMPTY_MAP;
         }
         return new ELPseudoMap() {
             private String DEFAULT_RESOURCE = "";
+
             @Override
             public Object get(Object resource) {
-                String resourceName = (String)resource;
+                String resourceName = (String) resource;
                 try {
-                    if(jcrNode.hasNode(resourceName)){
+                    if (jcrNode.hasNode(resourceName)) {
                         Node resourceNode = jcrNode.getNode(resourceName);
-                        if(resourceNode.isNodeType(HippoNodeType.NT_RESOURCE)){
-                           if(resourceNode instanceof HippoNode && ((HippoNode)resourceNode).getCanonicalNode() != null) {
-                               Node canonical = ((HippoNode)resourceNode).getCanonicalNode();
-                               log.info("resource location = " + "/binaries"+canonical.getPath());
-                               return "/binaries"+canonical.getPath();
-                           } else {
-                               log.info("resource location = " + "/binaries"+resourceNode.getPath());
-                               return "/binaries"+resourceNode.getPath();
-                           }
+                        if (resourceNode.isNodeType(HippoNodeType.NT_RESOURCE)) {
+                            if (resourceNode instanceof HippoNode
+                                    && ((HippoNode) resourceNode).getCanonicalNode() != null) {
+                                Node canonical = ((HippoNode) resourceNode).getCanonicalNode();
+                                log.info("resource location = " + "/binaries" + canonical.getPath());
+                                return "/binaries" + canonical.getPath();
+                            } else {
+                                log.info("resource location = " + "/binaries" + resourceNode.getPath());
+                                return "/binaries" + resourceNode.getPath();
+                            }
                         } else {
                             log.error(resourceName + "not of type hippo:resource. Returning default value");
                             return DEFAULT_RESOURCE;
                         }
                     }
                 } catch (RepositoryException e) {
-                    log.error("RepositoryException while looking for resource " + resourceName + "  :" + e.getMessage());
+                    log
+                            .error("RepositoryException while looking for resource " + resourceName + "  :"
+                                    + e.getMessage());
                 }
                 return DEFAULT_RESOURCE;
             }
         };
     }
-    
-      public String getDecodedName() {
-          try {
-              return ISO9075Helper.decodeLocalName(jcrNode.getName());
-          } catch (Exception e) {
-              log.error(e.getMessage(), e);
-              return "";
-          }
-      }
 
-      public String getName() {
-          try {
-              return jcrNode.getName();
-          } catch (Exception e) {
-              log.error(e.getMessage(), e);
-              return "";
-          }
-      }
+    public String getDecodedName() {
+        try {
+            return ISO9075Helper.decodeLocalName(jcrNode.getName());
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+            return "";
+        }
+    }
 
-      public String getPath() {
-          try {
-              return jcrNode.getPath();
-          } catch (RepositoryException e) {
-              log.error(e.getMessage(), e);
-              return "";
-          }
-      }
+    public String getName() {
+        try {
+            return jcrNode.getName();
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+            return "";
+        }
+    }
+
+    public String getPath() {
+        try {
+            return jcrNode.getPath();
+        } catch (RepositoryException e) {
+            log.error(e.getMessage(), e);
+            return "";
+        }
+    }
 }
