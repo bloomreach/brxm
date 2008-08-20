f59d673a547f7a96ae160d3c34d6206aabb566e1
Jeroen Reijn <j.reijn@onehippo.com>
2008-08-20 09:45:05 +0000

    HST-102: Refactored class
    svn path=/experimental/hst2/trunk/; revision=13090

--- site-toolkit/components/src/main/java/org/hippoecm/hst/components/modules/breadcrumb/RepositoryBasedBreadcrumbModule.java
+++ site-toolkit/components/src/main/java/org/hippoecm/hst/components/modules/breadcrumb/RepositoryBasedBreadcrumbModule.java
@@ -1,3 +1,18 @@
+/*
+ *  Copyright 2008 Hippo.
+ * 
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package org.hippoecm.hst.components.modules.breadcrumb;
 
 import java.util.ArrayList;
@@ -8,13 +23,11 @@ import javax.jcr.RepositoryException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.jsp.PageContext;
 
-import org.hippoecm.hst.components.modules.navigation.SubNavigationModule;
-import org.hippoecm.hst.components.modules.navigation.NavItem;
-
+import org.hippoecm.hst.components.modules.navigation.NavigationItem;
+import org.hippoecm.hst.components.modules.navigation.RepositoryBasedNavigationModule;
 import org.hippoecm.hst.core.template.ContextBase;
 import org.hippoecm.hst.core.template.HstFilterBase;
 import org.hippoecm.hst.core.template.TemplateException;
-import org.hippoecm.hst.core.template.module.navigation.RepositoryBasedNavigationModule;
 import org.hippoecm.hst.core.template.node.ModuleNode;
 import org.hippoecm.hst.core.template.node.PageNode;
 import org.hippoecm.repository.api.HippoNodeType;
@@ -23,7 +36,7 @@ import org.slf4j.LoggerFactory;
 
 public class RepositoryBasedBreadcrumbModule extends RepositoryBasedNavigationModule {
 
-	private static final Logger log = LoggerFactory.getLogger(SubNavigationModule.class);
+	private static final Logger log = LoggerFactory.getLogger(RepositoryBasedBreadcrumbModule.class);
 
 	public void render(PageContext pageContext) {
 		HttpServletRequest request = (HttpServletRequest) pageContext.getRequest();
@@ -37,12 +50,12 @@ public class RepositoryBasedBreadcrumbModule extends RepositoryBasedNavigationMo
 		}
 
         if(path == null) {
-            pageContext.setAttribute(getVar(),new ArrayList<NavItem>());
+            pageContext.setAttribute(getVar(),new ArrayList<NavigationItem>());
             return;
         }
         ContextBase ctxBase = (ContextBase) request.getAttribute(HstFilterBase.CONTENT_CONTEXT_REQUEST_ATTRIBUTE);
 
-	    List<NavItem> wrappedNodes = new ArrayList<NavItem>();
+	    List<NavigationItem> wrappedNodes = new ArrayList<NavigationItem>();
 	    try {
 
 	    	String currentLocation = path;
@@ -69,13 +82,13 @@ public class RepositoryBasedBreadcrumbModule extends RepositoryBasedNavigationMo
 	    			subNode = subNode.getNode(selectedItemsList.get(i).toString());
 	    		}
 	    		if(!subNode.isNodeType(HippoNodeType.NT_HANDLE)){
-	    			wrappedNodes.add(new NavItem(subNode,true));
+	    			wrappedNodes.add(new NavigationItem(subNode,true));
 	    		}
 	    	}
 
 		} catch (RepositoryException e) {
 			log.error(e.getMessage(), e);
-			wrappedNodes = new ArrayList<NavItem>();
+			wrappedNodes = new ArrayList<NavigationItem>();
 		}
 
 		pageContext.setAttribute(getVar(), wrappedNodes);
