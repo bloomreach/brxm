8cde352ba921f1c8a4c09f0d12e12fe7ab877bb5
Arthur Bogaart <a.bogaart@onehippo.com>
2008-09-02 09:43:43 +0000

    IN PROGRESS - issue HREPTWO-1399: Xinha/IE7: Resizing Xinha to full screen messed up
    - Created custom fullscreen plugin that a) does a restore instead of re-init and b) in IE fullscreen it moves Xinha to the body element and restores it in it original dom-location.
    - Changed AutoResize plugin to handle scrollbars correctly in IE and use the correct width (the original width of the textarea)
    - Added a minHeight for Xinha textArea's
    
    svn path=/hippo-ecm/trunk/; revision=13264

--- repository/addon/xinha/webResources/xinha/plugins/AutoResize/auto-resize.js
+++ repository/addon/xinha/webResources/xinha/plugins/AutoResize/auto-resize.js
@@ -1,5 +1,73 @@
+AutoResize._pluginInfo = {
+    name :"AutoResize",
+    version :"1.0",
+    developer :"Arthur Bogaart",
+    developer_url :"http://www.onehippo.org",
+    c_owner :"Arthur Bogaart",
+    sponsor :"",
+    sponsor_url :"",
+    license :""
+}
 
-function _isTemplateEditor(el) {
+function AutoResize(editor) {
+    this.editor = editor;
+    this.init   = {
+            initialized: false, 
+            scroll: false,
+            minHeight: 200,
+            marginWidth: 0, 
+            scrollbarWidth: this.getScrollbarWidth(), 
+            isTemplateEditor: this.isTemplateEditor(this.editor._textArea)
+    };
+    this.dim    = {width: 0, height: this.init.minHeight};
+    this.preDim = {width:0, height:0};
+
+    YAHOO.hippo.LayoutManager.registerResizeListener(this.editor._textArea,
+            this, function(type, args, me) {
+        
+        var unit = args[0];
+        var sizes = unit.getSizes();
+        if(sizes.body.w) me.dim.width  = parseInt(sizes.body.w);
+        if(sizes.body.h) me.dim.height = parseInt(sizes.body.h);
+        
+        var scrollBottom = unit.body.scrollHeight - (unit.body.scrollTop + unit.body.clientHeight); // height of element scroll
+        var scroll = unit.body.scrollTop + scrollBottom > 0;
+ 
+        if(!me.init.initialized) {
+            me.init.marginWidth = me.dim.width - parseInt(me.editor._initial_ta_size.w.substr(0, me.editor._initial_ta_size.w.length-2));
+            me.init.scroll = scroll;
+            me.init.initialized = true;
+        } else {
+            if (scroll && !me.init.scroll) 
+                me.dim.width -= me.init.scrollbarWidth;
+            else if(!scroll && me.init.scroll) 
+                me.dim.width += me.init.scrollbarWidth;
+        }
+        me.dim.width  = me.dim.width - me.init.marginWidth - me.getBrowserSpecificMargin();
+        var h = parseInt(me.dim.height * (14 / 30));
+        me.dim.height = h < me.init.minHeight ? me.init.minHeight : h; 
+        
+        if(me.preDim.width != me.dim.width || me.preDim.height != me.dim.height) {
+            me.doAutoResize(me.editor, me.dim.width, me.dim.height);
+            me.preDim.width = me.dim.width;
+            me.preDim.height = me.dim.height;
+        }
+    });
+}
+
+AutoResize.prototype.doAutoResize = function(editor, w, h) {
+    if (editor._iframe == null) {
+        var me = this;
+        var again = function() {
+            me.doAutoResize(editor, w, h);
+        }
+        window.setTimeout(again, 250);
+    } else {
+        editor.sizeEditor(w + 'px', h + 'px', true, true);
+    }
+};
+
+AutoResize.prototype.isTemplateEditor = function(el) {
     while (el != null && el != document.body) {
         if (YAHOO.util.Dom.hasClass(el, 'templateItemTable')) {
             return true;
@@ -7,45 +75,41 @@ function _isTemplateEditor(el) {
         el = el.parentNode;
     }
     return false;
-}
-
-function AutoResize(editor) {
-    this.editor = editor;
-    
-    var isTemplateEditor = _isTemplateEditor(this.editor._textArea);
-
-    YAHOO.hippo.LayoutManager.registerResizeListener(this.editor._textArea, this.editor, function(type, args, me) {
-        var w= args[0].body.w ? parseInt(args[0].body.w) : null;
-        var h= args[0].body.h ? parseInt(args[0].body.h) : null;
-        if(YAHOO.lang.isNumber(w)) {
-            w -= isTemplateEditor ? 73 : 30;
-        } 
-        if(YAHOO.lang.isNumber(h)) {
-            h =parseInt(h*(4/9));
-        }
-        doAutoResize(me, w, h);
-    });
-    
-}
-
-AutoResize._pluginInfo = {
-  name          : "AutoResize",
-  version       : "1.0",
-  developer     : "Arthur Bogaart",
-  developer_url : "http://www.onehippo.org",
-  c_owner       : "Arthur Bogaart",
-  sponsor       : "",
-  sponsor_url   : "",
-  license       : ""
-}
-
-function doAutoResize(editor, w, h) {
-    if (editor._iframe == null) {
-        var again  = function() {
-            doAutoResize(editor, w, h);
-        }
-        window.setTimeout(again, 100);
-    } else {
-        editor.sizeEditor(w + 'px', h + 'px', true, true);
-    }
 };
+
+AutoResize.prototype.getBrowserSpecificMargin = function() {
+    if(Xinha.is_ie) 
+        return 18;
+    else
+        return 2;
+};
+
+/**
+ * This function calculates the width of a scrollbar by adding an element without overflow
+ * to the dom, then adding a much bigger child element, setting the overflow to auto and
+ * calculating the difference from before and after. 
+ * @return The width of a scrollbar
+ */
+AutoResize.prototype.getScrollbarWidth = function() {
+    var parent = document.createElement('div');
+    parent.style.position = 'absolute';
+    parent.style.top = '-1000px';
+    parent.style.left = '-1000px';
+    parent.style.width = '100px';
+    parent.style.height = '50px';
+    parent.style.overflow = 'hidden';
+  
+    var child = document.createElement('div');
+    child.style.width = '100%';
+    child.style.height = '200px';
+  
+    parent.appendChild(child);
+    document.body.appendChild(parent);
+  
+    var beforeScroll = child.offsetWidth;
+    parent.style.overflow = 'auto';
+    var scrollWidth = beforeScroll - child.offsetWidth;
+  
+    document.body.removeChild(document.body.lastChild);
+    return scrollWidth;
+}
---
+++ repository/addon/xinha/webResources/xinha/plugins/FullscreenCompatible/fullscreen-compatible.js
@@ -0,0 +1,249 @@
+function FullscreenCompatible(editor, args)
+{
+  this.editor = editor;
+  this.originalSizes = null;
+  editor._superclean_on = false;
+  cfg = editor.config;
+
+  cfg.registerButton
+  ( 'fullscreen',
+    this._lc("Maximize/Minimize Editor"),
+    [_editor_url + cfg.imgURL + 'ed_buttons_main.gif',8,0], true,
+      function(e, objname, obj)
+      {
+        e._fullscreenCompatible();
+      }
+  );
+
+  // See if we can find 'popupeditor' and replace it with fullscreen
+  cfg.addToolbarElement("fullscreen", "fullscreen", 0);
+}
+
+FullscreenCompatible._pluginInfo =
+{
+  name     : "FullscreenCompatible",
+  version  : "1.0",
+  developer: "AB",
+  developer_url: "http://www.onehippo.com/",
+  c_owner      : "OneHippo",
+  license      : "al2",
+  sponsor      : "",
+  sponsor_url  : ""
+};
+
+FullscreenCompatible.prototype._lc = function(string) {
+    return Xinha._lc(string, {url : _editor_url + 'modules/FullScreen/lang/',context:"FullScreen"});
+};
+
+/** fullScreen makes an editor take up the full window space (and resizes when the browser is resized)
+ *  the principle is the same as the "popupwindow" functionality in the original htmlArea, except
+ *  this one doesn't popup a window (it just uses to positioning hackery) so it's much more reliable
+ *  and much faster to switch between
+ */
+
+Xinha.prototype._fullscreenCompatible = function()
+{
+  var e = this;
+  function sizeItUp()
+  {
+    if(!e._isFullScreen || e._sizing) return false;
+    e._sizing = true;
+    // Width & Height of window
+    var dim = Xinha.viewportSize();
+    e.originalSizes = {
+      x:   parseInt(e._htmlArea.style.width),
+      y:   parseInt(e._htmlArea.style.height),
+      dim: dim
+    };
+
+    var h = dim.y - e.config.fullScreenMargins[0] -  e.config.fullScreenMargins[2];
+    var w = dim.x - e.config.fullScreenMargins[1] -  e.config.fullScreenMargins[3];
+
+    e.sizeEditor(w + 'px', h + 'px', e.config.sizeIncludesBars, e.config.sizeIncludesPanels);
+    e._sizing = false;
+    if ( e._toolbarObjects.fullscreen ) e._toolbarObjects.fullscreen.swapImage([_editor_url + cfg.imgURL + 'ed_buttons_main.gif',9,0]); 
+  }
+
+  function sizeItDown()
+  {
+    if(e._isFullScreen || e._sizing) return false;
+    e._sizing = true;
+
+    if(e.originalSizes != null) 
+    {
+        var os = e.originalSizes;
+        var nDim = Xinha.viewportSize();
+        var nW = os.x + (nDim.x - os.dim.x);
+        var nH = os.y + (nDim.y - os.dim.y);
+        e.sizeEditor( nW + 'px', nH + 'px', e.config.sizeIncludesBars, e.config.sizeIncludesPanels);     
+        e.originalSizes = null;
+    }
+    else e.initSize();
+
+    e._sizing = false;
+    if ( e._toolbarObjects.fullscreen ) e._toolbarObjects.fullscreen.swapImage([_editor_url + cfg.imgURL + 'ed_buttons_main.gif',8,0]); 
+  }
+
+  /** It's not possible to reliably get scroll events, particularly when we are hiding the scrollbars
+   *   so we just reset the scroll ever so often while in fullscreen mode
+   */
+  function resetScroll()
+  {
+    if(e._isFullScreen)
+    {
+      window.scroll(0,0);
+      window.setTimeout(resetScroll,150);
+    }
+  }
+
+  if(typeof this._isFullScreen == 'undefined')
+  {
+    this._isFullScreen = false;
+    if(e.target != e._iframe)
+    {
+      Xinha._addEvent(window, 'resize', sizeItUp);
+    }
+  }
+
+  // Gecko has a bug where if you change position/display on a
+  // designMode iframe that designMode dies.
+  if(Xinha.is_gecko)
+  {
+    this.deactivateEditor();
+  }
+
+  if(this._isFullScreen)
+  {
+      
+    // Unmaximize
+    this._htmlArea.style.position = '';
+    if (!Xinha.is_ie ) this._htmlArea.style.border   = '';
+
+    try
+    {
+      if(Xinha.is_ie && document.compatMode == 'CSS1Compat')
+      {
+        var bod = document.getElementsByTagName('html');
+      }
+      else
+      {
+        var bod = document.getElementsByTagName('body');
+      }
+      bod[0].style.overflow='';
+    }
+    catch(e)
+    {
+      // Nutthin
+    }
+    this._isFullScreen = false;
+    sizeItDown();
+
+    // Restore all ancestor positions
+    var ancestor = this._htmlArea;
+    while((ancestor = ancestor.parentNode) && ancestor.style)
+    {
+      ancestor.style.position = ancestor._xinha_fullScreenOldPosition;
+      ancestor._xinha_fullScreenOldPosition = null;
+    }
+    
+    if ( Xinha.ie_version < 7 )
+    {
+      var selects = document.getElementsByTagName("select");
+      for ( var i=0;i<selects.length;++i )
+      {
+        selects[i].style.visibility = 'visible';
+      }
+    }
+    window.scroll(this._unScroll.x, this._unScroll.y);
+    
+    if (Xinha.is_ie) {
+        document.body.removeChild(this._htmlArea);
+        this._parentNode.appendChild(this._htmlArea);
+      }  
+
+  }
+  else
+  {
+      
+    // Get the current Scroll Positions
+    this._unScroll =
+    {
+     x:(window.pageXOffset)?(window.pageXOffset):(document.documentElement)?document.documentElement.scrollLeft:document.body.scrollLeft,
+     y:(window.pageYOffset)?(window.pageYOffset):(document.documentElement)?document.documentElement.scrollTop:document.body.scrollTop
+    };
+
+    if(Xinha.is_ie) {
+        this._parentNode = this._htmlArea.parentNode;
+        this._htmlArea.parentNode.removeChild(this._htmlArea);
+        document.body.appendChild(this._htmlArea);
+        
+    } 
+    // Make all ancestors position = static
+    var ancestor = this._htmlArea;
+    while((ancestor = ancestor.parentNode) && ancestor.style)
+    {
+      ancestor._xinha_fullScreenOldPosition = ancestor.style.position;
+      ancestor.style.position = 'static';
+
+    }
+    // very ugly bug in IE < 7 shows select boxes through elements that are positioned over them
+    if ( Xinha.ie_version < 7 )
+    {
+      var selects = document.getElementsByTagName("select");
+      var s, currentEditor;
+      for ( var i=0;i<selects.length;++i )
+      {
+        s = selects[i];
+        currentEditor = false;
+        while ( s = s.parentNode )
+        {
+          if ( s == this._htmlArea )
+          {
+            currentEditor = true;
+            break;
+          }
+        }
+        if ( !currentEditor && selects[i].style.visibility != 'hidden')
+        {
+          selects[i].style.visibility = 'hidden';
+        }
+      }
+    }
+    
+   
+    // Maximize
+    window.scroll(0,0);
+    this._htmlArea.style.position = 'absolute';
+    this._htmlArea.style.zIndex   = 999;
+    this._htmlArea.style.left     = e.config.fullScreenMargins[3] + 'px';
+    this._htmlArea.style.top      = e.config.fullScreenMargins[0] + 'px';
+    if ( !Xinha.is_ie && !Xinha.is_webkit ) this._htmlArea.style.border   = 'none';
+    this._isFullScreen = true;
+    resetScroll();
+
+    try
+    {
+      if(Xinha.is_ie && document.compatMode == 'CSS1Compat')
+      {
+        var bod = document.getElementsByTagName('html');
+      }
+      else
+      {
+        var bod = document.getElementsByTagName('body');
+      }
+      bod[0].style.overflow='hidden';
+    }
+    catch(e)
+    {
+      // Nutthin
+    }
+
+    sizeItUp();
+  }
+
+  if(Xinha.is_gecko)
+  {
+    this.activateEditor();
+  }
+  this.focusEditor();
+};
