6814c2a54944a1c7921a15d76e461c6e60888c7b
Jeroen Reijn <j.reijn@onehippo.com>
2008-09-03 14:43:26 +0000

    HREPTWO-1485: Add "Text plugin" to the editor plugins
    svn path=/hippo-ecm/trunk/; revision=13294

--- repository/addon/editor/src/main/java/org/hippoecm/frontend/editor/plugins/TextTemplatePlugin.java
+++ repository/addon/editor/src/main/java/org/hippoecm/frontend/editor/plugins/TextTemplatePlugin.java
@@ -22,7 +22,6 @@ import org.hippoecm.frontend.plugin.IPluginContext;
 import org.hippoecm.frontend.plugin.config.IPluginConfig;
 import org.hippoecm.frontend.service.render.RenderPlugin;
 import org.hippoecm.frontend.widgets.TextAreaWidget;
-import org.hippoecm.frontend.widgets.TextFieldWidget;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
