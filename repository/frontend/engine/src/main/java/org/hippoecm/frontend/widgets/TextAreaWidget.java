f1e3f6cccfcdece20cbe4283d5f8733c7ed76d62
Jeroen Reijn <j.reijn@onehippo.com>
2008-09-03 11:51:12 +0000

    HREPTWO-1485: Add "Text plugin" to the editor plugins
    svn path=/hippo-ecm/trunk/; revision=13292

--- repository/frontend/engine/src/main/java/org/hippoecm/frontend/widgets/TextAreaWidget.java
+++ repository/frontend/engine/src/main/java/org/hippoecm/frontend/widgets/TextAreaWidget.java
@@ -15,6 +15,7 @@
  */
 package org.hippoecm.frontend.widgets;
 
+import org.apache.wicket.markup.ComponentTag;
 import org.apache.wicket.markup.html.form.TextArea;
 import org.apache.wicket.model.IModel;
 
@@ -23,10 +24,40 @@ public class TextAreaWidget extends AjaxUpdatingWidget {
     private final static String SVN_ID = "$Id$";
 
     private static final long serialVersionUID = 1L;
+    private String rows;
+    private String cols;
 
     public TextAreaWidget(String id, IModel model) {
         super(id, model);
-        addFormField(new TextArea("widget", this.getModel()));
+        addFormField(new TextArea("widget", this.getModel()) {        
+        	 private static final long serialVersionUID = 1L;
+
+             @Override
+             protected void onComponentTag(final ComponentTag tag) {
+                 if (getRows() != null) {
+                     tag.put("rows", getRows());
+                 }
+                 if (getCols() != null) {
+                     tag.put("cols", getCols());
+                 }
+                 super.onComponentTag(tag);
+             }
+        });
+    }
+    public void setRows(String rows) {
+        this.rows = rows;
+    }
+
+    public String getRows() {
+        return rows;
+    }
+
+    public void setCols(String cols) {
+        this.cols=cols;
+    }
+    
+    public String getCols() {
+        return cols;
     }
 
 }
