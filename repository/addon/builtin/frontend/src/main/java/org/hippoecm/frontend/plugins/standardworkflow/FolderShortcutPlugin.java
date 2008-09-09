c08e63767173e1c109e8466d7b899543e7c46196
Berry van Halderen <b.vanhalderen@onehippo.com>
2008-09-09 22:49:33 +0000

    HREPTWO-1487: work in progres, but already functional
    svn path=/hippo-ecm/trunk/; revision=13380

--- repository/addon/builtin/frontend/src/main/java/org/hippoecm/frontend/plugins/standardworkflow/FolderShortcutPlugin.java
+++ repository/addon/builtin/frontend/src/main/java/org/hippoecm/frontend/plugins/standardworkflow/FolderShortcutPlugin.java
@@ -16,43 +16,34 @@
 package org.hippoecm.frontend.plugins.standardworkflow;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.lang.String;
 import java.rmi.RemoteException;
-import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Map;
 import java.util.Set;
-import java.util.TreeSet;
 
+import java.util.TreeMap;
 import javax.jcr.Node;
 import javax.jcr.PathNotFoundException;
 import javax.jcr.RepositoryException;
 import javax.jcr.Session;
-import javax.jcr.nodetype.NodeDefinition;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import org.apache.wicket.Component;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.ajax.markup.html.AjaxLink;
-import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.DropDownChoice;
-import org.apache.wicket.markup.html.panel.EmptyPanel;
-import org.apache.wicket.markup.repeater.Item;
-import org.apache.wicket.markup.repeater.data.IDataProvider;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
 import org.apache.wicket.model.PropertyModel;
 
 import org.hippoecm.frontend.dialog.AbstractDialog;
-import org.hippoecm.frontend.dialog.AbstractWorkflowDialog;
-import org.hippoecm.frontend.dialog.CustomizableDialogLink;
-import org.hippoecm.frontend.dialog.IDialogFactory;
 import org.hippoecm.frontend.dialog.IDialogService;
 import org.hippoecm.frontend.model.JcrItemModel;
 import org.hippoecm.frontend.model.JcrNodeModel;
@@ -60,13 +51,11 @@ import org.hippoecm.frontend.model.WorkflowsModel;
 import org.hippoecm.frontend.plugin.IPluginContext;
 import org.hippoecm.frontend.plugin.IServiceReference;
 import org.hippoecm.frontend.plugin.config.IPluginConfig;
-import org.hippoecm.frontend.plugin.impl.PluginContext;
-import org.hippoecm.frontend.plugin.workflow.AbstractWorkflowPlugin;
 import org.hippoecm.frontend.service.IBrowseService;
 import org.hippoecm.frontend.service.IEditService;
 import org.hippoecm.frontend.service.IJcrService;
+import org.hippoecm.frontend.service.render.RenderPlugin;
 import org.hippoecm.frontend.session.UserSession;
-import org.hippoecm.frontend.widgets.AbstractView;
 import org.hippoecm.frontend.widgets.NamespaceFriendlyChoiceRenderer;
 import org.hippoecm.frontend.widgets.TextFieldWidget;
 import org.hippoecm.repository.api.Document;
@@ -79,7 +68,7 @@ import org.hippoecm.repository.api.WorkflowManager;
 import org.hippoecm.repository.standardworkflow.EditableWorkflow;
 import org.hippoecm.repository.standardworkflow.FolderWorkflow;
 
-public class FolderShortcutPlugin extends AbstractWorkflowPlugin {
+public class FolderShortcutPlugin extends RenderPlugin {
     @SuppressWarnings("unused")
     private final static String SVN_ID = "$Id$";
 
@@ -87,7 +76,9 @@ public class FolderShortcutPlugin extends AbstractWorkflowPlugin {
 
     transient Logger log = LoggerFactory.getLogger(FolderShortcutPlugin.class);
 
-    String defaultDropLocation = "/content";
+    private String defaultDropLocation = "/content";
+    private String optionSelectOnly = null;
+    private boolean optionSelectFirst = false;
 
     public FolderShortcutPlugin(IPluginContext context, IPluginConfig config) {
         super(context, config);
@@ -96,17 +87,23 @@ public class FolderShortcutPlugin extends AbstractWorkflowPlugin {
             @Override
             public void onClick(AjaxRequestTarget target) {
                 IDialogService dialogService = getDialogService();
-                //dialogService.show(FolderShortcutPlugin.this.new Dialog(dialogService));
+                dialogService.show(FolderShortcutPlugin.this.new Dialog(dialogService));
             }
         };
         add(link);
 
+        if(config.containsKey("option.first"))
+            optionSelectFirst = config.getBoolean("option.first");
+        if(config.containsKey("option.only"))
+            optionSelectOnly = config.getString("option.only");
+        
         String path = config.getString("gallery.path");
         if (path != null) {
             defaultDropLocation = path;
             try {
-                while (defaultDropLocation.startsWith("/"))
+                while (defaultDropLocation.startsWith("/")) {
                     defaultDropLocation = defaultDropLocation.substring(1);
+                }
                 Session session = ((UserSession)org.apache.wicket.Session.get()).getJcrSession();
                 setModel(new JcrNodeModel(session.getRootNode().getNode(defaultDropLocation)));
                 // HREPTWO-1218 getModel returns null, which causes problems for the WizardDialog
@@ -186,4 +183,210 @@ public class FolderShortcutPlugin extends AbstractWorkflowPlugin {
         }
     }
 
+    public class Dialog extends AbstractDialog {
+        @SuppressWarnings("unused")
+        private final static String SVN_ID = "$Id$";
+
+        private static final long serialVersionUID = 1L;
+
+        private String templateCategory = null;
+        private String prototype = null;
+        private String name;
+        private IServiceReference<IJcrService> jcrServiceRef;
+        private Map<String, Set<String>> templates;
+        final DropDownChoice folderChoice;
+        final DropDownChoice categoryChoice;
+
+        public Dialog(IDialogService dialogWindow) {
+            super(FolderShortcutPlugin.this.getPluginContext(), dialogWindow);
+            ok.setModel(new Model("Create"));
+            
+            IPluginContext context = FolderShortcutPlugin.this.getPluginContext();
+            IPluginConfig config = FolderShortcutPlugin.this.getPluginConfig();
+            
+            IJcrService service = context.getService(IJcrService.class.getName(), IJcrService.class);
+            jcrServiceRef = context.getReference(service);
+
+            String workflowCategory = config.getString("gallery.workflow");
+            Session jcrSession = ((UserSession)org.apache.wicket.Session.get()).getJcrSession();
+            Node folder = ((JcrNodeModel)FolderShortcutPlugin.this.getModel()).getNode();
+            try {
+                boolean isDefaultFolder = false;
+                WorkflowManager manager = ((HippoWorkspace)(jcrSession.getWorkspace())).getWorkflowManager();
+                Workflow workflow = manager.getWorkflow(workflowCategory, folder);
+                if (workflow instanceof FolderWorkflow) {
+                    FolderWorkflow folderWorkflow = (FolderWorkflow)workflow;
+                    templates = folderWorkflow.list();
+                } else {
+                    folder = jcrSession.getRootNode().getNode(defaultDropLocation.startsWith("/") ?
+                                                              defaultDropLocation.substring(1) : defaultDropLocation);
+                    workflow = manager.getWorkflow(workflowCategory, folder);
+                    if (workflow instanceof FolderWorkflow) {
+                        FolderWorkflow folderWorkflow = (FolderWorkflow)workflow;
+                        templates = folderWorkflow.list();
+                        isDefaultFolder = true;
+                    } else {
+                        folder = null;
+                    }
+                }
+                
+                if(optionSelectFirst) {
+                    if(optionSelectOnly != null) {
+                        Map<String, Set<String>> newTemplates = new TreeMap<String, Set<String>>();
+                        if(templates.containsKey(optionSelectOnly))
+                            newTemplates.put(optionSelectOnly, templates.get(optionSelectOnly));
+                    } else {
+                        Map<String, Set<String>> newTemplates = new TreeMap<String, Set<String>>();
+                        if(templates.size() > 0) {
+                            Map.Entry<String,Set<String>> firstEntry = templates.entrySet().iterator().next();
+                            newTemplates.put(firstEntry.getKey(), firstEntry.getValue());
+                        }
+                    }
+                } else if(optionSelectOnly != null&& isDefaultFolder) {
+                    Map<String, Set<String>> newTemplates = new TreeMap<String, Set<String>>();
+                    if(templates.containsKey(optionSelectOnly))
+                        newTemplates.put(optionSelectOnly, templates.get(optionSelectOnly));
+                }
+            } catch(MappingException ex) {
+                log.warn("failure to initialize shortcut", ex);
+                folder = null;
+            } catch(WorkflowException ex) {
+                log.warn("failure to initialize shortcut", ex);
+                folder = null;
+            } catch(RepositoryException ex) {
+                log.warn("failure to initialize shortcut", ex);
+                folder = null;
+            } catch(RemoteException ex) {
+                log.warn("failure to initialize shortcut", ex);
+                folder = null;
+            }
+
+            add(new TextFieldWidget("name", new PropertyModel(this, "name")));
+
+            add(new Label("message", new Model("")));
+
+            List emptyList = new LinkedList();
+            emptyList.add("");
+            
+            add(folderChoice = new DropDownChoice("prototype", new PropertyModel(this, "prototype"), emptyList) {
+                    protected boolean wantOnSelectionChangedNotifications() {
+                        return true;
+                    }
+                    protected void onSelectionChanged(Object newSelection) {
+                        super.onSelectionChanged(newSelection);
+                        evaluateChoices();
+                    }
+                });
+            folderChoice.setNullValid(false);
+            folderChoice.setRequired(true);
+
+            add(categoryChoice = new DropDownChoice("template", new PropertyModel(this, "templateCategory"), emptyList) {
+                    protected boolean wantOnSelectionChangedNotifications() {
+                        return true;
+                    }
+                    protected void onSelectionChanged(Object newSelection) {
+                        super.onSelectionChanged(newSelection);
+                        prototype = null;
+                        evaluateChoices();
+                    }
+                });
+            folderChoice.setNullValid(false);
+            folderChoice.setRequired(true);
+
+            ok.setEnabled(false);
+            if(folder != null) {
+                try {
+                    List<String> categories = new LinkedList<String>();
+                    categories.add(workflowCategory);
+                    setModel(new WorkflowsModel(new JcrNodeModel(folder), categories));
+                    ok.setEnabled(true);
+                } catch(RepositoryException ex) {
+                    setModel(null);
+                }
+            } else {
+                setModel(null);
+            }
+            evaluateChoices();
+        }
+
+        private void evaluateChoices() {
+            if(templates.keySet().size() == 1) {
+                categoryChoice.setChoices(new LinkedList(templates.keySet()));
+                categoryChoice.setVisible(false);
+                templateCategory = templates.keySet().iterator().next();
+            } else if(templates.keySet().size() > 1) {
+                categoryChoice.setChoices(new LinkedList(templates.keySet()));
+                categoryChoice.setVisible(true);
+            } else {
+                categoryChoice.setVisible(false);
+            }
+            if(templateCategory != null) {
+                final List<String> prototypesList = new LinkedList<String>(templates.get(templateCategory));
+                folderChoice.setChoices(prototypesList);
+                folderChoice.setChoiceRenderer(new NamespaceFriendlyChoiceRenderer(prototypesList));
+                if(templates.get(templateCategory).size() > 1) {
+                    folderChoice.setVisible(true);
+                    folderChoice.setNullValid(false);
+                    folderChoice.setRequired(true);
+                    ok.setEnabled(false);
+                } else if(templates.get(templateCategory).size() == 1) {
+                    prototype = templates.get(templateCategory).iterator().next();
+                    folderChoice.setVisible(false);
+                    ok.setEnabled(true);
+                } else {
+                    folderChoice.setVisible(false);
+                    ok.setEnabled(false);
+                }
+            } else {
+                folderChoice.setVisible(false);
+                ok.setEnabled(false);
+            }
+            if(prototype != null) {
+                ok.setEnabled(true);
+            }
+        }
+
+        @Override
+        public void onDetach() {
+            if (jcrServiceRef != null) {
+                jcrServiceRef.detach();
+            }
+            super.onDetach();
+        }
+
+        public String getTitle() {
+             return "New document";
+        }
+
+        protected void ok() throws Exception {
+            IModel model = getModel();
+            if(model != null && model instanceof WorkflowsModel) {
+                Session jcrSession = ((UserSession)org.apache.wicket.Session.get()).getJcrSession();
+                WorkflowManager manager = ((HippoWorkspace)(jcrSession.getWorkspace())).getWorkflowManager();
+                FolderWorkflow workflow = (FolderWorkflow) manager.getWorkflow(((WorkflowsModel)model).getWorkflowDescriptor());
+                if (prototype == null) {
+                    throw new WorkflowException("You need to select a type");
+                }
+                if (workflow != null) {
+                    if (!templates.get(templateCategory).contains(prototype)) {
+                        log.error("unknown folder type " + prototype);
+                        throw new WorkflowException("Unknown folder type " + prototype);
+                    }
+                    String path = workflow.add(templateCategory, prototype, name);
+                    JcrNodeModel nodeModel = new JcrNodeModel(new JcrItemModel(path));
+                    select(nodeModel);
+
+                    IJcrService jcrService = jcrServiceRef.getService();
+                    jcrService.flush((JcrNodeModel)FolderShortcutPlugin.this.getModel());
+
+                } else {
+                    log.error("no workflow defined on model for selected node");
+                }
+            }
+        }
+
+        protected void cancel() {
+        }
+    }
 }
+
