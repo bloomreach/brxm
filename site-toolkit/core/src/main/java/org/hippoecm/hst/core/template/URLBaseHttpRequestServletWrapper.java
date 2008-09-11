36b2daa3630aa165cf16ac76cd0aef5469a97599
Mike Meijnhard <m.meijnhard@onehippo.com>
2008-09-11 12:04:21 +0000

    HSTTWO-38 small fix due to svn merge error
    svn path=/experimental/hst2/trunk/; revision=13428

--- site-toolkit/core/src/main/java/org/hippoecm/hst/core/template/URLBaseHttpRequestServletWrapper.java
+++ site-toolkit/core/src/main/java/org/hippoecm/hst/core/template/URLBaseHttpRequestServletWrapper.java
@@ -1,5 +1,8 @@
 package org.hippoecm.hst.core.template;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletRequestWrapper;
 
@@ -10,21 +13,40 @@ import org.slf4j.LoggerFactory;
 public class URLBaseHttpRequestServletWrapper extends HttpServletRequestWrapper {
 	private static final Logger log = LoggerFactory.getLogger(URLBaseHttpRequestServletWrapper.class);
     private String urlPrefix;
+    private String strippedRequestURI;
 	
 	
 	public URLBaseHttpRequestServletWrapper(HttpServletRequest request, String urlPrefix) {
 		super(request);	
 		this.urlPrefix = urlPrefix;
+		
+		//strip contextpath from requestURI
+		strippedRequestURI = request.getRequestURI().replaceFirst(request.getContextPath(), "");
+		//strip urlprefix from uri
+		strippedRequestURI = strippedRequestURI.replaceFirst(urlPrefix, "");
+		
+		//urldecode resulting requestURI
+		String urlDecodedURI = null;
+		try {
+		    urlDecodedURI = URLDecoder.decode(stripUrlPrefix(strippedRequestURI),"utf-8");
+		    strippedRequestURI = urlDecodedURI;
+        } catch (UnsupportedEncodingException e) {
+            log.warn("Url is not utf-8 encoded");            
+        }
+		
 	}
 
 	@Override
 	public String getRequestURI() {
-		return UrlUtilities.decodeUrl(stripUrlPrefix(super.getRequestURI()));
+		log.debug("getRequestURI()=" + super.getRequestURI() + " returns " + strippedRequestURI);
+		return UrlUtilities.decodeUrl(strippedRequestURI);
+
 	}
  
 	@Override
 	public StringBuffer getRequestURL() {
-		return new StringBuffer(stripUrlPrefix(super.getRequestURL().toString()));
+		//return new StringBuffer(stripUrlPrefix(super.getRequestURL().toString()));
+		return super.getRequestURL();
 	}
 
 	@Override
