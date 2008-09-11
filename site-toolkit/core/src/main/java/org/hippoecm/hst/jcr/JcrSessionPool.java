274115be1ed14d9d9bffae66004d12bf0681413d
Ard Schrijvers <a.schrijvers@onehippo.com>
2008-09-11 09:30:53 +0000

    HSTTWO-40 initial checkin: not used, not tested. Work in progress
    svn path=/experimental/hst2/trunk/; revision=13418

---
+++ site-toolkit/core/src/main/java/org/hippoecm/hst/jcr/JcrSessionPool.java
@@ -0,0 +1,111 @@
+/*
+ *  Copyright 2008 Hippo.
+ * 
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.hippoecm.hst.jcr;
+
+import java.util.IdentityHashMap;
+import java.util.LinkedList;
+import java.util.NoSuchElementException;
+
+import javax.jcr.LoginException;
+import javax.jcr.RepositoryException;
+import javax.jcr.Session;
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+
+import org.hippoecm.hst.core.HSTConfiguration;
+import org.hippoecm.repository.HippoRepository;
+import org.hippoecm.repository.HippoRepositoryFactory;
+
+public class JcrSessionPool {
+
+    private static final JcrSessionPool jcrSessionPool = new JcrSessionPool();
+
+    private final LinkedList<ReadOnlyPooledSession> idleSessions = new LinkedList<ReadOnlyPooledSession>();
+    private final IdentityHashMap<HttpServletRequest, ReadOnlyPooledSession> activeSessions = new IdentityHashMap<HttpServletRequest, ReadOnlyPooledSession>();
+
+    public static Session getSession(HttpServletRequest request) {
+        ReadOnlyPooledSession session = null;
+
+        synchronized (jcrSessionPool.activeSessions) {
+            session = jcrSessionPool.activeSessions.get(request);
+            if (session != null) {
+                if (session.isLive()) {
+                    return session;
+                } else {
+                    jcrSessionPool.activeSessions.remove(request);
+                }
+            }
+        }
+
+        // because both idleSessions and activeSessions modifying/reading, sychronize on jcrSessionPool
+        synchronized (jcrSessionPool) {
+            while (!jcrSessionPool.idleSessions.isEmpty()) {
+                try {
+                    session = jcrSessionPool.idleSessions.removeFirst();
+                    if (session.isLive() && session.isValid(System.currentTimeMillis())) {
+                        jcrSessionPool.activeSessions.put(request, session);
+                        return session;
+                    } else {
+                        // try next in the idleSessions untill none left
+                        if (session.isLive()) {
+                            session.getDelegatee().logout();
+                        }
+                        session = null;
+                    }
+                } catch (NoSuchElementException e) {
+                    // no session idle, create a new one
+                }
+            }
+        }
+
+        // No valid jcrsession we have so far: create a new one.
+        ServletContext sc = request.getSession().getServletContext();
+        String location = HSTConfiguration.get(sc, HSTConfiguration.KEY_REPOSITORY_ADRESS);
+        String username = HSTConfiguration.get(sc, HSTConfiguration.KEY_REPOSITORY_USERNAME);
+        String password = HSTConfiguration.get(sc, HSTConfiguration.KEY_REPOSITORY_PASSWORD);
+
+        HippoRepositoryFactory.setDefaultRepository(location);
+        Session jcrSession = null;
+        try {
+            HippoRepository repository = HippoRepositoryFactory.getHippoRepository();
+            jcrSession = repository.login(username, (password != null ? password.toCharArray() : null));
+        } catch (LoginException e) {
+            throw new JCRConnectionException("Cannot login with credentials");
+        } catch (RepositoryException e) {
+            e.printStackTrace();
+            throw new JCRConnectionException("Failed to initialize repository");
+        }
+        session = new ReadOnlyPooledSession(jcrSession, jcrSessionPool);
+        synchronized (jcrSessionPool.activeSessions) {
+            jcrSessionPool.activeSessions.put(request, session);
+        }
+        return session;
+    }
+
+    public void release(HttpServletRequest request) {
+        synchronized (jcrSessionPool.activeSessions) {
+            ReadOnlyPooledSession finishedSession = jcrSessionPool.activeSessions.remove(request);
+            if (finishedSession != null && finishedSession.isLive()) {
+                if (finishedSession.isValid(System.currentTimeMillis())) {
+                    idleSessions.add(finishedSession);
+                } else {
+                    finishedSession.getDelegatee().logout();
+                }
+            }
+        }
+    }
+
+}
---
+++ site-toolkit/core/src/main/java/org/hippoecm/hst/jcr/ReadOnlyPooledSession.java
@@ -0,0 +1,213 @@
+/*
+ *  Copyright 2008 Hippo.
+ * 
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.hippoecm.hst.jcr;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.security.AccessControlException;
+
+import javax.jcr.AccessDeniedException;
+import javax.jcr.Credentials;
+import javax.jcr.InvalidItemStateException;
+import javax.jcr.InvalidSerializedDataException;
+import javax.jcr.Item;
+import javax.jcr.ItemExistsException;
+import javax.jcr.ItemNotFoundException;
+import javax.jcr.LoginException;
+import javax.jcr.NamespaceException;
+import javax.jcr.Node;
+import javax.jcr.PathNotFoundException;
+import javax.jcr.Repository;
+import javax.jcr.RepositoryException;
+import javax.jcr.Session;
+import javax.jcr.UnsupportedRepositoryOperationException;
+import javax.jcr.ValueFactory;
+import javax.jcr.Workspace;
+import javax.jcr.lock.LockException;
+import javax.jcr.nodetype.ConstraintViolationException;
+import javax.jcr.nodetype.NoSuchNodeTypeException;
+import javax.jcr.version.VersionException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.xml.sax.ContentHandler;
+import org.xml.sax.SAXException;
+
+public class ReadOnlyPooledSession implements Session{
+
+    private static final Logger log = LoggerFactory.getLogger(ReadOnlyPooledSession.class);
+    
+    private Session delegatee;
+    private long creationTime;
+    //private JcrSessionPool jcrSessionPool;
+    
+    public ReadOnlyPooledSession(Session session, JcrSessionPool  jcrSessionPool) {
+        this.delegatee = session;
+        creationTime = System.currentTimeMillis();
+        //this.jcrSessionPool = jcrSessionPool;
+    }
+    
+    public boolean isValid(long currentTime){
+        return (currentTime - creationTime < 60*1000);
+    }
+    
+    public Session getDelegatee(){
+        return delegatee;
+    }
+    //public JcrSessionPool getJcrSessionPool(){
+    //    return this.jcrSessionPool;
+    //}
+     public void logout() {
+        //jcrSessionPool.release(this);
+         log.warn("logout unsupported in read only session");
+     }
+    
+     public void addLockToken(String lt) {
+         log.warn("addLockToken unsupported in read only session");
+     }
+     public void refresh(boolean keepChanges) throws RepositoryException {
+         log.warn("refresh is not allowed to in ReadOnlyPooledSession");
+         throw new UnsupportedRepositoryOperationException("refresh is not allowed to in ReadOnlyPooledSession");
+     }
+
+     public void move(String srcAbsPath, String destAbsPath) throws ItemExistsException, PathNotFoundException,
+             VersionException, ConstraintViolationException, LockException, RepositoryException {
+         log.warn("move is not allowed to in ReadOnlyPooledSession");
+         throw new UnsupportedRepositoryOperationException("move is not allowed to in ReadOnlyPooledSession");
+     }
+
+     public void save() throws AccessDeniedException, ItemExistsException, ConstraintViolationException,
+             InvalidItemStateException, VersionException, LockException, NoSuchNodeTypeException, RepositoryException {
+         log.warn("save is not allowed to in ReadOnlyPooledSession");
+         throw new UnsupportedRepositoryOperationException("save is not allowed to in ReadOnlyPooledSession");
+     }
+
+     public void removeLockToken(String lt) {
+         log.warn("removeLockToken is not allowed to in ReadOnlyPooledSession");
+     }
+
+     public void setNamespacePrefix(String prefix, String uri) throws NamespaceException, RepositoryException {
+         log.warn("setNamespacePrefix is not allowed to in ReadOnlyPooledSession");
+         throw new UnsupportedRepositoryOperationException("setNamespacePrefix is not allowed to in ReadOnlyPooledSession");
+     }
+     
+    public void checkPermission(String absPath, String actions) throws AccessControlException, RepositoryException {
+        delegatee.checkPermission(absPath, actions);
+    }
+
+    public void exportDocumentView(String absPath, ContentHandler contentHandler, boolean skipBinary, boolean noRecurse)
+            throws PathNotFoundException, SAXException, RepositoryException {
+        delegatee.exportDocumentView(absPath, contentHandler, skipBinary, noRecurse);
+    }
+
+    public void exportDocumentView(String absPath, OutputStream out, boolean skipBinary, boolean noRecurse)
+            throws IOException, PathNotFoundException, RepositoryException {
+        delegatee.exportDocumentView(absPath, out, skipBinary, noRecurse);
+    }
+
+    public void exportSystemView(String absPath, ContentHandler contentHandler, boolean skipBinary, boolean noRecurse)
+            throws PathNotFoundException, SAXException, RepositoryException {
+        delegatee.exportSystemView(absPath, contentHandler, skipBinary, noRecurse);
+    }
+
+    public void exportSystemView(String absPath, OutputStream out, boolean skipBinary, boolean noRecurse)
+            throws IOException, PathNotFoundException, RepositoryException {
+        delegatee.exportSystemView(absPath, out, skipBinary, noRecurse);
+    }
+
+    public Object getAttribute(String name) {
+       return delegatee.getAttribute(name);
+    }
+
+    public String[] getAttributeNames() {
+        return delegatee.getAttributeNames();
+    }
+
+    public ContentHandler getImportContentHandler(String parentAbsPath, int uuidBehavior) throws PathNotFoundException,
+            ConstraintViolationException, VersionException, LockException, RepositoryException {    
+        return delegatee.getImportContentHandler(parentAbsPath, uuidBehavior);
+    }
+
+    public Item getItem(String absPath) throws PathNotFoundException, RepositoryException {
+        return delegatee.getItem(absPath);
+    }
+
+    public String[] getLockTokens() {
+        return delegatee.getLockTokens();
+    }
+
+    public String getNamespacePrefix(String uri) throws NamespaceException, RepositoryException {
+        return delegatee.getNamespacePrefix(uri);
+    }
+
+    public String[] getNamespacePrefixes() throws RepositoryException {
+        return delegatee.getNamespacePrefixes();
+    }
+
+    public String getNamespaceURI(String prefix) throws NamespaceException, RepositoryException {
+        return delegatee.getNamespaceURI(prefix);
+    }
+
+    public Node getNodeByUUID(String uuid) throws ItemNotFoundException, RepositoryException {
+        return delegatee.getNodeByUUID(uuid);
+    }
+
+    public Repository getRepository() {
+        return delegatee.getRepository();
+    }
+
+    public Node getRootNode() throws RepositoryException {
+        return delegatee.getRootNode();
+    }
+
+    public String getUserID() {
+        return delegatee.getUserID();
+    }
+
+    public ValueFactory getValueFactory() throws UnsupportedRepositoryOperationException, RepositoryException {
+        return delegatee.getValueFactory();
+    }
+
+    public Workspace getWorkspace() {
+        return delegatee.getWorkspace();
+    }
+
+    public boolean hasPendingChanges() throws RepositoryException {
+        return delegatee.hasPendingChanges();
+    }
+
+    public Session impersonate(Credentials credentials) throws LoginException, RepositoryException {
+        return delegatee.impersonate(credentials);
+    }
+
+    public void importXML(String parentAbsPath, InputStream in, int uuidBehavior) throws IOException,
+            PathNotFoundException, ItemExistsException, ConstraintViolationException, VersionException,
+            InvalidSerializedDataException, LockException, RepositoryException {
+        
+        delegatee.importXML(parentAbsPath, in, uuidBehavior);
+    }
+
+    public boolean isLive() {
+        return delegatee.isLive();
+    }
+
+    public boolean itemExists(String absPath) throws RepositoryException {
+        return delegatee.itemExists(absPath);
+    }
+
+
+}
