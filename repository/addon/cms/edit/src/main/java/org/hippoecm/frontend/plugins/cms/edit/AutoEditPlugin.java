fab4521214465c1247fb5d92e9056375a7eb247e
Frank van Lankvelt <f.vanlankvelt@onehippo.com>
2008-09-15 11:35:20 +0000

    HREPTWO-1488: maximum of four editors
    The AutoEditPlugin opens the existing drafts.  In the deque, the multieditor is filtered by the preview.
    
    svn path=/hippo-ecm/trunk/; revision=13472

---
+++ repository/addon/cms/edit/src/main/java/org/hippoecm/frontend/plugins/cms/edit/AutoEditPlugin.java
@@ -0,0 +1,69 @@
+/*
+ *  Copyright 2008 Hippo.
+ * 
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.hippoecm.frontend.plugins.cms.edit;
+
+import javax.jcr.Node;
+import javax.jcr.NodeIterator;
+import javax.jcr.RepositoryException;
+import javax.jcr.query.Query;
+import javax.jcr.query.QueryManager;
+
+import org.apache.wicket.Session;
+import org.hippoecm.frontend.model.JcrNodeModel;
+import org.hippoecm.frontend.plugin.IPlugin;
+import org.hippoecm.frontend.plugin.IPluginContext;
+import org.hippoecm.frontend.plugin.config.IPluginConfig;
+import org.hippoecm.frontend.service.IEditService;
+import org.hippoecm.frontend.session.UserSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AutoEditPlugin implements IPlugin {
+    @SuppressWarnings("unused")
+    private final static String SVN_ID = "$Id$";
+
+    private static final long serialVersionUID = 1L;
+
+    public static final Logger log = LoggerFactory.getLogger(AutoEditPlugin.class);
+
+    public AutoEditPlugin(IPluginContext context, IPluginConfig config) {
+        if (config.getString("editor.id") != null) {
+            IEditService editService = context.getService(config.getString("editor.id"), IEditService.class);
+            if (editService != null) {
+                try {
+                    String user = ((UserSession) Session.get()).getCredentials().getString("username");
+                    QueryManager qMgr = ((UserSession) Session.get()).getJcrSession().getWorkspace().getQueryManager();
+                    Query query = qMgr.createQuery("select * from hippostd:publishable where hippostd:state='draft' "
+                            + "and hippostd:holder='" + user + "'", Query.SQL);
+                    NodeIterator iter = query.execute().getNodes();
+                    while (iter.hasNext()) {
+                        Node node = iter.nextNode();
+                        if (!node.getName().equals("hippo:prototype")) {
+                            editService.edit(new JcrNodeModel(node));
+                        }
+                    }
+                } catch (RepositoryException ex) {
+                    log.error(ex.getMessage());
+                }
+            } else {
+                log.warn("No edit service found");
+            }
+        } else {
+            log.warn("No edit service configured (editor.id)");
+        }
+    }
+
+}
