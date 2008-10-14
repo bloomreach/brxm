081e9321228b7233b8297eff5fa8bac3bd42526d
Jeroen Reijn <j.reijn@onehippo.com>
2008-10-14 16:00:27 +0000

    HSTTWO-86: Add module for listing all documents of a specific type
    svn path=/experimental/hst2/trunk/; revision=13971

---
+++ site-toolkit/components/src/main/java/org/hippoecm/hst/components/modules/list/DocumentsOfTypeModule.java
@@ -0,0 +1,81 @@
+package org.hippoecm.hst.components.modules.list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.jcr.Node;
+import javax.jcr.NodeIterator;
+import javax.jcr.RepositoryException;
+import javax.jcr.Session;
+import javax.jcr.query.Query;
+import javax.jcr.query.QueryManager;
+import javax.jcr.query.QueryResult;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.jsp.PageContext;
+
+import org.hippoecm.hst.core.HSTHttpAttributes;
+import org.hippoecm.hst.core.mapping.URLMapping;
+import org.hippoecm.hst.core.template.ContextBase;
+import org.hippoecm.hst.core.template.HstFilterBase;
+import org.hippoecm.hst.core.template.TemplateException;
+import org.hippoecm.hst.core.template.module.ModuleBase;
+import org.hippoecm.hst.core.template.node.el.ContentELNodeImpl;
+import org.hippoecm.hst.core.template.node.el.ELNode;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DocumentsOfTypeModule extends ModuleBase {
+	
+	private static final Logger log = LoggerFactory.getLogger(DocumentsOfTypeModule.class);
+	public static final String DOCUMENT_TYPE = "documentType";
+	private String docType="";
+	
+	public void render(PageContext pageContext) throws TemplateException{
+        HttpServletRequest request = (HttpServletRequest) pageContext.getRequest();
+        ContextBase ctxBase = (ContextBase) request.getAttribute(HstFilterBase.CONTENT_CONTEXT_REQUEST_ATTRIBUTE);
+        URLMapping urlMapping = (URLMapping)request.getAttribute(HSTHttpAttributes.URL_MAPPING_ATTR);
+        List<ELNode> wrappedNodes = new ArrayList<ELNode>();
+        
+        boolean params = false;
+        if (moduleParameters != null) {
+            params = true;
+        }
+        if (params && moduleParameters.containsKey(DOCUMENT_TYPE)) {
+            String type = moduleParameters.get(DOCUMENT_TYPE);
+            if (!"".equals(type)) {
+                setDocumentType(type);
+            }
+        }
+        
+        Session jcrSession = ctxBase.getSession();
+        wrappedNodes = getDocuments(jcrSession,urlMapping);
+        pageContext.setAttribute(getVar(), wrappedNodes);
+	
+	}
+        
+    private void setDocumentType(String type){
+      this.docType=type;	
+    }
+	
+	private List<ELNode> getDocuments(Session jcrSession, URLMapping urlMapping){
+		List<ELNode> wrappedNodes = new ArrayList<ELNode>();
+		if(docType!=null || !docType.equals("")) {
+	        try {
+	            QueryManager qMgr = jcrSession.getWorkspace().getQueryManager();
+	            QueryResult result = qMgr.createQuery("select * from " + docType + " where hippostd:state<>'draft'",
+	                    Query.SQL).execute();
+	            NodeIterator iter = result.getNodes();
+	            while (iter.hasNext()) {
+	                Node node = iter.nextNode();
+	                if (node != null && !node.getName().equals("hippo:prototype")) {
+	                	wrappedNodes.add(new ContentELNodeImpl(node,urlMapping));
+	                }
+	            }
+	            
+	        } catch (RepositoryException ex) {
+	           ex.printStackTrace();
+	        }
+		}
+        return wrappedNodes;
+	}
+}
