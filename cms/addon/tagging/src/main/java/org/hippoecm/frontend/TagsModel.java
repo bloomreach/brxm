a38ad1e2ae1d563e650925c3198b1096fcfd32b3
Jeroen Tietema <j.tietema@onehippo.com>
2008-10-15 13:58:43 +0000

    HREPTWO-1740 taginput frontend HREPTWO-1741 tagsuggestion frontend
    TODO
    - move classes to more specific package
    - implement backend
    - add to default content and war packages
    
    
    svn path=/hippo-ecm/trunk/; revision=14004

---
+++ cms/addon/tagging/src/main/java/org/hippoecm/frontend/TagsModel.java
@@ -0,0 +1,113 @@
+package org.hippoecm.frontend;
+
+import java.util.LinkedHashSet;
+
+import javax.jcr.RepositoryException;
+import javax.jcr.Value;
+import javax.jcr.ValueFormatException;
+import javax.jcr.lock.LockException;
+import javax.jcr.nodetype.ConstraintViolationException;
+import javax.jcr.version.VersionException;
+
+import org.apache.wicket.model.Model;
+import org.hippoecm.frontend.model.properties.JcrPropertyModel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TagsModel extends Model {
+    private static final long serialVersionUID = 1L;
+
+    static final Logger log = LoggerFactory.getLogger(TagsModel.class);
+    
+    private JcrPropertyModel propertyModel;
+    private LinkedHashSet<String> tags;
+
+    static final String TAG_SEPERATOR = ",";
+
+    public TagsModel(JcrPropertyModel propertyModel) {
+        this.propertyModel = propertyModel;
+        tags = parsePropertyModel(this.propertyModel);
+        super.setObject(buildModelObject(tags));
+    }
+    
+    private String buildModelObject(LinkedHashSet<String> set){
+        StringBuffer buffer = new StringBuffer();
+        for (String tag : set){
+            buffer.append(tag);
+            buffer.append(TAG_SEPERATOR);
+            buffer.append(" ");
+        }
+        return buffer.toString();
+    }
+    
+    private String[] buildPropertyModel(String tags) {
+        if (tags == null || tags.equals("")) {
+            return new String[] {};
+        } else {
+            String[] tagsArray = tags.split(TAG_SEPERATOR);
+            LinkedHashSet<String> set = new LinkedHashSet<String>();
+            for (int i = 0; i < tagsArray.length; i++){
+                set.add(tagsArray[i].trim());
+            }
+            return set.toArray(new String[0]);
+        }
+    }
+    
+    private LinkedHashSet<String> parsePropertyModel(JcrPropertyModel model){
+        LinkedHashSet<String> set = new LinkedHashSet<String>();
+        // the property does not exist so don't even try
+        if (model.getProperty() == null){
+            return set;
+        }
+        try {
+            for (Value val : model.getProperty().getValues()){
+                String tag = val.getString();
+                set.add(tag.trim());
+            }
+        } catch (ValueFormatException e) {
+            log.error("Unsupported value in the JcrPropertyModel.", e);
+        } catch (RepositoryException e) {
+            log.error("Repository error", e);
+        }
+        return set;
+    }
+    
+    public void addTag(String tag){
+        if (!tag.trim().equals("")){
+            log.debug("Adding tag: " + tag);
+            tags.add(tag.trim());
+            saveProperty(tags.toArray(new String[0]));
+            super.setObject(buildModelObject(tags));
+        }
+    }
+    
+    private void saveProperty(String[] tags){
+        try {
+            propertyModel.getProperty().setValue(tags);
+        } catch (ValueFormatException e) {
+            log.error("Failed trying to save insane tag.", e);
+        } catch (VersionException e) {
+            log.error("Versioning error.", e);
+        } catch (LockException e) {
+            log.error("Locking error.", e);
+        } catch (ConstraintViolationException e) {
+            log.error("Constraint violation", e);
+        } catch (RepositoryException e) {
+            log.error("Repository error.", e);
+        }
+    }
+
+    /**
+     * Sets the string object with comma separated tags but also updates
+     * the hippostd:tags property of the document with an updated String array
+     */
+    public void setObject(final Object object) {
+        log.debug("setObject");
+        super.setObject(object);
+
+        saveProperty(buildPropertyModel((String) object));
+        tags = parsePropertyModel(propertyModel);
+    }
+
+    
+}
---
+++ cms/addon/tagging/src/main/java/org/hippoecm/frontend/TagsPlugin.java
@@ -0,0 +1,114 @@
+package org.hippoecm.frontend;
+
+import javax.jcr.PathNotFoundException;
+import javax.jcr.RepositoryException;
+
+import org.apache.wicket.markup.html.basic.Label;
+import org.hippoecm.frontend.editor.ITemplateEngine;
+import org.hippoecm.frontend.model.IJcrNodeModelListener;
+import org.hippoecm.frontend.model.JcrNodeModel;
+import org.hippoecm.frontend.model.properties.JcrPropertyModel;
+import org.hippoecm.frontend.plugin.IPluginContext;
+import org.hippoecm.frontend.plugin.config.IPluginConfig;
+import org.hippoecm.frontend.plugins.standardworkflow.types.IFieldDescriptor;
+import org.hippoecm.frontend.plugins.standardworkflow.types.ITypeDescriptor;
+import org.hippoecm.frontend.service.IJcrService;
+import org.hippoecm.frontend.service.render.RenderPlugin;
+import org.hippoecm.frontend.widgets.TextAreaWidget;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Displays the current assigned tags of the document 
+ * @author jeroen
+ *
+ */
+public class TagsPlugin extends RenderPlugin implements IJcrNodeModelListener {
+    @SuppressWarnings("unused")
+    private final static String SVN_ID = "$Id$";
+
+    private static final long serialVersionUID = 1L;
+
+    static final Logger log = LoggerFactory.getLogger(TagsPlugin.class);
+
+    public static String FIELD_NAME = "hippostd:tags";
+
+    private ITemplateEngine engine;
+    JcrNodeModel nodeModel;
+    private String cols = "20";
+    private String rows = "3";
+
+    protected IFieldDescriptor field;
+
+    public TagsPlugin(IPluginContext context, IPluginConfig config) {
+        super(context, config);
+
+        context.registerService(this, IJcrService.class.getName());
+
+        engine = getPluginContext().getService(getPluginConfig().getString(ITemplateEngine.ENGINE),
+                ITemplateEngine.class);
+
+        nodeModel = (JcrNodeModel) getModel();
+        
+        try {
+            nodeModel.getNode().getProperty(FIELD_NAME);
+        } catch (PathNotFoundException e) {
+            log.warn(FIELD_NAME + " does not exist, attempting to create it.");
+            try{
+                nodeModel.getNode().setProperty(TagsPlugin.FIELD_NAME, new String[0]);
+            }catch (RepositoryException re){
+                log.error("Creation of " + FIELD_NAME + " failed.", re);
+            }
+        } catch (RepositoryException e) {
+            log.error("Repository error", e);
+        }
+        
+        String mode = config.getString("mode");
+        TagsModel tagModel = createTagModel(nodeModel);
+        if (tagModel == null) {
+            add(new Label("value", "TagModel not found"));
+            log.error("TagModel can not be null.");
+        } else if (ITemplateEngine.EDIT_MODE.equals(mode)) {
+            if (config.getString("rows") != null) {
+                rows = config.getString("rows");
+            }
+            if (config.getString("cols") != null) {
+                cols = config.getString("cols");
+            }
+            add(createTextArea(tagModel));
+        } else {
+            add(new Label("value", tagModel));
+        }
+    }
+
+    public void onFlush(JcrNodeModel nodeModel) {
+        log.debug("Received flush");
+        if (nodeModel.equals(this.nodeModel)) {
+            TagsModel tagModel = createTagModel(nodeModel);
+            replace(createTextArea(tagModel));
+            redraw();
+        }
+    }
+
+    public TextAreaWidget createTextArea(TagsModel tagModel) {
+        TextAreaWidget widget = new TextAreaWidget("value", tagModel);
+        widget.setRows(rows);
+        widget.setCols(cols);
+        return widget;
+    }
+
+    private TagsModel createTagModel(JcrNodeModel nodeModel) {
+        ITypeDescriptor type = engine.getType(nodeModel);
+        if (type == null) {
+            log.error("Type not found.");
+            return null;
+        }
+        JcrPropertyModel propertyModel = new JcrPropertyModel(nodeModel.getItemModel().getPath() + "/" + FIELD_NAME);
+        TagsModel tagModel = new TagsModel(propertyModel);
+        log.debug((String) tagModel.getObject());
+        return tagModel;
+    }
+
+    
+
+}
