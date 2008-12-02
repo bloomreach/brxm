9a7f244400d7af8428c55ce07d12ce493ff26c89
Ard Schrijvers <a.schrijvers@onehippo.com>
2008-12-02 13:07:42 +0000

    HREPTWO-1914 remove unused fieldname
    svn path=/hippo-ecm/trunk/; revision=15003

--- cms/repository/engine/src/main/java/org/hippoecm/repository/query/lucene/ServicingFieldNames.java
+++ cms/repository/engine/src/main/java/org/hippoecm/repository/query/lucene/ServicingFieldNames.java
@@ -58,10 +58,4 @@ public class ServicingFieldNames {
      */
     public static final String FACET_PROPERTIES_SET = "_:FACET_PROPERTIES_SET".intern();
     
-    /**
-     * Name of the field that contains the UUIDs of the hippo aggregated nodes. The
-     * terms are not tokenized and not stored, only indexed.
-     */
-    public static final String HIPPO_AGGREGATED_NODE_UUID = "_:HIPPO_AGGR_NODE_UUID".intern();
-
 }
