33b2331e3f45d8f413c1b4285c1927498a51cd40
Frank van Lankvelt <f.vanlankvelt@onehippo.com>
2009-01-09 10:56:19 +0000

    HREPTWO-2133: let Wicket cache translations
    removed code shows how to disable the wicket cache.
    
    
    svn path=/hippo-ecm/trunk/; revision=15847

--- cms/frontend/engine/src/main/java/org/hippoecm/frontend/Main.java
+++ cms/frontend/engine/src/main/java/org/hippoecm/frontend/Main.java
@@ -138,25 +138,6 @@ public class Main extends WebApplication {
             }
         });
 
-        // delegate i18n caching to providers
-        resourceSettings.setLocalizer(new Localizer() {
-            @Override
-            protected String getCacheKey(String key, Component component) {
-                IStringResourceProvider provider;
-                if (component instanceof IStringResourceProvider) {
-                    provider = (IStringResourceProvider) component;
-                } else {
-                    provider = (IStringResourceProvider) component
-                        .findParent(IStringResourceProvider.class);
-                }
-                if (provider != null) {
-                    return null;
-                } else {
-                    return super.getCacheKey(key, component);
-                }
-            }
-        });
-
         // replace current loaders with own list, starting with component-specific
         List<IStringResourceLoader> loaders = new ArrayList<IStringResourceLoader>(resourceSettings
                 .getStringResourceLoaders());
