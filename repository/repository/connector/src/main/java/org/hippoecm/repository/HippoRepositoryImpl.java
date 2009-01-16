3e0d2e589ab28ee1cf52bdc46356b060b49a1112
Berry van Halderen <b.vanhalderen@onehippo.com>
2009-01-16 13:38:28 +0000

    HREPTWO-16: remove unused import
    svn path=/hippo-ecm/trunk/; revision=15982

--- repository/repository/connector/src/main/java/org/hippoecm/repository/HippoRepositoryImpl.java
+++ repository/repository/connector/src/main/java/org/hippoecm/repository/HippoRepositoryImpl.java
@@ -34,7 +34,6 @@ import javax.jcr.SimpleCredentials;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import org.hippoecm.repository.UserTransactionImpl;
 import org.hippoecm.repository.api.RepositoryMap;
 import org.hippoecm.repository.api.ValueMap;
 
