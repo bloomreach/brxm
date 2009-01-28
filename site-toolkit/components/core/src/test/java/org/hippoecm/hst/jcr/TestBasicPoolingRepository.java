a9d56f936889c0e230d3cb9f5f6c8a64be0774a5
Woonsan Ko <w.ko@onehippo.com>
2009-01-28 10:44:48 +0000

    [HSTTWO-295] completed basic unit test for the session pool.
    svn path=/ecm/site-toolkit/branches/Branch-HSTTWO-295/; revision=16143

--- site-toolkit/components/core/src/test/java/org/hippoecm/hst/jcr/TestBasicPoolingRepository.java
+++ site-toolkit/components/core/src/test/java/org/hippoecm/hst/jcr/TestBasicPoolingRepository.java
@@ -1,7 +1,11 @@
 package org.hippoecm.hst.jcr;
 
+import javax.jcr.Credentials;
+import javax.jcr.Node;
 import javax.jcr.Repository;
 import javax.jcr.Session;
+import javax.jcr.SimpleCredentials;
+import javax.jcr.UnsupportedRepositoryOperationException;
 
 import org.hippoecm.hst.test.AbstractSpringTestCase;
 
@@ -20,6 +24,9 @@ public class TestBasicPoolingRepository extends AbstractSpringTestCase
             sessions[i] = repository.login();
         }
         
+        assertEquals("Active session count is not the same as the maximum available session count.", 
+                     poolingRepository.getMaxActive(), poolingRepository.getNumActive());
+        
         try
         {
             Session oneMore = repository.login();
@@ -28,11 +35,63 @@ public class TestBasicPoolingRepository extends AbstractSpringTestCase
         catch (Exception e)
         {
         }
-
+        
         for (int i = 0; i < poolingRepository.getMaxActive(); i++)
         {
             sessions[i].logout();
         }
+        
+        assertEquals("Active session count is not zero.", 0, poolingRepository.getNumActive());
+        
+        Session session = null;
+        
+        try
+        {
+            session = poolingRepository.login();
+            Node node = session.getRootNode();
+            assertNotNull("The root node is null.", node);
+            
+            try
+            {
+                session.save();
+                fail("The session from the pool should be read-only session by default login().");
+            }
+            catch (UnsupportedRepositoryOperationException uroe)
+            {
+                // good enough.
+            }
+        }
+        finally
+        {
+            if (session != null)
+                session.logout();
+        }
+        
+        session = null;
+        
+        try
+        {
+            // get a writable session by using credentials.
+            Credentials credentials = new SimpleCredentials("admin", "admin".toCharArray());
+
+            int previousActiveCount = poolingRepository.getNumActive();
+            
+            // Check if the session pool is not affected.
+            session = poolingRepository.login(credentials);
+            assertEquals("Active session count changed by creating a writable session.", 
+                         previousActiveCount, poolingRepository.getNumActive()); 
+            
+            session.save();
+        }
+        catch (UnsupportedRepositoryOperationException uroe)
+        {
+            fail("The session is not writable one: " + session);
+        }
+        finally
+        {
+            if (session != null)
+                session.logout();
+        }        
     }
     
 }
