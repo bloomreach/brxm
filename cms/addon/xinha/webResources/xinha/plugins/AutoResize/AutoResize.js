0336585736d35737bf99992ef652a64f3b8705e9
Arthur Bogaart <a.bogaart@onehippo.com>
2009-01-29 11:52:00 +0000

    HREPTWO-1747: Xinha's autoresizer plugin doesn't fit the textarea. It's takes about half  the field space. - Implemented new Autoresize plugin. - Width is taken from parent element (non-IE browsers) or calculated by difference in viewport dimension. - Height is based on the viewport height in relation to the minimal height.
    svn path=/hippo-ecm/trunk/; revision=16162

--- cms/addon/xinha/webResources/xinha/plugins/AutoResize/AutoResize.js
+++ cms/addon/xinha/webResources/xinha/plugins/AutoResize/AutoResize.js
@@ -9,130 +9,118 @@ AutoResize._pluginInfo = {
     license :""
 }
 
+Xinha.Config.prototype.AutoResize =
+{
+    'minHeight' : 150,
+    'minWidth'  : 150
+}
+
 function AutoResize(editor) {
     this.editor = editor;
-    this.init   = {
-            initialized: false, 
-            scroll: false,
-            minHeight: 200,
-            marginWidth: 0, 
-            scrollbarWidth: this.getScrollbarWidth(), 
-            isTemplateEditor: this.isTemplateEditor(this.editor._textArea),
-            nrOfXinhas: this.nrOfXinhas()
-    };
-    this.dim    = {width: 0, height: this.init.minHeight};
-    this.preDim = {width:0, height:0};
 
+    this.initialized = false;
+    this.timeout = null;
+
+    this.DOM = YAHOO.util.Dom;
+    
+    this.parentElement = this.findParent();
+    this.dim = {
+            w: -1,
+            h: -1,
+            viewWidth: 0,
+            viewHeight: this.DOM.getViewportHeight()
+    }
+    
     YAHOO.hippo.LayoutManager.registerResizeListener(this.editor._textArea,
             this, function(type, args, me) {
-    	var unit = args[0];
-    	
-        if(unit.body.w) me.dim.width  = parseInt(unit.body.w);
-        if(unit.body.h) me.dim.height = parseInt(unit.body.h);
-        
-        if(me.init.nrOfXinhas > 1) unit.scroll = true;
-        if(!me.init.initialized) {
-            me.init.scroll = unit.scroll;
-            me.init.marginWidth = me.dim.width - parseInt(me.editor._initial_ta_size.w.substr(0, me.editor._initial_ta_size.w.length-2));
-            me.init.initialized = true;
-        } 
-        
-        if (unit.scroll && !me.init.scroll) me.dim.width -= me.init.scrollbarWidth;
-        else if(!unit.scroll && me.init.scroll) me.dim.width += me.init.scrollbarWidth;
-        else {
-            //Temp workaround. We can't yet correctly predict wether we'll be seeing scrollbars or not
-            //so by default we pretend their visible.
-            me.dim.width -= me.init.scrollbarWidth;
-        }
-        
-        me.dim.width = me.dim.width - me.init.marginWidth - 14;
-        
-        if((me.init.nrOfXinhas * me.init.minHeight) > me.dim.height) {
-            me.dim.height = me.init.minHeight;
-        } else {
-            var a = YAHOO.util.Dom.getViewportHeight()/1024;
-            var rat = 1 - (a <= 1 ? a : 0.1);
-            var exp = me.init.nrOfXinhas*me.init.nrOfXinhas;
-            var min = 1 - exp * rat;
-            var extra = min > 0 ? me.init.minHeight* min : 0;
-            var h = parseInt(me.init.minHeight + extra);
-            
-            me.dim.height = h < me.init.minHeight ? me.init.minHeight : h; 
-        }
-        if(me.preDim.width != me.dim.width || me.preDim.height != me.dim.height) {
-            me.doAutoResize(me.editor, me.dim.width, me.dim.height);
-            me.preDim.width = me.dim.width;
-            me.preDim.height = me.dim.height;
-        }
+        me.checkResize(editor);
     });
 }
 
-AutoResize.prototype.doAutoResize = function(editor, w, h) {
-    if (editor._iframe == null) {
+AutoResize.prototype.getDim = function(vWidth, vHeight) {
+    var x,y;
+
+    var minHeight = this.editor.config.AutoResize.minHeight;
+    var minWidth = this.editor.config.AutoResize.minWidth;
+    
+    if(Xinha.is_ie) {
+        if(!this.initialized) {
+            x = this.parentElement.offsetWidth;
+            var xl = parseInt(this.DOM.getStyle(this.parentElement, 'padding-left'));
+            var xr = parseInt(this.DOM.getStyle(this.parentElement, 'padding-right'));
+            x -= (xl + xr);
+            this.initialized = true;
+        } else {
+            var diff = vWidth - this.dim.viewWidth;
+            x = this.dim.w + diff;
+        }
+    } else {
+        x = parseInt(this.DOM.getStyle(this.parentElement, 'width'));    
+    }
+    
+    if(x < minWidth) {
+        x = minWidth;
+    }
+    
+    var p = vHeight / minHeight;
+    var yy = 0;
+    if(p >= 2.2) {
+        yy = (minHeight/20)*p;
+    }
+
+    y = minHeight;
+    if(this.dim.viewHeight - vHeight > 0) {
+        if(y - yy > minHeight) {
+            y -= yy;
+        }
+    } else {
+        y += yy;
+    }
+    
+    return {
+        w: x, 
+        h: Math.round(y),
+        viewHeight: vHeight,
+        viewWidth: vWidth
+    };
+}
+
+AutoResize.prototype.checkResize = function(editor) {
+    if(this.timeout != null) {
+        window.clearTimeout(this.timeout);
+        this.timeout = null;
+    }
+    this.doResize(editor);
+}
+
+AutoResize.prototype.doResize = function(editor) {
+    if(editor._iframe == null) {
         var me = this;
         var again = function() {
-            me.doAutoResize(editor, w, h);
+            me.doResize(editor);
         }
-        window.setTimeout(again, 250);
+        this.timeout = window.setTimeout(again, 250);
     } else {
-        editor.sizeEditor(w + 'px', h + 'px', true, true);
+        this.timeout = null;
+        var vWidth = this.DOM.getViewportWidth();
+        var vHeight = this.DOM.getViewportHeight();
+        if(this.dim.viewWidth != vWidth || this.dim.viewHeight != vHeight) {
+            var newDim = this.getDim(vWidth, vHeight);
+            if(this.dim.w != newDim.w || this.dim.h != newDim.h) {
+                editor.sizeEditor(newDim.w + 'px', newDim.h + 'px', true, true);
+                this.dim = newDim;
+            }
+        }
     }
-};
+}
 
-AutoResize.prototype.isTemplateEditor = function(el) {
+AutoResize.prototype.findParent = function() {
+    var el = this.editor._textArea;
     while (el != null && el != document.body) {
-        if (YAHOO.util.Dom.hasClass(el, 'templateItemTable')) {
-            return true;
+        if (this.DOM.hasClass(el, 'hippo-editor-field-html')) {
+            return el;
         }
         el = el.parentNode;
     }
-    return false;
+    return null;
 };
-
-AutoResize.prototype.nrOfXinhas = function() {
-    var nr = YAHOO.util.Dom.getElementsByClassName('hippo-editor-field-html');
-    return nr.length;
-}
-
-
-/**
- * This function calculates the width of a scrollbar by adding an element without overflow
- * to the dom, then adding a much bigger child element, setting the overflow to auto and
- * calculating the difference from before and after. 
- * @return The width of a scrollbar
- */
-AutoResize.prototype.getScrollbarWidth = function() {
-    if(Xinha.is_ie) {
-        var a; 
-        var t = this.editor._textArea;   
-        var old = t.wrap;
-        t.wrap = 'off'; 
-        a = t.offsetHeight;   
-        t.wrap = 'soft'; 
-        a -= t.offsetHeight;
-        t.wrap = old;
-        return a;
-    } else {
-        var parent = document.createElement('div');
-        parent.style.position = 'absolute';
-        parent.style.top = '-1000px';
-        parent.style.left = '-1000px';
-        parent.style.width = '100px';
-        parent.style.height = '50px';
-        parent.style.overflow = 'hidden';
-      
-        var child = document.createElement('div');
-        child.style.width = '100%';
-        child.style.height = '200px';
-      
-        parent.appendChild(child);
-        document.body.appendChild(parent);
-      
-        var beforeScroll = child.offsetWidth;
-        parent.style.overflow = 'auto';
-        var scrollWidth = beforeScroll - child.offsetWidth;
-      
-        document.body.removeChild(document.body.lastChild);
-        return scrollWidth;
-    }
-}
