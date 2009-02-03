c8730a69ba99a7dacb44202297bed3dd18161ec7
Ard Schrijvers <a.schrijvers@onehippo.com>
2009-02-03 11:03:22 +0000

    HSTTWO-295 svn ignore
    svn path=/ecm/site-toolkit/branches/Branch-HSTTWO-295/; revision=16211

--- site-toolkit/api/src/main/java/org/hippoecm/hst/core/jcr/pool/PoolingRepositoryAware.java
+++
@@ -1,6 +0,0 @@
-package org.hippoecm.hst.core.jcr.pool;
-
-public interface PoolingRepositoryAware
-{
-    public void setPoolingRepository(PoolingRepository poolingRepository);
-}
--- site-toolkit/api/src/main/java/org/hippoecm/hst/core/jcr/pool/SessionDecorator.java
+++
@@ -1,8 +0,0 @@
-package org.hippoecm.hst.core.jcr.pool;
-
-import javax.jcr.Session;
-
-public interface SessionDecorator
-{
-    public Session decorate(Session session);
-}
