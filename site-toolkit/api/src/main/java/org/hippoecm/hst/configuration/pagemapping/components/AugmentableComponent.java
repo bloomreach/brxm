88f68cf0489b45a9f513722ed4b70bb0150e1fc5
Ard Schrijvers <a.schrijvers@onehippo.com>
2009-02-03 13:42:14 +0000

    HSTTWO-295 hst structure
    svn path=/ecm/site-toolkit/branches/Branch-HSTTWO-295/; revision=16217

--- site-toolkit/api/src/main/java/org/hippoecm/hst/configuration/pagemapping/components/AugmentableComponent.java
+++
@@ -1,6 +0,0 @@
-package org.hippoecm.hst.configuration.pagemapping.components;
-
-
-public interface AugmentableComponent extends Component{
-    public void addHierarchicalChildComponent(Component hierarchicalChildComponent);
-}
