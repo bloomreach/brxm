1554947c10fd153fc4397a1cf166bacbb02c432c
Woonsan Ko <w.ko@onehippo.com>
2009-02-05 09:23:04 +0000

    HSTTWO-295  changed location of hstrequestcontextimpl
    svn path=/ecm/site-toolkit/branches/Branch-HSTTWO-295/; revision=16264

--- site-toolkit/components/core/src/main/java/org/hippoecm/hst/core/filters/base/HstStartRequestFilter.java
+++ site-toolkit/components/core/src/main/java/org/hippoecm/hst/core/filters/base/HstStartRequestFilter.java
@@ -14,7 +14,7 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.hippoecm.hst.core.HSTHttpAttributes;
 import org.hippoecm.hst.core.request.HstRequestContext;
-import org.hippoecm.hst.core.request.HstRequestContextImpl;
+import org.hippoecm.hst.site.request.HstRequestContextImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
