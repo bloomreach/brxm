f5d2d0919283e78ac6dcf778439983031cfc6c1e
Bart van der Schans <b.vanderschans@onehippo.com>
2009-02-05 14:09:36 +0000

    HREPTWO-2232: Improve create links script
    svn path=/hippo-ecm/trunk/; revision=16278

--- repository/tools/eclipse/createlinks.sh
+++ repository/tools/eclipse/createlinks.sh
@@ -1,29 +1,63 @@
 #!/bin/sh
 # This script assumes that it's located in the hippo-ecm/tools/eclipse directory. 
- 
-cd $(dirname $0) 
-cd ../..
 
-cd package/war/src/main/webapp
-ln -s -v ../../../../skin/src/main/webapp skin
-ln -s -v ../../../../layout/src/main/webapp layout
-ln -s -v ../../../../../addon/xinha/src/main/webapp xinha
+LINK=/bin/ln
+RM=/bin/rm
 
-cd ../../../../../quickstart/war/src/main/webapp
-ln -s -v ../../../../../package/skin/src/main/webapp skin
-ln -s -v ../../../../../package/layout/src/main/webapp layout
-ln -s -v ../../../../../addon/xinha/src/main/webapp xinha
+BASEDIR=`dirname $0`/../..
+BASEDIR=`(cd "$BASEDIR"; pwd)`
 
-cd ../../../../../addon/xinha/src/main/webapp/xinha/plugins
-ln -s -v ../../../../../webResources/xinha/plugins/AutoSave AutoSave
-ln -s -v ../../../../../webResources/xinha/plugins/AutoResize AutoResize
-ln -s -v ../../../../../webResources/xinha/plugins/CreateLink CreateLink
-ln -s -v ../../../../../webResources/xinha/plugins/CreateExternalLink CreateExternalLink
-ln -s -v ../../../../../webResources/xinha/plugins/InsertImage InsertImage
-ln -s -v ../../../../../webResources/xinha/plugins/FullscreenCompatible FullscreenCompatible
 
-cd ../skins
-ln -s -v ../../../../../webResources/xinha/skins/hippo-gray hippo-gray
+cd $BASEDIR
+echo -n "Basedir: "
+pwd
 
-cd ../iconsets
-ln -s -v ../../../../../webResources/xinha/iconsets/Hippo Hippo
+# create new symlink
+create_link() {  
+  if [ -z "$2" ]; then
+    echo "create_link needs two arguments"
+    exit -1;
+  fi
+  # remove old link if it exists
+  if [ -L "$2" ]; then
+    #echo "removing old link $2"
+    $RM -f "$2"
+  fi
+
+  echo "Creating: $2"
+  $LINK -s "$1" "$2"
+}
+
+
+echo "Creating links in package war project:"
+create_link $BASEDIR/package/skin/src/main/webapp   $BASEDIR/package/war/src/main/webapp/skin
+create_link $BASEDIR/package/layout/src/main/webapp $BASEDIR/package/war/src/main/webapp/layout
+create_link $BASEDIR/addon/xinha/src/main/webapp    $BASEDIR/package/war/src/main/webapp/xinha
+echo
+
+echo "Creating links in quickstart war project:"
+create_link $BASEDIR/package/skin/src/main/webapp   $BASEDIR/quickstart/war/src/main/webapp/skin
+create_link $BASEDIR/package/layout/src/main/webapp $BASEDIR/quickstart/war/src/main/webapp/layout
+create_link $BASEDIR/addon/xinha/src/main/webapp    $BASEDIR/quickstart/war/src/main/webapp/xinha
+echo
+
+echo "Creating links for skin resources:"
+create_link $BASEDIR/addon/xinha/webResources/xinha/plugins/AutoSave             $BASEDIR/addon/xinha/src/main/webapp/xinha/plugins/AutoSave
+create_link $BASEDIR/addon/xinha/webResources/xinha/plugins/AutoResize           $BASEDIR/addon/xinha/src/main/webapp/xinha/plugins/AutoResize
+create_link $BASEDIR/addon/xinha/webResources/xinha/plugins/CreateLink           $BASEDIR/addon/xinha/src/main/webapp/xinha/plugins/CreateLink
+create_link $BASEDIR/addon/xinha/webResources/xinha/plugins/CreateExternalLink   $BASEDIR/addon/xinha/src/main/webapp/xinha/plugins/CreateExternalLink
+create_link $BASEDIR/addon/xinha/webResources/xinha/plugins/InsertImage          $BASEDIR/addon/xinha/src/main/webapp/xinha/plugins/InsertImage
+create_link $BASEDIR/addon/xinha/webResources/xinha/plugins/FullscreenCompatible $BASEDIR/addon/xinha/src/main/webapp/xinha/plugins/FullscreenCompatible
+echo
+
+echo "Creating link for xinha skin:"
+create_link $BASEDIR/addon/xinha/webResources/xinha/skins/hippo-gray  $BASEDIR/addon/xinha/src/main/webapp/xinha/skins/hippo-gray
+echo
+
+echo "Creating link for xinha iconset:"
+create_link $BASEDIR/addon/xinha/webResources/xinha/iconsets/Hippo  $BASEDIR/addon/xinha/src/main/webapp/xinha/iconsets/Hippo
+echo
+
+exit 0;
+
+
