6d5ce45958a17de7c1dd193d25bf1bee9df24ca3
Woonsan Ko <w.ko@onehippo.com>
2009-02-11 13:56:26 +0000

    [HSTTWO-295] Adds a skeleton pipeline execution test case.
    svn path=/ecm/site-toolkit/branches/Branch-HSTTWO-295/; revision=16375

---
+++ site-toolkit/components/core/src/test/java/org/hippoecm/hst/core/container/TestDefaultPipeline.java
@@ -0,0 +1,48 @@
+package org.hippoecm.hst.core.container;
+
+import static org.junit.Assert.fail;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.hippoecm.hst.core.request.HstRequestContext;
+import org.hippoecm.hst.site.request.HstRequestContextImpl;
+import org.hippoecm.hst.test.AbstractSpringTestCase;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestDefaultPipeline extends AbstractSpringTestCase {
+    
+    HttpServletRequest servletRequest;
+    HttpServletResponse servletResponse;
+    
+    protected Pipelines pipelines;
+    protected Pipeline defaultPipeline;
+
+    @Before
+    public void setUp() throws Exception {
+        super.setUp();
+        
+        this.pipelines = (Pipelines) getComponent(Pipelines.class.getName());
+        this.defaultPipeline = this.pipelines.getDefaultPipeline();
+        this.servletRequest = (HttpServletRequest) getComponent(HttpServletRequest.class.getName());
+        this.servletResponse = (HttpServletResponse) getComponent(HttpServletResponse.class.getName());
+    }
+    
+    @Test
+    public void testDefaultPipeline() throws ContainerException {
+        
+        HstRequestContext context = new HstRequestContextImpl();
+        
+        this.defaultPipeline.beforeInvoke(servletRequest, servletResponse, context);
+        
+        try {
+            this.defaultPipeline.invoke(servletRequest, servletResponse, context);
+        } catch (Exception e) {
+            fail("Exception!!!");
+        } finally {
+            this.defaultPipeline.afterInvoke(servletRequest, servletResponse, context);
+        }
+    }
+    
+}
