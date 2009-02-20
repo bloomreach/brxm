1a9adb49850f76d296ec9a920e7485ba7c2531ab
Woonsan Ko <w.ko@onehippo.com>
2009-02-20 11:35:40 +0000

    [HSTTWO-295] - Adds copyright notice to each source files - Removes some classes not used.
    svn path=/ecm/site-toolkit/branches/Branch-HSTTWO-295/; revision=16595

--- site-toolkit/components/core/src/test/java/org/hippoecm/hst/core/linking/TestLinks.java
+++ site-toolkit/components/core/src/test/java/org/hippoecm/hst/core/linking/TestLinks.java
@@ -1,3 +1,18 @@
+/*
+ *  Copyright 2008 Hippo.
+ * 
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package org.hippoecm.hst.core.linking;
 
 import static org.junit.Assert.assertEquals;
@@ -10,7 +25,6 @@ import javax.jcr.Session;
 
 import org.hippoecm.hst.configuration.HstSites;
 import org.hippoecm.hst.configuration.sitemap.HstSiteMapItem;
-import org.hippoecm.hst.core.jcr.pool.BasicPoolingRepository;
 import org.hippoecm.hst.service.ServiceFactory;
 import org.hippoecm.hst.service.ServiceNamespace;
 import org.hippoecm.hst.test.AbstractSpringTestCase;
