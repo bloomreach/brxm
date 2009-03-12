1b5173ea199f4415ad1db322dd31e93bd4266ded
Ard Schrijvers <a.schrijvers@onehippo.com>
2009-03-12 10:09:09 +0000

    HSTTWO-345 new setup
    svn path=/ecm/site-toolkit/trunk/; revision=16831

--- site-toolkit/applications/site/src/main/java/org/hippoecm/hst/components/GenericResourceServingHstComponent.java
+++ site-toolkit/applications/site/src/main/java/org/hippoecm/hst/components/GenericResourceServingHstComponent.java
@@ -13,7 +13,7 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-package org.hippoecm.hst.hstconfiguration.components;
+package org.hippoecm.hst.components;
 
 import javax.servlet.ServletConfig;
 
