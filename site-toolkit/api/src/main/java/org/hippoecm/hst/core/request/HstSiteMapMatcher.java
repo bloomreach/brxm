9c3a4c95a240d85325a9b1680584ab38ac5c24b4
Ard Schrijvers <a.schrijvers@onehippo.com>
2009-03-19 13:33:44 +0000

    HSTTWO-380 api docs
    svn path=/ecm/site-toolkit/trunk/; revision=17037

--- site-toolkit/api/src/main/java/org/hippoecm/hst/core/request/HstSiteMapMatcher.java
+++ site-toolkit/api/src/main/java/org/hippoecm/hst/core/request/HstSiteMapMatcher.java
@@ -17,10 +17,31 @@ package org.hippoecm.hst.core.request;
 
 import org.hippoecm.hst.configuration.HstSite;
 
+/**
+ * Implementations should be able to match a path (pathInfo) in combination with a <code>{@link HstSite}</code> object to a
+ * <code>{@link ResolvedSiteMapItem}</code>. Typically a <code>ResolvedSiteMapItem</code> is a request context based instance of a
+ * <code>{@link org.hippoecm.hst.configuration.sitemap.HstSiteMapItem}</code>, where possibly property placeholders in <code>{@link org.hippoecm.hst.configuration.sitemap.HstSiteMapItem#getParameters()}</code>
+ * are replaced by there request context sensitive values. For example, a ${1} parameter value might be replaced by the value that matched
+ * to the first <code>HstSiteMapItem</code> containing a wildcard. 
+ * <p/>
+ * When no <code>HstSiteMapItem</code> can be found to match the <code>pathInfo</code>, the implementation can return null for the 
+ * {@link #match(String, HstSite)}, but is also allowed to return some catch all <code>ResolvedSiteMapItem</code>
+ */
 public interface HstSiteMapMatcher {
     
+    /**
+     * method to match the pathInfo for the hstSite to a <code>HstSiteMapItem</code> if possible.
+     * 
+     * @param pathInfo the pathInfo that should be matched in the <code>HstSiteMapItem</code> tree
+     * @param hstSite the current <code>HstSite</code> that must match the pathInfo
+     * @return a ResolvedSiteMapItem and <code>null</code> if non matched
+     */
     ResolvedSiteMapItem match(String pathInfo,HstSite hstSite);
   
+    /**
+     * method that can be called if some event is triggered. For example if the <code>HstSiteMapMatcher</code> implementing class
+     * holds a cache that needs to be flushed after a change in the <code>HstSiteMap</code> configuration
+     */
     void invalidate();
 }
 
