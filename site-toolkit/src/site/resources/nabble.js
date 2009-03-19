9fe1d7d1a4249a1b1574674f447e0fcab82bbf6f
Arje Cahn <a.cahn@onehippo.com>
2009-03-19 10:00:53 +0000

    HSTTWODOCS-8: Public forums
    svn path=/ecm/site-toolkit/trunk/; revision=17014

---
+++ site-toolkit/src/site/resources/nabble.js
@@ -0,0 +1,5 @@
+// Create the link and load the javascript from Nabble.
+// This has to be done from Javascript, since Doxia removes the ID attribute from an anchor tag..
+
+document.write("<a href='http://n2.nabble.com/Hippo-Site-Toolkit-for-JSP-f2501836.html' id='nabblelink'>Hippo Site Toolkit for JSP Forums</a>");
+document.write("<script src='http://n2.nabble.com/embed/f2501836'></script>");
