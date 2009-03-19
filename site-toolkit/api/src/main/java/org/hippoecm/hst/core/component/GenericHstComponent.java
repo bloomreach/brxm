da7aab3bcf559521de5d615c082262900f6e3409
Ard Schrijvers <a.schrijvers@onehippo.com>
2009-03-19 11:00:51 +0000

    HSTTWO-380 little extra on the componentconfigurations only meant for layout
    svn path=/ecm/site-toolkit/trunk/; revision=17021

--- site-toolkit/api/src/main/java/org/hippoecm/hst/core/component/GenericHstComponent.java
+++ site-toolkit/api/src/main/java/org/hippoecm/hst/core/component/GenericHstComponent.java
@@ -20,11 +20,15 @@ import javax.servlet.ServletConfig;
 import org.hippoecm.hst.core.request.ComponentConfiguration;
 
 /**
- * The <CODE>GenericHstComponent</CODE> class provides a default implementation for
- * the <CODE>HstComponent</CODE> interface.
- * <p>
- * It provides a base class to be subclassed to create HstComponents. A subclass
- * of <CODE>GenericHstComponent</CODE> can override methods, usually
+ * The <code>GenericHstComponent</code> class provides a default implementation for
+ * the <code>HstComponent</code> interface. 
+ * <p/>
+ * It provides a base class to be subclassed to create HstComponents. Common practice is that 
+ * {@link org.hippoecm.hst.configuration.components.HstComponentConfiguration}'s whose only purpose is layout (markup) and do not have
+ * actual behavior, use this default <code>HstComponent</code> implementation as their component classname, see {@link org.hippoecm.hst.configuration.components.HstComponentConfiguration#getComponentClassName()}.
+ * 
+ * <p/>
+ * A subclass of <code>GenericHstComponent</code> can override methods, usually
  * one of the following:
  * <ul>
  * <li>doAction, to handle action requests</li>
@@ -32,7 +36,7 @@ import org.hippoecm.hst.core.request.ComponentConfiguration;
  * <li>doBeforeServeResource, to retrieve model objects from the repository to be passed to the resource serving page or servlet</li>
  * <li>init and destroy, to manage resources that are held for the life of the HstComponent</li>
  * </ul>
- * HstComponents typically run on multithreaded servers, so please note that a
+ * <strong>NOTE:</strong> <code>HstComponent</code>'s typically run on multithreaded servers, so take care that a
  * HstComponent must handle concurrent requests and be careful to synchronize access
  * to shared resources. Shared resources include in-memory data such as instance
  * or class variables and external objects such as files, database connections,
