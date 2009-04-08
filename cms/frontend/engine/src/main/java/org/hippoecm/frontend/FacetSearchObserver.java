3eaa424dd58710ab7e25682d5e9c4d3258865f55
Berry van Halderen <b.vanhalderen@onehippo.com>
2009-04-08 07:16:06 +0000

    HREPTWO-18: svn id, tabs
    svn path=/hippo-ecm/trunk/; revision=17233

--- cms/frontend/engine/src/main/java/org/hippoecm/frontend/FacetSearchObserver.java
+++ cms/frontend/engine/src/main/java/org/hippoecm/frontend/FacetSearchObserver.java
@@ -40,6 +40,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class FacetSearchObserver implements EventListener {
+    @SuppressWarnings("unused")
+    private final static String SVN_ID = "$Id: $";
 
     static final Logger log = LoggerFactory.getLogger(FacetSearchObserver.class);
 
