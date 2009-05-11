b3667108b9f4c2f9112fe8e88fb13ef6c1c9b7be
Vijay Kiran <v.kiran@onehippo.com>
2009-05-11 15:03:26 +0000

    - FIX For HSTTWO-537 (contd) - Changed GeneralPage.java package to beans from ocm. - Added license text to TextPage.java
    svn path=/ecm/site-toolkit/trunk/; revision=17709

--- site-toolkit/maven/archetype/src/main/resources/archetype-resources/site/src/main/java/beans/GeneralPage.java
+++ site-toolkit/maven/archetype/src/main/resources/archetype-resources/site/src/main/java/beans/GeneralPage.java
@@ -18,7 +18,7 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-package ${package}.ocm;
+package ${package}.beans;
 
 import org.hippoecm.hst.content.beans.standard.HippoDocument;
 
