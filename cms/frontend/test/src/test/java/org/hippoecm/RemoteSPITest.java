3f902cf2b33840fcb3e124bdd21931379b861142
Frank van Lankvelt <f.vanlankvelt@onehippo.com>
2009-05-12 09:12:59 +0000

    HREPTWO-2423: fix tests
    svn path=/hippo-ecm/trunk/; revision=17904

--- cms/frontend/test/src/test/java/org/hippoecm/RemoteSPITest.java
+++ cms/frontend/test/src/test/java/org/hippoecm/RemoteSPITest.java
@@ -13,7 +13,7 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-package org.hippoecm.frontend;
+package org.hippoecm;
 
 import java.net.MalformedURLException;
 import java.rmi.RemoteException;
