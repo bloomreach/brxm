05a6449c9f79f23755601ff506c105c87ae082ed
Berry van Halderen <b.vanhalderen@onehippo.com>
2009-05-14 14:48:11 +0000

    HREPTWO-2503, HREPTWO-2517: show no buttons for information, additional spacing between information and actions, translations
    svn path=/hippo-ecm/trunk/; revision=18105

---
+++ repository/addon/workflow/src/main/java/org/hippoecm/addon/workflow/MenuLabel.java
@@ -0,0 +1,41 @@
+/*
+ *  Copyright 2009 Hippo.
+ * 
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.hippoecm.addon.workflow;
+
+import org.apache.wicket.Component;
+import org.apache.wicket.markup.html.panel.Fragment;
+import org.apache.wicket.markup.html.panel.Panel;
+
+class MenuLabel extends Panel {
+    @SuppressWarnings("unused")
+    private final static String SVN_ID = "$Id$";
+
+    private static final long serialVersionUID = 1L;
+
+    public MenuLabel(String id, final ActionDescription wf) {
+        super(id);
+
+        Component fragment = wf.getFragment("text");
+        if (fragment instanceof ActionDescription.ActionDisplay) {
+            ((ActionDescription.ActionDisplay)fragment).substantiate();
+            add(fragment);
+        } else if (fragment instanceof Fragment) {
+            add(fragment);
+        } else {
+            // wf.setVisible(true);
+        }
+    }
+}
