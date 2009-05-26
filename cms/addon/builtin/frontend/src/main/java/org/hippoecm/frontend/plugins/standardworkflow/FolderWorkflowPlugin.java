294a098d69f7ba33e35f94abcb5d6f9b9f615423
Bart van der Schans <b.vanderschans@onehippo.com>
2009-05-26 17:05:34 +0000

    HREPTWO-2537: Fix translation issues, configure the service.translator.search plugin for workflows
    svn path=/hippo-ecm/trunk/; revision=18265

--- cms/addon/builtin/frontend/src/main/java/org/hippoecm/frontend/plugins/standardworkflow/FolderWorkflowPlugin.java
+++ cms/addon/builtin/frontend/src/main/java/org/hippoecm/frontend/plugins/standardworkflow/FolderWorkflowPlugin.java
@@ -332,6 +332,7 @@ public class FolderWorkflowPlugin extends CompatibilityWorkflowPlugin<FolderWork
 
             TextField text = new TextField("name", nameModel);
             text.setRequired(true);
+            System.out.println("NULL?? " +  FolderWorkflowPlugin.this);
             text.setLabel(new StringResourceModel("name-label", FolderWorkflowPlugin.this, null));
             setFocus(text);
             add(text);
