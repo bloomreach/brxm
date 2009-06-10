f4ec8e0dc1c3100f90e58ec20db0fa2b6de664de
Jeroen Reijn <j.reijn@onehippo.com>
2009-06-10 07:26:16 +0000

    HSTTWO-619: Add content bean for hippo:resource
    svn path=/ecm/site-toolkit/trunk/; revision=18466

---
+++ site-toolkit/content-beans/src/main/java/org/hippoecm/hst/content/beans/standard/HippoResource.java
@@ -0,0 +1,12 @@
+package org.hippoecm.hst.content.beans.standard;
+
+import org.hippoecm.hst.content.beans.Node;
+
+/**
+ * Bean mapping class for the 'hippo:resource' document type
+ *
+ */
+@Node(jcrType = "hippo:resource")
+public class HippoResource extends HippoItem implements HippoBean {
+
+}
