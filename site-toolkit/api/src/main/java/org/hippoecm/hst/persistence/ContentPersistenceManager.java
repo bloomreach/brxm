88bad62f7228202d308447a329254df00b6f4c48
Woonsan Ko <w.ko@onehippo.com>
2009-06-29 14:07:24 +0000

    HSTTWO-676: Adds persistency interface and unit test case.
    svn path=/ecm/site-toolkit/trunk/; revision=18698

---
+++ site-toolkit/api/src/main/java/org/hippoecm/hst/persistence/ContentPersistenceManager.java
@@ -0,0 +1,80 @@
+/*
+ *  Copyright 2008 Hippo.
+ * 
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.hippoecm.hst.persistence;
+
+/**
+ * <CODE>PersistenceManager</CODE> is the primary interface for HST-2-Persistence-aware application components.
+ * <P>
+ * An implementation of this interface should be able to convert content nodes to objects, and vice versa.
+ * Also it should have knowledges on how to create, update or remove a content node with node type and absolute path.
+ * </P>
+ * <P>
+ * Some sophisticated JCR repository engine already have their own workflow knowledges on 
+ * how to create, update and remove content nodes based on node types.
+ * </P>
+ * <P>
+ * Or, some domain specific content-based application should know the knowledges on their own content models.
+ * In this case, they should provide an implementation for this interface.
+ * </P>
+ * 
+ * @version $Id$
+ */
+public interface ContentPersistenceManager {
+    
+    /**
+     * Returns the content object at the specified absolute path.
+     * @param absPath
+     * @return an object mapped to the specified absolute path and its primary node type.
+     * @throws ContentPersistenceException
+     */
+    Object getObject(String absPath) throws ContentPersistenceException;
+    
+    /**
+     * Creates a content node with the specified node type at the specified absolute path.
+     * @param absPath
+     * @param nodeTypeName
+     * @param name the content node name
+     * @throws ContentPersistenceException
+     */
+    void create(String absPath, String nodeTypeName, String name) throws ContentPersistenceException;
+    
+    /**
+     * Updates the content node which is mapped to the object.
+     * @param content
+     * @throws ContentPersistenceException
+     */
+    void update(Object content) throws ContentPersistenceException;
+    
+    /**
+     * Removes the content node which is mapped to the object.
+     * @param content
+     * @throws ContentPersistenceException
+     */
+    void remove(Object content) throws ContentPersistenceException;
+    
+    /**
+     * Saves all pending changes. 
+     * @throws ContentPersistenceException
+     */
+    void save() throws ContentPersistenceException;
+    
+    /**
+     * Discards all pending changes and resets the current state.
+     * @throws ContentPersistenceException
+     */
+    void reset() throws ContentPersistenceException;
+    
+}
