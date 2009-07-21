1505bf8bf3e1342e27681298851a7fd93fe4496b
Frank van Lankvelt <f.vanlankvelt@onehippo.com>
2009-07-21 12:30:26 +0000

    HREPTWO-2846: organise fields in categories "primitive", "compound" and "custom"
    These categories are based on the type itself, without any additional categorization.
    Furthermore, the styling of the builder has been improved; the selected plugin now has a yellow bar, scrollbars have been eliminated.
    
    svn path=/hippo-ecm/trunk/; revision=18923

--- repository/addon/editor/frontend/src/main/java/org/hippoecm/frontend/editor/builder/TemplateListPlugin.java
+++ repository/addon/editor/frontend/src/main/java/org/hippoecm/frontend/editor/builder/TemplateListPlugin.java
@@ -15,22 +15,28 @@
  */
 package org.hippoecm.frontend.editor.builder;
 
-import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.LinkedList;
 import java.util.List;
+import java.util.SortedMap;
+import java.util.TreeMap;
 
 import org.apache.wicket.Component;
+import org.apache.wicket.MarkupContainer;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.ajax.markup.html.AjaxLink;
 import org.apache.wicket.behavior.AbstractBehavior;
 import org.apache.wicket.markup.ComponentTag;
+import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.list.ListItem;
+import org.apache.wicket.markup.html.list.ListView;
 import org.apache.wicket.markup.html.panel.Fragment;
 import org.apache.wicket.markup.repeater.Item;
 import org.apache.wicket.markup.repeater.data.IDataProvider;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
+import org.apache.wicket.model.ResourceModel;
 import org.hippoecm.frontend.editor.ITemplateEngine;
 import org.hippoecm.frontend.editor.TemplateEngineException;
 import org.hippoecm.frontend.i18n.types.TypeTranslator;
@@ -40,6 +46,7 @@ import org.hippoecm.frontend.model.event.IObserver;
 import org.hippoecm.frontend.model.nodetypes.JcrNodeTypeModel;
 import org.hippoecm.frontend.plugin.IPluginContext;
 import org.hippoecm.frontend.plugin.config.IPluginConfig;
+import org.hippoecm.frontend.plugins.standards.list.resolvers.CssClassAppender;
 import org.hippoecm.frontend.service.render.RenderPlugin;
 import org.hippoecm.frontend.types.ITypeDescriptor;
 import org.hippoecm.frontend.types.JavaFieldDescriptor;
@@ -56,128 +63,252 @@ public class TemplateListPlugin extends RenderPlugin {
 
     private static final Logger log = LoggerFactory.getLogger(TemplateListPlugin.class);
 
-    private IDataProvider templateProvider;
+    abstract class Category implements IDataProvider {
+        private static final long serialVersionUID = 1L;
+
+        private final ITemplateEngine engine;
+        private final List<String> editableTypes;
+        private List<ITypeDescriptor> list;
+        private String name;
+
+        Category(ITemplateEngine engine, List<String> editableTypes, String name) {
+            this.engine = engine;
+            this.editableTypes = editableTypes;
+            this.name = name;
+        }
+
+        void load() {
+            if (list == null) {
+                SortedMap<String, ITypeDescriptor> types = new TreeMap<String, ITypeDescriptor>();
+                for (String type : editableTypes) {
+                    try {
+                        ITypeDescriptor descriptor = engine.getType(type);
+                        if (descriptor.isType(HippoNodeType.NT_DOCUMENT)) {
+                            continue;
+                        }
+                        if (descriptor.isType(HippoNodeType.NT_TEMPLATETYPE)) {
+                            continue;
+                        }
+                        if (isTypeInCategory(descriptor)) {
+                            TypeTranslator translator = new TypeTranslator(new JcrNodeTypeModel(descriptor.getName()));
+                            String name = (String) translator.getTypeName().getObject();
+                            types.put(name, descriptor);
+                        }
+                    } catch (TemplateEngineException ex) {
+                        log.error("Failed to obtain descriptor for " + type);
+                    }
+                }
+                list = new ArrayList<ITypeDescriptor>(types.values());
+            }
+        }
+
+        abstract boolean isTypeInCategory(ITypeDescriptor descriptor);
+
+        public Iterator<ITypeDescriptor> iterator(int first, int count) {
+            load();
+            int toIndex = first + count;
+            if (toIndex > list.size()) {
+                toIndex = list.size();
+            }
+            return list.subList(first, toIndex).listIterator();
+        }
+
+        public IModel model(Object object) {
+            return new Model((ITypeDescriptor) object);
+        }
+
+        public int size() {
+            load();
+            return list.size();
+        }
+
+        public void detach() {
+            list = null;
+        }
+
+        public String getName() {
+            return name;
+        }
+    }
+
+    private final class CategoryView extends AbstractView {
+        private static final long serialVersionUID = 1L;
+
+        private CategoryView(String id, Category category) {
+            super(id, category);
+        }
+
+        @Override
+        public void populateItem(Item item) {
+            final ITypeDescriptor type = (ITypeDescriptor) item.getModelObject();
+            AjaxLink link = new AjaxLink("template") {
+                private static final long serialVersionUID = 1L;
+
+                @Override
+                public void onClick(AjaxRequestTarget target) {
+                    ITypeDescriptor containingType = (ITypeDescriptor) TemplateListPlugin.this.getModelObject();
+                    String prefix = containingType.getName();
+                    if (prefix.indexOf(':') > 0) {
+                        prefix = prefix.substring(0, prefix.indexOf(':'));
+                        containingType.addField(new JavaFieldDescriptor(prefix, type.getName()));
+                    } else {
+                        log.warn("adding a field to a primitive type is not supported");
+                    }
+                }
+            };
+            final String name = type.getName();
+            link.add(new Label("template-name", new TypeTranslator(new JcrNodeTypeModel(name)).getTypeName()));
+            item.add(link);
+
+            item.add(new AbstractBehavior() {
+                private static final long serialVersionUID = 1L;
+
+                @Override
+                public void onComponentTag(final Component component, final ComponentTag tag) {
+                    Object classes = tag.getUserData("class");
+                    if (classes instanceof String) {
+                        tag.put("class", (String) classes + " " + name.toLowerCase().replace(':', '_'));
+                    } else {
+                        tag.put("class", name.toLowerCase().replace(':', '_'));
+                    }
+                }
+            });
+        }
+
+        @Override
+        public void destroyItem(Item item) {
+            // nothing
+        }
+    }
+
+    static boolean hasPrefix(ITypeDescriptor descriptor, String prefix) {
+        String typeName = descriptor.getName();
+        if (typeName.indexOf(':') <= 0) {
+            return false;
+        }
+        typeName = typeName.substring(0, typeName.indexOf(':'));
+        if (prefix.equals(typeName)) {
+            return true;
+        }
+        return false;
+    }
+
+    private Category active;
+    private List<Category> categories;
 
     public TemplateListPlugin(IPluginContext context, IPluginConfig config) {
         super(context, config);
 
-        final ITemplateEngine engine = context.getService(config.getString(ITemplateEngine.ENGINE), ITemplateEngine.class);
-        final List<String> editableTypes = engine.getEditableTypes();
-        if (editableTypes instanceof IObservable) {
-            context.registerService(new IObserver() {
-                private static final long serialVersionUID = 1L;
-
-                public IObservable getObservable() {
-                    return (IObservable) editableTypes;
-                }
-
-                public void onEvent(Iterator<? extends IEvent> events) {
-                    redraw();
-                }
-                
-            }, IObserver.class.getName());
-        }
-        templateProvider = new IDataProvider() {
-            private static final long serialVersionUID = 1L;
-
-            private List<ITypeDescriptor> list;
-
-            void load() {
-                if (list == null) {
-                    list = new LinkedList<ITypeDescriptor>();
-                    for (String type : editableTypes) {
-                        try {
-                            ITypeDescriptor descriptor = engine.getType(type);
-                            if (descriptor.isType(HippoNodeType.NT_DOCUMENT)) {
-                                continue;
-                            }
-                            if (descriptor.isType(HippoNodeType.NT_TEMPLATETYPE)) {
-                                continue;
-                            }
-                            list.add(descriptor);
-                        } catch (TemplateEngineException ex) {
-                            log.error("Failed to obtain descriptor for " + type);
-                        }
-                    }
-                }
-            }
-
-            public Iterator iterator(int first, int count) {
-                load();
-                int toIndex = first + count;
-                if (toIndex > list.size())
-                {
-                    toIndex = list.size();
-                }
-                return list.subList(first, toIndex).listIterator();
-            }
-
-            public IModel model(Object object) {
-                return new Model((Serializable) object);
-            }
-
-            public int size() {
-                load();
-                return list.size();
-            }
-
-            public void detach() {
-                list = null;
-            }
-            
-        };
         String mode = getPluginConfig().getString("mode", "view");
         Fragment fragment = new Fragment("fragment", mode, this);
         add(fragment);
 
-        if("edit".equals(mode)) {
-            AbstractView templateView = new AbstractView("templates", templateProvider) {
+        if ("edit".equals(mode)) {
+            final ITemplateEngine engine = context.getService(config.getString(ITemplateEngine.ENGINE),
+                    ITemplateEngine.class);
+            final List<String> editableTypes = engine.getEditableTypes();
+            if (editableTypes instanceof IObservable) {
+                context.registerService(new IObserver() {
+                    private static final long serialVersionUID = 1L;
+
+                    public IObservable getObservable() {
+                        return (IObservable) editableTypes;
+                    }
+
+                    public void onEvent(Iterator<? extends IEvent> events) {
+                        redraw();
+                    }
+
+                }, IObserver.class.getName());
+            }
+
+            ITypeDescriptor containingType = (ITypeDescriptor) TemplateListPlugin.this.getModelObject();
+            String typeName = containingType.getName();
+            final String prefix = typeName.substring(0, typeName.indexOf(':'));
+
+            categories = new ArrayList<Category>(3);
+            categories.add(new Category(engine, editableTypes, "primitive") {
                 private static final long serialVersionUID = 1L;
-    
+
                 @Override
-                public void populateItem(Item item) {
-                    final ITypeDescriptor type = (ITypeDescriptor) item.getModelObject();
-                    AjaxLink link = new AjaxLink("template") {
+                boolean isTypeInCategory(ITypeDescriptor descriptor) {
+                    if (!descriptor.isNode()) {
+                        return true;
+                    }
+                    return false;
+                }
+            });
+            categories.add(new Category(engine, editableTypes, "compound") {
+                private static final long serialVersionUID = 1L;
+
+                @Override
+                boolean isTypeInCategory(ITypeDescriptor descriptor) {
+                    if (descriptor.isNode() && !hasPrefix(descriptor, prefix)) {
+                        return true;
+                    }
+                    return false;
+                }
+            });
+            categories.add(new Category(engine, editableTypes, "custom") {
+                private static final long serialVersionUID = 1L;
+
+                @Override
+                boolean isTypeInCategory(ITypeDescriptor descriptor) {
+                    if (descriptor.isNode() && hasPrefix(descriptor, prefix)) {
+                        return true;
+                    }
+                    return false;
+                }
+            });
+            active = categories.get(0);
+
+            fragment.add(new ListView("categories", categories) {
+                private static final long serialVersionUID = 1L;
+
+                @Override
+                protected void populateItem(ListItem item) {
+                    final Category category = (Category) item.getModelObject();
+
+                    AbstractView templateView = new CategoryView("templates", category);
+                    templateView.populate();
+                    MarkupContainer container = new WebMarkupContainer("container") {
                         private static final long serialVersionUID = 1L;
-    
+
+                        @Override
+                        public boolean isVisible() {
+                            return active == category;
+                        }
+
+                    };
+                    container.add(templateView);
+                    item.add(container);
+
+                    AjaxLink link = new AjaxLink("link") {
+                        private static final long serialVersionUID = 1L;
+
                         @Override
                         public void onClick(AjaxRequestTarget target) {
-                            ITypeDescriptor containingType = (ITypeDescriptor) TemplateListPlugin.this.getModelObject();
-                            String prefix = containingType.getName();
-                            if (prefix.indexOf(':') > 0) {
-                                prefix = prefix.substring(0, prefix.indexOf(':'));
-                                containingType.addField(new JavaFieldDescriptor(prefix, type.getName()));
-                            } else {
-                                log.warn("adding a field to a primitive type is not supported");
-                            }
+                            active = category;
+                            redraw();
                         }
                     };
-                    final String name = type.getName();
-                    link.add(new Label("template-name", new TypeTranslator(new JcrNodeTypeModel(name)).getTypeName()));
-                    item.add(link);
-    
-                    item.add(new AbstractBehavior() {
+                    link.add(new Label("category", new ResourceModel(category.getName())));
+                    link.add(new CssClassAppender(new Model() {
                         private static final long serialVersionUID = 1L;
-    
+
                         @Override
-                        public void onComponentTag(final Component component, final ComponentTag tag) {
-                            Object classes = tag.getUserData("class");
-                            if (classes instanceof String) {
-                                tag.put("class", (String) classes + " " + name.toLowerCase().replace(':', '_'));
-                            } else {
-                                tag.put("class", name.toLowerCase().replace(':', '_'));
+                        public Object getObject() {
+                            if (active == category) {
+                                return "focus";
                             }
+                            return "";
                         }
-                    });
+                    }));
+                    item.add(link);
                 }
-    
-                @Override
-                public void destroyItem(Item item) {
-                    // nothing
-                }
-            };
-            templateView.populate();
-            fragment.add(templateView);
+
+            });
+
         }
     }
 
