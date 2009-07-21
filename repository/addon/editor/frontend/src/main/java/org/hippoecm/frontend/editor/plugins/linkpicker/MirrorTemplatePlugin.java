2fa249872fb0dc289519d042785e8141ad9e10f5
Frank van Lankvelt <f.vanlankvelt@onehippo.com>
2009-07-21 16:10:31 +0000

    HREPTWO-2725: create separate template for hippo:mirror
    The hippo:mirror template is available as "Link".  The facetselect is still available, with some modified styling (a table for facet/mode/value).
    
    svn path=/hippo-ecm/trunk/; revision=18939

---
+++ repository/addon/editor/frontend/src/main/java/org/hippoecm/frontend/editor/plugins/linkpicker/MirrorTemplatePlugin.java
@@ -0,0 +1,142 @@
+/*
+ *  Copyright 2008 Hippo.
+ * 
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.hippoecm.frontend.editor.plugins.linkpicker;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.jcr.Node;
+import javax.jcr.PathNotFoundException;
+import javax.jcr.RepositoryException;
+import javax.jcr.ValueFormatException;
+
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.model.IChainingModel;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.Model;
+import org.hippoecm.frontend.dialog.AbstractDialog;
+import org.hippoecm.frontend.dialog.DialogLink;
+import org.hippoecm.frontend.dialog.IDialogFactory;
+import org.hippoecm.frontend.model.JcrNodeModel;
+import org.hippoecm.frontend.model.properties.JcrPropertyModel;
+import org.hippoecm.frontend.model.properties.JcrPropertyValueModel;
+import org.hippoecm.frontend.plugin.IPluginContext;
+import org.hippoecm.frontend.plugin.config.IPluginConfig;
+import org.hippoecm.frontend.service.render.RenderPlugin;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MirrorTemplatePlugin extends RenderPlugin {
+    @SuppressWarnings("unused")
+    private final static String SVN_ID = "$Id$";
+
+    private static final long serialVersionUID = 1L;
+
+    static final Logger log = LoggerFactory.getLogger(MirrorTemplatePlugin.class);
+
+    private final String mode;
+
+    public MirrorTemplatePlugin(final IPluginContext context, IPluginConfig config) {
+        super(context, config);
+
+        JcrNodeModel jcrNodeModel = (JcrNodeModel) getModel();
+        Node node = jcrNodeModel.getNode();
+
+        IModel displayModel = new Model() {
+            private static final long serialVersionUID = 1L;
+
+            @Override
+            public Object getObject() {
+                Node node = ((JcrNodeModel) MirrorTemplatePlugin.this.getModel()).getNode();
+                try {
+                    String docbaseUUID = node.getProperty("hippo:docbase").getString();
+                    if (docbaseUUID == null || docbaseUUID.equals("") || docbaseUUID.startsWith("cafebabe-")) {
+                        return "[...]";
+                    }
+                    return node.getSession().getNodeByUUID(docbaseUUID).getPath();
+                } catch (ValueFormatException e) {
+                    log.warn("Invalid value format for docbase " + e.getMessage());
+                    log.debug("Invalid value format for docbase ", e);
+                } catch (PathNotFoundException e) {
+                    log.warn("Docbase not found " + e.getMessage());
+                    log.debug("Docbase not found ", e);
+                } catch (RepositoryException e) {
+                    log.error("Invalid docbase" + e.getMessage(), e);
+                }
+                return "[...]";
+            }
+        };
+
+        mode = config.getString("mode", "view");
+        try {
+            if ("edit".equals(mode)) {
+                final List<String> nodetypes = new ArrayList<String>();
+                if (config.getStringArray("nodetypes") != null) {
+                    String[] nodeTypes = config.getStringArray("nodetypes");
+                    nodetypes.addAll(Arrays.asList(nodeTypes));
+                }
+                if (nodetypes.size() == 0) {
+                    log.debug("No configuration specified for filtering on nodetypes. No filtering will take place.");
+                }
+                final JcrPropertyValueModel docbaseModel = new JcrPropertyValueModel(new JcrPropertyModel(node
+                        .getProperty("hippo:docbase")));
+                //add(new TextFieldWidget("docbase", docbaseModel));
+                IDialogFactory dialogFactory = new IDialogFactory() {
+                    private static final long serialVersionUID = 1L;
+
+                    public AbstractDialog createDialog() {
+                        return new LinkPickerDialog(context, getPluginConfig(), new IChainingModel() {
+                            private static final long serialVersionUID = 1L;
+
+                            public Object getObject() {
+                                return docbaseModel.getObject();
+                            }
+
+                            public void setObject(Object object) {
+                                docbaseModel.setObject(object);
+                                redraw();
+                            }
+
+                            public IModel getChainedModel() {
+                                return docbaseModel;
+                            }
+
+                            public void setChainedModel(IModel model) {
+                                throw new UnsupportedOperationException("Value model cannot be changed");
+                            }
+
+                            public void detach() {
+                                docbaseModel.detach();
+                            }
+                        }, nodetypes);
+                    }
+                };
+                add(new DialogLink("docbase", displayModel, dialogFactory, getDialogService()));
+            } else {
+                add(new Label("docbase", displayModel));
+            }
+        } catch (PathNotFoundException ex) {
+            log.error("failed to read existing facet select", ex);
+        } catch (ValueFormatException ex) {
+            log.error("failed to read existing facet select", ex);
+        } catch (RepositoryException ex) {
+            log.error("failed to read existing facet select", ex);
+        }
+
+        setOutputMarkupId(true);
+    }
+}
