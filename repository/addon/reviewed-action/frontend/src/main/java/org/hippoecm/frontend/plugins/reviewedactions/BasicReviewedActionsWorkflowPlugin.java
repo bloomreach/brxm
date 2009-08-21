40a80adff16c748ffbbedff8109587048f1d1cdf
Berry van Halderen <b.vanhalderen@onehippo.com>
2009-08-21 10:38:21 +0000

    HREPTWO-3001: only show menu item for once variant
    svn path=/hippo-ecm/trunk/; revision=19340

--- repository/addon/reviewed-action/frontend/src/main/java/org/hippoecm/frontend/plugins/reviewedactions/BasicReviewedActionsWorkflowPlugin.java
+++ repository/addon/reviewed-action/frontend/src/main/java/org/hippoecm/frontend/plugins/reviewedactions/BasicReviewedActionsWorkflowPlugin.java
@@ -272,6 +272,7 @@ public class BasicReviewedActionsWorkflowPlugin extends CompatibilityWorkflowPlu
                 }
                 if (info.containsKey("status") && info.get("status") instanceof Boolean && !((Boolean)info.get("status")).booleanValue()) {
                     infoAction.setVisible(false);
+                    whereUsedAction.setVisible(false);
                 }
                 if (info.containsKey("inUseBy") && info.get("inUseBy") instanceof String) {
                     inUseBy = (String) info.get("inUseBy");
