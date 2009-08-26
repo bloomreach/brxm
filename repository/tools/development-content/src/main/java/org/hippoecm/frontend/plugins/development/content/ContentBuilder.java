440cccac4be01dfc018869b90479f8cd5f8ff6e1
Arthur Bogaart <a.bogaart@onehippo.com>
2009-08-26 08:26:02 +0000

    HREPTWO-3019: Tools: Add plugins that generate development content https://issues.onehippo.com/browse/HREPTWO-3019 - implemented cerate-folders - refactored wizard data into single settings classes
    svn path=/hippo-ecm/trunk/; revision=19396

--- repository/tools/development-content/src/main/java/org/hippoecm/frontend/plugins/development/content/ContentBuilder.java
+++ repository/tools/development-content/src/main/java/org/hippoecm/frontend/plugins/development/content/ContentBuilder.java
@@ -47,39 +47,104 @@ import org.slf4j.LoggerFactory;
 
 public class ContentBuilder implements IClusterable {
     private static final long serialVersionUID = 1L;
-    
+
     static final Logger log = LoggerFactory.getLogger(ContentBuilder.class);
-    
+
     public static class NameSettings implements IClusterable {
         private static final long serialVersionUID = 1L;
-        
+
         int minLength = 20;
         int maxLength = 35;
-        int amount = 5;
-        
+
         public NameSettings() {
         }
-        
-        public NameSettings(int min, int max, int amount) {
-            this.minLength = min;
-            this.maxLength = max;
-            this.amount = amount;
+    }
+
+    public static class NodeTypeSettings implements IClusterable {
+        private static final long serialVersionUID = 1L;
+
+        Collection<String> types = new LinkedList<String>();
+        boolean random = true;
+
+        public void setTypes(Collection<String> selectedTypes) {
+            this.types = selectedTypes;
+        }
+
+        public Collection<String> getTypes() {
+            return types;
+        }
+
+        public void setRandom(boolean random) {
+            this.random = random;
+        }
+
+        public boolean isRandom() {
+            return random;
+        }
+    }
+
+    public static class NodeSettings implements IClusterable {
+        private static final long serialVersionUID = 1L;
+
+        String folderUUID;
+
+        NameSettings naming;
+        NodeTypeSettings nodeTypes;
+
+        public NodeSettings() {
+            naming = new NameSettings();
+            nodeTypes = new NodeTypeSettings();
+        }
+
+        public NodeSettings(String folderUUID) {
+            this();
+            this.folderUUID = folderUUID;
+        }
+    }
+
+    public static class FolderSettings extends NodeSettings {
+        private static final long serialVersionUID = 1L;
+
+        int depth = 3;
+        int minimumChildNodes = 2;
+        int maximumChildNodes = 2;
+
+        public FolderSettings() {
+        }
+
+        public FolderSettings(String folderUUID) {
+            super(folderUUID);
+        }
+    }
+
+    public static class DocumentSettings extends NodeSettings {
+        private static final long serialVersionUID = 1L;
+
+        int amount = 5;
+
+        public DocumentSettings() {
+        }
+
+        public DocumentSettings(String folderUUID) {
+            super(folderUUID);
         }
     }
 
     private static final String DEFAULT_WORKFLOW_CATEGORY = "threepane";
     private static final String NEW_FOLDER_CATEGORY = "new-folder";
     private static final String NEW_DOCUMENT_CATEGORY = "new-document";
-    
+
     String folderPath;
     FolderWorkflow folderWorkflow;
     Names names;
-       
+
     Collection<String> docTypes;
     Collection<String> folderTypes;
 
     private String workflowCategory;
-    
+
+    Random generator = new Random();
+
     public ContentBuilder() {
         this(DEFAULT_WORKFLOW_CATEGORY);
     }
@@ -88,61 +153,90 @@ public class ContentBuilder implements IClusterable {
         this.workflowCategory = workflowCategory;
     }
 
-    public void createRandomDocuments(String folder, NameSettings nameSettings) {
-        Collection<String> types = getDocumentTypes(folder);
-        createDocuments(folder, types, nameSettings, true);
-    }
+    public void createDocuments(DocumentSettings settings) {
+        updateFolder(settings.folderUUID);
 
-    public void createDocuments(String folder, Collection<String> selectedTypes, NameSettings nameSettings) {
-        updateFolder(folder);
-        createDocuments(folder, selectedTypes, nameSettings, false);
-    }
-
-    private void createDocuments(String folder, Collection<String> types, NameSettings nameSettings, boolean random) {
+        if (settings.nodeTypes.random) {
+            settings.nodeTypes.types = getDocumentTypes(settings.folderUUID);
+        }
 
         if (folderWorkflow != null) {
-            String[] typeAr = types.toArray(new String[types.size()]);
-            for(int i=0; i<nameSettings.amount; i++) {
-                Random generator = new Random();
-                int index = generator.nextInt(types.size());   
-                String prototype = typeAr[index];
-                String targetName = generateName(nameSettings.minLength, nameSettings.maxLength);
-                try {
-                    folderWorkflow.add(NEW_DOCUMENT_CATEGORY, prototype, NodeNameCodec.encode(targetName, true));
-                } catch (MappingException e) {
-                    // TODO Auto-generated catch block
-                    e.printStackTrace();
-                } catch (RemoteException e) {
-                    // TODO Auto-generated catch block
-                    e.printStackTrace();
-                } catch (WorkflowException e) {
-                    // TODO Auto-generated catch block
-                    e.printStackTrace();
-                } catch (RepositoryException e) {
-                    // TODO Auto-generated catch block
-                    e.printStackTrace();
+            String[] typeAr = settings.nodeTypes.types.toArray(new String[settings.nodeTypes.types.size()]);
+
+            for (int i = 0; i < settings.amount; i++) {
+                createNode(NEW_DOCUMENT_CATEGORY, typeAr, settings.naming);
+            }
+        }
+    }
+
+    private String createNode(String category, String[] typeAr, NameSettings settings) {
+        int index = generator.nextInt(typeAr.length);
+        String prototype = typeAr[index];
+        String targetName = generateName(settings.minLength, settings.maxLength);
+        try {
+            String name = NodeNameCodec.encode(targetName, true);
+            folderWorkflow.add(category, prototype, name);
+            return name;
+        } catch (MappingException e) {
+            log.error("Error creating new node of category " + category, e);
+        } catch (RemoteException e) {
+            log.error("Error creating new node of category " + category, e);
+        } catch (WorkflowException e) {
+            log.error("Error creating new node of category " + category, e);
+        } catch (RepositoryException e) {
+            log.error("Error creating new node of category " + category, e);
+        }
+        return null;
+    }
+
+    public void createFolders(FolderSettings settings, int depth) {
+        updateFolder(settings.folderUUID);
+
+        if (settings.nodeTypes.random) {
+            settings.nodeTypes.types = getFolderTypes(settings.folderUUID);
+        }
+
+        if (settings.nodeTypes.types == null || settings.nodeTypes.types.size() == 0) {
+            return;
+        }
+
+        if (folderWorkflow != null) {
+            String[] nodeTypes = settings.nodeTypes.types.toArray(new String[settings.nodeTypes.types.size()]);
+
+            int amount = settings.minimumChildNodes;
+            int diff = settings.maximumChildNodes - settings.minimumChildNodes;
+            if (diff > 0) {
+                amount += generator.nextInt(diff);
+            }
+
+            List<String> newNodes = new LinkedList<String>();
+            for (int i = 0; i < amount; i++) {
+                newNodes.add(createNode(NEW_FOLDER_CATEGORY, nodeTypes, settings.naming));
+            }
+            if (depth < settings.depth) {
+                depth++;
+                String thisPath = folderPath;
+                for (String newNode : newNodes) {
+                    settings.folderUUID = path2uuid(thisPath + "/" + newNode);
+                    createFolders(settings, depth);
                 }
             }
         }
     }
 
-    public Collection<String> getFolderTypes(String folder) {
-        if(updateFolder(folder)) {
-            folderTypes = getTypes(NEW_FOLDER_CATEGORY, new LinkedList<String>()); 
-        }
-        return folderTypes;
+    public Collection<String> getFolderTypes(String folderUUID) {
+        updateFolder(folderUUID);
+        return getTypes(NEW_FOLDER_CATEGORY, new LinkedList<String>());
     }
 
-    
-    public Collection<String> getDocumentTypes(String folder) {
-        if(updateFolder(folder)) {
-            docTypes = getTypes(NEW_DOCUMENT_CATEGORY, new LinkedList<String>()); 
-        }
-        return docTypes;
+    public Collection<String> getDocumentTypes(String folderUUID) {
+        updateFolder(folderUUID);
+        return getTypes(NEW_DOCUMENT_CATEGORY, new LinkedList<String>());
     }
-    
+
+    @SuppressWarnings("unchecked")
     private Collection<String> getTypes(String category, List<String> types) {
-        if(folderWorkflow != null) {
+        if (folderWorkflow != null) {
             Map<String, Serializable> hints;
             try {
                 hints = folderWorkflow.hints();
@@ -165,9 +259,10 @@ public class ContentBuilder implements IClusterable {
         }
         return types;
     }
-    
-    private boolean updateFolder(String folder) {
-        if(folderPath == null || !folderPath.equals(folder)) {
+
+    private boolean updateFolder(String folderUUID) {
+        String folder = uuid2path(folderUUID);
+        if (folder != null && (folderPath == null || !folderPath.equals(folder))) {
             folderPath = folder;
             reloadWorkflow();
             return true;
@@ -176,7 +271,7 @@ public class ContentBuilder implements IClusterable {
     }
 
     private void reloadWorkflow() {
-        
+
         Session jcrSession = ((UserSession) org.apache.wicket.Session.get()).getJcrSession();
         WorkflowDescriptor folderWorkflowDescriptor = null;
         Node node = null;
@@ -202,32 +297,32 @@ public class ContentBuilder implements IClusterable {
     }
 
     public String generateName(int minLength, int maxLength) {
-        if(names == null) {
+        if (names == null) {
             names = new NamesFactory().newNames();
         }
         names.setMaximumLength(maxLength);
         String newName = "";
-        while(newName.length() < minLength) {
-            if(!newName.equals("")) {
+        while (newName.length() < minLength) {
+            if (!newName.equals("")) {
                 newName += " ";
             }
             String gen = names.generate();
-            if(newName.length() + gen.length() > maxLength) {
+            if (newName.length() + gen.length() > maxLength) {
                 names.setMaximumLength(maxLength - newName.length());
                 gen = names.generate();
             }
             newName += gen;
         }
-        if(newName.length() > maxLength) {
+        if (newName.length() > maxLength) {
             newName = newName.substring(0, maxLength);
-            if(newName.length() > maxLength) {
+            if (newName.length() > maxLength) {
                 throw new IllegalStateException("warning, imcapable programmer at work");
             }
         }
         return newName;
     }
 
-    public String uuid2path(String uuid) {
+    private String uuid2path(String uuid) {
         Session jcrSession = ((UserSession) org.apache.wicket.Session.get()).getJcrSession();
         try {
             Node node = jcrSession.getNodeByUUID(uuid);
@@ -240,5 +335,25 @@ public class ContentBuilder implements IClusterable {
         return null;
     }
 
-    
+    private String path2uuid(String path) {
+        if (path.startsWith("/")) {
+            path = path.substring(1);
+        }
+        //WorkflowManager manager = ((HippoWorkspace) (jcrSession.getWorkspace())).getWorkflowManager();
+        //manager.getSession().save();
+        Session jcrSession = ((UserSession) org.apache.wicket.Session.get()).getJcrSession();
+        try {
+            if (jcrSession.getRootNode().hasNode(path)) {
+                Node node = jcrSession.getRootNode().getNode(path);
+                return node.getUUID();
+            }
+            log.error("Could not find node with path " + path);
+        } catch (ItemNotFoundException e) {
+            log.error("Node node found for path " + path, e);
+        } catch (RepositoryException e) {
+            log.error("Error while retrieving uuid for node path" + path, e);
+        }
+        return null;
+    }
+
 }
