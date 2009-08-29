dc5e986b2c7b17f6abe711cbcbf2591ee375d9aa
Arthur Bogaart <a.bogaart@onehippo.com>
2009-08-29 21:25:53 +0000

    IN PROGRESS - issue HREPTWO-2930: Tree sizing issues https://issues.onehippo.com/browse/HREPTWO-2930  - Set scrollbars to tree  - no wrapping for add-root label
    svn path=/hippo-ecm/trunk/; revision=19462

--- cms/addon/yui/src/main/java/org/hippoecm/frontend/plugins/yui/inc/hippo/2.7.0/accordionmanager/accordionmanager.js
+++ cms/addon/yui/src/main/java/org/hippoecm/frontend/plugins/yui/inc/hippo/2.7.0/accordionmanager/accordionmanager.js
@@ -146,11 +146,11 @@ if (!YAHOO.hippo.AccordionManager) {
                 if (this.cfg.addScrollbar) {
                     //Dom.setStyle(id, 'overflow', 'scroll');
                     if(YAHOO.env.ua.ie > 0 && YAHOO.env.ua.ie < 7) {
-                        Dom.setStyle(id, '-ms-overflow-x', 'hidden');
-                        Dom.setStyle(id, '-ms-overflow-y', 'auto');
+                        Dom.setStyle(centerEl, '-ms-overflow-x', 'hidden');
+                        Dom.setStyle(centerEl, '-ms-overflow-y', 'auto');
                     } else {
-                        Dom.setStyle(id, 'overflow-x', 'hidden');
-                        Dom.setStyle(id, 'overflow-y', 'auto');
+                        Dom.setStyle(centerEl, 'overflow-x', 'hidden');
+                        Dom.setStyle(centerEl, 'overflow-y', 'auto');
                     }
                 }
                 if(height > 0) {
