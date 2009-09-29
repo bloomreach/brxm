8fcf860ab512b40fe06fa809ffdd3c6c272a78fc
Ard Schrijvers <a.schrijvers@onehippo.com>
2009-09-29 10:12:35 +0000

    HSTTWO-432 improve documentation
    svn path=/ecm/site-toolkit/trunk/; revision=19801

--- site-toolkit/api/src/main/java/org/hippoecm/hst/content/beans/query/HstQueryResult.java
+++ site-toolkit/api/src/main/java/org/hippoecm/hst/content/beans/query/HstQueryResult.java
@@ -17,9 +17,23 @@ package org.hippoecm.hst.content.beans.query;
 
 import org.hippoecm.hst.content.beans.standard.HippoBeanIterator;
 
+/**
+ * The result of the execution of the HstQuery. 
+ */
 public interface HstQueryResult {
     
+    /**
+     * Returns the total number of hits. A hit from a HstQuery is always a single hit. If the hit matches the search criteria multiple
+     * times, it still results as a single hit. It does however influence the Lucene scoring, hence, when no sorting is applied, the hit
+     * might have a higher precendence than other hits.
+     * @return the total number of hits. 
+     */
     int getSize();
     
+    /**
+     * This returns a HippoBeanIterator, which is a lazy loading proxy for accessing the beans in the HstQueryResult. This is really efficient
+     * as the beans are only really fetched when being called by {@link HippoBeanIterator#nextHippoBean()} or {@link HippoBeanIterator#next()}
+     * @return a HippoBeanIterator
+     */
     HippoBeanIterator getHippoBeans();
 }
