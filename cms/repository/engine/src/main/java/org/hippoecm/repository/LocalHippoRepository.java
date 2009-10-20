8e6cb767ae62cad01e13ec15e51b610d7dea9ac1
Berry van Halderen <b.vanhalderen@onehippo.com>
2009-10-20 20:04:25 +0000

    HREPTWO-3217: external nodes where not deleted from persistent storage
    svn path=/hippo-ecm/trunk/; revision=20178

--- cms/repository/engine/src/main/java/org/hippoecm/repository/LocalHippoRepository.java
+++ cms/repository/engine/src/main/java/org/hippoecm/repository/LocalHippoRepository.java
@@ -121,6 +121,9 @@ public class LocalHippoRepository extends HippoRepositoryImpl {
         "OR " + HippoNodeType.HIPPO_CONTENTDELETE + " IS NOT NULL) " +
         "ORDER BY " + HippoNodeType.HIPPO_SEQUENCE + " ASC";
 
+    /** The advised threshold on the number of modified nodes to hold in transient session state */
+    public static int BATCH_THRESHOLD = 96;
+
     /** hippo decorated root session */
     private HippoSession rootSession;
 
@@ -326,6 +329,9 @@ public class LocalHippoRepository extends HippoRepositoryImpl {
                 Session migrateSession = DecoratorFactoryImpl.getSessionDecorator(jcrRootSession.impersonate(new SimpleCredentials("system", new char[] {})));
                 needsRestart = UpdaterEngine.migrate(migrateSession);
                 migrateSession.logout();
+                if (needsRestart) {
+                    return;
+                }
             }
 
             try {
@@ -642,7 +648,7 @@ public class LocalHippoRepository extends HippoRepositoryImpl {
 
                             // verify that content root is not under the initialization node
                             String initPath = initializationNode.getPath();
-                            if (root.length() > initPath.length() && root.substring(0, initPath.length()) == initPath) {
+                            if (root.length() > initPath.length() && root.substring(0, initPath.length()).equals(initPath)) {
                                 log.error("Refusing to extract content to " + root);
                             } else {
                                 log.info("Initializing content from: " + contentName + " to " + root);
@@ -675,7 +681,7 @@ public class LocalHippoRepository extends HippoRepositoryImpl {
 
                             // verify that content root is not under the initialization node
                             String initPath = initializationNode.getPath();
-                            if (root.length() > initPath.length() && root.substring(0, initPath.length()) == initPath) {
+                            if (root.length() > initPath.length() && root.substring(0, initPath.length()).equals(initPath)) {
                                 log.error("Refusing to extract content to " + root);
                             } else {
                                 log.info("Initializing content from: " + contentName + " to " + root);
