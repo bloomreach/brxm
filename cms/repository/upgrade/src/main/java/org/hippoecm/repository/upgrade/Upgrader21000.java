4a8bec8c53ff645fc965567237dc60976846e238
Berry van Halderen <b.vanhalderen@onehippo.com>
2009-11-27 08:34:49 +0000

    HREPTWO-3346: upgrade framework to 2.10.00
    svn path=/hippo-ecm/trunk/; revision=20902

---
+++ cms/repository/upgrade/src/main/java/org/hippoecm/repository/upgrade/Upgrader21000.java
@@ -0,0 +1,48 @@
+/*
+ *  Copyright 2009 Hippo.
+ * 
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.hippoecm.repository.upgrade;
+
+import javax.jcr.Node;
+import javax.jcr.NodeIterator;
+import javax.jcr.RepositoryException;
+
+import org.hippoecm.repository.ext.UpdaterContext;
+import org.hippoecm.repository.ext.UpdaterItemVisitor;
+import org.hippoecm.repository.ext.UpdaterModule;
+
+public class Upgrader21000 implements UpdaterModule {
+    public void register(final UpdaterContext context) {
+        context.registerName("upgrade-v21000");
+        context.registerStartTag("v20902");
+        context.registerEndTag("v21000");
+        context.registerVisitor(new UpdaterItemVisitor.NodeTypeVisitor("rep:root") {
+            @Override
+            protected void leaving(final Node node, int level) throws RepositoryException {
+                /*
+                 * The removal of the entire /hippo:log tree seems to be appropriate.  This is relatively volatile data as
+                 * this is a sliding log file with the oldest entries being removed automatically.  Combine this with the
+                 * fact that old entries might not contain the same information and the effort of converting data which is
+                 * going to be removed quickly is unnecessary.
+                 */
+                if (node.hasNode("hippo:log")) {
+                    for(NodeIterator iter=node.getNode("hippo:log").getNodes(); iter.hasNext(); ) {
+                        iter.nextNode().remove();
+                    }
+                }
+            }
+        });
+    }
+}
