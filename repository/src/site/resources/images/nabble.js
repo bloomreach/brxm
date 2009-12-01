44632deb9f0ac6f79486729f636a3fd099b43f54
Berry van Halderen <b.vanhalderen@onehippo.com>
2009-12-01 23:51:02 +0000

    HREPTWO-3490: remove documentation currently on cms maintained documentation site
    svn path=/hippo-ecm/trunk/; revision=20969

--- repository/src/site/resources/images/nabble.js
+++
@@ -1,5 +0,0 @@
-// Create the link and load the javascript from Nabble.
-// This has to be done from Javascript, since Doxia removes the ID attribute from an anchor tag..
-
-document.write("<a href='http://n2.nabble.com/Hippo-CMS-7-User-List-f2274273.html' id='nabblelink'>Hippo CMS 7 Public Forums</a>");
-document.write("<script src='http://n2.nabble.com/embed/f2274273'></script>");
