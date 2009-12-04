9336a746db381b8b0024a67195c22b096b52aa42
Woonsan Ko <w.ko@onehippo.com>
2009-12-04 11:14:47 +0000

    HSTTWO-948: Providing utility method to return client ip address, requested server and port. Now, virtual host filter will use this.
    svn path=/ecm/site-toolkit/trunk/; revision=21015

--- site-toolkit/commons/src/main/java/org/hippoecm/hst/util/HstRequestUtils.java
+++ site-toolkit/commons/src/main/java/org/hippoecm/hst/util/HstRequestUtils.java
@@ -36,6 +36,11 @@ public class HstRequestUtils {
         
     }
     
+    /**
+     * Returns <CODE>HstRequest</CODE> object found in the servletRequest.
+     * @param servletRequest
+     * @return
+     */
     public static HstRequest getHstRequest(HttpServletRequest servletRequest) {
         HstRequest hstRequest = (HstRequest) servletRequest.getAttribute(ContainerConstants.HST_REQUEST);
         
@@ -46,6 +51,12 @@ public class HstRequestUtils {
         return hstRequest;
     }
     
+    /**
+     * Returns <CODE>HstResponse</CODE> object found in the servletRequest or servletResponse.
+     * @param servletRequest
+     * @param servletResponse
+     * @return
+     */
     public static HstResponse getHstResponse(HttpServletRequest servletRequest, HttpServletResponse servletResponse) {
         HstResponse hstResponse = (HstResponse) servletRequest.getAttribute(ContainerConstants.HST_RESPONSE);
         
@@ -56,22 +67,36 @@ public class HstRequestUtils {
         return hstResponse;
     }
     
-    public static String getPathInfo(HttpServletRequest request){
-        return getPathInfo(request,null);
+    /**
+     * Returns any extra path information associated with the URL the client sent when it made this request.
+     * The extra path information follows the servlet path but precedes the query string 
+     * and will start with a "/" character.
+     * This method extracts and decodes the path information from the request URI returned from
+     * <CODE>HttpServletRequest#getRequestURI()</CODE>.
+     * @param request
+     * @return
+     */
+    public static String getPathInfo(HttpServletRequest request) {
+        return getPathInfo(request, null);
     }
     
-    public static String getPathInfo(HttpServletRequest request, String characterEncoding){
-        String encodePathInfo = request.getRequestURI();
-        encodePathInfo = encodePathInfo.substring(request.getContextPath().length());
-        encodePathInfo = encodePathInfo.substring(request.getServletPath().length());
+    /**
+     * Returns any extra path information associated with the URL the client sent when it made this request.
+     * The extra path information follows the servlet path but precedes the query string 
+     * and will start with a "/" character.
+     * This method extracts and decodes the path information by the specified character encoding parameter
+     * from the request URI.
+     * @param request
+     * @param characterEncoding
+     * @return
+     */
+    public static String getPathInfo(HttpServletRequest request, String characterEncoding) {
+        String encodePathInfo = request.getRequestURI().substring(request.getContextPath().length() + request.getServletPath().length());
         
-        if (encodePathInfo == null) {
-            throw new IllegalArgumentException("Could not get pathinfo.");
-        }
-        
-        if(characterEncoding == null) {
+        if (characterEncoding == null) {
             characterEncoding = request.getCharacterEncoding();
-            if(characterEncoding == null) {
+            
+            if (characterEncoding == null) {
                 characterEncoding = "ISO-8859-1";
             }
         }
@@ -79,36 +104,106 @@ public class HstRequestUtils {
         try {
             return URLDecoder.decode(encodePathInfo, characterEncoding);
         } catch (UnsupportedEncodingException e) {
-            throw new IllegalArgumentException("Could not get pathinfo. ", e);
+            throw new IllegalArgumentException("Invalid character encoding: " + characterEncoding, e);
         }
         
     }
     
-    public static String getRequestServerName(HttpServletRequest request) {
-        // TODO: Possibly we can read some header information in a complicated proxied environment like the following example code:
-        //String xForwardedHost = request.getHeader("X-Forwarded-Host");
-        //if (xForwardedHost != null) {
-        //    String [] xForwardedHostNames = xForwardedHost.split(",");
-        //    if (xForwardedHostNames.length > 0) {
-        //        return xForwardedHostNames[0].trim();
-        //    }
-        //}
-        return request.getServerName();
+    /**
+     * Returns the original host informations requested by the client or the proxies 
+     * in the Host HTTP request headers.
+     * @param request
+     * @return
+     */
+    public static String [] getRequestHosts(HttpServletRequest request) {
+        String xfh = request.getHeader("X-Forwarded-Host");
+        
+        if (xfh != null) {
+            String [] hosts = xfh.split(",");
+            
+            for (int i = 0; i < hosts.length; i++) {
+                hosts[i] = hosts[i].trim();
+            }
+            
+            return hosts;
+        } else {
+            return new String [] { request.getHeader("Host") };
+        }
     }
     
+    /**
+     * Returns the original host information requested by the client.
+     * @param request
+     * @return
+     */
+    public static String getFarthestRequestHost(HttpServletRequest request) {
+        return getRequestHosts(request)[0];
+    }
+    
+    /**
+     * Returns the original host's server name requested by the client.
+     * @param request
+     * @return
+     */
+    public static String getRequestServerName(HttpServletRequest request) {
+        String requestHost = getFarthestRequestHost(request);
+        int offset = requestHost.indexOf(':');
+        
+        if (offset != -1) {
+            return requestHost.substring(0, offset);
+        } else {
+            return requestHost;
+        }
+    }
+    
+    /**
+     * Returns the original host' port number requested by the client.
+     * @param request
+     * @return
+     */
     public static int getRequestServerPort(HttpServletRequest request) {
-        // TODO: Possibly we can read some header information in a complicated proxied environment like the following example code:
-        //String xForwardedPort = request.getHeader("X-Forwarded-Port");
-        //if (xForwardedPort != null) {
-        //    String [] xForwardedPortNumbers = xForwardedPort.split(",");
-        //    if (xForwardedPortNumbers.length > 0) {
-        //        try {
-        //            return Integer.parseInt(xForwardedPortNumbers[0].trim());
-        //        } catch (NumberFormatException ignore) {
-        //        }
-        //    }
-        //}
-        return request.getServerPort();
+        String requestHost = getFarthestRequestHost(request);
+        int offset = requestHost.indexOf(':');
+        
+        if (offset != -1) {
+            return Integer.parseInt(requestHost.substring(offset + 1));
+        } else {
+            return ("https".equals(request.getScheme()) ? 443 : 80);
+        }
+    }
+    
+    /**
+     * Returns the remote host addresses related to this request.
+     * If there's any proxy server between the client and the server,
+     * then the proxy addresses are contained in the returned array.
+     * The lowest indexed element is the farthest downstream client and
+     * each successive proxy addresses are the next elements. 
+     * @param request
+     * @return
+     */
+    public static String [] getRemoteAddrs(HttpServletRequest request) {
+        String xff = request.getHeader("X-Forwarded-For");
+        
+        if (xff != null) {
+            String [] addrs = xff.split(",");
+            
+            for (int i = 0; i < addrs.length; i++) {
+                addrs[i] = addrs[i].trim();
+            }
+            
+            return addrs;
+        } else {
+            return new String [] { request.getRemoteAddr() };
+        }
+    }
+    
+    /**
+     * Returns the remote client address.
+     * @param request
+     * @return
+     */
+    public static String getFarthestRemoteAddr(HttpServletRequest request) {
+        return getRemoteAddrs(request)[0];
     }
     
 }
