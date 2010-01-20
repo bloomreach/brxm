042641e947fee585009f39d75a28f2774a9ab5bb
Ard Schrijvers <a.schrijvers@onehippo.com>
2010-01-20 13:38:07 +0000

    HSTTWO-432 added dummy cars config: Niels thx for helping with this!
    svn path=/ecm/site-toolkit/trunk/; revision=21368

--- site-toolkit/demosuite/site/src/main/java/org/hippoecm/hst/demo/components/DummyCarDocsCreator.java
+++ site-toolkit/demosuite/site/src/main/java/org/hippoecm/hst/demo/components/DummyCarDocsCreator.java
@@ -139,67 +139,65 @@ public class DummyCarDocsCreator {
     }
     
     
-    
     private void fill(List<Car> availableCars) {
-        addCars(availableCars, "Alfa Romeo", 8, 10000, 20000, 1, 200000, new String[] {"italian", "sportsmanlike", "said", "large company"});
-        addCars(availableCars, "Aston Martin", 2, 10000, 100000, 1, 200000, new String[] {"english", "david richards", "james bond"});
-        addCars(availableCars, "Audi", 7, 10000, 100000, 1, 200000, new String[] {"german", "ag", "high quality", "Ingolstadt", "volkswagon group", "August Horch", "expensive", "fast"});
-        addCars(availableCars, "Austin", 3, 10000, 100000, 1, 200000, new String[]{"english", "Birmingham", "Morris", "BMC"});
-        addCars(availableCars, "Bentley", 2, 10000, 200000, 1, 400000, new String[] {"english", "expensive", "fast", "luxury", "sportswagon"});
-        addCars(availableCars, "BMW", 7, 10000, 200000, 1, 200000, new String[] {"Bayerische Motoren Werke","high quality" ,"german", "expensive", "fast", "V12", "Rapp"});
+        addCars(availableCars, "Alfa Romeo", 8, 10000, 20000, 1, 200000, new String[] {"Italian", "FIAT Group", "sportsmanlike", "said", "large company"});
+        addCars(availableCars, "Aston Martin", 2, 10000, 100000, 1, 200000, new String[] {"English", "david richards", "james bond"});
+        addCars(availableCars, "Audi", 6, 10000, 100000, 1, 200000, new String[] {"German", "high quality", "Ingolstadt", "Volkswagon group", "August Horch", "expensive", "fast"});
+        addCars(availableCars, "Austin", 3, 10000, 100000, 1, 200000, new String[]{"English", "Birmingham", "Morris", "BMC"});
+        addCars(availableCars, "Bentley", 2, 30000, 400000, 1, 400000, new String[] {"English", "expensive", "fast", "luxury", "sportswagon"});
+        addCars(availableCars, "BMW", 6, 10000, 200000, 1, 200000, new String[] {"Bayerische Motoren Werke","high quality" ,"german", "expensive", "fast", "V12", "Rapp"});
         addCars(availableCars, "Buick", 2, 10000, 200000, 1, 200000, new String[] {"US", "General Motors", "big three US"});
-        addCars(availableCars, "Cadillac", 2, 10000, 200000, 1, 200000, new String[] {"US", "luxury", "high quality", "Henry Ford", "Leland", "William Durant"});
-        addCars(availableCars, "Chevrolet", 4, 10000, 40000, 1, 200000, new String[] {"US", "General Motors", "large quantity", "William Durant", "Louis Chevrolet", "big three US"});
-        addCars(availableCars, "Chrysler", 5, 10000, 50000, 1, 200000, new String[] {"US", "Walter Chrysler", "big three US", "Daimler-Benz", "DaimlerChrysler", "Cerberus", "Fiat"});
-        addCars(availableCars, "Citroën", 8, 10000, 30000, 1, 200000, new String[] {});
-        addCars(availableCars, "Daewoo", 5, 10000, 30000, 1, 200000);
-        addCars(availableCars, "Daihatsu", 5, 10000, 30000, 1, 200000);
-        addCars(availableCars, "Dodge", 5, 10000, 30000, 1, 200000);
-        addCars(availableCars, "Ferrari", 1, 40000, 300000, 1, 300000);
-        addCars(availableCars, "Fiat", 8, 3000, 25000, 1, 200000);
-        addCars(availableCars, "Ford", 7, 3000, 30000, 1, 200000, new String[] {"big three US"});
-        addCars(availableCars, "Honda", 7, 10000, 30000, 1, 200000);
-        addCars(availableCars, "Hummer", 1, 10000, 100000, 1, 200000);
-        addCars(availableCars, "Hyundai", 7, 10000, 25000, 1, 200000);
-        addCars(availableCars, "Jaguar", 4, 10000, 100000, 1, 200000);
-        addCars(availableCars, "Jeep", 4, 10000, 70000, 1, 200000);
-        addCars(availableCars, "Kia", 6, 1000, 20000, 1, 200000);
-        addCars(availableCars, "Lada", 5, 1000, 20000, 1, 200000);
-        addCars(availableCars, "Lamborghini", 3, 40000, 200000, 1, 200000);
-        addCars(availableCars, "Lancia", 5, 40000, 200000, 1, 200000);
-        addCars(availableCars, "Land Rover", 5, 10000, 20000, 1, 200000);
-        addCars(availableCars, "Lexus", 5, 4000, 20000, 1, 200000);
-        addCars(availableCars, "Lincoln", 5, 4000, 20000, 1, 200000);
-        addCars(availableCars, "Lotus", 5, 4000, 20000, 1, 200000);
-        addCars(availableCars, "Maserati", 3, 40000, 200000, 1, 200000);
-        addCars(availableCars, "Mazda", 8, 3000, 30000, 1, 200000);
-        addCars(availableCars, "Mercedes-Benz", 6, 3000, 40000, 1, 200000);
-        addCars(availableCars, "Mercury", 3, 3000, 40000, 1, 200000);
-        addCars(availableCars, "Mini", 6, 3000, 40000, 1, 200000);
-        addCars(availableCars, "Mitsubishi", 6, 3000, 20000, 1, 200000);
-        addCars(availableCars, "Nissan", 7, 3000, 20000, 1, 200000);
-        addCars(availableCars, "Opel", 8, 3000, 20000, 1, 200000);
-        addCars(availableCars, "Peugeot", 9, 3000, 20000, 1, 200000);
-        addCars(availableCars, "Pontiac", 5, 3000, 20000, 1, 200000);
-        addCars(availableCars, "Porsche", 4, 3000, 100000, 1, 200000);
-        addCars(availableCars, "Renault", 9, 3000, 20000, 1, 200000);
-        addCars(availableCars, "Rolls-Royce", 2, 3000, 100000, 1, 200000);
-        addCars(availableCars, "Rover", 4, 3000, 40000, 1, 200000);
-        addCars(availableCars, "Saab", 6, 3000, 60000, 1, 200000);
-        addCars(availableCars, "Seat", 6, 3000, 20000, 1, 200000);
-        addCars(availableCars, "Skoda", 6, 3000, 20000, 1, 200000);
-        addCars(availableCars, "Smart", 6, 3000, 20000, 1, 200000);
-        addCars(availableCars, "Ssangyong", 5, 3000, 20000, 1, 200000);
-        addCars(availableCars, "Subaru", 6, 3000, 20000, 1, 200000);
-        addCars(availableCars, "Suzuki", 6, 3000, 15000, 1, 200000);
-        addCars(availableCars, "Toyota", 8, 3000, 45000, 1, 200000);
-        addCars(availableCars, "Triumph", 5, 3000, 15000, 1, 200000);
-        addCars(availableCars, "Volkswagen", 9, 3000, 75000, 1, 200000);
-        addCars(availableCars, "Volvo", 8, 3000, 75000, 1, 200000);
-        addCars(availableCars, "Overige Auto's", 1, 3000, 75000, 1, 200000);
-       
-    }
+        addCars(availableCars, "Cadillac", 2, 10000, 200000, 1, 200000, new String[] {"US", "luxury", "high quality", "Henry Ford","Leland", "William Durant"});
+        addCars(availableCars, "Chevrolet", 4, 10000, 40000, 1,200000, new String[] {"US", "General Motors", "large quantity","William Durant", "Louis Chevrolet", "big three US"});
+        addCars(availableCars, "Chrysler", 4, 10000, 50000, 1, 200000,new String[] {"US", "Walter Chrysler", "big three US", "Daimler-Benz","DaimlerChrysler", "Cerberus", "Fiat"});
+        addCars(availableCars, "Citroën", 8, 10000, 30000, 1, 200000,new String[] {"French", "Mors", "Ssalomon", "cheap"});
+        addCars(availableCars, "Daewoo", 6, 10000, 30000, 1, 200000, new String[] {"South-Korean", "General Motors", "Saenara Motor", "GM Deawoo" });
+        addCars(availableCars, "Daihatsu", 6, 10000, 30000, 1, 200000, new String[] {"Japanese", "kei cars", " Hatsudoki", "Toyota", "cheap",});
+        addCars(availableCars, "Ferrari", 1, 40000, 900000, 1, 300000, new String[] {"Italian", "FIAT Group", "exclusive", "sportsmanlike", "fast", "Enzo Ferrari"});
+        addCars(availableCars, "Fiat", 8, 3000, 25000, 1, 200000, new String[] {"Italian", "FIAT Group", "cheap", "innovative", "large company"});
+        addCars(availableCars, "Ford", 7, 3000, 30000, 1, 200000, new String[] {"US", "big three US", "Ford Motor Company", "Volvo", "Lincoln", "Mercury", "large company", "innovative", "cheap" });
+        addCars(availableCars, "Honda", 7, 20000, 30000, 1, 200000, new String[] {"Japanese", "motorcycles", "large company", "cheap", "big Asian four"});
+        addCars(availableCars, "Hummer", 1, 50000, 300000, 1, 200000, new String[] {"US", "off-road", "General Motors", "army", "large company" });
+        addCars(availableCars, "Hyundai", 7, 10000, 25000, 1, 200000, new String[] {"South-Korean", "Kia", "large company", "big Asian four"});
+        addCars(availableCars, "Jaguar", 4, 10000, 100000, 1, 200000, new String[] {"English", "luxery", "Ford", "large company", "expensive", "high quality"});
+        addCars(availableCars, "Jeep", 4, 10000, 70000, 1, 200000, new String[] {"US", "off-road", "Chrysler", "Nekaf", "army", "SUV"});
+        addCars(availableCars, "Kia", 6, 1000, 20000, 1, 200000, new String[] {"South-Korean", "Hyundai Motor Company", "motorcycles", "large company", "cheap"});
+        addCars(availableCars, "Lada", 5, 1000, 20000, 1, 200000, new String[] {"Russian", "AvtoVAZ", "large company", "cheap"});
+        addCars(availableCars, "Lamborghini", 3, 90000, 400000, 1, 200000, new String[] {"Italian", "Ferruccio Lamborghini", "fast", "sportswagon", "expensive", "high quality", "Volkswagon group"});
+        addCars(availableCars, "Lancia", 5, 40000, 200000, 1, 200000, new String[] {"Italian", "Vincenzo Lancia", "FIAT Group", });
+        addCars(availableCars, "Land Rover", 5, 10000, 20000, 1, 200000, new String[] {"English", "SUV", "Tata Motors"});
+        addCars(availableCars, "Lexus", 5, 100000, 200000, 1, 200000, new String[] {"Japanese", "Toyota", "luxury", "fast", "expensive", "high quality"});
+        addCars(availableCars, "Lincoln", 5, 40000, 200000, 1, 200000, new String[] {"US", "Ford Motor Company", "luxury", "expensive"});
+        addCars(availableCars, "Lotus", 5, 40000, 200000, 1, 200000, new String[] {"English", "luxury", "sportswagon"});
+        addCars(availableCars, "Maserati", 3, 40000, 200000, 1, 200000, new String[] {"Italian", "sportswagon", "Alfa Romeo", "FIAT Group", "motorcycles"});
+        addCars(availableCars, "Mazda", 8, 3000, 300000, 1, 200000, new String[] {"Japanese", "motorcycles", "large company"});
+        addCars(availableCars, "Mercedes-Benz", 6, 3000, 40000, 1, 200000, new String[] {"German", "Daimler AG", "Team McLaren", "large company", "luxury"});
+        addCars(availableCars, "Mercury", 3, 3000, 40000, 1, 200000, new String[] {"US", "SUV", "army", "Ford Motor Company"});
+        addCars(availableCars, "Mini", 6, 3000, 40000, 1, 200000, new String[] {"English", "BMW", "very small"});
+        addCars(availableCars, "Mitsubishi", 6, 3000, 20000, 1, 200000, new String[] {"Japenese", "Mitsubishi Group", "large company"});
+        addCars(availableCars, "Nissan", 7, 3000, 30000, 1, 200000, new String[] {"Japanese", "large company", "Nissan Skyline", "big Asian four"});
+        addCars(availableCars, "Opel", 8, 3000, 40000, 1, 200000, new String[] {"German", "General Motors", "big three US", "Vauxhall", "motorcycles"});
+        addCars(availableCars, "Peugeot", 9, 3000, 120000, 1, 200000, new String[] {"French", "motorcycles", "large company", "bicycle"});
+        addCars(availableCars, "Pontiac", 5, 70000, 200000, 1, 200000, new String[] {"US", "General Motors", "large company", "sportswagon"});
+        addCars(availableCars, "Porsche", 4, 60000, 300000, 1, 200000, new String[] {"German", "sportswagon", "bicycle", "fast"});
+        addCars(availableCars, "Renault", 9, 3000, 20000, 1, 200000, new String[] {"French", "cheap", "Voiturette", "large company"});
+        addCars(availableCars, "Rolls-Royce", 2, 13000, 500000, 1, 200000, new String[] {"German", "BMW", "expensive", "luxury"});
+        addCars(availableCars, "Rover", 4, 3000, 40000, 1, 200000, new String[] {"English", "bankrupt", "bicycle", "motorcycles", "Land Rover"});
+        addCars(availableCars, "Saab", 6, 3000, 60000, 1, 200000, new String[] {"Swedish", "General Motors", "big three US", "sportswagon", "innovative" });
+        addCars(availableCars, "Seat", 6, 3000, 20000, 1, 200000, new String[] {"Spanish", "cheap", "Volkswagon group"});
+        addCars(availableCars, "Skoda", 6, 3000, 20000, 1, 200000, new String[] {"Czech", "bicycles", "cheap", "Volkswagon group"});
+        addCars(availableCars, "Smart", 6, 3000, 20000, 1, 200000, new String[] {"German", "smart centers", "Daimler AG", "very small", "large company"});
+        addCars(availableCars, "Ssangyong", 5, 3000, 20000, 1, 200000, new String[] {"South-Korean", "army", "jeep"});
+        addCars(availableCars, "Subaru", 6, 3000, 20000, 1, 200000, new String[] {"Japanese", "Fuji Heavy Industries", "innovative"});
+        addCars(availableCars, "Suzuki", 6, 3000, 15000, 1, 200000, new String[] {"Janapese", "motorcycles", "cheap"});
+        addCars(availableCars, "Toyota", 8, 3000, 45000, 1, 200000, new String[] {"Japanese", "large company", "Hybrid", "big Asian four"});
+        addCars(availableCars, "Triumph", 5, 3000, 15000, 1, 200000, new String[] {"English", "bicycles", "BMW", "sportswagon"});
+        addCars(availableCars, "Volkswagen", 9, 3000, 75000, 1, 200000, new String[] {"German", "large company", "cheap", "Volkswagon group"});
+        addCars(availableCars, "Volvo", 8, 3000, 75000, 1, 200000, new String[] {"Swedish", "Ford Motor Company", "luxury", "high quality"});
+        addCars(availableCars, "Overige Auto's", 1, 3000, 75000, 1, 200000, new String[] {"Imaginary", "random", "cheap"});
 
+    }
+    
     private void addCars(List<Car> availableCars, String brand, int ocurrences, double lowerPrice, double upperPrice, long lowerMileage, long upperMileage) {
         this.addCars(availableCars, brand, ocurrences, lowerPrice, upperPrice, lowerMileage, upperMileage, null);
     }
