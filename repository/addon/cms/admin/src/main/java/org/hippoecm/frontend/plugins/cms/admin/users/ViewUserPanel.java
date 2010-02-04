5db6088134a013e5829840001e5583db11be3476
Frank van Lankvelt <f.vanlankvelt@onehippo.com>
2010-02-04 15:58:14 +0000

    HREPTWO-16: add close bracket
    svn path=/hippo-ecm/trunk/; revision=21513

--- repository/addon/cms/admin/src/main/java/org/hippoecm/frontend/plugins/cms/admin/users/ViewUserPanel.java
+++ repository/addon/cms/admin/src/main/java/org/hippoecm/frontend/plugins/cms/admin/users/ViewUserPanel.java
@@ -151,7 +151,7 @@ public class ViewUserPanel extends AdminBreadCrumbPanel {
     private void deleteUser(IModel model) {
         User user = (User) model.getObject();
         if (user == null) {
-            log.info("No user model found when trying to delete user. Probably the Ok button was double clicked.";
+            log.info("No user model found when trying to delete user. Probably the Ok button was double clicked.");
             return;
         }
         String username = user.getUsername();
