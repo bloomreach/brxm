e36c0fbfcd648a05efa9aa7e5b9b8fadb6393fe1
Ard Schrijvers <a.schrijvers@onehippo.com>
2010-02-10 10:49:01 +0000

    HSTTWO-989 add the 7_3 archetype, which creates a skeleton against the 2.12.xx ecm versions
    svn path=/ecm/site-toolkit/trunk/; revision=21614

---
+++ site-toolkit/maven/archetype_7_3/src/main/resources/archetype-resources/site/src/main/java/beans/BaseDocument.java
@@ -0,0 +1,36 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+#set($hyphen = '-')
+#set($empty = '')
+/*
+ *  Copyright 2008 Hippo.
+ * 
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package ${package}.beans;
+
+import org.hippoecm.hst.content.beans.Node;
+import org.hippoecm.hst.content.beans.standard.HippoDocument;
+
+@Node(jcrType="${rootArtifactId.replace($hyphen,$empty)}:basedocument")
+public class BaseDocument extends HippoDocument {
+
+    public String getTitle() {
+        return getProperty("${rootArtifactId.replace($hyphen,$empty)}:title");
+    }
+    
+    public String getSummary() {
+        return getProperty("${rootArtifactId.replace($hyphen,$empty)}:summary");
+    }
+}
