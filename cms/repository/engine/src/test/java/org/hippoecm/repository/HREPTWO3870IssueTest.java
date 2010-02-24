bf9cd8d6dd5dbdd7deed29556ad55b4d16fb462e
Bart van der Schans <b.vanderschans@onehippo.com>
2010-02-24 00:27:15 +0000

    HREPTWO-3870: Typos in comments
    svn path=/hippo-ecm/trunk/; revision=21852

--- cms/repository/engine/src/test/java/org/hippoecm/repository/HREPTWO3870IssueTest.java
+++ cms/repository/engine/src/test/java/org/hippoecm/repository/HREPTWO3870IssueTest.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2008 Hippo.
+ *  Copyright 2010 Hippo.
  * 
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -108,7 +108,7 @@ public class HREPTWO3870IssueTest extends TestCase {
         try {
             session.save();
 
-            // test integrety
+            // test integrity
             facetsearch = session.getRootNode().getNode("test").getNode("tags");
             facetsearch.getPath();
             iter = facetsearch.getNodes();
