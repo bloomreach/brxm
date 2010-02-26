6a903a5eed28ab1a6abe41d968183277d7131c0a
Berry van Halderen <b.vanhalderen@onehippo.com>
2010-02-26 09:03:08 +0000

    HREPTWO-18: remove tabs, align copyrights, enter svn ids (forward port)
    svn path=/hippo-ecm/trunk/; revision=21951

--- cms/addon/cms/browse/src/main/java/org/hippoecm/frontend/plugins/cms/browse/section/OnEnterAjaxBehavior.java
+++ cms/addon/cms/browse/src/main/java/org/hippoecm/frontend/plugins/cms/browse/section/OnEnterAjaxBehavior.java
@@ -1,3 +1,18 @@
+/*
+ *  Copyright 2010 Hippo.
+ * 
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package org.hippoecm.frontend.plugins.cms.browse.section;
 
 import org.apache.wicket.Component;
@@ -13,6 +28,9 @@ import org.hippoecm.frontend.plugins.yui.header.IYuiContext;
 import org.hippoecm.frontend.plugins.yui.webapp.IYuiManager;
 
 public abstract class OnEnterAjaxBehavior extends AjaxFormSubmitBehavior {
+    @SuppressWarnings("unused")
+    private final static String SVN_ID = "$Id$";
+
     private static final long serialVersionUID = 1L;
 
     private IYuiContext _helper;
