a7a3027993726cad084e17cc1da082a24305a319
Berry van Halderen <b.vanhalderen@onehippo.com>
2010-04-07 14:31:21 +0000

    HREPTWO-4070: harden test by clearing repository data
    svn path=/hippo-ecm/trunk/; revision=22358

--- cms/addon/reviewed-action/test/src/test/java/org/hippoecm/repository/reviewedactions/VersioningWorkflowTest.java
+++ cms/addon/reviewed-action/test/src/test/java/org/hippoecm/repository/reviewedactions/VersioningWorkflowTest.java
@@ -18,6 +18,7 @@ package org.hippoecm.repository.reviewedactions;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
+import java.io.File;
 import java.rmi.RemoteException;
 import java.util.Calendar;
 import java.util.Map;
@@ -46,9 +47,29 @@ public class VersioningWorkflowTest extends ReviewedActionsWorkflowAbstractTest 
 
     static final String[] languages = new String[] { "aa", "ab", "ae", "af", "ak", "am", "an", "ar", "as", "av", "ay", "az", "ba", "be", "bg", "bh", "bi", "bm", "bn", "bo", "br", "bs", "ca", "ce", "ch", "co", "cr", "cs", "cu", "cv", "cy", "da", "de", "dv", "dz", "ee", "el", "en", "eo", "es", "et", "eu", "fa", "ff", "fi", "fj", "fo", "fr", "fy", "ga", "gd", "gl", "gn", "gu", "gv", "ha", "he", "hi", "ho", "hr", "ht", "hu", "hy", "hz", "ia", "id", "ie", "ig", "ii", "ik", "io", "is", "it", "iu", "ja", "jv", "ka", "kg", "ki", "kj", "kk", "kl", "km", "kn", "kr", "ks", "ku", "kv", "kw", "ky", "la", "lb", "lg", "li", "ln", "lo", "lt", "lu", "lv", "mg", "mh", "mi", "mk", "ml", "mn", "mo", "mr", "ms", "mt", "my", "na", "nb", "nd", "ne", "ng", "nl", "nn", "no", "nr", "nv", "ny", "oc", "oj", "om", "or", "os", "pa", "pi", "pl", "ps", "pt", "qu", "rm", "rn", "ro", "ru", "rw", "sa", "sc", "sd", "se", "sg", "sh", "si", "sk", "sl", "sm", "sn", "so", "sq", "sr", "ss", "st", "su", "sv", "sw", "ta", "te", "tg", "th", "ti", "tk", "tl", "tn", "to", "tr", "ts", "tt", "tw", "ty", "ug", "uk", "ur", "uz", "ve", "vi", "vo", "wa", "wo", "xh", "yi", "yo", "za", "zh", "zu" };
 
+    static private void delete(File path) {
+        if(path.exists()) {
+            if(path.isDirectory()) {
+                File[] files = path.listFiles();
+                for(int i=0; i<files.length; i++)
+                    delete(files[i]);
+            }
+            path.delete();
+        }
+    }
+
+    static private void clear() {
+        String[] files = new String[] { ".lock", "repository", "version", "workspaces" };
+        for(int i=0; i<files.length; i++) {
+            File file = new File(files[i]);
+            delete(file);
+        }
+    }
+
     @Override
     @Before
     public void setUp() throws Exception {
+        clear();
         super.setUp();
 
         Node node, root = session.getRootNode();
