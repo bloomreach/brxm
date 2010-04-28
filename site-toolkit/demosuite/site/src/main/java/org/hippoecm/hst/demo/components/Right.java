0d81773cfbfd8aab684ce6a08f4a85289e6b7de4
Ard Schrijvers <a.schrijvers@onehippo.com>
2010-04-28 15:38:40 +0000

    HSTTWO-1066 remove old ocm
    svn path=/ecm/site-toolkit/trunk/; revision=22601

--- site-toolkit/demosuite/site/src/main/java/org/hippoecm/hst/demo/components/Right.java
+++ site-toolkit/demosuite/site/src/main/java/org/hippoecm/hst/demo/components/Right.java
@@ -17,10 +17,10 @@ package org.hippoecm.hst.demo.components;
 
 import org.hippoecm.hst.component.support.bean.BaseHstComponent;
 import org.hippoecm.hst.content.beans.standard.HippoBean;
+import org.hippoecm.hst.content.beans.standard.HippoFolder;
 import org.hippoecm.hst.core.component.HstComponentException;
 import org.hippoecm.hst.core.component.HstRequest;
 import org.hippoecm.hst.core.component.HstResponse;
-import org.hippoecm.hst.jackrabbit.ocm.HippoStdFolder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -34,7 +34,7 @@ public class Right extends BaseHstComponent{
         super.doBeforeRender(request, response);
         HippoBean n = this.getContentBean(request);
 
-        request.setAttribute("folders", ((HippoStdFolder)n).getFolders());
+        request.setAttribute("folders", ((HippoFolder)n).getFolders());
         request.setAttribute("curnode", n);
         
     }
