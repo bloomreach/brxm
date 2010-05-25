5a4aff725134836c7eccd04f78c512a7cf0b4f7c
Woonsan Ko <w.ko@onehippo.com>
2010-05-25 14:43:08 +0000

    HSTTWO-1100: Adding node type string filtering argument in HstQueryManager.createQuery()
    svn path=/ecm/site-toolkit/trunk/; revision=22874

--- site-toolkit/api/src/main/java/org/hippoecm/hst/content/beans/query/HstQueryManager.java
+++ site-toolkit/api/src/main/java/org/hippoecm/hst/content/beans/query/HstQueryManager.java
@@ -41,7 +41,19 @@ public interface HstQueryManager {
      * @throws QueryException
      */
     HstQuery createQuery(Node scope, Class<? extends HippoBean> filterBean, boolean includeSubTypes) throws QueryException;
-
+    
+    /**
+     * Creates a query, with scope and Filter for node types. If includeSubTypes is <code>true</code>,
+     * the result may also contain node types whose primary type is a subtype of the filter node type. 
+     * 
+     * @param scope
+     * @param nodeType
+     * @param includeSubTypes
+     * @return a new <code>{@link HstQuery}</code> with scope & filter
+     * @throws QueryException
+     */
+    HstQuery createQuery(Node scope, String nodeType, boolean includeSubTypes) throws QueryException;
+    
     /**
      * Creates a empty query, with scope
      * @param scope 
@@ -74,16 +86,39 @@ public interface HstQueryManager {
     HstQuery createQuery(Node scope, Class<? extends HippoBean>... filterBeans) throws QueryException;
     
     /**
+     * Creates a query, with the scope HippoBean and with a Filter that filters to only return HippoBeans of the types that are 
+     * added as variable arguments. It is not possible to retrieve subtypes of the applied primary node types. If needed, use 
+     * {@link #createQuery(HstRequestContext, HippoBean, Class, boolean)} instead.
+     * 
+     * @param scope
+     * @param primaryNodeTypes
+     * @return a new <code>{@link HstQuery}</code> with scope & filter on jcr primary nodetype of the filterBean
+     * @throws QueryException
+     */
+    HstQuery createQuery(Node scope, String ... primaryNodeTypes) throws QueryException;
+    
+    /**
      * Creates a query, with a scope and with a Filter that filters to only return HippoBeans of the types that are 
      * added as variable arguments. It is not possible to retrieve subtypes of the applied filterBeans with this method. If needed, use 
      * {@link #createQuery(HstRequestContext, Node, Class, boolean)} instead.
      * 
      * @param scope
-     * @param varargs filterBean
+     * @param filterBean
      * @return a new <code>{@link HstQuery}</code> with scope and filter on jcr primary nodetype of the filterBean
      * @throws QueryException
      */
     HstQuery createQuery(HippoBean scope, Class<? extends HippoBean>... filterBean) throws QueryException;
     
-
+    /**
+     * Creates a query, with a scope and with a Filter that filters to only return HippoBeans of the types that are 
+     * added as variable arguments. It is not possible to retrieve subtypes of the applied primary node types with this method. If needed, use 
+     * {@link #createQuery(HstRequestContext, Node, Class, boolean)} instead.
+     * 
+     * @param scope
+     * @param primaryNodeTypes
+     * @return a new <code>{@link HstQuery}</code> with scope and filter on jcr primary nodetype of the filterBean
+     * @throws QueryException
+     */
+    HstQuery createQuery(HippoBean scope, String ... primaryNodeTypes) throws QueryException;
+    
 }
