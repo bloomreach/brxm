d4aa88eb3ed9ca06a943e98647326e178c1331fc
Berry van Halderen <b.vanhalderen@onehippo.com>
2010-05-30 23:32:21 +0000

    HREPTWO-18: copyright headers, svn ids
    svn path=/hippo-ecm/trunk/; revision=22932

--- repository/addon/reviewed-action/frontend/src/main/java/org/hippoecm/frontend/plugins/reviewedactions/UnpublishedReferenceNodeProvider.java
+++ repository/addon/reviewed-action/frontend/src/main/java/org/hippoecm/frontend/plugins/reviewedactions/UnpublishedReferenceNodeProvider.java
@@ -32,6 +32,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public final class UnpublishedReferenceNodeProvider implements ISortableDataProvider<Node> {
+    @SuppressWarnings("unused")
+    private final static String SVN_ID = "$Id$";
+
     private static final long serialVersionUID = 1L;
 
     static final Logger log = LoggerFactory.getLogger(UnpublishedReferenceNodeProvider.class);
