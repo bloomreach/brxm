781c11fba16b04496847152f3dc340d3380ef6fc
Frank van Lankvelt <f.vanlankvelt@onehippo.com>
2010-06-22 07:25:35 +0000

    HREPTWO-16: fix build
    Use plugintest base; wicket session is used by node model.
    
    svn path=/hippo-ecm/trunk/; revision=23251

--- repository/frontend/test/src/test/java/org/hippoecm/frontend/i18n/model/NodeTranslatorTest.java
+++ repository/frontend/test/src/test/java/org/hippoecm/frontend/i18n/model/NodeTranslatorTest.java
@@ -18,16 +18,43 @@ package org.hippoecm.frontend.i18n.model;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import java.util.Iterator;
+
 import javax.jcr.Node;
 import javax.jcr.version.Version;
 import javax.jcr.version.VersionHistory;
 import javax.jcr.version.VersionIterator;
 
+import org.apache.wicket.Session;
+import org.apache.wicket.model.IModel;
+import org.hippoecm.frontend.PluginTest;
 import org.hippoecm.frontend.model.JcrNodeModel;
-import org.hippoecm.repository.TestCase;
+import org.hippoecm.frontend.model.event.IObservable;
+import org.hippoecm.frontend.model.event.IObserver;
 import org.junit.Test;
 
-public class NodeTranslatorTest extends TestCase {
+public class NodeTranslatorTest extends PluginTest {
+    private static final class NodeNameObserver implements IObserver {
+        private static final long serialVersionUID = 1L;
+
+        private final IModel<String> nodeName;
+
+        String name;
+        
+        private NodeNameObserver(IModel<String> nodeName) {
+            this.nodeName = nodeName;
+            this.name = nodeName.getObject();
+        }
+
+        public IObservable getObservable() {
+            return (IObservable) nodeName;
+        }
+
+        public void onEvent(Iterator events) {
+            this.name = nodeName.getObject();
+        }
+    }
+
     @SuppressWarnings("unused")
     private final static String SVN_ID = "$Id$";
 
@@ -63,4 +90,27 @@ public class NodeTranslatorTest extends TestCase {
         assertTrue(version.getNode("jcr:frozenNode").isNodeType("mix:referenceable"));
     }
 
+    @Test
+    public void testObservation() throws Exception {
+        Node parent = session.getRootNode().addNode("test", "nt:unstructured");
+        Node child = parent.addNode("child", "nt:unstructured");
+        child.addMixin("hippo:translated");
+        Node translation = child.addNode("hippo:translation", "hippo:translation");
+        translation.setProperty("hippo:language", Session.get().getLocale().getLanguage());
+        translation.setProperty("hippo:message", "kind");
+        session.save();
+        
+        NodeTranslator nt = new NodeTranslator(new JcrNodeModel(child));
+        final IModel<String> nodeName = nt.getNodeName();
+        assertEquals("kind", nodeName.getObject());
+
+        NodeNameObserver observer = new NodeNameObserver(nodeName);
+        context.registerService(observer, IObserver.class.getName());
+
+        translation.setProperty("hippo:message", "kindje");
+        nodeName.detach();
+        home.processEvents();
+
+        assertEquals("kindje", observer.name);
+    }
 }
