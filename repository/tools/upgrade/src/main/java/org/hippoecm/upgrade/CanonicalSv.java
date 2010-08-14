9ad028d9808a8a221bcde2707e53f5a90863030d
Frank van Lankvelt <f.vanlankvelt@onehippo.com>
2010-08-14 08:07:33 +0000

    HREPTWO-16: add a JSON output format
    svn path=/hippo-ecm/trunk/; revision=23695

--- repository/tools/upgrade/src/main/java/org/hippoecm/upgrade/CanonicalSv.java
+++ repository/tools/upgrade/src/main/java/org/hippoecm/upgrade/CanonicalSv.java
@@ -17,19 +17,14 @@ package org.hippoecm.upgrade;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.OutputStream;
-import java.io.PrintStream;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Set;
-import java.util.SortedMap;
-import java.util.TreeMap;
-import java.util.TreeSet;
+import java.util.LinkedList;
+import java.util.List;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
+import org.hippoecm.upgrade.Value.ValueType;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
@@ -41,21 +36,36 @@ public class CanonicalSv {
     private final static String SVN_ID = "$Id$";
 
     public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException {
-        if (args.length >= 1 && (args[0].startsWith("-h") || args[0].startsWith("--h"))) {
-            System.err.println("Generate a canonical representation from a system view xml file.");
-            System.err.println("  Child nodes and properties are ordered according to name and, if necessary, value.");
-            System.err.println("  Ordering due to orderable node types is ignored.");
-            System.err.println("");
-            System.err.println("Usage: CanonicalSv [<file>]");
-            System.err.println("  Where <file> is an xml file in the system view JCR format.");
-            System.err.println("  With no file argument, standard input is used.");
-            System.exit(0);
+        File file = null;
+        AbstractOutput output = new TextOutput(System.out);
+        for (int i = 0; i < args.length; i++) {
+            if (args[i].startsWith("-")) {
+                if (args[i].startsWith("-j")) {
+                    output = new JsonOutput(System.out);
+                } else if (args[i].startsWith("-h")) {
+                    System.err.println("Usage: CanonicalSv [OPTIONS] [<file>]");
+                    System.err.println("Generate a canonical representation from a system view xml file.");
+                    System.err.println("Child nodes and properties are ordered according to name and, if necessary, value.");
+                    System.err.println("Ordering due to orderable node types is ignored.");
+                    System.err.println();
+                    System.err.println("  -h print this help text");
+                    System.err.println("  -j output JSON formatted string");
+                    System.err.println();
+                    System.err.println("<file> is an xml file in the system view JCR format.");
+                    System.err.println("With no file argument, standard input is used.");
+                    System.exit(0);
+                }
+            } else if (file == null) {
+                file = new File(args[i]);
+            } else {
+                System.err.print("Two input files found; only one is supported");
+                System.exit(-1);
+            }
         }
         DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
         DocumentBuilder db = dbf.newDocumentBuilder();
         Document doc;
-        if (args.length >= 1) {
-            File file = new File(args[0]);
+        if (file != null) {
             doc = db.parse(file);
         } else {
             doc = db.parse(System.in);
@@ -63,82 +73,10 @@ public class CanonicalSv {
         doc.getDocumentElement().normalize();
         Item root = new Item("root");
         process(doc.getDocumentElement(), root);
-        Output output = new Output(System.out);
-        output.render(root, "");
+        output.render(root);
     }
 
     static String INDENT = "  ";
-
-    static class Output {
-
-        final PrintStream out;
-
-        Output() {
-            out = System.out;
-        }
-
-        Output(OutputStream stream) {
-            out = new PrintStream(stream);
-        }
-
-        void render(Item item, String prefix) {
-            out.println(prefix + "+ " + item.name + " [" + item.get("jcr:primaryType") + "]");
-            for (Map.Entry<String, String> entry : item.entrySet()) {
-                if ("jcr:primaryType".equals(entry.getKey())) {
-                    continue;
-                }
-                out.println(prefix + INDENT + "- " + entry.getKey() + ": " + entry.getValue());
-            }
-            for (Map.Entry<String, Set<Item>> entry : item.children.entrySet()) {
-                for (Item child : entry.getValue()) {
-                    render(child, prefix + INDENT);
-                }
-            }
-        }
-    }
-
-    static class Item extends TreeMap<String, String> implements Comparable<Item> {
-        String name;
-        SortedMap<String, Set<Item>> children = new TreeMap<String, Set<Item>>();
-
-        Item(String name) {
-            this.name = name;
-        }
-
-        void add(Item child) {
-            if (!children.containsKey(child.name)) {
-                children.put(child.name, new TreeSet<Item>());
-            }
-            children.get(child.name).add(child);
-        }
-
-        public int compareTo(Item o) {
-            if (o == this) {
-                return 0;
-            }
-            Iterator<Map.Entry<String, String>> thisIter = entrySet().iterator();
-            Iterator<Map.Entry<String, String>> thatIter = o.entrySet().iterator();
-            while (thisIter.hasNext()) {
-                if (!thatIter.hasNext()) {
-                    return 1;
-                }
-                Map.Entry<String, String> thisEntry = thisIter.next();
-                Map.Entry<String, String> thatEntry = thatIter.next();
-                int keyCmp = thisEntry.getKey().compareTo(thatEntry.getKey());
-                if (keyCmp != 0) {
-                    return keyCmp;
-                }
-                int valueCmp = thisEntry.getValue().compareTo(thatEntry.getValue());
-                if (valueCmp != 0) {
-                    return valueCmp;
-                }
-            }
-            if (thatIter.hasNext()) {
-                return -1;
-            }
-            return 1;
-        }
-    }
 
     static void process(Element xmlRoot, Item jcrRoot) {
         Item item = new Item(xmlRoot.getAttribute("sv:name"));
@@ -150,23 +88,18 @@ public class CanonicalSv {
                 Element element = (Element) node;
                 if (element.getTagName().equals("sv:property")) {
                     String name = element.getAttribute("sv:name");
-                    StringBuilder value = new StringBuilder();
-                    boolean first = true;
-                    NodeList values = element.getElementsByTagName("sv:value");
-                    for (int j = 0; j < values.getLength(); j++) {
-                        Node valueNode = values.item(j);
+                    ValueType type = ValueType.fromString(element.getAttribute("sv:type"));
+                    List<Value> values = new LinkedList<Value>();
+                    NodeList valueNodes = element.getElementsByTagName("sv:value");
+                    for (int j = 0; j < valueNodes.getLength(); j++) {
+                        Node valueNode = valueNodes.item(j);
                         if (valueNode instanceof Element) {
                             Element valueEl = (Element) valueNode;
                             String content = valueEl.getTextContent();
-                            if (first) {
-                                first = false;
-                            } else {
-                                value.append(' ');
-                            }
-                            value.append(content);
+                            values.add(new Value(content, type));
                         }
                     }
-                    item.put(name, value.toString());
+                    item.put(name, values.toArray(new Value[values.size()]));
                 } else if (element.getTagName().equals("sv:node")) {
                     process(element, item);
                 }
