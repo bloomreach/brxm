071ab295b9b4b8e30ecf36b177d82259102234fa
Ard Schrijvers <a.schrijvers@onehippo.com>
2010-09-13 21:14:26 +0000

    HSTTWO-1175 do not return resultset as a folder for facet navigation root bean
    svn path=/ecm/site-toolkit/trunk/; revision=23916

--- site-toolkit/content-beans/src/main/java/org/hippoecm/hst/content/beans/standard/facetnavigation/AbstractHippoFacetChildNavigation.java
+++ site-toolkit/content-beans/src/main/java/org/hippoecm/hst/content/beans/standard/facetnavigation/AbstractHippoFacetChildNavigation.java
@@ -1,44 +1,34 @@
+/*
+ *  Copyright 2010 Hippo.
+ * 
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package org.hippoecm.hst.content.beans.standard.facetnavigation;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.hippoecm.hst.content.beans.standard.HippoFacetChildNavigationBean;
-import org.hippoecm.hst.content.beans.standard.HippoFolderBean;
-import org.hippoecm.hst.content.beans.standard.HippoResultSetBean;
-import org.hippoecm.repository.api.HippoNodeType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * The base class for child item of faceted navigation
  */
 abstract public class AbstractHippoFacetChildNavigation extends HippoFacetNavigation implements HippoFacetChildNavigationBean {
-
-    public Long getCount() {
-        return this.getProperty(HippoNodeType.HIPPO_COUNT);
-    }
-
-    public HippoResultSetBean getResultSet() {
-        return this.getBean(HippoNodeType.HIPPO_RESULTSET);
-    }
     
-    /**
-     * AbstractHippoFacetChildNavigation does not return the HippoResultSetBean as a folder, as this is normally a 
-     * folder that is not wanted to be displayed, and can be accessed through {@link #getResultSet()} already
-     */
-    @Override
-    public List<HippoFolderBean> getFolders(boolean sorted){
-        List<HippoFolderBean> folders = super.getFolders(sorted);
-        
-        List<HippoFolderBean> remove = new ArrayList<HippoFolderBean>();
-        for(HippoFolderBean folder : folders) {
-            if(folder instanceof HippoResultSetBean) {
-                remove.add(folder);
-            }
-        }
-        this.hippoFolders.removeAll(remove);
-        return this.hippoFolders;
-    }
-
+    private static Logger log = LoggerFactory.getLogger(AbstractHippoFacetChildNavigation.class);
+  
+  
     /**
      * The ancestors and self of this AbstractHippoFacetChildNavigation. Note that only the ancestors of the same bean type are returned. 
      * @return the ancestors (only ancestors of the same type as 'this') + self list of AbstractHippoFacetChildNavigation's
