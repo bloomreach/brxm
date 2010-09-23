e63b79e1a15505cc6c015c985b88ee963daa2f51
Woonsan Ko <w.ko@onehippo.com>
2010-09-23 01:05:30 +0000

    HSTTWO-1171: Adding default jcr authenticationProvider impl
    svn path=/ecm/site-toolkit/trunk/; revision=24025

---
+++ site-toolkit/components/security/src/main/java/org/hippoecm/hst/security/impl/JcrAuthenticationProvider.java
@@ -0,0 +1,162 @@
+/*
+ *  Copyright 2008 Hippo.
+ * 
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.hippoecm.hst.security.impl;
+
+import java.text.MessageFormat;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.jcr.Credentials;
+import javax.jcr.LoginException;
+import javax.jcr.NodeIterator;
+import javax.jcr.Repository;
+import javax.jcr.RepositoryException;
+import javax.jcr.Session;
+import javax.jcr.SimpleCredentials;
+import javax.jcr.query.Query;
+import javax.jcr.query.QueryResult;
+
+import org.hippoecm.hst.security.AuthenticationProvider;
+import org.hippoecm.hst.security.Role;
+import org.hippoecm.hst.security.TransientRole;
+import org.hippoecm.hst.security.TransientUser;
+import org.hippoecm.hst.security.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * JcrAuthenticationProvider
+ * @version $Id$
+ */
+public class JcrAuthenticationProvider implements AuthenticationProvider {
+    
+    static final Logger log = LoggerFactory.getLogger(JcrAuthenticationProvider.class);
+    
+    // TODO: Find out the proper query instead of using group nodes.
+    public static final String DEFAULT_ROLES_OF_USER_QUERY = 
+        "//element(*, hipposys:group)[(@hipposys:members = ''{0}'' or @hipposys:members = ''*'') and @hipposys:securityprovider = ''internal'']";
+    
+    private Repository systemRepository;
+    private Credentials systemCreds;
+    private Repository userRepository;
+    
+    private String rolesOfUserQuery = DEFAULT_ROLES_OF_USER_QUERY;
+    private String queryLanguage = Query.XPATH;
+    
+    public JcrAuthenticationProvider(Repository systemRepository, Credentials systemCreds, Repository userRepository) {
+        this.systemRepository = systemRepository;
+        this.systemCreds = systemCreds;
+        this.userRepository = userRepository;
+    }
+    
+    public void setRolesOfUserQuery(String rolesOfUserQuery) {
+        this.rolesOfUserQuery = rolesOfUserQuery;
+    }
+    
+    public void setQueryLanguage(String queryLanguage) {
+        this.queryLanguage = queryLanguage;
+    }
+    
+    public User authenticate(String userName, char[] password) throws SecurityException {
+        Session session = null;
+        SimpleCredentials creds = new SimpleCredentials(userName, password);
+        
+        try {
+            session = userRepository.login(new SimpleCredentials(userName, password));
+        } catch (LoginException e) {
+            throw new SecurityException(e.getLocalizedMessage(), e);
+        } catch (RepositoryException e) {
+            throw new SecurityException(e.getLocalizedMessage(), e);
+        } finally {
+            if (session != null) {
+                try {
+                    session.logout();
+                } catch (Exception ignore) {
+                }
+            }
+        }
+        
+        return new TransientUser(creds.getUserID());
+    }
+    
+    public Set<Role> getRoles(User user) throws SecurityException {
+        Set<Role> roleSet = null;
+        
+        try {
+            Set<String> roleNameSet = getRoleNamesOfUser(user);
+            roleSet = new HashSet<Role>();
+            
+            for (String roleName : roleNameSet) {
+                roleSet.add(new TransientRole(roleName));
+            }
+        } catch (LoginException e) {
+            throw new SecurityException("System repository throws LoginException: " + e, e);
+        } catch (RepositoryException e) {
+            throw new SecurityException("System repository throws RepositoryException: " + e, e);
+        }
+        
+        if (roleSet == null) {
+            roleSet = Collections.emptySet();
+        }
+        
+        return roleSet;
+    }
+    
+    protected Set<String> getRoleNamesOfUser(User user) throws LoginException, RepositoryException {
+        Set<String> roleNameSet = null;
+        Session session = null;
+        
+        try {
+            if (systemCreds != null) {
+                session = systemRepository.login(systemCreds);
+            } else {
+                session = systemRepository.login();
+            }
+            
+            String statement = MessageFormat.format(rolesOfUserQuery, user.getName());
+            
+            if (log.isDebugEnabled()) {
+                log.debug("Searching roles of user with query: " + statement);
+            }
+            
+            Query q = session.getWorkspace().getQueryManager().createQuery(statement, queryLanguage);
+            QueryResult result = q.execute();
+            NodeIterator nodeIt = result.getNodes();
+            
+            roleNameSet = new HashSet<String>();
+            
+            while (nodeIt.hasNext()) {
+                String roleName = nodeIt.nextNode().getName();
+                roleNameSet.add(roleName);
+            }
+        } finally {
+            if (session != null) {
+                try {
+                    session.logout();
+                } catch (Exception ignore) {
+                }
+            }
+        }
+        
+        if (roleNameSet == null) {
+            roleNameSet = Collections.emptySet();
+        }
+        
+        return roleNameSet;
+    }
+    
+}
---
+++ site-toolkit/components/security/src/test/java/org/hippoecm/hst/security/impl/TestJcrAuthenticationProvider.java
@@ -0,0 +1,81 @@
+/*
+ *  Copyright 2008 Hippo.
+ * 
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.hippoecm.hst.security.impl;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.util.Set;
+
+import javax.jcr.Credentials;
+import javax.jcr.Repository;
+import javax.jcr.SimpleCredentials;
+
+import org.hippoecm.hst.security.AuthenticationProvider;
+import org.hippoecm.hst.security.Role;
+import org.hippoecm.hst.security.TransientRole;
+import org.hippoecm.hst.security.User;
+import org.hippoecm.hst.test.AbstractHstTestCase;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * TestJcrAuthenticationProvider
+ * @version $Id$
+ */
+public class TestJcrAuthenticationProvider extends AbstractHstTestCase {
+    
+    private AuthenticationProvider authenticationProvider;
+
+    @Override
+    @Before
+    public void setUp() throws Exception {
+        super.setUp();
+
+        Repository systemRepo = getRepository();
+        Credentials systemCreds = new SimpleCredentials("admin", "admin".toCharArray());
+        Repository userRepo = getRepository();
+        
+        authenticationProvider = new JcrAuthenticationProvider(systemRepo, systemCreds, userRepo);
+    }
+
+    @Override
+    @After
+    public void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    @Test
+    public void testAuthentication() {
+        User user = null;
+        
+        try {
+            user = authenticationProvider.authenticate("admin", "admin".toCharArray());
+            assertEquals("admin", user.getName());
+        } catch (SecurityException e) {
+            fail("Failed to log on by admin: " + e);
+        }
+        
+        Set<Role> roleSet = authenticationProvider.getRoles(user);
+        assertTrue(roleSet.contains(new TransientRole("everybody")));
+        assertTrue(roleSet.contains(new TransientRole("admin")));
+        assertFalse(roleSet.contains(new TransientRole("editor")));
+    }
+
+}
