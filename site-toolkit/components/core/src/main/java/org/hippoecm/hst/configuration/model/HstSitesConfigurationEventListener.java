bloomreach/brxm

commit d51d20690e4de33036c2f700e4bf156a40c68c25

HSTTWO-1186 mainly better naming
svn path=/ecm/site-toolkit/trunk/; revision=24017

site-toolkit/components/core/src/main/java/org/hippoecm/hst/configuration/model/HstSitesConfigurationEventListener.java
@@ -13,7 +13,7 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-package org.hippoecm.hst.configuration.hosting;
+package org.hippoecm.hst.configuration.model;
 
 import javax.jcr.RepositoryException;
 import javax.jcr.observation.Event;
@@ -24,15 +24,15 @@ import org.hippoecm.hst.core.jcr.GenericEventListener;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class VirtualHostsConfigurationEventListener extends GenericEventListener {
+public class HstSitesConfigurationEventListener extends GenericEventListener {
     
-    static Logger log = LoggerFactory.getLogger(VirtualHostsConfigurationEventListener.class);
+    static Logger log = LoggerFactory.getLogger(HstSitesConfigurationEventListener.class);
     
-    protected VirtualHostsManager virtualHostsManager;
+    protected HstManager hstManager;
     protected HstComponentRegistry componentRegistry;
     
-    public void setVirtualHostsManager(VirtualHostsManager virtualHostsManager) {
-        this.virtualHostsManager = virtualHostsManager;
+    public void setVirtualHostsManager(HstManager hstManager) {
+        this.hstManager = hstManager;
     }
     
     public void setComponentRegistry(HstComponentRegistry componentRegistry) {
@@ -69,6 +69,6 @@ public class VirtualHostsConfigurationEventListener extends GenericEventListener
     
     private void doInvalidation(String path) {
         this.componentRegistry.unregisterAllComponents();
-        this.virtualHostsManager.invalidate(path);
+        this.hstManager.invalidate(path);
     }
 }

site-toolkit/api/src/main/java/org/hippoecm/hst/configuration/hosting/SiteMount.java
@@ -19,6 +19,7 @@ import java.util.Set;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.hippoecm.hst.configuration.model.HstManager;
 import org.hippoecm.hst.configuration.site.HstSite;
 import org.hippoecm.hst.core.request.HstSiteMapMatcher;
 import org.hippoecm.hst.core.request.ResolvedSiteMapItem;
@@ -220,7 +221,7 @@ public interface SiteMount {
     String getNamedPipeline();
 
     /**
-     * This is a shortcut method fetching the HstSiteMapMatcher from the backing {@link VirtualHostsManager}
+     * This is a shortcut method fetching the HstSiteMapMatcher from the backing {@link HstManager}
      * @return the HstSiteMapMatcher implementation
      */
     HstSiteMapMatcher getHstSiteMapMatcher();

site-toolkit/api/src/main/java/org/hippoecm/hst/configuration/hosting/VirtualHosts.java
@@ -15,9 +15,7 @@
  */
 package org.hippoecm.hst.configuration.hosting;
 
-import java.util.List;
-
-import org.hippoecm.hst.configuration.site.HstSite;
+import org.hippoecm.hst.configuration.model.HstManager;
 import org.hippoecm.hst.core.container.HstContainerURL;
 import org.hippoecm.hst.core.request.ResolvedSiteMapItem;
 import org.hippoecm.hst.core.request.ResolvedSiteMount;
@@ -31,9 +29,9 @@ import org.hippoecm.hst.core.request.ResolvedVirtualHost;
 public interface VirtualHosts {
 
     /**
-     * @return the {@link VirtualHostsManager} for this VirtualHosts object
+     * @return the {@link HstManager} for this VirtualHosts object
      */
-    VirtualHostsManager getVirtualHostsManager();
+    HstManager getHstManager();
     
     /**
      * 

site-toolkit/api/src/main/java/org/hippoecm/hst/configuration/model/HstManager.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2008 Hippo.
+ *  Copyright 2010 Hippo.
  * 
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -13,30 +13,30 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-package org.hippoecm.hst.configuration.hosting;
+package org.hippoecm.hst.configuration.model;
 
+import org.hippoecm.hst.configuration.hosting.VirtualHosts;
 import org.hippoecm.hst.core.component.HstURLFactory;
 import org.hippoecm.hst.core.container.RepositoryNotAvailableException;
 import org.hippoecm.hst.core.request.HstSiteMapMatcher;
 import org.hippoecm.hst.core.sitemapitemhandler.HstSiteMapItemHandler;
 import org.hippoecm.hst.core.sitemapitemhandler.HstSiteMapItemHandlerFactory;
 
-
-public interface VirtualHostsManager {
+public interface HstManager {
 
     /**
-     * @return the <code>VirtualHosts</code> managed by this VirtualHostsManager
+     * @return the <code>VirtualHosts</code> 
      */
     VirtualHosts getVirtualHosts() throws RepositoryNotAvailableException;
     
     
     /**
-     * @return the HstURLFactory for this VirtualHostsManager
+     * @return the HstURLFactory
      */
     HstURLFactory getUrlFactory();
     
     /**
-     * VirtualHostsManager must contain a reference to the {@link HstSiteMapMatcher} that is being used. You can inject your own
+     * a HstSitesManager must contain a reference to the {@link HstSiteMapMatcher} that is being used. You can inject your own
      *  {@link HstSiteMapMatcher} implementation if needed
      * @return the global HstSiteMapMatcher implementation used for all the hosts & sites
      */
@@ -48,8 +48,9 @@ public interface VirtualHostsManager {
     HstSiteMapItemHandlerFactory getSiteMapItemHandlerFactory();
     
     /**
-     * Invalidates this VirtualHostsManager. Typically this invalidate is called after a received event indicating that for example
+     * Invalidates this HstSitesManager. Typically this invalidate is called after a received event indicating that for example
      * the backing hosts configuration has been changed.
      */
     void invalidate(String path);
+
 }

site-toolkit/api/src/main/java/org/hippoecm/hst/configuration/model/HstWebSitesManager.java
@@ -1,32 +1,0 @@
-/*
- *  Copyright 2010 Hippo.
- * 
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- * 
- *       http://www.apache.org/licenses/LICENSE-2.0
- * 
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- */
-package org.hippoecm.hst.configuration.model;
-
-import java.util.Map;
-
-import org.hippoecm.hst.core.container.RepositoryNotAvailableException;
-
-public interface HstWebSitesManager {
-
-    HstNode getVirtualHostsNode();
-
-    Map<String, HstNode> getConfigurationRootNodes();
-    
-    Map<String, HstSiteRootNode> getHstSiteRootNodes();
-
-    void populate() throws RepositoryNotAvailableException ;
-
-}

site-toolkit/client/src/main/java/org/hippoecm/hst/sitemapitemhandler/AbstractHstSiteMapHandler.java
@@ -82,7 +82,7 @@ public abstract class AbstractHstSiteMapHandler implements HstSiteMapItemHandler
      * @return the HstURLFactory 
      */
     public HstURLFactory getURLFactory(ResolvedSiteMapItem resolvedSiteMapItem) {
-        HstURLFactory factory = resolvedSiteMapItem.getResolvedSiteMount().getResolvedVirtualHost().getVirtualHost().getVirtualHosts().getVirtualHostsManager().getUrlFactory();
+        HstURLFactory factory = resolvedSiteMapItem.getResolvedSiteMount().getResolvedVirtualHost().getVirtualHost().getVirtualHosts().getHstManager().getUrlFactory();
         return factory;
     }
 }

site-toolkit/commons/src/main/java/org/hippoecm/hst/container/HstFilter.java
@@ -30,7 +30,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.hippoecm.hst.configuration.hosting.MatchException;
 import org.hippoecm.hst.configuration.hosting.VirtualHosts;
-import org.hippoecm.hst.configuration.hosting.VirtualHostsManager;
+import org.hippoecm.hst.configuration.model.HstManager;
 import org.hippoecm.hst.configuration.sitemapitemhandlers.HstSiteMapItemHandlerConfiguration;
 import org.hippoecm.hst.core.component.HstURLFactory;
 import org.hippoecm.hst.core.container.ComponentManager;
@@ -78,7 +78,7 @@ public class HstFilter implements Filter {
     protected String clientComponentManagerContextAttributeName = CLIENT_COMPONENT_MANANGER_DEFAULT_CONTEXT_ATTRIBUTE_NAME;
     protected HstContainerConfig requestContainerConfig;
     
-    protected VirtualHostsManager virtualHostsManager;
+    protected HstManager hstSitesManager;
     protected HstSiteMapItemHandlerFactory siteMapItemHandlerFactory;
     
     public void init(FilterConfig filterConfig) throws ServletException {
@@ -119,14 +119,14 @@ public class HstFilter implements Filter {
 
         Logger logger = HstServices.getLogger(LOGGER_CATEGORY_NAME);
         
-        virtualHostsManager = HstServices.getComponentManager().getComponent(VirtualHostsManager.class.getName());
-        if(virtualHostsManager != null) {
-            siteMapItemHandlerFactory = virtualHostsManager.getSiteMapItemHandlerFactory();
+        hstSitesManager = HstServices.getComponentManager().getComponent(HstManager.class.getName());
+        if(hstSitesManager != null) {
+            siteMapItemHandlerFactory = hstSitesManager.getSiteMapItemHandlerFactory();
             if(siteMapItemHandlerFactory == null) {
                 logger.error("Cannot find the siteMapItemHandlerFactory component");
             }
         } else {
-            logger.error("Cannot find the virtualHostsManager component for '{}'", VirtualHostsManager.class.getName());
+            logger.error("Cannot find the virtualHostsManager component for '{}'", HstManager.class.getName());
         }
         
         if (clientComponentManager != null) {
@@ -217,7 +217,7 @@ public class HstFilter implements Filter {
     			doInit(filterConfig);
     		}
 
-    		if(this.siteMapItemHandlerFactory == null || this.virtualHostsManager == null) {
+    		if(this.siteMapItemHandlerFactory == null || this.hstSitesManager == null) {
     			res.sendError(HttpServletResponse.SC_SERVICE_UNAVAILABLE);
     			logger.error("The HST virtualHostsManager or siteMapItemHandlerFactory is not available");
     			return;
@@ -231,7 +231,7 @@ public class HstFilter implements Filter {
 
     		if (logger.isDebugEnabled()) {request.setAttribute(REQUEST_START_TICK_KEY, System.nanoTime());}
 
-    		VirtualHosts vHosts = virtualHostsManager.getVirtualHosts();
+    		VirtualHosts vHosts = hstSitesManager.getVirtualHosts();
 
     		if(vHosts == null || vHosts.isExcluded(HstRequestUtils.getRequestPath(req))) {
     			chain.doFilter(request, response);
@@ -267,7 +267,7 @@ public class HstFilter implements Filter {
     			} 
     		}
     		
-			HstURLFactory factory = virtualHostsManager.getUrlFactory();
+			HstURLFactory factory = hstSitesManager.getUrlFactory();
 			
     		HstContainerURL hstContainerURL = requestContext.getBaseURL();
     		if (hstContainerURL == null) {
@@ -371,7 +371,7 @@ public class HstFilter implements Filter {
                 throw new MatchException("Error resolving request to sitemap item: '"+HstRequestUtils.getFarthestRequestHost(req)+"' and '"+req.getRequestURI()+"'");
             }
             requestContext.setResolvedSiteMapItem(resolvedSiteMapItem);
-            requestContext.setBaseURL(virtualHostsManager.getUrlFactory().getContainerURLProvider().createURL(requestContext.getBaseURL(), forwardPathInfo));
+            requestContext.setBaseURL(hstSitesManager.getUrlFactory().getContainerURLProvider().createURL(requestContext.getBaseURL(), forwardPathInfo));
             
             processResolvedSiteMapItem(req, res, requestContext, true, logger);
         }

site-toolkit/components/core/src/main/java/org/hippoecm/hst/configuration/hosting/PortMountService.java
@@ -2,7 +2,7 @@ package org.hippoecm.hst.configuration.hosting;
 
 import org.hippoecm.hst.configuration.HstNodeTypes;
 import org.hippoecm.hst.configuration.model.HstNode;
-import org.hippoecm.hst.configuration.model.HstWebSitesManager;
+import org.hippoecm.hst.configuration.model.HstManagerImpl;
 import org.hippoecm.hst.service.ServiceException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,7 +22,7 @@ public class PortMountService implements PortMount {
      */
     private SiteMount rootSiteMount;
     
-    public PortMountService(HstNode portMount, VirtualHost virtualHost, HstWebSitesManager hstNodeManager) throws ServiceException {
+    public PortMountService(HstNode portMount, VirtualHost virtualHost, HstManagerImpl hstManager) throws ServiceException {
         String nodeName = portMount.getValueProvider().getName();
         try {
             portNumber = Integer.parseInt(nodeName);
@@ -36,7 +36,7 @@ public class PortMountService implements PortMount {
         HstNode siteMount = portMount.getNode(HstNodeTypes.SITEMOUNT_HST_ROOTNAME);
         if(siteMount != null && HstNodeTypes.NODETYPE_HST_SITEMOUNT.equals(siteMount.getNodeTypeName())) {
             try {
-                rootSiteMount = new SiteMountService(siteMount, null, virtualHost, hstNodeManager);
+                rootSiteMount = new SiteMountService(siteMount, null, virtualHost, hstManager);
             } catch (ServiceException e) {
                 log.warn("The host '{}' for port '"+portNumber+"' contains an incorrect configured SiteMount. The host with port cannot be used for hst request processing: {}", virtualHost.getHostName(), e.getMessage());
             } 

commit 564502f1d33bedb780bf76c093195242f480ffcc

HSTTWO-1171: property setter name problem which seems caused by r24017.
svn path=/ecm/site-toolkit/trunk/; revision=24027

site-toolkit/components/core/src/main/java/org/hippoecm/hst/configuration/model/HstSitesConfigurationEventListener.java
@@ -31,7 +31,7 @@ public class HstSitesConfigurationEventListener extends GenericEventListener {
     protected HstManager hstManager;
     protected HstComponentRegistry componentRegistry;
     
-    public void setVirtualHostsManager(HstManager hstManager) {
+    public void setHstManager(HstManager hstManager) {
         this.hstManager = hstManager;
     }
     