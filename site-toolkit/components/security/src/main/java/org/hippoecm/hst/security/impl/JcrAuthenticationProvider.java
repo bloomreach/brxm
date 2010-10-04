77dc96638238028da237c1983265a7b5e5cff515
Woonsan Ko <w.ko@onehippo.com>
2010-10-04 17:38:13 +0000

    HSTTWO-1199: Using hipposys:authrole for user-role mapping by default.
    svn path=/ecm/site-toolkit/trunk/; revision=24174

--- site-toolkit/components/security/src/main/java/org/hippoecm/hst/security/impl/JcrAuthenticationProvider.java
+++ site-toolkit/components/security/src/main/java/org/hippoecm/hst/security/impl/JcrAuthenticationProvider.java
@@ -40,13 +40,16 @@ import org.slf4j.LoggerFactory;
 
 /**
  * JcrAuthenticationProvider
+ * <P>
+ * Basic authentication provider implementation which queries role nodes by the specified <CODE>rolesOfUserQuery</CODE>.
+ * The default configuration is to query group nodes from the Hippo Repository.
+ * </P>
  * @version $Id$
  */
 public class JcrAuthenticationProvider implements AuthenticationProvider {
     
     static final Logger log = LoggerFactory.getLogger(JcrAuthenticationProvider.class);
     
-    // TODO: Find out the proper query instead of using group nodes.
     public static final String DEFAULT_ROLES_OF_USER_QUERY = 
         "//element(*, hipposys:group)[(@hipposys:members = ''{0}'' or @hipposys:members = ''*'') and @hipposys:securityprovider = ''internal'']";
     
@@ -57,18 +60,48 @@ public class JcrAuthenticationProvider implements AuthenticationProvider {
     private String rolesOfUserQuery = DEFAULT_ROLES_OF_USER_QUERY;
     private String queryLanguage = Query.XPATH;
     
+    private String defaultRoleName;
+    
     public JcrAuthenticationProvider(Repository systemRepository, Credentials systemCreds, Repository userAuthRepository) {
         this.systemRepository = systemRepository;
         this.systemCreds = systemCreds;
         this.userAuthRepository = userAuthRepository;
     }
     
+    public Repository getSystemRepository() {
+        return systemRepository;
+    }
+    
+    public Credentials getSystemCredentials() {
+        return systemCreds;
+    }
+    
+    public Repository getUserAuthRepository() {
+        return userAuthRepository;
+    }
+
     public void setRolesOfUserQuery(String rolesOfUserQuery) {
         this.rolesOfUserQuery = rolesOfUserQuery;
     }
     
+    public String getRolesOfUserQuery() {
+        return rolesOfUserQuery;
+    }
+    
     public void setQueryLanguage(String queryLanguage) {
         this.queryLanguage = queryLanguage;
+    }
+    
+    public String getQueryLanguage() {
+        return queryLanguage;
+    }
+    
+    public void setDefaultRoleName(String defaultRoleName) {
+        this.defaultRoleName = defaultRoleName;
+    }
+    
+    public String getDefaultRoleName() {
+        return defaultRoleName;
     }
     
     public User authenticate(String userName, char[] password) throws SecurityException {
@@ -100,8 +133,24 @@ public class JcrAuthenticationProvider implements AuthenticationProvider {
             Set<String> roleNameSet = getRoleNamesOfUser(username);
             roleSet = new HashSet<Role>();
             
-            for (String roleName : roleNameSet) {
-                roleSet.add(new TransientRole(roleName));
+            if (defaultRoleName == null) {
+                for (String roleName : roleNameSet) {
+                    roleSet.add(new TransientRole(roleName));
+                }
+            } else {
+                boolean defaultRoleAdded = false;
+                
+                for (String roleName : roleNameSet) {
+                    roleSet.add(new TransientRole(roleName));
+                    
+                    if (defaultRoleName.equals(roleName)) {
+                        defaultRoleAdded = true;
+                    }
+                }
+                
+                if (!defaultRoleAdded) {
+                    roleSet.add(new TransientRole(defaultRoleName));
+                }
             }
         } catch (LoginException e) {
             throw new SecurityException("System repository throws LoginException: " + e, e);
@@ -121,19 +170,19 @@ public class JcrAuthenticationProvider implements AuthenticationProvider {
         Session session = null;
         
         try {
-            if (systemCreds != null) {
-                session = systemRepository.login(systemCreds);
+            if (getSystemCredentials() != null) {
+                session = getSystemRepository().login(getSystemCredentials());
             } else {
-                session = systemRepository.login();
+                session = getSystemRepository().login();
             }
             
-            String statement = MessageFormat.format(rolesOfUserQuery, username);
+            String statement = MessageFormat.format(getRolesOfUserQuery(), username);
             
             if (log.isDebugEnabled()) {
                 log.debug("Searching roles of user with query: " + statement);
             }
             
-            Query q = session.getWorkspace().getQueryManager().createQuery(statement, queryLanguage);
+            Query q = session.getWorkspace().getQueryManager().createQuery(statement, getQueryLanguage());
             QueryResult result = q.execute();
             NodeIterator nodeIt = result.getNodes();
             
