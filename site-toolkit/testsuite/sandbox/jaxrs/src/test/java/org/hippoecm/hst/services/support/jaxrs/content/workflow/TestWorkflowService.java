757d96d7b8b95ffa97df89a4cc859d21b2a43e27
Woonsan Ko <w.ko@onehippo.com>
2010-10-20 20:47:12 +0000

    HSTTWO-1003: Fixing unit test failures
    svn path=/ecm/site-toolkit/trunk/; revision=24526

--- site-toolkit/testsuite/sandbox/jaxrs/src/test/java/org/hippoecm/hst/services/support/jaxrs/content/workflow/TestWorkflowService.java
+++ site-toolkit/testsuite/sandbox/jaxrs/src/test/java/org/hippoecm/hst/services/support/jaxrs/content/workflow/TestWorkflowService.java
@@ -59,7 +59,9 @@ import org.w3c.dom.Document;
  */
 public class TestWorkflowService extends AbstractJaxrsSpringTestCase {
     
-    private static final String SITE_MOUNT_POINT = "/hst:hst/hst:sites/testproject-preview";
+    private static final String MOUNT_POINT = "/hst:hst/hst:sites/testproject-preview";
+    private static final String MOUNT_CONTENTPATH = "/hst:hst/hst:sites/testproject-preview/hst:content";
+    private static final String MOUNT_CANONICAL_CONTENTPATH = "/documents/testproject";
     
     protected Pipelines pipelines;
     protected Pipeline jaxrsPipeline;
@@ -97,7 +99,10 @@ public class TestWorkflowService extends AbstractJaxrsSpringTestCase {
         EasyMock.expect(resolvedVirtualHost.getPortNumber()).andReturn(8085).anyTimes();
 
         siteMount = EasyMock.createNiceMock(SiteMount.class);
-        EasyMock.expect(siteMount.getMountPoint()).andReturn(SITE_MOUNT_POINT).anyTimes();
+        EasyMock.expect(siteMount.getMountPoint()).andReturn(MOUNT_POINT).anyTimes();
+        EasyMock.expect(siteMount.getContentPath()).andReturn(MOUNT_CONTENTPATH).anyTimes();
+        EasyMock.expect(siteMount.getCanonicalContentPath()).andReturn(MOUNT_CANONICAL_CONTENTPATH).anyTimes();
+        EasyMock.expect(siteMount.isSiteMount()).andReturn(true).anyTimes();
         
         resolvedSiteMount = EasyMock.createNiceMock(ResolvedSiteMount.class);
         EasyMock.expect(resolvedSiteMount.getResolvedVirtualHost()).andReturn(resolvedVirtualHost).anyTimes();
