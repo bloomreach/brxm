124f9be440f5a7a6e17192cb4ba48b42695aa3e2
Arthur Bogaart <a.bogaart@onehippo.com>
2010-10-26 00:13:51 +0000

    HSTTWO-1256: added devmode switch
    svn path=/ecm/site-toolkit/trunk/; revision=24644

--- site-toolkit/client-modules/page-composer/src/main/java/org/hippoecm/hst/pagecomposer/container/PageComposerBootstrapValve.java
+++ site-toolkit/client-modules/page-composer/src/main/java/org/hippoecm/hst/pagecomposer/container/PageComposerBootstrapValve.java
@@ -72,7 +72,8 @@ public class PageComposerBootstrapValve extends AbstractValve
             writer.append("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01//EN\" \"http://www.w3.org/TR/html4/strict.dtd\">");
             writer.append("\n<html><head><meta http-equiv=\"Content-Type\" content=\"text/html;charset=utf-8\">");
 
-            DependencyManager manager = new DependencyManager();
+            boolean devMode = requestContext.getContainerConfiguration().isDevelopmentMode();
+            DependencyManager manager = new DependencyManager(devMode);
             manager.add(new ExtApp());
 
             String editableUrl = creator.create("/", parentMount).toUrlForm(requestContext, false);
