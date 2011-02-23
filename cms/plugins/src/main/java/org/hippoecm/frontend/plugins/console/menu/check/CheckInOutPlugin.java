2986fd4962ba64fd6cfbf1b4c262e45e7b9773c9
Unico Hommes <u.hommes@onehippo.com>
2011-02-23 15:00:59 +0000

    CMS7-4996  make check in/out and auto export enable/disable ajax links instead of dialogs

--- cms/plugins/src/main/java/org/hippoecm/frontend/plugins/console/menu/check/CheckInOutPlugin.java
+++ cms/plugins/src/main/java/org/hippoecm/frontend/plugins/console/menu/check/CheckInOutPlugin.java
@@ -1,28 +1,112 @@
 package org.hippoecm.frontend.plugins.console.menu.check;
 
 import javax.jcr.Node;
+import javax.jcr.RepositoryException;
+import javax.jcr.Session;
+import javax.jcr.UnsupportedRepositoryOperationException;
+import javax.jcr.version.VersionManager;
 
+import org.apache.wicket.AttributeModifier;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.markup.html.AjaxLink;
+import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.model.Model;
-import org.hippoecm.frontend.dialog.DialogLink;
-import org.hippoecm.frontend.dialog.IDialogFactory;
-import org.hippoecm.frontend.dialog.IDialogService.Dialog;
 import org.hippoecm.frontend.plugin.IPluginContext;
 import org.hippoecm.frontend.plugin.config.IPluginConfig;
 import org.hippoecm.frontend.service.render.RenderPlugin;
+import org.hippoecm.frontend.session.UserSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class CheckInOutPlugin extends RenderPlugin<Node> {
 
     private static final long serialVersionUID = 1L;
-    
+    private static final Logger log = LoggerFactory.getLogger(CheckInOutPlugin.class);
+    private final AjaxLink<Void> link;
+            
     public CheckInOutPlugin(IPluginContext context, IPluginConfig config) {
         super(context, config);
-        IDialogFactory factory = new IDialogFactory() {
+
+        final Label label = new Label("link-text", new Model<String>() {
             private static final long serialVersionUID = 1L;
-            public Dialog createDialog() {
-                return new CheckInOutDialog(CheckInOutPlugin.this);
+            @Override public String getObject() {
+                return isCheckedOut() ? "Check In" : "Check Out";
+            }
+        });
+        label.setOutputMarkupId(true);
+        label.add(new AttributeModifier("style", true, new Model<String>() {
+            private static final long serialVersionUID = 1L;
+            @Override
+            public String getObject() {
+                if (!isVersionable()) {
+                    return "color:grey";
+                }
+                return isCheckedOut() ? "color:green" : "color:red";
+            }
+        }));
+        link = new AjaxLink<Void>("link") {
+            private static final long serialVersionUID = 1L;
+            @Override
+            public void onClick(AjaxRequestTarget target) {
+                if (isVersionable()) {
+                    if (isCheckedOut()) {
+                        checkin();
+                    }
+                    else {
+                        checkout();
+                    }
+                }
+                target.addComponent(label);
             }
         };
-        add(new DialogLink("link", new Model<String>("Check In/Out"), factory, getDialogService()));
+        link.add(label);
+        link.setEnabled(isVersionable());
+        add(link);
+    }
+    
+    private boolean isCheckedOut() {
+        try {
+            return getModelObject().isCheckedOut();
+        } catch (RepositoryException e) {
+            log.error("An error occurred determining if node is checked out.", e);
+            return false;
+        }
+    }
+    
+    private boolean isVersionable() {
+        try {
+            return getModelObject().isNodeType("mix:versionable");
+        } catch (RepositoryException e) {
+            log.error("An error occurred determining if node is versionable.", e);
+            return false;
+        }
+    }
+    
+    private void checkin() {
+        try {
+            getModelObject().checkin();
+        } catch (RepositoryException e) {
+            log.error("An error occurred trying to check in node.", e);
+        }
+    }
+    
+    private void checkout() {
+        try {
+            getModelObject().checkout();
+        } catch (RepositoryException e) {
+            log.error("An error occurred trying to check out node.", e);
+        }
+    }
+    
+    private Session getJcrSession() {
+        Session session = ((UserSession) org.apache.wicket.Session.get()).getJcrSession();
+        return session;
+    }
+    
+    @Override
+    protected void onModelChanged() {
+        link.setEnabled(isVersionable());
+        redraw();
     }
 
 }
--- cms/plugins/src/main/java/org/hippoecm/frontend/plugins/console/menu/export/AutoExportPlugin.java
+++ cms/plugins/src/main/java/org/hippoecm/frontend/plugins/console/menu/export/AutoExportPlugin.java
@@ -1,28 +1,117 @@
 package org.hippoecm.frontend.plugins.console.menu.export;
 
 import javax.jcr.Node;
+import javax.jcr.PathNotFoundException;
+import javax.jcr.RepositoryException;
+import javax.jcr.Session;
+import javax.jcr.SimpleCredentials;
 
+import org.apache.wicket.AttributeModifier;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.markup.html.AjaxLink;
+import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.model.Model;
-import org.hippoecm.frontend.dialog.DialogLink;
-import org.hippoecm.frontend.dialog.IDialogFactory;
-import org.hippoecm.frontend.dialog.IDialogService.Dialog;
 import org.hippoecm.frontend.plugin.IPluginContext;
 import org.hippoecm.frontend.plugin.config.IPluginConfig;
 import org.hippoecm.frontend.service.render.RenderPlugin;
+import org.hippoecm.frontend.session.UserSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class AutoExportPlugin extends RenderPlugin<Node> {
 
     private static final long serialVersionUID = 1L;
+    private static final String CONFIG_NODE_PATH = "/hippo:configuration/hippo:modules/autoexport/hippo:moduleconfig";
+    private static final Logger log = LoggerFactory.getLogger("org.hippoecm.repository.export");
 
     public AutoExportPlugin(IPluginContext context, IPluginConfig config) {
         super(context, config);
-        IDialogFactory factory = new IDialogFactory() {
+        
+        final String location = getExportLocation();
+        
+        // enable / disable link
+        final Label label = new Label("link-text", new Model<String>() {
             private static final long serialVersionUID = 1L;
-            public Dialog createDialog() {
-                return new AutoExportDialog(AutoExportPlugin.this);
+            
+            @Override public String getObject() {
+                if (location == null) {
+                    return "Auto export unavailable";
+                }
+                return isExportEnabled() ? "Disable Auto Export" : "Enable Auto Export";
             }
+        });
+        label.setOutputMarkupId(true);
+        label.add(new AttributeModifier("style", true, new Model<String>() {
+            private static final long serialVersionUID = 1L;
+            @Override
+            public String getObject() {
+                if (location == null) {
+                    return "color:red";
+                }
+                return isExportEnabled() ? "color:green" : "color:red";
+            }
+        }));
+        AjaxLink<Void> link = new AjaxLink<Void>("link") {
+
+            private static final long serialVersionUID = 1L;
+
+            @Override
+            public void onClick(AjaxRequestTarget target) {
+                setExportEnabled(!isExportEnabled());
+                target.addComponent(label);
+            }
+            
         };
-        add(new DialogLink("link", new Model<String>("Auto Export"), factory, getDialogService()));
+        link.add(label);
+        link.setEnabled(location != null);
+        add(link);
     }
 
+    private boolean isExportEnabled() {
+        boolean enabled = true;
+        try {
+            Node node = getJcrSession().getNode(CONFIG_NODE_PATH);
+            enabled = node.getProperty("hipposys:enabled").getValue().getBoolean();
+        } catch (PathNotFoundException e) {
+            log.warn("No such item: " + CONFIG_NODE_PATH + "/hipposys:enabled");
+        } catch (RepositoryException e) {
+            log.error("An error occurred reading export enabled flag", e);
+        }
+        return enabled;
+    }
+    
+    private void setExportEnabled(boolean enabled) {
+        Session session = getJcrSession();
+        try {
+            // we use a separate session in order that other changes made in the console
+            // don't get persisted upon save() here
+            session = session.impersonate(new SimpleCredentials(session.getUserID(),new char[] {}));
+            Node node = session.getNode(CONFIG_NODE_PATH);
+            node.setProperty("hipposys:enabled", enabled);
+            session.save();
+            session.logout();
+        } catch (PathNotFoundException e) {
+            log.warn("No such item: " + CONFIG_NODE_PATH + "/hipposys:enabled");
+        } catch (RepositoryException e) {
+            log.error("An error occurred trying to set export enabled flag", e);
+        }
+    }
+
+    private String getExportLocation() {
+        String location = null;
+        try {
+            Node node = getJcrSession().getNode(CONFIG_NODE_PATH);
+            location = node.getProperty("hipposys:location").getString();
+        } catch (PathNotFoundException e) {
+            log.debug("No such item: " + CONFIG_NODE_PATH + "/hipposys:location");
+        } catch (RepositoryException e) {
+            log.error("An error occurred looking up export location", e);
+        }
+        return location;
+    }
+    
+    private Session getJcrSession() {
+        Session session = ((UserSession) org.apache.wicket.Session.get()).getJcrSession();
+        return session;
+    }
 }
