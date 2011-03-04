7cfb88f588a3345eef45c7399b9b50370d389c91
Minos Chatzidakis <m.chatzidakis@onehippo.com>
2011-03-04 16:12:36 +0000

    CMS7-5022 Many hacks to override the style of the flash button (Upload new file)

--- cms/gallery/frontend/src/main/java/org/hippoecm/frontend/plugins/gallery/editor/ImageCropPlugin.java
+++ cms/gallery/frontend/src/main/java/org/hippoecm/frontend/plugins/gallery/editor/ImageCropPlugin.java
@@ -15,19 +15,34 @@
  */
 package org.hippoecm.frontend.plugins.gallery.editor;
 
+import java.awt.Dimension;
+
 import javax.jcr.Node;
 import javax.jcr.RepositoryException;
 
 
+import org.apache.wicket.ResourceReference;
+import org.apache.wicket.ajax.AjaxEventBehavior;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.ajax.markup.html.AjaxLink;
+import org.apache.wicket.behavior.AttributeAppender;
+import org.apache.wicket.markup.html.CSSPackageResource;
+import org.apache.wicket.markup.html.WebMarkupContainer;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.model.AbstractReadOnlyModel;
 import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.Model;
+import org.apache.wicket.model.StringResourceModel;
 import org.hippoecm.frontend.dialog.IDialogService;
 import org.hippoecm.frontend.plugin.IPluginContext;
 import org.hippoecm.frontend.plugin.config.IPluginConfig;
 import org.hippoecm.frontend.plugins.gallery.model.DefaultGalleryProcessor;
+import org.hippoecm.frontend.plugins.gallery.model.GalleryException;
 import org.hippoecm.frontend.plugins.gallery.model.GalleryProcessor;
+import org.hippoecm.frontend.plugins.yui.header.IYuiContext;
 import org.hippoecm.frontend.service.render.RenderPlugin;
+import org.hippoecm.repository.gallery.HippoGalleryNodeType;
+import org.onehippo.yui.YahooNamespace;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -47,26 +62,44 @@ public class ImageCropPlugin extends RenderPlugin {
         String mode = config.getString("mode", "edit");
         final IModel<Node> jcrImageNodeModel = getModel();
 
+        add(CSSPackageResource.getHeaderContribution(ImageCropPlugin.class, "crop-plugin.css"));
+
         try{
             boolean isOriginal = "hippogallery:original".equals(jcrImageNodeModel.getObject().getName());
 
             final GalleryProcessor processor = context.getService(getPluginConfig().getString("gallery.processor.id", "gallery.processor.service"), GalleryProcessor.class);
 
+            Dimension thumbnailDimension = (processor == null ? new DefaultGalleryProcessor() : processor).getDesiredResourceDimension((Node) jcrImageNodeModel.getObject());
+            Node originalImageNode = ((Node) getModelObject()).getParent().getNode("hippogallery:original");
+            Dimension originalImageDimension = new Dimension(
+                        (int) originalImageNode.getProperty(HippoGalleryNodeType.IMAGE_WIDTH).getLong(),
+                        (int) originalImageNode.getProperty(HippoGalleryNodeType.IMAGE_HEIGHT).getLong());
+
+            boolean originalImageSmallerThanThumbSize = thumbnailDimension.getHeight() > originalImageDimension.getHeight() || thumbnailDimension.getWidth() > originalImageDimension.getWidth();
+
+
             //The edit image button
-            AjaxLink<String> cropLink = new AjaxLink<String>("crop-link") {
+            Label cropLink = new Label("crop-link", new StringResourceModel("crop-link-label", this, null));
+            cropLink.add(new AjaxEventBehavior("onclick") {
                 @Override
-                public void onClick(AjaxRequestTarget ajaxRequestTarget) {
+                protected void onEvent(final AjaxRequestTarget target) {
                     IDialogService dialogService = context.getService(IDialogService.class.getName(), IDialogService.class);
                     dialogService.show(new ImageCropEditorDialog(jcrImageNodeModel, (processor == null ? new DefaultGalleryProcessor() : processor)));
                 }
-            };
+            });
 
-            cropLink.setVisible("edit".equals(mode) && !isOriginal);
+            cropLink.setVisible("edit".equals(mode) && !isOriginal && !originalImageSmallerThanThumbSize);
             add(cropLink);
 
         } catch(RepositoryException ex){
             error(ex);
             log.error(ex.getMessage());
+        }  catch(GalleryException ex){
+            error(ex);
+            log.error(ex.getMessage());
         }
     }
+
+
+
 }
