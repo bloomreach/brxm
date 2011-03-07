dde930375b4a4423ee50a0e50b07c5edce5f432f
Unico Hommes <u.hommes@onehippo.com>
2011-03-07 12:18:17 +0000

    CMS7-5010  use same fix as repository servlet (redirect to requestURI + /) to make it work with proxies as well

--- repository/servlets/src/main/java/org/hippoecm/repository/LoggingServlet.java
+++ repository/servlets/src/main/java/org/hippoecm/repository/LoggingServlet.java
@@ -113,6 +113,9 @@ public class LoggingServlet extends HttpServlet {
             BasicAuth.setRequestAuthorizationHeaders(res, "LoggingServlet");
             return;
         }
+        
+        String path = req.getRequestURI();
+        if (!path.endsWith("/")) res.sendRedirect(path + "/");
 
         res.setStatus(HttpServletResponse.SC_OK);
         res.setContentType("text/html");
@@ -142,7 +145,7 @@ public class LoggingServlet extends HttpServlet {
         String lastLogger = req.getParameter("logger");
         String lastLevel = req.getParameter("level");
         SortedMap<String, String> loggerLevelMap = getLoggerLevelMap();
-        printForm(writer, loggerLevelMap, lastLogger, lastLevel, req.getRequestURI());
+        printForm(writer, loggerLevelMap, lastLogger, lastLevel);
         printLoggerLevels(writer, loggerLevelMap);
 
         writer.println("</p>");
@@ -211,8 +214,8 @@ public class LoggingServlet extends HttpServlet {
         }
     }
 
-    private void printForm(PrintWriter writer, Map<String, String> loggerLevelMap, String lastLogger, String lastLevel, String actionPath) {
-        writer.println("  <form action=\"" + actionPath + "\" method=\"POST\" enctype=\"application/x-www-form-urlencoded\">");
+    private void printForm(PrintWriter writer, Map<String, String> loggerLevelMap, String lastLogger, String lastLevel) {
+        writer.println("  <form action=\".\" method=\"POST\" enctype=\"application/x-www-form-urlencoded\">");
         writer.println("    <table>");
         writer.println("      <tr>");
         writer.println("        <td>");
