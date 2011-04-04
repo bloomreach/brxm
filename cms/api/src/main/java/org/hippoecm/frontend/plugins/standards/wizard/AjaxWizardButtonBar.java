1f1f9b4ddf7c9e6fe64adb014c1ef1eae85e3e72
Mathijs den Burger <m.denburger@onehippo.com>
2011-04-04 11:49:09 +0000

    CMS7-5130: refactored anonymous inner classes to named static inner classes (one for each button)

--- cms/api/src/main/java/org/hippoecm/frontend/plugins/standards/wizard/AjaxWizardButtonBar.java
+++ cms/api/src/main/java/org/hippoecm/frontend/plugins/standards/wizard/AjaxWizardButtonBar.java
@@ -34,77 +34,97 @@ public class AjaxWizardButtonBar extends WizardButtonBar {
 
         setOutputMarkupId(true);
 
-        addOrReplace(new AjaxWizardButton("next", wizard, "next") {
-            private static final long serialVersionUID = 1L;
+        addOrReplace(new NextButton(wizard));
+        addOrReplace(new PreviousButton(wizard));
+        addOrReplace(new FinishButton(wizard));
 
-            @Override
-            public final boolean isEnabled() {
-                return getWizardModel().isNextAvailable();
+    }
+
+    private class NextButton extends AjaxWizardButton {
+        private static final long serialVersionUID = 1L;
+        private final Wizard wizard;
+
+        public NextButton(final Wizard wizard) {
+            super("next", wizard, "next");
+            this.wizard = wizard;
+        }
+
+        @Override
+        public final boolean isEnabled() {
+            return getWizardModel().isNextAvailable();
+        }
+
+        @Override
+        protected void onClick(AjaxRequestTarget target, Form form) {
+            IWizardModel wizardModel = getWizardModel();
+            IWizardStep step = wizardModel.getActiveStep();
+
+            // let the step apply any state
+            step.applyState();
+
+            // if the step completed after applying the state, move the model onward
+            if (step.isComplete()) {
+                wizardModel.next();
+            } else {
+                error(getLocalizer().getString(
+                        "org.apache.wicket.extensions.wizard.NextButton.step.did.not.complete", this));
             }
 
-            @Override
-            protected void onClick(AjaxRequestTarget target, Form form) {
-                IWizardModel wizardModel = getWizardModel();
-                IWizardStep step = wizardModel.getActiveStep();
+            target.addComponent(wizard);
+        }
 
-                // let the step apply any state
-                step.applyState();
+    }
 
-                // if the step completed after applying the state, move the model onward
-                if (step.isComplete()) {
-                    wizardModel.next();
-                } else {
-                    error(getLocalizer().getString(
-                            "org.apache.wicket.extensions.wizard.NextButton.step.did.not.complete", this));
-                }
+    private static class PreviousButton extends AjaxWizardButton {
+        private static final long serialVersionUID = 1L;
+        private final Wizard wizard;
 
-                target.addComponent(wizard);
+        public PreviousButton(final Wizard wizard) {
+            super("previous", wizard, "prev");
+            this.wizard = wizard;
+        }
+
+        @Override
+        public final boolean isEnabled() {
+            return getWizardModel().isPreviousAvailable();
+        }
+
+        @Override
+        protected void onClick(AjaxRequestTarget target, Form form) {
+            getWizardModel().previous();
+            target.addComponent(wizard);
+        }
+    }
+
+    private class FinishButton extends AjaxWizardButton {
+        private static final long serialVersionUID = 1L;
+
+        public FinishButton(final Wizard wizard) {
+            super("finish", wizard, "finish");
+        }
+
+        @Override
+        public final boolean isEnabled() {
+            IWizardStep activeStep = getWizardModel().getActiveStep();
+            return (activeStep != null && getWizardModel().isLastStep(activeStep));
+        }
+
+        @Override
+        protected void onClick(AjaxRequestTarget target, Form form) {
+            IWizardModel wizardModel = getWizardModel();
+            IWizardStep step = wizardModel.getActiveStep();
+
+            // let the step apply any state
+            step.applyState();
+
+            // if the step completed after applying the state, notify the wizard
+            if (step.isComplete()) {
+                getWizardModel().finish();
+            } else {
+                error(getLocalizer().getString(
+                        "org.apache.wicket.extensions.wizard.FinishButton.step.did.not.complete", this));
             }
-
-        });
-
-        addOrReplace(new AjaxWizardButton("previous", wizard, "prev") {
-            private static final long serialVersionUID = 1L;
-
-            @Override
-            public final boolean isEnabled() {
-                return getWizardModel().isPreviousAvailable();
-            }
-
-            @Override
-            protected void onClick(AjaxRequestTarget target, Form form) {
-                getWizardModel().previous();
-                target.addComponent(wizard);
-            }
-        });
-
-        addOrReplace(new AjaxWizardButton("finish", wizard, "finish") {
-            private static final long serialVersionUID = 1L;
-
-            @Override
-            public final boolean isEnabled() {
-                IWizardStep activeStep = getWizardModel().getActiveStep();
-                return (activeStep != null && getWizardModel().isLastStep(activeStep));
-            }
-
-            @Override
-            protected void onClick(AjaxRequestTarget target, Form form) {
-                IWizardModel wizardModel = getWizardModel();
-                IWizardStep step = wizardModel.getActiveStep();
-
-                // let the step apply any state
-                step.applyState();
-
-                // if the step completed after applying the state, notify the wizard
-                if (step.isComplete()) {
-                    getWizardModel().finish();
-                } else {
-                    error(getLocalizer().getString(
-                            "org.apache.wicket.extensions.wizard.FinishButton.step.did.not.complete", this));
-                }
-                // target.addComponent(wizard);
-            }
-        });
-
+            // target.addComponent(wizard);
+        }
     }
 }
