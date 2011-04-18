8e5c9b069c3eb4f5892dd947af96df4e71f17da2
Ard Schrijvers <a.schrijvers@onehippo.com>
2011-04-18 14:44:47 +0000

    HSTTWO-1565 complete javadocs for ParameterInfo interface

--- site-toolkit/api/src/main/java/org/hippoecm/hst/configuration/components/ParametersInfo.java
+++ site-toolkit/api/src/main/java/org/hippoecm/hst/configuration/components/ParametersInfo.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2008 Hippo.
+ *  Copyright 2010 - 2011 Hippo.
  * 
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -20,7 +20,31 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Retention(RetentionPolicy.RUNTIME)
+import org.hippoecm.hst.core.component.HstComponent;
+
+/**
+ * <p>
+ * This is an annotation to be used for {@link HstComponent}'s. This way, an {@link HstComponent}'s parameters can be <i>bridged</i> to 
+ * the actual parameters to be expected in the {@link HstComponentConfiguration}. Also, this annotation can be used to inform YUI tools about 
+ * which parameters can be configured for the {@link HstComponent} that has this annotation.
+ * </p>
+ * <p>
+ * For example you can write your {@link HstComponent} for a search as follows:
+ * <blockquote>
+ * <pre>
+ * {@code
+ * @ParametersInfo(type = SearchInfo.class)
+ *    public class Search extends BaseHstComponent {
+ *    public static final Logger log = LoggerFactory.getLogger(SearchNoParameterInfo.class);
+ *    @Override
+ *    public void doBeforeRender(HstRequest request, HstResponse response) throws HstComponentException {
+ *      SearchInfo info = getParametersInfo(request);
+ * }
+ * </pre>
+ * </blockquote>
+ * </p>  
+ */
+@Retention(RetentionPolicy.RUNTIME)  
 @Target({ElementType.TYPE})
 public @interface ParametersInfo {
     
