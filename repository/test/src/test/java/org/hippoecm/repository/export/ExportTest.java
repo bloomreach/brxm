f60b1fc4aa8fbb7d06ff0362a21d62d0c6059a57
Berry van Halderen <b.vanhalderen@onehippo.com>
2011-05-02 14:27:39 +0000

    REPO-2: correct copyright headers

--- repository/test/src/test/java/org/hippoecm/repository/export/ExportTest.java
+++ repository/test/src/test/java/org/hippoecm/repository/export/ExportTest.java
@@ -13,7 +13,6 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
- */
 package org.hippoecm.repository.export;
 
 import static org.custommonkey.xmlunit.DifferenceConstants.ATTR_SEQUENCE_ID;
