9ce1be5d4f5cbe976b833d868a71a9a775d40a71
Berry van Halderen <b.vanhalderen@onehippo.com>
2011-05-02 14:26:26 +0000

    REPO-2: correct copyright headers

--- repository/export/src/main/java/org/hippoecm/repository/export/ContentResourceInstruction.java
+++ repository/export/src/main/java/org/hippoecm/repository/export/ContentResourceInstruction.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2011 Hippo (www.hippo.nl).
+ *  Copyright 2011 Hippo.
  * 
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
