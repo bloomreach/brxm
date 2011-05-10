dbeb2eee4e36d47cb99188d0e19955d8d88cba6e
Jannis Bloemendal <j.bloemendal@onehippo.com>
2011-05-10 08:25:22 +0000

    CMS7-5262 Breadcrumb Panel Perspective - add perspective (forgot to commit those files earlier)

---
+++ cms/admin/src/main/java/org/hippoecm/frontend/plugins/cms/admin/AdminPerspective.java
@@ -0,0 +1,45 @@
+/**
+  * Copyright 2011 Hippo
+  *
+  * Licensed under the Apache License, Version 2.0 (the  "License");
+  * you may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at
+  *
+  * http://www.apache.org/licenses/LICENSE-2.0
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS,
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+*/
+package org.hippoecm.frontend.plugins.cms.admin;
+
+
+import org.apache.wicket.ResourceReference;
+import org.apache.wicket.markup.html.CSSPackageResource;
+import org.apache.wicket.model.IModel;
+import org.hippoecm.frontend.plugin.IPluginContext;
+import org.hippoecm.frontend.plugin.config.IPluginConfig;
+import org.hippoecm.frontend.plugins.standards.panelperspective.PanelPluginPerspective;
+import org.hippoecm.frontend.plugins.standards.panelperspective.breadcrumb.PanelPluginBreadCrumbPanel;
+import org.hippoecm.frontend.service.IconSize;
+
+public class AdminPerspective extends PanelPluginPerspective {
+
+    public AdminPerspective(IPluginContext context, IPluginConfig config) {
+        super(context, config);
+        add(CSSPackageResource.getHeaderContribution(AdminPerspective.class, "admin-perspective.css"));
+    }
+
+    @Override
+    public ResourceReference getIcon(IconSize type) {
+        return new ResourceReference(AdminPerspective.class, "admin-perspective-" + type.getSize() + ".png");
+    }
+
+    @Override
+    public String getPanelServiceId() {
+        return AdminPanelPlugin.ADMIN_PANEL_SERVICE_ID;
+    }
+
+}
