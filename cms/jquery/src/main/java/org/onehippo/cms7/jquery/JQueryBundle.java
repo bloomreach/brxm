c82948cf29fc504ca7d09ff0a9c5f70f409bb9b3
Jannis Bloemendal <j.bloemendal@onehippo.com>
2011-05-18 14:29:25 +0000

    CMS7-5278 Create jQuery module to bundle the resources - added jQuery module

---
+++ cms/jquery/src/main/java/org/onehippo/cms7/jquery/JQueryBundle.java
@@ -0,0 +1,17 @@
+package org.onehippo.cms7.jquery;
+
+public class JQueryBundle {
+
+    public static String JQUERY_CORE = "core/jquery-1.4.3.js";
+    public static String JQUERY_CORE_MIN = "core/jquery/1.4.3.min.js";
+
+    public static String JQUERY_CLASS_PLUGIN = "plugins/jquery.class.js";
+
+    public static String JQUERY_NAMESPACE_PLUGIN = "plugins/jquery.namespace.js";
+
+    public static String JQUERY_UI = "ui/jquery-ui-1.8.5.js";
+    public static String JQUERY_UI_MIN = "ui/jquery-ui-1.8.5.min.js";
+
+    public static String JQUERY_UI_SORTABLE = "ui/custom/jquery.ui.sortable";
+
+}
