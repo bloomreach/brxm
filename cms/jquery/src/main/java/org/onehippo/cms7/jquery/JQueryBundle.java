2d73597086a5c0a36879505e8054f318540796e1
Jannis Bloemendal <j.bloemendal@onehippo.com>
2011-05-19 11:28:02 +0000

    CMS7-5278 Create jQuery module to bundle the resources - fixed typos

--- cms/jquery/src/main/java/org/onehippo/cms7/jquery/JQueryBundle.java
+++ cms/jquery/src/main/java/org/onehippo/cms7/jquery/JQueryBundle.java
@@ -3,7 +3,7 @@ package org.onehippo.cms7.jquery;
 public class JQueryBundle {
 
     public static String JQUERY_CORE = "core/jquery-1.4.3.js";
-    public static String JQUERY_CORE_MIN = "core/jquery/1.4.3.min.js";
+    public static String JQUERY_CORE_MIN = "core/jquery-1.4.3.min.js";
 
     public static String JQUERY_CLASS_PLUGIN = "plugins/jquery.class.js";
 
@@ -12,6 +12,6 @@ public class JQueryBundle {
     public static String JQUERY_UI = "ui/jquery-ui-1.8.5.js";
     public static String JQUERY_UI_MIN = "ui/jquery-ui-1.8.5.min.js";
 
-    public static String JQUERY_UI_SORTABLE = "ui/custom/jquery.ui.sortable";
+    public static String JQUERY_UI_SORTABLE = "ui/custom/jquery.ui.sortable.js";
 
 }
