1caefb993d30650f4994653e4e276a76edf22cc4
Jannis Bloemendal <j.bloemendal@onehippo.com>
2011-05-23 09:55:48 +0000

    CMS7-5278 Create jQuery module to bundle the resources - added js minification and aggregation

--- cms/jquery/src/main/java/org/onehippo/cms7/jquery/JQueryBundle.java
+++ cms/jquery/src/main/java/org/onehippo/cms7/jquery/JQueryBundle.java
@@ -3,17 +3,24 @@ package org.onehippo.cms7.jquery;
 public class JQueryBundle {
 
     public static String JQUERY_CORE = "core/jquery-1.4.3.js";
-    public static String JQUERY_CORE_MIN = "core/jquery-1.4.3.min.js";
+    public static String JQUERY_CORE_MIN = "core/jquery-1.4.3-min.js";
 
     public static String JQUERY_CLASS_PLUGIN = "plugins/jquery.class.js";
+    public static String JQUERY_CLASS_PLUGIN_MIN = "plugins/jquery.class-min.js";
 
     public static String JQUERY_NAMESPACE_PLUGIN = "plugins/jquery.namespace.js";
+    public static String JQUERY_NAMESPACE_PLUGIN_MIN = "plugins/jquery.namespace-min.js";
 
     public static String JQUERY_HEAD_PLUGIN = "plugins/jquery.head-1.0.3b.js";
+    public static String JQUERY_HEAD_PLUGIN_MIN = "plugins/jquery.head-1.0.3b-min.js";
 
     public static String JQUERY_UI = "ui/jquery-ui-1.8.5.js";
-    public static String JQUERY_UI_MIN = "ui/jquery-ui-1.8.5.min.js";
+    public static String JQUERY_UI_MIN = "ui/jquery-ui-1.8.5-min.js";
 
     public static String JQUERY_UI_SORTABLE = "ui/custom/jquery.ui.sortable.js";
+    public static String JQUERY_UI_SORTABLE_MIN = "ui/custom/jquery.ui.sortable_min.js";
+
+    public static String JQUERY_ALL = "jquery-all.js";
+    public static String JQUERY_ALL_MIN = "jquery-all-min.js";
 
 }
