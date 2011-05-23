3b39e98c368b92b6983e6273569cd9ec386fdd30
Jannis Bloemendal <j.bloemendal@onehippo.com>
2011-05-23 10:44:04 +0000

    CMS7-5270 Template Composer Integration into the Channel Manager - added the bundle js reference classes

---
+++ channel-manager/frontend/src/main/java/org/onehippo/cms7/channelmanager/templatecomposer/pageeditor/PageEditorBundle.java
@@ -0,0 +1,22 @@
+package org.onehippo.cms7.channelmanager.templatecomposer.pageeditor;
+
+public class PageEditorBundle {
+
+    public static final String GLOBALS = "globals.js";
+    public static final String GLOBALS_MIN = "globals-min.js";
+
+    public static final String PAGE_EDITOR_CSS = "PageEditor.css";
+
+    public static final String PAGE_EDITOR = "PageEditor.js";
+    public static final String PAGE_EDITOR_MIN = "PageEditor-min.js";
+
+    public static final String PAGE_MODEL = "PageModel.js";
+    public static final String PAGE_MODEL_MIN = "PageModel-min.js";
+
+    public static final String PROPERTIES_PANEL = "PropertiesPanel.js";
+    public static final String PROPERTIES_PANEL_MIN = "PropertiesPanel-min.js";
+
+    public static final String ALL = "page-editor-all.js";
+    public static final String ALL_MIN = "page-editor-all-min.js";
+
+}
---
+++ channel-manager/frontend/src/main/java/org/onehippo/cms7/channelmanager/templatecomposer/plugins/PluginsBundle.java
@@ -0,0 +1,23 @@
+package org.onehippo.cms7.channelmanager.templatecomposer.plugins;
+
+public class PluginsBundle {
+
+    public static final String BASE_GRID = "basegrid/BaseGrid.js";
+    public static final String BASE_GRID_MIN = "basegrid/BaseGrid-min.js";
+
+    public static final String COLOR_FIELD = "colorfield/colorfield.js";
+    public static final String COLOR_FIELD_CSS = "colorfield/colorfield.css";
+
+    public static final String FLOATING_WINDOW = "floatingwindow/FloatingWindow.js";
+    public static final String FLOATING_WINDOW_MIN = "floatingwindow/FloatingWindow-min.js";
+
+    public static final String MI_FRAME = "miframe/miframe.js";
+    public static final String MI_FRAME_MIN = "miframe/miframe-min.js";
+
+    public static final String MI_FRAME_MSG = "miframe/modules/mifmsg.js";
+    public static final String MI_FRAME_MSG_MIN = "miframe/modules/mifmsg-min.js";
+
+    public static final String ALL = "plugins-all.js";
+    public static final String ALL_MIN = "plugins-all-min.js";
+
+}
