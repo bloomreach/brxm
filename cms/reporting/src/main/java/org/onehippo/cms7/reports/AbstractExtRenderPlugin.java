c0dd0f53509256f118e0b68f6881ba907fd85f56
Jannis Bloemendal <j.bloemendal@onehippo.com>
2011-07-05 14:28:41 +0000

    CMS7-5344 Move reporting dashboard to open source - cleanup, remove redundant code to write easier documentation

--- cms/reporting/src/main/java/org/onehippo/cms7/reports/AbstractExtRenderPlugin.java
+++ cms/reporting/src/main/java/org/onehippo/cms7/reports/AbstractExtRenderPlugin.java
@@ -15,24 +15,26 @@
  */
 package org.onehippo.cms7.reports;
 
-import org.hippoecm.frontend.plugin.IPlugin;
+import java.util.List;
+
 import org.hippoecm.frontend.plugin.IPluginContext;
 import org.hippoecm.frontend.plugin.config.IPluginConfig;
 import org.hippoecm.frontend.service.render.RenderPlugin;
 import org.wicketstuff.js.ext.ExtComponent;
 
-/**
- * The root class for the plugins which want to be treated as ExtJS based plugins.
- * The ExtJs based plugins provide the {@link ExtComponent} rather than a plain wicket component,
- * which in turn be added to any ExtPlugin aware root component, e.g. {@link com.onehippo.cms7.reports.layout.ReportsRenderPlugin}
- */
-public interface ExtPlugin extends IPlugin {
+public abstract class AbstractExtRenderPlugin<T> extends RenderPlugin<T> implements ExtPlugin {
+
+     private static final long serialVersionUID = 1L;
+
+    public AbstractExtRenderPlugin(IPluginContext context, IPluginConfig config) {
+        super(context, config);
+    }
 
     /**
      * Should return the ExtPanel that needs to be added to the parent plugin, that manages this plugin.
      *
      * @return ExtComponent - The root ExtComponent that is provided by this plugin.
      */
-    public ExtComponent getExtComponent();
+    public abstract ExtComponent getExtComponent();
 
 }
