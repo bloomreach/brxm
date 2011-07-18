bea4dfd08b483ece87c8a6c7b6bce0439c39dec5
Berry van Halderen <b.vanhalderen@onehippo.com>
2011-07-18 13:21:32 +0000

    RDB-70: move wfdropbox from brokenlinks to repository trunk

---
+++ repository/test/src/main/java/org/onehippo/cms7/test/TestWorkflowImpl.java
@@ -0,0 +1,44 @@
+/*
+ *  Copyright 2011 Hippo.
+ * 
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.onehippo.cms7.test;
+
+import java.rmi.RemoteException;
+import java.util.Date;
+import javax.jcr.Node;
+import javax.jcr.RepositoryException;
+import javax.jcr.Session;
+import org.hippoecm.repository.api.WorkflowContext;
+import org.hippoecm.repository.api.WorkflowException;
+import org.hippoecm.repository.ext.InternalWorkflow;
+import org.hippoecm.repository.ext.WorkflowImpl;
+
+public class TestWorkflowImpl extends WorkflowImpl implements TestWorkflow, InternalWorkflow {
+    private final static String SVN_ID = "$Id$";
+
+    public static int invocationCountNoArg = 0;
+    public static int invocationCountDateArg = 0;
+    public TestWorkflowImpl(WorkflowContext context, Session rootSession, Session userSession, Node subject) throws RemoteException {
+    }
+    public void test() throws RemoteException, WorkflowException, RepositoryException {
+        ++invocationCountNoArg;
+    }
+    public void test(Date date) throws RemoteException, WorkflowException, RepositoryException {
+        ++invocationCountDateArg;
+    }
+    public void schedule(Date date) throws RemoteException, WorkflowException, RepositoryException {
+        ((TestWorkflow)getWorkflowContext(date).getWorkflow("test")).test();
+    }
+}
