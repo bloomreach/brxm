d6e54eb7b79edbd70155d1e471be5f08adae43f8
Ard Schrijvers <a.schrijvers@onehippo.com>
2011-08-01 14:23:03 +0000

    HSTTWO-1003 added license header and remove 'created with Intelij' ... who cares?

--- site-toolkit/client-modules/page-composer/src/main/java/org/hippoecm/hst/pagecomposer/jaxrs/model/Document.java
+++ site-toolkit/client-modules/page-composer/src/main/java/org/hippoecm/hst/pagecomposer/jaxrs/model/Document.java
@@ -1,11 +1,22 @@
+/*
+ *  Copyright 2011 Hippo.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package org.hippoecm.hst.pagecomposer.jaxrs.model;
 
 import javax.xml.bind.annotation.XmlRootElement;
 
-/**
- * Created by IntelliJ IDEA. User: vijaykiran Date: 5/1/11 Time: 2:41 PM To change this template use File | Settings |
- * File Templates.
- */
 @XmlRootElement(name = "document")
 public class Document {
     private String path;
