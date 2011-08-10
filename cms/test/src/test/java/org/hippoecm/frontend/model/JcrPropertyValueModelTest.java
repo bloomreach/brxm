2e6a8e8a12fa48a7e992aa2477f0633afd69305f
Unico Hommes <u.hommes@onehippo.com>
2011-08-10 09:14:27 +0000

    CMS7-5403 reenable test

--- cms/test/src/test/java/org/hippoecm/frontend/model/JcrPropertyValueModelTest.java
+++ cms/test/src/test/java/org/hippoecm/frontend/model/JcrPropertyValueModelTest.java
@@ -32,7 +32,6 @@ import org.hippoecm.frontend.PluginTest;
 import org.hippoecm.frontend.model.properties.JcrPropertyModel;
 import org.hippoecm.frontend.model.properties.JcrPropertyValueModel;
 import org.junit.Test;
-import org.junit.Ignore;
 
 public class JcrPropertyValueModelTest extends PluginTest {
     @SuppressWarnings("unused")
@@ -107,8 +106,6 @@ public class JcrPropertyValueModelTest extends PluginTest {
     }
 
     @Test
-    // ignore until CMS7-5403 is resolved
-    @Ignore
     public void testSetNullRemovesSingleValuedProperty() throws Exception {
         Node test = this.root.addNode("test", "frontendtest:model");
         test.setProperty("frontendtest:string", "y");
