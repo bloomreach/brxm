f343c427a02e29b70eb85732123fa415c4076203
Jannis Bloemendal <j.bloemendal@onehippo.com>
2011-09-13 17:09:45 +0000

    CMS7-5490: fixed page model store api urls

--- channel-manager/frontend/src/main/java/org/onehippo/cms7/channelmanager/templatecomposer/pageeditor/PageEditor.js
+++ channel-manager/frontend/src/main/java/org/onehippo/cms7/channelmanager/templatecomposer/pageeditor/PageEditor.js
@@ -1246,54 +1246,59 @@ Hippo.ChannelManager.TemplateComposer.PageModelStore = Ext.extend(Hippo.ChannelM
         var composerRestMountUrl = config.composerRestMountUrl;
         var ignoreRenderHostParameterName = config.ignoreRenderHostParameterName;
 
-        var proxy = new Ext.data.HttpProxy({
-            api: {
-                read     : composerRestMountUrl + config.mountId + './pagemodel/'+config.pageId+"/?"+ignoreRenderHostParameterName+'=true'
-                ,create  : '#' // see beforewrite
-                ,update  : '#'
-                ,destroy : '#'
-            },
-
-            listeners : {
-                beforeload: {
-                    fn: function (store, options) {
-                        Hippo.Msg.wait(config.resources['page-model-store-before-load-message']);
-                    }
-                },
-                beforewrite : {
-                    fn : function(proxy, action, rs, params) {
-                        Hippo.Msg.wait(config.resources['page-model-store-before-write-message']);
-                        if (action == 'create') {
-                            var prototypeId = rs.get('id');
-                            var parentId = rs.get('parentId');
-                            proxy.setApi(action, {url: composerRestMountUrl + parentId + './create/' + prototypeId + '?' + ignoreRenderHostParameterName + '=true', method: 'POST'});
-                        } else if (action == 'update') {
-                            //Ext appends the item ID automatically
-                            var id = rs.get('id');
-                            proxy.setApi(action, {url: composerRestMountUrl + id + './update' + '?' + ignoreRenderHostParameterName + '=true', method: 'POST'});
-                        } else if (action == 'destroy') {
-                            //Ext appends the item ID automatically
-                            var parentId = rs.get('parentId');
-                            proxy.setApi(action, {url: composerRestMountUrl + parentId + './delete' + '?' + ignoreRenderHostParameterName + '=true', method: 'GET'});
-                        }
-                    }
-                },
-                write :{
-                    fn: function(store, action, result, res, rs) {
-                        Hippo.Msg.hide();
-                        Hippo.ChannelManager.TemplateComposer.Instance.refreshIframe();
-                    }
-                },
-                load : {
-                    fn: function (store, records, options) {
-                        Hippo.Msg.hide();
-                    }
-                }
+        var PageModelProxy = Ext.extend(Ext.data.HttpProxy, {
+            buildUrl : function() {
+                 return PageModelProxy.superclass.buildUrl.apply(this, arguments) + '?' + ignoreRenderHostParameterName + '=true';
             }
         });
+
         var cfg = {
             id: 'PageModelStore',
-            proxy: proxy,
+            proxy: new PageModelProxy({
+                api: {
+                    read     : composerRestMountUrl + config.mountId + './pagemodel/' + config.pageId + "/"
+                    ,create  : '#' // see beforewrite
+                    ,update  : '#'
+                    ,destroy : '#'
+                },
+
+                listeners : {
+                    beforeload: {
+                        fn: function (store, options) {
+                            Hippo.Msg.wait(config.resources['page-model-store-before-load-message']);
+                        }
+                    },
+                    beforewrite : {
+                        fn : function(proxy, action, rs, params) {
+                            Hippo.Msg.wait(config.resources['page-model-store-before-write-message']);
+                            if (action == 'create') {
+                                var prototypeId = rs.get('id');
+                                var parentId = rs.get('parentId');
+                                proxy.setApi(action, {url: composerRestMountUrl + parentId + './create/' + prototypeId, method: 'POST'});
+                            } else if (action == 'update') {
+                                //Ext appends the item ID automatically
+                                var id = rs.get('id');
+                                proxy.setApi(action, {url: composerRestMountUrl + id + './update', method: 'POST'});
+                            } else if (action == 'destroy') {
+                                //Ext appends the item ID automatically
+                                var parentId = rs.get('parentId');
+                                proxy.setApi(action, {url: composerRestMountUrl + parentId + './delete', method: 'GET'});
+                            }
+                        }
+                    },
+                    write :{
+                        fn: function(store, action, result, res, rs) {
+                            Hippo.Msg.hide();
+                            Hippo.ChannelManager.TemplateComposer.Instance.refreshIframe();
+                        }
+                    },
+                    load : {
+                        fn: function (store, records, options) {
+                            Hippo.Msg.hide();
+                        }
+                    }
+                }
+            }),
             prototypeRecord : Hippo.ChannelManager.TemplateComposer.PageModel.ReadRecord
         };
 
