5b9a589c90f94a6a00fbb5c897f08a076b2e9a93
Mathijs den Burger <m.denburger@onehippo.com>
2011-09-23 08:28:34 +0000

    CMS7-5518: removed unused and non-existing imports

--- channel-manager/test/src/test/java/org/onehippo/cms7/channelmanager/ExtTest.java
+++ channel-manager/test/src/test/java/org/onehippo/cms7/channelmanager/ExtTest.java
@@ -3,8 +3,6 @@ package org.onehippo.cms7.channelmanager;
 import com.gargoylesoftware.htmlunit.html.HtmlElement;
 
 import org.junit.Test;
-import org.onehippo.cms7.channelmanager.yuitest.YuiTestBundle;
-import org.wicketstuff.js.ext.ExtBundle;
 
 import static junit.framework.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
