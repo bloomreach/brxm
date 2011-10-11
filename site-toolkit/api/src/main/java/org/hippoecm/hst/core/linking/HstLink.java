dc66581276fadfb9cba7b352593751a739672c39
Mathijs den Burger <m.denburger@onehippo.com>
2011-10-11 10:10:56 +0000

    HSTTWO-1003: fixed javadoc of setContainerResource

--- site-toolkit/api/src/main/java/org/hippoecm/hst/core/linking/HstLink.java
+++ site-toolkit/api/src/main/java/org/hippoecm/hst/core/linking/HstLink.java
@@ -66,7 +66,7 @@ public interface HstLink {
     boolean getContainerResource();
     
     /**
-     * @param sets the containerResource
+     * @param containerResource sets the containerResource
      */
     void setContainerResource(boolean containerResource);
     
