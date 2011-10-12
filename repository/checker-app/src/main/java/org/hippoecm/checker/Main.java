eacad66fa382e8de36c0c0d3ba2c4cfd2ac0cfe7
Berry van Halderen <b.vanhalderen@onehippo.com>
2011-10-12 09:13:05 +0000

    REPO-126, TENNET-14: re-enable consistency fix

--- repository/checker-app/src/main/java/org/hippoecm/checker/Main.java
+++ repository/checker-app/src/main/java/org/hippoecm/checker/Main.java
@@ -6,7 +6,7 @@ import org.hippoecm.repository.LocalHippoRepository;
 public class Main {
     public static void main(String[] args) {
         try {
-            LocalHippoRepository.check(null, false);
+            LocalHippoRepository.check(null, true);
         } catch(RepositoryException ex) {
             System.err.println(ex.getClass().getName()+": "+ex.getMessage());
             ex.printStackTrace(System.err);
