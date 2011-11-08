d48b371e9450297dc21be74cdb42334a505a722d
Jannis Bloemendal <j.bloemendal@onehippo.com>
2011-11-08 16:21:04 +0000

    CMS7-5122: restore overwritten destroy method on YUI Layout class

--- cms/api/src/main/java/org/hippoecm/frontend/plugins/yui/inc/hippo/281/layoutmanager/layoutmanager-debug.js
+++ cms/api/src/main/java/org/hippoecm/frontend/plugins/yui/inc/hippo/281/layoutmanager/layoutmanager-debug.js
@@ -1008,6 +1008,38 @@ if (!YAHOO.hippo.LayoutManager) { // Ensure only one layout manager exists
 ( function() {
     var Dom = YAHOO.util.Dom, Event = YAHOO.util.Event, Lang = YAHOO.lang;
 
+    YAHOO.widget.Layout.prototype.destroy = function() {
+        var par = this.get('parent');
+        if (par) {
+            par.removeListener('resize', this.resize, this, true);
+        }
+        Event.removeListener(window, 'resize', this.resize, this, true);
+        this.unsubscribeAll();
+        for (var u in this._units) {
+            if (Lang.hasOwnProperty(this._units, u)) {
+                if (this._units[u]) {
+                    this._units[u].destroy(true);
+                }
+            }
+        }
+
+        Event.purgeElement(this.get('element'));
+        //this.get('parentNode').removeChild(this.get('element'));
+
+        delete YAHOO.widget.Layout._instances[this.get('id')];
+        //Brutal Object Destroy
+        for (var i in this) {
+            if (Lang.hasOwnProperty(this, i)) {
+                this[i] = null;
+                delete this[i];
+            }
+        }
+
+        if (par) {
+            //par.resize();
+        }
+    }
+
     YAHOO.widget.LayoutUnit.prototype.destroy = function(force) {
         if (this._resize) {
             this._resize.destroy();
