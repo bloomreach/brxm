578bc64d33c1f01d4291ba5cd3aeefe26ca021e0
Unico Hommes <u.hommes@onehippo.com>
2011-12-13 11:01:50 +0000

    REPO-235 test that document view export does not recurse in virtual layers

---
+++ repository/engine/src/test/java/org/hippoecm/repository/DocumentViewExportTest.java
@@ -0,0 +1,44 @@
+package org.hippoecm.repository;
+
+import java.io.ByteArrayOutputStream;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class DocumentViewExportTest extends TestCase {
+    
+    String[] content = new String[] {
+        "/test",              "nt:unstructured",
+        "jcr:mixinTypes",     "mix:referenceable",
+        "/test/fs",           "hippo:facetselect",
+        "hippo:docbase",      "/test",
+        "hippo:values",       null,
+        "hippo:facets",       null,
+        "hippo:modes",        null
+    };
+
+    @After
+    @Override
+    public void tearDown() throws Exception {
+        if(session.getRootNode().hasNode("test")) {
+            session.getRootNode().getNode("test").remove();
+        }
+        session.save();
+        super.tearDown();
+    }
+    
+    @Before
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+    
+    @Test
+    public void testDocumentViewExportDoesNotRecurseInVirtualLayers() throws Exception {
+        build(session, content);
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        session.exportDocumentView("/test", out, false, false);
+    }
+
+}
