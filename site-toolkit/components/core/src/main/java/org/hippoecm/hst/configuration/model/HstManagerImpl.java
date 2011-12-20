a8772404c8785d13bfd989d59b2bccd6bed0a926
Ard Schrijvers <a.schrijvers@onehippo.com>
2011-12-20 09:37:48 +0000

    CMS7-5706 small formatting

--- site-toolkit/components/core/src/main/java/org/hippoecm/hst/configuration/model/HstManagerImpl.java
+++ site-toolkit/components/core/src/main/java/org/hippoecm/hst/configuration/model/HstManagerImpl.java
@@ -15,6 +15,23 @@
  */
 package org.hippoecm.hst.configuration.model;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import javax.jcr.Credentials;
+import javax.jcr.Node;
+import javax.jcr.NodeIterator;
+import javax.jcr.PathNotFoundException;
+import javax.jcr.Repository;
+import javax.jcr.RepositoryException;
+import javax.jcr.Session;
+import javax.jcr.observation.Event;
+import javax.jcr.observation.EventIterator;
+
 import org.hippoecm.hst.configuration.HstNodeTypes;
 import org.hippoecm.hst.configuration.StringPool;
 import org.hippoecm.hst.configuration.channel.MutableChannelManager;
@@ -33,11 +50,6 @@ import org.hippoecm.hst.provider.jcr.JCRValueProviderImpl;
 import org.hippoecm.hst.service.ServiceException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import javax.jcr.*;
-import javax.jcr.observation.Event;
-import javax.jcr.observation.EventIterator;
-import java.util.*;
 
 /**
  * This class handles the loading of {@link HstNodeImpl}'s. 
@@ -216,7 +228,6 @@ public class HstManagerImpl implements HstManager {
     
     public VirtualHosts getVirtualHosts() throws RepositoryNotAvailableException {
 
-        //
         VirtualHosts currentHosts = virtualHosts;
         if (currentHosts == null) {
             synchronized(this) {
