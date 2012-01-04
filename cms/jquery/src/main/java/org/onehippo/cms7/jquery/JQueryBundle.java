2ffe25e7a717f65a48a07d077c2bb317fb726623
Frank van Lankvelt <f.vanlankvelt@onehippo.com>
2012-01-04 11:33:06 +0000

    CMS7-5772: use jquery 1.6.2, jquery-ui 1.8.16

--- cms/jquery/src/main/java/org/onehippo/cms7/jquery/JQueryBundle.java
+++ cms/jquery/src/main/java/org/onehippo/cms7/jquery/JQueryBundle.java
@@ -2,8 +2,8 @@ package org.onehippo.cms7.jquery;
 
 public class JQueryBundle {
 
-    public static String JQUERY_CORE = "core/jquery-1.6.1.js";
-    public static String JQUERY_CORE_MIN = "core/jquery-1.6.1-min.js";
+    public static String JQUERY_CORE = "core/jquery-1.6.2.js";
+    public static String JQUERY_CORE_MIN = "core/jquery-1.6.2-min.js";
 
     public static String JQUERY_CLASS_PLUGIN = "plugins/jquery.class.js";
     public static String JQUERY_CLASS_PLUGIN_MIN = "plugins/jquery.class-min.js";
@@ -14,8 +14,8 @@ public class JQueryBundle {
     public static String JQUERY_HEAD_PLUGIN = "plugins/jquery.head-1.0.3b.js";
     public static String JQUERY_HEAD_PLUGIN_MIN = "plugins/jquery.head-1.0.3b-min.js";
 
-    public static String JQUERY_UI = "ui/jquery-ui-1.8.13.js";
-    public static String JQUERY_UI_MIN = "ui/jquery-ui-1.8.13-min.js";
+    public static String JQUERY_UI = "ui/jquery-ui-1.8.16.js";
+    public static String JQUERY_UI_MIN = "ui/jquery-ui-1.8.16-min.js";
 
     public static String JQUERY_ALL = "jquery-all.js";
     public static String JQUERY_ALL_MIN = "jquery-all-min.js";
