63c0fc46e36f06a2cc54a75a3933e93cde34dac7
Simon Voortman <s.voortman@onehippo.com>
2012-01-09 16:09:02 +0000

    HSTTWO-1971: Adding wikipedia document type with bean, including a category.
    When adding wikipedia documents, a category might be extracted (only the first word, to keep the number of categories down).
    
    Adding faceted navigation on the abovementioned categories

--- testsuite/site/src/main/java/org/hippoecm/hst/demo/components/SimpleNonWorkflowWikiImporterComponent.java
+++ testsuite/site/src/main/java/org/hippoecm/hst/demo/components/SimpleNonWorkflowWikiImporterComponent.java
@@ -17,8 +17,12 @@ package org.hippoecm.hst.demo.components;
 
 import java.io.File;
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
 import java.util.Random;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.jcr.Node;
 import javax.jcr.RepositoryException;
@@ -40,25 +44,30 @@ import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
 
 /**
- * Simple plain wikipedia to jcr Node. 
+ * Simple plain wikipedia to jcr Node.
  * 
- * Note that this is <strong>not</strong> production code! Real code should use workflow instead of the low-level jcr calls in this class.
- * This just serves to import many wiki documents as efficient as possible
+ * Note that this is <strong>not</strong> production code! Real code should use
+ * workflow instead of the low-level jcr calls in this class. This just serves
+ * to import many wiki documents as efficient as possible
  * 
- *
+ * 
  */
 public class SimpleNonWorkflowWikiImporterComponent extends BaseHstComponent {
 
     public static final Logger log = LoggerFactory.getLogger(SimpleNonWorkflowWikiImporterComponent.class);
-    
+
+    // Matches the first word for each category (to keep the number of
+    // categories down)
+    private Pattern categoryPattern = Pattern.compile("\\[\\[Category:(\\w+).*?]]");
+
     @Override
     public void doBeforeRender(HstRequest request, HstResponse response) throws HstComponentException {
-       request.setAttribute("message", request.getParameter("message"));
+        request.setAttribute("message", request.getParameter("message"));
     }
 
     @Override
     public void doAction(HstRequest request, HstResponse response) throws HstComponentException {
-        
+
         SAXParserFactoryImpl impl = new SAXParserFactoryImpl();
         SAXParser parser;
         String numberStr = request.getParameter("number");
@@ -66,67 +75,73 @@ public class SimpleNonWorkflowWikiImporterComponent extends BaseHstComponent {
         try {
             numberOfWikiDocs = Integer.parseInt(numberStr);
         } catch (NumberFormatException e) {
-            response.setRenderParameter("message", "number must be a number but was '"+numberStr+"'");
+            response.setRenderParameter("message", "number must be a number but was '" + numberStr + "'");
             return;
         }
-        
-        if(numberOfWikiDocs <= 0) {
-            response.setRenderParameter("message", "number must be a number larger than 0 but was '"+numberStr+"'");
+
+        if (numberOfWikiDocs <= 0) {
+            response.setRenderParameter("message", "number must be a number larger than 0 but was '" + numberStr + "'");
             return;
         }
-        
+
         String wikiContentFileSystem = request.getParameter("filesystemLocation");
-        
-        if(numberOfWikiDocs > 100 && StringUtils.isEmpty(wikiContentFileSystem)) {
-            response.setRenderParameter("message", "When number is larger than 100, you need to specify the filesystem location (for exmaple /home/use/Downloads/enwiki-20100622-pages-articles.xml) where wikipedia content can be found that " +
-            		"has more than 100 docs. If you choose less than 100, a built in wikipedia xml is used that contains 100 items");
+
+        if (numberOfWikiDocs > 100 && StringUtils.isEmpty(wikiContentFileSystem)) {
+            response.setRenderParameter(
+                    "message",
+                    "When number is larger than 100, you need to specify the filesystem location (for exmaple /home/use/Downloads/enwiki-20100622-pages-articles.xml) where wikipedia content can be found that "
+                            + "has more than 100 docs. If you choose less than 100, a built in wikipedia xml is used that contains 100 items");
             return;
         }
-        
+
         try {
             parser = impl.newSAXParser();
             InputStream wikiStream = null;
             File f = null;
-            if(StringUtils.isEmpty(wikiContentFileSystem)) {
-                wikiStream = SimpleNonWorkflowWikiImporterComponent.class.getClassLoader().getResourceAsStream("enwiki-20081008-pages-articles.xml.100.top.xml");
+            if (StringUtils.isEmpty(wikiContentFileSystem)) {
+                wikiStream = SimpleNonWorkflowWikiImporterComponent.class.getClassLoader().getResourceAsStream(
+                        "enwiki-20081008-pages-articles.xml.100.top.xml");
             } else {
                 f = new File(wikiContentFileSystem);
             }
-          
+
             WikiPediaToJCRHandler handler = null;
             long start = System.currentTimeMillis();
             try {
-                 
+
                 Session writableSession = this.getPersistableSession(request);
                 Node canonicalSiteRootNode = NodeUtils.getDeref(getSiteContentBaseBean(request).getNode());
                 Node baseNode = writableSession.getNode(canonicalSiteRootNode.getPath());
-                
+
                 Node wikiFolder;
-                
-                if(!baseNode.hasNode("wikipedia")) {
+
+                if (!baseNode.hasNode("wikipedia")) {
                     wikiFolder = baseNode.addNode("wikipedia", "hippostd:folder");
                     wikiFolder.addMixin("hippo:harddocument");
                 } else {
                     wikiFolder = baseNode.getNode("wikipedia");
                 }
                 handler = new WikiPediaToJCRHandler(wikiFolder, numberOfWikiDocs);
-                
-                if(wikiStream == null) {
+
+                if (wikiStream == null) {
                     parser.parse(f, handler);
                 } else {
                     parser.parse(wikiStream, handler);
                 }
-            }catch (ForcedStopException e) {
-                // succesfull handler quits after numberOfWikiDocs has been achieved
-            }catch (Exception e) {
+            } catch (ForcedStopException e) {
+                // succesfull handler quits after numberOfWikiDocs has been
+                // achieved
+            } catch (Exception e) {
                 log.warn("Exception during importing wikipedia docs", e);
-                response.setRenderParameter("message", "An exception happened. Did not import wiki docs. " + e.toString());
+                response.setRenderParameter("message",
+                        "An exception happened. Did not import wiki docs. " + e.toString());
             }
-            response.setRenderParameter("message", "Successfully imported '"+handler.count+"' wikipedia documents in '"+(System.currentTimeMillis() - start)+"' ms.");
+            response.setRenderParameter("message", "Successfully imported '" + handler.count
+                    + "' wikipedia documents in '" + (System.currentTimeMillis() - start) + "' ms.");
         } catch (ParserConfigurationException e) {
             response.setRenderParameter("message", "Did not import wiki: " + e.toString());
-        } 
-             
+        }
+
     }
 
     class WikiPediaToJCRHandler extends DefaultHandler {
@@ -144,7 +159,8 @@ public class SimpleNonWorkflowWikiImporterComponent extends BaseHstComponent {
         int maxDocsPerFolder = 50;
         int maxSubFolders = 25;
         long startTime = 0;
-        private final String[] users = {"ard","bard","arje","artur","reijn","berry","frank","mathijs","junaid","ate","tjeerd","verberg","simon","jannis"};
+        private final String[] users = { "ard", "bard", "arje", "artur", "reijn", "berry", "frank", "mathijs",
+                "junaid", "ate", "tjeerd", "verberg", "simon", "jannis" };
         private Random rand;
 
         public WikiPediaToJCRHandler(Node wikiFolder, int total) throws Exception {
@@ -163,23 +179,25 @@ public class SimpleNonWorkflowWikiImporterComponent extends BaseHstComponent {
                 startTime = System.currentTimeMillis();
             }
             if (qName.equals("page")) {
-                try { 
-                    count++; 
+                try {
+                    count++;
                     if (count >= total) {
                         System.out.println(total);
-                        wikiFolder.getSession().save(); 
+                        wikiFolder.getSession().save();
 
-                        System.out.println("Total added wiki docs = " + count + ". It took " + (System.currentTimeMillis() - startTime) + " ms.");
+                        System.out.println("Total added wiki docs = " + count + ". It took "
+                                + (System.currentTimeMillis() - startTime) + " ms.");
                         throw new ForcedStopException();
-                    }   
+                    }
                     if ((count % maxDocsPerFolder) == 0) {
                         wikiFolder.getSession().save();
-                        if(numberOfSubFolders >= maxSubFolders) {
+                        if (numberOfSubFolders >= maxSubFolders) {
                             currentFolder = wikiFolder.addNode("wiki-" + System.currentTimeMillis(), "hippostd:folder");
                             currentFolder.addMixin("hippo:harddocument");
                             numberOfSubFolders = 0;
                         }
-                        currentSubFolder = currentFolder.addNode("wiki-" + System.currentTimeMillis(), "hippostd:folder");
+                        currentSubFolder = currentFolder.addNode("wiki-" + System.currentTimeMillis(),
+                                "hippostd:folder");
                         currentSubFolder.addMixin("hippo:harddocument");
                         numberOfSubFolders++;
                         System.out.println("Counter = " + count);
@@ -196,18 +214,18 @@ public class SimpleNonWorkflowWikiImporterComponent extends BaseHstComponent {
                     translation.setProperty("hippo:message", docName);
                     translation.setProperty("hippo:language", "");
 
-                    doc = handle.addNode(docName, "demosite:textdocument");
+                    doc = handle.addNode(docName, "demosite:wikidocument");
                     doc.addMixin("hippo:harddocument");
 
-                    String[] availability = {"live", "preview"};
-                    doc.setProperty("hippo:availability", availability );
+                    String[] availability = { "live", "preview" };
+                    doc.setProperty("hippo:availability", availability);
                     doc.setProperty("hippostd:stateSummary", "live");
                     doc.setProperty("hippostd:state", "published");
                     doc.setProperty("hippostdpubwf:lastModifiedBy", users[rand.nextInt(users.length)]);
                     doc.setProperty("hippostdpubwf:createdBy", users[rand.nextInt(users.length)]);
-                    doc.setProperty("hippostdpubwf:lastModificationDate",  Calendar.getInstance());
-                    doc.setProperty("hippostdpubwf:creationDate", Calendar.getInstance() );
-                    doc.setProperty("hippostdpubwf:publicationDate", Calendar.getInstance() );
+                    doc.setProperty("hippostdpubwf:lastModificationDate", Calendar.getInstance());
+                    doc.setProperty("hippostdpubwf:creationDate", Calendar.getInstance());
+                    doc.setProperty("hippostdpubwf:publicationDate", Calendar.getInstance());
                 } catch (RepositoryException e) {
                     e.printStackTrace();
                 }
@@ -241,13 +259,21 @@ public class SimpleNonWorkflowWikiImporterComponent extends BaseHstComponent {
                 checkCorrectDoc();
                 try {
                     String text = fieldText.toString().trim();
-                    if(text.length() > 100) {
-                        doc.setProperty("demosite:summary", text.substring(0,100));
+                    if (text.length() > 100) {
+                        doc.setProperty("demosite:summary", text.substring(0, 100));
                     } else {
                         doc.setProperty("demosite:summary", text);
                     }
-                    Node body = doc.addNode("demosite:body","hippostd:html");
+                    Node body = doc.addNode("demosite:body", "hippostd:html");
                     body.setProperty("hippostd:content", text);
+
+                    Matcher m = categoryPattern.matcher(text);
+                    List<String> categories = new ArrayList<String>();
+                    while (m.find()) {
+                        categories.add(m.group(1));
+                    }
+                    doc.setProperty("demosite:categories", categories.toArray(new String[categories.size()]));
+
                     fieldText = new StringBuilder();
                 } catch (RepositoryException e) {
                     throw new SAXException(e);
