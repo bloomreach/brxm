17139d9a723750124f31381acd14b78b0f64af08
Mathijs den Burger <m.denburger@onehippo.com>
2012-01-20 11:00:59 +0000

    HSTTWO-1973: added javadoc for the 'query' parameter of getFacetedNavigationResultDocument

--- site-toolkit/client/src/main/java/org/hippoecm/hst/utils/BeanUtils.java
+++ site-toolkit/client/src/main/java/org/hippoecm/hst/utils/BeanUtils.java
@@ -342,7 +342,9 @@ public class BeanUtils {
      * 
      * @param <T>
      * @param hstRequest the hstRequest
-     * @param query the free text search as String that is used for this faceted navigation
+     * @param query the free text search as String that is used for this faceted navigation. The query should already
+     *              have been processed by {@link org.hippoecm.hst.util.SearchInputParsingUtils#parse(String, boolean)}
+     *              if necessary.
      * @param relPath the relative path from site base content to the faceted navigation node, which must not start with a / and is relative to the site content base path
      * @param objectConverter
      * @param beanMappingClass the class T must be of 
