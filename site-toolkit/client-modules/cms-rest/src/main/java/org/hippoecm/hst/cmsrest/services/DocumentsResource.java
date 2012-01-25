76a86e6fe1b7caafa99d333e6b43867b4ca6ca01
Mathijs den Burger <m.denburger@onehippo.com>
2012-01-25 13:52:46 +0000

    HSTTWO-1003: removed unused imports

--- site-toolkit/client-modules/cms-rest/src/main/java/org/hippoecm/hst/cmsrest/services/DocumentsResource.java
+++ site-toolkit/client-modules/cms-rest/src/main/java/org/hippoecm/hst/cmsrest/services/DocumentsResource.java
@@ -20,11 +20,6 @@ import java.util.Collections;
 import java.util.List;
 
 import javax.jcr.Node;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
 
 import org.apache.commons.lang.StringUtils;
 import org.hippoecm.hst.configuration.channel.Channel;
