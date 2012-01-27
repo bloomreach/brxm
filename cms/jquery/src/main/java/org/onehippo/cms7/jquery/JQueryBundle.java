403809602b2e0fd2edcbe49a6f7de5a00bc47c63
Frank van Lankvelt <f.vanlankvelt@onehippo.com>
2012-01-27 16:05:54 +0000

    CMS7-16: make constants final

--- cms/jquery/src/main/java/org/onehippo/cms7/jquery/JQueryBundle.java
+++ cms/jquery/src/main/java/org/onehippo/cms7/jquery/JQueryBundle.java
@@ -1,23 +1,26 @@
 package org.onehippo.cms7.jquery;
 
-public class JQueryBundle {
+public final class JQueryBundle {
 
-    public static String JQUERY_CORE = "core/jquery-1.6.2.js";
-    public static String JQUERY_CORE_MIN = "core/jquery-1.6.2-min.js";
+    private JQueryBundle() {
+    }
 
-    public static String JQUERY_CLASS_PLUGIN = "plugins/jquery.class.js";
-    public static String JQUERY_CLASS_PLUGIN_MIN = "plugins/jquery.class-min.js";
+    public static final String JQUERY_CORE = "core/jquery-1.6.2.js";
+    public static final String JQUERY_CORE_MIN = "core/jquery-1.6.2-min.js";
 
-    public static String JQUERY_NAMESPACE_PLUGIN = "plugins/jquery.namespace.js";
-    public static String JQUERY_NAMESPACE_PLUGIN_MIN = "plugins/jquery.namespace-min.js";
+    public static final String JQUERY_CLASS_PLUGIN = "plugins/jquery.class.js";
+    public static final String JQUERY_CLASS_PLUGIN_MIN = "plugins/jquery.class-min.js";
 
-    public static String JQUERY_HEAD_PLUGIN = "plugins/jquery.head-1.0.3b.js";
-    public static String JQUERY_HEAD_PLUGIN_MIN = "plugins/jquery.head-1.0.3b-min.js";
+    public static final String JQUERY_NAMESPACE_PLUGIN = "plugins/jquery.namespace.js";
+    public static final String JQUERY_NAMESPACE_PLUGIN_MIN = "plugins/jquery.namespace-min.js";
 
-    public static String JQUERY_UI = "ui/jquery-ui-1.8.16.js";
-    public static String JQUERY_UI_MIN = "ui/jquery-ui-1.8.16-min.js";
+    public static final String JQUERY_HEAD_PLUGIN = "plugins/jquery.head-1.0.3b.js";
+    public static final String JQUERY_HEAD_PLUGIN_MIN = "plugins/jquery.head-1.0.3b-min.js";
 
-    public static String JQUERY_ALL = "jquery-all.js";
-    public static String JQUERY_ALL_MIN = "jquery-all-min.js";
+    public static final String JQUERY_UI = "ui/jquery-ui-1.8.16.js";
+    public static final String JQUERY_UI_MIN = "ui/jquery-ui-1.8.16-min.js";
+
+    public static final String JQUERY_ALL = "jquery-all.js";
+    public static final String JQUERY_ALL_MIN = "jquery-all-min.js";
 
 }
