4efc04cb32319a84cc300ba0d0f9ea3d01da8f16
Mohammad Nour <m.nour@onehippo.com>
2012-03-12 15:06:55 +0000

    CMS7-5870: Use the jax-rs channel manager service in the channel manager user interface   - Removed un-required import of Credentials

--- cms/engine/src/main/java/org/hippoecm/frontend/service/restproxy/RestProxyServicePlugin.java
+++ cms/engine/src/main/java/org/hippoecm/frontend/service/restproxy/RestProxyServicePlugin.java
@@ -5,7 +5,6 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 
-import javax.jcr.Credentials;
 import javax.jcr.SimpleCredentials;
 import javax.security.auth.Subject;
 import javax.ws.rs.core.MediaType;
