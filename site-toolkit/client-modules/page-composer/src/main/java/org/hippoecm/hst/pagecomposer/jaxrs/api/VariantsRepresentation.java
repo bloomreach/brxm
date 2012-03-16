5e77b3fb0e0d98113563d3e01da4de3530c42943
Ard Schrijvers <a.schrijvers@onehippo.com>
2012-03-16 13:44:55 +0000

    HSTTWO-2073 introduce a Variants abstract representation that clients can extend when they want to implement a custom variant

---
+++ site-toolkit/client-modules/page-composer/src/main/java/org/hippoecm/hst/pagecomposer/jaxrs/api/VariantsRepresentation.java
@@ -0,0 +1,61 @@
+/*
+ *  Copyright 2012 Hippo.
+ * 
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.hippoecm.hst.pagecomposer.jaxrs.api;
+
+/**
+ * If a developer wants to inject 'custom' variants (a variant reflects a 'prefix' in hst component configuration parameters),
+ * his representation must comply to this interface : This is because CMS UI relies on it
+ */
+public abstract class VariantsRepresentation {
+
+    private String id;
+    private String name;
+    private String description;
+    private String avatarName;
+    
+    public String getId() {
+        return id;
+    }
+
+    public void setId(final String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(final String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(final String description) {
+        this.description = description;
+    }
+
+    public String getAvatarName() {
+        return avatarName;
+    }
+
+    public void setAvatarName(final String avatarName) {
+        this.avatarName = avatarName;
+    }
+
+}
