d35d04c52e75224b7b373e783fa1464afa44bcee
Ard Schrijvers <a.schrijvers@onehippo.com>
2012-03-20 13:13:49 +0000

    HSTTWO-1003 remove unused logger

--- site-toolkit/toolkit-resources/addon/toolkit-cnd/cnd/src/main/java/org/hippoecm/hst/upgrade/Upgrader_MicroCndChange_2_1_2.java
+++ site-toolkit/toolkit-resources/addon/toolkit-cnd/cnd/src/main/java/org/hippoecm/hst/upgrade/Upgrader_MicroCndChange_2_1_2.java
@@ -18,14 +18,10 @@ package org.hippoecm.hst.upgrade;
 import org.hippoecm.repository.ext.UpdaterContext;
 import org.hippoecm.repository.ext.UpdaterItemVisitor.NamespaceVisitor;
 import org.hippoecm.repository.ext.UpdaterModule;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class Upgrader_MicroCndChange_2_1_2 implements UpdaterModule {
     @SuppressWarnings("unused")
     private final static String SVN_ID = "$Id: Upgrader2_24_A.java 31706 2011-12-07 12:34:06Z aschrijvers $";
-
-    private static Logger log = LoggerFactory.getLogger(Upgrader_MicroCndChange_2_1_2.class);
 
     public void register(final UpdaterContext context) {
         context.registerName("upgrader-microchange_2_1_2");
