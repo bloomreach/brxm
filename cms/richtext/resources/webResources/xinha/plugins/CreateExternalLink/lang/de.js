2a797250a6880bafa192ac2cdb9e9155464ef1db
Jannis Bloemendal <j.bloemendal@onehippo.com>
2012-04-11 16:20:09 +0000

    CMS7-6068 Internationalisation of Xinha Plugins

---
+++ cms/richtext/resources/webResources/xinha/plugins/CreateExternalLink/lang/de.js
@@ -0,0 +1,6 @@
+// I18N constants
+// LANG: "de", ENCODING: UTF-8
+
+{
+  "External link": "Externer Link"
+}
---
+++ cms/richtext/resources/webResources/xinha/plugins/CreateLink/lang/de.js
@@ -0,0 +1,6 @@
+// I18N constants
+// LANG: "de", ENCODING: UTF-8
+
+{
+  "Internal link": "Interner Link"
+}
