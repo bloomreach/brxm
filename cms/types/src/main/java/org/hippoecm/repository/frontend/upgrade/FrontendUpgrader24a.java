2eb4039109a59b07caf3f8f125f218d483aaa3ce
Ard Schrijvers <a.schrijvers@onehippo.com>
2012-05-01 15:32:38 +0000

    CMS7-5661 added upgrader. Because the frontend.cnd did not specify a start tag yet, we need to remove the FrontendUpgrader24A.java again from the pom.xml when we change the frojntend.cnd again. I've added comments for this.

---
+++ cms/types/src/main/java/org/hippoecm/repository/frontend/upgrade/FrontendUpgrader24a.java
@@ -0,0 +1,33 @@
+/*
+ *  Copyright 2010 Hippo.
+ * 
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.hippoecm.repository.frontend.upgrade;
+
+import org.hippoecm.repository.ext.UpdaterContext;
+import org.hippoecm.repository.ext.UpdaterItemVisitor.NamespaceVisitor;
+import org.hippoecm.repository.ext.UpdaterModule;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class FrontendUpgrader24a implements UpdaterModule {
+
+    private static Logger log = LoggerFactory.getLogger(FrontendUpgrader24a.class);
+
+    public void register(final UpdaterContext context) {
+        context.registerName("v24a-cms-frontend-upgrader");
+        context.registerEndTag("frontend-v24");
+        context.registerVisitor(new NamespaceVisitor(context, "frontend", getClass().getClassLoader().getResourceAsStream("frontend.cnd")));
+    }
+}
