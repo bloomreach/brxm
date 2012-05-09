51dce6580a9e4f1187aa7ea554da2607191be6e0
Frank van Lankvelt <f.vanlankvelt@onehippo.com>
2012-05-09 12:00:50 +0000

    CMS7-5605: add reference validator for links
    applying patch from Arthur; thanks!
    
    A NodeReferenceValidator checks if the value is null, empty or points to the
    default *empty_node*, currently the Jcr-root. The validator is configured for
    the hippo:mirror field.
    
    Translations still need to be added.

---
+++ cms/editor/frontend/src/main/java/org/hippoecm/frontend/editor/validator/plugins/NodeReferenceValidator.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2012 Hippo.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.hippoecm.frontend.editor.validator.plugins;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.apache.wicket.model.IModel;
+import org.hippoecm.frontend.model.JcrNodeModel;
+import org.hippoecm.frontend.plugin.IPluginContext;
+import org.hippoecm.frontend.plugin.config.IPluginConfig;
+import org.hippoecm.frontend.validation.IFieldValidator;
+import org.hippoecm.frontend.validation.ValidationException;
+import org.hippoecm.frontend.validation.Violation;
+
+public class NodeReferenceValidator extends AbstractCmsValidator {
+
+    public NodeReferenceValidator(IPluginContext context, IPluginConfig config) {
+        super(context, config);
+    }
+
+    @Override
+    public void preValidation(final IFieldValidator type) throws ValidationException {
+        if (!"String".equals(type.getFieldType().getType())) {
+            throw new ValidationException("Invalid validation exception; cannot validate non-string field for emptiness");
+        }
+    }
+
+    @Override
+    public Set<Violation> validate(final IFieldValidator fieldValidator, final JcrNodeModel model, final IModel childModel) throws ValidationException {
+        Set<Violation> violations = new HashSet<Violation>();
+        String ref = (String) childModel.getObject();
+        if(ref == null || ref.equals("") || ref.equals("cafebabe-cafe-babe-cafe-babecafebabe")) {
+            violations.add(fieldValidator.newValueViolation(childModel, "reference-is-empty"));
+        }
+        return violations;
+    }
+}
