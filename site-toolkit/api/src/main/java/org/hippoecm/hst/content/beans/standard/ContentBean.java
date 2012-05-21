ee9a897f4a55be2b0f62acde6227cb79d4c1548e
Ard Schrijvers <a.schrijvers@onehippo.com>
2012-05-21 15:46:44 +0000

    HSTTWO-2151 introduce a new base ContentBean which interface which is the base marker interface for all beans, and add an interface that extends from this one that is the IdentifiableContentBean : This is a bean that must have a getPath and setPath impl : these beans can be indexed. A Compound bean, for example Address, could only implement ContentBean as it is not needed to be identifiable. Also added lots of unit tests for creating Solr documents, and fixed the DocumentObjectBinder whereever was needed

---
+++ site-toolkit/api/src/main/java/org/hippoecm/hst/content/beans/standard/ContentBean.java
@@ -0,0 +1,61 @@
+/*
+ *  Copyright 2012 Hippo.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.hippoecm.hst.content.beans.standard;
+
+import org.hippoecm.hst.content.beans.index.IndexField;
+
+/**
+ * The marker interface for all beans: This includes beans that can be completely
+ * independent of jcr, The beans implementing this {@link ContentBean} don't need a {@link org.hippoecm.hst.content.beans.standard.IdentifiableContentBean#getPath()}
+ * identifier. Beans that should be possible to be indexed in something like an inversed index, need to implement {@link IdentifiableContentBean}. 
+ * Typically classes that implement this {@link ContentBean} but not {@link IdentifiableContentBean} are compounds of an {@link IdentifiableContentBean}
+ *
+ * For example:
+ *
+ * <pre>
+ * <code>
+ *     public class NewsBean implements IdentifiableContentBean {
+ *
+ *
+ *         public  String getPath() {
+ *             // return path
+ *         }
+ *
+ *        public  void setPath(String path) {
+ *            // set path
+ *        }
+ *
+ *        public Author getAuthor() {
+ *            // return author
+ *        }
+ *
+ *     }
+ *
+ *     public class Author implements ContentBean {
+ *
+ *         public String getName() {
+ *             // return name
+ *         }
+ *
+ *     }
+ *
+ * </code>
+ * </pre>
+ *
+ */
+public interface ContentBean {
+
+}
