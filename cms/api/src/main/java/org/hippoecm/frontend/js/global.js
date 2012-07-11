df16f55593b2b8e5dbdf50d8007eb1a7efc5f730
Jannis Bloemendal <j.bloemendal@onehippo.com>
2012-07-11 10:51:25 +0000

    CMS7-6279 Add String.format() to CMS API global javascript

--- cms/api/src/main/java/org/hippoecm/frontend/js/global.js
+++ cms/api/src/main/java/org/hippoecm/frontend/js/global.js
@@ -37,5 +37,14 @@
         };
     }
 
+    String.prototype.format = function() {
+        var formatted = this;
+        for (var i = 0; i < arguments.length; i++) {
+            var regexp = new RegExp('\\{' + i + '\\}', 'gi');
+            formatted = formatted.replace(regexp, arguments[i]);
+        }
+        return formatted;
+    };
+
 })();
 
