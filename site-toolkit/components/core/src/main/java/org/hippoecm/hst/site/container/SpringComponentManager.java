530eb2669a5869b604a979195a96cb660e96d4ae
Woonsan Ko <w.ko@onehippo.com>
2012-07-28 22:00:48 +0000

    HSTTWO-2237: Restoring the state to the previous state because I think I have fixed this issue without explicit separate issue.

--- site-toolkit/components/core/src/main/java/org/hippoecm/hst/site/container/SpringComponentManager.java
+++ site-toolkit/components/core/src/main/java/org/hippoecm/hst/site/container/SpringComponentManager.java
@@ -156,7 +156,7 @@ public class SpringComponentManager implements ComponentManager {
                 log.warn("Failed to close non-starting module instance, " + addonModuleInstance.getFullName() + ".", e);
             }
             
-            addonModuleInstancesMap.remove(addonModuleInstance.getName());
+            addonModuleInstancesMap.remove(addonModuleInstance);
             addonModuleInstancesList.remove(addonModuleInstance);
         }
     }
