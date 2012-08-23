731f26147d8ff5a65bd9ffc22a0da2df5c3b90f1
Unico Hommes <u.hommes@onehippo.com>
2012-08-23 13:44:23 +0000

    REPO-464 unit test for updater executor

---
+++ repository/engine/src/test/java/org/onehippo/repository/update/UpdaterExecutorTest.java
@@ -0,0 +1,173 @@
+/*
+ *  Copyright 2012 Hippo.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.onehippo.repository.update;
+
+import javax.jcr.Node;
+import javax.jcr.NodeIterator;
+import javax.jcr.RepositoryException;
+
+import org.hippoecm.repository.TestCase;
+import org.hippoecm.repository.util.JcrUtils;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+public class UpdaterExecutorTest extends TestCase {
+
+    private final String[] content = {
+            "/test", "nt:unstructured",
+            "/test/foo", "nt:unstructured",
+            "/test/foo/bar", "nt:unstructured",
+            "/test/bar", "nt:unstructured",
+            "/test/bar/foo", "nt:unstructured"
+    };
+
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+        build(session, content);
+        session.save();
+    }
+
+    @Override
+    public void tearDown() throws Exception {
+        final Node registry = JcrUtils.getNodeIfExists("/hippo:configuration/hippo:update/hippo:registry", session);
+        if (registry != null) {
+            final NodeIterator updaters = registry.getNodes();
+            while (updaters.hasNext()) {
+                updaters.nextNode().remove();
+            }
+        }
+        final Node test = JcrUtils.getNodeIfExists("/test", session);
+        if (test != null) {
+            final NodeIterator nodes = test.getNodes();
+            while (nodes.hasNext()) {
+                nodes.nextNode().remove();
+            }
+        }
+        session.save();
+        super.tearDown();
+    }
+
+    @Test
+    public void testRunPathVisitorUpdate() throws Exception {
+        testRunPathVisitor(false, false);
+    }
+
+    @Test
+    public void testRunQueryVisitorUpdate() throws Exception {
+        testRunQueryVisitor(false, false);
+    }
+
+    @Test
+    public void testRunPathVisitorRevert() throws Exception {
+        testRunPathVisitor(true, false);
+    }
+
+    @Test
+    public void testRunQueryVisitorRevert() throws Exception {
+        testRunQueryVisitor(true, false);
+    }
+
+    @Test
+    public void testDryRunDoesNotSaveChanges() throws Exception {
+        testRunQueryVisitor(false, true);
+    }
+
+
+    private void testRunPathVisitor(boolean revert, boolean dryRun) throws Exception {
+        final String[] content = new String[] {
+                "/hippo:configuration/hippo:update/hippo:registry/pathtest", "hipposys:updaterinfo",
+                "hipposys:class", "org.onehippo.repository.update.UpdaterExecutorTest$TestUpdater",
+                "hipposys:path", "/test"
+        };
+        build(session, content);
+        final Node updaterNode = session.getNode("/hippo:configuration/hippo:update/hippo:registry/pathtest");
+        updaterNode.setProperty("hipposys:revert", revert);
+        updaterNode.setProperty("hipposys:dryrun", dryRun);
+        session.save();
+
+        final UpdaterExecutor updaterExecutor = new UpdaterExecutor(updaterNode, session);
+        updaterExecutor.execute();
+        updaterExecutor.destroy();
+        assertEquals(2, JcrUtils.getLongProperty(updaterNode, "hipposys:updatedcount", -1l));
+        assertEquals(1, JcrUtils.getLongProperty(updaterNode, "hipposys:failedcount", -1l));
+        assertEquals(2, JcrUtils.getLongProperty(updaterNode, "hipposys:skippedcount", -1l));
+        final String qux = JcrUtils.getStringProperty(session.getNode("/test/bar"), "qux", null);
+        if (dryRun) {
+            assertNull(qux);
+        } else {
+            assertEquals(revert ? "reverted" : "updated", qux);
+        }
+    }
+
+    private void testRunQueryVisitor(boolean revert, boolean dryRun) throws Exception {
+        final String[] content = new String[] {
+                "/hippo:configuration/hippo:update/hippo:registry/querytest", "hipposys:updaterinfo",
+                "hipposys:class", "org.onehippo.repository.update.UpdaterExecutorTest$TestUpdater",
+                "hipposys:query", "/jcr:root/test//element(*, nt:unstructured)"
+        };
+        build(session, content);
+        final Node updaterNode = session.getNode("/hippo:configuration/hippo:update/hippo:registry/querytest");
+        updaterNode.setProperty("hipposys:revert", revert);
+        updaterNode.setProperty("hipposys:dryrun", dryRun);
+        session.save();
+
+        final UpdaterExecutor updaterExecutor = new UpdaterExecutor(updaterNode, session);
+        updaterExecutor.execute();
+        updaterExecutor.destroy();
+        assertEquals(2, JcrUtils.getLongProperty(updaterNode, "hipposys:updatedcount", -1l));
+        assertEquals(1, JcrUtils.getLongProperty(updaterNode, "hipposys:failedcount", -1l));
+        assertEquals(1, JcrUtils.getLongProperty(updaterNode, "hipposys:skippedcount", -1l));
+        final String qux = JcrUtils.getStringProperty(session.getNode("/test/bar"), "qux", null);
+        if (dryRun) {
+            assertNull(qux);
+        } else {
+            assertEquals(revert ? "reverted" : "updated", qux);
+        }
+    }
+
+    public static class TestUpdater extends BaseUpdater {
+
+        @Override
+        public boolean update(final Node node) throws RepositoryException {
+            if (shouldUpdate(node)) {
+                node.setProperty("qux", "updated");
+                return true;
+            }
+            return false;
+        }
+
+        @Override
+        public boolean revert(final Node node) throws RepositoryException {
+            if (shouldUpdate(node)) {
+                node.setProperty("qux", "reverted");
+                return true;
+            }
+            return false;
+        }
+
+        private boolean shouldUpdate(final Node node) throws RepositoryException {
+            if (node.getPath().equals("/test/foo/bar")) {
+                throw new RuntimeException("Deliberate exception for testing");
+            }
+            return node.getPath().startsWith("/test/bar");
+        }
+
+    }
+
+}
