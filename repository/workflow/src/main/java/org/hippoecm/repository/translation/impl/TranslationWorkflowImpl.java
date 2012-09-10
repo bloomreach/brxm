a8a8148555b386b01300957241839909bfa74349
Frank van Lankvelt <f.vanlankvelt@onehippo.com>
2012-09-10 12:06:42 +0000

    CMS7-6468: move translation workflow and virtual provider into the repository

---
+++ repository/workflow/src/main/java/org/hippoecm/repository/translation/impl/TranslationWorkflowImpl.java
@@ -0,0 +1,272 @@
+/*
+ *  Copyright 2010 Hippo.
+ * 
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.hippoecm.repository.translation.impl;
+
+import java.io.Serializable;
+import java.rmi.RemoteException;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.TreeSet;
+
+import javax.jcr.AccessDeniedException;
+import javax.jcr.ItemNotFoundException;
+import javax.jcr.Node;
+import javax.jcr.NodeIterator;
+import javax.jcr.RepositoryException;
+import javax.jcr.Session;
+
+import org.hippoecm.repository.HippoStdNodeType;
+import org.hippoecm.repository.api.Document;
+import org.hippoecm.repository.api.HippoNode;
+import org.hippoecm.repository.api.HippoNodeType;
+import org.hippoecm.repository.api.MappingException;
+import org.hippoecm.repository.api.Workflow;
+import org.hippoecm.repository.api.WorkflowContext;
+import org.hippoecm.repository.api.WorkflowException;
+import org.hippoecm.repository.ext.InternalWorkflow;
+import org.hippoecm.repository.standardworkflow.CopyWorkflow;
+import org.hippoecm.repository.standardworkflow.FolderWorkflow;
+import org.hippoecm.repository.translation.HippoTranslationNodeType;
+import org.hippoecm.repository.translation.TranslationWorkflow;
+
+public class TranslationWorkflowImpl implements TranslationWorkflow, InternalWorkflow {
+
+    private static final long serialVersionUID = 1L;
+
+    private final Session userSession;
+    private final Session rootSession;
+    private final WorkflowContext workflowContext;
+    private final Node rootSubject;
+    private final Node userSubject;
+
+    public TranslationWorkflowImpl(WorkflowContext context, Session userSession, Session rootSession, Node subject)
+            throws RemoteException, RepositoryException {
+        this.workflowContext = context;
+        this.userSession = userSession;
+        this.rootSession = rootSession;
+        this.userSubject = this.userSession.getNodeByUUID(subject.getUUID());
+        this.rootSubject = rootSession.getNodeByUUID(subject.getUUID());
+
+        if (!userSubject.isNodeType(HippoTranslationNodeType.NT_TRANSLATED)) {
+            throw new RepositoryException("Node is not of type " + HippoTranslationNodeType.NT_TRANSLATED);
+        }
+    }
+
+    public Document addTranslation(String language, String name) throws WorkflowException, MappingException,
+            RepositoryException, RemoteException {
+        Node translations = rootSubject.getNode(HippoTranslationNodeType.TRANSLATIONS);
+        if (translations.hasNode(language)) {
+            throw new WorkflowException("Translation already exists");
+        }
+
+        Node lclContainingFolder = getContainingFolder();
+        if (!lclContainingFolder.isNodeType(HippoTranslationNodeType.NT_TRANSLATED)) {
+            throw new WorkflowException("No translated ancestor found");
+        }
+
+        // find target
+        Node folderTranslation;
+        Node folderTranslations = lclContainingFolder.getNode(HippoTranslationNodeType.TRANSLATIONS);
+        if (!folderTranslations.hasNode(language)) {
+            throw new WorkflowException("Folder was not translated to " + language);
+        }
+        folderTranslation = ((HippoNode) folderTranslations.getNode(language)).getCanonicalNode();
+        if (folderTranslation == null) {
+            throw new WorkflowException("Could not find canonical equivalent of translated folder");
+        }
+        Document targetFolder = new Document(folderTranslation.getUUID());
+        Node copiedDoc = null;
+        if (userSubject.getParent().isNodeType(HippoNodeType.NT_HANDLE)) {
+            Workflow defaultWorkflow = workflowContext.getWorkflowContext(null).getWorkflow("translation-copy",
+                    new Document(rootSubject.getUUID()));
+            if (defaultWorkflow instanceof CopyWorkflow) {
+                ((CopyWorkflow) defaultWorkflow).copy(targetFolder, name);
+            } else {
+                throw new WorkflowException("Unknown default workflow; cannot copy document");
+            }
+            NodeIterator siblings = folderTranslation.getNodes(name);
+            while (siblings.hasNext()) {
+                Node sibling = siblings.nextNode();
+                if (sibling.isNodeType(HippoNodeType.NT_HANDLE)) {
+                    copiedDoc = sibling;
+                }
+            }
+            if (copiedDoc == null) {
+                throw new WorkflowException("Could not locate handle for document after copying");
+            }
+            copiedDoc = copiedDoc.getNode(copiedDoc.getName());
+            if (!copiedDoc.isCheckedOut()) {
+                copiedDoc.checkout();
+            }
+        } else {
+            Workflow internalWorkflow = workflowContext.getWorkflowContext(null).getWorkflow("internal", targetFolder);
+            if (!(internalWorkflow instanceof FolderWorkflow)) {
+                throw new WorkflowException("Target folder does not have a folder workflow in the category 'internal'.");
+            }
+            Map<String, Set<String>> prototypes = (Map<String, Set<String>>) internalWorkflow.hints().get("prototypes");
+            if (prototypes == null) {
+                throw new WorkflowException("No prototype hints available in workflow of target folder.");
+            }
+
+            // find best matching category and type from prototypes
+            String primaryType = userSubject.getPrimaryNodeType().getName();
+            String category = null;
+            String type = null;
+            for (Map.Entry<String, Set<String>> candidate : prototypes.entrySet()) {
+                String categoryName = candidate.getKey();
+                Set<String> types = candidate.getValue();
+                if (types.contains(primaryType)) {
+                    category = categoryName;
+                    type = primaryType;
+                    break;
+                }
+                if (category == null) {
+                    category = categoryName;
+                }
+                if (type == null && types.size() > 0) {
+                    type = types.iterator().next();
+                }
+            }
+
+            if (category != null && type != null) {
+                String path = ((FolderWorkflow) internalWorkflow).add(category, type, name);
+                copiedDoc = rootSession.getNode(path);
+            } else {
+                throw new WorkflowException("No category found to use for adding translation to target folder");
+            }
+            if (!copiedDoc.isCheckedOut()) {
+                copiedDoc.checkout();
+            }
+            if (!copiedDoc.isNodeType(HippoTranslationNodeType.NT_TRANSLATED)) {
+                copiedDoc.addMixin(HippoTranslationNodeType.NT_TRANSLATED);
+            }
+            copiedDoc.setProperty(HippoTranslationNodeType.ID, userSubject.getProperty(HippoTranslationNodeType.ID)
+                    .getString());
+        }
+
+        copiedDoc.setProperty(HippoTranslationNodeType.LOCALE, language);
+        Document copy = new Document(copiedDoc.getUUID());
+
+        rootSession.save();
+        rootSession.refresh(false);
+        return copy;
+    }
+
+    public void addTranslation(String language, Document document) throws WorkflowException, MappingException,
+            RepositoryException, RemoteException {
+        if (userSubject.getNode(HippoTranslationNodeType.TRANSLATIONS).hasNode(language)) {
+            throw new WorkflowException("Language already exists");
+        }
+
+        Node copiedDocNode = rootSession.getNodeByUUID(document.getIdentity());
+        if (!copiedDocNode.isCheckedOut()) {
+            copiedDocNode.checkout();
+        }
+        if (!copiedDocNode.isNodeType(HippoTranslationNodeType.NT_TRANSLATED)) {
+            copiedDocNode.addMixin(HippoTranslationNodeType.NT_TRANSLATED);
+        }
+        copiedDocNode.setProperty(HippoTranslationNodeType.LOCALE, language);
+        copiedDocNode.setProperty(HippoTranslationNodeType.ID, userSubject.getProperty(HippoTranslationNodeType.ID)
+                .getString());
+
+        rootSession.save();
+        rootSession.refresh(false);
+    }
+
+    public Map<String, Serializable> hints() throws WorkflowException, RemoteException, RepositoryException {
+        Map<String, Serializable> hints = new TreeMap<String, Serializable>();
+
+        if (rootSubject.isNodeType(HippoStdNodeType.NT_PUBLISHABLE)) {
+            String state = rootSubject.getProperty(HippoStdNodeType.HIPPOSTD_STATE).getString();
+            if ("draft".equals(state)) {
+                hints.put("addTranslation", Boolean.FALSE);
+            } else {
+                NodeIterator siblings = rootSubject.getParent().getNodes(rootSubject.getName());
+                Node unpublished = null;
+                Node published = null;
+                while (siblings.hasNext()) {
+                    Node sibling = siblings.nextNode();
+                    if (sibling.isNodeType(HippoStdNodeType.NT_PUBLISHABLE)) {
+                        String siblingState = sibling.getProperty(HippoStdNodeType.HIPPOSTD_STATE).getString();
+                        if ("unpublished".equals(siblingState)) {
+                            unpublished = sibling;
+                        } else if ("published".equals(siblingState)) {
+                            published = sibling;
+                        }
+                    }
+                }
+                if (unpublished != null && published != null) {
+                    if ("published".equals(state)) {
+                        hints.put("addTranslation", Boolean.FALSE);
+                    }
+                }
+            }
+        }
+
+        Set<String> translations = new TreeSet<String>();
+        for (NodeIterator children = rootSubject.getNode(HippoTranslationNodeType.TRANSLATIONS).getNodes(); children
+                .hasNext();) {
+            Node child = children.nextNode();
+            translations.add(child.getName());
+        }
+        hints.put("locales", (Serializable) translations);
+
+        Set<String> available = new TreeSet<String>();
+        for (Node container = getContainingFolder(); !container.isSame(rootSession.getRootNode()); container = container
+                .getParent()) {
+            if (container.isNodeType(HippoTranslationNodeType.NT_TRANSLATED)) {
+                for (NodeIterator iter = container.getNode(HippoTranslationNodeType.TRANSLATIONS).getNodes(); iter
+                        .hasNext();) {
+                    Node translation = iter.nextNode();
+                    available.add(translation.getName());
+                }
+            }
+        }
+        hints.put("available", (Serializable) available);
+
+        hints.put("locale", getLocale(userSubject));
+        return hints;
+    }
+
+    private String getLocale(Node node) throws RepositoryException {
+        while (node.getDepth() != 0) {
+            if (node.isNodeType(HippoTranslationNodeType.NT_TRANSLATED)) {
+                if (node.hasProperty(HippoTranslationNodeType.LOCALE)) {
+                    return node.getProperty(HippoTranslationNodeType.LOCALE).getString();
+                }
+            }
+            node = node.getParent();
+        }
+        return null;
+    }
+
+    private Node getContainingFolder() throws ItemNotFoundException, AccessDeniedException, RepositoryException,
+            WorkflowException {
+        Node parent = rootSubject.getParent();
+        if (parent.isNodeType(HippoNodeType.NT_HANDLE)) {
+            parent = parent.getParent();
+        }
+        while (!parent.isSame(rootSession.getRootNode())) {
+            if (parent.isNodeType(HippoNodeType.NT_DOCUMENT)) {
+                return parent;
+            }
+            parent = parent.getParent();
+        }
+        return null;
+    }
+
+}
---
+++ repository/workflow/src/test/java/org/hippoecm/repository/translation/TranslationWorkflowTest.java
@@ -0,0 +1,204 @@
+/*
+ *  Copyright 2010 Hippo.
+ * 
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.hippoecm.repository.translation;
+
+import java.io.Serializable;
+import java.util.Map;
+import java.util.Set;
+import java.util.UUID;
+
+import javax.jcr.Node;
+import javax.jcr.NodeIterator;
+
+import org.hippoecm.repository.TestCase;
+import org.hippoecm.repository.api.Document;
+import org.hippoecm.repository.api.HippoNodeType;
+import org.hippoecm.repository.api.HippoSession;
+import org.hippoecm.repository.api.HippoWorkspace;
+import org.hippoecm.repository.api.Workflow;
+import org.hippoecm.repository.api.WorkflowManager;
+import org.hippoecm.repository.standardworkflow.FolderWorkflow;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class TranslationWorkflowTest extends TestCase {
+    private static final String INVALID_ID = "invalid id - to be overwritten by folderworkflow";
+
+    
+    static final String FOLDER_T9N_ID = "700a09f1-eac5-482c-a09e-ec0a6a5d6abc";
+    static final String DOCUMENT_T9N_ID = "dbe51269-1211-4695-9dd4-1d6ea578f134";
+    
+    String[] content = {
+        "/test/folder", "hippostd:folder",
+            "jcr:mixinTypes", "hippo:harddocument",
+        "/test/folder/document", "hippo:handle",
+            "jcr:mixinTypes", "hippo:hardhandle",
+        "/test/folder/document/document", "hippo:testdocument",
+            "hippostd:state", "unpublished",
+            "hippostd:holder", "admin",
+        "/test/folder_nl", "hippostd:folder",
+            "jcr:mixinTypes", "hippo:harddocument",
+        "/test/hipposysedit:prototype", "hippo:testdocument",
+            "hippostd:state", "draft",
+            "hippostd:holder", "admin",
+    };
+
+    @Override
+    @Before
+    public void setUp() throws Exception {
+        super.setUp(true);
+        Node root = session.getRootNode();
+        if (root.hasNode("test"))
+            root.getNode("test").remove();
+        root = root.addNode("test");
+        session.save();
+
+        build(session, content);
+
+        for (String category : new String[] { "translation", "embedded", "translation-copy", "translation-internal" }) {
+            Node workflowsNode = session.getRootNode().getNode("hippo:configuration/hippo:workflows/" + category);
+            for (NodeIterator handlers = workflowsNode.getNodes(); handlers.hasNext();) {
+                Node wfNode = handlers.nextNode();
+                if (wfNode.hasProperty(HippoNodeType.HIPPO_PRIVILEGES)) {
+                    wfNode.getProperty(HippoNodeType.HIPPO_PRIVILEGES).remove();
+                }
+            }
+        }
+        Node newDocTemplateQuery = session.getRootNode().getNode("hippo:configuration/hippo:queries/hippo:templates/new-document");
+        newDocTemplateQuery.setProperty("jcr:statement", "/jcr:root/test/hipposysedit:prototype");
+        Node prototype = session.getNode("/test/hipposysedit:prototype");
+        prototype.addMixin(HippoTranslationNodeType.NT_TRANSLATED);
+        prototype.setProperty(HippoTranslationNodeType.LOCALE, "en");
+        prototype.setProperty(HippoTranslationNodeType.ID, INVALID_ID);
+
+        Node document = session.getRootNode().getNode("test/folder/document/document");
+        document.addMixin("hippo:harddocument");
+
+        Node folder = session.getRootNode().getNode("test/folder");
+        folder.addMixin(HippoTranslationNodeType.NT_TRANSLATED);
+        folder.setProperty(HippoTranslationNodeType.LOCALE, "en");
+        folder.setProperty(HippoTranslationNodeType.ID, FOLDER_T9N_ID);
+
+        Node folderNl = session.getRootNode().getNode("test/folder_nl");
+        folderNl.addMixin(HippoTranslationNodeType.NT_TRANSLATED);
+        folderNl.setProperty(HippoTranslationNodeType.LOCALE, "nl");
+        folderNl.setProperty(HippoTranslationNodeType.ID, FOLDER_T9N_ID);
+
+        session.save();
+        session.refresh(false);
+    }
+
+    @Override
+    @After
+    public void tearDown() throws Exception {
+        Node root = session.getRootNode();
+        if (root.hasNode("test"))
+            root.getNode("test").remove();
+        super.tearDown();
+    }
+
+    @Test
+    public void testTranslateDocumentInTranslatedFolder() throws Exception {
+        WorkflowManager manager = ((HippoWorkspace) session.getWorkspace()).getWorkflowManager();
+        Node handle = session.getRootNode().getNode("test/folder/document");
+        Node document = handle.getNode(handle.getName());
+        document.addMixin(HippoTranslationNodeType.NT_TRANSLATED);
+        document.setProperty(HippoTranslationNodeType.ID, DOCUMENT_T9N_ID);
+        document.setProperty(HippoTranslationNodeType.LOCALE, "en");
+        session.save();
+        session.refresh(false);
+
+        Workflow workflowInterface = manager.getWorkflow("translation", document);
+        assertTrue(workflowInterface instanceof TranslationWorkflow);
+        TranslationWorkflow workflow = (TranslationWorkflow) workflowInterface;
+        Map<String, Serializable> hints = workflow.hints();
+        assertTrue(hints.containsKey("available"));
+        Set<String> languages = (Set<String>) hints.get("available");
+        assertTrue(languages.contains("nl"));
+
+        workflow.addTranslation("nl", "dokument");
+
+        session.refresh(false);
+        assertTrue(session.nodeExists("/test/folder_nl/dokument"));
+        assertTrue(session.nodeExists("/test/folder_nl/dokument/dokument"));
+        assertTrue(session.nodeExists("/test/folder/document/document/" + HippoTranslationNodeType.TRANSLATIONS
+                + "/nl"));
+    }
+
+    @Test
+    public void testTranslateFolder() throws Exception {
+        Node deFolder = session.getRootNode().getNode("test").addNode("folder_de", "hippostd:folder");
+        deFolder.addMixin(HippoNodeType.NT_HARDDOCUMENT);
+        session.save();
+        session.refresh(false);
+
+        WorkflowManager manager = ((HippoWorkspace) session.getWorkspace()).getWorkflowManager();
+        Workflow workflowInterface = manager.getWorkflow("translation", session.getRootNode().getNode("test/folder"));
+        assertTrue(workflowInterface instanceof TranslationWorkflow);
+        TranslationWorkflow workflow = (TranslationWorkflow) workflowInterface;
+        workflow.addTranslation("de", new Document(deFolder.getUUID()));
+
+        session.refresh(false);
+        assertTrue(session.nodeExists("/test/folder/" + HippoTranslationNodeType.TRANSLATIONS + "/de"));
+    }
+
+    @Test
+    public void testTranslatedFolderIsEmpty() throws Exception {
+        Node subFolder = session.getRootNode().getNode("test/folder").addNode("subfolder", "hippostd:folder");
+        subFolder.addMixin(HippoNodeType.NT_HARDDOCUMENT);
+        subFolder.addMixin(HippoTranslationNodeType.NT_TRANSLATED);
+        String id = UUID.randomUUID().toString();
+        subFolder.setProperty(HippoTranslationNodeType.ID, id);
+        subFolder.setProperty(HippoTranslationNodeType.LOCALE, "en");
+
+        ((HippoSession) session).copy(session.getNode("/test/folder/document"), "/test/folder/subfolder/document");
+
+        session.save();
+        session.refresh(false);
+
+        WorkflowManager manager = ((HippoWorkspace) session.getWorkspace()).getWorkflowManager();
+        Workflow workflowInterface = manager.getWorkflow("translation", session.getRootNode().getNode("test/folder/subfolder"));
+        assertTrue(workflowInterface instanceof TranslationWorkflow);
+        TranslationWorkflow workflow = (TranslationWorkflow) workflowInterface;
+        workflow.addTranslation("nl", "ondermap");
+
+        session.refresh(false);
+        assertTrue(session.nodeExists("/test/folder_nl/ondermap"));
+        assertEquals(id, session.getNode("/test/folder_nl/ondermap").getProperty(HippoTranslationNodeType.ID).getString());
+        assertFalse(session.nodeExists("/test/folder_nl/ondermap/document"));
+    }
+
+    @Test
+    public void testFolderWorkflowCreatesNewDocumentThatInheritsLocale() throws Exception {
+        WorkflowManager manager = ((HippoWorkspace) session.getWorkspace()).getWorkflowManager();
+        Workflow workflowInterface = manager.getWorkflow("internal", session.getRootNode().getNode("test/folder"));
+        assertTrue(workflowInterface instanceof FolderWorkflow);
+        FolderWorkflow fw = (FolderWorkflow) workflowInterface;
+        fw.add("new-document", "hippo:testdocument", "test-document");
+        session.refresh(true);
+
+        assertTrue(session.nodeExists("/test/folder/test-document/test-document"));
+        Node docNode = session.getNode("/test/folder/test-document/test-document");
+        assertTrue(docNode.isNodeType(HippoTranslationNodeType.NT_TRANSLATED));
+        assertEquals("en", docNode.getProperty(HippoTranslationNodeType.LOCALE).getString());
+        assertFalse(INVALID_ID.equals(docNode.getProperty(HippoTranslationNodeType.ID).getString()));
+    }
+}
