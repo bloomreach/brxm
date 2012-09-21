04df9f0ef8f8418de32acefca2981b1dd3aafd08
Unico Hommes <u.hommes@onehippo.com>
2012-09-21 07:43:23 +0000

    CMS7-6484 try to load the expected files from the target directory instead of from the source directory; some code improvements

--- cms/automatic-export/repository/src/test/java/org/onehippo/cms7/autoexport/AutoExportTest.java
+++ cms/automatic-export/repository/src/test/java/org/onehippo/cms7/autoexport/AutoExportTest.java
@@ -19,7 +19,6 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
-import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -28,20 +27,20 @@ import javax.jcr.Node;
 import javax.jcr.NodeIterator;
 import javax.jcr.PathNotFoundException;
 import javax.jcr.RepositoryException;
-import javax.jcr.UnsupportedRepositoryOperationException;
 import javax.jcr.nodetype.NoSuchNodeTypeException;
 import javax.jcr.nodetype.NodeTypeManager;
 import javax.jcr.nodetype.NodeTypeTemplate;
 import javax.jcr.observation.EventIterator;
 import javax.jcr.observation.EventListener;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.jackrabbit.core.NamespaceRegistryImpl;
 import org.apache.jackrabbit.spi.Event;
+import org.apache.tika.io.IOUtils;
 import org.custommonkey.xmlunit.Diff;
 import org.custommonkey.xmlunit.Difference;
 import org.custommonkey.xmlunit.DifferenceListener;
 import org.custommonkey.xmlunit.ElementQualifier;
-import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.Test;
@@ -69,7 +68,7 @@ import static org.onehippo.cms7.autoexport.Constants.CONFIG_NODE_PATH;
 public class AutoExportTest extends RepositoryTestCase {
 
     private static final Logger log = LoggerFactory.getLogger("org.onehippo.cms7.autoexport.test");
-    private static final long SLEEP_AFTER_SAVE = 3*1000 + 500;
+    private static final long TEN_SECONDS = 10*1000;
     
     private static final String TEST_HOME;
     private static final String PROJECT_BASE;
@@ -80,9 +79,12 @@ public class AutoExportTest extends RepositoryTestCase {
         int idx = resource.indexOf("/target/test-classes/autoexporttest/simple/hippoecm-extension.xml");
         String moduleHome = resource.substring(0, idx);
 
-        TEST_HOME = moduleHome + "/src/test/resources/autoexporttest";
+        TEST_HOME = moduleHome + "/target/test-classes/autoexporttest";
         PROJECT_BASE = moduleHome + "/target/autoexporttest";
+
+        assertTrue(new File(TEST_HOME).exists());
     }
+
     // /export-test
     private Node testRoot;
 
@@ -96,7 +98,7 @@ public class AutoExportTest extends RepositoryTestCase {
         File projectBase = new File(PROJECT_BASE);
         if (projectBase.exists()) {
             log.debug("deleting project base dir: " + projectBase.getPath());
-            delete(projectBase);
+            FileUtils.deleteDirectory(projectBase);
         }
         System.setProperty("project.basedir", PROJECT_BASE);
 
@@ -141,7 +143,7 @@ public class AutoExportTest extends RepositoryTestCase {
         build(session, content);
         session.save();
         waitForAutoExport();
-        compare("simple");
+        checkExportedFiles("simple");
         assertTrue(hasInitializeItemNode("et-simple"));
     }
 
@@ -155,7 +157,7 @@ public class AutoExportTest extends RepositoryTestCase {
         build(session, content);
         session.save();
         waitForAutoExport();
-        compare("deep");
+        checkExportedFiles("deep");
         assertTrue(hasInitializeItemNode("et-foo"));
         assertTrue(hasInitializeItemNode("et-foo-et-foo-et-foo"));
     }
@@ -170,7 +172,7 @@ public class AutoExportTest extends RepositoryTestCase {
         node1.remove();
         session.save();
         waitForAutoExport();
-        compare("simple");
+        checkExportedFiles("simple");
         // case: add and remove a context node
         // (should add and remove content resource instruction)
         Node node2 = testRoot.addNode("et:simple2", "et:node");
@@ -180,7 +182,7 @@ public class AutoExportTest extends RepositoryTestCase {
         session.save();
         waitForAutoExport();
         // result should be the same
-        compare("simple");
+        checkExportedFiles("simple");
         assertTrue(!hasInitializeItemNode("et-simple2"));
     }
     
@@ -194,7 +196,7 @@ public class AutoExportTest extends RepositoryTestCase {
         session.move("/et:tobemoved", "/et:simple");
         session.save();
         waitForAutoExport();
-        compare("simple");
+        checkExportedFiles("simple");
         assertTrue(!hasInitializeItemNode("et-tobemoved"));
     }
     
@@ -209,7 +211,7 @@ public class AutoExportTest extends RepositoryTestCase {
         session.move("/et:bar", "/et:foo");
         session.save();
         waitForAutoExport();
-        compare("deep");
+        checkExportedFiles("deep");
     }
     
 //    @Test
@@ -221,13 +223,13 @@ public class AutoExportTest extends RepositoryTestCase {
 //        sub = node.addNode("et:foo", "et:node");
 //        sub.addNode("et:foo", "et:node");
 //        super.session.save();
-//        Thread.sleep(SLEEP_AFTER_SAVE);
+//        Thread.sleep(TEN_SECONDS);
 //        // move
 //        super.session.removeItem("/et:foo");
 //        super.session.move("/et:bar", "/et:foo");
 //        super.session.save();
-//        Thread.sleep(SLEEP_AFTER_SAVE);
-//        compare("deep");
+//        Thread.sleep(TEN_SECONDS);
+//        checkExportedFiles("deep");
 //    }
 
     @Test
@@ -240,7 +242,7 @@ public class AutoExportTest extends RepositoryTestCase {
         node.remove();
         session.save();
         waitForAutoExport();
-        compare("namespace");
+        checkExportedFiles("namespace");
         assertTrue(hasInitializeItemNode("etx"));
     }
 
@@ -267,7 +269,7 @@ public class AutoExportTest extends RepositoryTestCase {
         node.remove();
         super.session.save();
         waitForAutoExport();
-        compare("namespace");
+        checkExportedFiles("namespace");
         assertTrue(hasInitializeItemNode("etx"));
     }
     
@@ -279,7 +281,7 @@ public class AutoExportTest extends RepositoryTestCase {
         ntm.registerNodeType(template, false);
         super.session.save();
         waitForAutoExport();
-        compare("nodetype");
+        checkExportedFiles("nodetype");
     }
     
     @Test
@@ -297,7 +299,7 @@ public class AutoExportTest extends RepositoryTestCase {
         ntm.registerNodeType(template2, false);
         super.session.save();
         waitForAutoExport();
-        compare("nodetypes");
+        checkExportedFiles("nodetypes");
     }
     
     @Test
@@ -312,7 +314,7 @@ public class AutoExportTest extends RepositoryTestCase {
         node.setProperty("quz", "quz");
         session.save();
         waitForAutoExport();
-        compare("basicdelta");
+        checkExportedFiles("basicdelta");
     }
 
     @Test
@@ -324,7 +326,7 @@ public class AutoExportTest extends RepositoryTestCase {
         node.addNode("et:bar").addNode("et:baz");
         session.save();
         waitForAutoExport();
-        compare("nesteddelta");
+        checkExportedFiles("nesteddelta");
     }
 
     @Test
@@ -339,7 +341,7 @@ public class AutoExportTest extends RepositoryTestCase {
         build(session, content);
         session.save();
         waitForAutoExport();
-        compare("filteruuids");
+        checkExportedFiles("filteruuids");
     }
 
     private void waitForAutoExport() throws RepositoryException {
@@ -352,23 +354,23 @@ public class AutoExportTest extends RepositoryTestCase {
         }, Event.NODE_ADDED | Event.NODE_REMOVED, "/hippo:configuration/hippo:initialize", true, null, null, true);
         try {
             synchronized (monitor) {
-                monitor.wait(SLEEP_AFTER_SAVE);
+                monitor.wait(TEN_SECONDS);
             }
         } catch (InterruptedException ignore) {}
     }
 
-    private void compare(String testCase) throws Exception {
+    private void checkExportedFiles(String testCase) throws Exception {
         Map<String, Reader> changes = new HashMap<String, Reader>();
-        loadFiles(new File(PROJECT_BASE + "/content/src/main/resources"), new File(PROJECT_BASE + "/content/src/main/resources"), changes);
+        createReadersForExportedFiles(new File(PROJECT_BASE + "/content/src/main/resources"), new File(PROJECT_BASE + "/content/src/main/resources"), changes);
         Map<String, Reader> expected = new HashMap<String, Reader>();
-        loadFiles(new File(TEST_HOME, testCase), new File(TEST_HOME, testCase), expected);
+        createReadersForExportedFiles(new File(TEST_HOME, testCase), new File(TEST_HOME, testCase), expected);
         assertEquals(expected, changes);
     }
 
-    private void loadFiles(File basedir, File file, Map<String, Reader> readers) throws IOException {
+    private void createReadersForExportedFiles(File basedir, File file, Map<String, Reader> readers) throws IOException {
         if (file.isDirectory()) {
             for (File child : file.listFiles()) {
-                loadFiles(basedir, child, readers);
+                createReadersForExportedFiles(basedir, child, readers);
             }
         } else if (file.getName().endsWith(".xml") || file.getName().endsWith(".cnd")) {
             Reader reader = new FileReader(file);
@@ -401,15 +403,8 @@ public class AutoExportTest extends RepositoryTestCase {
                 } catch (IOException e) {
                     log.error("Error comparing cnd files", e);
                 } finally {
-                    try {
-                        change.close();
-                    } catch (IOException e) {
-                    }
-                    try {
-                        er.close();
-                    } catch (IOException e) {
-                    }
-
+                    IOUtils.closeQuietly(change);
+                    IOUtils.closeQuietly(er);
                 }
             }
         }
@@ -433,8 +428,6 @@ public class AutoExportTest extends RepositoryTestCase {
             if (difference.getId() == ATTR_SEQUENCE_ID) {
                 return true;
             }
-            // TODO: sequence is only irrelevant for property nodes
-            // in hippoecm-extension.xml files
             if (difference.getId() == CHILD_NODELIST_SEQUENCE_ID) {
                 return true;
             }
@@ -459,7 +452,6 @@ public class AutoExportTest extends RepositoryTestCase {
     private static class MyElementQualifier implements ElementQualifier {
         @Override
         public boolean qualifyForComparison(Element control, Element test) {
-//                        log.debug("compare " + control.getNodeName() + " with " + test.getNodeName() + "?");
             if (control.getNodeName().equals("sv:property") && test.getNodeName().equals("sv:property")) {
                 return control.getAttribute("sv:name").equals(test.getAttribute("sv:name"));
             }
@@ -469,16 +461,7 @@ public class AutoExportTest extends RepositoryTestCase {
             return (control.getNodeName().equals("sv:value") && test.getNodeName().equals("sv:value"));
         }
     }
-    
-    private static void delete(File file) {
-        if (file.isDirectory()) {
-            for (File child : file.listFiles()) {
-                delete(child);
-            }
-        }
-        file.delete();
-    }
-    
+
     private void disableExport() {
         try {
             session.getNode(CONFIG_NODE_PATH).setProperty(CONFIG_ENABLED_PROPERTY_NAME, false);
