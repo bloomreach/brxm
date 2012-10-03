a69afa2ae6f053deb59ae04eebf310cc5aa0d603
Unico Hommes <u.hommes@onehippo.com>
2012-10-03 12:19:26 +0000

    CMS7-6531 move faceted date addon frontend module to cms core

---
+++ cms/editor/frontend/src/main/java/org/hippoecm/faceteddate/editor/DatePickerPlugin.java
@@ -0,0 +1,83 @@
+/*
+ *  Copyright 2008 Hippo.
+ * 
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.hippoecm.faceteddate.editor;
+
+import java.util.Date;
+
+import javax.jcr.Node;
+import javax.jcr.RepositoryException;
+
+import org.apache.wicket.datetime.StyleDateConverter;
+import org.apache.wicket.datetime.markup.html.basic.DateLabel;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.panel.Fragment;
+import org.apache.wicket.model.Model;
+import org.hippoecm.frontend.i18n.types.TypeTranslator;
+import org.hippoecm.frontend.model.JcrNodeModel;
+import org.hippoecm.frontend.model.nodetypes.JcrNodeTypeModel;
+import org.hippoecm.frontend.model.properties.JcrPropertyModel;
+import org.hippoecm.frontend.model.properties.JcrPropertyValueModel;
+import org.hippoecm.frontend.plugin.IPluginContext;
+import org.hippoecm.frontend.plugin.config.IPluginConfig;
+import org.hippoecm.frontend.plugins.yui.datetime.DateFieldWidget;
+import org.hippoecm.frontend.service.render.RenderPlugin;
+
+import static org.hippoecm.repository.HippoStdNodeType.HIPPOSTD_DATE;
+import static org.hippoecm.repository.HippoStdNodeType.HIPPOSTD_DAYOFWEEK;
+import static org.hippoecm.repository.HippoStdNodeType.HIPPOSTD_DAYOFYEAR;
+import static org.hippoecm.repository.HippoStdNodeType.HIPPOSTD_WEEKOFYEAR;
+
+public class DatePickerPlugin extends RenderPlugin<Date> {
+
+    private static final long serialVersionUID = 1L;
+
+    public DatePickerPlugin(IPluginContext context, IPluginConfig config) throws RepositoryException {
+        super(context, config);
+
+        setOutputMarkupId(true);
+
+        Node dateNode = ((JcrNodeModel) getDefaultModel()).getNode();
+        JcrPropertyValueModel<Date> valueModel = new JcrPropertyValueModel<Date>(
+                new JcrPropertyModel(dateNode.getProperty(HIPPOSTD_DATE)));
+
+        String mode = config.getString("mode", "view");
+        add("view".equals(mode) ? new View("value", dateNode, valueModel) : new DateFieldWidget("value",
+                valueModel, context, config));
+    }
+
+    class View extends Fragment {
+
+        public View(String id, Node dateNode, JcrPropertyValueModel<Date> valueModel) throws RepositoryException {
+            super(id, "view", DatePickerPlugin.this);
+
+            add(new DateLabel("label", valueModel, new StyleDateConverter(true)));
+
+            String weekOfYear = dateNode.hasProperty(HIPPOSTD_WEEKOFYEAR) ? dateNode.getProperty(HIPPOSTD_WEEKOFYEAR).getString() : "-";
+            add(new Label("weekofyear", weekOfYear));
+
+            if (dateNode.hasProperty(HIPPOSTD_DAYOFWEEK)) {
+                add(new Label("dayofweek", new TypeTranslator(new JcrNodeTypeModel(HIPPOSTD_DATE)).getValueName(
+                        HIPPOSTD_DAYOFWEEK, new Model<String>(dateNode.getProperty(HIPPOSTD_DAYOFWEEK).getString()))));
+            } else {
+                add(new Label("dayofweek", "-"));
+            }
+
+            String dayOfYear = dateNode.hasProperty(HIPPOSTD_DAYOFYEAR) ? dateNode.getProperty(HIPPOSTD_DAYOFYEAR).getString() : "-";
+            add(new Label("dayofyear", dayOfYear));
+
+        }
+    }
+}
