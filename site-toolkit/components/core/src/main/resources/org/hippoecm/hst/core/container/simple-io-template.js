226646fc77da592f4a492f7008fc68ec55428ade
Jannis Bloemendal <j.bloemendal@onehippo.com>
2012-10-03 11:14:25 +0000

    HSTTWO-2314 Decompress and simplify javascript of async component requests

--- site-toolkit/components/core/src/main/resources/org/hippoecm/hst/core/container/simple-io-template.js
+++ site-toolkit/components/core/src/main/resources/org/hippoecm/hst/core/container/simple-io-template.js
@@ -1,1 +1,54 @@
-if (typeof Hippo === 'undefined') {Hippo = {};}if (typeof Hippo.Hst === 'undefined') {Hippo.Hst = {};}Hippo.Hst.AsyncPage = {xmlHttpFactories : [function () { return new XMLHttpRequest() },function () { return new ActiveXObject("Msxml2.XMLHTTP") },function () { return new ActiveXObject("Msxml3.XMLHTTP") },function () { return new ActiveXObject("Microsoft.XMLHTTP") }],load : function() {var result = document.getElementsByClassName("_async");for (var i=0, length=result.length; i< length; i++) {(function(element) {this.sendRequest(element.id, function(xmlHttp) {var fragment = document.createDocumentFragment();var tmpDiv = document.createElement('tmpDiv');tmpDiv.innerHTML = xmlHttp.response;while (tmpDiv.firstChild) {fragment.appendChild(tmpDiv.firstChild);}var parent = element.parentNode;parent.replaceChild(fragment, element);});}).call(this, result[i]);}},sendRequest : function(url, callback) {var xmlHttp = this.createXmlHttpObject();if (!xmlHttp) {return;}xmlHttp.open("GET", url, true);xmlHttp.setRequestHeader('User-Agent', 'XMLHTTP/1.0');var self = this;xmlHttp.onreadystatechange = function () {if (xmlHttp.readyState !== 4) {return;}if (xmlHttp.status !== 200 && xmlHttp.status !== 304) {return;}callback.call(self, xmlHttp);};xmlHttp.send();},createXmlHttpObject : function() {var xmlHttp = false;for (var i = 0; i < this.xmlHttpFactories.length; i++) {try {xmlHttp = this.xmlHttpFactories[i]();} catch (e) {continue;}break;}return xmlHttp;}};
+if (typeof Hippo === 'undefined') {
+    Hippo = {};
+}
+if (typeof Hippo.Hst === 'undefined') {
+    Hippo.Hst = {};
+}
+
+Hippo.Hst.AsyncPage = {
+
+    load : function() {
+        var result = document.getElementsByClassName("_async");
+
+        for (var i=0, length=result.length; i< length; i++) {
+            (function(element) {
+                this.sendRequest(element.id, function(xmlHttp) {
+                    var fragment = document.createDocumentFragment();
+                    var tmpDiv = document.createElement('tmpDiv');
+                    tmpDiv.innerHTML = xmlHttp.response;
+                    while (tmpDiv.firstChild) {
+                        fragment.appendChild(tmpDiv.firstChild);
+                    }
+                    var parent = element.parentNode;
+                    parent.replaceChild(fragment, element);
+                });
+            }).call(this, result[i]);
+        }
+    },
+
+    sendRequest : function(url, callback) {
+        var self, xmlHttpRequest;
+        try {
+            xmlHttpRequest = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject("Microsoft.XMLHTTP");
+        } catch (e) {
+            if (window.console) {
+                console.error();
+            }
+        }
+
+        xmlHttpRequest.open("GET", url, true);
+        xmlHttpRequest.setRequestHeader('User-Agent', 'XMLHTTP/1.0');
+        self = this;
+        xmlHttpRequest.onreadystatechange = function () {
+            if (xmlHttpRequest.readyState !== 4) {
+                return;
+            }
+            if (xmlHttpRequest.status !== 200 && xmlHttpRequest.status !== 304) {
+                return;
+            }
+            callback.call(self, xmlHttpRequest);
+        };
+
+        xmlHttpRequest.send();
+    }
+};
