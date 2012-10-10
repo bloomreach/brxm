594dae1a164871d9752b3c9b7296a6a678b70477
Unico Hommes <u.hommes@onehippo.com>
2012-10-10 07:34:08 +0000

    CMS7-6535 narrow the scope of the listener to avoid receiving events from unrelated parts of the system

--- cms/automatic-export/repository/src/test/java/org/onehippo/cms7/autoexport/EventTest.java
+++ cms/automatic-export/repository/src/test/java/org/onehippo/cms7/autoexport/EventTest.java
@@ -108,9 +108,7 @@ public class EventTest extends RepositoryTestCase {
         try {
             listener = addListener();
             build(session, add);
-            log.debug("Saving ..");
             session.save();
-            log.debug("Saved");
             Collection<Event> events = listener.getEvents();
             assertEquals(6, events.size());
             assertTrue(events.contains(new ExportEvent(Event.NODE_ADDED, "/test/quux")));
@@ -240,7 +238,7 @@ public class EventTest extends RepositoryTestCase {
         TestListener listener = new TestListener();
         int eventTypes = Event.NODE_ADDED | Event.NODE_REMOVED | Event.NODE_MOVED
                 | Event.PROPERTY_ADDED | Event.PROPERTY_CHANGED | Event.PROPERTY_REMOVED;
-        manager.addEventListener(listener, eventTypes, "/", true, null, null, false);
+        manager.addEventListener(listener, eventTypes, "/test", true, null, null, false);
         return listener;
     }
     
