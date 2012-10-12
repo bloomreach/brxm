a531fc7330f37aead55c06229ec0fea223fedefb
Mathijs den Burger <m.denburger@onehippo.com>
2012-10-12 16:37:22 +0000

    HSTTWO-2318: added separate interfaces for global and component-specific variants

---
+++ site-toolkit/client-modules/page-composer/src/main/java/org/hippoecm/hst/pagecomposer/jaxrs/api/ComponentVariantRepresentation.java
@@ -0,0 +1,26 @@
+/*
+ *  Copyright 2012 Hippo.
+ * 
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.hippoecm.hst.pagecomposer.jaxrs.api;
+
+/**
+ * If a developer wants to inject 'custom' component-specific variants (a variant reflects a 'prefix' in hst component
+ * configuration parameters), his representation must comply to this interface : This is because CMS UI relies on it.
+ */
+public interface ComponentVariantRepresentation extends GlobalVariantRepresentation {
+
+    public Object getData();
+
+}
