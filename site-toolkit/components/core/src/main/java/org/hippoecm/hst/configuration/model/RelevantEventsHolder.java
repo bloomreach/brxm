f66a5f4e5647096e3354e78ce04c5ff12d1d2dbc
Ard Schrijvers <a.schrijvers@onehippo.com>
2012-11-02 08:05:40 +0000

    HSTTWO-2325 + the missing classes. Apologies Implemented : 1) do a complete re-implementation of the event processing 2) do finegrained reloading on some *major* nodes instead of finegrained reloads in some structure 3) do not rely on property change, node add or node remove but just mark a path as changed 4) do a pre-validate in the hstManagerImpl before going to build the enhanced hst model : During pre-validate, I can already identify problems. In case of problems, a fullblown reload is done 5) introduce a ModelLoadingException that is a runtime exception that can be thrown when it is detected that the model is incorrect : When this exception is thrown, a fullblown reload is done. Note that I try to detect whether the model is loaded incorrectly by comparing with loaded sites in a previous run. This is because of course the hst configuration can also be *really* broken in jcr
    Result:
    Where I used to get an error within 10 seconds when publishing and editing hst configs in a cluster and at the same time request the sites, I now did not get these failures any more. 
    From time to time, you see this message:
    01.11.2012 14:54:30 WARN  [org.hippoecm.hst.configuration.model.HstManagerImpl.getVirtualHosts():263] Model was possibly not build correctly. A total rebuild will be done now after flushing all caches. Reason : org.hippoecm.hst.configuration.model.HstManagerImpl$ModelLoadingException: Found HST SITE '/hst:hst/hst:sites/hippogogreen' that does not have a configuration node. However, it was loaded correctly before. 
    
    This is an indication that a retry will be done. I think it can only happen when editing and publishing a single site a couple of times within seconds (the other cluster node has pulled in some changes and send out jcr events, and then pull in more changes during reloading the model but cannot yet process the jcr events in the hst manager)
    
    TODO I still need to check
     1) REST plain pipeline mounts that have a mountpoint that does not point to a hst:site node but for example directly to assets
      2) Whether preview mounts still work correctly with login

---
+++ site-toolkit/components/core/src/main/java/org/hippoecm/hst/configuration/model/RelevantEventsHolder.java
@@ -0,0 +1,284 @@
+/*
+ *  Copyright 2012 Hippo.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.hippoecm.hst.configuration.model;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
+import javax.jcr.RepositoryException;
+import javax.jcr.observation.Event;
+
+import org.apache.commons.lang.StringUtils;
+import org.hippoecm.hst.configuration.HstNodeTypes;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * This is a utility class to process jcr events in a from jcr detached hst events. This class
+ * is targeted for keeping track of which configuration nodes are deleted, modified, or added. It also collapses events
+ * that result in the same behavior: For example, an event for a node remove at /foo/bar/lux is not added if there is
+ * already a node remove event for /foo/bar. 
+ * 
+ */
+public class RelevantEventsHolder {
+
+    private static final Logger log = LoggerFactory.getLogger(RelevantEventsHolder.class);
+
+    final private String configPath;
+    final private  String commonCatalogPath;
+    final private String hostsPath;
+    final private String blueprintsPath;
+    final private String channelsPath;
+
+
+    enum EventFor {
+        ROOT_CONFIGURATION,
+        CATALOG,
+        HOSTS,
+        BLUEPRINTS,
+        CHANNELS,
+        SITE
+    }
+
+    private Set<RelevantEvent> configurationEvents = new HashSet<RelevantEvent>();
+    private List<String> allEvents = new ArrayList<String>();
+    
+    public RelevantEventsHolder(String rootPath) {
+        configPath = rootPath+"/hst:configurations";
+        commonCatalogPath = configPath+"/hst:catalog";
+        hostsPath = rootPath+"/hst:hosts";
+        blueprintsPath = rootPath+"/hst:blueprints";
+        channelsPath = rootPath+"/hst:channels";
+    }
+
+    public void clear() {
+        configurationEvents.clear();
+        allEvents.clear();
+    }
+
+    public void addEvent(final Event jcrEvent) throws RepositoryException {
+        allEvents.add(jcrEvent.getPath());
+        if (isPropertyEventAndNotRelevant(jcrEvent)) {
+            return;
+        }
+        RelevantEvent event = new RelevantEvent(jcrEvent);
+        boolean added = configurationEvents.add(event);
+        if (added) {
+            log.debug("Added event '{}'", event.relevantPath);
+        }
+    }
+
+    public List<String> getAllEvents() {
+        return allEvents;
+    }
+
+    public boolean hasEvents() {
+        return !configurationEvents.isEmpty();
+    }
+
+    public boolean hasHstRootConfigurationEvents() {
+        return hasEventsOfType(EventFor.ROOT_CONFIGURATION);
+    }
+
+    public Iterator<String> getHstRootConfigurationPathEvents() {
+        return getEventsForOfType(EventFor.ROOT_CONFIGURATION);
+    }
+
+    public boolean hasSiteEvents() {
+        return hasEventsOfType(EventFor.SITE);
+    }
+
+    public Iterator<String> getRootSitePathEvents() {
+        return getEventsForOfType(EventFor.SITE);
+    }
+
+    public boolean hasCommonCatalogEvents() {
+        return hasEventsOfType(EventFor.CATALOG);
+    }
+
+    public Iterator<String> getCommonCatalogPathEvents() {
+        return getEventsForOfType(EventFor.CATALOG);
+    }
+
+    public boolean hasHostEvents() {
+        return hasEventsOfType(EventFor.HOSTS);
+    }
+
+    public Iterator<String> getHostPathEvents() {
+        return getEventsForOfType(EventFor.HOSTS);
+    }
+
+    public boolean hasBlueprintEvents() {
+        return hasEventsOfType(EventFor.BLUEPRINTS);
+    }
+
+    public Iterator<String> getBlueprintPathEvents() {
+        return getEventsForOfType(EventFor.BLUEPRINTS);
+    }
+
+    public boolean hasChannelEvents() {
+        return hasEventsOfType(EventFor.CHANNELS);
+    }
+
+    public Iterator<String> getChannelPathEvents() {
+        return getEventsForOfType(EventFor.CHANNELS);
+    }
+
+    private boolean hasEventsOfType(final EventFor eventFor) {
+        for (RelevantEvent event : configurationEvents) {
+            if (event.eventFor == eventFor) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private Iterator<String> getEventsForOfType(final EventFor eventFor) {
+        List<String> eventsForOfType = new ArrayList<String>();
+        for (RelevantEvent event : configurationEvents) {
+            if (event.eventFor == eventFor) {
+                eventsForOfType.add(event.relevantPath);
+            }
+        }
+        return eventsForOfType.iterator();
+    }
+
+    private boolean isPropertyEventAndNotRelevant(final Event jcrEvent) throws RepositoryException {
+        if (isPropertyEvent(jcrEvent)) {
+            String propName = StringUtils.substringAfterLast(jcrEvent.getPath(), "/");
+            if (StringUtils.equals(HstNodeTypes.GENERAL_PROPERTY_LOCKED_BY, propName) || StringUtils.equals(HstNodeTypes.GENERAL_PROPERTY_LOCKED_ON, propName)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean isPropertyEvent(final Event jcrEvent) {
+        return jcrEvent.getType() == Event.PROPERTY_ADDED
+                || jcrEvent.getType() == Event.PROPERTY_CHANGED
+                || jcrEvent.getType() == Event.PROPERTY_REMOVED;
+    }
+
+    private class RelevantEvent {
+      
+        private String relevantPath;
+        private EventFor eventFor;
+
+        RelevantEvent(final Event jcrEvent) throws RepositoryException {
+            String nodePath;
+            if (isPropertyEvent(jcrEvent)) {
+                String eventPath = jcrEvent.getPath();
+                nodePath = StringUtils.substringBeforeLast(eventPath, "/");
+            } else {
+                nodePath = jcrEvent.getPath();
+            }
+            eventFor = getEventFor(nodePath);
+            relevantPath = getRelevantPathOfEventFor(eventFor, nodePath);
+
+        }
+
+        private String getRelevantPathOfEventFor(final EventFor eventFor, final String nodePath) {
+            switch (eventFor) {
+                case BLUEPRINTS :
+                    // blueprints always get completely reloaded 
+                    return blueprintsPath;
+                case CHANNELS :
+                    // channels always get completely reloaded 
+                    return channelsPath;
+                case HOSTS :
+                    // hosts always get completely reloaded
+                    return hostsPath;
+                case CATALOG :
+                    // commont catalog is completely reloaded, so just use commonCatalogPath
+                    return commonCatalogPath;
+                case SITE :
+                    return getRelevantPathForSiteEvent(nodePath);
+                case ROOT_CONFIGURATION:
+                    return getRelevantPathForHstConfiguration(nodePath);
+            }
+            throw new IllegalStateException("Unknown EventFor '"+eventFor+"'");
+        }
+
+        private String getRelevantPathForHstConfiguration(final String nodePath) {
+            if (nodePath.equals(configPath)) {
+                return nodePath;
+            }
+            // for any change below a hst:configuration node including itself, we reload that entire 
+            // hst:configuration node
+            StringBuilder relevantHstConfigurationPath = new StringBuilder(configPath).append("/");
+            String nodePathFromHstConfigurationNode =  nodePath.substring(configPath.length() + 1);
+            String hstConfigurationNodeName = StringUtils.substringBefore(nodePathFromHstConfigurationNode, "/");
+            relevantHstConfigurationPath.append(hstConfigurationNodeName);
+            return relevantHstConfigurationPath.toString();
+        }
+
+        private String getRelevantPathForSiteEvent(final String nodePath) {
+            // an event for a hst site is typically something like /hst:hst/hst:sites/myproject
+            // however, it may also be at /hst:hst/hst:sites/myproject/hst:content : The latter hst:content we are not
+            // interested in and can be removed (then the hst:site myproject will be reloaded)
+            
+            // below returns original if does not end with "/hsT:content"
+            return StringUtils.substringBefore(nodePath, "/hsT:content");
+           
+        }
+
+        private EventFor getEventFor(final String path) {
+            if (path.startsWith(commonCatalogPath + "/") || path.equals(commonCatalogPath)) {
+                return EventFor.CATALOG;
+            } else if (path.startsWith(configPath + "/") || path.equals(configPath)) {
+                return EventFor.ROOT_CONFIGURATION;
+            } else if (path.startsWith(hostsPath + "/") || path.equals(hostsPath)) {
+                return EventFor.HOSTS;
+            } else if (path.startsWith(blueprintsPath + "/") || path.equals(blueprintsPath)) {
+                return EventFor.BLUEPRINTS;
+            } else if (path.startsWith(channelsPath + "/") || path.equals(channelsPath)) {
+                return EventFor.CHANNELS;
+            } else {
+                // since the hst:sites node is a free name, we cannot check the path. If it
+                // does not start with anything above, it must be an event for a hst:sites or hst:site node
+                return EventFor.SITE;
+            }
+        }
+
+        @Override
+        public boolean equals(final Object obj) {
+            if(obj instanceof RelevantEvent) {
+                RelevantEvent compare = (RelevantEvent)obj;
+                return relevantPath.equals(compare.relevantPath);
+            } else {
+                return false;
+            }
+        }
+
+        @Override
+        public int hashCode() {
+            return relevantPath.hashCode();
+        }
+
+        @Override
+        public String toString() {
+            StringBuilder sb = new StringBuilder(128);
+            sb.append(" { ");
+            sb.append("nodePath=\"").append(relevantPath).append("\", ");
+            sb.append(" }");
+            return sb.toString();
+        }
+    }
+
+}
