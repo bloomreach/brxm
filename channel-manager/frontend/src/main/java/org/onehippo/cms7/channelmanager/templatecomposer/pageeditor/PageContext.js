7c1c7e7a006ad2d9203b080645e4bfa3365a487d
Mathijs den Burger <m.denburger@onehippo.com>
2012-11-13 15:11:56 +0000

    CMS7-6634: fixed error reporting code path

--- channel-manager/frontend/src/main/java/org/onehippo/cms7/channelmanager/templatecomposer/pageeditor/PageContext.js
+++ channel-manager/frontend/src/main/java/org/onehippo/cms7/channelmanager/templatecomposer/pageeditor/PageContext.js
@@ -204,7 +204,7 @@ Hippo.ChannelManager.TemplateComposer.PageContext = Ext.extend(Ext.util.Observab
                         ];
                         Hippo.Future.join(futures).when(function() {
                             onSuccess();
-                        }).otherwise(onFail);
+                        }).otherwise(onFail("Failed to initialize page model"));
                     } else {
                         onSuccess();
                     }
