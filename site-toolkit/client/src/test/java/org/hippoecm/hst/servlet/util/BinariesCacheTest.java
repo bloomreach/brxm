1b0ee84592612575f64935d674fa27936bf182e2
Ard Schrijvers <a.schrijvers@onehippo.com>
2012-11-20 10:35:58 +0000

    HSTTWO-2357 added unit tests to illustrate the blockingcache behavior regarding a get that returns null

--- site-toolkit/client/src/test/java/org/hippoecm/hst/servlet/util/BinariesCacheTest.java
+++ site-toolkit/client/src/test/java/org/hippoecm/hst/servlet/util/BinariesCacheTest.java
@@ -15,14 +15,11 @@
  */
 package org.hippoecm.hst.servlet.util;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
-
 import java.io.UnsupportedEncodingException;
-
-import net.sf.ehcache.CacheManager;
-import net.sf.ehcache.Ehcache;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import org.hippoecm.hst.cache.HstCache;
 import org.hippoecm.hst.cache.ehcache.HstCacheEhCacheImpl;
@@ -31,6 +28,15 @@ import org.hippoecm.hst.servlet.utils.BinaryPage;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import net.sf.ehcache.CacheManager;
+import net.sf.ehcache.Ehcache;
+import net.sf.ehcache.constructs.blocking.BlockingCache;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 public class BinariesCacheTest {
     
@@ -45,7 +51,10 @@ public class BinariesCacheTest {
         cacheManager = CacheManager.create();
         cacheManager.addCache("binariesCache");
         Ehcache ehBinariesCache = cacheManager.getCache("binariesCache");
-        hstBinariesCache = new HstCacheEhCacheImpl(ehBinariesCache);
+        BlockingCache cache = new BlockingCache(ehBinariesCache);
+        // after 10 ms a LockTimeoutException is thrown when a lock cannot be acquired
+       // cache.setTimeoutMillis(10);
+        hstBinariesCache = new HstCacheEhCacheImpl(cache);
         bc = new BinariesCache(hstBinariesCache);
     }
     
@@ -54,6 +63,7 @@ public class BinariesCacheTest {
         cacheManager.shutdown();
     }
 
+    @Test
     public void testPutGet() {
         bc.putPage(page);
         assertNotNull(bc.getPageFromBlockingCache(page.getResourcePath()));
@@ -115,4 +125,145 @@ public class BinariesCacheTest {
         assertFalse(bc.isPageStale(page, 1234L));
     }
 
+
+    @Test
+    public void testGetNonNullIsNotLocking() {
+        // PUT in cache so the item is available
+        bc.putPage(page);
+
+        Thread [] getters = new Thread[2];
+        for (int i = 0; i < getters.length; i++) {
+            getters[i] = new Thread(new Runnable() {
+                public void run() {
+                    BinaryPage elem = bc.getPageFromBlockingCache(page.getResourcePath());
+                    assertNotNull(elem);
+                }
+            });
+        }
+
+        for (int i = 0; i < getters.length; i++) {
+            getters[i].start();
+        }
+
+        try {
+            for (int i = 0; i < getters.length; i++) {
+                getters[i].join();
+            }
+        } catch (InterruptedException e) {
+        }
+
+    }
+
+
+    private volatile boolean nextThreadIsSecond =false;            
+    @Test
+    public void testGetNullIsLockingForEverWhenNoTimeoutSet() throws InterruptedException {
+        // NO put so page.getResourcePath() is not available in cache
+        final long WAIT_BEFORE_PUT = 100;
+        final List<Throwable> throwables = Collections.synchronizedList(new LinkedList<Throwable>());
+        final Thread [] getters = new Thread[2];
+        for (int i = 0; i < getters.length; i++) {
+            getters[i] = new Thread(new Runnable() {
+                public void run() {
+                    if (nextThreadIsSecond) {
+                        BinaryPage elem = bc.getPageFromBlockingCache(page.getResourcePath());
+                        // this thread will have been blocked for WAIT_BEFORE_PUT by the the 
+                        // first thread which acquired the lock and only freed it after the put
+                        try {
+                             assertNotNull(elem);
+                        } catch (AssertionError e) {
+                            throwables.add(e); 
+                        }
+                    } else {
+                        nextThreadIsSecond = true;
+                        BinaryPage elem = bc.getPageFromBlockingCache(page.getResourcePath());
+                        try {
+                            assertNull(elem);
+                        } catch (AssertionError e) {
+                            throwables.add(e);
+                        }
+                        // after WAIT_BEFORE_PUT ms we PUT an element that should free the lock again
+                        try {
+                            Thread.sleep(WAIT_BEFORE_PUT);
+                            bc.putPage(page);
+                        } catch (InterruptedException e) {
+                            
+                        }
+                    }
+                }
+            });
+        }
+
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < getters.length; i++) {
+            Thread.sleep(10);
+            getters[i].start();
+        }
+        
+        for (int i = 0; i < getters.length; i++) {
+            getters[i].join();
+        }
+        
+        assertTrue((System.currentTimeMillis() - start) > WAIT_BEFORE_PUT);
+        
+        assertTrue(throwables.isEmpty());
+    
+    }
+
+    @Test
+    public void testGetNullIsLockedUntilTimeout() {
+
+        Ehcache ehBinariesCache = cacheManager.getCache("binariesCache");
+        BlockingCache cache = new BlockingCache(ehBinariesCache);
+        // lock is held for 100 ms if item for get returns null
+        final int TIME_OUT_MILLIS = 100;
+        cache.setTimeoutMillis(TIME_OUT_MILLIS);
+        hstBinariesCache = new HstCacheEhCacheImpl(cache);
+        bc = new BinariesCache(hstBinariesCache);
+
+        // NO put so page.getResourcePath() is not available in cache
+        Thread[] getters = new Thread[2];
+        final AtomicInteger counter = new AtomicInteger(0);
+        final List<Exception> exceptions = Collections.synchronizedList(new LinkedList<Exception>());
+        final long start = System.currentTimeMillis();
+        for (int i = 0; i < getters.length; i++) {
+            getters[i] = new Thread(new Runnable() {
+                public void run() {
+                    try {
+                        BinaryPage elem = bc.getPageFromBlockingCache(page.getResourcePath());
+                        int count = counter.incrementAndGet();
+                        assertNull(elem);
+                        if (count == 2) {
+                            // THE second getter will be blocked by the FIRST one for at least cache.setTimeoutMillis(TIME_OUT_MILLIS);
+                            if((System.currentTimeMillis() - start) <= TIME_OUT_MILLIS) {
+                                throw new IllegalStateException("Not possible that this thread finished before the lock timeout of TIME_OUT_MILLIS");
+                            }
+                        }
+                    } catch (RuntimeException e) {
+                        exceptions.add(e);
+                    }
+                }
+            });
+        }
+
+        for (int i = 0; i < getters.length; i++) {
+            getters[i].start();
+        }
+
+        try {
+            for (int i = 0; i < getters.length; i++) {
+                getters[i].join();
+            }
+        } catch (InterruptedException e) {
+        }
+
+        if (!exceptions.isEmpty()) {
+            StringBuilder exInfo = new StringBuilder();
+            for (Exception ex : exceptions) {
+                exInfo.append("    " + ex.toString() + " " + ex.getMessage() + "\n");
+            }
+            fail("Failed :\n" + exInfo + "\n");
+        }
+    }
 }
+
