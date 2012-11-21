e241220ce9cf00cac20920f12e8b825b39a159d6
Ard Schrijvers <a.schrijvers@onehippo.com>
2012-11-21 11:14:19 +0000

    HSTTWO-2184 and add new solr class and jsp for indexing single documents per one

---
+++ testsuite/components/src/main/java/org/hippoecm/hst/demo/components/SolrIndexer.java
@@ -0,0 +1,80 @@
+/*
+ *  Copyright 2012 Hippo.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.hippoecm.hst.demo.components;
+
+import java.io.IOException;
+
+import javax.jcr.RepositoryException;
+
+import org.apache.solr.client.solrj.SolrServerException;
+import org.hippoecm.hst.component.support.bean.BaseHstComponent;
+import org.hippoecm.hst.content.beans.ObjectBeanManagerException;
+import org.hippoecm.hst.content.beans.query.HstQuery;
+import org.hippoecm.hst.content.beans.query.exceptions.QueryException;
+import org.hippoecm.hst.content.beans.standard.HippoBean;
+import org.hippoecm.hst.core.component.HstComponentException;
+import org.hippoecm.hst.core.component.HstRequest;
+import org.hippoecm.hst.core.component.HstResponse;
+import org.hippoecm.hst.site.HstServices;
+import org.hippoecm.hst.solr.HippoSolrClient;
+
+public class SolrIndexer extends BaseHstComponent{
+
+
+    public static final String SOLR_MODULE_NAME = "org.hippoecm.hst.solr";
+
+    @Override
+    public void doBeforeRender(final HstRequest request, final HstResponse response) throws HstComponentException {
+        String uuid = request.getParameter("indexed");
+        if (uuid != null) {
+            try {
+                String path = request.getRequestContext().getSession().getNodeByIdentifier(uuid).getPath();
+                request.setAttribute("message", "indexed succesfully : " +  path);
+            } catch (RepositoryException e) {
+                throw new HstComponentException(e);
+            }
+        }
+
+        try {
+            final HstQuery query = getQueryManager(request).createQuery(getSiteContentBaseBean(request));
+            query.setLimit(1000);
+            request.setAttribute("result", query.execute());
+        } catch (QueryException e) {
+            throw new HstComponentException(e);
+        }
+    }
+
+    @Override
+    public void doAction(final HstRequest request, final HstResponse response) throws HstComponentException {
+        String uuid = request.getParameter("uuid");
+        try {
+            HippoBean bean = (HippoBean) getObjectConverter().getObject(uuid, request.getRequestContext().getSession());
+            HippoSolrClient solrClient = HstServices.getComponentManager().getComponent(HippoSolrClient.class.getName(), SOLR_MODULE_NAME);
+            solrClient.getSolrServer().addBean(bean);
+            solrClient.getSolrServer().commit();
+            response.setRenderParameter("indexed", uuid);
+            
+        } catch (ObjectBeanManagerException e) {
+            throw new HstComponentException(e);
+        } catch (RepositoryException e) {
+            throw new HstComponentException(e);
+        } catch (IOException e) {
+            throw new HstComponentException(e);
+        } catch (SolrServerException e) {
+            throw new HstComponentException(e);
+        }
+    }
+}
---
+++ testsuite/resources/src/main/webapp/WEB-INF/jsp/components/main/solrindexer.jsp
@@ -0,0 +1,59 @@
+<%--
+  Copyright 2012 Hippo
+
+  Licensed under the Apache License, Version 2.0 (the  "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS"
+  BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License. --%>
+
+<%@ page language="java" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/xml" prefix="x" %>
+<%@ taglib uri="http://www.hippoecm.org/jsp/hst/core" prefix='hst'%>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt"%>
+
+<hst:headContribution keyHint="title"><title>Index ALL JCR Documents</title></hst:headContribution>
+
+<hst:defineObjects/>
+<div id="yui-u">
+    <c:choose>
+      <c:when test="${hstRequest.requestContext.preview}">
+       <h1>Indexing JCR Documents is only possible in LIVE </h1>
+      </c:when>
+      <c:otherwise>
+
+        <h1>Index JCR Documents for current site</h1>
+
+        <c:if test="${not empty message}">
+          <h2>${message}</h2>
+        </c:if>
+
+        <c:forEach var="bean" items="${result.hippoBeans}"
+                   varStatus="indexer">
+          <hst:link var="link" hippobean="${bean}" />
+          <ul class="list-overview">
+            <li class="title">
+              ${bean.title}
+                <form action="<hst:actionURL/>" method="POST">
+                  <input type="hidden" name="uuid" value="${bean.canonicalHandleUUID}"/>
+                  <input type="submit" value="INDEX"/>
+                </form>
+            </li>
+          </ul>
+        </c:forEach>
+        
+        <form action="<hst:actionURL/>" method="post">
+          <input type="submit" value="index"/>
+        </form>
+
+      </c:otherwise>
+    </c:choose>
+
+</div>
