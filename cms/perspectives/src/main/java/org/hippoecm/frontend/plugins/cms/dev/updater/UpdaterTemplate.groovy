86b953e4863307f523d4dd2bc6a52587358f2134
Mathijs den Burger <m.denburger@onehippo.com>
2012-11-29 14:09:54 +0000

    CMS7-16: simplified default Groovy script

--- cms/perspectives/src/main/java/org/hippoecm/frontend/plugins/cms/dev/updater/UpdaterTemplate.groovy
+++ cms/perspectives/src/main/java/org/hippoecm/frontend/plugins/cms/dev/updater/UpdaterTemplate.groovy
@@ -3,15 +3,15 @@ package org.hippoecm.frontend.plugins.cms.dev.updater
 import org.onehippo.repository.update.BaseNodeUpdateVisitor
 import javax.jcr.Node
 
-public class UpdaterTemplate extends BaseNodeUpdateVisitor {
+class UpdaterTemplate extends BaseNodeUpdateVisitor {
 
-  public boolean doUpdate(Node node) {
-    log.debug("Updating node " + node.getPath())
-    throw new UnsupportedOperationException("Updater doesn't implement doUpdate method")
+  boolean doUpdate(Node node) {
+    log.debug "Updating node ${node.path}"
+    return false
   }
 
-  public boolean undoUpdate(Node node) {
-    throw new UnsupportedOperationException("Updater doesn't implement undoUpdate method")
+  boolean undoUpdate(Node node) {
+    throw new UnsupportedOperationException('Updater does not implement undoUpdate method')
   }
 
 }
