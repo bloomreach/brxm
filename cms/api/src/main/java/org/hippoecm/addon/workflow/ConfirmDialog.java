18266a409154bc04b86535304eb1c3427e685608
Frank van Lankvelt <f.vanlankvelt@onehippo.com>
2012-12-07 16:00:19 +0000

    CMS7-6756: include the new public dialogs

---
+++ cms/api/src/main/java/org/hippoecm/addon/workflow/ConfirmDialog.java
@@ -0,0 +1,76 @@
+/*
+ *  Copyright 2012 Hippo.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.hippoecm.addon.workflow;
+
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.basic.MultiLineLabel;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.util.value.IValueMap;
+import org.hippoecm.frontend.dialog.AbstractDialog;
+import org.hippoecm.frontend.dialog.DialogConstants;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public abstract class ConfirmDialog extends AbstractDialog implements IWorkflowInvoker {
+    private static final long serialVersionUID = 1L;
+
+    static final Logger log = LoggerFactory.getLogger(ConfirmDialog.class);
+
+    private IModel<String> title;
+
+    public ConfirmDialog(IModel<String> title, IModel<String> question) {
+        this(title, null, null, question);
+    }
+
+    public ConfirmDialog(IModel<String> title, IModel<String> intro, IModel<String> text, IModel<String> question) {
+        super();
+        this.title = title;
+        if (intro == null) {
+            Label component;
+            add(component = new Label("intro"));
+            component.setVisible(false);
+        } else {
+            add(new Label("intro", intro));
+        }
+        if (text == null) {
+            Label component;
+            add(component = new Label("text"));
+            component.setVisible(false);
+        } else {
+            add(new MultiLineLabel("text", text));
+        }
+        add(new Label("question", question));
+    }
+
+    @Override
+    public IModel getTitle() {
+        return title;
+    }
+
+    @Override
+    public IValueMap getProperties() {
+        return DialogConstants.SMALL;
+    }
+
+    @Override
+    protected void onOk() {
+        try {
+            invokeWorkflow();
+        } catch (Exception e) {
+            log.error("Could not execute workflow.", e);
+        }
+    }
+}
---
+++ cms/api/src/main/java/org/hippoecm/addon/workflow/TextDialog.java
@@ -0,0 +1,53 @@
+/*
+ *  Copyright 2012 Hippo.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.hippoecm.addon.workflow;
+
+import org.apache.wicket.behavior.AttributeAppender;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.Model;
+import org.apache.wicket.util.value.IValueMap;
+import org.hippoecm.frontend.dialog.AbstractDialog;
+import org.hippoecm.frontend.dialog.DialogConstants;
+import org.hippoecm.frontend.widgets.TextAreaWidget;
+
+public abstract class TextDialog extends AbstractDialog implements IWorkflowInvoker {
+    private static final long serialVersionUID = 1L;
+
+    private IModel<String> title;
+
+    public TextDialog(IModel<String> title, IModel<String> question, IModel<String> textModel) {
+        super();
+        this.title = title;
+        add(new Label("question", question));
+
+        TextAreaWidget textfield;
+        add(textfield = new TextAreaWidget("value", textModel));
+        textfield.addBehaviourOnFormComponent(
+                new AttributeAppender("class", true, new Model<String>("text-dialog-textarea"), " "));
+        setFocus(textfield.getFocusComponent());
+    }
+
+    @Override
+    public IModel<String> getTitle() {
+        return title;
+    }
+
+    @Override
+    public IValueMap getProperties() {
+        return DialogConstants.MEDIUM;
+    }
+}
