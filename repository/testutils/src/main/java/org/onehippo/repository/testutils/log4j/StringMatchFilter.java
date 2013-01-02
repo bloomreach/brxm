d25c7ef78ab93d91f67cb3394495f87e9d22daca
Unico Hommes <u.hommes@onehippo.com>
2013-01-02 12:00:26 +0000

    REPO-572 add log4j filter that can be configured with a file of messages to filter out

---
+++ repository/testutils/src/main/java/org/onehippo/repository/testutils/log4j/StringMatchFilter.java
@@ -0,0 +1,80 @@
+/*
+ *  Copyright 2013 Hippo.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.onehippo.repository.testutils.log4j;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.log4j.spi.Filter;
+import org.apache.log4j.spi.LoggingEvent;
+
+/**
+ * This is a very simple filter based on string matching.
+ *
+ * <p>The filter reads lines from a called <code>log4j-filters.txt</code>
+ * that is expected to be on the classpath. Each line represents a string to
+ * to match the log message to filter against. If the log message contains one
+ * of the configured strings it is filtered out.</p>
+ */
+public class StringMatchFilter extends Filter {
+
+    private final List<String> stringsToMatch = new ArrayList<String>();
+
+    public StringMatchFilter() {
+        final InputStream resourceAsStream = getClass().getClassLoader().getResourceAsStream("log4j-filters.txt");
+        if (resourceAsStream != null) {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(resourceAsStream));
+            try {
+                String line;
+                while ((line = reader.readLine()) != null && !(line = line.trim()).isEmpty()) {
+                    stringsToMatch.add(line);
+                }
+            } catch (IOException e) {
+                System.err.println("Error while initializing log4j StringMatchFilter: " + e.toString());
+            }
+        } else {
+            System.err.println("StringMatchFilter: Could not find log4j-filters.txt");
+        }
+    }
+
+    @Override
+    public int decide(final LoggingEvent event) {
+        String msg = event.getRenderedMessage();
+
+        if(msg == null || stringsToMatch.isEmpty()) {
+            return Filter.NEUTRAL;
+        }
+
+        if (matches(msg)) {
+            return Filter.DENY;
+        }
+        return Filter.NEUTRAL;
+    }
+
+    private boolean matches(final String msg) {
+        for (String s : stringsToMatch) {
+            if (msg.contains(s)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
