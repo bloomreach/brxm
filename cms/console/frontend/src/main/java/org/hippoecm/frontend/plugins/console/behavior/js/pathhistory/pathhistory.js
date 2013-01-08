971566cc0d3deeca56639604460ed2416f150029
Mathijs den Burger <m.denburger@onehippo.com>
2013-01-08 11:46:11 +0000

    CMS7-6797: fixed all errors reported by jslint in the 'console/frontend' module
    The build of the console/frontend module now fails when new errors are introduced.

--- cms/console/frontend/src/main/java/org/hippoecm/frontend/plugins/console/behavior/js/pathhistory/pathhistory.js
+++ cms/console/frontend/src/main/java/org/hippoecm/frontend/plugins/console/behavior/js/pathhistory/pathhistory.js
@@ -1,5 +1,3 @@
-"use strict";
-
 /**
  * @description
  * <p>
@@ -9,59 +7,64 @@
  * @requires hippoajax, hashmap, hippodom
  * @module pathhistory
  */
+(function() {
+    "use strict";
 
+    YAHOO.namespace('hippo');
 
-YAHOO.namespace('hippo');
+    if (!YAHOO.hippo.PathHistory) {
 
-if (!YAHOO.hippo.PathHistory) {
+        var Lang = YAHOO.lang;
 
-    var Dom = YAHOO.util.Dom, Lang = YAHOO.lang, HippoAjax = YAHOO.hippo.HippoAjax;
+        YAHOO.hippo.PathHistoryImpl = function() {
+            this.initialized = false;
+        };
 
-    YAHOO.hippo.PathHistoryImpl = function() {
-        this.initialized = false;
-    };
+        YAHOO.hippo.PathHistoryImpl.prototype = {
 
-    YAHOO.hippo.PathHistoryImpl.prototype = {
+            init : function(url, callback) {
+                if (this.initialized) {
+                    return;
+                }
 
-        init : function(url, callback) {
-            if (this.initialized) {
-                return;
+                var handleUrlChange = function() {
+                    this.onUrlChange();
+                }.bind(this);
+                window.addEventListener('popstate', handleUrlChange);
+
+                this.url = url;
+                this.callback = callback;
+
+                this.initialized = true;
+            },
+
+            setPath : function(path) {
+                var url = '?path=' + path;
+                history.pushState(null, null, url);
+            },
+
+            onUrlChange : function() {
+                var path, url;
+                path = this.getParameter('path');
+                if (Lang.isUndefined(path)) {
+                    path = "/";
+                }
+                url = this.url + "&path=" + path;
+                this.callback(url);
+            },
+
+            getParameter : function(name) {
+                name=(new RegExp('[?&]'+encodeURIComponent(name)+'=([^&]*)')).exec(location.search);
+                if (name) {
+                    return decodeURIComponent(name[1]);
+                }
             }
+        };
 
-            var handleUrlChange = function() {
-                this.onUrlChange();
-            }.bind(this);
-            window.addEventListener('popstate', handleUrlChange);
+        YAHOO.hippo.PathHistory = new YAHOO.hippo.PathHistoryImpl();
+        YAHOO.register("pathhistory", YAHOO.hippo.PathHistory, {
+            version : "2.7.0", build : "1799"
+        });
+    }
 
-            this.url = url;
-            this.callback = callback;
-
-            this.initialized = true;
-        },
-
-        setPath : function(path) {
-            var url = '?path=' + path;
-            history.pushState(null, null, url);
-        },
-
-        onUrlChange : function() {
-            var path = this.getParameter('path');
-            if (Lang.isUndefined(path)) {
-                path = "/";
-            }
-            var url = this.url + "&path=" + path;
-            this.callback(url);
-        },
-
-        getParameter : function(name){
-            if(name=(new RegExp('[?&]'+encodeURIComponent(name)+'=([^&]*)')).exec(location.search))
-                return decodeURIComponent(name[1]);
-        }
-    };
-
-    YAHOO.hippo.PathHistory = new YAHOO.hippo.PathHistoryImpl();
-    YAHOO.register("pathhistory", YAHOO.hippo.PathHistory, {
-        version : "2.7.0", build : "1799"
-    });
-}
-
+}());
