7dab82235de822afca2a45255435eea8d9dc8376
Mathijs den Burger <m.denburger@onehippo.com>
2013-01-09 16:14:45 +0000

    CMS7-6812: corrected license headers, added apache-rat plugin configuration

--- site-toolkit/content-beans/src/main/java/org/hippoecm/hst/util/SearchInputParsingUtils.java
+++ site-toolkit/content-beans/src/main/java/org/hippoecm/hst/util/SearchInputParsingUtils.java
@@ -1,7 +1,7 @@
 package org.hippoecm.hst.util;
 
 /**
- * Copyright 2011 Hippo
+ * Copyright 2011-2013 Hippo B.V. (http://www.onehippo.com)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
