e6c6425e0bd2d7c2aedf09f1fc6484429f11b51e
Frank van Lankvelt <f.vanlankvelt@onehippo.com>
2013-04-26 09:59:50 +0000

    CMS7-7088: make sure sections are started when the browse service needs to navigate to a document or folder.

--- cms/perspectives/src/main/java/org/hippoecm/frontend/plugins/cms/browse/Navigator.java
+++ cms/perspectives/src/main/java/org/hippoecm/frontend/plugins/cms/browse/Navigator.java
@@ -15,12 +15,17 @@
  */
 package org.hippoecm.frontend.plugins.cms.browse;
 
+import javax.jcr.Node;
+
 import org.apache.wicket.model.IModel;
 import org.hippoecm.frontend.PluginRequestTarget;
 import org.hippoecm.frontend.model.IChangeListener;
 import org.hippoecm.frontend.model.JcrNodeModel;
+import org.hippoecm.frontend.plugin.IClusterControl;
 import org.hippoecm.frontend.plugin.IPluginContext;
+import org.hippoecm.frontend.plugin.config.IClusterConfig;
 import org.hippoecm.frontend.plugin.config.IPluginConfig;
+import org.hippoecm.frontend.plugin.config.IPluginConfigService;
 import org.hippoecm.frontend.plugins.cms.browse.model.BrowserSections;
 import org.hippoecm.frontend.plugins.cms.browse.model.DocumentCollection;
 import org.hippoecm.frontend.plugins.cms.browse.service.BrowseService;
@@ -38,22 +43,38 @@ public class Navigator extends RenderPlugin {
 
     static final Logger log = LoggerFactory.getLogger(Navigator.class);
 
+    public static final String CLUSTER_NAME = "cluster.name";
+    public static final String CLUSTER_PARAMETERS = "cluster.config";
+
     private BrowseService browseService;
     private DocumentCollectionView docView;
     private BrowserSectionAccordion accordion;
 
+    private boolean clusterStarted;
+
     public Navigator(IPluginContext context, final IPluginConfig config) {
         super(context, config);
 
+        // pretend that cluster has already been started to prevent it's creation in the BrowseService constructor
+        clusterStarted = true;
         browseService = new BrowseService(context, config,
                 new JcrNodeModel(config.getString("model.default.path", "/"))) {
             private static final long serialVersionUID = 1L;
+
+            @Override
+            public void browse(final IModel<Node> model) {
+                if (!clusterStarted) {
+                    startCluster();
+                }
+                super.browse(model);
+            }
 
             @Override
             protected void onBrowse() {
                 focus(null);
             }
         };
+        clusterStarted = false;
 
         IModel<DocumentCollection> collectionModel = browseService.getCollectionModel();
         docView = new DocumentCollectionView("documents", context, config, collectionModel, this) {
@@ -97,9 +118,32 @@ public class Navigator extends RenderPlugin {
 
     @Override
     public void render(PluginRequestTarget target) {
+        if (!clusterStarted && isActive()) {
+            startCluster();
+        }
         super.render(target);
         docView.render(target);
         accordion.render(target);
     }
 
+    private void startCluster() {
+        clusterStarted = true;
+
+        IPluginConfig config = getPluginConfig();
+        String clusterName = config.getString(CLUSTER_NAME);
+        if (clusterName != null) {
+            IPluginContext context = getPluginContext();
+            IPluginConfigService pluginConfigService = context.getService(IPluginConfigService.class.getName(),
+                    IPluginConfigService.class);
+
+            IClusterConfig cluster = pluginConfigService.getCluster(clusterName);
+            if (cluster == null) {
+                log.warn("Unable to find cluster '" + clusterName + "'. Does it exist in repository?");
+            } else {
+                IPluginConfig parameters = config.getPluginConfig(CLUSTER_PARAMETERS);
+                IClusterControl control = context.newCluster(cluster, parameters);
+                control.start();
+            }
+        }
+    }
 }
