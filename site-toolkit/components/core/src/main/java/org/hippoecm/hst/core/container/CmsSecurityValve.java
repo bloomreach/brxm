d5b16a1855c6915e3a3bffa5a846444bd5d2a917
Mathijs den Burger <m.denburger@onehippo.com>
2013-07-24 07:58:09 +0000

    HSTTWO-2619: put the correct CMS user id on the HTTP session
    With overlayed JCR sessions, the user id becomes a concatenation of the user ids of the overlayed sessions.
    This combined 'user name' was put on the HTTP session. However, various code relies on the fact that this 
    cms user name session attribute corresponds to actual CMS user names. For example, the CmsComponentWindowResponseAppender 
    checks at line 94 whether the current user matches the user that locked a container in the template composer. 
    This check was no longer valid, causing a container to become locked for the current user when components 
    in it where reordered.

--- site-toolkit/components/core/src/main/java/org/hippoecm/hst/core/container/CmsSecurityValve.java
+++ site-toolkit/components/core/src/main/java/org/hippoecm/hst/core/container/CmsSecurityValve.java
@@ -21,6 +21,7 @@ import java.security.SignatureException;
 
 import javax.jcr.Credentials;
 import javax.jcr.Session;
+import javax.jcr.SimpleCredentials;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -157,6 +158,7 @@ public class CmsSecurityValve extends AbstractBaseOrderableValve {
                 CredentialCipher credentialCipher = CredentialCipher.getInstance();
                 try {
                     Credentials cred = credentialCipher.decryptFromString(key, credentialParam);
+                    httpSession.setAttribute(CMS_USER_ID_ATTR, ((SimpleCredentials) cred).getUserID());
                     httpSession.setAttribute(ContainerConstants.CMS_SSO_REPO_CREDS_ATTR_NAME, cred);
                     httpSession.setAttribute(ContainerConstants.CMS_SSO_AUTHENTICATED, true);
                 } catch (SignatureException se) {
@@ -186,7 +188,6 @@ public class CmsSecurityValve extends AbstractBaseOrderableValve {
                 }
 
                 if (jcrSession != null) {
-                    httpSession.setAttribute(CMS_USER_ID_ATTR, jcrSession.getUserID());
                     // only set the cms based lazySession on the request context when the context is the cms context
                     ((HstMutableRequestContext) requestContext).setSession(jcrSession);
                 }
@@ -242,7 +243,8 @@ public class CmsSecurityValve extends AbstractBaseOrderableValve {
     private Session createCmsRestSession(final HttpSession httpSession) throws ContainerException {
         long start = System.currentTimeMillis();
         try {
-            Session session = sessionSecurityDelegation.getDelegatedSession((Credentials) httpSession.getAttribute(ContainerConstants.CMS_SSO_REPO_CREDS_ATTR_NAME));
+            final Credentials credentials = (Credentials) httpSession.getAttribute(ContainerConstants.CMS_SSO_REPO_CREDS_ATTR_NAME);
+            Session session = sessionSecurityDelegation.getDelegatedSession(credentials);
             log.debug("Acquiring cms rest session took '{}' ms.", (System.currentTimeMillis() - start));
             return session;
         } catch (Exception e) {
