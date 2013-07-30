16c3e52ef292a017eda1f020b7fbea6826546515
Unico Hommes <u.hommes@onehippo.com>
2013-07-30 11:08:50 +0000

    CMS7-7267 utility base class for creating jcr system view xml filters

---
+++ utilities/src/main/java/org/onehippo/cms7/utilities/xml/SystemViewFilter.java
@@ -0,0 +1,153 @@
+/*
+ *  Copyright 2013 Hippo B.V. (http://www.onehippo.com)
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.onehippo.cms7.utilities.xml;
+
+import java.util.Stack;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.ContentHandler;
+import org.xml.sax.SAXException;
+
+/**
+ * Base class for SAX content handlers that implement a JCR system view xml filter.
+ */
+public class SystemViewFilter extends ProxyContentHandler {
+
+    protected static final String NODE = "node";
+    protected static final String PROPERTY = "property";
+    protected static final String NAME = "name";
+    protected static final String SV_URI = "http://www.jcp.org/jcr/sv/1.0";
+
+    protected final Path path;
+
+    private boolean skip = false;
+    private String context = null;
+
+    public SystemViewFilter(final ContentHandler handler, final String rootPath) {
+        super(handler);
+        this.path = new Path(rootPath);
+    }
+
+    @Override
+    public void startElement(String uri, String localName, String qName, Attributes atts) throws SAXException {
+        if ((localName.equals(NODE) || localName.equals(PROPERTY)) && uri.equals(SV_URI)) {
+            String name = atts.getValue(SV_URI, NAME);
+            path.push(name);
+            if (!skip) {
+                final String absPath = path.toString();
+                skip = (localName.equals(NODE) && shouldFilterNode(absPath, name))
+                        || (localName.equals(PROPERTY) && shouldFilterProperty(absPath, name));
+                if (skip) {
+                    context = absPath;
+                }
+            }
+        }
+        if (skip) {
+            return;
+        }
+        handler.startElement(uri, localName, qName, atts);
+    }
+
+    /**
+     * Override this method to signal which properties need to be filtered from the system view xml.
+     *
+     * @param path  the path of the property
+     * @param name  the name of the property
+     * @return  whether to exclude the property from the system view xml
+     */
+    protected boolean shouldFilterProperty(final String path, final String name) {
+        return false;
+    }
+
+    /**
+     * Override this method to signal which nodes need to be filtered from the system view xml.
+     *
+     * @param path  the path of the node
+     * @param name  the name of the node
+     * @return  whether to exclude the node from system view xml
+     */
+    protected boolean shouldFilterNode(final String path, final String name) {
+        return false;
+    }
+
+    @Override
+    public void endElement(String uri, String localName, String qName) throws SAXException {
+        if ((localName.equals(NODE) || localName.equals(PROPERTY)) && uri.equals(SV_URI)) {
+            if (skip && context.equals(path.toString())) {
+                context = null;
+                skip = false;
+                path.pop();
+                return;
+            }
+            path.pop();
+        }
+        if (skip) {
+            return;
+        }
+        handler.endElement(uri, localName, qName);
+    }
+
+    @Override
+    public void characters(char[] ch, int start, int length) throws SAXException {
+        if (skip) {
+            return;
+        }
+        handler.characters(ch, start, length);
+    }
+
+    @Override
+    public void ignorableWhitespace(char[] ch, int start, int length) throws SAXException {
+        if (skip) {
+            return;
+        }
+        handler.ignorableWhitespace(ch, start, length);
+    }
+
+
+    private static final class Path {
+        private final Stack<String> stack = new Stack<String>();
+        private final String prefixPath;
+
+        private String stringValue;
+
+        private Path(String rootPath) {
+            this.prefixPath = rootPath.equals("/") ? "" : rootPath;
+        }
+
+        void push(String element) {
+            stack.push(element);
+            stringValue = null;
+        }
+
+        void pop() {
+            stack.pop();
+            stringValue = null;
+        }
+
+        @Override
+        public String toString() {
+            if (stringValue == null) {
+                StringBuilder sb = new StringBuilder();
+                for (String element : stack) {
+                    sb.append("/").append(element);
+                }
+                stringValue = prefixPath + sb.toString();
+            }
+            return stringValue;
+        }
+    }
+
+}
