a9e6159c466db2dc7dae942db6c1fb45da6c9f8a
Mathijs den Burger <m.denburger@onehippo.com>
2013-08-20 10:58:33 +0000

    CMS7-7307: added unit test for CKEditorPanelPickerBehavior

---
+++ cms/richtext/frontend/src/test/java/org/hippoecm/frontend/plugins/ckeditor/CKEditorPanelPickerBehaviorTest.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright 2013 Hippo B.V. (http://www.onehippo.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.hippoecm.frontend.plugins.ckeditor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.wicket.behavior.AbstractAjaxBehavior;
+import org.easymock.classextension.EasyMock;
+import org.hippoecm.frontend.plugins.ckeditor.dialog.images.ImagePickerBehavior;
+import org.hippoecm.frontend.plugins.ckeditor.dialog.links.DocumentPickerBehavior;
+import org.hippoecm.frontend.plugins.ckeditor.hippopicker.HippoPicker;
+import org.json.JSONObject;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.easymock.EasyMock.expect;
+import static org.easymock.classextension.EasyMock.replay;
+import static org.easymock.classextension.EasyMock.verify;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Tests {@link CKEditorPanelPickerBehavior}.
+ */
+public class CKEditorPanelPickerBehaviorTest {
+
+    private DocumentPickerBehavior documentPickerBehavior;
+    private ImagePickerBehavior imagePickerBehavior;
+    private CKEditorPanelPickerBehavior behavior;
+
+    @Before
+    public void setUp() {
+        documentPickerBehavior = EasyMock.createMock(DocumentPickerBehavior.class);
+        imagePickerBehavior = EasyMock.createMock(ImagePickerBehavior.class);
+        behavior = new CKEditorPanelPickerBehavior(documentPickerBehavior, imagePickerBehavior);
+    }
+
+    @Test
+    public void callbackUrlsAreAddedToCKEditorConfiguration() throws Exception {
+        final String documentPickerCallbackUrl = "./documentpicker/callback";
+        final String imagePickerCallbackUrl = "./imagepicker/callback";
+
+        expect(documentPickerBehavior.getCallbackUrl()).andReturn(documentPickerCallbackUrl);
+        expect(imagePickerBehavior.getCallbackUrl()).andReturn(imagePickerCallbackUrl);
+
+        replay(documentPickerBehavior, imagePickerBehavior);
+
+        final JSONObject editorConfig = new JSONObject();
+        behavior.addCKEditorConfiguration(editorConfig);
+
+        verify(documentPickerBehavior, imagePickerBehavior);
+
+        assertTrue("CKEditor config has configuration for the hippopicker plugin", editorConfig.has(HippoPicker.CONFIG_KEY));
+        JSONObject hippoPickerConfig = editorConfig.getJSONObject(HippoPicker.CONFIG_KEY);
+
+        assertTrue("hippopicker configuration has configuration for the internal link picker", hippoPickerConfig.has(HippoPicker.INTERNAL_LINK_PICKER_CONFIG_KEY));
+        JSONObject internalLinkPickerConfig = hippoPickerConfig.getJSONObject(HippoPicker.INTERNAL_LINK_PICKER_CONFIG_KEY);
+        assertEquals(documentPickerCallbackUrl, internalLinkPickerConfig.getString(HippoPicker.INTERNAL_LINK_PICKER_CONFIG_CALLBACK_URL));
+
+        assertTrue("hippopicker configuration has configuration for the image picker", hippoPickerConfig.has(HippoPicker.IMAGE_PICKER_CONFIG_KEY));
+        JSONObject imagePickerConfig = hippoPickerConfig.getJSONObject(HippoPicker.IMAGE_PICKER_CONFIG_KEY);
+        assertEquals(imagePickerCallbackUrl, imagePickerConfig.getString(HippoPicker.IMAGE_PICKER_CONFIG_CALLBACK_URL));
+    }
+
+    @Test
+    public void testGetAjaxBehaviors() throws Exception {
+        List ajaxBehaviors = new ArrayList();
+        for (AbstractAjaxBehavior ajaxBehavior : behavior.getAjaxBehaviors()) {
+            ajaxBehaviors.add(ajaxBehavior);
+        }
+        assertEquals(2, ajaxBehaviors.size());
+        assertTrue(ajaxBehaviors.contains(documentPickerBehavior));
+        assertTrue(ajaxBehaviors.contains(imagePickerBehavior));
+    }
+
+}
