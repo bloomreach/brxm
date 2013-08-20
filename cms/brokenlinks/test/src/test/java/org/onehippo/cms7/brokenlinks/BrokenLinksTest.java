cfe5eed41c89fc05fc67cf08740874d4e28e08b1
Woonsan Ko <w.ko@onehippo.com>
2013-08-20 20:23:37 +0000

    CMS7-7301: adding unit test for internal link checking feature

--- cms/brokenlinks/test/src/test/java/org/onehippo/cms7/brokenlinks/BrokenLinksTest.java
+++ cms/brokenlinks/test/src/test/java/org/onehippo/cms7/brokenlinks/BrokenLinksTest.java
@@ -20,8 +20,11 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import java.rmi.RemoteException;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 import java.util.Stack;
 
@@ -35,6 +38,8 @@ import javax.jcr.SimpleCredentials;
 import javax.jcr.query.Query;
 import javax.jcr.query.QueryResult;
 
+import org.apache.commons.lang.ArrayUtils;
+import org.hippoecm.repository.api.HippoNodeType;
 import org.hippoecm.repository.api.HippoWorkspace;
 import org.hippoecm.repository.api.MappingException;
 import org.hippoecm.repository.api.WorkflowException;
@@ -50,6 +55,12 @@ import org.slf4j.LoggerFactory;
 
 public class BrokenLinksTest extends RepositoryTestCase {
 
+    /**
+     * The html compound node name created by the {@link #createDocuments(Node, Stack, int, DocumentText, List)} calls.
+     */
+    private static final String HTML_COMPOUND_NODE_NAME = "text";
+
+    private static final String DEFAULT_INTERNAL_DOC_LINK_NAME = "internal_doc";
 
     private static final Logger log = LoggerFactory.getLogger(BrokenLinksTest.class);
     private static final String GOOD = TestHttpClient.OK_URL;
@@ -57,6 +68,7 @@ public class BrokenLinksTest extends RepositoryTestCase {
     private static final String TEXT1 = "<html><body>\n<H1>Wat is Lorem Ipsum</H1>\n<B><A HREF=\""+GOOD+"\">Lorem Ipsum</A></B> is slechts een proeftekst uit het drukkerij- en zetterijwezen. Lorem Ipsum is de standaard proeftekst in deze bedrijfstak sinds de 16e eeuw, toen een onbekende drukker een zethaak met letters nam en ze door elkaar husselde om een font-catalogus te maken. Het heeft niet alleen vijf eeuwen overleefd maar is ook, vrijwel onveranderd, overgenomen in elektronische letterzetting. Het is in de jaren '60 populair geworden met de introductie van Letraset vellen met Lorem Ipsum passages en meer recentelijk door desktop publishing software zoals Aldus PageMaker die versies van Lorem Ipsum bevatten.\n<H1>Waar komt het vandaan?</H1>\n<P>In tegenstelling tot wat algemeen aangenomen wordt is Lorem Ipsum niet zomaar willekeurige tekst. het heeft zijn wortels in een stuk klassieke <A HREF=\""+BAD+"\">latijnse</A> literatuur uit 45 v.Chr. en is dus meer dan 2000 jaar oud. Richard McClintock, een professor <A HREF=\""+BAD+"\">latijn</A> aan de Hampden-Sydney College in Virginia, heeft één van de meer obscure <A HREF=\""+BAD+"\">latijnse</A> woorden, consectetur, uit een Lorem Ipsum passage opgezocht, en heeft tijdens het zoeken naar het woord in de klassieke literatuur de onverdachte bron ontdekt. Lorem Ipsum komt uit de secties 1.10.32 en 1.10.33 van &quot;de Finibus Bonorum et Malorum&quot; (De uitersten van goed en kwaad) door Cicero, geschreven in 45 v.Chr. Dit boek is een verhandeling over de theorie der ethiek, erg populair tijdens de renaissance. De eerste regel van Lorem Ipsum, &quot;Lorem ipsum dolor sit amet..&quot;, komt uit een zin in sectie 1.10.32.</P>\n<P>Het standaard stuk van Lorum Ipsum wat sinds de 16e eeuw wordt gebruikt is hieronder, voor wie er interesse in heeft, weergegeven. Secties 1.10.32 en 1.10.33 van &quot;de Finibus Bonorum et Malorum&quot; door Cicero zijn ook weergegeven in hun exacte originele vorm, vergezeld van engelse versies van de 1914 vertaling door H. Rackham.</P><H1>Waarom gebruiken we het?</H1>\n<P>Het is al geruime tijd een bekend gegeven dat een lezer, tijdens het bekijken van de layout van een pagina, afgeleid wordt door de tekstuele inhoud. Het belangrijke punt van het gebruik van Lorem Ipsum is dat het uit een min of meer normale verdeling van letters bestaat, in tegenstelling tot &quot;Hier uw tekst, hier uw tekst&quot; wat het tot min of meer leesbaar nederlands maakt. Veel desktop publishing pakketten en web pagina editors gebruiken tegenwoordig Lorem Ipsum als hun standaard model tekst, en een zoekopdracht naar &quot;lorem ipsum&quot; ontsluit veel websites die nog in aanbouw zijn. Verscheidene versies hebben zich ontwikkeld in de loop van de jaren, soms per ongeluk soms expres (ingevoegde humor en dergelijke).</P>\n<H1>Waar kan ik het vinden?</H1>\n<P>Er zijn vele variaties van passages van Lorem Ipsum beschikbaar maar het merendeel heeft te lijden gehad van wijzigingen in een of andere vorm, door ingevoegde humor of willekeurig gekozen woorden die nog niet half geloofwaardig ogen. Als u een passage uit Lorum Ipsum gaat gebruiken dient u zich ervan te verzekeren dat er niets beschamends midden in de tekst verborgen zit. Alle Lorum Ipsum generators op Internet hebben de eigenschap voorgedefinieerde stukken te herhalen waar nodig zodat dit de eerste echte generator is op internet. Het gebruikt een woordenlijst van 200 <A HREF=\""+GOOD+"\">latijnse</A> woorden gecombineerd met een handvol zinsstructuur modellen om een Lorum Ipsum te genereren die redelijk overkomt. De gegenereerde Lorum Ipsum is daardoor altijd vrij van herhaling, ingevoegde humor of ongebruikelijke woorden etc.</P>\n</body></html>\n";
     private static final String TEXT2 = "<html><body><A HREF=\""+BAD+"one\">One</A><A HREF=\""+BAD+"two\">Two</A><A HREF=\""+BAD+"three\">Three</A><A HREF=\""+BAD+"four\">Four</A><A HREF=\""+BAD+"five\">Five</A><A HREF=\""+BAD+"six\">Six</A><A HREF=\""+BAD+"seven\">Seven</A><A HREF=\""+BAD+"eight\">Eight</A><A HREF=\""+BAD+"nine\">Nine</A><A HREF=\""+BAD+"ten\">Ten</A><A HREF=\""+BAD+"eleven\">Eleven</A><A HREF=\""+BAD+"twelve\">Twelve</A><A HREF=\""+BAD+"thirteen\">Thirteen</A><A HREF=\""+BAD+"fourteen\">Fourteen</A><A HREF=\""+BAD+"fifteen\">Fifteen</A><A HREF=\""+BAD+"sixteen\">Sixteen</A><A HREF=\""+BAD+"seventeen\">Seventeen</A></body></html>";
     private static final String TEXT3 = "<html><body>\n<H1>Wat is Lorem Ipsum</H1>\n<B><A HREF=\""+GOOD+"\">Lorem Ipsum</A></B>\n</body></html>\n";
+    private static final String TEXT4 = "<html><body>\n<H1>Wat is Lorem Ipsum</H1>\n<A HREF=\"" + DEFAULT_INTERNAL_DOC_LINK_NAME + "\">Internal Document</A>\n</body></html>\n";
     private Stack<Integer> levels;
 
     private RepositoryJobExecutionContext jobContext;
@@ -315,8 +327,8 @@ public class BrokenLinksTest extends RepositoryTestCase {
             ++countDocuments;
             assertTrue(child.hasNode(child.getName()));
             child = child.getNode(child.getName());
-            assertTrue(child.hasNode("text"));
-            child = child.getNode("text");
+            assertTrue(child.hasNode(HTML_COMPOUND_NODE_NAME));
+            child = child.getNode(HTML_COMPOUND_NODE_NAME);
             assertTrue(child.hasProperty("hippostd:content"));
             child.setProperty("hippostd:content", TEXT3);
         }
@@ -334,11 +346,125 @@ public class BrokenLinksTest extends RepositoryTestCase {
         assertEquals(0, countDocuments);
     }
 
+    @Test
+    public void testMissingInternalLinks() throws Exception {
+        DocumentText documents = new DocumentText() {
+            public String getTextForDocument(int index) {
+                return TEXT4;
+            }
+        };
+        for (int count : new int[]{10, 10}) {
+            levels.push(count);
+        }
+        createDocuments(session.getRootNode().getNode("test"), levels, 0, documents);
+
+        new BrokenLinksCheckingJob().execute(jobContext);
+
+        session.refresh(false);
+        QueryResult result = session.getWorkspace().getQueryManager().createQuery("SELECT * FROM [brokenlinks:brokenlinks]", Query.JCR_SQL2).execute();
+        int countDocuments = 0;
+        for (NodeIterator iter = result.getNodes(); iter.hasNext();) {
+            Node child = iter.nextNode();
+            ++countDocuments;
+        }
+        assertEquals(100, countDocuments);
+    }
+
+    @Test
+    public void testBadInternalLinks() throws Exception {
+        DocumentText documents = new DocumentText() {
+            public String getTextForDocument(int index) {
+                return TEXT4;
+            }
+        };
+        for (int count : new int[]{10, 10}) {
+            levels.push(count);
+        }
+
+        List<String> createdDocIds = new ArrayList<String>(100);
+        createDocuments(session.getRootNode().getNode("test"), levels, 0, documents, createdDocIds);
+        int documentCount = createdDocIds.size();
+
+        // build valid linked nodes under the html compound node of each created document.
+        for (int i = 0; i < documentCount; i++) {
+            String docId = createdDocIds.get(i);
+            Node docNode = session.getNodeByIdentifier(docId);
+            Node htmlNode = docNode.getNode(HTML_COMPOUND_NODE_NAME);
+            Node internalDocLinkNode = htmlNode.addNode(DEFAULT_INTERNAL_DOC_LINK_NAME, HippoNodeType.NT_FACETSELECT);
+            // set a bad UUID here to see if it check the invalidity!!!
+            internalDocLinkNode.setProperty(HippoNodeType.HIPPO_DOCBASE, "00000000-0000-0000-0000-000000000000");
+            internalDocLinkNode.setProperty(HippoNodeType.HIPPO_FACETS, ArrayUtils.EMPTY_STRING_ARRAY);
+            internalDocLinkNode.setProperty(HippoNodeType.HIPPO_MODES, ArrayUtils.EMPTY_STRING_ARRAY);
+            internalDocLinkNode.setProperty(HippoNodeType.HIPPO_VALUES, ArrayUtils.EMPTY_STRING_ARRAY);
+            session.save();
+        }
+
+        session.refresh(false);
+
+        new BrokenLinksCheckingJob().execute(jobContext);
+
+        session.refresh(false);
+        QueryResult result = session.getWorkspace().getQueryManager().createQuery("SELECT * FROM [brokenlinks:brokenlinks]", Query.JCR_SQL2).execute();
+        int countDocuments = 0;
+        for (NodeIterator iter = result.getNodes(); iter.hasNext();) {
+            Node child = iter.nextNode();
+            ++countDocuments;
+        }
+        assertEquals(100, countDocuments);
+    }
+
+    @Test
+    public void testGoodInternalLinks() throws Exception {
+        DocumentText documents = new DocumentText() {
+            public String getTextForDocument(int index) {
+                return TEXT4;
+            }
+        };
+        for (int count : new int[]{10, 10}) {
+            levels.push(count);
+        }
+
+        List<String> createdDocIds = new ArrayList<String>(100);
+        createDocuments(session.getRootNode().getNode("test"), levels, 0, documents, createdDocIds);
+        int documentCount = createdDocIds.size();
+
+        // build valid linked nodes under the html compound node of each created document.
+        for (int i = 0; i < documentCount; i++) {
+            String docId = createdDocIds.get(i);
+            String linkedDocId = createdDocIds.get(documentCount - i - 1);
+            Node docNode = session.getNodeByIdentifier(docId);
+            Node htmlNode = docNode.getNode(HTML_COMPOUND_NODE_NAME);
+            Node internalDocLinkNode = htmlNode.addNode(DEFAULT_INTERNAL_DOC_LINK_NAME, HippoNodeType.NT_FACETSELECT);
+            internalDocLinkNode.setProperty(HippoNodeType.HIPPO_DOCBASE, linkedDocId);
+            internalDocLinkNode.setProperty(HippoNodeType.HIPPO_FACETS, ArrayUtils.EMPTY_STRING_ARRAY);
+            internalDocLinkNode.setProperty(HippoNodeType.HIPPO_MODES, ArrayUtils.EMPTY_STRING_ARRAY);
+            internalDocLinkNode.setProperty(HippoNodeType.HIPPO_VALUES, ArrayUtils.EMPTY_STRING_ARRAY);
+            session.save();
+        }
+
+        session.refresh(false);
+
+        new BrokenLinksCheckingJob().execute(jobContext);
+
+        session.refresh(false);
+        QueryResult result = session.getWorkspace().getQueryManager().createQuery("SELECT * FROM [brokenlinks:brokenlinks]", Query.JCR_SQL2).execute();
+        int countDocuments = 0;
+        for (NodeIterator iter = result.getNodes(); iter.hasNext();) {
+            Node child = iter.nextNode();
+            ++countDocuments;
+        }
+        assertEquals(0, countDocuments);
+    }
+
     interface DocumentText {
         public String getTextForDocument(int index);
     }
 
     private int createDocuments(Node folder, Stack<Integer> levels, int numof, DocumentText documentText) throws MappingException, WorkflowException, RepositoryException, RemoteException {
+        return createDocuments(folder, levels, numof, documentText, new LinkedList<String>());
+    }
+
+    private int createDocuments(Node folder, Stack<Integer> levels, int numof, DocumentText documentText, List<String> docIds) throws MappingException, WorkflowException, RepositoryException, RemoteException {
         Stack<Integer> subLevels = new Stack<Integer>();
         subLevels.addAll(levels);
         int count = subLevels.pop();
@@ -347,14 +473,15 @@ public class BrokenLinksTest extends RepositoryTestCase {
                 String subName = "folder" + i;
                 final Node childFolder = folder.addNode(subName, "hippostd:folder");
                 childFolder.addMixin("hippo:harddocument");
-                numof = createDocuments(childFolder, subLevels, numof, documentText);
+                numof = createDocuments(childFolder, subLevels, numof, documentText, docIds);
             } else {
                 String subName = "document" + i;
                 final Node handleNode = folder.addNode(subName, "hippo:handle");
                 handleNode.addMixin("hippo:hardhandle");
                 final Node documentNode = handleNode.addNode(subName, "brokenlinks:test");
                 documentNode.addMixin("hippo:harddocument");
-                Node htmlNode = documentNode.addNode("text", "hippostd:html");
+                docIds.add(documentNode.getIdentifier());
+                Node htmlNode = documentNode.addNode(HTML_COMPOUND_NODE_NAME, "hippostd:html");
                 htmlNode.setProperty("hippostd:content", documentText.getTextForDocument(i));
                 ++numof;
             }
