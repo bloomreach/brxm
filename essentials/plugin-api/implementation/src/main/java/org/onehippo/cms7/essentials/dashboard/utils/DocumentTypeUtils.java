9ed6b0d98ba9222901c2c585c3bd5ad21f7415db
Marijan Milicevic <m.milicevic@onehippo.com>
2013-09-16 10:04:49 +0000

    - add skeleton

--- essentials/plugin-api/implementation/src/main/java/org/onehippo/cms7/essentials/dashboard/utils/DocumentTypeUtils.java
+++ essentials/plugin-api/implementation/src/main/java/org/onehippo/cms7/essentials/dashboard/utils/DocumentTypeUtils.java
@@ -1,5 +1,7 @@
 package org.onehippo.cms7.essentials.dashboard.utils;
 
+import javax.jcr.Node;
+
 import org.onehippo.cms7.essentials.dashboard.ctx.PluginContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,17 +13,18 @@ public final class DocumentTypeUtils {
 
     private static Logger log = LoggerFactory.getLogger(DocumentTypeUtils.class);
 
-
     /**
-     * Register document type for given base type and namespace
-     * @param documentName  name of the document e.g. {@code newsdocument}
-     * @param baseType base type of document we are registering, e.g. {@code basedocument}
-     * @param namespace namespace of document we are registering e.g. {@code myproject}
-     * @param context plugin context
+     * Register document type for given base type and namespace. If document already exists, we'll just return existing node
+     *
+     * @param documentName name of the document e.g. {@code newsdocument}
+     * @param baseType     base type of document we are registering, e.g. {@code basedocument}
+     * @param namespace    namespace of document we are registering e.g. {@code myproject}
+     * @param context      plugin context
+     * @return node namespace node or null if failed.
      */
-    public static void registerDocumentType(final String documentName, final String baseType, final String namespace, final PluginContext context){
+    public static Node registerDocumentType(final String documentName, final String baseType, final String namespace, final PluginContext context) {
 
-
+         return null;
     }
 
 }
