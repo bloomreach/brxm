f405afc74b4604c8f690c7bb1cdcd5349e136368
Marijan Milicevic <m.milicevic@onehippo.com>
2013-09-16 11:53:54 +0000

    - upgrade poms - add panel skeleton

---
+++ essentials/dashboard-plugins/components-plugin/components-gui/src/main/java/org/onehippo/cms7/essentials/components/gui/panel/DocumentRegisterPanel.java
@@ -0,0 +1,18 @@
+package org.onehippo.cms7.essentials.components.gui.panel;
+
+import org.apache.wicket.markup.html.panel.Panel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @version "$Id$"
+ */
+public class DocumentRegisterPanel extends Panel {
+
+    private static final long serialVersionUID = 1L;
+    private static Logger log = LoggerFactory.getLogger(DocumentRegisterPanel.class);
+
+    public DocumentRegisterPanel(final String id) {
+        super(id);
+    }
+}
