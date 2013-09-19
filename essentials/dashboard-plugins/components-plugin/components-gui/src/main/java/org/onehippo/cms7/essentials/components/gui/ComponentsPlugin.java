99846795f74b0f76db9e2c2b59a07d2a959ce4eb
Kenan Salic <k.salic@onehippo.com>
2013-09-19 13:07:46 +0000

    moved wizard panel to api implementation

--- essentials/dashboard-plugins/components-plugin/components-gui/src/main/java/org/onehippo/cms7/essentials/components/gui/ComponentsPlugin.java
+++ essentials/dashboard-plugins/components-plugin/components-gui/src/main/java/org/onehippo/cms7/essentials/components/gui/ComponentsPlugin.java
@@ -10,23 +10,25 @@ import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
+import javax.jcr.Node;
+import javax.jcr.NodeIterator;
 import javax.jcr.RepositoryException;
 import javax.jcr.Session;
 
+import com.google.common.base.Strings;
+import com.google.common.collect.ImmutableBiMap;
+
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang.StringUtils;
+import org.apache.wicket.ajax.AjaxEventBehavior;
 import org.apache.wicket.ajax.AjaxRequestTarget;
-import org.apache.wicket.ajax.markup.html.form.AjaxButton;
-import org.apache.wicket.markup.html.basic.Label;
-import org.apache.wicket.markup.html.form.Check;
-import org.apache.wicket.markup.html.form.CheckGroup;
-import org.apache.wicket.markup.html.form.CheckGroupSelector;
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.form.ListChoice;
-import org.apache.wicket.markup.html.list.ListItem;
-import org.apache.wicket.markup.html.list.ListView;
-import org.apache.wicket.model.IModel;
+import org.apache.wicket.markup.html.panel.FeedbackPanel;
 import org.apache.wicket.model.PropertyModel;
 import org.onehippo.cms7.essentials.dashboard.Asset;
 import org.onehippo.cms7.essentials.dashboard.InstallablePlugin;
@@ -34,15 +36,13 @@ import org.onehippo.cms7.essentials.dashboard.Plugin;
 import org.onehippo.cms7.essentials.dashboard.ctx.PluginContext;
 import org.onehippo.cms7.essentials.dashboard.model.CatalogObject;
 import org.onehippo.cms7.essentials.dashboard.model.hst.HstTemplate;
+import org.onehippo.cms7.essentials.dashboard.model.hst.TemplateExistsException;
+import org.onehippo.cms7.essentials.dashboard.ui.EssentialsFeedbackPanel;
 import org.onehippo.cms7.essentials.dashboard.utils.ComponentsUtils;
 import org.onehippo.cms7.essentials.dashboard.utils.HstUtils;
 import org.onehippo.cms7.essentials.dashboard.utils.ProjectUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import com.google.common.base.Strings;
-import com.google.common.collect.ImmutableBiMap;
-import com.google.common.collect.ImmutableSet;
 
 /**
  * @version "$Id: ComponentsPlugin.java 164085 2013-05-13 10:05:42Z mmilicevic $"
@@ -50,7 +50,7 @@ import com.google.common.collect.ImmutableSet;
 public class ComponentsPlugin extends InstallablePlugin<ComponentsInstaller> {
 
     public static final ImmutableBiMap<String, CatalogObject> COMPONENTS_MAPPING = new ImmutableBiMap.Builder<String, CatalogObject>()
-            .put("Document component",
+            .put("Document Component",
                     new CatalogObject("essentials-component-document", "Essentials Document Component")
                             .setComponentClassName("org.onehippo.cms7.essentials.components.EssentialsDocumentComponent")
                             .setIconPath("images/essentials/essentials-component-document.png")
@@ -58,14 +58,14 @@ public class ComponentsPlugin extends InstallablePlugin<ComponentsInstaller> {
                             .setTemplate("hippo.essentials.components.document")
 
             )
-            .put("Events component",
+            .put("Events Component",
                     new CatalogObject("essentials-component-events", "Essentials Events Component")
                             .setComponentClassName("org.onehippo.cms7.essentials.components.EssentialsEventsComponent")
                             .setIconPath("images/essentials/essentials-component-events.png")
                             .setType("HST.Item")
                             .setTemplate("hippo.essentials.components.events")
 
-            ).put("List component",
+            ).put("List Component",
                     new CatalogObject("essentials-component-list", "Essentials List Component")
                             .setComponentClassName("org.onehippo.cms7.essentials.components.EssentialsListComponent")
                             .setIconPath("images/essentials/essentials-component-list.png")
@@ -73,126 +73,195 @@ public class ComponentsPlugin extends InstallablePlugin<ComponentsInstaller> {
                             .setTemplate("hippo.essentials.components.list")
 
             )
-
-
-
-                    /*.put("Events component", "org.onehippo.cms7.essentials.components.EssentialsEventsComponent")
-                    .put("News component", "org.onehippo.cms7.essentials.components.EssentialsNewsComponent")
-                    .put("List component", "org.onehippo.cms7.essentials.components.EssentialsDocumentListComponent")*/
             .build();
+
+
     // TODO refactor / make dynamic
     public static final String JSP_FOLDER = File.separator + "essentials" + File.separator + "components" + File.separator;
     private static final long serialVersionUID = 1L;
+
     private static Logger log = LoggerFactory.getLogger(ComponentsPlugin.class);
-    final ListChoice<String> sitesChoice;
+
+    private final ListChoice<String> sitesChoice;
+    private final ListChoice<String> availableTypesListChoice;
+    private final ListChoice<String> addToTypesListChoice;
+
     private String selectedSite;
-    private List<String> selectedComponents = new ArrayList<>();
 
     public ComponentsPlugin(final String id, final Plugin descriptor, final PluginContext context) {
         super(id, descriptor, context);
+        final FeedbackPanel feedbackPanel = new EssentialsFeedbackPanel("feedback");
+        feedbackPanel.setOutputMarkupId(true);
+        add(feedbackPanel);
+
         final Form<?> form = new Form("form");
-
-
         //############################################
         // SITES SELECT
         //############################################
-
-
         final List<String> sites = ComponentsUtils.getAllAvailableSites(context);
 
         final PropertyModel<String> siteModel = new PropertyModel<>(this, "selectedSite");
         sitesChoice = new ListChoice<>("sites", siteModel, sites);
+        sitesChoice.setNullValid(false);
         sitesChoice.setOutputMarkupId(true);
 
-        //############################################
-        // CHECKBOXES
-        //############################################
-
-        final CheckGroup<String> group = new CheckGroup<>("group", new PropertyModel<List<String>>(this, "selectedComponents"));
-        group.add(new CheckGroupSelector("groupselector"));
-        final ImmutableSet<String> componentSet = COMPONENTS_MAPPING.keySet();
-        List<String> componentNames = new ArrayList<>(componentSet);
-        final ListView<String> components = new ListView<String>("components", componentNames) {
+        sitesChoice.add(new AjaxEventBehavior("onchange") {
             private static final long serialVersionUID = 1L;
 
             @Override
-            protected void populateItem(final ListItem<String> item) {
-                final IModel<String> model = item.getModel();
-                item.add(new Check<>("checkbox", model));
-                item.add(new Label("label", ' ' + model.getObject()));
-            }
-        };
-        final AjaxButton saveButton = new
+            protected void onEvent(final AjaxRequestTarget target) {
+                final String input = sitesChoice.getInput();
+                if (StringUtils.isNotEmpty(input) && StringUtils.isNumeric(input)) {
+                    final String site = sitesChoice.getChoices().get(Integer.parseInt(input));
+                    selectedSite = site;
+                    try {
+                        final List<String> addedComponents = Util.getAddedComponents(context, site);
 
-                AjaxButton("save") {
-                    private static final long serialVersionUID = 1L;
+                        addToTypesListChoice.setChoices(addedComponents);
+                        availableTypesListChoice.setChoices(Util.getAvailableComponents(COMPONENTS_MAPPING.keySet(), addedComponents));
 
-                    @Override
-                    protected void onSubmit(final AjaxRequestTarget target, final Form<?> form) {
-                        super.onSubmit(target, form);
-                        onFormSubmit(this, target);
+                        target.add(addToTypesListChoice, availableTypesListChoice);
+                    } catch (RepositoryException e) {
+                        log.error("repository exception while trying to select site", e);
                     }
-                };
-        components.setReuseItems(true);
-        group.add(sitesChoice);
-        group.add(components);
-        form.add(group);
-        form.add(saveButton);
+                } else {
+                    availableTypesListChoice.setChoices(Collections.<String>emptyList());
+                    addToTypesListChoice.setChoices(Collections.<String>emptyList());
+                    target.add(availableTypesListChoice, addToTypesListChoice);
+                }
+            }
+        });
+        form.add(sitesChoice);
+
+        //############################################
+        // Component add components
+        //############################################
+
+        final List<String> available = new ArrayList();
+        final List<String> toAdd = new ArrayList();
+
+        availableTypesListChoice = new ListChoice<>("available-types", available);
+        availableTypesListChoice.setOutputMarkupId(true);
+
+        addToTypesListChoice = new ListChoice<>("add-to-types", toAdd);
+        addToTypesListChoice.setOutputMarkupId(true);
+
+        availableTypesListChoice.add(new AjaxEventBehavior("onchange") {
+            private static final long serialVersionUID = 1L;
+
+            @Override
+            protected void onEvent(final AjaxRequestTarget target) {
+                if (selectedSite != null) {
+                    final List<String> availableChoices = (List<String>) availableTypesListChoice.getChoices();
+                    final String input = availableChoices.get(Integer.parseInt(availableTypesListChoice.getInput()));
+
+                    //todo some magic;
+                    final boolean b = addCatalogToSite(context, input, selectedSite);
+
+                    List<String> addedComponents = null;
+                    try {
+                        addedComponents = Util.getAddedComponents(context, selectedSite);
+                    } catch (RepositoryException e) {
+                        log.error("", e);
+                    }
+
+                    addToTypesListChoice.setChoices(addedComponents);
+                    availableTypesListChoice.setChoices(Util.getAvailableComponents(COMPONENTS_MAPPING.keySet(), addedComponents));
+
+                    target.add(availableTypesListChoice, addToTypesListChoice);
+                } else {
+                    error("Please add and/or select a site");
+                    target.add(feedbackPanel);
+                }
+            }
+        });
+        form.add(availableTypesListChoice);
+
+        addToTypesListChoice.add(new AjaxEventBehavior("onchange") {
+
+            private static final long serialVersionUID = 1L;
+
+            @Override
+            protected void onEvent(final AjaxRequestTarget target) {
+                if (selectedSite != null) {
+                    final String input = addToTypesListChoice.getChoices().get(Integer.parseInt(addToTypesListChoice.getInput()));
+
+                    //todo some magic;
+                    final boolean b = removeCatalogFromSite(context, input, selectedSite);
+
+                    List<String> addedComponents = null;
+                    try {
+                        addedComponents = Util.getAddedComponents(context, selectedSite);
+                    } catch (RepositoryException e) {
+                        log.error("", e);
+                    }
+
+                    addToTypesListChoice.setChoices(addedComponents);
+                    availableTypesListChoice.setChoices(Util.getAvailableComponents(COMPONENTS_MAPPING.keySet(), addedComponents));
+
+                    target.add(availableTypesListChoice, addToTypesListChoice);
+                } else {
+                    error("Please add and/or select a site");
+                    target.add(feedbackPanel);
+                }
+            }
+        });
+        form.add(addToTypesListChoice);
+
         add(form);
 
     }
 
-    private void onFormSubmit(final AjaxButton button, final AjaxRequestTarget target) {
+    private boolean addCatalogToSite(final PluginContext context, final String input, final String selectedSite) {
+        final CatalogObject catalogObject = COMPONENTS_MAPPING.get(input);
+        catalogObject.setSiteName(selectedSite);
+        ComponentsUtils.addToCatalog(catalogObject, context);
 
-        if (Strings.isNullOrEmpty(selectedSite)) {
-            // TODO add error
-            return;
-        }
-
-        final PluginContext context = getContext();
-        // TODO: check if component exists:
-        for (String selectedComponent : selectedComponents) {
-            final CatalogObject object = COMPONENTS_MAPPING.get(selectedComponent);
-            object.setSiteName(selectedSite);
-            ComponentsUtils.addToCatalog(object, getContext());
-            // copy icons:
-            final File folder = ProjectUtils.getSiteImagesFolder();
-            if (folder.exists()) {
-                final File essentials = FileUtils.getFile(folder, "essentials");
-                if (!essentials.exists()) {
-                    essentials.mkdir();
-                }
-                final Asset asset = context.getDescriptor().getAsset(selectedComponent);
-                if (asset != null && !Strings.isNullOrEmpty(asset.getUrl())) {
-                    final File file = new File(essentials.getAbsoluteFile() + File.separator + asset.getUrl());
-                    copyAsset(file, asset);
-                }
+        final File folder = ProjectUtils.getSiteImagesFolder();
+        if (folder.exists()) {
+            final File essentials = FileUtils.getFile(folder, "essentials");
+            if (!essentials.exists()) {
+                essentials.mkdir();
             }
-            // copy JSP:
-            final File siteJspFolder = ProjectUtils.getSiteJspFolder();
-            final String templateName = object.getName() + ".jsp";
-            final Asset asset = context.getDescriptor().getAsset(templateName);
+            final Asset asset = context.getDescriptor().getAsset(input);
             if (asset != null && !Strings.isNullOrEmpty(asset.getUrl())) {
-                final File file = new File(siteJspFolder.getAbsoluteFile() + JSP_FOLDER + asset.getUrl());
+                final File file = new File(essentials.getAbsoluteFile() + File.separator + asset.getUrl());
                 copyAsset(file, asset);
             }
-            final HstTemplate template = new HstTemplate(object.getTemplate());
-            template.setNamed(true);
-            try {
-                template.setRenderPath("jsp/essentials/components/" + templateName);
-                final Session session = context.getSession();
-                HstUtils.addTemplateNodeToConfiguration(session, selectedSite, template);
-                session.save();
-
-            } catch (RepositoryException e) {
-                log.error("Error writing template", e);
-            }
-
-
         }
-
-
+        // copy JSP:
+        final File siteJspFolder = ProjectUtils.getSiteJspFolder();
+        final String templateName = catalogObject.getName() + ".jsp";
+        final Asset asset = context.getDescriptor().getAsset(templateName);
+        if (asset != null && !Strings.isNullOrEmpty(asset.getUrl())) {
+            final File file = new File(siteJspFolder.getAbsoluteFile() + JSP_FOLDER + asset.getUrl());
+            copyAsset(file, asset);
+        }
+        final HstTemplate template = new HstTemplate(catalogObject.getTemplate());
+        template.setNamed(true);
+        try {
+            template.setRenderPath("jsp/essentials/components/" + templateName);
+            final Session session = context.getSession();
+            HstUtils.addTemplateNodeToConfiguration(session, selectedSite, template);
+            session.save();
+            return true;
+        } catch (RepositoryException e) {
+            log.error("Error writing template", e);
+        } catch (TemplateExistsException e) {
+            log.warn("template already exists.", e);
+            error("template already exists.");
+            return true;
+        }
+        return false;
     }
+
+    private boolean removeCatalogFromSite(final PluginContext context, final String input, final String selectedSite) {
+        final CatalogObject catalogObject = COMPONENTS_MAPPING.get(input);
+        catalogObject.setSiteName(selectedSite);
+        ComponentsUtils.removeFromCatalog(catalogObject, context);
+        return true;
+    }
+
 
     private void copyAsset(final File target, final Asset asset) {
         final InputStream resourceAsStream = getClass().getResourceAsStream(asset.getUrl());
@@ -215,4 +284,42 @@ public class ComponentsPlugin extends InstallablePlugin<ComponentsInstaller> {
         return new ComponentsInstaller();
     }
 
+
+    private final static class Util {
+
+        public static final String CATALOG_PATH = "hst:catalog";
+        public static final String HST_CONFIG_PATH = "hst:hst/hst:configurations";
+        public static final String HIPPOESSENTIALS_CATALOG = "hippoessentials-catalog";
+        public static final String HIPPOESSENTIALS_PREFIX = "Essentials ";
+
+        public static List<String> getAddedComponents(PluginContext context, String siteName) throws RepositoryException {
+            final List<String> addedComponents = new ArrayList<>();
+            final Session session = context.getSession();
+            final Node rootNode = session.getRootNode();
+            final Node node = rootNode.getNode(HST_CONFIG_PATH);
+            if (node.hasNode(siteName + "/" + CATALOG_PATH + "/" + HIPPOESSENTIALS_CATALOG)) {
+                //final Node site = node.getNode(siteName);
+                //todo get parent stuff..
+                final Node essentialsCatalog = node.getNode(siteName + "/" + CATALOG_PATH + "/" + HIPPOESSENTIALS_CATALOG);
+                final NodeIterator it = essentialsCatalog.getNodes();
+                while (it.hasNext()) {
+                    final Node essentialContainerItem = it.nextNode();
+                    final String label = essentialContainerItem.getProperty("hst:label").getString();
+                    final String key = label.replace(HIPPOESSENTIALS_PREFIX, "");
+                    if (COMPONENTS_MAPPING.containsKey(key)) {
+                        final CatalogObject catalogObject = COMPONENTS_MAPPING.get(key);
+                        addedComponents.add(key);
+                    }
+                }
+            }
+            return addedComponents;
+        }
+
+        public static List<String> getAvailableComponents(Collection<String> available, Collection<String> added) {
+            List<String> abv = new ArrayList<>(available);
+            abv.removeAll(added);
+            return abv;
+        }
+
+    }
 }
