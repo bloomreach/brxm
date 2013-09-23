38ad561b5bdfe1d41207d0fb446d2a9c585f6e66
Woonsan Ko <w.ko@onehippo.com>
2013-09-23 18:58:52 +0000

    CMS7-7374: flagging ReportBrokenLinksWorkflow not to leave audit logs

--- cms/brokenlinks/common/src/main/java/org/onehippo/cms7/brokenlinks/ReportBrokenLinksWorkflow.java
+++ cms/brokenlinks/common/src/main/java/org/onehippo/cms7/brokenlinks/ReportBrokenLinksWorkflow.java
@@ -1,12 +1,12 @@
 /*
  *  Copyright 2011-2013 Hippo B.V. (http://www.onehippo.com)
- * 
+ *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
  *  You may obtain a copy of the License at
- * 
+ *
  *       http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  *  Unless required by applicable law or agreed to in writing, software
  *  distributed under the License is distributed on an "AS IS" BASIS,
  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -22,6 +22,7 @@ import javax.jcr.RepositoryException;
 
 import org.hippoecm.repository.api.Workflow;
 import org.hippoecm.repository.api.WorkflowException;
+import org.hippoecm.repository.api.annotation.WorkflowAction;
 
 public interface ReportBrokenLinksWorkflow extends Workflow {
 
@@ -34,5 +35,7 @@ public interface ReportBrokenLinksWorkflow extends Workflow {
      * @throws RepositoryException
      * @throws RemoteException
      */
+    @WorkflowAction(loggable=false)
     void reportBrokenLinks(Collection<Link> brokenLinks) throws WorkflowException, RepositoryException, RemoteException;
+
 }
