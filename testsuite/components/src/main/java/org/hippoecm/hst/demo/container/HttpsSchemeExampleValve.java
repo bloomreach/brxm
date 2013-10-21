58b5e1ba07916e333c628e8c2484a6dae5f10f2c
Ard Schrijvers <a.schrijvers@onehippo.com>
2013-10-21 14:48:54 +0000

    HSTTWO-2697 added example for https redirecting valve.

---
+++ testsuite/components/src/main/java/org/hippoecm/hst/demo/container/HttpsSchemeExampleValve.java
@@ -0,0 +1,40 @@
+/*
+ *  Copyright 2013 Hippo B.V. (http://www.onehippo.com)
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.hippoecm.hst.demo.container;
+
+import javax.jcr.RepositoryException;
+
+import org.hippoecm.hst.content.beans.standard.HippoBean;
+import org.hippoecm.hst.core.container.AbstractHttpsSchemeValve;
+import org.hippoecm.hst.core.container.ValveContext;
+import org.hippoecm.hst.core.jcr.RuntimeRepositoryException;
+
+public class HttpsSchemeExampleValve extends AbstractHttpsSchemeValve {
+
+    @Override
+    public boolean shouldBeSecure(final ValveContext context) {
+        final HippoBean contentBean = context.getRequestContext().getContentBean();
+        if (contentBean == null ) {
+            return false;
+        }
+        try {
+            return contentBean.getNode().isNodeType("demosite:secure");
+        } catch (RepositoryException e) {
+            throw new RuntimeRepositoryException(e);
+        }
+    }
+    
+}
