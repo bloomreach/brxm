bef2734cac7b1953e07d011b20899cca2824633b
Marijan Milicevic <m.milicevic@onehippo.com>
2013-10-22 13:56:04 +0000

    - introduce new page

--- essentials/plugin-dashboard/src/main/java/org/onehippo/cms7/essentials/installer/WicketApplication.java
+++ essentials/plugin-dashboard/src/main/java/org/onehippo/cms7/essentials/installer/WicketApplication.java
@@ -3,6 +3,7 @@ package org.onehippo.cms7.essentials.installer;
 import org.apache.wicket.RuntimeConfigurationType;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.protocol.http.WebApplication;
+import org.onehippo.cms7.essentials.setup.SetupPage;
 
 /**
  * Application object for your web application. If you want to run this application without deploying, run the Start class.
@@ -15,7 +16,7 @@ public class WicketApplication extends WebApplication {
      */
     @Override
     public Class<? extends WebPage> getHomePage() {
-        return HomePage.class;
+        return SetupPage.class;
     }
 
     /**
