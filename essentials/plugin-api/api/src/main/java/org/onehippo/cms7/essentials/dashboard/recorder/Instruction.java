bc0de3524cb4832dad668d1a766886ecffb5c7a2
Marijan Milicevic <m.milicevic@onehippo.com>
2013-10-25 12:50:51 +0000

    - xsd first cut

--- essentials/plugin-api/api/src/main/java/org/onehippo/cms7/essentials/dashboard/recorder/Instruction.java
+++ essentials/plugin-api/api/src/main/java/org/onehippo/cms7/essentials/dashboard/recorder/Instruction.java
@@ -9,32 +9,6 @@ package org.onehippo.cms7.essentials.dashboard.recorder;
  */
 public interface Instruction {
 
-    /**
-     * Instruction description
-     *
-     * @return instruction description
-     */
-    //String description();
-
-    /**
-     * Returns type of instruction
-     *
-     * @return type of instruction
-     */
-    //InstructionType getType();
-
-    /**
-     * Executes an instruction.
-     *
-     * @return returns itself
-     */
     Instruction record();
 
-    /**
-     * rollbacks an instruction.
-     * <p><strong>NOTE:</strong> some instructions cannot be rolled back</p>
-     *
-     * @return returns itself
-     */
-    //Instruction rollback();
 }
