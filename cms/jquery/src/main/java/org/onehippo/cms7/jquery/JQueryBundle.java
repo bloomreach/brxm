8ddee73f7e8f0b4a30422b4bf97867757e8d5f49
Unico Hommes <u.hommes@onehippo.com>
2013-11-11 13:30:47 +0000

    CMS7-7489 use jquery 1.8.3, jquery-ui 1.9.2

--- cms/jquery/src/main/java/org/onehippo/cms7/jquery/JQueryBundle.java
+++ cms/jquery/src/main/java/org/onehippo/cms7/jquery/JQueryBundle.java
@@ -20,8 +20,10 @@ public final class JQueryBundle {
     private JQueryBundle() {
     }
 
-    public static final String JQUERY_CORE = "core/jquery-1.6.2.js";
-    public static final String JQUERY_CORE_MIN = "core/jquery-1.6.2-min.js";
+//    public static final String JQUERY_CORE = "core/jquery-1.6.2.js";
+//    public static final String JQUERY_CORE_MIN = "core/jquery-1.6.2-min.js";
+    public static final String JQUERY_CORE = "core/jquery-1.8.3.js";
+    public static final String JQUERY_CORE_MIN = "core/jquery-1.8.3-min.js";
 
     public static final String JQUERY_CLASS_PLUGIN = "plugins/jquery.class.js";
     public static final String JQUERY_CLASS_PLUGIN_MIN = "plugins/jquery.class-min.js";
@@ -32,8 +34,10 @@ public final class JQueryBundle {
     public static final String JQUERY_HEAD_PLUGIN = "plugins/jquery.head-1.0.3b.js";
     public static final String JQUERY_HEAD_PLUGIN_MIN = "plugins/jquery.head-1.0.3b-min.js";
 
-    public static final String JQUERY_UI = "ui/jquery-ui-1.8.16.js";
-    public static final String JQUERY_UI_MIN = "ui/jquery-ui-1.8.16-min.js";
+//    public static final String JQUERY_UI = "ui/jquery-ui-1.8.16.js";
+//    public static final String JQUERY_UI_MIN = "ui/jquery-ui-1.8.16-min.js";
+    public static final String JQUERY_UI = "ui/jquery-ui-1.9.2.js";
+    public static final String JQUERY_UI_MIN = "ui/jquery-ui-1.9.2-min.js";
 
     public static final String JQUERY_ALL = "jquery-all.js";
     public static final String JQUERY_ALL_MIN = "jquery-all-min.js";
