1c54cf7f3fabaaf837c0826bd1ed5bae3e10d362
Woonsan Ko <w.ko@onehippo.com>
2013-11-26 16:55:52 +0000

    REPO-844: changing the package name for example scxml model test

--- repository/workflow/src/test/java/org/onehippo/repository/documentworkflow/model/example/ExampleWorkflowParallelModelInitializationTest.java
+++ repository/workflow/src/test/java/org/onehippo/repository/documentworkflow/model/example/ExampleWorkflowParallelModelInitializationTest.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.onehippo.repository.documentworkflow.model;
+package org.onehippo.repository.documentworkflow.model.example;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
--- repository/workflow/src/test/java/org/onehippo/repository/documentworkflow/model/example/VariantCopyAction.java
+++ repository/workflow/src/test/java/org/onehippo/repository/documentworkflow/model/example/VariantCopyAction.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.onehippo.repository.documentworkflow.model;
+package org.onehippo.repository.documentworkflow.model.example;
 
 import java.util.Collection;
 import java.util.HashMap;
