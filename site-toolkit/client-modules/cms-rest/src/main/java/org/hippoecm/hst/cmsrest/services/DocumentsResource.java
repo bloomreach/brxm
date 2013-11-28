2f34c35ad11bf14456415e8544a984ae878d1679
Ard Schrijvers <a.schrijvers@onehippo.com>
2013-11-28 15:53:30 +0000

    HSTTWO-2770 of course compare with bestLin. Thanks for spotting Mathijs!

--- site-toolkit/client-modules/cms-rest/src/main/java/org/hippoecm/hst/cmsrest/services/DocumentsResource.java
+++ site-toolkit/client-modules/cms-rest/src/main/java/org/hippoecm/hst/cmsrest/services/DocumentsResource.java
@@ -209,7 +209,7 @@ public class DocumentsResource extends BaseResource  implements DocumentService 
         HstLink bestLink = secondCandidateList.get(0);
         if (secondCandidateList.size() > 1) {
             for (HstLink link : secondCandidateList) {
-                if (link == secondCandidateList) {
+                if (link == bestLink) {
                     continue;
                 }
                 if (hasLinkMoreMountAncestorsThanBestLink(link, bestLink)) {
