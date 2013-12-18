ea7506844795ed76689f39d67854a3bd0c585f2c
Kenan Salic <k.salic@onehippo.com>
2013-12-18 16:18:07 +0000

    ESSENTIALS-88 plugin - content blocks latest merge

---
+++ essentials-components/plugin-dashboard/src/main/java/org/onehippo/cms7/essentials/rest/model/contentblocks/Compounds.java
@@ -0,0 +1,35 @@
+package org.onehippo.cms7.essentials.rest.model.contentblocks;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+import org.onehippo.cms7.essentials.rest.model.KeyValueRestful;
+import org.onehippo.cms7.essentials.rest.model.Restful;
+import org.onehippo.cms7.essentials.rest.model.RestfulList;
+
+/**
+ * @version "$Id$"
+ */
+@XmlRootElement(name = "items")
+public class Compounds extends KeyValueRestful implements Restful {
+
+    private static final long serialVersionUID = 1L;
+
+    private String path;
+
+    public Compounds() {
+    }
+
+    public Compounds(final String key, final String value, final String path) {
+        super(key, value);
+        this.path = path;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(final String path) {
+        this.path = path;
+    }
+}
