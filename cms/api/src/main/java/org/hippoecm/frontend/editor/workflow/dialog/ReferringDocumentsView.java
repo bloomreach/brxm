b5607ba653ce5bb924ff9957891387fee8cc2407
Arthur Bogaart <a.bogaart@onehippo.com>
2014-01-16 13:33:38 +0000

    CMS7-7550: Improvement on CMS7 - 7535: The last item checkbox in the referring documents dialog can not be activated  - Added marker className to doclisting-name column  - Added 100% width to the header of the doclisting-name column to ensure it is always the same width. If we leave this out, the first column containing the checkbox will take more width than intended if there is no document name that overflows the doclisting-name cells.  - Set width of 'tr.hippo-list-paging td' to be 75em which forces the paging widget to always align completely to the right, even in the big dialog of the 'Schedule take offline...' action.

--- cms/api/src/main/java/org/hippoecm/frontend/editor/workflow/dialog/ReferringDocumentsView.java
+++ cms/api/src/main/java/org/hippoecm/frontend/editor/workflow/dialog/ReferringDocumentsView.java
@@ -1,12 +1,12 @@
 /*
  *  Copyright 2009-2013 Hippo B.V. (http://www.onehippo.com)
- * 
+ *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
  *  You may obtain a copy of the License at
- * 
+ *
  *       http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  *  Unless required by applicable law or agreed to in writing, software
  *  distributed under the License is distributed on an "AS IS" BASIS,
  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -191,6 +191,7 @@ public class ReferringDocumentsView extends Panel implements IPagingDefinition {
 
         column = new ListColumn(new StringResourceModel("doclisting-name", this, null), null);
         column.setAttributeModifier(new DocumentAttributeModifier());
+        column.setCssClass("doclisting-name");
         columns.add(column);
 
         column = new ListColumn(new StringResourceModel("doclisting-state", this, null), null);
