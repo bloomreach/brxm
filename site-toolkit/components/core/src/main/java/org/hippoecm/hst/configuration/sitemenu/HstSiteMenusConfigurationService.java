cf74d348602dd4b4b64b4426d0042bb3b732e46c
Ard Schrijvers <a.schrijvers@onehippo.com>
2014-01-27 09:08:30 +0000

    HSTTWO-2818 account for review from Mathijs

--- site-toolkit/components/core/src/main/java/org/hippoecm/hst/configuration/sitemenu/HstSiteMenusConfigurationService.java
+++ site-toolkit/components/core/src/main/java/org/hippoecm/hst/configuration/sitemenu/HstSiteMenusConfigurationService.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2008-2013 Hippo B.V. (http://www.onehippo.com)
+ *  Copyright 2008-2014 Hippo B.V. (http://www.onehippo.com)
  * 
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -25,7 +25,6 @@ import org.hippoecm.hst.configuration.HstNodeTypes;
 import org.hippoecm.hst.configuration.cache.CompositeConfigurationNodes;
 import org.hippoecm.hst.configuration.model.HstNode;
 import org.hippoecm.hst.configuration.site.HstSite;
-import org.hippoecm.hst.configuration.site.HstSiteService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
