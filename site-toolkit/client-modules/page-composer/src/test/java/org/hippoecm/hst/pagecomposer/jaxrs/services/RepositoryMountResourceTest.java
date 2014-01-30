27885777d0861ce27bdd327ecdafccf4bdaa2ef0
Ard Schrijvers <a.schrijvers@onehippo.com>
2014-01-30 10:50:39 +0000

    HSTTWO-1003 better named test: When it is against a bootstrapped repository, make this clean in the name

--- site-toolkit/client-modules/page-composer/src/test/java/org/hippoecm/hst/pagecomposer/jaxrs/services/RepositoryMountResourceTest.java
+++ site-toolkit/client-modules/page-composer/src/test/java/org/hippoecm/hst/pagecomposer/jaxrs/services/RepositoryMountResourceTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013 Hippo B.V. (http://www.onehippo.com)
+ * Copyright 2013-2014 Hippo B.V. (http://www.onehippo.com)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -38,7 +38,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-public class MountResourceTest extends AbstractPageComposerTest {
+public class RepositoryMountResourceTest extends AbstractPageComposerTest {
 
     @Test
     public void testEditAndPublishMount() throws Exception {
