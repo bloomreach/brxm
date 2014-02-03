c1f09aae25605d9ee39644d773ade7e5c34cbb82
Jeroen Reijn <j.reijn@onehippo.com>
2014-02-03 14:48:52 +0000

    Added some more test coverage

--- essentials/plugin-api/implementation/src/test/java/org/onehippo/cms7/essentials/dashboard/utils/HippoNodeUtilsTest.java
+++ essentials/plugin-api/implementation/src/test/java/org/onehippo/cms7/essentials/dashboard/utils/HippoNodeUtilsTest.java
@@ -17,6 +17,7 @@ import org.onehippo.cms7.essentials.BaseRepositoryTest;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 /**
  * @version "$Id: HippoNodeUtilsTest.java 167907 2013-06-17 08:34:55Z mmilicevic $"
@@ -44,12 +45,39 @@ public class HippoNodeUtilsTest extends BaseRepositoryTest {
         assertEquals("prefix", HippoNodeUtils.getPrefixFromType("prefix:name"));
         assertEquals("", HippoNodeUtils.getPrefixFromType(":name"));
         assertEquals(null, HippoNodeUtils.getPrefixFromType("name"));
+        assertEquals(null, HippoNodeUtils.getPrefixFromType(""));
     }
 
     @Test
     public void testResolvePath() throws Exception {
         assertEquals("/hippo:namespaces/someprefix/document", HippoNodeUtils.resolvePath("someprefix:document"));
         assertEquals("/hippo:namespaces/system/String", HippoNodeUtils.resolvePath("String"));
-
     }
+
+    @Test
+    public void testGetCompounds() throws Exception {
+        final Set<String> compounds = HippoNodeUtils.getCompounds(session);
+        assertTrue(compounds.size()==0);
+    }
+
+    @Test(expected = Exception.class)
+    public void testExceptionWhenNameIsEmpty() throws Exception {
+        HippoNodeUtils.checkName("");
+    }
+
+    @Test(expected = Exception.class)
+    public void testExceptionWhenNameContainsNumbers() throws Exception {
+        HippoNodeUtils.checkName("aBc2e");
+    }
+
+    @Test(expected = Exception.class)
+    public void testExceptionWhenUriIsEmpty() throws Exception {
+        HippoNodeUtils.checkURI("");
+    }
+
+    @Test(expected = Exception.class)
+    public void testExceptionWhenInvalidURI() throws Exception {
+        HippoNodeUtils.checkURI("htp://invalidUrl");
+    }
+
 }
