f38cbd46b36ce485b2bb21cc669c8049c713e386
Ate Douma <a.douma@onehippo.com>
2014-02-19 16:08:31 +0000

    CMS7-7733: fix permission check in SCXML documentworkflow using the workflow *subject* (e.g. handle) its session, not the workflow userSession

--- repository/workflow/src/main/java/org/onehippo/repository/documentworkflow/DocumentHandle.java
+++ repository/workflow/src/main/java/org/onehippo/repository/documentworkflow/DocumentHandle.java
@@ -138,14 +138,14 @@ public class DocumentHandle implements SCXMLDataModel {
     }
 
     /**
-     * Checks if specific privileges (e.g. hippo:editor) are granted to the current user session against a specific
+     * Checks if specific privileges (e.g. hippo:editor) are granted to the current workflow subject (e.g. handle) its session for a specific
      * {@link Document}.
      * <p> Implementation note: previously evaluated privileges are cached against the Document node its path
      * within the DocumentHandle instance </p>
      *
-     * @param document the document to check permission against
+     * @param document the document to check permission for
      * @param privileges the privileges (, separated) to check permission for
-     * @return true if the current user session has been granted all of the specified privileges for the document node
+     * @return true if the current subject session has been granted all of the specified privileges for the document node
      */
     public boolean isGranted(Document document, String privileges) {
         if (privileges == null || document == null || document.getIdentity() == null) {
@@ -157,10 +157,10 @@ public class DocumentHandle implements SCXMLDataModel {
             return false;
         }
 
-        final Session userSession = context.getUserSession();
         try {
-            Node userDocumentNode = userSession.getNodeByIdentifier(document.getIdentity());
-            String userDocumentPath = userDocumentNode.getPath();
+            final Session subjectSession = handle.getSession();
+            Node subjectDocumentNode = subjectSession.getNodeByIdentifier(document.getIdentity());
+            String userDocumentPath = subjectDocumentNode.getPath();
             for (String priv : privs) {
                 Map<String, Boolean> privilegesMap = pathPrivilegesMap.get(userDocumentPath);
                 if (privilegesMap == null) {
@@ -171,7 +171,7 @@ public class DocumentHandle implements SCXMLDataModel {
                 if (hasPrivilege == null) {
                     hasPrivilege = Boolean.FALSE;
                     try {
-                        hasPrivilege = userSession.hasPermission(userDocumentPath, priv);
+                        hasPrivilege = subjectSession.hasPermission(userDocumentPath, priv);
                     } catch (AccessControlException | IllegalArgumentException | RepositoryException ignore) {
                     }
                     privilegesMap.put(priv, hasPrivilege);
