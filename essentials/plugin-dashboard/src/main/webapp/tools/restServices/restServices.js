298b4208bc491430f5694bc794410b77061a96e1
Marijan Milicevic <m.milicevic@onehippo.com>
2014-02-20 14:19:54 +0000

    - add layout and type choice

--- essentials/plugin-dashboard/src/main/webapp/tools/restServices/restServices.js
+++ essentials/plugin-dashboard/src/main/webapp/tools/restServices/restServices.js
@@ -5,6 +5,13 @@
             .controller('restServicesCtrl', function ($scope, $sce, $log, $rootScope, $http) {
                 $scope.endpoint = $rootScope.REST.dynamic + 'restservices/';
                 $scope.resultMessages = [];
+                $scope.restType = null;
+                $scope.selectedType = "Choose REST";
+                $scope.apiName = "restapi";
+
+                $scope.checkType = function () {
+                    console.log($scope.restType);
+                };
                 $scope.runRestSetup = function () {
                     $http.post($scope.endpoint).success(function (data) {
 
