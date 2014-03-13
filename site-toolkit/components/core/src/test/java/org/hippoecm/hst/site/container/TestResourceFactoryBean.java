b4895d3c26b783fcb3f625fc6f5676db0b4e62ca
Woonsan Ko <w.ko@onehippo.com>
2014-03-13 20:10:17 +0000

    HSTTWO-2887: adding fallback option when loading a resource through ResourceFactoryBean

--- site-toolkit/components/core/src/test/java/org/hippoecm/hst/site/container/TestResourceFactoryBean.java
+++ site-toolkit/components/core/src/test/java/org/hippoecm/hst/site/container/TestResourceFactoryBean.java
@@ -21,62 +21,89 @@ import static org.junit.Assert.assertTrue;
 import java.io.File;
 import java.net.URI;
 import java.net.URL;
+import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.FactoryBean;
 import org.springframework.context.ResourceLoaderAware;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
-import org.springframework.core.io.ResourceLoader;
 
 
 public class TestResourceFactoryBean {
-    
-    private ResourceLoader resourceLoader;
-    
+
+    private ClassPathXmlApplicationContext appContext;
+
     @Before
     public void setUp() throws Exception {
-        resourceLoader = new ClassPathXmlApplicationContext();
+        appContext = new ClassPathXmlApplicationContext("META-INF/assembly/example-resource-factory-beans.xml");
     }
-    
+
     @Test
     public void testResourceFactoryBean() throws Exception {
         String resourcePath = "classpath:/" + ResourceFactoryBean.class.getName().replace('.', '/') + ".class";
-        
+
         FactoryBean factoryBean = new ResourceFactoryBean(resourcePath);
-        ((ResourceLoaderAware) factoryBean).setResourceLoader(resourceLoader);
+        ((ResourceLoaderAware) factoryBean).setResourceLoader(appContext);
         Object bean = factoryBean.getObject();
         assertTrue(bean instanceof Resource);
         assertEquals(Resource.class, factoryBean.getObjectType());
-        
+
         factoryBean = new ResourceFactoryBean(resourcePath, URL.class);
-        ((ResourceLoaderAware) factoryBean).setResourceLoader(resourceLoader);
+        ((ResourceLoaderAware) factoryBean).setResourceLoader(appContext);
         bean = factoryBean.getObject();
         assertTrue(bean instanceof URL);
         assertTrue(bean.toString().endsWith("/ResourceFactoryBean.class"));
         assertEquals(URL.class, factoryBean.getObjectType());
-        
+
         factoryBean = new ResourceFactoryBean(resourcePath, URI.class);
-        ((ResourceLoaderAware) factoryBean).setResourceLoader(resourceLoader);
+        ((ResourceLoaderAware) factoryBean).setResourceLoader(appContext);
         bean = factoryBean.getObject();
         assertTrue(bean instanceof URI);
         assertTrue(bean.toString().endsWith("/ResourceFactoryBean.class"));
         assertEquals(URI.class, factoryBean.getObjectType());
-        
+
         factoryBean = new ResourceFactoryBean(resourcePath, String.class);
-        ((ResourceLoaderAware) factoryBean).setResourceLoader(resourceLoader);
+        ((ResourceLoaderAware) factoryBean).setResourceLoader(appContext);
         bean = factoryBean.getObject();
         assertTrue(bean instanceof String);
         assertTrue(bean.toString().endsWith("/ResourceFactoryBean.class"));
         assertEquals(String.class, factoryBean.getObjectType());
-        
+
         factoryBean = new ResourceFactoryBean(resourcePath, File.class);
-        ((ResourceLoaderAware) factoryBean).setResourceLoader(resourceLoader);
+        ((ResourceLoaderAware) factoryBean).setResourceLoader(appContext);
         bean = factoryBean.getObject();
         assertTrue(bean instanceof File);
         assertEquals(((File) bean).getName(), "ResourceFactoryBean.class");
         assertEquals(File.class, factoryBean.getObjectType());
+    }
+
+    @Test
+    public void testDefaultHstJaasLoginModuleConfigUrlInSpringAssembly() throws Exception {
+        Object configUrl = appContext.getBean("defaultHstJaasLoginModuleConfigUrlString");
+        assertEquals("java.lang.String", configUrl.getClass().getName());
+        assertTrue(configUrl.toString().endsWith("/META-INF/example-login.conf"));
+
+        List<?> resourceBeansList = (List<?>) appContext.getBean("resourceBeansList");
+        assertEquals(1, resourceBeansList.size());
+        Object configUrl2 = resourceBeansList.get(0);
+        assertEquals("java.lang.String", configUrl2.getClass().getName());
+        assertTrue(configUrl2.toString().endsWith("/META-INF/example-login.conf"));
+    }
+
+    @Test
+    public void testResourceFactoryBeanFallbackToDefaultObject() throws Exception {
+        String resourcePath = "classpath:/some/non/existing/resource/path/really.no";
+        String defaultResourceValue = "file:/a/default/path/really.yes";
+
+        ResourceFactoryBean factoryBean = new ResourceFactoryBean(resourcePath, String.class, defaultResourceValue);
+        ((ResourceLoaderAware) factoryBean).setResourceLoader(appContext);
+        factoryBean.setIgnoreCreationError(true);
+
+        Object bean = factoryBean.getObject();
+        assertEquals("java.lang.String", bean.getClass().getName());
+        assertEquals(String.class, factoryBean.getObjectType());
+        assertEquals(defaultResourceValue, bean);
     }
 }
