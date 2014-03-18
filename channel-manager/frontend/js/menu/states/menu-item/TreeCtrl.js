ffa9d7c208f10479253c85156dc4760f94d5d714
Niels Dequeker <n.dequeker@onehippo.com>
2014-03-18 14:14:20 +0000

    CMS7-7797 Hide server side feedback when navigating to a different menu item + move dragged menu item back when the move operation fails server side

--- channel-manager/frontend/js/menu/states/menu-item/TreeCtrl.js
+++ channel-manager/frontend/js/menu/states/menu-item/TreeCtrl.js
@@ -41,6 +41,8 @@
                 $scope.callbacks = {
                     itemClicked: function (itemScope) {
                         MenuService.saveMenuItem($scope.$parent.selectedMenuItem).then(function () {
+                                $scope.$parent.feedback = '';
+
                                 $state.go('menu-item.edit', {
                                     menuItemId: itemScope.id
                                 });
@@ -54,14 +56,19 @@
                         // created an issue for the Tree component, to add a disabled state
                         // link: https://github.com/JimLiu/angular-ui-tree/issues/63
                         if (!FormValidationService.getValidity()) {
-                            // revert tree
                             // move the item back at it's original place in the DOM
                             var removedItem = destScope.sortableModelValue.splice(destIndex, 1)[0];
                             sourceScope.sortableModelValue.splice(sourceIndex, 0, removedItem);
                         } else {
                             var parentData = destScope.parentItemScope();
                             var destId = (!parentData) ? ConfigService.menuId : parentData.itemData().id;
-                            MenuService.moveMenuItem(modelData.id, destId, destIndex).then(onSuccess, onError);
+                            MenuService.moveMenuItem(modelData.id, destId, destIndex).then(onSuccess, function (errorResponse) {
+                                $scope.$parent.feedback = FeedbackService.getFeedback(errorResponse);
+
+                                // move the item back at it's original place in the DOM
+                                var removedItem = destScope.sortableModelValue.splice(destIndex, 1)[0];
+                                sourceScope.sortableModelValue.splice(sourceIndex, 0, removedItem);
+                            });
                         }
 
                         // prevent move action when the edit menu item form is invalid
