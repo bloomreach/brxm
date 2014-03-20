fb84a89bfd756516f478189f41ef01982ecb538f
Marijan Milicevic <m.milicevic@onehippo.com>
2014-03-20 17:20:25 +0000

    - delete old test skeleton

--- essentials/pluginTestSkeleton/js/wizard-controller.js
+++
@@ -1,8 +0,0 @@
-(function () {
-    "use strict";
-    angular.module('hippo.essentials')
-            .controller('wizardCtrl', function ($scope) {
-                $scope.message = "Wizard test";
-
-            })
-})();
--- essentials/pluginTestSkeleton/readme.txt
+++
@@ -1,2 +0,0 @@
-works with intellij liveedit plugin and  built in server, @see:
-http://blog.jetbrains.com/webide/2013/03/built-in-server-in-webstorm-6/
