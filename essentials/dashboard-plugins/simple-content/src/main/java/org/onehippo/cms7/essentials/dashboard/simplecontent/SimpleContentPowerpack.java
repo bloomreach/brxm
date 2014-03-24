e4c166a00ac60a2b63dc4244f72fe57fd27f9bc3
Marijan Milicevic <m.milicevic@onehippo.com>
2014-03-24 21:20:28 +0000

    - add skeleton for sample data and templating

--- essentials/dashboard-plugins/simple-content/src/main/java/org/onehippo/cms7/essentials/dashboard/simplecontent/SimpleContentPowerpack.java
+++ essentials/dashboard-plugins/simple-content/src/main/java/org/onehippo/cms7/essentials/dashboard/simplecontent/SimpleContentPowerpack.java
@@ -1,9 +1,31 @@
 package org.onehippo.cms7.essentials.dashboard.simplecontent;
 
+import java.util.Set;
+
 import org.onehippo.cms7.essentials.dashboard.packaging.DefaultPowerpack;
+import org.onehippo.cms7.essentials.dashboard.utils.EssentialConst;
+
+import com.google.common.base.Strings;
+import com.google.common.collect.ImmutableSet;
 
 
 public class SimpleContentPowerpack extends DefaultPowerpack {
+
+    @Override
+    public Set<String> groupNames() {
+        final Boolean sampleData = Boolean.valueOf((String) getProperties().get("sampleData"));
+        final String templateName = (String) getProperties().get("templateName");
+        final String templateGroup = Strings.isNullOrEmpty(templateName) ? "jsp" : templateName;
+        if (sampleData) {
+            return new ImmutableSet.Builder<String>()
+                    .add(EssentialConst.INSTRUCTION_GROUP_DEFAULT)
+                    .add("sampleData")
+                    .add(templateGroup).build();
+        }
+        return new ImmutableSet.Builder<String>()
+                .add(EssentialConst.INSTRUCTION_GROUP_DEFAULT)
+                .add(templateGroup).build();
+    }
 
     @Override
     public String getInstructionPath() {
--- essentials/dashboard-plugins/simple-content/src/main/resources/META-INF/resources/powerpacks/simpleContent/simpleContent.js
+++ essentials/dashboard-plugins/simple-content/src/main/resources/META-INF/resources/powerpacks/simpleContent/simpleContent.js
@@ -1,13 +1,17 @@
 (function () {
     "use strict";
     angular.module('hippo.essentials')
-            .controller('simpleContentCtrl', function ($scope, $sce, $log, $rootScope, $http) {
-                $scope.endpoint = $rootScope.REST.dynamic + 'simpleContent/';
-                $scope.message = {};
-                $scope.run = function() {
-                                                    $http.post($scope.endpoint).success(function (data) {
-                            $scope.message = data;
-                                            });
-                };
-            })
+        .controller('simpleContentCtrl', function ($scope, $sce, $log, $rootScope, $http) {
+            $scope.endpoint = $rootScope.REST.dynamic + 'simpleContent/';
+            $scope.sampleData = true;
+            $scope.templateName = 'jsp';
+            $scope.message = {};
+            $scope.run = function () {
+                var payload = Essentials.addPayloadData("sampleData", $scope.sampleData, null);
+                Essentials.addPayloadData("templateName", $scope.templateName, payload);
+                $http.post($scope.endpoint, payload).success(function (data) {
+                    // globally handled
+                });
+            };
+        })
 })();
