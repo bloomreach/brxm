9b0551d78258e1ec6db587118a75431549d3c7dd
Marijan Milicevic <m.milicevic@onehippo.com>
2014-04-18 12:36:48 +0000

    Merge branch 'master' of github.com:onehippo/essentials

--- essentials-components/dashboard-plugins/components-plugin/components-cms/src/main/java/org/onehippo/cms7/essentials/components/cms/modules/BlogListenerModule.java
+++ essentials-components/dashboard-plugins/components-plugin/components-cms/src/main/java/org/onehippo/cms7/essentials/components/cms/modules/BlogListenerModule.java
@@ -16,14 +16,17 @@
 
 package org.onehippo.cms7.essentials.components.cms.modules;
 
-import java.util.Calendar;
-import java.util.Date;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.jcr.Node;
+import javax.jcr.Property;
 import javax.jcr.RepositoryException;
 import javax.jcr.Session;
+import javax.jcr.Value;
 import javax.jcr.observation.Event;
 
+import org.apache.commons.lang.ArrayUtils;
 import org.apache.jackrabbit.api.observation.JackrabbitEvent;
 import org.hippoecm.repository.util.JcrUtils;
 import org.onehippo.cms7.essentials.components.cms.blog.BlogImporterJob;
@@ -35,11 +38,10 @@ import org.onehippo.repository.scheduling.RepositoryJobCronTrigger;
 import org.onehippo.repository.scheduling.RepositoryJobInfo;
 import org.onehippo.repository.scheduling.RepositoryJobTrigger;
 import org.onehippo.repository.scheduling.RepositoryScheduler;
-import org.quartz.Trigger;
-import org.quartz.TriggerUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.google.common.base.Joiner;
 import com.google.common.base.Strings;
 
 /**
@@ -56,7 +58,7 @@ public class BlogListenerModule extends AbstractReconfigurableDaemonModule {
 
     private final Object lock = new Object();
     private static final String CONFIG_MODULECONFIGPATH = "moduleconfigpath";
-    private static final String CONFIG_CRONEXPRESSION_PROPERTY = "cronexpression";
+    private static final String CONFIG_CRONEXPRESSION_PROPERTY = "cronExpression";
     private static final String CONFIG_LOCK_ISDEEP_PROPERTY = "jcr:lockIsDeep";
     private static final String CONFIG_LOCK_OWNER = "jcr:lockOwner";
 
@@ -64,21 +66,62 @@ public class BlogListenerModule extends AbstractReconfigurableDaemonModule {
     private Session session;
     private AuthorFieldHandler listener;
 
-    private RepositoryJobInfo jobInfo;
 
     private String cronExpression;
+    private Boolean active;
+    private Boolean runNow;
+    private String blogBasePath;
+    private String authorsBasePath;
+    private String projectNamespace;
+    private String[] urls;
+    private String[] authors;
+
     public static final String SCHEDULER_NAME = "BlogListenerModule";
     public static final String SCHEDULER_GROUP_NAME = "essentials";
+    private static final String JOB_NAME = SCHEDULER_NAME + "Job";
 
     @Override
     protected void doConfigure(final Node moduleConfig) throws RepositoryException {
         cronExpression = JcrUtils.getStringProperty(moduleConfig, CONFIG_CRONEXPRESSION_PROPERTY, null);
-        String projectNamespace = JcrUtils.getStringProperty(moduleConfig, "projectNamespace", null);
+        projectNamespace = JcrUtils.getStringProperty(moduleConfig, "projectNamespace", null);
         if (!Strings.isNullOrEmpty(projectNamespace)) {
             listener = new AuthorFieldHandler(projectNamespace);
         } else {
-            log.warn("No projectNamespacePath configured in [org.onehippo.cms7.essentials.components.cms.modules.EventBusListenerModule]");
+            log.warn("No projectNamespace configured in [org.onehippo.cms7.essentials.components.cms.modules.EventBusListenerModule]");
         }
+        active = JcrUtils.getBooleanProperty(moduleConfig, "active", Boolean.FALSE);
+        runNow = JcrUtils.getBooleanProperty(moduleConfig, "runInstantly", Boolean.FALSE);
+        blogBasePath = JcrUtils.getStringProperty(moduleConfig, BlogImporterJob.BLOGS_BASE_PATH, null);
+        authorsBasePath = JcrUtils.getStringProperty(moduleConfig, BlogImporterJob.AUTHORS_BASE_PATH, null);
+        urls = readStrings(moduleConfig, BlogImporterJob.URLS);
+        authors = readStrings(moduleConfig, BlogImporterJob.AUTHORS);
+        if (authors.length != urls.length) {
+            log.error("Authors and URL size mismatch, no blogs will be imported.");
+            authors = ArrayUtils.EMPTY_STRING_ARRAY;
+            urls = ArrayUtils.EMPTY_STRING_ARRAY;
+        }
+    }
+
+
+    private String[] readStrings(final Node node, final String propertyName) {
+        try {
+            if (node.hasProperty(propertyName)) {
+                final Property property = node.getProperty(propertyName);
+                final List<String> retVal = new ArrayList<>();
+                if (property.isMultiple()) {
+                    final Value[] values = property.getValues();
+                    for (Value value : values) {
+                        final String myUrl = value.getString();
+                        retVal.add(myUrl);
+                    }
+                }
+                return retVal.toArray(new String[retVal.size()]);
+            }
+
+        } catch (RepositoryException e) {
+            log.error("Error reading property", e);
+        }
+        return ArrayUtils.EMPTY_STRING_ARRAY;
     }
 
     @Override
@@ -93,46 +136,49 @@ public class BlogListenerModule extends AbstractReconfigurableDaemonModule {
 
 
     private void scheduleJob() throws RepositoryException {
-        if (Strings.isNullOrEmpty(cronExpression)) {
+        if (!active || Strings.isNullOrEmpty(cronExpression) || Strings.isNullOrEmpty(projectNamespace) || urls.length == 0) {
             return;
         }
 
         final RepositoryScheduler scheduler = HippoServiceRegistry.getService(RepositoryScheduler.class);
 
-        final String jobName = SCHEDULER_NAME + "Job";
-        if (scheduler.checkExists(jobName, SCHEDULER_GROUP_NAME)) {
+
+        if (scheduler.checkExists(JOB_NAME, SCHEDULER_GROUP_NAME)) {
             return;
         }
 
-        jobInfo = new RepositoryJobInfo(jobName, BlogImporterJob.class);
-        jobInfo.setAttribute(CONFIG_MODULECONFIGPATH, moduleConfigPath);
+        final RepositoryJobInfo jobInfo = createJobInfo(JOB_NAME);
+
 
         final RepositoryJobTrigger trigger = new RepositoryJobCronTrigger(SCHEDULER_NAME + "Trigger", cronExpression);
         scheduler.scheduleJob(jobInfo, trigger);
-    }
+        if (runNow) {
 
-    private void unScheduleJob() throws RepositoryException {
-        if (jobInfo != null) {
-            final RepositoryScheduler repositoryScheduler = HippoServiceRegistry.getService(RepositoryScheduler.class);
-            repositoryScheduler.deleteJob(jobInfo.getName(), jobInfo.getGroup());
-            jobInfo = null;
+            final String name = SCHEDULER_NAME + "TriggerNow";
+            final RepositoryJobInfo nowJobInfo = createJobInfo(name);
+            final RepositoryJobTrigger runNowTrigger = new RepositoryJobCronTrigger(name, cronExpression);
+            scheduler.scheduleJob(nowJobInfo, runNowTrigger);
+            // execute and remove:
+            scheduler.executeJob(name, nowJobInfo.getGroup());
+            scheduler.deleteJob(name, nowJobInfo.getGroup());
         }
     }
 
-
-    private Trigger createRunNowTrigger(final RepositoryJobInfo jobInfo) {
-
-        Trigger trigger = TriggerUtils.makeImmediateTrigger(0, 0);
-        Calendar calendar = Calendar.getInstance();
-        calendar.setTime(new Date());
-        calendar.add(Calendar.SECOND, 5);
-        trigger.setStartTime(calendar.getTime());
-
-        trigger.setName(jobInfo.getName());
-        trigger.setGroup(jobInfo.getGroup());
-        return trigger;
-
+    private RepositoryJobInfo createJobInfo(final String jobName) {
+        final RepositoryJobInfo jobInfo = new RepositoryJobInfo(jobName, SCHEDULER_GROUP_NAME, BlogImporterJob.class);
+        jobInfo.setAttribute(BlogImporterJob.PROJECT_NAMESPACE, projectNamespace);
+        jobInfo.setAttribute(BlogImporterJob.AUTHORS_BASE_PATH, authorsBasePath);
+        jobInfo.setAttribute(BlogImporterJob.BLOGS_BASE_PATH, blogBasePath);
+        jobInfo.setAttribute(BlogImporterJob.AUTHORS, Joiner.on(BlogImporterJob.SPLITTER).join(authors));
+        jobInfo.setAttribute(BlogImporterJob.URLS, Joiner.on(BlogImporterJob.SPLITTER).join(urls));
+        return jobInfo;
     }
+
+    private void unScheduleJob() throws RepositoryException {
+        final RepositoryScheduler repositoryScheduler = HippoServiceRegistry.getService(RepositoryScheduler.class);
+        repositoryScheduler.deleteJob(JOB_NAME, SCHEDULER_GROUP_NAME);
+    }
+
 
     //############################################
     //
@@ -161,7 +207,7 @@ public class BlogListenerModule extends AbstractReconfigurableDaemonModule {
 
     @Override
     protected void doShutdown() {
-
+        session.logout();
     }
 
 }
