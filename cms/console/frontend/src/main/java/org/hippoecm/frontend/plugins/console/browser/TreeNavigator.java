26b7332aa17573e85a961210a0e0080f7481b675
Arthur Bogaart <a.bogaart@onehippo.com>
2014-04-27 20:09:05 +0000

    CMS7-7879: Prevent moving left in the tree (using keyboard left key) to go past the root  - merge from Hippo Friday branch  - simplified newSelection loop for down()

--- cms/console/frontend/src/main/java/org/hippoecm/frontend/plugins/console/browser/TreeNavigator.java
+++ cms/console/frontend/src/main/java/org/hippoecm/frontend/plugins/console/browser/TreeNavigator.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2012-2013 Hippo B.V. (http://www.onehippo.com)
+ *  Copyright 2012-2014 Hippo B.V. (http://www.onehippo.com)
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -75,19 +75,18 @@ public class TreeNavigator implements IClusterable {
         if (treeState.isNodeExpanded(node) && node.getChildCount() > 0) {
             newSelection = node.getChildAt(0);
         } else {
-            do {
+            while (newSelection == null) {
                 final TreeNode parent = node.getParent();
                 if (parent == null) {
                     break;
                 }
                 final int nodeIndex = parent.getIndex(node);
-                if (nodeIndex < (parent.getChildCount() - 1)) {
+                if (nodeIndex < parent.getChildCount() - 1) {
                     newSelection = parent.getChildAt(nodeIndex + 1);
-                    break;
                 } else {
                     node = parent;
                 }
-            } while (newSelection == null);
+            }
         }
 
         if (newSelection != null) {
@@ -103,7 +102,7 @@ public class TreeNavigator implements IClusterable {
 
         if (treeState.isNodeExpanded(node)) {
             treeState.collapseNode(node);
-        } else {
+        } else if (node.getParent() != null) {
             treeState.selectNode(node.getParent(), true);
         }
     }
