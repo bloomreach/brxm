5d24b44278432ba7b39a04d72d66e06aa79075a1
Marijan Milicevic <m.milicevic@onehippo.com>
2014-05-02 14:16:56 +0000

    - add clone component

--- essentials/dashboard-plugins/clone-component/src/main/java/org/onehippo/cms7/essentials/plugin/clonecomponent/CloneComponentResource.java
+++ essentials/dashboard-plugins/clone-component/src/main/java/org/onehippo/cms7/essentials/plugin/clonecomponent/CloneComponentResource.java
@@ -21,6 +21,7 @@ import java.util.List;
 
 import javax.jcr.Node;
 import javax.jcr.NodeIterator;
+import javax.jcr.Property;
 import javax.jcr.RepositoryException;
 import javax.jcr.Session;
 import javax.servlet.ServletContext;
@@ -34,9 +35,8 @@ import javax.ws.rs.core.MediaType;
 
 import org.onehippo.cms7.essentials.dashboard.ctx.PluginContext;
 import org.onehippo.cms7.essentials.dashboard.rest.BaseResource;
-import org.onehippo.cms7.essentials.dashboard.rest.KeyValueRestful;
+import org.onehippo.cms7.essentials.dashboard.rest.ErrorMessageRestful;
 import org.onehippo.cms7.essentials.dashboard.rest.MessageRestful;
-import org.onehippo.cms7.essentials.dashboard.rest.PostPayloadRestful;
 import org.onehippo.cms7.essentials.dashboard.utils.GlobalUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -51,13 +51,14 @@ public class CloneComponentResource extends BaseResource {
 
     private static final Logger log = LoggerFactory.getLogger(CloneComponentResource.class);
     public static final String HST_TEMPLATE = "hst:template";
+    public static final String HST_LABEL = "hst:label";
 
     @GET
-    public List<KeyValueRestful> runCloneComponent(@Context ServletContext servletContext) {
+    public List<CloneRestful> runCloneComponent(@Context ServletContext servletContext) {
 
         final PluginContext context = getContext(servletContext);
         final Session session = context.createSession();
-        List<KeyValueRestful> componentList = new ArrayList<>();
+        List<CloneRestful> componentList = new ArrayList<>();
         try {
 
             if (session.nodeExists(COMPONENTS_ROOT)) {
@@ -67,12 +68,11 @@ public class CloneComponentResource extends BaseResource {
 
                     final Node componentNode = nodes.nextNode();
                     if (componentNode.hasProperty(HST_TEMPLATE)) {
-                        componentList.add(new KeyValueRestful(componentNode.getName(), componentNode.getProperty(HST_TEMPLATE).getString()));
+                        componentList.add(new CloneRestful(componentNode.getIdentifier(), componentNode.getName(),
+                                componentNode.getProperty(HST_TEMPLATE).getString(), componentNode.getProperty(HST_LABEL).getString()));
                     } else {
                         log.warn("Component node does not have a template: {}", componentNode.getPath());
                     }
-
-
                 }
             }
         } catch (RepositoryException e) {
@@ -84,22 +84,48 @@ public class CloneComponentResource extends BaseResource {
     }
 
     @POST
-    public MessageRestful cloneComponent(final PostPayloadRestful payload, @Context ServletContext servletContext) {
+    public MessageRestful cloneComponent(final List<CloneRestful> payload, @Context ServletContext servletContext) {
 
         final PluginContext context = getContext(servletContext);
         final Session session = context.createSession();
+        final StringBuilder builder = new StringBuilder();
         try {
-            if (session.nodeExists(COMPONENTS_ROOT)) {
-                session.getNode(COMPONENTS_ROOT);
+
+            for (CloneRestful restful : payload) {
+                final Node cloneNode = session.getNodeByIdentifier(restful.getUuid());
+                final Node parent = cloneNode.getParent();
+                final Node newNode = parent.addNode(restful.getKey(), "hst:containeritemcomponent");
+                newNode.setProperty("hst:label", restful.getLabel());
+                newNode.setProperty("hst:template", restful.getValue());
+                // copy :
+                setProperty(newNode, cloneNode, "hst:componentclassname");
+                setProperty(newNode, cloneNode, "hst:iconpath");
+                setProperty(newNode, cloneNode, "hst:template");
+                setProperty(newNode, cloneNode, "hst:xtype");
+                session.save();
+                builder.append(restful.getKey());
+                builder.append(',');
+
             }
+
+
         } catch (RepositoryException e) {
-            log.error("", e);
+            log.error("Error cloning component", e);
+            return new ErrorMessageRestful("Error cloning components: " + e.getMessage());
+
         } finally {
             GlobalUtils.cleanupSession(session);
         }
 
 
-        return new MessageRestful("Not implemented yet");
+        return new MessageRestful("Following components created: : " + builder);
+    }
+
+    private void setProperty(final Node newNode,final Node cloneNode,  final String propertyName) throws RepositoryException {
+        if(cloneNode.hasProperty(propertyName)){
+            final Property property = cloneNode.getProperty(propertyName);
+            newNode.setProperty(propertyName, property.getValue());
+        }
     }
 
 
