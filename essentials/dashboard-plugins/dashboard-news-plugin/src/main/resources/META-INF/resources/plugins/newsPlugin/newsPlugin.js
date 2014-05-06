7ef2f4fed7cec197dad6f4a58cda850861d2b500
Marijan Milicevic <m.milicevic@onehippo.com>
2014-05-06 10:12:54 +0000

    ESSENTIALS-175 Split the "News and Events PowerPack" into two separate features (/plugins)
    - add generic list and content component

--- essentials/dashboard-plugins/dashboard-news-plugin/src/main/resources/META-INF/resources/plugins/newsPlugin/newsPlugin.js
+++ essentials/dashboard-plugins/dashboard-news-plugin/src/main/resources/META-INF/resources/plugins/newsPlugin/newsPlugin.js
@@ -18,14 +18,14 @@
     "use strict";
     angular.module('hippo.essentials')
         .controller('newsPluginCtrl', function ($scope, $sce, $log, $rootScope, $http) {
-            $scope.endpoint = $rootScope.REST.dynamic + 'newsPlugin/';
             $scope.sampleData = true;
             $scope.templateName = 'jsp';
             $scope.message = {};
             $scope.run = function () {
                 var payload = Essentials.addPayloadData("sampleData", $scope.sampleData, null);
                 Essentials.addPayloadData("templateName", $scope.templateName, payload);
-                $http.post($scope.endpoint, payload).success(function (data) {
+                Essentials.addPayloadData("pluginId", "newsPlugin", payload);
+                $http.post($rootScope.REST.powerpacks_install, payload).success(function (data) {
                     // globally handled
                 });
             };
