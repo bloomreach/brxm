e0bb01ce0b15309a6e3b34dc92d79acc34fccdf9
Unico Hommes <u.hommes@onehippo.com>
2014-05-13 13:28:16 +0000

    REPO-965 cleanup test code; explicitly wait for requests to have been handled before proceeding to check results

--- repository/workflow/src/test/java/org/hippoecm/repository/reviewedactions/AcceptRequestTest.java
+++ repository/workflow/src/test/java/org/hippoecm/repository/reviewedactions/AcceptRequestTest.java
@@ -16,6 +16,7 @@
 package org.hippoecm.repository.reviewedactions;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
@@ -29,6 +30,8 @@ import javax.jcr.Node;
 import javax.jcr.Property;
 import javax.jcr.RepositoryException;
 import javax.jcr.Value;
+import javax.jcr.observation.EventIterator;
+import javax.jcr.observation.EventListener;
 
 import org.hippoecm.repository.api.Document;
 import org.hippoecm.repository.api.HippoNode;
@@ -41,6 +44,7 @@ import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.onehippo.repository.documentworkflow.DocumentWorkflow;
 import org.onehippo.repository.testutils.RepositoryTestCase;
 
 public class AcceptRequestTest extends RepositoryTestCase {
@@ -53,28 +57,23 @@ public class AcceptRequestTest extends RepositoryTestCase {
     @Before
     public void setUp() throws Exception {
         super.setUp();
+        removeNode("/test");
 
-        Node node, root = session.getRootNode();
-        while(root.hasNode("test")) {
-            root.getNode("test").remove();
-            root.save();
-            root.refresh(false);
-        }
-        node = root.addNode("test");
-        node = node.addNode("myarticle", "hippo:handle");
-        node.addMixin("mix:referenceable");
-        node = node.addNode("myarticle", "hippostdpubwf:test");
-        node.addMixin("mix:versionable");
-        node.addMixin("hippostd:publishableSummary");
-        node.setProperty("hippostdpubwf:content", LOREM);
-        node.setProperty("hippostd:holder", "admin");
-        node.setProperty("hippostd:state", "published");
-        node.setProperty("hippo:availability", new String[] { "live", "preview" });
-        node.setProperty("hippostd:stateSummary", "live");
-        node.setProperty("hippostdpubwf:createdBy", "admin");
-        node.setProperty("hippostdpubwf:creationDate", "2010-02-04T16:32:28.068+02:00");
-        node.setProperty("hippostdpubwf:lastModifiedBy", "admin");
-        node.setProperty("hippostdpubwf:lastModificationDate", "2010-02-04T16:32:28.068+02:00");
+        Node test = session.getRootNode().addNode("test");
+        Node handle = test.addNode("myarticle", "hippo:handle");
+        handle.addMixin("mix:referenceable");
+        Node variant = handle.addNode("myarticle", "hippostdpubwf:test");
+        variant.addMixin("mix:versionable");
+        variant.addMixin("hippostd:publishableSummary");
+        variant.setProperty("hippostdpubwf:content", LOREM);
+        variant.setProperty("hippostd:holder", "admin");
+        variant.setProperty("hippostd:state", "published");
+        variant.setProperty("hippo:availability", new String[] { "live", "preview" });
+        variant.setProperty("hippostd:stateSummary", "live");
+        variant.setProperty("hippostdpubwf:createdBy", "admin");
+        variant.setProperty("hippostdpubwf:creationDate", "2010-02-04T16:32:28.068+02:00");
+        variant.setProperty("hippostdpubwf:lastModifiedBy", "admin");
+        variant.setProperty("hippostdpubwf:lastModificationDate", "2010-02-04T16:32:28.068+02:00");
 
         session.save();
     }
@@ -82,147 +81,103 @@ public class AcceptRequestTest extends RepositoryTestCase {
     @Override
     @After
     public void tearDown() throws Exception {
-        Node root = session.getRootNode();
-        while(root.hasNode("test")) {
-            root.getNode("test").remove();
-        }
-        root.save();
+        removeNode("/test");
         super.tearDown();
     }
 
     @Test
-    public void testRequestWhenModified() throws WorkflowException, MappingException, RepositoryException, RemoteException {
-        Node node = null;
+    public void testRequestWhenModified() throws Exception {
         
-        // set proper fixture
-        {
-            node = getNode("test/myarticle/myarticle");
-            node.setProperty("hippostd:state", "published");
-            session.save();
-        }
-        
+        // set up
+        Node variant = getNode("test/myarticle/myarticle");
+        variant.setProperty("hippostd:state", "published");
+        session.save();
+
         // steps taken by an author
-        {
-            node = getNode("test/myarticle/myarticle");
-            BasicReviewedActionsWorkflow workflow = (BasicReviewedActionsWorkflow) getWorkflow(node, "default");
-            assertNotNull("No applicable workflow where there should be one", workflow);
-            Document document = workflow.obtainEditableInstance();
-            session.save();
-            session.refresh(true);
-            node = getNode("test/myarticle/myarticle[@hippostd:state='draft']");
-            assertTrue(node.getIdentifier().equals(document.getIdentity()));
-            Property prop = node.getProperty("hippostdpubwf:content");
-            prop.setValue(prop.getString() + ",");
-            session.save();
-            session.refresh(true);
+        final Node handle = getNode("test/myarticle");
+        DocumentWorkflow workflow = (DocumentWorkflow) getWorkflow(handle, "default");
+        assertNotNull("No document workflow on handle", workflow);
 
-            node = getNode("test/myarticle/myarticle[@hippostd:state='draft']");
-            BasicReviewedActionsWorkflow reviewedWorkflow = (BasicReviewedActionsWorkflow) getWorkflow(node, "default");
-            reviewedWorkflow.commitEditableInstance();
-            session.save();
-            session.refresh(true);
+        Document document = workflow.obtainEditableInstance();
+        Node draft = document.getNode(session);
+        Property prop = draft.getProperty("hippostdpubwf:content");
+        prop.setValue("edited");
+        session.save();
+        workflow.commitEditableInstance();
 
-            reviewedWorkflow = (BasicReviewedActionsWorkflow) getWorkflow(node, "default");
-            assertNotNull("No applicable workflow where there should be one", reviewedWorkflow);
-            reviewedWorkflow.requestPublication();
-            session.save();
-            session.refresh(true);
+        workflow.requestPublication();
+
+        poll(new Executable() {
+            @Override
+            public void execute() throws Exception {
+                assertTrue("Publication request not found", handle.hasNode("hippo:request"));
+            }
+        }, 10);
+
+        Node request = handle.getNode("hippo:request");
+        workflow.acceptRequest(request.getIdentifier());
+
+        poll(new Executable() {
+            @Override
+            public void execute() throws Exception {
+                assertFalse("Scheduled publication not executed", handle.hasNode("hippo:request"));
+            }
+        }, 10);
+
+        Node published = getNode("test/myarticle/myarticle[@hippostd:state='published']");
+        if (published != null) {
+            final Value[] values = published.getProperty("hippo:availability").getValues();
+            assertEquals(1, values.length);
+            assertEquals("edited", published.getProperty("hippostdpubwf:content").getString());
+        } else {
+            Assert.fail("Published document not found");
         }
 
-        // These steps would be taken by editor:
-        {
-            node = getNode("test/myarticle/hippo:request[@hippostdpubwf:type='publish']");
-            FullRequestWorkflow workflow = (FullRequestWorkflow) getWorkflow(node, "default");
-            workflow.acceptRequest();
-            session.save();
-            session.refresh(true);
-        }
     }
 
     @Test
-    public void testScheduledPublication() throws WorkflowException, MappingException, RepositoryException, RemoteException, InterruptedException {
-        Node node;
-        int secondsToWait;
-        boolean found;
+    public void testScheduledPublication() throws Exception {
 
-        // set proper fixture
-        {
-            node = getNode("test/myarticle/myarticle");
-            node.setProperty("hippostd:state", "unpublished");
-            session.save();
-            node = getNode("test/myarticle/myarticle[@hippostd:state='unpublished']");
-            assertNotNull(node);
-            assertEquals("unpublished", node.getProperty("hippostd:state").getString());
-            node = getNode("test/myarticle/myarticle[@hippostd:state='published']");
-            assertNull(node);
-        }
+        // Set up
+        Node variant = getNode("test/myarticle/myarticle");
+        variant.setProperty("hippostd:state", "unpublished");
+        session.save();
 
-        // steps taken by an author
-        {
-            node = getNode("test/myarticle/myarticle[@hippostd:state='unpublished']");
-            BasicReviewedActionsWorkflow reviewedWorkflow = (BasicReviewedActionsWorkflow) getWorkflow(node, "default");
-            assertNotNull("No applicable workflow where there should be one", reviewedWorkflow);
+        // Request publication
+        final Node handle = getNode("test/myarticle");
+        DocumentWorkflow workflow = (DocumentWorkflow) getWorkflow(handle, "default");
+        assertNotNull("No applicable workflow where there should be one", workflow);
 
-            reviewedWorkflow.requestPublication(new Date(System.currentTimeMillis()+100));
-            session.save();
-        }
+        workflow.requestPublication(new Date(System.currentTimeMillis() + 1000));
+        session.save();
 
-        // These steps would be taken by editor:
-        {
-            secondsToWait = 5;
-            found = false;
-            do {
-                session.refresh(false);
-                node = getNode("test/myarticle/hippo:request");
-                if (node != null) {
-                    FullRequestWorkflow workflow = (FullRequestWorkflow) getWorkflow(node, "default");
-                    Map<String, Serializable> hints = workflow.hints();
-                    if (hints.containsKey("acceptRequest") && ((Boolean)hints.get("acceptRequest")).booleanValue()) {
-                        workflow.acceptRequest();
-                        session.save();
-                        found = true;
-                    }
-                    else {
-                        Assert.fail("Publication request not acceptable");
-                    }
-                }
-                else {
-                    secondsToWait--;
-                    if (secondsToWait > 0) {
-                        Thread.sleep(1000);
-                    }
-                }
-
-            } while (!found && secondsToWait > 0);
-
-            if (!found) {
-                Assert.fail("No acceptable publication request found");
+        poll(new Executable() {
+            @Override
+            public void execute() throws Exception {
+                assertTrue("Publication request not found", handle.hasNode("hippo:request"));
             }
-        }
+        }, 10);
 
-        secondsToWait = 5;
-        found = false;
+        // Accept request for publication
+        Node request = handle.getNode("hippo:request");
+        workflow.acceptRequest(request.getIdentifier());
 
-        do {
-            session.refresh(false);
-            node = getNode("test/myarticle/myarticle[@hippostd:state='published']");
-            if (node != null) {
-                assertEquals("published", node.getProperty("hippostd:state").getString());
-                final Value[] values = node.getProperty("hippo:availability").getValues();
-                assertEquals(1, values.length);
-                found = true;
+        poll(new Executable() {
+            @Override
+            public void execute() throws Exception {
+                assertFalse("Publication not executed", handle.hasNode("hippo:request"));
             }
-            else {
-                secondsToWait--;
-                if (secondsToWait > 0) {
-                    Thread.sleep(1000);
-                }
-            }
-        } while (!found && secondsToWait > 0);
+        }, 10);
 
-        if (!found) {
+        Node published = getNode("test/myarticle/myarticle[@hippostd:state='published']");
+        if (published != null) {
+            assertEquals("published", published.getProperty("hippostd:state").getString());
+            final Value[] values = published.getProperty("hippo:availability").getValues();
+            assertEquals(1, values.length);
+        } else {
             Assert.fail("Published document not found");
         }
+
     }
  
     protected Workflow getWorkflow(Node node, String category) throws RepositoryException {
@@ -244,5 +199,23 @@ public class AcceptRequestTest extends RepositoryTestCase {
 
     protected Node getNode(String path) throws RepositoryException {
         return ((HippoWorkspace)session.getWorkspace()).getHierarchyResolver().getNode(session.getRootNode(), path);
+    }
+
+    private void poll(Executable executable, int seconds) throws Exception {
+        while (true) {
+            try {
+                executable.execute();
+                return;
+            } catch (AssertionError e) {
+                if (seconds-- <= 0) {
+                    throw e;
+                }
+                Thread.sleep(1000);
+            }
+        }
+    }
+
+    private static interface Executable {
+        void execute() throws Exception;
     }
 }
