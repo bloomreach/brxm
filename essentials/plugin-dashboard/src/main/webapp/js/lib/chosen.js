c1b65a40affd083877d0a267cecdb8a1d91a3839
Marijan Milicevic <m.milicevic@onehippo.com>
2014-06-20 15:42:08 +0000

    - fix chosen empty rendering

--- essentials/plugin-dashboard/src/main/webapp/js/lib/chosen.js
+++ essentials/plugin-dashboard/src/main/webapp/js/lib/chosen.js
@@ -21,7 +21,8 @@
   angular.module('localytics.directives', []);
 
   angular.module('localytics.directives').directive('chosen', function() {
-    var CHOSEN_OPTION_WHITELIST, NG_OPTIONS_REGEXP, chosen, isEmpty, snakeCase;
+    var CHOSEN_OPTION_WHITELIST, NG_OPTIONS_REGEXP, isEmpty, snakeCase;
+
     NG_OPTIONS_REGEXP = /^\s*(.*?)(?:\s+as\s+(.*?))?(?:\s+group\s+by\s+(.*))?\s+for\s+(?:([\$\w][\$\w]*)|(?:\(\s*([\$\w][\$\w]*)\s*,\s*([\$\w][\$\w]*)\s*\)))\s+in\s+(.*?)(?:\s+track\s+by\s+(.*?))?$/;
     CHOSEN_OPTION_WHITELIST = ['noResultsText', 'allowSingleDeselect', 'disableSearchThreshold', 'disableSearch', 'enableSplitWordSearch', 'inheritSelectClasses', 'maxSelectedOptions', 'placeholderTextMultiple', 'placeholderTextSingle', 'searchContains', 'singleBackstrokeDelete', 'displayDisabledOptions', 'displaySelectedOptions', 'width'];
     snakeCase = function(input) {
@@ -31,6 +32,7 @@
     };
     isEmpty = function(value) {
       var key;
+
       if (angular.isArray(value)) {
         return value.length === 0;
       } else if (angular.isObject(value)) {
@@ -42,12 +44,13 @@
       }
       return true;
     };
-    return chosen = {
+    return {
       restrict: 'A',
       require: '?ngModel',
       terminal: true,
-      link: function(scope, element, attr, ctrl) {
-        var disableWithMessage, empty, initOrUpdate, initialized, match, options, origRender, removeEmptyMessage, startLoading, stopLoading, valuesExpr, viewWatch;
+      link: function(scope, element, attr, ngModel) {
+        var chosen, defaultText, disableWithMessage, empty, initOrUpdate, match, options, origRender, removeEmptyMessage, startLoading, stopLoading, valuesExpr, viewWatch;
+
         element.addClass('localytics-chosen');
         options = scope.$eval(attr.chosen) || {};
         angular.forEach(attr, function(value, key) {
@@ -61,56 +64,56 @@
         stopLoading = function() {
           return element.removeClass('loading').attr('disabled', false).trigger('chosen:updated');
         };
-        initialized = false;
+        chosen = null;
+        defaultText = null;
         empty = false;
         initOrUpdate = function() {
-          if (initialized) {
+          if (chosen) {
             return element.trigger('chosen:updated');
           } else {
-            element.chosen(options);
-            return initialized = true;
+            chosen = element.chosen(options).data('chosen');
+            return defaultText = chosen.default_text;
           }
         };
         removeEmptyMessage = function() {
           empty = false;
-          return element.find('option.empty').remove();
+          return element.attr('data-placeholder', defaultText);
         };
-        disableWithMessage = function(message) {
+        disableWithMessage = function() {
           empty = true;
-          return element.empty().append("<option selected class=\"empty\">" + message + "</option>").attr('disabled', true).trigger('chosen:updated');
+          return element.attr('data-placeholder', chosen.results_none_found).attr('disabled', true).trigger('chosen:updated');
         };
-        if (ctrl) {
-          origRender = ctrl.$render;
-          ctrl.$render = function() {
+        if (ngModel) {
+          origRender = ngModel.$render;
+          ngModel.$render = function() {
             origRender();
             return initOrUpdate();
           };
           if (attr.multiple) {
             viewWatch = function() {
-              return ctrl.$viewValue;
+              return ngModel.$viewValue;
             };
-            scope.$watch(viewWatch, ctrl.$render, true);
+            scope.$watch(viewWatch, ngModel.$render, true);
           }
         } else {
           initOrUpdate();
         }
-        attr.$observe('disabled', function(value) {
+        attr.$observe('disabled', function() {
           return element.trigger('chosen:updated');
         });
-        if (attr.ngOptions) {
+        if (attr.ngOptions && ngModel) {
           match = attr.ngOptions.match(NG_OPTIONS_REGEXP);
           valuesExpr = match[7];
-          if (angular.isUndefined(scope.$eval(valuesExpr))) {
-            startLoading();
-          }
           return scope.$watchCollection(valuesExpr, function(newVal, oldVal) {
-            if (newVal !== oldVal) {
+            if (angular.isUndefined(newVal)) {
+              return startLoading();
+            } else {
               if (empty) {
                 removeEmptyMessage();
               }
               stopLoading();
               if (isEmpty(newVal)) {
-                return disableWithMessage(options.no_results_text || 'No values available');
+                return disableWithMessage();
               }
             }
           });
