fc31f2e2a8eb16b1f4c1e8626ec1ebffa5b7d57d
Marijan Milicevic <m.milicevic@onehippo.com>
2014-06-23 11:45:08 +0000

    - add repository support & testcases

---
+++ essentials-components/plugin-api/implementation/src/main/java/org/onehippo/cms7/essentials/dashboard/model/SnapshotRestful.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2014 Hippo B.V. (http://www.onehippo.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.onehippo.cms7.essentials.dashboard.model;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * @version "$Id$"
+ */
+@XmlRootElement(name = "snapshots")
+public class SnapshotRestful implements Snapshot, Restful {
+
+    private static final long serialVersionUID = 1L;
+    private Boolean enabled;
+
+    @Override
+    public Boolean getEnabled() {
+        return enabled;
+    }
+
+    @Override
+    public void setEnabled(final Boolean enabled) {
+        this.enabled = enabled;
+    }
+}
