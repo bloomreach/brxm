799c386094b84e0ef899eeb7a225d54fdd1761a7
Ard Schrijvers <a.schrijvers@onehippo.com>
2014-07-14 08:21:36 +0000

    HSTTWO-1003 add class I forgot to check in

---
+++ testsuite/intranet/src/main/java/org/hippoecm/hst/intranet/components/subsite/SubsiteHeader.java
@@ -0,0 +1,43 @@
+/*
+ *  Copyright 2014 Hippo B.V. (http://www.onehippo.com)
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.hippoecm.hst.intranet.components.subsite;
+
+import org.hippoecm.hst.component.support.bean.BaseHstComponent;
+import org.hippoecm.hst.configuration.hosting.Mount;
+import org.hippoecm.hst.core.component.HstComponentException;
+import org.hippoecm.hst.core.component.HstRequest;
+import org.hippoecm.hst.core.component.HstResponse;
+import org.hippoecm.hst.intranet.channels.SubsiteInfo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SubsiteHeader extends BaseHstComponent {
+
+    public static final Logger log = LoggerFactory.getLogger(SubsiteHeader.class);
+
+    @Override
+    public void doBeforeRender(final HstRequest request, final HstResponse response) throws HstComponentException {
+        final Mount mount = request.getRequestContext().getResolvedMount().getMount();
+        final SubsiteInfo info = mount.getChannelInfo();
+
+        if (info != null) {
+            request.setAttribute("headerName", info.getHeaderName());
+        } else {
+            log.info("No channel info available for subsite '{}'", mount.getMountPath());
+        }
+    }
+
+}
