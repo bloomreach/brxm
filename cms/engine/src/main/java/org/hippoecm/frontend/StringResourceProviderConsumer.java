af72272717a310004ab114b2c47f722082d3c469
Ard Schrijvers <a.schrijvers@onehippo.com>
2014-07-16 15:40:18 +0000

    CMS7-8119 add a very simple cache for already attempted finding of resources (note namely that a lot of the resources are tried to be found through searches which pretty much always return the same result).

--- cms/engine/src/main/java/org/hippoecm/frontend/StringResourceProviderConsumer.java
+++ cms/engine/src/main/java/org/hippoecm/frontend/StringResourceProviderConsumer.java
@@ -18,12 +18,19 @@ package org.hippoecm.frontend;
 import java.util.Locale;
 import java.util.Map;
 import java.util.TreeMap;
+import java.util.concurrent.TimeUnit;
+
+import com.google.common.base.Optional;
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
 
 import org.apache.wicket.Component;
 import org.apache.wicket.resource.loader.IStringResourceLoader;
 import org.apache.wicket.util.value.IValueMap;
 import org.apache.wicket.util.value.ValueMap;
 import org.hippoecm.repository.api.HippoNodeType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * An IStringResourceLoader that uses the first IStringResourceProvider parent of
@@ -35,10 +42,15 @@ import org.hippoecm.repository.api.HippoNodeType;
  */
 public class StringResourceProviderConsumer implements IStringResourceLoader {
 
+    private static final Logger log = LoggerFactory.getLogger(StringResourceProviderConsumer.class);
+
+    private Cache<CacheKey, Optional<String>> stringResourceCache =
+            CacheBuilder.newBuilder().maximumSize(10000).expireAfterWrite(5, TimeUnit.MINUTES).build();
 
     @Override
     public String loadStringResource(final Component component, final String key, final Locale locale, final String style, final String variation) {
         IStringResourceProvider provider;
+        long start = System.nanoTime();
         if (component instanceof IStringResourceProvider) {
             provider = (IStringResourceProvider) component;
         } else if (component != null) {
@@ -47,7 +59,15 @@ public class StringResourceProviderConsumer implements IStringResourceLoader {
             return null;
         }
         if (provider != null) {
-            Map<String, String> keys = new TreeMap<String, String>();
+
+            CacheKey cacheKey =  new CacheKey(provider.getClass().getName(), key, locale);
+            final Optional<String> optional = stringResourceCache.getIfPresent(cacheKey);
+            if (optional != null) {
+                final String s = optional.orNull();
+                log.info("Found cached value '{}' for key '{}'",s, cacheKey);
+                return s;
+            }
+            Map<String, String> keys = new TreeMap<>();
             String realKey;
             if (key.indexOf(',') > 0) {
                 realKey = key.substring(0, key.indexOf(','));
@@ -79,9 +99,19 @@ public class StringResourceProviderConsumer implements IStringResourceLoader {
             }
 
             String result = provider.getString(keys);
+
             if (result != null) {
-                return result;
+                if (log.isInfoEnabled()) {
+                    log.info("Took {} milliseconds to find resource '{}' for key '{}'.", ((System.nanoTime() - start)/1000000.0), result, cacheKey);
+                }
+            } else {
+                if (log.isInfoEnabled()) {
+                    log.info("Took {} milliseconds to not find resource for key '{}'.", ((System.nanoTime() - start)/1000000.0), cacheKey);
+                }
             }
+            // regardless null or not, cache result
+            stringResourceCache.put(cacheKey, Optional.fromNullable(result));
+            return result;
         }
         return null;
     }
@@ -89,5 +119,59 @@ public class StringResourceProviderConsumer implements IStringResourceLoader {
     @Override
     public String loadStringResource(final Class<?> clazz, final String key, final Locale locale, final String style, final String variation) {
         return null;
+    }
+
+    private class CacheKey {
+
+        private final String className;
+        private final String key;
+        private final Locale locale;
+
+        public CacheKey(final String className, final String key, final Locale locale) {
+            this.className = className;
+            this.key = key;
+            this.locale = locale;
+        }
+
+        @Override
+        public boolean equals(final Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+
+            final CacheKey cacheKey = (CacheKey) o;
+
+            if (!key.equals(cacheKey.key)) {
+                return false;
+            }
+            if (!locale.equals(cacheKey.locale)) {
+                return false;
+            }
+            if (!className.equals(cacheKey.className)) {
+                return false;
+            }
+
+            return true;
+        }
+
+        @Override
+        public int hashCode() {
+            int result = className.hashCode();
+            result = 31 * result + key.hashCode();
+            result = 31 * result + locale.hashCode();
+            return result;
+        }
+
+        @Override
+        public String toString() {
+            return "CacheKey{" +
+                    "className='" + className + '\'' +
+                    ", key='" + key + '\'' +
+                    ", locale=" + locale +
+                    '}';
+        }
     }
 }
