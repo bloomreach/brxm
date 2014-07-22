211dbc713e91a366ae8825b3e2ca865432fa3ef6
Mathijs den Burger <m.denburger@onehippo.com>
2014-07-22 08:52:05 +0000

    CMS7-8126: refactored null check, no functional changes
    Also bumped copyright year

--- cms/api/src/main/java/org/hippoecm/frontend/i18n/model/NodeTranslator.java
+++ cms/api/src/main/java/org/hippoecm/frontend/i18n/model/NodeTranslator.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2008-2013 Hippo B.V. (http://www.onehippo.com)
+ *  Copyright 2008-2014 Hippo B.V. (http://www.onehippo.com)
  * 
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -109,9 +109,12 @@ public class NodeTranslator extends NodeModelWrapper<NodeTranslator> {
             if (obContext != null) {
                 stopObservation();
             }
+
             accessed = new HashSet<JcrNodeModel>();
-            if (nodeModel.getParentModel() != null) {
-                accessed.add(nodeModel.getParentModel());
+
+            final JcrNodeModel parentModel = nodeModel.getParentModel();
+            if (parentModel != null) {
+                accessed.add(parentModel);
             }
 
             try {
