cc286462a81502dabd5d0367c386977c4e7a6b81
Woonsan Ko <w.ko@onehippo.com>
2014-07-31 18:35:15 +0000

    CMS7-7802: merging unit tests and fixing error due to missing Session in ThreadContext

--- cms/api/src/test/java/org/hippoecm/frontend/plugins/standards/util/ByteSizeFormatterTest.java
+++ cms/api/src/test/java/org/hippoecm/frontend/plugins/standards/util/ByteSizeFormatterTest.java
@@ -22,12 +22,90 @@ import java.util.Locale;
 import org.apache.wicket.Session;
 import org.apache.wicket.ThreadContext;
 import org.easymock.classextension.EasyMock;
+import org.junit.Before;
 import org.junit.Test;
 
 /**
  * ByteSizeFormatterTest
  */
 public class ByteSizeFormatterTest {
+
+    @Before
+    public void before() throws Exception {
+        Session session = EasyMock.createNiceMock(Session.class);
+        EasyMock.expect(session.getLocale()).andReturn(Locale.FRANCE).anyTimes();
+        EasyMock.replay(session);
+        ThreadContext.setSession(session);
+    }
+
+    @Test
+    public void testZeroDecimalPlaces() throws Exception {
+        ByteSizeFormatter format = new ByteSizeFormatter(0);
+
+        assertEquals("512 bytes", format.format(512L));
+
+        assertEquals("1 KB", format.format(1024L));
+        assertEquals("1 KB", format.format(1034L));
+        assertEquals("4 KB", format.format(4096L));
+        assertEquals("4 KB", format.format(3584L));
+        assertEquals("2 KB", format.format(1584L));
+
+        assertEquals("1 MB", format.format(1048576L));
+        assertEquals("4 MB", format.format(4194304L));
+        assertEquals("4 MB", format.format(3670016L));
+        assertEquals("2 MB", format.format(1670016L));
+
+        assertEquals("1 GB", format.format(1073741824L));
+        assertEquals("4 GB", format.format(4294967296L));
+        assertEquals("4 GB", format.format(3758096384L));
+        assertEquals("3 GB", format.format(3558096384L));
+    }
+
+    @Test
+    public void testOneDecimalPlaces() throws Exception {
+        ByteSizeFormatter format = new ByteSizeFormatter(1);
+        
+        assertEquals("512 bytes", format.format(512L));
+
+        assertEquals("1 KB", format.format(1024L));
+        assertEquals("1 KB", format.format(1034L));
+        assertEquals("4 KB", format.format(4096L));
+        assertEquals("3,5 KB", format.format(3584L));
+        assertEquals("1,5 KB", format.format(1584L));
+
+        assertEquals("1 MB", format.format(1048576L));
+        assertEquals("4 MB", format.format(4194304L));
+        assertEquals("3,5 MB", format.format(3670016L));
+        assertEquals("1,6 MB", format.format(1670016L));
+
+        assertEquals("1 GB", format.format(1073741824L));
+        assertEquals("4 GB", format.format(4294967296L));
+        assertEquals("3,5 GB", format.format(3758096384L));
+        assertEquals("3,3 GB", format.format(3558096384L));
+    }
+
+    @Test
+    public void testTwoDecimalPlaces() throws Exception {
+        ByteSizeFormatter format = new ByteSizeFormatter(2);
+        
+        assertEquals("512 bytes", format.format(512L));
+
+        assertEquals("1 KB", format.format(1024L));
+        assertEquals("1,01 KB", format.format(1034L));
+        assertEquals("4 KB", format.format(4096L));
+        assertEquals("3,5 KB", format.format(3584L));
+        assertEquals("1,55 KB", format.format(1584L));
+
+        assertEquals("1 MB", format.format(1048576L));
+        assertEquals("4 MB", format.format(4194304L));
+        assertEquals("3,5 MB", format.format(3670016L));
+        assertEquals("1,59 MB", format.format(1670016L));
+
+        assertEquals("1 GB", format.format(1073741824L));
+        assertEquals("4 GB", format.format(4294967296L));
+        assertEquals("3,5 GB", format.format(3758096384L));
+        assertEquals("3,31 GB", format.format(3558096384L));
+    }
 
     @Test
     public void testSizeFormattingInUSALocale() throws Exception {
