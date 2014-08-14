a5bc48f6744300a475a167f4db2258c468149a63
Woonsan Ko <w.ko@onehippo.com>
2014-08-14 14:46:48 +0000

    HSTTWO-3042: set log level to info in EditableMenuItemImpl

--- site-toolkit/commons/src/main/java/org/hippoecm/hst/core/sitemenu/EditableMenuItemImpl.java
+++ site-toolkit/commons/src/main/java/org/hippoecm/hst/core/sitemenu/EditableMenuItemImpl.java
@@ -38,7 +38,7 @@ public class EditableMenuItemImpl extends AbstractMenuItem implements EditableMe
     
     public EditableMenuItemImpl(EditableMenuItem parentItem){
         if(parentItem == null) {
-            log.error("Cannot add a Editable menu item when parent item is null");
+            log.info("Can cause an invalid Editable menu item because parent item is null.");
             return;
         }
         this.parentItem = parentItem;
