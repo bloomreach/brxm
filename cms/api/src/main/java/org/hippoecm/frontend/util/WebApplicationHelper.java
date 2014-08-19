44f1368cd287a7b3698c89bf5adc77e5a602afe7
Unico Hommes <u.hommes@onehippo.com>
2014-08-19 13:15:33 +0000

    CMS7-8052 anti-cache key based on last modified dates of jar files

--- cms/api/src/main/java/org/hippoecm/frontend/util/WebApplicationHelper.java
+++ cms/api/src/main/java/org/hippoecm/frontend/util/WebApplicationHelper.java
@@ -15,7 +15,22 @@
  */
 package org.hippoecm.frontend.util;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Enumeration;
+import java.util.LinkedList;
+import java.util.List;
+
 import javax.servlet.http.Cookie;
+import javax.xml.bind.DatatypeConverter;
 
 import org.apache.wicket.Component;
 import org.apache.wicket.Page;
@@ -27,12 +42,14 @@ import org.apache.wicket.request.http.WebResponse;
 import org.apache.wicket.request.resource.UrlResourceReference;
 import org.hippoecm.frontend.PluginApplication;
 
+import static org.slf4j.LoggerFactory.getLogger;
+
 
 public class WebApplicationHelper {
 
     public static final String HIPPO_AUTO_LOGIN_COOKIE_BASE_NAME = "hal";
     public static final String REMEMBERME_COOKIE_BASE_NAME = "rememberme";
-    private static final String ANTI_CACHE_KEY = Long.toString(System.currentTimeMillis());
+    public static final String APPLICATION_HASH = calculateApplicationHash();
 
     private WebApplicationHelper() {
     }
@@ -72,7 +89,7 @@ public class WebApplicationHelper {
     }
 
     public static UrlResourceReference createUniqueUrlResourceReference(Url url) {
-        url.addQueryParameter("antiCache", ANTI_CACHE_KEY);
+        url.addQueryParameter("antiCache", APPLICATION_HASH);
         return new UrlResourceReference(url);
     }
 
@@ -80,6 +97,74 @@ public class WebApplicationHelper {
         if (value == null || "".equals(value)) {
             throw new IllegalArgumentException("Null or '' are not allowed values!");
         }
+    }
+
+    /**
+     * Unique hash based on the last modified dates of all the jars on the classpath
+     */
+    private static String calculateApplicationHash() {
+        try {
+            final MessageDigest md5 = MessageDigest.getInstance("MD5");
+            final List<File> jarFiles = getAllJarFiles();
+            for (File jarFile : jarFiles) {
+                md5.update(String.valueOf(jarFile.lastModified()).getBytes());
+            }
+            String s = DatatypeConverter.printBase64Binary(md5.digest());
+            // make url friendly
+            return s.replace('+', '.').replace('/', '-').replace('=', '_');
+        } catch (NoSuchAlgorithmException | IOException e) {
+            final String msg = "Failed to calculate application hash";
+            getLogger(WebApplicationHelper.class).error(msg, e);
+        }
+        return String.valueOf(System.currentTimeMillis());
+    }
+
+    private static List<File> getAllJarFiles() throws IOException {
+        List<File> jarFiles = new ArrayList<>();
+        Enumeration<URL> manifests = Thread.currentThread().getContextClassLoader().getResources("META-INF/MANIFEST.MF");
+        while (manifests.hasMoreElements()) {
+            final URL manifest = manifests.nextElement();
+            try {
+                final File baseJarFile = getBaseJarFileFromURL(manifest);
+                if (baseJarFile != null) {
+                    jarFiles.add(baseJarFile);
+                } else {
+                    final String msg = String.format("Manifest %s does not seem to be inside a jar", manifest.getFile());
+                    getLogger(WebApplicationHelper.class).debug(msg);
+                }
+            } catch (URISyntaxException e) {
+                final String msg = String.format("Failed to get jar file from manifest %s", manifest.getFile());
+                getLogger(WebApplicationHelper.class).error(msg, e);
+            }
+        }
+        Collections.sort(jarFiles, new Comparator<File>() {
+            @Override
+            public int compare(final File o1, final File o2) {
+                return o1.getName().compareTo(o2.getName());
+            }
+        });
+        return jarFiles;
+    }
+
+    private static File getBaseJarFileFromURL(final URL url) throws URISyntaxException {
+        String file = url.getFile();
+        int offset = file.indexOf(".jar!");
+
+        if (offset == -1) {
+            return null;
+        }
+
+        file = file.substring(0, offset + 4);
+
+        if (!file.startsWith("file:")) {
+            if (file.startsWith("/")) {
+                file = "file://" + file;
+            } else {
+                file = "file:///" + file;
+            }
+        }
+
+        return new File(URI.create(file));
     }
 
 }
