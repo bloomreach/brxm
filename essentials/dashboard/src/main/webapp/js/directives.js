d976a5edad846af841b9ffa30cf32ce05c96c0bd
Marijan Milicevic <m.milicevic@onehippo.com>
2014-09-03 08:53:06 +0000

    ESSENTIALS-419 Toggle in top bar has strange effect

--- essentials/dashboard/src/main/webapp/js/directives.js
+++ essentials/dashboard/src/main/webapp/js/directives.js
@@ -262,6 +262,9 @@
                                 value.fullDate = date;
                             }
                         });
+                        if ($scope.archiveMessages.length == 0) {
+                            $scope.archiveMessages.push({type: "info", message: 'No archived messages', visible: true, date: now, fullDate: date})
+                        }
                         // newer messages first:
                         $scope.archiveMessages.reverse();
                         if ($scope.activeMessages.length > 1) {
