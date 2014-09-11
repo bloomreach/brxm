d01dc7f4a0ebc3be35b6f682ed75fe75c0d24ff9
Unico Hommes <u.hommes@onehippo.com>
2014-09-11 11:06:24 +0000

    REPO-857 Run each repository in an isolated classloader to avoid sharing of static caches. Only access to the JCR API is shared with the test class. Repository internals must be accessed using reflection.

--- repository/test/src/test/java/org/onehippo/repository/clustering/ClusterTest.java
+++ repository/test/src/test/java/org/onehippo/repository/clustering/ClusterTest.java
@@ -18,25 +18,22 @@ package org.onehippo.repository.clustering;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.net.URL;
 import java.net.URLClassLoader;
+import java.util.List;
+import java.util.Set;
 
 import javax.jcr.Credentials;
+import javax.jcr.Repository;
 import javax.jcr.RepositoryException;
 import javax.jcr.Session;
 import javax.jcr.SimpleCredentials;
 
 import org.apache.commons.io.FileUtils;
-import org.apache.jackrabbit.core.persistence.bundle.AbstractBundlePersistenceManager;
-import org.apache.jackrabbit.core.persistence.bundle.ConsistencyCheckerImpl;
-import org.apache.jackrabbit.core.persistence.check.ConsistencyReport;
-import org.apache.jackrabbit.core.persistence.check.ReportItem;
-import org.apache.jackrabbit.core.query.lucene.ConsistencyCheck;
-import org.apache.jackrabbit.core.query.lucene.ConsistencyCheckError;
-import org.apache.jackrabbit.core.query.lucene.SearchIndex;
 import org.h2.tools.Server;
-import org.hippoecm.repository.HippoRepository;
-import org.hippoecm.repository.decorating.RepositoryDecorator;
-import org.hippoecm.repository.jackrabbit.RepositoryImpl;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
@@ -48,6 +45,9 @@ import static org.junit.Assume.assumeTrue;
 /**
  * Starts up two repositories backed by the same database
  * allowing subclasses to perform cluster concurrency tests.
+ * The Repositories are run in their own isolated classloaders
+ * to prevent sharing of static caches. Only JCR API classes
+ * are shared with the test classes.
  * <p>
  *     By default starts an h2 database server as a backend for
  *     the repositories. MySQL can also be used by running with the option
@@ -76,8 +76,8 @@ public abstract class ClusterTest {
     private static String repo1Path;
     private static String repo2Path;
 
-    protected static HippoRepository repo1;
-    protected static HippoRepository repo2;
+    protected static Object repo1;
+    protected static Object repo2;
 
     private static String h2Path;
     private static Server server;
@@ -121,15 +121,23 @@ public abstract class ClusterTest {
     @AfterClass
     public static void shutDownClass() throws Exception {
         if (repo1 != null) {
-            repo1.close();
+            closeRepository(repo1);
         }
         if (repo2 != null) {
-            repo2.close();
+            closeRepository(repo2);
         }
         if (server != null) {
             server.stop();
         }
         cleanup();
+    }
+
+    private static void closeRepository(final Object repo) {
+        try {
+            repo.getClass().getMethod("close").invoke(repo);
+        } catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+            log.error("Failed to close repository: " + e);
+        }
     }
 
     private static void cleanup() throws IOException {
@@ -151,8 +159,8 @@ public abstract class ClusterTest {
 
     @Before
     public void setUp() throws Exception {
-        session1 = repo1.login(CREDENTIALS);
-        session2 = repo2.login(CREDENTIALS);
+        session1 = loginSession(repo1);
+        session2 = loginSession(repo2);
 
         session1.getRootNode().addNode("test");
         session1.save();
@@ -177,14 +185,18 @@ public abstract class ClusterTest {
         session1.save();
     }
 
-    private static HippoRepository createRepository(final String repoPath, final String repoConfig) throws Exception {
+    /**
+     * Create a LocalHippoRepository running in its own isolated classloader. Because of this classloader
+     * isolation all access to the repository internals must be done using reflection.
+     */
+    private static Object createRepository(final String repoPath, final String repoConfig) throws Exception {
         URLClassLoader contextClassLoader = (URLClassLoader) Thread.currentThread().getContextClassLoader();
-        URLClassLoader classLoader = new URLClassLoader(contextClassLoader.getURLs());
+        URLClassLoader classLoader = new RepositoryClassLoader(contextClassLoader.getURLs(), contextClassLoader);
         Thread.currentThread().setContextClassLoader(classLoader);
         try {
-            return (HippoRepository) Class.forName("org.hippoecm.repository.LocalHippoRepository", true, classLoader).
-                    getMethod("create", String.class, String.class).
-                    invoke(null, repoPath, repoConfig);
+            return Class.forName("org.hippoecm.repository.LocalHippoRepository", true, classLoader).
+                getMethod("create", String.class, String.class).
+                invoke(null, repoPath, repoConfig);
         } finally {
             Thread.currentThread().setContextClassLoader(contextClassLoader);
         }
@@ -196,40 +208,67 @@ public abstract class ClusterTest {
         return getTestSystemViewXML(session1).equals(getTestSystemViewXML(session2));
     }
 
-    protected boolean checkIndexConsistency(HippoRepository repo) throws RepositoryException, IOException {
-        final ConsistencyCheck consistencyCheck = getSearchIndex(repo).runConsistencyCheck();
-        if (consistencyCheck.getErrors().size() > 0) {
-            for (ConsistencyCheckError consistencyCheckError : consistencyCheck.getErrors()) {
-                log.error("Index inconsistency: " + consistencyCheckError);
-            }
-            return false;
+    /**
+     * Check the index consistency of a repository. All access to the repository must be invoked using
+     * reflection because it runs in an isolated classloader.
+     */
+    protected boolean checkIndexConsistency(Object repo) throws RepositoryException, IOException {
+        final Object searchIndex = getSearchIndex(repo);
+        final List errors;
+        try {
+            final Object consistencyCheck = searchIndex.getClass().getMethod("runConsistencyCheck").invoke(searchIndex);
+            errors = (List) consistencyCheck.getClass().getMethod("getErrors").invoke(consistencyCheck);
+            return errors.isEmpty();
+        } catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+            log.error("Failed to run index consistency check: " + e);
         }
-        return true;
+        return false;
     }
 
-    protected boolean checkDatabaseConsistency(HippoRepository repo, Session session) throws RepositoryException {
-        ConsistencyCheckerImpl checker = new ConsistencyCheckerImpl(getPersistenceManager(repo), null, null, null);
-        checker.check(new String[] { session.getNode("/test").getIdentifier() }, true);
-        // todo: check() with a determinate list of ids seems to be broken, we need to run a double check because of that
-        checker.doubleCheckErrors();
-        final ConsistencyReport report = checker.getReport();
-        if (!report.getItems().isEmpty()) {
-            for (ReportItem reportItem : report.getItems()) {
-                log.error("Database inconsistency: " + reportItem);
-            }
-            return false;
+    /**
+     * Check the database consistency of a repository. All access to the repository must be invoked using
+     * reflection because it runs in an isolated classloader.
+     */
+    protected boolean checkDatabaseConsistency(Object repo, Session session) throws RepositoryException {
+        Object persistenceManager = getPersistenceManager(repo);
+        ClassLoader classLoader = persistenceManager.getClass().getClassLoader();
+        try {
+            final Class<?> checkerClass = Class.forName("org.apache.jackrabbit.core.persistence.bundle.ConsistencyCheckerImpl", true, classLoader);
+            final Class<?> pmClass = Class.forName("org.apache.jackrabbit.core.persistence.bundle.AbstractBundlePersistenceManager", true, classLoader);
+            final Class<?> listenerClass = Class.forName("org.apache.jackrabbit.core.persistence.check.ConsistencyCheckListener", true, classLoader);
+            final Class<?> channelClass = Class.forName("org.apache.jackrabbit.core.cluster.UpdateEventChannel", true, classLoader);
+            final Constructor<?> checkerConstructor = checkerClass.getConstructor(pmClass, listenerClass, String.class, channelClass);
+            final Object checker = checkerConstructor.newInstance(persistenceManager, null, null, null);
+            checkerClass.getMethod("check", String[].class, boolean.class).invoke(checker, new String[] { session.getNode("/test").getIdentifier() }, true);
+            // todo: check() with a determinate list of ids seems to be broken, we need to run a double check because of that
+            checkerClass.getMethod("doubleCheckErrors").invoke(checker);
+            final Object report = checkerClass.getMethod("getReport").invoke(checker);
+            Set errors = (Set) report.getClass().getMethod("getItems").invoke(report);
+            return errors.isEmpty();
+        } catch (ClassNotFoundException | NoSuchMethodException | InstantiationException | IllegalAccessException | InvocationTargetException e) {
+            log.error("Failed to run database consistency check: " + e);
         }
-        return true;
+        return false;
     }
 
-    private AbstractBundlePersistenceManager getPersistenceManager(HippoRepository repo) throws RepositoryException {
-        final RepositoryImpl repository = (RepositoryImpl) RepositoryDecorator.unwrap(repo.getRepository());
-        return (AbstractBundlePersistenceManager) repository.getPersistenceManager("default");
+    private Object getPersistenceManager(Object repo) throws RepositoryException {
+        final Repository repository = getRepository(repo);
+        try {
+            return repository.getClass().getMethod("getPersistenceManager", String.class).invoke(repository, "default");
+        } catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+            log.error("Failed to get PersistenceManager from RepositoryImpl: " + e);
+        }
+        return null;
     }
 
-    private SearchIndex getSearchIndex(HippoRepository repo) throws RepositoryException {
-        final RepositoryImpl repository = (RepositoryImpl) RepositoryDecorator.unwrap(repo.getRepository());
-        return (SearchIndex) repository.getHippoQueryHandler("default");
+    private Object getSearchIndex(Object repo) throws RepositoryException {
+        final Repository repository = getRepository(repo);
+        try {
+            return repository.getClass().getMethod("getHippoQueryHandler", String.class).invoke(repository, "default");
+        } catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+            log.error("Failed to get QueryHandler from RepositoryImpl:" + e);
+        }
+        return null;
     }
 
     private String getTestSystemViewXML(Session session) throws RepositoryException, IOException {
@@ -238,4 +277,46 @@ public abstract class ClusterTest {
         return out.toString();
     }
 
+    private Repository getRepository(Object repo) {
+        try {
+            Object decorator = repo.getClass().getMethod("getRepository").invoke(repo);
+            final Field repository = decorator.getClass().getDeclaredField("repository");
+            repository.setAccessible(true);
+            return (Repository) repository.get(decorator);
+        } catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException | NoSuchFieldException e) {
+            log.error("Failed to get internal RepositorImpl from LocalHippoRepository: " + e);
+        }
+        return null;
+    }
+
+    private Session loginSession(Object repo) {
+        try {
+            return (Session) repo.getClass().getMethod("login", Credentials.class).invoke(repo, CREDENTIALS);
+        } catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+            log.error("Failed to log in session: " + e);
+        }
+        return null;
+    }
+
+    /**
+     * ClassLoader to run each repository in isolation to each other. Only the JCR API
+     * is shared with the test class so that test cases can use JCR without reflection.
+     */
+    private static class RepositoryClassLoader extends URLClassLoader {
+
+        private final URLClassLoader shared;
+
+        public RepositoryClassLoader(final URL[] urls, final URLClassLoader shared) {
+            super(urls, null);
+            this.shared = shared;
+        }
+
+        @Override
+        public Class<?> loadClass(final String name, boolean resolve) throws ClassNotFoundException {
+            if (name.startsWith("javax.jcr")) {
+                return shared.loadClass(name);
+            }
+            return super.loadClass(name, resolve);
+        }
+    }
 }
