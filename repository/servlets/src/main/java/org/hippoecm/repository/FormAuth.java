8a39c5447fc94b3fca0371dd359b1506e7d08caf
Minos Chatzidakis <m.chatzidakis@onehippo.com>
2014-10-17 14:41:06 +0000

    REPO-1102: Moved login screen html to freemarker template

--- repository/servlets/src/main/java/org/hippoecm/repository/FormAuth.java
+++ repository/servlets/src/main/java/org/hippoecm/repository/FormAuth.java
@@ -16,8 +16,11 @@
 package org.hippoecm.repository;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.PrintWriter;
 import java.net.InetAddress;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.jcr.LoginException;
 import javax.jcr.Session;
@@ -27,10 +30,17 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang.StringUtils;
 import org.hippoecm.repository.api.HippoSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import freemarker.cache.ClassTemplateLoader;
+import freemarker.template.Configuration;
+import freemarker.template.DefaultObjectWrapper;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
 
 public class FormAuth {
 
@@ -38,6 +48,7 @@ public class FormAuth {
     private static final String AUTHORIZED = "cms-user-authorized";
     private static final String AUTHORIZATION_CREDENTIALS = "cms-user-credentials";
 
+    private static Template formAuthRenderingTemplate;
 
     public static final Session authorize(HttpServletRequest request, HippoRepository repository) throws ServletException, IOException {
 
@@ -84,69 +95,27 @@ public class FormAuth {
         request.getSession().removeAttribute(AUTHORIZATION_CREDENTIALS);
     }
 
-    public static final void showLoginPage(HttpServletRequest request, HttpServletResponse response, String message) throws ServletException, IOException {
-        PrintWriter writer = response.getWriter();
-        response.setStatus(HttpServletResponse.SC_OK);
-        response.setContentType("text/html");
-
-        writer.println("<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\"");
-        writer.println("    \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">");
-        writer.println("<html xmlns=\"http://www.w3.org/1999/xhtml\">");
-        writer.println("<head>");
-        writer.println("  <title>Hippo Repository Browser - Login</title>");
-        writer.println("  <meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" />");
-        writer.println("  <style type=\"text/css\">");
-        writer.println("    h3 {margin:2px}");
-        writer.println("    table.params {font-size:small}");
-        writer.println("    td.header {text-align: left; vertical-align: top; padding: 10px;}");
-        writer.println("    td {text-align: left}");
-        writer.println("    th {text-align: left}");
-        writer.println("    * {font-family: tahoma, arial, sans-serif, helvetica;}");
-        writer.println("    * {font-size: 14px;}");
-        writer.println("    form {margin-top: 10px}");
-        writer.println("    div.message {font-size: small; color: red}");
-        writer.println("    .hippo-header {background: #32629b; height: 25px; background-image: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB0AAAAYCAYAAAAGXva8AAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAAAWZJREFUeNrsVtFRwkAQ9WjA60A6MB2YEqACsQKxA6xArCBSQbQCsQPoIHQQKojv6QuzZC4hkwQ/HHbmzYW73L3s7ts9XFEUV39t7kJ6VjtFivUIGA/J9+Opc85Oegxz4BbYmvevAZJ/4P23Xk5aT+XVJ8eGTTMg1cf1I+UhIvQtNkZdiY/Ci5EhXeN503IzCRMgY8jNUsxzOI+zslOkKcZphy8fK9e0e+BV4x64Ab6sBg6kEk7eRyBl2DE8As/0Er9fzPKW5x9yCiTA5Azl6Ct6ick30voTcDd453Eu1yPJV8r3L6kWfdcyaEFeCmpz1JHk+mKgsDKUs1JoSl8a7EhK/Kpt2dQQJiohr5R5eTsNdiSzqakpLNQY4pBwKooNNodRYD4PNXiGTEJgWT2oFqsWqT4bLUTK0EwC0meRLykKCW9Xc/tkre5Te8uIZF7mwti7zbW5jaq2NKVS33svf1f+Fem3AAMA5G0u/dkz/GsAAAAASUVORK5CYII=); background-repeat: no-repeat; background-position-x: 5px;");
-        writer.println("  </style>");
-        writer.println("</head>");
-        writer.println("<body>");
-        writer.println("  <div class=\"hippo-header\"></div>");
-        writer.println("  <table summary=\"infotable\">");
-        writer.println("    <tr>");
-        writer.println("      <td class=\"header\">");
-        writer.println("        <h3>Log in</h3>");
-        writer.println("        <div class=\"message\">" + message + "</div>");
-        writer.println("          <form method=\"post\" action=\"\" accept-charset=\"UTF-8\">");
-        writer.println("            <table style=\"params\" summary=\"searching\">");
-        writer.println("              <tr>");
-        writer.println("                <th>Username: </th>");
-        writer.println("                <td>");
-        writer.println("                    <input name=\"username\" type=\"text\" size=\"15\"/>");
-        writer.println("                </td>");
-        writer.println("              </tr>");
-        writer.println("              <tr>");
-        writer.println("                <th>Password: </th>");
-        writer.println("                <td>");
-        writer.println("                  <input name=\"password\" type=\"password\" size=\"15\"/>");
-        writer.println("                </td>");
-        writer.println("              </tr>");
-        writer.println("              <tr>");
-        writer.println("                <th>&nbsp;</th>");
-        writer.println("                <td>");
-        writer.println("                  <input type=\"submit\" value=\"Log in\"/>");
-        writer.println("                </td>");
-        writer.println("              </tr>");
-        writer.println("            </table>");
-        writer.println("          </form>");
-        writer.println("        </td>");
-        writer.println("      </tr>");
-        writer.println("    </table>");
-        writer.println("  </body>");
-        writer.println("</html>");
-
-        writer.close();
+    public static synchronized final void showLoginPage(HttpServletRequest request, HttpServletResponse response, String message) throws ServletException, IOException {
+        PrintWriter out = null;
+        try {
+            out = response.getWriter();
+            Map<String, Object> context = new HashMap<String, Object>();
+            context.put("request", request);
+            context.put("response", response);
+            context.put("message", message);
+            if( formAuthRenderingTemplate == null) {
+                formAuthRenderingTemplate = getRenderTemplate(request);
+            }
+            formAuthRenderingTemplate.process(context, out);
+            out.flush();
+        } catch (TemplateException e) {
+            log.warn("Failed to render freemarker template.", e);
+        } finally {
+            if (out != null) {
+                out.close();
+            }
+        }
     }
-
-
 
     public static SimpleCredentials parseAuthorizationForm(HttpServletRequest request) throws ServletException, IOException {
         String username = request.getParameter("username");
@@ -158,4 +127,21 @@ public class FormAuth {
         return new SimpleCredentials(username, password.toCharArray());
     }
 
+    private static Template getRenderTemplate(final HttpServletRequest request) throws IOException, TemplateException {
+        Configuration cfg = new Configuration();
+
+        cfg.setObjectWrapper(new DefaultObjectWrapper());
+        cfg.setTemplateLoader(new ClassTemplateLoader(FormAuth.class, ""));
+
+        InputStream propsInput = null;
+        final String propsResName = FormAuth.class.getSimpleName() + "-ftl.properties";
+
+        try {
+            propsInput = FormAuth.class.getResourceAsStream(propsResName);
+            cfg.setSettings(propsInput);
+            return cfg.getTemplate(FormAuth.class.getSimpleName() + "-html.ftl");
+        } finally {
+            IOUtils.closeQuietly(propsInput);
+        }
+    }
 }
---
+++ repository/servlets/src/main/resources/org/hippoecm/repository/FormAuth-html.ftl
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<#--
+  Copyright 2014 Hippo B.V. (http://www.onehippo.com)
+
+  Licensed under the Apache License, Version 2.0 (the  "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS"
+  BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+-->
+
+${response.setContentType("text/html;charset=UTF-8")}
+
+
+<html>
+<head>
+    <title>Hippo Repository Browser - Login</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <style type="text/css">
+      h3 {margin:2px}
+      table.params {font-size:small}
+      td.header {text-align: left; vertical-align: top; padding: 10px;}
+      td {text-align: left}
+      th {text-align: left}
+      * {font-family: tahoma, arial, sans-serif, helvetica;}
+      * {font-size: 14px;}
+      form {margin-top: 10px}
+      div.message {font-size: small; color: red}
+      .hippo-header {background: #32629b; height: 25px; background-image: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB0AAAAYCAYAAAAGXva8AAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAAAWZJREFUeNrsVtFRwkAQ9WjA60A6MB2YEqACsQKxA6xArCBSQbQCsQPoIHQQKojv6QuzZC4hkwQ/HHbmzYW73L3s7ts9XFEUV39t7kJ6VjtFivUIGA/J9+Opc85Oegxz4BbYmvevAZJ/4P23Xk5aT+XVJ8eGTTMg1cf1I+UhIvQtNkZdiY/Ci5EhXeN503IzCRMgY8jNUsxzOI+zslOkKcZphy8fK9e0e+BV4x64Ab6sBg6kEk7eRyBl2DE8As/0Er9fzPKW5x9yCiTA5Azl6Ct6ick30voTcDd453Eu1yPJV8r3L6kWfdcyaEFeCmpz1JHk+mKgsDKUs1JoSl8a7EhK/Kpt2dQQJiohr5R5eTsNdiSzqakpLNQY4pBwKooNNodRYD4PNXiGTEJgWT2oFqsWqT4bLUTK0EwC0meRLykKCW9Xc/tkre5Te8uIZF7mwti7zbW5jaq2NKVS33svf1f+Fem3AAMA5G0u/dkz/GsAAAAASUVORK5CYII=); background-repeat: no-repeat; background-position-x: 5px;
+    </style>
+  </head>
+<body>
+  <div class="hippo-header"></div>
+  <table summary="infotable">
+      <tr>
+          <td class="header">
+              <h3>Log in</h3>
+              <div class="message">${message}</div>
+                <form method="post" action="" accept-charset="UTF-8">
+                    <table style="params" summary="searching">
+                        <tr>
+                            <th>Username: </th>
+                            <td>
+                                  <input name="username" type="text" size="15" autofocus="autofocus"/>
+                              </td>
+                          </tr>
+                        <tr>
+                            <th>Password: </th>
+                            <td>
+                                <input name="password" type="password" size="15"/>
+                              </td>
+                          </tr>
+                        <tr>
+                            <th>&nbsp;</th>
+                            <td>
+                                <input type="submit" value="Log in"/>
+                              </td>
+                          </tr>
+                      </table>
+                  </form>
+              </td>
+          </tr>
+      </table>
+  </body>
+</html>
