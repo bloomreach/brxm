79ba8929baa6ba3b5ea6c00e3c82e6b5976613ef
Mathijs den Burger <m.denburger@onehippo.com>
2014-12-09 16:40:33 +0000

    CMS7-8417: re-add CSS class hippo-typeslist
    It is used by QA essentials to locate the types listing.

--- cms/perspectives/src/main/java/org/hippoecm/frontend/plugins/cms/browse/list/TypesListingPlugin.java
+++ cms/perspectives/src/main/java/org/hippoecm/frontend/plugins/cms/browse/list/TypesListingPlugin.java
@@ -66,7 +66,7 @@ public final class TypesListingPlugin extends ExpandCollapseListingPlugin<Node> 
     public TypesListingPlugin(IPluginContext context, IPluginConfig config) {
         super(context, config);
 
-        setClassName(DocumentListColumn.DOCUMENT_LIST_CSS_CLASS);
+        setClassName("hippo-typeslist " + DocumentListColumn.DOCUMENT_LIST_CSS_CLASS);
 
         IModel<Node> nodeModel = getModel();
         if (nodeModel != null && nodeModel.getObject() != null) {
