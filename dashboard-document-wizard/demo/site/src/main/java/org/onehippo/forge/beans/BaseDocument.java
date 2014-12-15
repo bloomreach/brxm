74286156d1a2b7d0585f0b2a955e0788ded897d6
Ate Douma <a.douma@onehippo.com>
2014-12-15 22:41:17 +0000

    HIPPLUG-970: initial import of the dashboard-document-wizard standard plugin for CMS 7.10, cloned (unmodified) from the Forge dashboard-document-wizard plugin

---
+++ dashboard-document-wizard/demo/site/src/main/java/org/onehippo/forge/beans/BaseDocument.java
@@ -0,0 +1,9 @@
+package org.onehippo.forge.beans;
+
+import org.hippoecm.hst.content.beans.Node;
+import org.hippoecm.hst.content.beans.standard.HippoDocument;
+
+@Node(jcrType="dashboarddocumentwizarddemo:basedocument")
+public class BaseDocument extends HippoDocument {
+
+}
