2108d330cc60a7a971782190cf497ee3c026623e
Frank van Lankvelt <f.vanlankvelt@onehippo.com>
2014-12-15 20:08:41 +0000

    HSTTWO-3139: post publish and discard events
    When publishing or discarding changes, post (synchronous) events on the ComponentManager event bus.

---
+++ site-toolkit/api/src/main/java/org/hippoecm/hst/container/event/ChannelPublicationEvent.java
@@ -0,0 +1,57 @@
+/**
+ * Copyright 2012-2013 Hippo B.V. (http://www.onehippo.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.hippoecm.hst.container.event;
+
+import javax.jcr.Session;
+import java.util.EventObject;
+import java.util.List;
+
+public class ChannelPublicationEvent extends EventObject {
+
+    public enum Action {
+        PUBLISH,
+        DISCARD
+    }
+
+    private final Action action;
+    private final List<String> userIds;
+    private final String mountId;
+    private final Session session;
+
+    public ChannelPublicationEvent(Action action, List<String> userIds, String mountId, Session session) {
+        super(mountId);
+        this.action = action;
+        this.userIds = userIds;
+        this.mountId = mountId;
+        this.session = session;
+    }
+
+    public Action getAction() {
+        return action;
+    }
+
+    public List<String> getUserIds() {
+        return userIds;
+    }
+
+    public String getMountId() {
+        return mountId;
+    }
+
+    public Session getSession() {
+        return session;
+    }
+}
