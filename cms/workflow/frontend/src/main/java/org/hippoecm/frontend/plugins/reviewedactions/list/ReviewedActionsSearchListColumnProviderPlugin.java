c603278e6bf5faca17eb747ca7f1809d1f79b2dd
Mathijs den Burger <m.denburger@onehippo.com>
2014-12-17 15:53:38 +0000

    CMS7-8468: restyle the search results

--- cms/workflow/frontend/src/main/java/org/hippoecm/frontend/plugins/reviewedactions/list/ReviewedActionsSearchListColumnProviderPlugin.java
+++ cms/workflow/frontend/src/main/java/org/hippoecm/frontend/plugins/reviewedactions/list/ReviewedActionsSearchListColumnProviderPlugin.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2010-2013 Hippo B.V. (http://www.onehippo.com)
+ *  Copyright 2010-2014 Hippo B.V. (http://www.onehippo.com)
  * 
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -34,6 +34,12 @@ import org.hippoecm.frontend.plugins.standards.list.ListColumn;
 import org.hippoecm.frontend.plugins.standards.list.resolvers.EmptyRenderer;
 import org.hippoecm.frontend.skin.DocumentListColumn;
 
+/**
+ * Renders the 'state' column in a document listing.
+ *
+ * @deprecated use the @{link DocumentIconAndStateRenderer} instead.
+ */
+@Deprecated
 public class ReviewedActionsSearchListColumnProviderPlugin extends AbstractListColumnProviderPlugin {
 
     private static final long serialVersionUID = 1L;
