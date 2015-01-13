dd741723a545bbb6a090517cd562a18b361e8235
Ard Schrijvers <a.schrijvers@onehippo.com>
2015-01-13 15:04:28 +0000

    HSTTWO-3167 organize

--- site-toolkit/client/src/main/java/org/hippoecm/hst/freemarker/jcr/WebResourceTemplateLoader.java
+++ site-toolkit/client/src/main/java/org/hippoecm/hst/freemarker/jcr/WebResourceTemplateLoader.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2014 Hippo B.V. (http://www.onehippo.com)
+ *  Copyright 2014-2015 Hippo B.V. (http://www.onehippo.com)
  * 
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -17,11 +17,9 @@ package org.hippoecm.hst.freemarker.jcr;
 
 import java.io.IOException;
 
-import org.apache.commons.lang.StringUtils;
 import org.hippoecm.hst.container.RequestContextProvider;
 import org.hippoecm.hst.core.container.ContainerConstants;
 import org.hippoecm.hst.core.request.HstRequestContext;
-import org.hippoecm.hst.core.request.ResolvedMount;
 import org.hippoecm.hst.util.PathUtils;
 import org.hippoecm.hst.util.WebResourceUtils;
 import org.onehippo.cms7.services.webresources.WebResourcesService;
