f25e8698ca4ffcc14bc88331efdffc4427b3d23c
Mathijs den Burger <m.denburger@onehippo.com>
2015-01-21 12:55:38 +0000

    CMS7-8698: prevent NPE in non-Ajax setting

--- cms/engine/src/main/java/org/hippoecm/frontend/behaviors/ContextMenuBehavior.java
+++ cms/engine/src/main/java/org/hippoecm/frontend/behaviors/ContextMenuBehavior.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2008-2013 Hippo B.V. (http://www.onehippo.com)
+ *  Copyright 2008-2015 Hippo B.V. (http://www.onehippo.com)
  * 
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -76,10 +76,12 @@ public class ContextMenuBehavior extends AbstractDefaultAjaxBehavior {
      */
     public void collapseAll() {
         AjaxRequestTarget target = RequestCycle.get().find(AjaxRequestTarget.class);
-        for (IContextMenu menu : getMenus(false)) {
-            menu.collapse(target);
+        if (target != null) {
+            for (IContextMenu menu : getMenus(false)) {
+                menu.collapse(target);
+            }
+            hide(target);
         }
-        hide(target);
     }
 
     /**
