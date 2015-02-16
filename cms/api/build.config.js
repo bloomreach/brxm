08d8157a2849f44a46ab352a33598fc268e79d79
Unico Hommes <u.hommes@onehippo.com>
2015-02-16 14:55:39 +0000

    CMS7-8852 store bower components in the target directory, output images and skin files directly to the target/classes directory

--- cms/api/build.config.js
+++ cms/api/build.config.js
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014 Hippo B.V. (http://www.onehippo.com)
+ * Copyright 2015 Hippo B.V. (http://www.onehippo.com)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,10 +15,10 @@
  */
 
 module.exports = {
-    bower:      'src/main/styling/components',
+    bower:      'target/bower/components',
     file:       'hippo-cms-theme',
-    images:     'src/main/resources/org/hippoecm/frontend/skin/images',
-    skin:       'src/main/resources/skin/hippo-cms',
+    images:     'target/classes/org/hippoecm/frontend/skin/images',
+    skin:       'target/classes/skin/hippo-cms',
     src:        'src/main/styling',
     target:     'target',
     tmp:        'target/.tmp'
