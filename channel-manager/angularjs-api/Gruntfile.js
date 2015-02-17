8108b5a0b9fecd0c118a1c0591f6ac59f3b452cc
Michiel Eggermont <m.eggermont@onehippo.com>
2015-02-17 11:37:42 +0000

    CHANNELMGR-123 Fix grunt watch
    Problem was that cfg.demoSrc did not exists so the watch task 
    started watching all html files under root. Since there is no
    html in this module I simply removed the line.

--- channel-manager/angularjs-api/Gruntfile.js
+++ channel-manager/angularjs-api/Gruntfile.js
@@ -62,7 +62,6 @@ module.exports = function (grunt) {
             },
             livereload: {
                 files: [
-                    '<%= cfg.demoSrc %>/**/*.html',
                     'dist/css/**/*.css'
                 ]
             }
