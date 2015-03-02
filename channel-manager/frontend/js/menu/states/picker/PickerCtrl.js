4e12db44a757938ffcd38e201dee7a0f3f427604
Mark Lenser <m.lenser@onehippo.com>
2015-03-02 14:58:49 +0000

    CHANNELMGR-133: indenation to 4. add plain list

--- channel-manager/frontend/js/menu/states/picker/PickerCtrl.js
+++ channel-manager/frontend/js/menu/states/picker/PickerCtrl.js
@@ -14,20 +14,91 @@
  * limitations under the License.
  */
 (function () {
-  "use strict";
+    "use strict";
 
-  angular.module('hippo.channel.menu')
+    angular.module('hippo.channel.menu')
 
-      .controller('hippo.channel.menu.PickerCtrl', [
-        '$scope',
-        '$state',
-        '$stateParams',
-        function ($scope, $state, $stateParams) {
-          $scope.cancelPicker = function() {
-            $state.go('menu-item.edit', {
-              menuItemId: $stateParams.menuItemId
-            });
-          };
-        }
-      ]);
+        .controller('hippo.channel.menu.PickerCtrl', [
+            '$scope',
+            '$state',
+            '$stateParams',
+            function ($scope, $state, $stateParams) {
+                $scope.cancelPicker = function() {
+                    $state.go('menu-item.edit', {
+                        menuItemId: $stateParams.menuItemId
+                    });
+                };
+                $scope.pickerTreeItems = [
+                    {
+                        collapsed: true,
+                        hasFolders: true,
+                        id: 'item-a',
+                        title: 'Item A',
+                        items: [
+                            {
+                                collapsed: true,
+                                id: 'item-a1',
+                                title: 'Item A.1',
+                                items: []
+                            },
+                            {
+                                collapsed: true,
+                                hasFolders: true,
+                                id: 'item-a2',
+                                title: 'Item A.2 has a very long name that probably will not fit on a single row when tree becomes very small.' +
+                                        'Words are fun, lots of words. Please more words, lots of words.',
+                                items: [
+                                    {
+                                        collapsed: true,
+                                        hasFolders: true,
+                                        id: 'item-2-1',
+                                        title: 'Item A.2.1',
+                                        items: [
+                                            {
+                                                collapsed: true,
+                                                id: 'item-2-1-1',
+                                                title: 'Item A.2.1.1',
+                                                items: []
+                                            }
+                                        ]
+                                    },
+                                    {
+                                        collapsed: true,
+                                        id: 'item-2-2',
+                                        title: 'Item A.2.2',
+                                        items: []
+                                    }
+                                ]
+                            },
+                            {
+                                collapsed: true,
+                                id: 'item-a3',
+                                title: 'Item A.3',
+                                items: []
+                            }
+                        ]
+                    },
+                    {
+                        collapsed: true,
+                        id: 'item-b',
+                        title: 'Item B',
+                        items: []
+                    },
+                    {
+                        collapsed: true,
+                        hasFolders: true,
+                        id: 'item-c',
+                        title: 'Item C',
+                        items: [
+                            {
+                                collapsed: true,
+                                id: 'item-c1',
+                                title: 'Item C.1',
+                                items: []
+                            }
+                        ]
+                    }
+                ];
+            }
+        ]);
 }());
