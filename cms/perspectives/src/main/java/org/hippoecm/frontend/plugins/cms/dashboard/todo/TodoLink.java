e0bd3bd75fd01f06faaacfb065c351f23f94b477
Oscar Scholten <o.scholten@onehippo.com>
2015-03-19 15:53:10 +0000

    CMS7-8527: restyling the todo section of the dashboard - added a dedicated link rendering wicket component - updating the EN and NL properties files to have proper sentences - updating the FR and DE properties files to have at least the correct class for each span

---
+++ cms/perspectives/src/main/java/org/hippoecm/frontend/plugins/cms/dashboard/todo/TodoLink.java
@@ -0,0 +1,79 @@
+/*
+ *  Copyright 2015 Hippo B.V. (http://www.onehippo.com)
+ * 
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.hippoecm.frontend.plugins.cms.dashboard.todo;
+
+import org.apache.wicket.AttributeModifier;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.markup.html.AjaxLink;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.PropertyModel;
+import org.hippoecm.frontend.model.JcrNodeModel;
+import org.hippoecm.frontend.plugin.IPluginContext;
+import org.hippoecm.frontend.plugin.config.IPluginConfig;
+import org.hippoecm.frontend.plugins.cms.dashboard.BrowseLinkTarget;
+import org.hippoecm.frontend.service.IBrowseService;
+import org.hippoecm.repository.util.JcrUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TodoLink extends Panel {
+
+    private static final long serialVersionUID = 1L;
+
+    private static final Logger log = LoggerFactory.getLogger(TodoLink.class);
+
+    public TodoLink(final IPluginContext context, final IPluginConfig config, String id,
+                      final BrowseLinkTarget browseLinkTarget, IModel<String> userLabelModel,
+                      IModel<String> operationLabelModel) {
+        super(id);
+
+        AjaxLink<Void> link = new AjaxLink<Void>("link") {
+
+            @SuppressWarnings("unchecked")
+            @Override
+            public void onClick(AjaxRequestTarget target) {
+                final JcrNodeModel nodeModel = browseLinkTarget.getBrowseModel();
+                final String browserId = config.getString("browser.id");
+                final IBrowseService browseService = context.getService(browserId, IBrowseService.class);
+                if (browseService != null) {
+                    browseService.browse(nodeModel);
+                } else {
+                    log.warn("no browse service found with id '{}', cannot browse to '{}'", browserId, JcrUtils.getNodePathQuietly(nodeModel.getNode()));
+                }
+            }
+
+            @Override
+            public boolean isEnabled() {
+                return browseLinkTarget.getBrowseModel() != null;
+            }
+        };
+        add(link);
+        link.add(new AttributeModifier("title", new PropertyModel(browseLinkTarget, "displayPath")));
+
+        Label userLabel = new Label("username", userLabelModel);
+        link.add(userLabel);
+
+        Label operationLabel = new Label("operation", operationLabelModel);
+        operationLabel.setEscapeModelStrings(false);
+        link.add(operationLabel);
+
+        Label documentLabel = new Label("document", '"' + browseLinkTarget.getName() + '"');
+        link.add(documentLabel);
+    }
+
+}
--- cms/perspectives/src/main/java/org/hippoecm/frontend/plugins/cms/dashboard/todo/TodoPlugin.java
+++ cms/perspectives/src/main/java/org/hippoecm/frontend/plugins/cms/dashboard/todo/TodoPlugin.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2008-2013 Hippo B.V. (http://www.onehippo.com)
+ *  Copyright 2008-2015 Hippo B.V. (http://www.onehippo.com)
  * 
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -21,21 +21,16 @@ import java.util.Iterator;
 import javax.jcr.Node;
 import javax.jcr.RepositoryException;
 
-import org.apache.wicket.AttributeModifier;
 import org.apache.wicket.Component;
-import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.repeater.Item;
 import org.apache.wicket.markup.repeater.RefreshingView;
 import org.apache.wicket.markup.repeater.data.IDataProvider;
-import org.apache.wicket.model.AbstractReadOnlyModel;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.PropertyModel;
 import org.apache.wicket.model.StringResourceModel;
 import org.hippoecm.frontend.plugin.IPluginContext;
 import org.hippoecm.frontend.plugin.config.IPluginConfig;
-import org.hippoecm.frontend.plugins.cms.dashboard.BrowseLink;
 import org.hippoecm.frontend.plugins.cms.dashboard.BrowseLinkTarget;
-import org.hippoecm.frontend.plugins.standards.list.resolvers.CssClass;
 import org.hippoecm.frontend.service.render.RenderPlugin;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -75,22 +70,13 @@ public class TodoPlugin extends RenderPlugin {
 
         @Override
         protected void populateItem(final Item item) {
-            item.add(CssClass.append(new AbstractReadOnlyModel() {
-                @Override
-                public Object getObject() {
-                    return (item.getIndex() % 2 == 1) ? "even" : "odd";
-                }
-            }));
-
             try {
                 Node node = (Node) item.getModelObject();
                 BrowseLinkTarget target = new BrowseLinkTarget(node.getPath());
-                item.add(new BrowseLink(getPluginContext(), getPluginConfig(), "request", target,
-                        new PropertyModel<String>(target, "name")));
-
                 Request request = new Request(item.getModel(), this);
-                item.add(new Label("request-description", new PropertyModel(request, "localType")));
-                item.add(new Label("request-owner", new PropertyModel(request, "username")));
+                item.add(new TodoLink(getPluginContext(), getPluginConfig(), "link", target,
+                        new PropertyModel<>(request, "username"),
+                        new PropertyModel<>(request, "localType")));
             } catch (RepositoryException e) {
                 log.error("Failed to create todo item from publication request node", e);
             }
