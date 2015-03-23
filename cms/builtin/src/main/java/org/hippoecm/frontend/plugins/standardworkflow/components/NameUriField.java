60e54a7bef1436673fd1b9ed2a39264fc0e60749
Arthur Bogaart <a.bogaart@onehippo.com>
2015-03-23 18:54:13 +0000

    CMS7-8970: Add the AjaxLink uriAction to the AjaxRequestTarget when clicked. This will redraw the label

--- cms/builtin/src/main/java/org/hippoecm/frontend/plugins/standardworkflow/components/NameUriField.java
+++ cms/builtin/src/main/java/org/hippoecm/frontend/plugins/standardworkflow/components/NameUriField.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2010-2015 Hippo B.V. (http://www.onehippo.com)
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *  http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -83,7 +83,7 @@ public class NameUriField extends WebMarkupContainer {
     private String encode(final IModel<String> text) {
         return codecModel.getObject().encode(text.getObject());
     }
-    
+
     private FormComponent createNameComponent(final PropertyModel<String> nameModel) {
         final FormComponent nameComponent = new TextField<>("name", new IModel<String>() {
             private static final long serialVersionUID = 1L;
@@ -153,6 +153,7 @@ public class NameUriField extends WebMarkupContainer {
                     target.focusComponent(urlComponent);
                 }
                 target.add(urlComponent);
+                target.add(this);
             }
         };
         uriAction.add(new Label("uriActionLabel", new AbstractReadOnlyModel<String>() {
