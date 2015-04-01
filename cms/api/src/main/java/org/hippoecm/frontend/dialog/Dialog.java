c0e5c3f1c9b6d6345501a58c5845a8c92545f3d7
Arthur Bogaart <a.bogaart@onehippo.com>
2015-04-01 23:34:12 +0000

    CMS7-8770: Fix overflow:hidden on bottom-left panel (IE10) - Detect IE10 and if found add a small script that sets the width of the dialog-bottom-left panel so that it fills the leftover space. Other browsers do this using flexbox

--- cms/api/src/main/java/org/hippoecm/frontend/dialog/Dialog.java
+++ cms/api/src/main/java/org/hippoecm/frontend/dialog/Dialog.java
@@ -19,9 +19,14 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 import org.apache.commons.lang.StringUtils;
+import org.apache.wicket.Session;
+import org.apache.wicket.core.request.ClientInfo;
+import org.apache.wicket.markup.head.IHeaderResponse;
+import org.apache.wicket.markup.head.OnLoadHeaderItem;
 import org.apache.wicket.markup.html.panel.EmptyPanel;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
+import org.apache.wicket.protocol.http.request.WebClientInfo;
 import org.apache.wicket.util.value.IValueMap;
 import org.apache.wicket.util.value.ValueMap;
 
@@ -46,6 +51,24 @@ public class Dialog<T> extends AbstractDialog<T> {
         setButtonCssClass("hippo-button");
 
         add(new EmptyPanel(BOTTOM_LEFT_ID).setVisible(false));
+    }
+
+    @Override
+    public void renderHead(final IHeaderResponse response) {
+        ClientInfo info = Session.get().getClientInfo();
+        if (info instanceof WebClientInfo) {
+            WebClientInfo webInfo = (WebClientInfo) info;
+            // IE10 needs a width in pixels on the .hippo-dialog.bottom-left panel in order to render
+            // items that have overflow:hidden correctly.
+            if (webInfo.getProperties().isBrowserInternetExplorer() &&
+                    webInfo.getProperties().getBrowserVersionMajor() == 10) {
+                final String script = String.format("$('%s').width($('%s').width() - $('%s').width())",
+                        ".hippo-dialog-bottom-left", ".hippo-dialog-bottom", ".hippo-dialog-bottom-right");
+
+                response.render(OnLoadHeaderItem.forScript(script));
+            }
+        }
+        super.renderHead(response);
     }
 
     @Override
