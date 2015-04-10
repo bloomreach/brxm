98bda275e2b276b49a46ae774b2e359bb6a2cffc
Arthur Bogaart <a.bogaart@onehippo.com>
2015-04-10 14:53:13 +0000

    CMS7-9061: Removed old ajax-indicator GIF and replaced it with a temporary SVG

--- cms/api/src/main/java/org/hippoecm/frontend/plugins/yui/ajax/AjaxIndicatorBehavior.java
+++ cms/api/src/main/java/org/hippoecm/frontend/plugins/yui/ajax/AjaxIndicatorBehavior.java
@@ -1,12 +1,12 @@
 /*
- *  Copyright 2008-2013 Hippo B.V. (http://www.onehippo.com)
- * 
+ *  Copyright 2008-2015 Hippo B.V. (http://www.onehippo.com)
+ *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
  *  You may obtain a copy of the License at
- * 
+ *
  *       http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  *  Unless required by applicable law or agreed to in writing, software
  *  distributed under the License is distributed on an "AS IS" BASIS,
  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,30 +19,33 @@ import java.util.Map;
 
 import org.apache.wicket.Component;
 import org.apache.wicket.extensions.ajax.markup.html.AjaxIndicatorAppender;
-import org.apache.wicket.request.cycle.RequestCycle;
-import org.apache.wicket.request.resource.PackageResourceReference;
-import org.apache.wicket.request.resource.ResourceReference;
+import org.apache.wicket.request.Response;
 import org.apache.wicket.util.collections.MiniMap;
 import org.hippoecm.frontend.plugins.yui.AbstractYuiBehavior;
 import org.hippoecm.frontend.plugins.yui.HippoNamespace;
 import org.hippoecm.frontend.plugins.yui.header.IYuiContext;
+import org.hippoecm.frontend.service.IconSize;
+import org.hippoecm.frontend.skin.CmsIcon;
 
 public class AjaxIndicatorBehavior extends AbstractYuiBehavior {
-
-    private static final long serialVersionUID = 1L;
-
-    private final static ResourceReference AJAX_LOADER_GIF = new PackageResourceReference(AjaxIndicatorBehavior.class,
-            "ajax-loader.gif");
 
     final private AjaxIndicatorAppender ajaxIndicator;
 
     public AjaxIndicatorBehavior() {
         ajaxIndicator = new AjaxIndicatorAppender() {
-            private static final long serialVersionUID = 1L;
 
             @Override
-            protected CharSequence getIndicatorUrl() {
-                return RequestCycle.get().urlFor(AJAX_LOADER_GIF, null);
+            public void afterRender(final Component component) {
+                final Response r = component.getResponse();
+
+                r.write("<div style=\"display:none;\" class=\"");
+                r.write(getSpanClass());
+                r.write("\" ");
+                r.write("id=\"");
+                r.write(getMarkupId());
+                r.write("\">");
+                r.write(CmsIcon.FLOPPY_TIMES_CIRCLE.getInlineSvg(IconSize.XL));
+                r.write("</div>");
             }
         };
     }
@@ -51,7 +54,7 @@ public class AjaxIndicatorBehavior extends AbstractYuiBehavior {
     public void addHeaderContribution(IYuiContext helper) {
         helper.addModule(HippoNamespace.NS, "ajaxindicator");
 
-        Map<String, Object> parameters = new MiniMap(1);
+        Map<String, Object> parameters = new MiniMap<>(1);
         parameters.put("id", ajaxIndicator.getMarkupId());
         helper.addTemplate(AjaxIndicatorBehavior.class, "init_ajax_indicator.js", parameters);
     }
