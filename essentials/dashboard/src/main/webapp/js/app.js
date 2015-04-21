7f1ad49459314fa7dcaec3a0913ad92c4273c04d
Tobias Jeger <t.jeger@onehippo.com>
2015-04-21 07:55:51 +0000

    ESSENTIALS-658: Add default GET headers to bypass IE's caching.

--- essentials/dashboard/src/main/webapp/js/app.js
+++ essentials/dashboard/src/main/webapp/js/app.js
@@ -112,6 +112,13 @@
                 };
             });
             $httpProvider.interceptors.push('MyHttpInterceptor');
+
+            // IE caches GET requests. Below configuration bypasses this to make Essentials work in IE.
+            if (!$httpProvider.defaults.headers.get) {
+                $httpProvider.defaults.headers.get = {};
+            }
+            $httpProvider.defaults.headers.get['If-Modified-Since'] = 'Mon, 26 Jul 1997 05:00:00 GMT';
+            $httpProvider.defaults.headers.get['Cache-Control'] = 'no-cache';
         })
 
 //############################################
