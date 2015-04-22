cb88e5c5fdd48f036cba919a13c0f3accffb1a98
Mathijs den Burger <m.denburger@onehippo.com>
2015-04-22 15:13:32 +0000

    CHANNELMGR-17: change indentation to 2 spaces

--- channel-manager/frontend/src/main/resources/org/onehippo/cms7/channelmanager/widgets/ExtLinkPicker.js
+++ channel-manager/frontend/src/main/resources/org/onehippo/cms7/channelmanager/widgets/ExtLinkPicker.js
@@ -13,161 +13,161 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-(function() {
-    "use strict";
+(function () {
+  "use strict";
 
-    Ext.namespace('Hippo.ChannelManager');
+  Ext.namespace('Hippo.ChannelManager');
 
-    Hippo.ChannelManager.ExtLinkPickerFactory = Ext.extend(Ext.util.Observable, {
+  Hippo.ChannelManager.ExtLinkPickerFactory = Ext.extend(Ext.util.Observable, {
 
-        constructor: function(config) {
-            Hippo.ChannelManager.ExtLinkPickerFactory.Instance = this;
+    constructor: function (config) {
+      Hippo.ChannelManager.ExtLinkPickerFactory.Instance = this;
 
-            this.addEvents('pick', 'picked', 'select');
+      this.addEvents('pick', 'picked', 'select');
 
-            this.listeners = config.listeners;
+      this.listeners = config.listeners;
 
-            Hippo.ChannelManager.ExtLinkPickerFactory.superclass.constructor.call(this, config);
-        },
+      Hippo.ChannelManager.ExtLinkPickerFactory.superclass.constructor.call(this, config);
+    },
 
-        openPicker: function(currentValue, pickerConfig, cb) {
-            var config = JSON.parse(JSON.stringify(pickerConfig));
-            config.rootPath = encodeURI(pickerConfig.rootPath || '');
-            config.initialPath = encodeURI(pickerConfig.initialPath || '');
-            this.on('picked', cb, this, {single: true});
-            this.fireEvent('pick', currentValue, Ext.util.JSON.encode(config));
+    openPicker: function (currentValue, pickerConfig, cb) {
+      var config = JSON.parse(JSON.stringify(pickerConfig));
+      config.rootPath = encodeURI(pickerConfig.rootPath || '');
+      config.initialPath = encodeURI(pickerConfig.initialPath || '');
+      this.on('picked', cb, this, {single: true});
+      this.fireEvent('pick', currentValue, Ext.util.JSON.encode(config));
+    }
+  });
+
+  Hippo.ChannelManager.ExtLinkPicker = Ext.extend(Ext.form.TwinTriggerField, {
+
+    constructor: function (config) {
+      this.pickerConfig = config.pickerConfig;
+      this.defaultValue = config.defaultValue;
+      this.renderStripValue = config.renderStripValue;
+      this.renderTextFieldInvalid = false;
+      this.setValue(this.defaultValue);
+
+      Hippo.ChannelManager.ExtLinkPicker.superclass.constructor.call(this, config);
+    },
+
+    initComponent: function () {
+      Hippo.ChannelManager.ExtLinkPicker.superclass.initComponent.call(this);
+      this.on('valid', this.validateRenderTextField, this);
+      this.on('invalid', this.invalidateRenderTextField, this);
+      this.on('resize', this.resizeRenderTextField, this);
+      this.on('afterrender', this.updateClearButton, this);
+    },
+
+    editable: false,
+    trigger1Class: 'x-form-clear-trigger',
+    trigger2Class: 'x-form-search-trigger',
+
+    onTriggerClick: function () {
+      this.openPicker();
+    },
+
+    onTrigger1Click: function () {
+      this.picked(this.defaultValue);
+    },
+
+    onTrigger2Click: function () {
+      this.openPicker();
+    },
+
+    setDefaultValue: function (value) {
+      var oldDefaultValue = this.defaultValue;
+      this.defaultValue = value;
+      if (this.getValue() === oldDefaultValue) {
+        this.setValue(this.defaultValue);
+      }
+    },
+
+    openPicker: function () {
+      if (!this.disabled) {
+        Hippo.ChannelManager.ExtLinkPickerFactory.Instance.openPicker(
+          this.getValue(),
+          this.pickerConfig,
+          Ext.createDelegate(this.picked, this)
+        );
+      }
+    },
+
+    picked: function (value) {
+      var newValue = Ext.isDefined(value) ? value : '';
+      this.setValue(newValue);
+      this.updateClearButton();
+      this.fireEvent('select', this, newValue);
+    },
+
+    updateClearButton: function () {
+      var clearTrigger = this.getTrigger(0);
+      if (this.getValue() === this.defaultValue) {
+        clearTrigger.hide();
+      } else {
+        clearTrigger.show();
+      }
+    },
+
+    setValue: function (value) {
+      Hippo.ChannelManager.ExtLinkPicker.superclass.setValue.apply(this, arguments);
+      if (this.rendered) {
+        var valueToRender = this.getValue();
+        if (Ext.isDefined(this.renderStripValue)) {
+          valueToRender = valueToRender.replace(this.renderStripValue, '');
         }
-    });
+        this.renderTextField.dom.value = valueToRender;
+      }
+    },
 
-    Hippo.ChannelManager.ExtLinkPicker = Ext.extend(Ext.form.TwinTriggerField, {
+    resizeRenderTextField: function (width, height, options) {
+      if (this.renderTextField) {
+        this.renderTextField.dom.style.width = this.el.dom.style.width;
+      }
+    },
 
-        constructor: function(config) {
-            this.pickerConfig = config.pickerConfig;
-            this.defaultValue = config.defaultValue;
-            this.renderStripValue = config.renderStripValue;
-            this.renderTextFieldInvalid = false;
-            this.setValue(this.defaultValue);
+    validateRenderTextField: function () {
+      if (this.rendered) {
+        this.renderTextField.removeClass('x-form-invalid');
+      } else {
+        this.renderTextFieldInvalid = false;
+      }
+    },
 
-            Hippo.ChannelManager.ExtLinkPicker.superclass.constructor.call(this, config);
-        },
+    invalidateRenderTextField: function () {
+      if (this.rendered) {
+        this.renderTextField.addClass('x-form-invalid');
+      } else {
+        this.renderTextFieldInvalid = true;
+      }
+    },
 
-        initComponent: function() {
-            Hippo.ChannelManager.ExtLinkPicker.superclass.initComponent.call(this);
-            this.on('valid', this.validateRenderTextField, this);
-            this.on('invalid', this.invalidateRenderTextField, this);
-            this.on('resize', this.resizeRenderTextField, this);
-            this.on('afterrender', this.updateClearButton, this);
-        },
+    onRender: function () {
+      Hippo.ChannelManager.ExtLinkPicker.superclass.onRender.apply(this, arguments);
 
-        editable: false,
-        trigger1Class: 'x-form-clear-trigger',
-        trigger2Class: 'x-form-search-trigger',
+      this.el.dom.style.display = "none";
 
-        onTriggerClick: function() {
-            this.openPicker();
-        },
+      var value = this.getValue();
+      if (Ext.isDefined(this.renderStripValue)) {
+        value = value.replace(this.renderStripValue, '');
+      }
 
-        onTrigger1Click: function() {
-            this.picked(this.defaultValue);
-        },
+      this.renderTextField = Ext.DomHelper.insertBefore(this.el, {
+        tag: 'input',
+        type: 'text',
+        readonly: 'readonly',
+        value: value,
+        width: this.el.getWidth()
+      }, true);
+      this.renderTextField.addClass('x-form-text');
+      this.renderTextField.addClass('x-form-field');
+      if (this.renderTextFieldInvalid) {
+        this.renderTextField.addClass('x-form-invalid');
+      }
+    }
 
-        onTrigger2Click: function() {
-            this.openPicker();
-        },
+  });
 
-        setDefaultValue: function(value) {
-            var oldDefaultValue = this.defaultValue;
-            this.defaultValue = value;
-            if (this.getValue() === oldDefaultValue) {
-                this.setValue(this.defaultValue);
-            }
-        },
-
-        openPicker: function() {
-            if (!this.disabled) {
-                Hippo.ChannelManager.ExtLinkPickerFactory.Instance.openPicker(
-                        this.getValue(),
-                        this.pickerConfig,
-                        Ext.createDelegate(this.picked, this)
-                );
-            }
-        },
-
-        picked: function(value) {
-            var newValue = Ext.isDefined(value) ? value : '';
-            this.setValue(newValue);
-            this.updateClearButton();
-            this.fireEvent('select', this, newValue);
-        },
-
-        updateClearButton: function() {
-            var clearTrigger = this.getTrigger(0);
-            if (this.getValue() === this.defaultValue) {
-                clearTrigger.hide();
-            } else {
-                clearTrigger.show();
-            }
-        },
-
-        setValue: function(value) {
-            Hippo.ChannelManager.ExtLinkPicker.superclass.setValue.apply(this, arguments);
-            if (this.rendered) {
-                var valueToRender = this.getValue();
-                if (Ext.isDefined(this.renderStripValue)) {
-                    valueToRender = valueToRender.replace(this.renderStripValue, '');
-                }
-                this.renderTextField.dom.value = valueToRender;
-            }
-        },
-
-        resizeRenderTextField: function(width, height, options) {
-            if (this.renderTextField) {
-                this.renderTextField.dom.style.width = this.el.dom.style.width;
-            }
-        },
-
-        validateRenderTextField: function() {
-            if (this.rendered) {
-                this.renderTextField.removeClass('x-form-invalid');
-            } else {
-                this.renderTextFieldInvalid = false;
-            }
-        },
-
-        invalidateRenderTextField: function() {
-            if (this.rendered) {
-                this.renderTextField.addClass('x-form-invalid');
-            } else {
-                this.renderTextFieldInvalid = true;
-            }
-        },
-
-        onRender: function() {
-            Hippo.ChannelManager.ExtLinkPicker.superclass.onRender.apply(this, arguments);
-
-            this.el.dom.style.display = "none";
-
-            var value = this.getValue();
-            if (Ext.isDefined(this.renderStripValue)) {
-                value = value.replace(this.renderStripValue, '');
-            }
-
-            this.renderTextField = Ext.DomHelper.insertBefore(this.el, {
-                tag: 'input',
-                type: 'text',
-                readonly: 'readonly',
-                value: value,
-                width: this.el.getWidth()
-            }, true);
-            this.renderTextField.addClass('x-form-text');
-            this.renderTextField.addClass('x-form-field');
-            if (this.renderTextFieldInvalid) {
-                this.renderTextField.addClass('x-form-invalid');
-            }
-        }
-
-    });
-
-    Ext.reg('linkpicker', Hippo.ChannelManager.ExtLinkPicker);
+  Ext.reg('linkpicker', Hippo.ChannelManager.ExtLinkPicker);
 
 }());
