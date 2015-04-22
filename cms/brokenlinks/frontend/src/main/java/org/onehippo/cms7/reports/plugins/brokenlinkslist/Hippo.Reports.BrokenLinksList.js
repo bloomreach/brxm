c02bf76529858911cae6736c5739af620c0762eb
Mathijs den Burger <m.denburger@onehippo.com>
2015-04-22 08:22:56 +0000

    CMS7-9095: reindent to 2 spaces

--- cms/brokenlinks/frontend/src/main/java/org/onehippo/cms7/reports/plugins/brokenlinkslist/Hippo.Reports.BrokenLinksList.js
+++ cms/brokenlinks/frontend/src/main/java/org/onehippo/cms7/reports/plugins/brokenlinkslist/Hippo.Reports.BrokenLinksList.js
@@ -17,104 +17,106 @@ Ext.ns('Hippo.Reports');
 
 Hippo.Reports.BrokenLinksListPanel = Ext.extend(Hippo.Reports.Portlet, {
 
-    constructor: function(config) {
-        var self, grid;
+  constructor: function (config) {
+    var self, grid;
 
-        self = this;
+    self = this;
 
-        this.store = config.store;
-        this.pageSize = config.pageSize;
-        this.paging = config.paging;
-        this.pagingText = config.resources['documents-paging'];
-        this.noDataText = config.noDataText;
-        this.updateText = config.updateText;
-        this.autoExpandColumn = config.autoExpandColumn;
+    this.store = config.store;
+    this.pageSize = config.pageSize;
+    this.paging = config.paging;
+    this.pagingText = config.resources['documents-paging'];
+    this.noDataText = config.noDataText;
+    this.updateText = config.updateText;
+    this.autoExpandColumn = config.autoExpandColumn;
 
-        this.columns = config.columns;
+    this.columns = config.columns;
 
-        grid = new Ext.grid.GridPanel({
-            store: self.store,
-            colModel: new Ext.grid.ColumnModel({
-                defaults: {
-                    menuDisabled: true,
-                    sortable: false
-                },
-                columns: self.columns
-            }),
-            loadMask: false,
-            autoExpandColumn: self.autoExpandColumn,
-            border: false,
-            disableSelection: true,
-            viewConfig: {
-                forceFit: true
-            },
-            bbar: self.paging ? new Ext.PagingToolbar({
-                pageSize: self.pageSize,
-                store: self.store,
-                displayInfo: true,
-                displayMsg: self.pagingText,
-                emptyMsg: '',
-                afterPageText: '',
-                listeners: {
-                    afterrender: function(bbar) {
-                        bbar.last.hideParent = true;
-                        bbar.last.hide();
-                        bbar.refresh.hideParent = true;
-                        bbar.refresh.hide();
-                    }
-                }
-            }) : null,
-            tbar:[{
-                xtype: 'tbtext',
-                text: self.updateText
-            }],
-            listeners: {
-                cellclick: function(grid, rowIndex, columnIndex, event) {
-                    var record = grid.getStore().getAt(rowIndex),
-                        columnId = grid.getColumnModel().getColumnId(columnIndex);
-
-                    if (columnId !== 'brokenlinksLinks') {
-                        self.fireEvent('documentSelected', record.data.path);
-                    }
-                }
-            }
-        });
-
-        config = Ext.apply(config, {
-            bodyCssClass: 'hippo-reports-broken-links',
-            items:[ grid ]
-        });
-
-        Hippo.Reports.BrokenLinksListPanel.superclass.constructor.call(this, config);
-
-    },
-
-    loadStore: function() {
-        this.store.load({
-            params: {
-                start: 0,
-                limit: this.pageSize
-            }
-        });
-    },
-
-    checkNoData: function(component) {
-        if (this.store.getTotalCount() === 0) {
-            this.showMessage(this.noDataText+"\n"+this.updateText);
+    grid = new Ext.grid.GridPanel({
+      store: self.store,
+      colModel: new Ext.grid.ColumnModel({
+        defaults: {
+          menuDisabled: true,
+          sortable: false
+        },
+        columns: self.columns
+      }),
+      loadMask: false,
+      autoExpandColumn: self.autoExpandColumn,
+      border: false,
+      disableSelection: true,
+      viewConfig: {
+        forceFit: true
+      },
+      bbar: self.paging ? new Ext.PagingToolbar({
+        pageSize: self.pageSize,
+        store: self.store,
+        displayInfo: true,
+        displayMsg: self.pagingText,
+        emptyMsg: '',
+        afterPageText: '',
+        listeners: {
+          afterrender: function (bbar) {
+            bbar.last.hideParent = true;
+            bbar.last.hide();
+            bbar.refresh.hideParent = true;
+            bbar.refresh.hide();
+          }
         }
-    },
+      }) : null,
+      tbar: [
+        {
+          xtype: 'tbtext',
+          text: self.updateText
+        }
+      ],
+      listeners: {
+        cellclick: function (grid, rowIndex, columnIndex) {
+          var record = grid.getStore().getAt(rowIndex),
+            columnId = grid.getColumnModel().getColumnId(columnIndex);
 
-    initComponent: function() {
-        Hippo.Reports.BrokenLinksListPanel.superclass.initComponent.call(this);
-        this.store.on('load', this.checkNoData, this);
-        Hippo.Reports.RefreshObservableInstance.addListener("refresh", this.loadStore, this);
-        this.loadStore();
-    },
+          if (columnId !== 'brokenlinksLinks') {
+            self.fireEvent('documentSelected', record.data.path);
+          }
+        }
+      }
+    });
 
-    destroy: function() {
-        Hippo.Reports.BrokenLinksListPanel.superclass.destroy.call(this);
-        Hippo.Reports.RefreshObservableInstance.removeListener("refresh", this.loadStore, this);
+    config = Ext.apply(config, {
+      bodyCssClass: 'hippo-reports-broken-links',
+      items: [grid]
+    });
+
+    Hippo.Reports.BrokenLinksListPanel.superclass.constructor.call(this, config);
+
+  },
+
+  loadStore: function () {
+    this.store.load({
+      params: {
+        start: 0,
+        limit: this.pageSize
+      }
+    });
+  },
+
+  checkNoData: function () {
+    if (this.store.getTotalCount() === 0) {
+      this.showMessage(this.noDataText + "\n" + this.updateText);
     }
+  },
+
+  initComponent: function () {
+    Hippo.Reports.BrokenLinksListPanel.superclass.initComponent.call(this);
+    this.store.on('load', this.checkNoData, this);
+    Hippo.Reports.RefreshObservableInstance.addListener("refresh", this.loadStore, this);
+    this.loadStore();
+  },
+
+  destroy: function () {
+    Hippo.Reports.BrokenLinksListPanel.superclass.destroy.call(this);
+    Hippo.Reports.RefreshObservableInstance.removeListener("refresh", this.loadStore, this);
+  }
 
 });
 
