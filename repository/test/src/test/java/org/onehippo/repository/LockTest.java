81ca6a4451c266433b7a0266a99f3ea008830039
Unico Hommes <u.hommes@onehippo.com>
2015-04-29 14:00:12 +0000

    REPO-1 move LockTest to test module to run it during the integration phase

--- repository/test/src/test/java/org/onehippo/repository/LockTest.java
+++ repository/test/src/test/java/org/onehippo/repository/LockTest.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.hippoecm.repository;
+package org.onehippo.repository;
 
 import javax.jcr.Node;
 import javax.jcr.Session;
