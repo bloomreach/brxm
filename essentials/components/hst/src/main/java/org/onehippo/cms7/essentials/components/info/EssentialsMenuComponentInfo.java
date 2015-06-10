630e3131f8d633500b2ca15d749e6d69a3436eee
Tobias Jeger <t.jeger@onehippo.com>
2015-06-10 09:21:32 +0000

    ESSENTIALS-703: Remove unused component parameters and update library screenshot.

--- essentials/components/hst/src/main/java/org/onehippo/cms7/essentials/components/info/EssentialsMenuComponentInfo.java
+++ essentials/components/hst/src/main/java/org/onehippo/cms7/essentials/components/info/EssentialsMenuComponentInfo.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014 Hippo B.V. (http://www.onehippo.com)
+ * Copyright 2014-2015 Hippo B.V. (http://www.onehippo.com)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,7 +16,6 @@
 
 package org.onehippo.cms7.essentials.components.info;
 
-import org.hippoecm.hst.core.parameters.DropDownList;
 import org.hippoecm.hst.core.parameters.Parameter;
 
 /**
@@ -24,20 +23,6 @@ import org.hippoecm.hst.core.parameters.Parameter;
  */
 public interface EssentialsMenuComponentInfo {
 
-
     @Parameter(name = "menu", required = true, displayName = "Menu name")
     String getSiteMenu();
-
-    @Parameter(name = "level", required = false, defaultValue = "1", displayName = "Menu display level (depth)", description = "How many menu levels will be displayed")
-    @DropDownList(value = {"1", "2", "3"})
-    String getLevel();
-
-    /**
-     * When multi menu levels are shown, do we show only selected menu item and it's children items or all of them
-     *
-     * @return true by default
-     */
-    @Parameter(name = "selectedMenu", required = false, defaultValue = "true", displayName = "Only show selected menu level")
-    Boolean getShowOnlySelectedLevel();
-
 }
