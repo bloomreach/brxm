5fad9317dbb2d2771b2c6aafc8fea10e82348149
Canh Ngo <c.ngo@onehippo.com>
2015-06-12 08:02:06 +0000

    ESSENTIALS-709: - replaced usages of the old interface EssentialsDocumentListComponentInfo to the new renamed interface EssentialsListComponentInfo  - removed unnecessary comments

--- essentials/components/hst/src/main/java/org/onehippo/cms7/essentials/components/info/EssentialsSearchComponentInfo.java
+++ essentials/components/hst/src/main/java/org/onehippo/cms7/essentials/components/info/EssentialsSearchComponentInfo.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014 Hippo B.V. (http://www.onehippo.com)
+ * Copyright 2015 Hippo B.V. (http://www.onehippo.com)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,10 +18,7 @@ package org.onehippo.cms7.essentials.components.info;
 
 import org.hippoecm.hst.core.parameters.Parameter;
 
-/**
- * @version "$Id$"
- */
-public interface EssentialsSearchComponentInfo extends EssentialsDocumentListComponentInfo {
+public interface EssentialsSearchComponentInfo extends EssentialsListComponentInfo {
 
     // override default to "on"
     @Override
