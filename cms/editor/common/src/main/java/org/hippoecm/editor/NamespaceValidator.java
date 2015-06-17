66e2e2cbf0a8540127636e20928229df5f100e09
Mathijs den Burger <m.denburger@onehippo.com>
2015-06-17 12:27:21 +0000

    CMS7-9381: refactoring: share logic between validate methods

--- cms/editor/common/src/main/java/org/hippoecm/editor/NamespaceValidator.java
+++ cms/editor/common/src/main/java/org/hippoecm/editor/NamespaceValidator.java
@@ -26,34 +26,39 @@ public class NamespaceValidator {
     private static final Pattern NAME_PATTERN = Pattern.compile("^[A-Za-z]+$");
     private static final Pattern URL_PATTERN = Pattern.compile("^http:.*/[0-9].[0-9]$");
 
+    private enum ValidationResult { OK, ERROR_EMPTY, ERROR_INVALID }
+
     private NamespaceValidator() {
     }
 
     public static void checkName(String name) throws Exception {
-        if (name == null || "".equals(name)) {
-            throw new Exception("No name specified");
-        }
-        if (!NAME_PATTERN.matcher(name).matches()) {
-            throw new Exception("Invalid name; only alphabetic characters allowed in lower- or uppercase");
+        switch (validateName(name)) {
+            case ERROR_EMPTY:
+                throw new Exception("No name specified");
+            case ERROR_INVALID:
+                throw new Exception("Invalid name; only alphabetic characters allowed in lower- or uppercase");
         }
     }
 
     public static void checkURI(String name) throws Exception {
-        if (name == null) {
-            throw new Exception("No URI specified");
-        }
-        if (!URL_PATTERN.matcher(name).matches()) {
-            throw new Exception("Invalid URL; ");
+        switch (validateUrl(name)) {
+            case ERROR_EMPTY:
+                throw new Exception("No URI specified");
+            case ERROR_INVALID:
+                throw new Exception("Invalid URL; ");
         }
     }
 
     public static IValidator<String> createNameValidator() {
         return validatable -> {
             final String name = validatable.getValue();
-            if (StringUtils.isBlank(name)) {
-                validatable.error(message -> new ClassResourceModel("namespace.name.empty", NamespaceValidator.class).getObject());
-            } else if (!NAME_PATTERN.matcher(name).matches()) {
-                validatable.error(message -> new ClassResourceModel("namespace.name.invalid", NamespaceValidator.class).getObject());
+            switch (validateName(name)) {
+                case ERROR_EMPTY:
+                    validatable.error(message -> translate("namespace.name.empty"));
+                    break;
+                case ERROR_INVALID:
+                    validatable.error(message -> translate("namespace.name.invalid"));
+                    break;
             }
         };
     }
@@ -61,11 +66,37 @@ public class NamespaceValidator {
     public static IValidator<String> createUrlValidator() {
         return validatable -> {
             final String url = validatable.getValue();
-            if (StringUtils.isBlank(url)) {
-                validatable.error(message -> new ClassResourceModel("namespace.url.empty", NamespaceValidator.class).getObject());
-            } else if (!URL_PATTERN.matcher(url).matches()) {
-                validatable.error(message -> new ClassResourceModel("namespace.url.invalid", NamespaceValidator.class).getObject());
+            switch (validateUrl(url)) {
+                case ERROR_EMPTY:
+                    validatable.error(message -> translate("namespace.url.empty"));
+                    break;
+                case ERROR_INVALID:
+                    validatable.error(message -> translate("namespace.url.invalid"));
+                    break;
             }
         };
     }
+
+    private static ValidationResult validateName(final String name) {
+        if (StringUtils.isBlank(name)) {
+            return ValidationResult.ERROR_EMPTY;
+        } else if (!NAME_PATTERN.matcher(name).matches()) {
+            return ValidationResult.ERROR_INVALID;
+        }
+        return ValidationResult.OK;
+    }
+
+    private static ValidationResult validateUrl(final String url) {
+        if (StringUtils.isBlank(url)) {
+            return ValidationResult.ERROR_EMPTY;
+        } else if (!URL_PATTERN.matcher(url).matches()) {
+            return ValidationResult.ERROR_INVALID;
+        }
+        return ValidationResult.OK;
+    }
+
+    private static String translate(final String key) {
+        return new ClassResourceModel(key, NamespaceValidator.class).getObject();
+    }
+
 }
