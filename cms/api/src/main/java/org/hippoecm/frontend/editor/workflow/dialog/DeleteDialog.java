d8bfc1358faa5d5a94621d65c0706208cb2ce53f
Arthur Bogaart <a.bogaart@onehippo.com>
2015-06-18 15:09:05 +0000

    CMS7-9286: Restyled delete dialog - Size depends on number of referred documents

--- cms/api/src/main/java/org/hippoecm/frontend/editor/workflow/dialog/DeleteDialog.java
+++ cms/api/src/main/java/org/hippoecm/frontend/editor/workflow/dialog/DeleteDialog.java
@@ -1,12 +1,12 @@
 /*
  *  Copyright 2008-2015 Hippo B.V. (http://www.onehippo.com)
- * 
+ *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
  *  You may obtain a copy of the License at
- * 
+ *
  *       http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  *  Unless required by applicable law or agreed to in writing, software
  *  distributed under the License is distributed on an "AS IS" BASIS,
  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,13 +15,13 @@
  */
 package org.hippoecm.frontend.editor.workflow.dialog;
 
+import javax.jcr.Node;
 import javax.jcr.RepositoryException;
 
 import org.apache.wicket.MarkupContainer;
 import org.apache.wicket.WicketRuntimeException;
 import org.apache.wicket.model.IModel;
-import org.apache.wicket.util.value.IValueMap;
-import org.hippoecm.addon.workflow.AbstractWorkflowDialog;
+import org.hippoecm.addon.workflow.AbstractWorkflowDialogRestyling;
 import org.hippoecm.addon.workflow.IWorkflowInvoker;
 import org.hippoecm.addon.workflow.WorkflowDescriptorModel;
 import org.hippoecm.frontend.dialog.DialogConstants;
@@ -30,45 +30,35 @@ import org.hippoecm.frontend.model.JcrNodeModel;
 import org.hippoecm.frontend.plugins.standards.list.resolvers.CssClass;
 import org.hippoecm.frontend.service.IEditorManager;
 import org.hippoecm.repository.api.WorkflowDescriptor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-public class DeleteDialog extends AbstractWorkflowDialog<WorkflowDescriptor> {
+public class DeleteDialog extends AbstractWorkflowDialogRestyling<WorkflowDescriptor> {
 
-    static final Logger log = LoggerFactory.getLogger(DeleteDialog.class);
+    public DeleteDialog(IModel<String> title, WorkflowDescriptorModel model, IModel<String> notification,
+                        IWorkflowInvoker invoker, IEditorManager editorMgr) {
+        super(invoker, model, title);
 
-    private IModel<String> title;
+        setNotification(notification);
+        setFocusOnCancel();
 
-    public DeleteDialog(IModel<String> title, WorkflowDescriptorModel wdm, IModel<String> message, IWorkflowInvoker action, IEditorManager editorMgr) {
-        super(wdm, message, action);
+        add(CssClass.append("hippo-delete-dialog"));
 
-        this.title = title;
-
+        final Node node;
         try {
-            ReferringDocumentsProvider provider = new ReferringDocumentsProvider(new JcrNodeModel(wdm.getNode()), true);
-        MarkupContainer rdv = new ReferringDocumentsView("links", provider, editorMgr) {
+            node = model.getNode();
+        } catch (RepositoryException e) {
+            throw new WicketRuntimeException("No document node present", e);
+        }
+
+        final JcrNodeModel nodeModel = new JcrNodeModel(node);
+        final ReferringDocumentsProvider provider = new ReferringDocumentsProvider(nodeModel, true);
+        final MarkupContainer documentsView = new ReferringDocumentsView("links", provider, editorMgr) {
             @Override
             public int getPageSize() {
                 return 5;
             }
         };
-        add(rdv);
-        } catch (RepositoryException e) {
-            throw new WicketRuntimeException("No document node present", e);
-        }
-        add(CssClass.append("hippo-delete-dialog"));
+        add(documentsView);
 
-        setFocusOnCancel();
+        setSize(provider.size() > 0 ? DialogConstants.LARGE_AUTO : DialogConstants.MEDIUM_AUTO);
     }
-
-    @Override
-    public IValueMap getProperties() {
-        return DialogConstants.MEDIUM;
-    }
-
-    @Override
-    public IModel<String> getTitle() {
-        return title;
-    }
-
 }
