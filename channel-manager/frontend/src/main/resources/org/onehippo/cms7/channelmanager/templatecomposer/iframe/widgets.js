5abccde736e88f02c7f0c63162ac432ef2763aab
Joeri de Gooijer <j.degooijer@onehippo.com>
2015-06-24 13:18:24 +0000

    Revert: CHANNELMGR-179: open 'edit menu' dialog when 'edit menu' button behind container item overlay is 'clicked'   Reverted because there is a much simpler fix using z-index

--- channel-manager/frontend/src/main/resources/org/onehippo/cms7/channelmanager/templatecomposer/iframe/widgets.js
+++ channel-manager/frontend/src/main/resources/org/onehippo/cms7/channelmanager/templatecomposer/iframe/widgets.js
@@ -126,8 +126,8 @@
       }, function () {
         self.onMouseOut(this);
       });
-      overlay.click(function (event) {
-        self.onClick(event);
+      overlay.click(function () {
+        self.onClick();
       });
       this.overlay = overlay;
       this._syncOverlay();
@@ -154,7 +154,7 @@
       return this.overlay;
     },
 
-    onClick: function (event) {
+    onClick: function () {
     },
 
     onMouseOver: function (element) {
@@ -840,47 +840,17 @@
       return $(this.element);
     },
 
-    getElementFromPointBehindOverlay: function(pageX, pageY) {
-      var frame, viewportX, viewportY, pageOverlay, element;
-
-      frame = $(window);
-      viewportX = pageX - frame.scrollLeft();
-      viewportY = pageY - frame.scrollTop();
-
-      pageOverlay = $('.hst-overlay-root');
-      pageOverlay.hide();
-      element = document.elementFromPoint(viewportX, viewportY);
-      pageOverlay.show();
-
-      return element;
-    },
-
-    getEditMenuButton: function(pageX, pageY) {
-      var clickedElement = this.getElementFromPointBehindOverlay(pageX, pageY),
-        editMenuButton = $(clickedElement).closest('a.hst-cmseditmenu');
-
-      if (editMenuButton.length) {
-        return editMenuButton[0];
-      }
-      return null;
-    },
-
-    onClick: function (event) {
-      var id, editMenuButton, containerDisabled;
+    onClick: function () {
+      var id, containerDisabled;
       if (this.isTemporary) {
         iframeToHost.publish('refresh');
       } else {
         id = this.element.getAttribute('id');
-        editMenuButton = this.getEditMenuButton(event.pageX, event.pageY);
-        if (editMenuButton) {
-          editMenuButton.click();
-        } else {
-          containerDisabled = Hippo.Util.getBoolean(this.el.attr(HST.ATTR.HST_CONTAINER_DISABLED));
-          iframeToHost.publish('onclick', {
-            elementId: id,
-            containerDisabled: containerDisabled
-          });
-        }
+        containerDisabled = Hippo.Util.getBoolean(this.el.attr(HST.ATTR.HST_CONTAINER_DISABLED));
+        iframeToHost.publish('onclick', {
+          elementId: id,
+          containerDisabled: containerDisabled
+        });
       }
     },
 
