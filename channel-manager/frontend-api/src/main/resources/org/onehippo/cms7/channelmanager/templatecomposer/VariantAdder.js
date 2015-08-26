3e8cbe4b6c6ebcbd11f0af8597864b1cf0b11f91
Mathijs den Burger <m.denburger@onehippo.com>
2015-08-26 14:31:01 +0000

    CHANNELMGR-278: change indentation to 2 spaces

--- channel-manager/frontend-api/src/main/resources/org/onehippo/cms7/channelmanager/templatecomposer/VariantAdder.js
+++ channel-manager/frontend-api/src/main/resources/org/onehippo/cms7/channelmanager/templatecomposer/VariantAdder.js
@@ -13,74 +13,74 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-(function() {
-    "use strict";
+(function () {
+  "use strict";
 
-    Ext.namespace('Hippo.ChannelManager.TemplateComposer');
+  Ext.namespace('Hippo.ChannelManager.TemplateComposer');
 
-    Hippo.ChannelManager.TemplateComposer.VariantAdder = Ext.extend(Ext.FormPanel, {
+  Hippo.ChannelManager.TemplateComposer.VariantAdder = Ext.extend(Ext.FormPanel, {
 
-        autoHeight: true,
-        autoScroll: true,
-        border: false,
-        defaults: {
-            anchor: '100%'
+    autoHeight: true,
+    autoScroll: true,
+    border: false,
+    defaults: {
+      anchor: '100%'
+    },
+    labelSeparator: '',
+    labelWidth: 100,
+    padding: 10,
+
+    composerRestMountUrl: null,
+    componentId: null,
+
+    constructor: function (config) {
+      this.title = '<span style="font-size: 140%;">' + config.title + '</span>';
+      this.composerRestMountUrl = config.composerRestMountUrl;
+      this.componentId = config.componentId;
+      this.variantsUuid = config.variantsUuid;
+      this.getCurrentVariant = config.getCurrentVariant;
+
+      this.globalVariantsStore = new Hippo.ChannelManager.TemplateComposer.GlobalVariantsStore({
+        composerRestMountUrl: this.composerRestMountUrl,
+        locale: config.locale,
+        skipIds: config.skipVariantIds,
+        variantsUuid: this.variantsUuid
+      });
+
+      Hippo.ChannelManager.TemplateComposer.VariantAdder.superclass.constructor.call(this, config);
+    },
+
+    initComponent: function () {
+      Hippo.ChannelManager.TemplateComposer.VariantAdder.superclass.initComponent.apply(this, arguments);
+      this.addEvents('beforeactive', 'save');
+    },
+
+    getGlobalVariantsStore: function () {
+      return this.globalVariantsStore;
+    },
+
+    saveVariant: function (variant) {
+      Ext.Ajax.request({
+        method: 'POST',
+        url: this.composerRestMountUrl + '/' + this.componentId + './' + encodeURIComponent(variant) + '?FORCE_CLIENT_HOST=true',
+        success: function () {
+          this.fireEvent('save', variant);
         },
-        labelSeparator: '',
-        labelWidth: 100,
-        padding: 10,
+        scope: this
+      });
+    },
 
-        composerRestMountUrl: null,
-        componentId: null,
+    copyVariant: function (existingVariantId, newVariant) {
+      this.fireEvent('copy', existingVariantId, newVariant);
+    },
 
-        constructor: function(config) {
-            this.title = '<span style="font-size: 140%;">' + config.title + '</span>';
-            this.composerRestMountUrl = config.composerRestMountUrl;
-            this.componentId = config.componentId;
-            this.variantsUuid = config.variantsUuid;
-            this.getCurrentVariant = config.getCurrentVariant;
-
-            this.globalVariantsStore = new Hippo.ChannelManager.TemplateComposer.GlobalVariantsStore({
-                composerRestMountUrl: this.composerRestMountUrl,
-                locale: config.locale,
-                skipIds: config.skipVariantIds,
-                variantsUuid: this.variantsUuid
-            });
-
-            Hippo.ChannelManager.TemplateComposer.VariantAdder.superclass.constructor.call(this, config);
-        },
-
-        initComponent: function() {
-            Hippo.ChannelManager.TemplateComposer.VariantAdder.superclass.initComponent.apply(this, arguments);
-            this.addEvents('beforeactive', 'save');
-        },
-
-        getGlobalVariantsStore: function() {
-            return this.globalVariantsStore;
-        },
-
-        saveVariant: function(variant) {
-            Ext.Ajax.request({
-                method: 'POST',
-                url: this.composerRestMountUrl + '/' + this.componentId + './' + encodeURIComponent(variant) + '?FORCE_CLIENT_HOST=true',
-                success: function() {
-                    this.fireEvent('save', variant);
-                },
-                scope: this
-            });
-        },
-
-        copyVariant: function(existingVariantId, newVariant) {
-            this.fireEvent('copy', existingVariantId, newVariant);
-        },
-
-        load: function() {
-            return new Hippo.Future(function(success, fail) {
-                this.globalVariantsStore.on('load', success, {single: true});
-                this.globalVariantsStore.on('exception', fail, {single: true});
-                this.globalVariantsStore.load();
-            }.createDelegate(this));
-        }
-    });
+    load: function () {
+      return new Hippo.Future(function (success, fail) {
+        this.globalVariantsStore.on('load', success, {single: true});
+        this.globalVariantsStore.on('exception', fail, {single: true});
+        this.globalVariantsStore.load();
+      }.createDelegate(this));
+    }
+  });
 
 }());
