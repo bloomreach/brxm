b7868600f67bbe91a355f4f90fb30156ac0cd4d1
Woonsan Ko <w.ko@onehippo.com>
2015-09-04 11:50:02 +0000

    HSTTWO-3392 moving HDC API into commons to use it even in non-hst applications

---
+++ commons/src/main/java/org/hippoecm/hst/diagnosis/HDC.java
@@ -0,0 +1,71 @@
+/**
+ * Copyright 2012-2015 Hippo B.V. (http://www.onehippo.com)
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.hippoecm.hst.diagnosis;
+
+
+/**
+ * Hierarchical Diagnostic Context
+ */
+public class HDC {
+
+    public static final Task NOOP_TASK = new NOOPTaskImpl();
+
+    private static ThreadLocal<Task> tlRootTask = new ThreadLocal<Task>();
+    private static ThreadLocal<Task> tlCurrentTask = new ThreadLocal<Task>();
+
+    private HDC() {
+    }
+
+    public static Task start(String name) {
+        Task rootTask = tlRootTask.get();
+
+        if (rootTask != null) {
+            throw new IllegalStateException("The root task was already started.");
+        }
+
+        rootTask = new DefaultTaskImpl(null, name);
+        tlRootTask.set(rootTask);
+        return rootTask;
+    }
+
+    public static boolean isStarted() {
+        return (tlRootTask.get() != null);
+    }
+
+    public static Task getRootTask() {
+        Task rootTask = tlRootTask.get();
+        return (rootTask != null ? rootTask : NOOP_TASK);
+    }
+
+    public static Task getCurrentTask() {
+        Task current = tlCurrentTask.get();
+
+        if (current != null) {
+            return current;
+        }
+
+        return getRootTask();
+    }
+
+    public static void setCurrentTask(Task currentTask) {
+        tlCurrentTask.set(currentTask);
+    }
+
+    public static void cleanUp() {
+        tlCurrentTask.remove();
+        tlRootTask.remove();
+    }
+}
---
+++ commons/src/test/java/org/hippoecm/hst/diagnosis/TestHDC.java
@@ -0,0 +1,235 @@
+/**
+ * Copyright 2012-2015 Hippo B.V. (http://www.onehippo.com)
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.hippoecm.hst.diagnosis;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.hippoecm.hst.diagnosis.HDC;
+import org.hippoecm.hst.diagnosis.Task;
+import org.hippoecm.hst.diagnosis.TaskLogFormatUtils;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * TestHDC
+ */
+public class TestHDC {
+
+    private static Logger log = LoggerFactory.getLogger(TestHDC.class);
+
+    private Valve1 valve1 = new Valve1();
+    private Valve2 valve2 = new Valve2();
+    private Component1 comp1 = new Component1();
+    private Component2 comp2 = new Component2();
+    private Query query1 = new Query();
+    private Query query2 = new Query();
+
+    @Test
+    public void testDefaultExample() throws Exception {
+        // first, the HST container will start the root task first somewhere. e.g., HstFilter or InitializationValve
+        Task rootTask = HDC.start("request-processing");
+
+        // when invoking each valve, HST container can start a subtask
+        {
+            Task valveTask = HDC.getCurrentTask().startSubtask("valve1");
+            valve1.execute();
+            // if the container started a subtask, then it should stop the task.
+            // in reality, it should use try ~ finally to guarantee this call.
+            valveTask.stop();
+        }
+
+        {
+            Task valveTask = HDC.getCurrentTask().startSubtask("valve2");
+            valve2.execute();
+            valveTask.stop();
+        }
+
+        // also the container will stop the root task.
+        rootTask.stop();
+
+        // all the task execution information can be collected and reported later (maybe in another valve before cleanupValve)
+        final String logSummary = logSummary();
+        assertTrue(logSummary.contains("valve2"));
+
+        final String logSummaryWithDepth0 = logSummary(0);
+        assertFalse(logSummaryWithDepth0.contains("valve2"));
+
+        final String logSummaryWithDepthMinus1 = logSummary(-1);
+        assertTrue(logSummaryWithDepthMinus1.contains("valve2"));
+
+        final String logSummaryWithDepth1 = logSummary(1);
+        assertTrue(logSummaryWithDepth1.contains("valve2"));
+
+        // clean up all the stored thread context information..
+        HDC.cleanUp();
+    }
+
+    @Test
+    public void testNOOPExample() throws Exception {
+
+        // when invoking each valve, HST container can start a subtask
+        {
+            Task valveTask = HDC.getCurrentTask().startSubtask("valve1");
+            valve1.execute();
+            // if the container started a subtask, then it should stop the task.
+            // in reality, it should use try ~ finally to guarantee this call.
+            valveTask.stop();
+        }
+
+        {
+            Task valveTask = HDC.getCurrentTask().startSubtask("valve2");
+            valve2.execute();
+            valveTask.stop();
+        }
+
+        // all the task execution information can be collected and reported later (maybe in another valve before cleanupValve)
+        final String logSummary = logSummary();
+
+        assertFalse(logSummary.contains("valve2"));
+
+        // clean up all the stored thread context information..
+        HDC.cleanUp();
+    }
+
+    @Test
+    public void testRootTaskOnlyExample() throws Exception {
+        // first, the HST container will start the root task first somewhere. e.g., HstFilter or InitializationValve
+        Task rootTask = HDC.start("request-processing");
+        HDC.setCurrentTask(HDC.NOOP_TASK);
+
+        // when invoking each valve, HST container can start a subtask
+        {
+            Task valveTask = HDC.getCurrentTask().startSubtask("valve1");
+            valve1.execute();
+            // if the container started a subtask, then it should stop the task.
+            // in reality, it should use try ~ finally to guarantee this call.
+            valveTask.stop();
+        }
+
+        {
+            Task valveTask = HDC.getCurrentTask().startSubtask("valve2");
+            valve2.execute();
+            valveTask.stop();
+        }
+
+        // also the container will stop the root task.
+        rootTask.stop();
+
+        // all the task execution information can be collected and reported later (maybe in another valve before cleanupValve)
+        final String logSummary = logSummary();
+
+        assertFalse(logSummary.contains("valve2"));
+
+        // clean up all the stored thread context information..
+        HDC.cleanUp();
+    }
+
+
+    private String logSummary() {
+        Task rootTask = HDC.getRootTask();
+        return TaskLogFormatUtils.getTaskLog(rootTask);
+    }
+
+    private String logSummary(final int depth) {
+        Task rootTask = HDC.getRootTask();
+        return TaskLogFormatUtils.getTaskLog(rootTask, depth);
+    }
+
+
+    private void sleepRandom(long max) {
+        try {
+            Thread.sleep(Math.abs(Math.round(Math.random() * max)));
+        } catch (InterruptedException e) {
+        }
+    }
+
+    class Valve1 {
+        public void execute() {
+            sleepRandom(10);
+
+            // A valve can also start a subtask from its current context task.
+            Task compTask = HDC.getCurrentTask().startSubtask("comp1");
+            Task compTaskA = HDC.getCurrentTask().startSubtask("comp1A");
+            comp1.execute();
+            compTaskA.stop();
+            comp1.execute();
+            compTask.stop();
+
+
+
+            sleepRandom(10);
+
+            compTask = HDC.getCurrentTask().startSubtask("comp2");
+            comp2.execute();
+            compTask.stop();
+
+            sleepRandom(10);
+        }
+    }
+
+    class Valve2 {
+        public void execute() {
+            sleepRandom(10);
+
+            Task compTask = HDC.getCurrentTask().startSubtask("comp1");
+            comp1.execute();
+            compTask.stop();
+
+            sleepRandom(10);
+
+            compTask = HDC.getCurrentTask().startSubtask("comp2");
+            comp2.execute();
+            compTask.stop();
+
+            sleepRandom(10);
+        }
+    }
+
+    // Normally component developers do not need to manage tasks by themselves
+    // because the task context for each component will be provided by the container.
+    // e.g., by HstComponentInvoker or an AOP for HstComponentInvoker, etc.
+    // so, the following component examples doesn't contain task management codes.
+    class Component1 {
+        public void execute() {
+            sleepRandom(10);
+            query1.execute("//element[jcr:contains(., 'hippo')]");
+            sleepRandom(10);
+        }
+    }
+
+    class Component2 {
+        public void execute() {
+            sleepRandom(10);
+            query2.execute("//element[jcr:contains(., 'cms')]");
+            sleepRandom(10);
+        }
+    }
+
+    // HST Content Beans may manage its task context. e.g., HstQuery, HippoBeansIterator, etc.
+    class Query {
+        public void execute(String statement) {
+            sleepRandom(10);
+            Task queryTask = HDC.getCurrentTask().startSubtask("query");
+            sleepRandom(10);
+            queryTask.setAttribute("statement", statement);
+            queryTask.stop();
+            sleepRandom(10);
+        }
+    }
+
+}
