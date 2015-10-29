f80e20b5f20d9f25d349d70c0b1c0bbe22c193a0
Arthur Bogaart <a.bogaart@onehippo.com>
2015-10-29 15:45:03 +0000

    CMS-9053: Moved labels to AbstractDocumentWorkflowPlugin(_locale).properties and re-use existing titleModel from WorkflowDialog constructor (just like the title for PublishDialog and DepublishDialog)

--- cms/workflow/frontend/src/main/java/org/hippoecm/frontend/plugins/reviewedactions/dialogs/SchedulePublishDialog.java
+++ cms/workflow/frontend/src/main/java/org/hippoecm/frontend/plugins/reviewedactions/dialogs/SchedulePublishDialog.java
@@ -21,11 +21,9 @@ import javax.jcr.Node;
 
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.ResourceModel;
-import org.apache.wicket.model.StringResourceModel;
-import org.hippoecm.addon.workflow.WorkflowDialog;
 import org.hippoecm.addon.workflow.IWorkflowInvoker;
+import org.hippoecm.addon.workflow.WorkflowDialog;
 import org.hippoecm.frontend.dialog.Dialog;
-import org.hippoecm.frontend.i18n.model.NodeTranslator;
 import org.hippoecm.frontend.plugins.reviewedactions.UnpublishedReferenceNodeProvider;
 import org.hippoecm.frontend.plugins.reviewedactions.model.ReferenceProvider;
 import org.hippoecm.frontend.plugins.reviewedactions.model.UnpublishedReferenceProvider;
@@ -33,14 +31,11 @@ import org.hippoecm.frontend.service.IEditorManager;
 
 public class SchedulePublishDialog extends WorkflowDialog<Node> {
 
-    private final IModel<String> titleModel;
-
     public SchedulePublishDialog(final IWorkflowInvoker invoker, final IModel<Node> nodeModel,
-                                 final IModel<Date> dateModel, final IEditorManager editorMgr) {
-        super(invoker, nodeModel);
+                                 final IModel<Date> dateModel, final IModel<String> titleModel,
+                                 final IEditorManager editorMgr) {
+        super(invoker, nodeModel, titleModel);
 
-        final IModel<String> displayDocumentName = new NodeTranslator(nodeModel).getNodeName();
-        titleModel = new StringResourceModel("schedule-publish-title", this, null, displayDocumentName);
         setCssClass("hippo-workflow-dialog");
         setFocusOnCancel();
 
@@ -49,18 +44,5 @@ public class SchedulePublishDialog extends WorkflowDialog<Node> {
         add(new UnpublishedReferencesView("links", provider, editorMgr));
 
         addOrReplace(new DatePickerComponent(Dialog.BOTTOM_LEFT_ID, dateModel, new ResourceModel("schedule-publish-text")));
-    }
-
-    @Override
-    public IModel<String> getTitle() {
-        return titleModel;
-    }
-
-    @Override
-    protected void onDetach() {
-        if (titleModel != null) {
-            titleModel.detach();
-        }
-        super.onDetach();
     }
 }
