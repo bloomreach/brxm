4bc9cb58749529274b55036958555b8175699e1c
Joeri de Gooijer <j.degooijer@onehippo.com>
2015-12-15 17:48:26 +0000

    CHANNELMGR-332: Update to hippo-build tag 0.0.3 makes karma work on phantomjs

--- channel-manager/frontend-ng/karma.conf.js
+++ channel-manager/frontend-ng/karma.conf.js
@@ -2,30 +2,47 @@ var hippoBuild = require('hippo-build');
 var customConfig = require('./build.conf.js');
 var cfg = hippoBuild.buildConfig(customConfig);
 
-module.exports = function(config) {
+module.exports = function (config) {
   var options = {};
 
   options.basePath = '.';
-  options.frameworks = ['jasmine'];
+  options.frameworks = ['systemjs', 'jasmine'];
   options.files = [
-    cfg.bowerDir + 'angular/angular.js',
-    cfg.bowerDir + 'angular-ui-router/release/angular-ui-router.js',
-    cfg.bowerDir + 'angular-translate/angular-translate.js',
-    cfg.bowerDir + 'angular-translate-loader-static-files/angular-translate-loader-static-files.js',
-    cfg.bowerDir + 'angular-mocks/angular-mocks.js',
-    cfg.npmDir + 'babel-core/external-helpers.js',
-    cfg.npmDir + 'systemjs/dist/system-polyfills.js',
-    cfg.npmDir + 'systemjs/dist/system-register-only.js',
-    cfg.dist.indexScript,
     cfg.src.unitTests
   ];
+  options.systemjs = {
+    config: {
+      transpiler: 'babel',
+      defaultJSExtensions: true,
+      paths: {
+        'babel': cfg.npmDir + 'babel-core/browser.js',
+        'systemjs': cfg.npmDir + 'systemjs/dist/system.js',
+        'system-polyfills': cfg.npmDir + 'systemjs/dist/system-polyfills.js',
+        'es6-module-loader': cfg.npmDir + 'es6-module-loader/dist/es6-module-loader.js',
+        'phantomjs-polyfill': cfg.npmDir + 'phantomjs-polyfill/bind-polyfill.js'
+      }
+    },
+    includeFiles: [
+      cfg.bowerDir + 'angular/angular.js',
+      cfg.bowerDir + 'angular-ui-router/release/angular-ui-router.js',
+      cfg.bowerDir + 'angular-translate/angular-translate.js',
+      cfg.bowerDir + 'angular-translate-loader-static-files/angular-translate-loader-static-files.js',
+      cfg.bowerDir + 'angular-mocks/angular-mocks.js',
+      cfg.npmDir + 'babel-core/external-helpers.js',
+      cfg.npmDir + 'systemjs/dist/system-polyfills.js',
+      cfg.npmDir + 'systemjs/dist/system-register-only.js',
+      cfg.dist.indexScript
+    ]
+  };
   options.reporters = ['progress', 'coverage'];
   options.coverageReporter = {
-    reporters: [{
-      type: 'html'
-    }, {
-      type: 'text-summary'
-    }]
+    reporters: [
+      {
+        type: 'html'
+      }, {
+        type: 'text-summary'
+      }
+    ]
   };
   options.autoWatch = false;
   options.browsers = ['Chrome'];
