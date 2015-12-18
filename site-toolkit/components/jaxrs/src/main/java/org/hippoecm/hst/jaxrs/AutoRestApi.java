c42e402ad109bbf551bc3673d7992447d8c22064
Oscar Scholten <o.scholten@onehippo.com>
2015-12-18 14:13:49 +0000

    HSTTWO-3500: checking in initial implementation of document detail (not working yet)

--- site-toolkit/components/jaxrs/src/main/java/org/hippoecm/hst/jaxrs/AutoRestApi.java
+++ site-toolkit/components/jaxrs/src/main/java/org/hippoecm/hst/jaxrs/AutoRestApi.java
@@ -17,10 +17,12 @@
 package org.hippoecm.hst.jaxrs;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.UUID;
 
 import javax.jcr.ItemNotFoundException;
 import javax.jcr.Node;
+import javax.jcr.Property;
 import javax.jcr.RepositoryException;
 import javax.jcr.Session;
 import javax.ws.rs.GET;
@@ -64,6 +66,103 @@ public class AutoRestApi {
 
     public void setContext(Context context) {
         this.context = context;
+    }
+
+    final class Link {
+        public String url;
+    }
+
+    final class SearchResultItem {
+        public String name;
+        public String uuid;
+        public Link[] links;
+    }
+
+    final class SearchResult {
+        public long offset;
+        public long max;
+        public long count;
+        public boolean more;
+        public long total;
+        public SearchResultItem[] items;
+    }
+
+    final class Error {
+        public int status;
+        public String description;
+        Error(int status, String description) {
+            this.status = status;
+            this.description = description;
+        }
+    }
+
+    @GET
+    @Path("/content/documents")
+    public Response getDocuments() {
+        try {
+            SearchService searchService = getSearchService();
+            Query query = searchService.createQuery()
+                    .from("/content/documents")
+                    .ofType("myhippoproject:basedocument") // TODO change to blacklisting mechanism
+                    .returnParentNode()
+                    .orderBy(HippoStdPubWfNodeType.HIPPOSTDPUBWF_PUBLICATION_DATE)
+                    .descending();
+            QueryResult queryResult = searchService.search(query);
+
+            SearchResult returnValue = new SearchResult();
+            returnValue.offset = 0;
+            returnValue.max = 100;
+            returnValue.count = queryResult.getTotalHitCount();
+            returnValue.more = false;
+            returnValue.total = queryResult.getTotalHitCount();
+
+            int count = (int) queryResult.getTotalHitCount();
+            int current = 0;
+            returnValue.items = new SearchResultItem[count];
+            HitIterator iterator = queryResult.getHits();
+            while (iterator.hasNext()) {
+                Hit hit = iterator.nextHit();
+                returnValue.items[current] = new SearchResultItem();
+                String uuid = hit.getSearchDocument().getContentId().toIdentifier();
+                Node node = context.getSession().getNodeByIdentifier(uuid);
+                returnValue.items[current].name = node.getName();
+                returnValue.items[current].uuid = uuid;
+                returnValue.items[current].links = new Link[1];
+                returnValue.items[current].links[0] = new Link();
+                returnValue.items[current].links[0].url = uuid;
+                current++;
+            }
+
+            return Response.status(200).entity(returnValue).build();
+        } catch (RepositoryException e) {
+            return buildErrorResponse(500, e);
+        }
+    }
+
+    @GET
+    @Path("/content/documents/{uuid}")
+    public Response getDocumentsByUUID(@PathParam("uuid") String uuid) {
+        try {
+            Session session = context.getSession();
+
+            // check uuid validity, if not valid this throws an IllegalArgumentException
+            UUID.fromString(uuid);
+
+            // this could throw an ItemNotFoundException
+            Node node = session.getNodeByIdentifier(uuid);
+
+            HashMap<String, Object> hashMap = new HashMap<>();
+
+            nodeToHashMap(node, hashMap);
+
+            return Response.status(200).entity(hashMap).build();
+        } catch (IllegalArgumentException iae) {
+            return buildErrorResponse(400, iae, "The string '" + uuid + "' is not a valid UUID.");
+        } catch (ItemNotFoundException infe) {
+            return buildErrorResponse(404, infe);
+        } catch (RepositoryException re) {
+            return buildErrorResponse(500, re);
+        }
     }
 
     /* TODO
@@ -110,101 +209,33 @@ public class AutoRestApi {
         return searchServiceFactory.createSearchService(context.getSession());
     }
 
-    final class Link {
-        public String url;
-    }
+    @SuppressWarnings("unchecked")
+    private void nodeToHashMap(Node node, HashMap<String, Object> hashMap) throws RepositoryException {
+        // Property values are serialized by the custom serializer found in JcrPropertyValueSerializer
 
-    final class SearchResultItem {
-        public String name;
-        public String uuid;
-        public Link[] links;
-    }
-
-    final class SearchResult {
-        public long offset;
-        public long max;
-        public long count;
-        public boolean more;
-        public long total;
-        public SearchResultItem[] items;
-    }
-
-    final class Error {
-        public int status;
-        public String description;
-        Error(int status, String description) {
-            this.status = status;
-            this.description = description;
-        }
-    }
-
-    @GET
-    @Path("/content/documents")
-    public Response getDocuments() {
-        try {
-            SearchService searchService = getSearchService();
-            Query query = searchService.createQuery()
-                    .from("/content/documents")
-                    .ofType("myhippoproject:basedocument")
-                    .returnParentNode()
-                    .orderBy(HippoStdPubWfNodeType.HIPPOSTDPUBWF_PUBLICATION_DATE)
-                    .descending();
-            QueryResult queryResult = searchService.search(query);
-
-            SearchResult returnValue = new SearchResult();
-            returnValue.offset = 0;
-            returnValue.max = 100;
-            returnValue.count = queryResult.getTotalHitCount();
-            returnValue.more = false;
-            returnValue.total = queryResult.getTotalHitCount();
-
-            int count = (int) queryResult.getTotalHitCount();
-            int current = 0;
-            returnValue.items = new SearchResultItem[count];
-            HitIterator iterator = queryResult.getHits();
-            while (iterator.hasNext()) {
-                Hit hit = iterator.nextHit();
-                returnValue.items[current] = new SearchResultItem();
-                String uuid = hit.getSearchDocument().getContentId().toIdentifier();
-                Node node = context.getSession().getNodeByIdentifier(uuid);
-                returnValue.items[current].name = node.getName();
-                returnValue.items[current].uuid = uuid;
-                returnValue.items[current].links = new Link[1];
-                returnValue.items[current].links[0] = new Link();
-                returnValue.items[current].links[0].url = uuid;
-                current++;
+        // Iterate over all properties and add those to the hashMap.
+        Iterator<Property> propertyIterator = node.getProperties();
+        while (propertyIterator.hasNext()) {
+            Property property = propertyIterator.next();
+            if (property.isMultiple()) {
+                hashMap.put(property.getName(), property.getValues());
+            } else {
+                hashMap.put(property.getName(), property.getValue());
             }
-
-            return Response.status(200).entity(returnValue).build();
-        } catch (RepositoryException e) {
-            return buildErrorResponse(500, e);
         }
-    }
 
-    @GET
-    @Path("/content/documents/{uuid}")
-    public Response getDocumentsByUUID(@PathParam("uuid") String uuid) {
-        try {
-            Session session = context.getSession();
-
-            // this could throw a IllegalArgumentException
-            UUID.fromString(uuid);
-
-            // this could throw a ItemNotFoundException
-            Node node = session.getNodeByIdentifier(uuid);
-
-            HashMap<String, Object> returnValue = new HashMap<>();
-
-            returnValue.put("this", "that");
-            returnValue.put("something", returnValue.clone());
-
-            return Response.status(200).entity(returnValue).build();
-        } catch (IllegalArgumentException iae) {
-            return buildErrorResponse(400, iae, "The string '" + uuid + "' is not a valid UUID.");
-        } catch (ItemNotFoundException infe) {
-            return buildErrorResponse(404, infe);
-        } catch (RepositoryException re) {
-            return buildErrorResponse(500, re);
+        // TODO discuss with Ate
+        // Iterate over all nodes and add those to the hashMap.
+        // In case of a property and a sub node with the same name, this overwrites the property.
+        // In Hippo 10.x and up, it is not possible to create document types through the document type editor that
+        // have this type of same-name-siblings. It is possible when creating document types in the console or when
+        // upgrading older projects. For now, it is acceptable that in those exceptional situations there is data-loss.
+        Iterator<Node> nodeIterator = node.getNodes();
+        while (nodeIterator.hasNext()) {
+            Node childNode = nodeIterator.next();
+            HashMap<String, Object> childHashMap = new HashMap<>();
+            hashMap.put(childNode.getName(), childHashMap);
+            nodeToHashMap(childNode, childHashMap);
         }
     }
 
