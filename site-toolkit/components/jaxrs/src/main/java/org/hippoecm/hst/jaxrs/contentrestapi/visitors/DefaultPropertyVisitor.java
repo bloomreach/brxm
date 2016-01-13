1bdac483259e8300680784f17b23678bf091ebc6
Oscar Scholten <o.scholten@onehippo.com>
2016-01-13 17:18:01 +0000

    HSTTWO-3549: improving value rendering

--- site-toolkit/components/jaxrs/src/main/java/org/hippoecm/hst/jaxrs/contentrestapi/visitors/DefaultPropertyVisitor.java
+++ site-toolkit/components/jaxrs/src/main/java/org/hippoecm/hst/jaxrs/contentrestapi/visitors/DefaultPropertyVisitor.java
@@ -23,8 +23,14 @@ import javax.jcr.RepositoryException;
 import javax.jcr.Value;
 
 import org.hippoecm.hst.jaxrs.contentrestapi.ResourceContext;
+import org.onehippo.cms7.services.contenttype.ContentType;
+import org.onehippo.cms7.services.contenttype.ContentTypeProperty;
 
 import static javax.jcr.PropertyType.BINARY;
+import static javax.jcr.PropertyType.BOOLEAN;
+import static javax.jcr.PropertyType.DECIMAL;
+import static javax.jcr.PropertyType.DOUBLE;
+import static javax.jcr.PropertyType.LONG;
 
 class DefaultPropertyVisitor extends AbstractBaseVisitor {
 
@@ -34,24 +40,42 @@ class DefaultPropertyVisitor extends AbstractBaseVisitor {
 
     @Override
     public void visit(final ResourceContext context, final Property property, final Map<String, Object> destination) throws RepositoryException {
-        if (property.isMultiple()) {
+        final ContentType contentType = context.getContentTypes().getContentTypeForNode(property.getParent());
+        final ContentTypeProperty propertyType = contentType.getProperties().get(property.getName());
+
+        if (propertyType == null) {
+            return;
+        }
+
+        if (propertyType.isMultiple()) {
             final Value[] jcrValues = property.getValues();
-            final String[] stringValues = new String[jcrValues.length];
+            final Object[] representations = new Object[jcrValues.length];
             for (int i = 0; i < jcrValues.length; i++) {
                 final Value jcrValue = jcrValues[i];
-                stringValues[i] = getStringRepresentation(jcrValue);
+                representations[i] = getValueRepresentation(jcrValue);
             }
-            destination.put(property.getName(), stringValues);
+            destination.put(property.getName(), representations);
         } else {
-            destination.put(property.getName(), getStringRepresentation(property.getValue()));
+            destination.put(property.getName(), getValueRepresentation(property.getValue()));
         }
     }
 
-    private String getStringRepresentation(final Value jcrValue) throws RepositoryException {
-        if (jcrValue.getType() == BINARY) {
-            return "Retrieving the content of binary property values is not yet supported in the Content REST API. Use images and assets instead.";
-        } else {
-            return jcrValue.getString();
+    private Object getValueRepresentation(final Value jcrValue) throws RepositoryException {
+        // Date is rendered by JackRabbit including timezone, no need for special handling
+
+        switch (jcrValue.getType()) {
+            case BINARY:
+                return "Retrieving the content of binary property values is not yet supported in the Content REST API. Use images and assets instead.";
+            case LONG:
+                return jcrValue.getLong();
+            case DOUBLE:
+                return jcrValue.getDouble();
+            case BOOLEAN:
+                return jcrValue.getBoolean();
+            case DECIMAL:
+                return jcrValue.getDecimal();
+            default:
+                return jcrValue.getString();
         }
     }
 }
