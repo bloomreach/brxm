190f8d054aabbb29f6521a98c7dfdd48ccb1b61e
Ate Douma <a.douma@onehippo.com>
2016-01-20 15:42:55 +0000

    ESSENTIALS-823: adjust for renamed HST restapi pipeline

--- essentials/plugins/rest-services/src/main/java/org/onehippo/cms7/essentials/dashboard/restservices/RestPluginConst.java
+++ essentials/plugins/rest-services/src/main/java/org/onehippo/cms7/essentials/dashboard/restservices/RestPluginConst.java
@@ -20,7 +20,7 @@ public final class RestPluginConst {
 
     public static final String GENERIC_API_ENABLED = "genericApiEnabled";
     public static final String GENERIC_REST_NAME = "genericRestName";
-    public static final String GENERIC_PIPELINE_NAME = "ContentApiPipeline";
+    public static final String GENERIC_PIPELINE_NAME = "RestApiPipeline";
     public static final String MANUAL_API_ENABLED = "manualApiEnabled";
     public static final String MANUAL_REST_NAME = "manualRestName";
     public static final String MANUAL_PIPELINE_NAME = "JaxrsRestPlainPipeline";
