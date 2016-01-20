65aecf37ab5f1ace8bf7729bc66ce6dd1f215355
Ard Schrijvers <a.schrijvers@onehippo.com>
2016-01-20 13:43:40 +0000

    HSTTWO-3565 move the ResourceBundleVisitor to the resourcebundle artifact. I've added it below 'contentrest' package. The ResourceBundleVisitor will be picked up by annotation scanning

--- site-toolkit/components/resourcebundle/src/main/java/org/hippoecm/hst/resourcebundle/contentrest/ResourceBundleVisitor.java
+++ site-toolkit/components/resourcebundle/src/main/java/org/hippoecm/hst/resourcebundle/contentrest/ResourceBundleVisitor.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package org.hippoecm.hst.contentrestapi.annotated.visitors;
+package org.hippoecm.hst.resourcebundle.contentrest;
 
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
