77066c5c367aee04baeead7bc50c43e02ca89a54
Arthur Bogaart <a.bogaart@onehippo.com>
2016-02-02 09:53:40 +0000

    CHANNELMGR-445: Clean up karma.conf.js
    Removed duplicate data from hippo-build karma conf

--- channel-manager/frontend-ng/karma.conf.js
+++ channel-manager/frontend-ng/karma.conf.js
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015 Hippo B.V. (http://www.onehippo.com)
+ * Copyright 2015-2016 Hippo B.V. (http://www.onehippo.com)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,73 +18,29 @@ var hippoBuild = require('hippo-build');
 var customConfig = require('./build.conf.js');
 var cfg = hippoBuild.buildConfig(customConfig);
 
-module.exports = function(config) {
-  var options = {};
+module.exports = function karmaConfig(config) {
+  var options = cfg.karma;
 
-  options.basePath = '.';
-
-  options.frameworks = ['systemjs', 'jasmine', 'es6-shim'];
-  options.reporters = ['progress', 'coverage'];
-  options.preprocessors = {};
-
-  options.preprocessors[cfg.src.scripts] = ['coverage'];
-  options.coverageReporter = {
-    instrumenters: {
-      isparta: require('isparta')
-    },
-    instrumenter: {
-      '**/*.js': 'isparta'
-    },
-    reporters: [{
-      type: 'html'
-    }, {
-      type: 'text-summary'
-    }]
-  };
-
-  options.preprocessors[cfg.src.templates] = ['ng-html2js'];
-  options.ngHtml2JsPreprocessor = {
-    stripPrefix: 'src/angularjs/',
-    moduleName: cfg.projectName + '-templates'
-  };
+  options.systemjs.includeFiles = [
+    cfg.bowerDir + 'angular/angular.js',
+    cfg.bowerDir + 'angular-animate/angular-animate.js',
+    cfg.bowerDir + 'angular-aria/angular-aria.js',
+    cfg.bowerDir + 'angular-material/angular-material.js',
+    cfg.bowerDir + 'angular-ui-router/release/angular-ui-router.js',
+    cfg.bowerDir + 'angular-translate/angular-translate.js',
+    cfg.bowerDir + 'angular-translate-loader-static-files/angular-translate-loader-static-files.js',
+    cfg.bowerDir + 'angular-mocks/angular-mocks.js',
+    cfg.npmDir + 'babel-core/external-helpers.js',
+    cfg.npmDir + 'systemjs/dist/system-polyfills.js',
+    cfg.npmDir + 'systemjs/dist/system-register-only.js'
+  ];
 
   options.files = [
     cfg.src.templates,
     cfg.src.scripts,
-    cfg.srcDir + './angularjs/mock.environment.spec.js',
-    cfg.src.unitTests
+    cfg.src.unitTests,
+    cfg.srcDir + './angularjs/mock.environment.spec.js'
   ];
-
-  options.systemjs = {
-    config: {
-      transpiler: 'babel',
-      defaultJSExtensions: true,
-      paths: {
-        'babel': cfg.npmDir + 'babel-core/browser.js',
-        'systemjs': cfg.npmDir + 'systemjs/dist/system.js',
-        'system-polyfills': cfg.npmDir + 'systemjs/dist/system-polyfills.js',
-        'es6-module-loader': cfg.npmDir + 'es6-module-loader/dist/es6-module-loader.js',
-        'phantomjs-polyfill': cfg.npmDir + 'phantomjs-polyfill/bind-polyfill.js'
-      }
-    },
-    includeFiles: [
-      cfg.bowerDir + 'angular/angular.js',
-      cfg.bowerDir + 'angular-animate/angular-animate.js',
-      cfg.bowerDir + 'angular-aria/angular-aria.js',
-      cfg.bowerDir + 'angular-material/angular-material.js',
-      cfg.bowerDir + 'angular-ui-router/release/angular-ui-router.js',
-      cfg.bowerDir + 'angular-translate/angular-translate.js',
-      cfg.bowerDir + 'angular-translate-loader-static-files/angular-translate-loader-static-files.js',
-      cfg.bowerDir + 'angular-mocks/angular-mocks.js',
-      cfg.npmDir + 'babel-core/external-helpers.js',
-      cfg.npmDir + 'systemjs/dist/system-polyfills.js',
-      cfg.npmDir + 'systemjs/dist/system-register-only.js'
-    ]
-  };
-
-  options.browsers = ['PhantomJS'];
-  options.autoWatch = false;
-  options.singleRun = true;
 
   config.set(options);
 };
