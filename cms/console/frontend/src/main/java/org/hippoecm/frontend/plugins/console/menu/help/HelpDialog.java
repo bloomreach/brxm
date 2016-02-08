7820456c164997ce37159b9c054571feff60f640
Tobias Jeger <t.jeger@onehippo.com>
2016-02-08 14:17:37 +0100

    CMS-9161: remove Cancel button from Console Help dialog.

--- cms/console/frontend/src/main/java/org/hippoecm/frontend/plugins/console/menu/help/HelpDialog.java
+++ cms/console/frontend/src/main/java/org/hippoecm/frontend/plugins/console/menu/help/HelpDialog.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2013 Hippo B.V. (http://www.onehippo.com)
+ * Copyright 2012-2016 Hippo B.V. (http://www.onehippo.com)
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -53,6 +53,8 @@ public class HelpDialog extends AbstractDialog<Void> {
                 item.add(new Label("shortcut", user.getShortcutName()));
             }
         });
+
+        setCancelVisible(false);
     }
 
 
