8860f24be3057b0d95674e2842c82d518b50a089
Ard Schrijvers <a.schrijvers@onehippo.com>
2016-02-09 10:34:10 +0100

    CMS-9917 keep the original wicket (apache) license because of original fork and add a comment below it why it was forked and from which location. Added a rat exclusion for the CsrfPreventRequestCyclyListener

--- cms/engine/src/main/java/org/hippoecm/frontend/http/CsrfPreventionRequestCycleListener.java
+++ cms/engine/src/main/java/org/hippoecm/frontend/http/CsrfPreventionRequestCycleListener.java
@@ -1,17 +1,22 @@
 /*
- *  Copyright 2016 Hippo B.V. (http://www.onehippo.com)
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
  *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
- *       http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
+ * Original fork of wicket's  6.21.0 CsrfPreventionRequestCycleListener,
+ * see https://github.com/apache/wicket/blob/build/wicket-6.21.0/wicket-core/src/main/java/org/apache/wicket/protocol/http/CsrfPreventionRequestCycleListener.java.
+ * This class has been forked and modified to support a CsrfPreventionRequestCycleListener working behind proxies like httpd
  */
 package org.hippoecm.frontend.http;
 
@@ -39,12 +44,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import static org.hippoecm.frontend.util.RequestUtils.getFarthestRequestScheme;
-
-
-/**
- * Semi-fork of wicket's CsrfPreventionRequestCycleListener. First check in is the CsrfPreventionRequestCycleListener from wicket 6.21.0 as is.
- *
- */
 
 /**
  * Prevents CSRF attacks on Wicket components by checking the {@code Origin} HTTP header for cross
