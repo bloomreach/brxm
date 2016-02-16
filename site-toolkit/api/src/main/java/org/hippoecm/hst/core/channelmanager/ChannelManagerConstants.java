41cd665d31a6a6a94d00e5ef6777c2ab9f81982f
Arthur Bogaart <a.bogaart@onehippo.com>
2016-02-16 13:44:28 +0100

    HSTTWO-1003 Remove redundant static final modifiers on String constants

--- site-toolkit/api/src/main/java/org/hippoecm/hst/core/channelmanager/ChannelManagerConstants.java
+++ site-toolkit/api/src/main/java/org/hippoecm/hst/core/channelmanager/ChannelManagerConstants.java
@@ -20,22 +20,21 @@ public interface ChannelManagerConstants {
     String HST_TYPE = "HST-Type";
     String HST_PATH_INFO = "HST-Path-Info";
     String HST_CHANNEL_ID = "HST-Channel-Id";
-    static final String HST_MOUNT_ID = "HST-Mount-Id";
-    static final String HST_SITE_ID = "HST-Site-Id";
-    static final String HST_PAGE_ID = "HST-Page-Id";
-    static final String HST_SITEMAP_ID = "HST-Sitemap-Id";
-    static final String HST_SITEMAPITEM_ID = "HST-SitemapItem-Id";
-    static final String HST_PAGE_EDITABLE = "HST-Page-Editable";
-    static final String HST_RENDER_VARIANT = "HST-Render-Variant";
-    static final String HST_SITE_HAS_PREVIEW_CONFIG = "HST-Site-HasPreviewConfig";
+    String HST_MOUNT_ID = "HST-Mount-Id";
+    String HST_SITE_ID = "HST-Site-Id";
+    String HST_PAGE_ID = "HST-Page-Id";
+    String HST_SITEMAP_ID = "HST-Sitemap-Id";
+    String HST_SITEMAPITEM_ID = "HST-SitemapItem-Id";
+    String HST_PAGE_EDITABLE = "HST-Page-Editable";
+    String HST_RENDER_VARIANT = "HST-Render-Variant";
+    String HST_SITE_HAS_PREVIEW_CONFIG = "HST-Site-HasPreviewConfig";
+
     // below used outside HST
-    static final String HST_PAGE_REQUEST_VARIANTS = "HST-Page-Request-Variants";
-
-    static final String HST_LOCKED_BY = "HST-LockedBy";
-    static final String HST_LOCKED_BY_CURRENT_USER = "HST-LockedBy-Current-User";
-    static final String HST_LOCKED_ON = "HST-LockedOn";
-    static final String HST_LAST_MODIFIED = "HST-LastModified";
-
+    String HST_PAGE_REQUEST_VARIANTS = "HST-Page-Request-Variants";
+    String HST_LOCKED_BY = "HST-LockedBy";
+    String HST_LOCKED_BY_CURRENT_USER = "HST-LockedBy-Current-User";
+    String HST_LOCKED_ON = "HST-LockedOn";
+    String HST_LAST_MODIFIED = "HST-LastModified";
     String HST_TYPE_PAGE_META_DATA = "PAGE-META-DATA";
 
 }
