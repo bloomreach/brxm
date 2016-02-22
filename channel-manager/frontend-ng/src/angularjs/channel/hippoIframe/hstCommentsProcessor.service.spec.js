769a3d65372a76753bf1a1c2af5204bb86d80d21
Arthur Bogaart <a.bogaart@onehippo.com>
2016-02-23 00:49:01 +0100

    CHANNELMGR-339 Add missing license and simplify test
    Merged broken JSON fixture with generic fixture file

--- channel-manager/frontend-ng/src/angularjs/channel/hippoIframe/hstCommentsProcessor.service.spec.js
+++ channel-manager/frontend-ng/src/angularjs/channel/hippoIframe/hstCommentsProcessor.service.spec.js
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2016 Hippo B.V. (http://www.onehippo.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 describe('HstCommentsProcessorService', function () {
   'use strict';
 
@@ -10,11 +26,12 @@ describe('HstCommentsProcessorService', function () {
     inject(function (_hstCommentsProcessorService_) {
       hstCommentsProcessorService = _hstCommentsProcessorService_;
     });
+
+    jasmine.getFixtures().load('channel/hippoIframe/hstCommentsProcessor.service.fixture.html');
   });
 
   // PhantomJS does not support XPath querying through document.evaluate (https://github.com/ariya/phantomjs/issues/10161)
   it('should use DOM-walking when XPath querying is not available', function () {
-    jasmine.getFixtures().load('channel/hippoIframe/hstCommentsProcessor.service.fixture.html');
     spyOn(hstCommentsProcessorService, 'processCommentsWithDomWalking');
     hstCommentsProcessorService.run($j('#jasmine-fixtures')[0], NOOP);
     expect(hstCommentsProcessorService.processCommentsWithDomWalking).toHaveBeenCalled();
@@ -22,9 +39,9 @@ describe('HstCommentsProcessorService', function () {
 
 
   it('should process comments with DOM-walking', function () {
+    var fixture = $j('#jasmine-fixtures')[0];
     var gatheredData = [];
-    jasmine.getFixtures().load('channel/hippoIframe/hstCommentsProcessor.service.fixture.html');
-    hstCommentsProcessorService.processCommentsWithDomWalking($j('#jasmine-fixtures')[0], function (element, json) {
+    hstCommentsProcessorService.processCommentsWithDomWalking(fixture, function (element, json) {
       gatheredData.push(json);
     });
 
@@ -41,11 +58,10 @@ describe('HstCommentsProcessorService', function () {
 
 
   it('should not invoke callback when JSON data is invalid', function () {
+    var fixture = $j('#qa-invalid-json')[0];
     var observer = { callback: NOOP };
     spyOn(observer, 'callback');
-
-    jasmine.getFixtures().load('channel/hippoIframe/hstCommentsProcessor.service.invalid.json.fixture.html');
-    hstCommentsProcessorService.processCommentsWithDomWalking($j('#jasmine-fixtures')[0], observer.callback);
+    hstCommentsProcessorService.processCommentsWithDomWalking(fixture, observer.callback);
 
     expect(observer.callback).not.toHaveBeenCalled();
   });
