9580b765fa5bb352de4c52954318e074ce2ba926
Unico Hommes <u.hommes@onehippo.com>
2016-02-23 14:08:00 +0100

    CMS-9944 show translated document type names in document types listing

--- cms/api/src/main/java/org/hippoecm/frontend/plugins/standards/list/resolvers/TypeRenderer.java
+++ cms/api/src/main/java/org/hippoecm/frontend/plugins/standards/list/resolvers/TypeRenderer.java
@@ -21,9 +21,13 @@ import javax.jcr.RepositoryException;
 import org.apache.wicket.Component;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.Model;
 import org.hippoecm.frontend.i18n.types.TypeTranslator;
 import org.hippoecm.frontend.model.nodetypes.JcrNodeTypeModel;
-import org.hippoecm.repository.api.HippoNodeType;
+
+import static org.hippoecm.repository.api.HippoNodeType.NT_DOCUMENT;
+import static org.hippoecm.repository.api.HippoNodeType.NT_HANDLE;
+import static org.hippoecm.repository.api.HippoNodeType.NT_TEMPLATETYPE;
 
 public class TypeRenderer extends AbstractNodeRenderer {
 
@@ -42,10 +46,20 @@ public class TypeRenderer extends AbstractNodeRenderer {
     }
 
     private IModel<String> getLabelModel(Node node) throws RepositoryException {
-        if (node.isNodeType(HippoNodeType.NT_HANDLE) && node.hasNode(node.getName())) {
+        if (node.isNodeType(NT_HANDLE) && node.hasNode(node.getName())) {
             node = node.getNode(node.getName());
         }
-        return new TypeTranslator(new JcrNodeTypeModel(node.getPrimaryNodeType())).getTypeName();
+        String type = null;
+        if (node.isNodeType(NT_DOCUMENT)) {
+            type = node.getPrimaryNodeType().getName();
+        }
+        if (node.isNodeType(NT_TEMPLATETYPE)) {
+            type = node.getParent().getName() + ":" + node.getName();
+        }
+        if (type != null) {
+            return new TypeTranslator(new JcrNodeTypeModel(type)).getTypeName();
+        }
+        return new Model<>("unknown");
     }
 
 }
