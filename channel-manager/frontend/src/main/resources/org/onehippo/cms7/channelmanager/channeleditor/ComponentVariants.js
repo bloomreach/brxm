ced025e9e53db417e4929aab21dc0eccfcc360bc
Mathijs den Burger <m.denburger@onehippo.com>
2016-02-26 13:58:15 +0100

    CHANNELMGR-374 format code

--- channel-manager/frontend/src/main/resources/org/onehippo/cms7/channelmanager/channeleditor/ComponentVariants.js
+++ channel-manager/frontend/src/main/resources/org/onehippo/cms7/channelmanager/channeleditor/ComponentVariants.js
@@ -15,143 +15,148 @@
  */
 (function() {
 
-    "use strict";
+  "use strict";
 
-    Ext.namespace('Hippo.ChannelManager.ChannelEditor');
+  Ext.namespace('Hippo.ChannelManager.ChannelEditor');
 
-    /**
-     * Retrieves all variants for the component specified by the config property 'componentId'.
-     * The REST endpoint for retrieving the global variants is determined by the config property 'variantsUuid'.
-     * When 'variantsUuid' is empty or undefined, a single variant with the ID 'hippo-default' will always be returned.
-     *
-     * The retrieved variants will be cached. Calling 'invalidate()' will invalidate the cache and fire the
-     * 'invalidated' event. The next call to 'get()' will then retrieve the component variants again.
-     *
-     * @type {*}
-     */
-    Hippo.ChannelManager.ChannelEditor.ComponentVariants = Ext.extend(Ext.util.Observable, {
+  /**
+   * Retrieves all variants for the component specified by the config property 'componentId'.
+   * The REST endpoint for retrieving the global variants is determined by the config property 'variantsUuid'.
+   * When 'variantsUuid' is empty or undefined, a single variant with the ID 'hippo-default' will always be returned.
+   *
+   * The retrieved variants will be cached. Calling 'invalidate()' will invalidate the cache and fire the
+   * 'invalidated' event. The next call to 'get()' will then retrieve the component variants again.
+   *
+   * @type {*}
+   */
+  Hippo.ChannelManager.ChannelEditor.ComponentVariants = Ext.extend(Ext.util.Observable, {
 
-        variantsFuture: null,
+    variantsFuture: null,
 
-        constructor: function(config) {
-            this.variantsUuid = config.variantsUuid;
-            this.componentId = config.componentId;
-            this.lastModified = config.lastModified;
-            this.composerRestMountUrl = config.composerRestMountUrl;
-            this.locale = config.locale;
+    constructor: function(config) {
+      this.variantsUuid = config.variantsUuid;
+      this.componentId = config.componentId;
+      this.lastModified = config.lastModified;
+      this.composerRestMountUrl = config.composerRestMountUrl;
+      this.locale = config.locale;
 
-            Hippo.ChannelManager.ChannelEditor.ComponentVariants.superclass.constructor.call(this, config);
+      Hippo.ChannelManager.ChannelEditor.ComponentVariants.superclass.constructor.call(this, config);
 
-            this.addEvents('invalidated');
-        },
+      this.addEvents('invalidated');
+    },
 
-        isMultivariate: function() {
-            return !Ext.isEmpty(this.variantsUuid);
-        },
+    isMultivariate: function() {
+      return !Ext.isEmpty(this.variantsUuid);
+    },
 
-        get: function() {
-            if (this.variantsFuture === null) {
-                this.variantsFuture = this._loadVariants();
-            }
-            return this.variantsFuture;
-        },
+    get: function() {
+      if (this.variantsFuture === null) {
+        this.variantsFuture = this._loadVariants();
+      }
+      return this.variantsFuture;
+    },
 
-        invalidate: function(changedVariantIds, activeVariantId) {
-            this.variantsFuture = null;
-            this.fireEvent('invalidated', changedVariantIds, activeVariantId);
-        },
+    invalidate: function(changedVariantIds, activeVariantId) {
+      this.variantsFuture = null;
+      this.fireEvent('invalidated', changedVariantIds, activeVariantId);
+    },
 
-        cleanup: function() {
-            var cleanupFuture = this.isMultivariate() ? this._cleanupVariants() : Hippo.Future.constant();
-            return cleanupFuture;
-        },
+    cleanup: function() {
+      var cleanupFuture = this.isMultivariate() ? this._cleanupVariants() : Hippo.Future.constant();
+      return cleanupFuture;
+    },
 
-        _loadVariants: function() {
-            if (!this.isMultivariate()) {
-                return Hippo.Future.constant([
-                    {id: 'hippo-default', name: Hippo.ChannelManager.ChannelEditor.Resources['component-variants-default']}
-                ]);
-            } else {
-                var self = this;
-                return new Hippo.Future(function(success, fail) {
-                    if (self.componentId) {
-                        Ext.Ajax.request({
-                            url: self.composerRestMountUrl + '/' + self.componentId,
-                            headers: {
-                                'FORCE_CLIENT_HOST': 'true'
-                            },
-                            success: function(result) {
-                                var jsonData = Ext.util.JSON.decode(result.responseText),
-                                    variantIds = jsonData.data;
+    _loadVariants: function() {
+      if (!this.isMultivariate()) {
+        return Hippo.Future.constant([{
+          id: 'hippo-default',
+          name: Hippo.ChannelManager.ChannelEditor.Resources['component-variants-default']
+        }]);
+      } else {
+        var self = this;
+        return new Hippo.Future(function(success, fail) {
+          if (self.componentId) {
+            Ext.Ajax.request({
+              url: self.composerRestMountUrl + '/' + self.componentId,
+              headers: {
+                'FORCE_CLIENT_HOST': 'true'
+              },
+              success: function(result) {
+                var jsonData = Ext.util.JSON.decode(result.responseText),
+                  variantIds = jsonData.data;
 
-                                self._loadComponentVariants(variantIds).when(function(variants) {
-                                    variants.push({id: 'plus', name: Hippo.ChannelManager.ChannelEditor.Resources['component-variants-plus']});
-                                    success(variants);
-                                }).otherwise(function(response) {
-                                    fail(response);
-                                });
-                            },
-                            failure: function(result) {
-                                fail(result.response);
-                            }
-                        });
-                    } else {
-                        success([
-                            {id: 'hippo-default', name: Hippo.ChannelManager.ChannelEditor.Resources['component-variants-default']}
-                        ]);
-                    }
+                self._loadComponentVariants(variantIds).when(function(variants) {
+                  variants.push({
+                    id: 'plus',
+                    name: Hippo.ChannelManager.ChannelEditor.Resources['component-variants-plus']
+                  });
+                  success(variants);
+                }).otherwise(function(response) {
+                  fail(response);
                 });
+              },
+              failure: function(result) {
+                fail(result.response);
+              }
+            });
+          } else {
+            success([{
+              id: 'hippo-default',
+              name: Hippo.ChannelManager.ChannelEditor.Resources['component-variants-default']
+            }]);
+          }
+        });
+      }
+    },
+
+    _loadComponentVariants: function(variantIds) {
+      return new Hippo.Future(function(success, fail) {
+        Ext.Ajax.request({
+          url: this.composerRestMountUrl + '/' + this.variantsUuid + './componentvariants?locale=' + this.locale,
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            'FORCE_CLIENT_HOST': 'true'
+          },
+          params: Ext.util.JSON.encode(variantIds),
+          success: function(result) {
+            var jsonData = Ext.util.JSON.decode(result.responseText),
+              variants = jsonData.data;
+            success(variants);
+          },
+          failure: function(result) {
+            fail(result.response);
+          },
+          scope: this
+        });
+      }.createDelegate(this));
+    },
+
+    _cleanupVariants: function() {
+      return new Hippo.Future(function(success) {
+        this.get().when(function(variants) {
+          var variantIds = [];
+          Ext.each(variants, function(variant) {
+            if (variant.id !== 'hippo-default' && variant.id !== 'plus') {
+              variantIds.push(variant.id);
             }
-        },
-
-        _loadComponentVariants: function(variantIds) {
-            return new Hippo.Future(function(success, fail) {
-                Ext.Ajax.request({
-                    url: this.composerRestMountUrl + '/' + this.variantsUuid + './componentvariants?locale=' + this.locale,
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                        'FORCE_CLIENT_HOST': 'true'
-                    },
-                    params: Ext.util.JSON.encode(variantIds),
-                    success: function(result) {
-                        var jsonData = Ext.util.JSON.decode(result.responseText),
-                            variants = jsonData.data;
-                        success(variants);
-                    },
-                    failure: function(result) {
-                        fail(result.response);
-                    },
-                    scope: this
-                });
-            }.createDelegate(this));
-        },
-
-        _cleanupVariants: function() {
-            return new Hippo.Future(function(success) {
-                this.get().when(function(variants) {
-                    var variantIds = [];
-                    Ext.each(variants, function(variant) {
-                        if (variant.id !== 'hippo-default' && variant.id !== 'plus') {
-                            variantIds.push(variant.id);
-                        }
-                    });
-                    Ext.Ajax.request({
-                        method: 'POST',
-                        url: this.composerRestMountUrl + '/' + this.componentId,
-                        headers: {
-                            'FORCE_CLIENT_HOST': 'true',
-                            'Content-Type': 'application/json',
-                            'lastModifiedTimestamp': this.lastModified
-                        },
-                        params: Ext.util.JSON.encode(variantIds),
-                        scope: this,
-                        success: success,
-                        failure: success  // ignore failures silently, try cleanup next time
-                    });
-                }.createDelegate(this));
-            }.createDelegate(this));
-        }
-    });
+          });
+          Ext.Ajax.request({
+            method: 'POST',
+            url: this.composerRestMountUrl + '/' + this.componentId,
+            headers: {
+              'FORCE_CLIENT_HOST': 'true',
+              'Content-Type': 'application/json',
+              'lastModifiedTimestamp': this.lastModified
+            },
+            params: Ext.util.JSON.encode(variantIds),
+            scope: this,
+            success: success,
+            failure: success  // ignore failures silently, try cleanup next time
+          });
+        }.createDelegate(this));
+      }.createDelegate(this));
+    }
+  });
 
 }());
