d2497c14e311611ac5c543a48a3138bde27da13c
Mathijs den Burger <m.denburger@onehippo.com>
2016-03-18 16:40:34 +0100

    CHANNELMGR-468 remove duplicate method 'removeComponent'
    Keep the one that accepts a component.

--- channel-manager/frontend-ng/src/angularjs/channel/page/element/containerElement.js
+++ channel-manager/frontend-ng/src/angularjs/channel/page/element/containerElement.js
@@ -76,21 +76,6 @@ export class ContainerElement extends PageStructureElement {
     return this.items.find((item) => item.getId() === componentId);
   }
 
-  /**
-   * Remove the component identified by given Id from its container
-   * @param componentId
-   * @returns {*} the removed component
-   */
-  removeComponent(componentId) {
-    const component = this.getComponent(componentId);
-    if (component) {
-      this.items.splice(this.items.indexOf(component), 1);
-      return component;
-    }
-
-    return null;
-  }
-
   getComponentByIframeElement(iframeElement) {
     return this.items.find((item) => item.getJQueryElement('iframe').is(iframeElement));
   }
