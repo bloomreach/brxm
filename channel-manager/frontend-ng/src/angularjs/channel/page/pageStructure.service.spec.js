8552f71fb5073542ddf80c66515532cad340d8bc
Tobias Jeger <t.jeger@onehippo.com>
2016-03-18 16:55:21 +0100

    CHANNELMGR-467: fixed some pageStructure service tests.

--- channel-manager/frontend-ng/src/angularjs/channel/page/pageStructure.service.spec.js
+++ channel-manager/frontend-ng/src/angularjs/channel/page/pageStructure.service.spec.js
@@ -21,6 +21,7 @@ describe('PageStructureService', () => {
   let PageMetaDataService;
   let ChannelService;
   let HstService;
+  let RenderingService;
   let $document;
   let $q;
   let $log;
@@ -30,7 +31,8 @@ describe('PageStructureService', () => {
   beforeEach(() => {
     module('hippo-cm.channel.page');
 
-    inject((_$q_, _$rootScope_, _$log_, _$document_, _$window_, _PageStructureService_, _PageMetaDataService_, _ChannelService_, _HstService_) => {
+    inject((_$q_, _$rootScope_, _$log_, _$document_, _$window_, _PageStructureService_, _PageMetaDataService_,
+            _ChannelService_, _HstService_, _RenderingService_) => {
       $q = _$q_;
       $rootScope = _$rootScope_;
       $log = _$log_;
@@ -40,6 +42,7 @@ describe('PageStructureService', () => {
       PageMetaDataService = _PageMetaDataService_;
       ChannelService = _ChannelService_;
       HstService = _HstService_;
+      RenderingService = _RenderingService_;
     });
   });
 
@@ -61,53 +64,78 @@ describe('PageStructureService', () => {
     expect($log.warn).toHaveBeenCalled();
   });
 
-  it('registers containers in the correct order', () => {
-    const container1 = $j('#container1', $document);
-    const container2 = $j('#container2', $document);
+  const jsonify = (element) => JSON.parse(element.data);
 
-    PageStructureService.registerParsedElement(container1[0].previousSibling, {
-      'HST-Type': 'CONTAINER_COMPONENT',
-      'HST-Label': 'container 1',
-    });
-    PageStructureService.registerParsedElement(container2[0].previousSibling, {
-      'HST-Type': 'CONTAINER_COMPONENT',
-      'HST-Label': 'container 2',
-    });
+  const childComment = (element) => {
+    var children = element.childNodes;
+    for (let i = 0; i < children.length; i++) {
+      const child = children[i];
+      if (child.nodeType === 8) {
+        return child;
+      }
+    }
+    console.log('No child comment found!');
+    return null;
+  };
+
+  const previousComment = (element) => {
+    while (element.previousSibling) {
+      element = element.previousSibling;
+      if (element.nodeType === 8) {
+        return element;
+      }
+    }
+    console.log('no previous comment found!');
+    return null;
+  };
+
+  const nextComment = (element) => {
+    while (element.nextSibling) {
+      element = element.nextSibling;
+      if (element.nodeType === 8) {
+        return element;
+      }
+    }
+    console.log('no next comment found!');
+    return null;
+  };
+
+  it('registers containers in the correct order', () => {
+    const container1 = $j('#container11', $document)[0];
+    const container2 = $j('#container22', $document)[0];
+    const comment1 = previousComment(container1);
+    const comment2 = childComment(container2);
+
+    PageStructureService.registerParsedElement(comment1, jsonify(comment1));
+    PageStructureService.registerParsedElement(comment2, jsonify(comment2));
 
     expect(PageStructureService.containers.length).toEqual(2);
 
     expect(PageStructureService.containers[0].type).toEqual('container');
     expect(PageStructureService.containers[0].isEmpty()).toEqual(true);
     expect(PageStructureService.containers[0].getComponents()).toEqual([]);
-    expect(PageStructureService.containers[0].getJQueryElement('iframeBoxElement')).toEqual(container1);
+    expect(PageStructureService.containers[0].getJQueryElement('iframeBoxElement')[0]).toEqual(container1);
     expect(PageStructureService.containers[0].getLabel()).toEqual('container 1');
 
     expect(PageStructureService.containers[1].type).toEqual('container');
     expect(PageStructureService.containers[1].isEmpty()).toEqual(true);
     expect(PageStructureService.containers[1].getComponents()).toEqual([]);
-    expect(PageStructureService.containers[1].getJQueryElement('iframeBoxElement')).toEqual(container2);
+    expect(PageStructureService.containers[1].getJQueryElement('iframeBoxElement')[0]).toEqual(container2);
     expect(PageStructureService.containers[1].getLabel()).toEqual('container 2');
   });
 
   it('adds components to the most recently registered container', () => {
-    const container = $j('#container1', $document);
-    const componentA = $j('#componentA', $document);
-    const componentB = $j('#componentB', $document);
+    const container1 = $j('#container11', $document)[0];
+    const componentA = $j('#componentAA', $document)[0];
+    const componentB = $j('#componentBB', $document)[0];
+    const comment1 = previousComment(container1);
+    const commentA = childComment(componentA);
+    const commentB = childComment(componentB);
 
-    PageStructureService.registerParsedElement(container[0].previousSibling, {
-      'HST-Type': 'CONTAINER_COMPONENT',
-    });
-    PageStructureService.registerParsedElement(container[0].previousSibling, {
-      'HST-Type': 'CONTAINER_COMPONENT',
-    });
-    PageStructureService.registerParsedElement(componentA[0].childNodes[0], {
-      'HST-Type': 'CONTAINER_ITEM_COMPONENT',
-      'HST-Label': 'component A',
-    });
-    PageStructureService.registerParsedElement(componentB[0].childNodes[0], {
-      'HST-Type': 'CONTAINER_ITEM_COMPONENT',
-      'HST-Label': 'component B',
-    });
+    PageStructureService.registerParsedElement(comment1, jsonify(comment1));
+    PageStructureService.registerParsedElement(comment1, jsonify(comment1));
+    PageStructureService.registerParsedElement(commentA, jsonify(commentA));
+    PageStructureService.registerParsedElement(commentB, jsonify(commentB));
 
     expect(PageStructureService.containers.length).toEqual(2);
     expect(PageStructureService.containers[0].isEmpty()).toEqual(true);
@@ -115,22 +143,21 @@ describe('PageStructureService', () => {
     expect(PageStructureService.containers[1].getComponents().length).toEqual(2);
 
     expect(PageStructureService.containers[1].getComponents()[0].type).toEqual('component');
-    expect(PageStructureService.containers[1].getComponents()[0].getJQueryElement('iframeBoxElement')).toEqual(componentA);
+    expect(PageStructureService.containers[1].getComponents()[0].getJQueryElement('iframeBoxElement')[0]).toBe(componentA);
     expect(PageStructureService.containers[1].getComponents()[0].getLabel()).toEqual('component A');
     expect(PageStructureService.containers[1].getComponents()[0].container).toEqual(PageStructureService.containers[1]);
 
     expect(PageStructureService.containers[1].getComponents()[1].type).toEqual('component');
-    expect(PageStructureService.containers[1].getComponents()[1].getJQueryElement('iframeBoxElement')).toEqual(componentB);
+    expect(PageStructureService.containers[1].getComponents()[1].getJQueryElement('iframeBoxElement')[0]).toBe(componentB);
     expect(PageStructureService.containers[1].getComponents()[1].getLabel()).toEqual('component B');
     expect(PageStructureService.containers[1].getComponents()[1].container).toEqual(PageStructureService.containers[1]);
   });
 
   it('clears the page structure', () => {
-    const container = $j('#container1', $document);
+    const container = $j('#container11', $document)[0];
+    const comment = previousComment(container);
 
-    PageStructureService.registerParsedElement(container[0].previousSibling, {
-      'HST-Type': 'CONTAINER_COMPONENT',
-    });
+    PageStructureService.registerParsedElement(comment, jsonify(comment));
 
     expect(PageStructureService.containers.length).toEqual(1);
 
@@ -140,85 +167,54 @@ describe('PageStructureService', () => {
   });
 
   it('registers additional elements', () => {
-    const container1 = $j('#container1', $document);
-    const container2 = $j('#container2', $document);
+    const container1 = $j('#container11', $document)[0];
+    const container2 = $j('#container22', $document);
+    const comment1 = previousComment(container1);
 
-    PageStructureService.registerParsedElement(container1[0].previousSibling, {
-      'HST-Type': 'CONTAINER_COMPONENT',
-    });
+    PageStructureService.registerParsedElement(comment1, jsonify(comment1));
     PageStructureService.containers[0].setJQueryElement('test', container2);
 
     expect(PageStructureService.containers[0].getJQueryElement('test')).toEqual(container2);
   });
 
   it('finds the DOM element of a transparent container as parent of the comment', () => {
-    const containerT = $j('#container-transparent', $document);
+    const container = $j('#container22', $document)[0];
+    const comment = childComment(container);
 
-    PageStructureService.registerParsedElement(containerT[0].childNodes[0], {
-      'HST-Type': 'CONTAINER_COMPONENT',
-      'HST-XType': 'hst.transparent',
-    });
+    PageStructureService.registerParsedElement(comment, jsonify(comment));
 
     expect(PageStructureService.containers.length).toEqual(1);
-    expect(PageStructureService.containers[0].getJQueryElement('iframeBoxElement')).toEqual(containerT);
+    expect(PageStructureService.containers[0].getJQueryElement('iframeBoxElement')[0]).toBe(container);
   });
 
   it('finds the DOM element of a component of a transparent container as next sibling of the comment', () => {
-    const containerT = $j('#container-transparent', $document);
-    const componentT = $j('#component-transparent', $document);
+    const container2 = $j('#container22', $document)[0];
+    const componentD = $j('#componentDD', $document)[0];
+    const comment2 = childComment(container2);
+    const commentD = previousComment(componentD);
 
-    PageStructureService.registerParsedElement(containerT[0].childNodes[0], {
-      'HST-Type': 'CONTAINER_COMPONENT',
-      'HST-XType': 'hst.transparent',
-    });
-    PageStructureService.registerParsedElement(componentT[0].previousSibling, {
-      'HST-Type': 'CONTAINER_ITEM_COMPONENT',
-      uuid: 'test-uuid',
-    });
+    PageStructureService.registerParsedElement(comment2, jsonify(comment2));
+    PageStructureService.registerParsedElement(commentD, jsonify(commentD));
 
     expect(PageStructureService.containers.length).toEqual(1);
     expect(PageStructureService.containers[0].isEmpty()).toEqual(false);
     expect(PageStructureService.containers[0].getComponents().length).toEqual(1);
-    expect(PageStructureService.containers[0].getComponents()[0].getJQueryElement('iframeBoxElement')).toEqual(componentT);
+    expect(PageStructureService.containers[0].getComponents()[0].getJQueryElement('iframeBoxElement')[0]).toBe(componentD);
     expect(PageStructureService.containers[0].getComponents()[0].hasNoIFrameDomElement()).not.toEqual(true);
   });
 
-  it('registers a placeholder iframe element in case of a transparent, empty component', () => {
-    const containerT = $j('#container-transparent', $document);
-    const componentTE = $j('#component-transparent-empty', $document);
-    const comment = $j(componentTE[0].nextSibling);
+  it('registers no iframe box element in case of a transparent, empty component', () => {
+    const container2 = $j('#container22', $document)[0];
+    const comment2 = childComment(container2);
+    const commentC = nextComment(comment2);
 
-    PageStructureService.registerParsedElement(containerT[0].childNodes[0], {
-      'HST-Type': 'CONTAINER_COMPONENT',
-      'HST-XType': 'hst.transparent',
-    });
-    PageStructureService.registerParsedElement(comment[0], {
-      'HST-Type': 'CONTAINER_ITEM_COMPONENT',
-      uuid: 'test-uuid',
-    });
+    PageStructureService.registerParsedElement(comment2, jsonify(comment2));
+    PageStructureService.registerParsedElement(commentC, jsonify(commentC));
 
     expect(PageStructureService.containers.length).toEqual(1);
     expect(PageStructureService.containers[0].isEmpty()).toEqual(false);
     expect(PageStructureService.containers[0].getComponents().length).toEqual(1);
     expect(PageStructureService.containers[0].getComponents()[0].getJQueryElement('iframeBoxElement').length).toEqual(0);
-    expect(PageStructureService.containers[0].getComponents()[0].hasNoIFrameDomElement()).toEqual(true);
-  });
-
-  it('ignores components of a transparent container which have no root DOM element', () => {
-    const containerT = $j('#container-transparent', $document);
-    const componentA = $j('#componentA', $document);
-    spyOn($log, 'debug');
-
-    PageStructureService.registerParsedElement(containerT[0].childNodes[0], {
-      'HST-Type': 'CONTAINER_COMPONENT',
-      'HST-XType': 'hst.transparent',
-    });
-    PageStructureService.registerParsedElement(componentA[0].childNodes[0], {
-      'HST-Type': 'CONTAINER_ITEM_COMPONENT',
-    });
-
-    expect(PageStructureService.containers[0].isEmpty()).toEqual(true);
-    expect($log.debug).toHaveBeenCalled();
   });
 
   it('parses the page meta-data and adds it to the PageMetaDataService', () => {
@@ -265,26 +261,19 @@ describe('PageStructureService', () => {
   });
 
   it('returns a known component', () => {
-    const emptyContainer = $j('#container1', $document);
-    const filledContainer = $j('#container2', $document);
-    const component = $j('#componentA', $document);
+    const container1 = $j('#container11', $document)[0];
+    const componentA = $j('#componentAA', $document)[0];
+    const comment1 = previousComment(container1);
+    const commentA = childComment(componentA);
 
-    PageStructureService.registerParsedElement(emptyContainer[0].previousSibling, {
-      'HST-Type': 'CONTAINER_COMPONENT',
-    });
-    PageStructureService.registerParsedElement(filledContainer[0].previousSibling, {
-      'HST-Type': 'CONTAINER_COMPONENT',
-    });
-    PageStructureService.registerParsedElement(component[0].childNodes[0], {
-      'HST-Type': 'CONTAINER_ITEM_COMPONENT',
-      'HST-Label': 'Test Component',
-      uuid: '1234',
-    });
+    PageStructureService.registerParsedElement(comment1, jsonify(comment1));
+    PageStructureService.registerParsedElement(comment1, jsonify(comment1));
+    PageStructureService.registerParsedElement(commentA, jsonify(commentA));
 
-    const pageComponent = PageStructureService.getComponentById('1234');
+    const pageComponent = PageStructureService.getComponentById('aaaa');
     expect(pageComponent).not.toBeNull();
-    expect(pageComponent.getId()).toEqual('1234');
-    expect(pageComponent.getLabel()).toEqual('Test Component');
+    expect(pageComponent.getId()).toEqual('aaaa');
+    expect(pageComponent.getLabel()).toEqual('component A');
   });
 
   it('returns null when getting an unknown component', () => {
@@ -292,25 +281,22 @@ describe('PageStructureService', () => {
   });
 
   it('removes a valid component and calls HST successfully', () => {
-    const container = $j('#container1', $document);
-    const component = $j('#componentA', $document);
+    const container1 = $j('#container11', $document)[0];
+    const componentA = $j('#componentAA', $document)[0];
+    const comment1 = previousComment(container1);
+    const commentA = childComment(componentA);
 
-    PageStructureService.registerParsedElement(container[0].previousSibling, {
-      'HST-Type': 'CONTAINER_COMPONENT',
-      uuid: 'container-123',
-    });
-    PageStructureService.registerParsedElement(component[0].childNodes[0], {
-      'HST-Type': 'CONTAINER_ITEM_COMPONENT',
-      'HST-Label': 'Test Component',
-      uuid: 'component-1234',
-    });
-    spyOn(HstService, 'doGet').and.returnValue($q.when([]));
+    PageStructureService.registerParsedElement(comment1, jsonify(comment1));
+    PageStructureService.registerParsedElement(commentA, jsonify(commentA));
 
-    PageStructureService.removeComponentById('component-1234');
+    spyOn(HstService, 'removeHstComponent').and.returnValue($q.when([]));
+    spyOn(RenderingService, 'fetchContainerMarkup').and.returnValue($q.when(''));
+
+    PageStructureService.removeComponentById('aaaa');
 
     $rootScope.$digest();
 
-    expect(HstService.doGet).toHaveBeenCalledWith('container-123', 'delete', 'component-1234');
+    expect(HstService.removeHstComponent).toHaveBeenCalledWith('1111', 'aaaa');
   });
 
   it('removes a valid component but fails to call HST', () => {
