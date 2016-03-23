ebf3d0916a447b16dd7fa4db49aa60a5a1345f1c
Unico Hommes <u.hommes@onehippo.com>
2016-03-23 13:27:11 +0100

    CMS-9994 check modules and log warnings for missing and orphaned files

--- cms/automatic-export/repository/src/main/java/org/onehippo/cms7/autoexport/AutoExportModule.java
+++ cms/automatic-export/repository/src/main/java/org/onehippo/cms7/autoexport/AutoExportModule.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2011-2013 Hippo B.V. (http://www.onehippo.com)
+ *  Copyright 2011-2016 Hippo B.V. (http://www.onehippo.com)
  * 
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -63,7 +63,10 @@ public final class AutoExportModule implements DaemonModule {
         } catch (Exception e) {
             log.error("Failed to initialize auto export. Auto export will not be available", e);
         }
-
+        
+        for (Module module : processor.getModules()) {
+            module.check();
+        }
     }
 
     @Override
