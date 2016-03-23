eec2b1c164531ba16b1e2af6969eb2135f86e77b
Arthur Bogaart <a.bogaart@onehippo.com>
2016-03-23 02:16:29 +0100

    CHANNELMGR-470 Fix an issue in Firefox where the height of the iframe is incorrect upon load
    The throttle function executes the callback and then starts blocking incoming calls. This means that the state of the callback owner can be out of sync, as happens in Firefox when using the throttleService to throttle MutationObserver callbacks. Moving the callback call into the timeout handler ensures the state is correct, but might trigger a second callback. This might need a different approach, like maybe re-using the throttle from lodash.

--- channel-manager/frontend-ng/src/angularjs/utils/throttle.service.js
+++ channel-manager/frontend-ng/src/angularjs/utils/throttle.service.js
@@ -23,10 +23,10 @@ export class ThrottleService {
     let wait = false;            // Initially, we're not waiting
     return () => {               // We return a throttled function
       if (!wait) {               // If we're not waiting
-        callback.call();         // Execute users function
         wait = true;             // Prevent future invocations
         setTimeout(() => {       // After a period of time
           wait = false;          // And allow future invocations
+          callback.call();         // Execute users function
         }, limit);
       }
     };
