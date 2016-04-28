2ca55d87c0db8d1c5f35980fb0ce8d5c6bc8b96c
Unico Hommes <u.hommes@onehippo.com>
2016-04-28 16:43:31 +0200

    Revert "HSTTWO-1003: correct typo in license header"
    This reverts commit b3752014f23b86bfae9ab8898f7691a3ed350014

--- site-toolkit/components/core/src/main/java/org/hippoecm/hst/core/linking/HstLinkImpl.java
+++ site-toolkit/components/core/src/main/java/org/hippoecm/hst/core/linking/HstLinkImpl.java
@@ -1,7 +1,7 @@
 /*
  *  Copyright 2008-2015 Hippo B.V. (http://www.onehippo.com)
  * 
- *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  Licensed under the BHSMMApache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
  *  You may obtain a copy of the License at
  * 
