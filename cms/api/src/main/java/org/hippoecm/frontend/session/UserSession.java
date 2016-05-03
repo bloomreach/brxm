9815b3afc5c58211d41c8fa7d00e48cb21910115
Arthur Bogaart <a.bogaart@onehippo.com>
2016-05-03 13:17:52 +0200

    CMS-10070 Expose TimeZone from UserSession
    Simpler code and easier mock in unt test

--- cms/api/src/main/java/org/hippoecm/frontend/session/UserSession.java
+++ cms/api/src/main/java/org/hippoecm/frontend/session/UserSession.java
@@ -1,12 +1,12 @@
 /*
- *  Copyright 2008-2013 Hippo B.V. (http://www.onehippo.com)
- * 
+ *  Copyright 2008-2016 Hippo B.V. (http://www.onehippo.com)
+ *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
  *  You may obtain a copy of the License at
- * 
+ *
  *       http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  *  Unless required by applicable law or agreed to in writing, software
  *  distributed under the License is distributed on an "AS IS" BASIS,
  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,12 +15,14 @@
  */
 package org.hippoecm.frontend.session;
 
+import java.util.TimeZone;
+
 import javax.jcr.RepositoryException;
 import javax.jcr.Session;
 import javax.jcr.observation.ObservationManager;
 import javax.jcr.query.QueryManager;
 
-import org.apache.wicket.ThreadContext;
+import org.apache.wicket.protocol.http.ClientProperties;
 import org.apache.wicket.protocol.http.WebSession;
 import org.apache.wicket.request.Request;
 import org.hippoecm.frontend.observation.IFacetRootsObserver;
@@ -67,11 +69,15 @@ public abstract class UserSession extends WebSession {
     public abstract void logout();
 
     public abstract Session getJcrSession();
-    
+
     public abstract void releaseJcrSession();
 
     public abstract String getApplicationName();
 
     public abstract HippoRepository getHippoRepository() throws RepositoryException;
 
+    public TimeZone getTimeZone() {
+        final ClientProperties properties = getClientInfo().getProperties();
+        return properties.getTimeZone();
+    }
 }
