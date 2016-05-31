c6987dd3db9214e145be06226e29eb0baaafa8d1
Tobias Jeger <t.jeger@onehippo.com>
2016-05-31 13:32:06 +0200

    CHANNELMGR-683 Add unit test for new API method

--- channel-manager/frontend-ng/src/angularjs/api/hst.service.spec.js
+++ channel-manager/frontend-ng/src/angularjs/api/hst.service.spec.js
@@ -340,4 +340,22 @@ describe('HstService', () => {
     hstService.doPut({ foo: 1 }, 'some-uuid', 'one', 'two', 'three').catch(fail);
     $httpBackend.flush();
   });
+
+  it('should relay the success result when retrieving features', () => {
+    const promiseSpy = jasmine.createSpy('promiseSpy');
+    const success = { key: 'value' };
+    $httpBackend.expectGET(`${contextPath}${apiUrlPrefix}/cafebabe./features`).respond(200, success);
+    hstService.getFeatures().then(promiseSpy).catch(() => fail());
+    $httpBackend.flush();
+    expect(promiseSpy).toHaveBeenCalledWith(success);
+  });
+
+  it('should relay the error result when retrieving features', () => {
+    const catchSpy = jasmine.createSpy('catchSpy');
+    const error = { key: 'value' };
+    $httpBackend.expectGET(`${contextPath}${apiUrlPrefix}/cafebabe./features`).respond(500, error);
+    hstService.getFeatures().then(() => fail()).catch(catchSpy);
+    $httpBackend.flush();
+    expect(catchSpy).toHaveBeenCalledWith(error);
+  });
 });
