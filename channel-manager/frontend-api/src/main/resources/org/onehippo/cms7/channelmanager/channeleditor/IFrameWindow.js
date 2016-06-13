0ab5886b170204a64cb400ab844b6442e945ec7a
Mathijs den Burger <m.denburger@onehippo.com>
2016-06-13 15:01:07 +0200

    CHANNELMGR-677 Remove close handshake from IFrameWindow
    The 'close handshake' is not used anymore by any of the remaining
    little Angular apps, so remove it.

--- channel-manager/frontend-api/src/main/resources/org/onehippo/cms7/channelmanager/channeleditor/IFrameWindow.js
+++ channel-manager/frontend-api/src/main/resources/org/onehippo/cms7/channelmanager/channeleditor/IFrameWindow.js
@@ -22,19 +22,12 @@
    * Window that shows a single IFramePanel. The ID of the enclosed IFramePanel is passed to the iframe
    * as a query parameter 'parentExtIFramePanelId'.
    *
-   * The 'close' icon of the window triggers a handshake with the iframe. The host sends a 'close-request'
-   * message to the iframe, to which the iframe should respond with either a 'close-reply-ok' or
-   * 'close-reply-not-ok' message. The former will close the window, the latter will cancel the close.
-   *
    * @type {*}
    */
   Hippo.ChannelManager.ChannelEditor.IFrameWindow = Ext.extend(Ext.Window, {
 
-    performCloseHandshake: true,
-
     constructor: function (config) {
-      var isClosing = false,
-        url = config.iframeUrl;
+      var url = config.iframeUrl;
 
       this.iframePanelId = Ext.id();
 
@@ -51,48 +44,6 @@
             iframeConfig: config.iframeConfig
           }
         ],
-        listeners: {
-          'afterrender': function (self) {
-            var messageBus = self.getIFramePanel().iframeToHost;
-
-            messageBus.subscribe('close-reply', function () {
-              // make sure AngularJS in the iframe is properly destroyed by changing the
-              // location to about:blank before removing the iframe element from the DOM
-              var iframePanel = Ext.getCmp(self.iframePanelId);
-              iframePanel.on('locationchanged', function() {
-                isClosing = true;
-                self.close();
-                isClosing = false;
-              }, this, { single: true });
-              iframePanel.setLocation('about:blank');
-            });
-
-
-/*
-            TODO: potential dead code. Determine if it is still used and remove it if not.
-
-            messageBus.subscribe('browseTo', function (pathInfo, mountId) {
-              if (Ext.isDefined(mountId)) {
-                Hippo.ChannelManager.ChannelEditor.Instance.fireEvent('mountChanged', {
-                  mountId: mountId,
-                  renderPathInfo: pathInfo
-                });
-              } else {
-                // TODO: open page
-                console.log('TODO: open page ' + pathInfo);
-                // old code: Hippo.ChannelManager.ChannelEditor.Instance.browseTo({renderPathInfo: pathInfo});
-              }
-              self.close();
-            });
-*/
-          },
-          'beforeclose': function (self) {
-            if (self.performCloseHandshake && !isClosing) {
-              self.getIFramePanel().hostToIFrame.publish('close-request');
-              return false;
-            }
-          }
-        }
       });
 
       Hippo.ChannelManager.ChannelEditor.IFrameWindow.superclass.constructor.call(this, config);
