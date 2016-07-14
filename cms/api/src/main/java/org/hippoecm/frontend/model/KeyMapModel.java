5edfdd501701698a124b6fa06b7f379f8d0165a5
Canh Ngo <c.ngo@onehippo.com>
2016-07-14 12:24:03 +0200

    CMS-10271 created KeyMapModel to use with IPluginConfig
    The IPluginConfig is a map containing properties. Even the
    PropertyModel can be used to bind a value in the map, it produces
    warning logs. The KeyMapModel solves this issue.
    
    (cherry picked from commit bb7b1ba861c0b6ef12d47bcc8fd4823f2d60c58a)

---
+++ cms/api/src/main/java/org/hippoecm/frontend/model/KeyMapModel.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2016 Hippo B.V. (http://www.onehippo.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package org.hippoecm.frontend.model;
+
+import java.util.Map;
+
+import org.apache.wicket.model.IModel;
+
+/**
+ * A model binding to a value object identified by the key object in the map
+ *
+ * @param <K> type of the key in the map
+ * @param <V> type of the value object in the map
+ */
+public class KeyMapModel<K, V> implements IModel<V> {
+
+    private final K key;
+    private final Map<K, V> map;
+
+    public KeyMapModel(final Map map, final K key) {
+        this.map = map;
+        this.key = key;
+    }
+
+    @Override
+    public V getObject() {
+        return map.get(key);
+    }
+
+    @Override
+    public void setObject(final V object) {
+        map.put(key, object);
+    }
+
+    @Override
+    public void detach() {
+    }
+}
