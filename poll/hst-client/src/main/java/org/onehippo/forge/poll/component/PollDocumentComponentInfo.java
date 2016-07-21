94e4f1213256913ad2b2aac66c4f26ccdefdb236
Jeroen Hoffman <j.hoffman@onehippo.com>
2016-07-21 12:23:28 +0200

    HIPPLUG-1340 make interfaces PollComponentInfo and PollDocumentComponentInfo public again

--- poll/hst-client/src/main/java/org/onehippo/forge/poll/component/PollDocumentComponentInfo.java
+++ poll/hst-client/src/main/java/org/onehippo/forge/poll/component/PollDocumentComponentInfo.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015 Hippo B.V. (http://www.onehippo.com)
+ * Copyright 2015-2016 Hippo B.V. (http://www.onehippo.com)
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,7 +18,7 @@ package org.onehippo.forge.poll.component;
 import org.hippoecm.hst.core.parameters.JcrPath;
 import org.hippoecm.hst.core.parameters.Parameter;
 
-interface PollDocumentComponentInfo extends PollComponentInfo {
+public interface PollDocumentComponentInfo extends PollComponentInfo {
 
     @Override
     @Parameter(name = "poll-dataPath", displayName = "Path to store poll data", hideInChannelManager = true)
