2e66ea4621c95ef00794722efaec76bc1954a930
Tobias Jeger <t.jeger@onehippo.com>
2016-08-22 12:19:44 +0200

    HSTTWO-3773 Extend validator for better error feedback

--- site-toolkit/client-modules/page-composer/src/main/java/org/hippoecm/hst/pagecomposer/jaxrs/services/validators/HasNoChildMountNodeValidator.java
+++ site-toolkit/client-modules/page-composer/src/main/java/org/hippoecm/hst/pagecomposer/jaxrs/services/validators/HasNoChildMountNodeValidator.java
@@ -16,8 +16,12 @@
 
 package org.hippoecm.hst.pagecomposer.jaxrs.services.validators;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.apache.commons.lang.StringUtils;
 import org.hippoecm.hst.configuration.hosting.Mount;
 import org.hippoecm.hst.core.request.HstRequestContext;
 import org.hippoecm.hst.pagecomposer.jaxrs.services.exceptions.ClientError;
@@ -40,7 +44,18 @@ public class HasNoChildMountNodeValidator implements Validator {
                 .anyMatch(mount -> !mount.getChildMounts().isEmpty());
 
         if (hasMountWithChildren) {
-            throw new ClientException("Child mount exists", ClientError.CHILD_MOUNT_EXISTS);
+            final Map<String, String> parameterMap = new HashMap<>();
+            final List<String> mountChildren = new ArrayList<>();
+
+            mounts.stream().forEach(mount -> {
+                for (Mount child : mount.getChildMounts()) {
+                    mountChildren.add(child.getMountPath());
+                }
+            });
+            parameterMap.put("childMountList", StringUtils.join(mountChildren, ", "));
+            parameterMap.put("channel", mounts.get(0).getChannel().getName());
+
+            throw new ClientException("Child mount exists", ClientError.CHILD_MOUNT_EXISTS, parameterMap);
         }
     }
 }
