d2108afdf9cad96150bbfe809d887bba2d7202ae
Ate Douma <a.douma@onehippo.com>
2016-09-05 07:22:39 +0200

    CMS-10424: using CmsContextService for handshake and context bridging with HST applications
    This depends on HSTTWO-3794 providing the implementation and handling from HST side.

--- cms/engine/src/main/java/org/hippoecm/frontend/Main.java
+++ cms/engine/src/main/java/org/hippoecm/frontend/Main.java
@@ -26,10 +26,10 @@ import java.util.Locale;
 import javax.jcr.Node;
 import javax.jcr.PathNotFoundException;
 import javax.jcr.RepositoryException;
-import javax.jcr.SimpleCredentials;
 import javax.jcr.observation.EventListener;
 import javax.jcr.observation.EventListenerIterator;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.apache.commons.lang.StringUtils;
 import org.apache.commons.lang.math.NumberUtils;
@@ -102,7 +102,11 @@ import org.hippoecm.repository.HippoRepository;
 import org.hippoecm.repository.HippoRepositoryFactory;
 import org.hippoecm.repository.api.HippoNodeType;
 import org.hippoecm.repository.api.HippoWorkspace;
-import org.onehippo.sso.CredentialCipher;
+import org.onehippo.cms7.services.cmscontext.CmsInternalCmsContextService;
+import org.onehippo.cms7.services.cmscontext.CmsSessionContext;
+import org.onehippo.cms7.services.cmscontext.CmsContextService;
+import org.onehippo.cms7.services.cmscontext.CmsContextServiceImpl;
+import org.onehippo.cms7.services.HippoServiceRegistry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -163,6 +167,9 @@ public class Main extends PluginApplication {
     }
 
     private HippoRepository repository;
+
+    private CmsContextServiceImpl cmsContextServiceImpl;
+    private CmsInternalCmsContextService cmsContextService;
 
     @Override
     protected void init() {
@@ -298,74 +305,93 @@ public class Main extends PluginApplication {
             }
         }));
 
+        String applicationName = getPluginApplicationName();
 
-        /*
-         * HST SAML kind of authentication handler needed for Template Composer integration
-         *
-         */
-        mount(new MountMapper("auth", new IMountedRequestMapper() {
+        if (PLUGIN_APPLICATION_VALUE_CMS.equals(applicationName)) {
 
-            @Override
-            public IRequestHandler mapRequest(final Request request, final MountParameters mountParams) {
+            // the following is only applicable and needed for the CMS application, not the Console
 
-                PluginUserSession userSession = (PluginUserSession) Session.get();
-                final UserCredentials userCredentials = userSession.getUserCredentials();
+            /*
+             * HST SAML kind of authentication handler needed for Template Composer integration
+             *
+             */
+            cmsContextService = (CmsInternalCmsContextService)HippoServiceRegistry.getService(CmsContextService.class);
+            if ( cmsContextService == null) {
+                cmsContextServiceImpl = new CmsContextServiceImpl();
+                cmsContextService = cmsContextServiceImpl;
+                HippoServiceRegistry.registerService(cmsContextServiceImpl, new Class[]{CmsContextService.class,CmsInternalCmsContextService.class});
+            }
+            mount(new MountMapper("auth", new IMountedRequestMapper() {
 
-                IRequestHandler requestTarget = new BookmarkablePageRequestHandler(new PageProvider(getHomePage(), null));
+                @Override
+                public IRequestHandler mapRequest(final Request request, final MountParameters mountParams) {
 
-                IRequestParameters requestParameters = request.getRequestParameters();
-                final List<StringValue> keyParams = requestParameters.getParameterValues("key");
-                final List<StringValue> destinationUrlParams = requestParameters.getParameterValues("destinationUrl");
+                    IRequestHandler requestTarget = new BookmarkablePageRequestHandler(new PageProvider(getHomePage(), null));
 
-                if (userCredentials != null && destinationUrlParams.size() > 0) {
+                    IRequestParameters requestParameters = request.getRequestParameters();
+                    final List<StringValue> cmsCSIDParams = requestParameters.getParameterValues("cmsCSID");
+                    final List<StringValue> destinationUrlParams = requestParameters.getParameterValues("destinationUrl");
 
-                    requestTarget = new IRequestHandler() {
+                    PluginUserSession userSession = (PluginUserSession) Session.get();
+                    final UserCredentials userCredentials = userSession.getUserCredentials();
 
-                        @Override
-                        public void respond(IRequestCycle requestCycle) {
-                            String destinationUrl = destinationUrlParams.get(0).toString();
-                            WebResponse response = (WebResponse) RequestCycle.get().getResponse();
-                            if (keyParams == null || keyParams.get(0) == null) {
-                                // redirect to destinationURL and include marker that it is a retry. This way
-                                // the destination can choose to not redirect for SSO handshake again if it still does not
-                                // have a key
-                                if (destinationUrl.contains("?")) {
-                                    response.sendRedirect(destinationUrl + "&retry");
-                                } else {
-                                    response.sendRedirect(destinationUrl + "?retry");
+                    HttpSession httpSession = ((ServletWebRequest)request).getContainerRequest().getSession();
+                    final CmsSessionContext cmsSessionContext = CmsSessionContext.getContext(httpSession);
+
+                    if (destinationUrlParams.size() > 0 && (cmsSessionContext != null || userCredentials != null)) {
+
+                        requestTarget = new IRequestHandler() {
+
+                            @Override
+                            public void respond(IRequestCycle requestCycle) {
+                                String destinationUrl = destinationUrlParams.get(0).toString();
+                                WebResponse response = (WebResponse) RequestCycle.get().getResponse();
+                                String cmsCSID = cmsCSIDParams == null ? null : cmsCSIDParams.get(0) == null ? null : cmsCSIDParams.get(0).toString();
+                                if (!cmsContextService.getId().equals(cmsCSID)) {
+                                    // redirect to destinationURL and include marker that it is a retry. This way
+                                    // the destination can choose to not redirect for SSO handshake again if it still does not
+                                    // have a key
+                                    if (destinationUrl.contains("?")) {
+                                        response.sendRedirect(destinationUrl + "&retry");
+                                    } else {
+                                        response.sendRedirect(destinationUrl + "?retry");
+                                    }
+                                    return;
                                 }
-                                return;
-                            }
-                            String key = keyParams.get(0).toString();
-                            CredentialCipher cipher = CredentialCipher.getInstance();
-                            String encryptedString = cipher.getEncryptedString(key, (SimpleCredentials) userCredentials.getJcrCredentials());
+                                String cmsSessionContextId = cmsSessionContext != null ? cmsSessionContext.getId() : null;
+                                if (cmsSessionContextId == null) {
+                                    CmsSessionContext newCmsSessionContext = cmsContextService.create(httpSession);
+                                    cmsContextService.setData(newCmsSessionContext, CmsSessionContext.REPOSITORY_CREDENTIALS, userCredentials.getJcrCredentials());
+                                    cmsSessionContextId = newCmsSessionContext.getId();
 
-                            if (destinationUrl.contains("?")) {
-                                response.sendRedirect(destinationUrl + "&cred=" + encryptedString);
-                            } else {
-                                response.sendRedirect(destinationUrl + "?cred=" + encryptedString);
+                                }
+                                if (destinationUrl.contains("?")) {
+                                    response.sendRedirect(destinationUrl + "&cmsCSID="+ cmsContextService.getId()+"&cmsSCID=" + cmsSessionContextId);
+                                } else {
+                                    response.sendRedirect(destinationUrl + "?cmsCSID="+ cmsContextService.getId()+"&cmsSCID=" + cmsSessionContextId);
+                                }
                             }
-                        }
 
-                        @Override
-                        public void detach(IRequestCycle requestCycle) {
-                            //Nothing to detach.
-                        }
-                    };
+                            @Override
+                            public void detach(IRequestCycle requestCycle) {
+                                //Nothing to detach.
+                            }
+                        };
+                    }
+                    return requestTarget;
                 }
-                return requestTarget;
-            }
 
-            @Override
-            public int getCompatibilityScore(final Request request) {
-                return 0;
-            }
+                @Override
+                public int getCompatibilityScore(final Request request) {
+                    return 0;
+                }
 
-            @Override
-            public Mount mapHandler(final IRequestHandler requestHandler) {
-                return null;
-            }
-        }));
+                @Override
+                public Mount mapHandler(final IRequestHandler requestHandler) {
+                    return null;
+                }
+            }));
+        }
 
         // caching resource stream locator implementation that allows the class argument to be null.
         final IResourceStreamLocator resourceStreamLocator = resourceSettings.getResourceStreamLocator();
@@ -473,7 +499,6 @@ public class Main extends PluginApplication {
         });
 
         if (log.isInfoEnabled()) {
-            String applicationName = getPluginApplicationName();
             log.info("Hippo CMS application " + applicationName + " has started");
         }
     }
@@ -581,6 +606,11 @@ public class Main extends PluginApplication {
 
     @Override
     protected void onDestroy() {
+        if (cmsContextServiceImpl != null) {
+            HippoServiceRegistry.unregisterService(cmsContextServiceImpl, CmsContextService.class);
+            cmsContextServiceImpl = null;
+        }
+        cmsContextService = null;
         if (repository != null) {
             // remove listeners
             JcrObservationManager jom = JcrObservationManager.getInstance();
--- cms/engine/src/main/java/org/hippoecm/frontend/service/restproxy/RestProxyServicePlugin.java
+++ cms/engine/src/main/java/org/hippoecm/frontend/service/restproxy/RestProxyServicePlugin.java
@@ -17,14 +17,12 @@ package org.hippoecm.frontend.service.restproxy;
 
 import java.lang.annotation.Annotation;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Set;
 
 import javax.jcr.Credentials;
-import javax.jcr.SimpleCredentials;
 import javax.security.auth.Subject;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import javax.ws.rs.core.MediaType;
 
 import org.apache.commons.proxy.Interceptor;
@@ -33,6 +31,7 @@ import org.apache.commons.proxy.ProxyFactory;
 import org.apache.cxf.common.util.StringUtils;
 import org.apache.cxf.jaxrs.client.JAXRSClientFactory;
 import org.apache.cxf.jaxrs.client.WebClient;
+import org.apache.wicket.protocol.http.servlet.ServletWebRequest;
 import org.apache.wicket.request.cycle.RequestCycle;
 import org.hippoecm.frontend.plugin.IPluginContext;
 import org.hippoecm.frontend.plugin.Plugin;
@@ -43,7 +42,10 @@ import org.hippoecm.frontend.session.PluginUserSession;
 import org.hippoecm.frontend.session.UserSession;
 import org.hippoecm.hst.diagnosis.HDC;
 import org.hippoecm.hst.diagnosis.Task;
-import org.onehippo.sso.CredentialCipher;
+import org.onehippo.cms7.services.cmscontext.CmsInternalCmsContextService;
+import org.onehippo.cms7.services.cmscontext.CmsSessionContext;
+import org.onehippo.cms7.services.cmscontext.CmsContextService;
+import org.onehippo.cms7.services.HippoServiceRegistry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -63,10 +65,9 @@ public class RestProxyServicePlugin extends Plugin implements IRestProxyService 
 
     private static final Logger log = LoggerFactory.getLogger(IRestProxyService.class);
 
-    private static final String CMSREST_CREDENTIALS_HEADER = "X-CMSREST-CREDENTIALS";
+    private static final String HEADER_CMS_CONTEXT_SERVICE_ID = "X-CMS-CS-ID";
+    private static final String HEADER_CMS_SESSION_CONTEXT_ID = "X-CMS-SC-ID";
     private static final String CMSREST_CMSHOST_HEADER = "X-CMSREST-CMSHOST";
-    // This is really bad workaround but it is used only for the time being
-    private static final String CREDENTIAL_CIPHER_KEY = "ENC_DEC_KEY";
     public static final String CONFIG_REST_URI = "rest.uri";
     public static final String CONFIG_CONTEXT_PATH = "context.path";
     public static final String CONFIG_SERVICE_ID = "service.id";
@@ -170,11 +171,18 @@ public class RestProxyServicePlugin extends Plugin implements IRestProxyService 
     public <T> T createSecureRestProxy(final Class<T> restServiceApiClass, final List<Object> additionalProviders) {
         T clientProxy = JAXRSClientFactory.create(restUri, restServiceApiClass, getProviders(additionalProviders));
 
-        Subject subject = getSubject();
+        HttpSession httpSession = ((ServletWebRequest)RequestCycle.get().getRequest()).getContainerRequest().getSession();
+        CmsSessionContext cmsSessionContext = CmsSessionContext.getContext(httpSession);
+        if (cmsSessionContext == null) {
+            CmsInternalCmsContextService cmsContextService = (CmsInternalCmsContextService)HippoServiceRegistry.getService(CmsContextService.class);
+            cmsSessionContext = cmsContextService.create(httpSession);
+            cmsContextService.setData(cmsSessionContext, CmsSessionContext.REPOSITORY_CREDENTIALS, ((PluginUserSession) UserSession.get()).getCredentials());
+        }
         // The accept method is called to solve an issue as the REST call was sent with 'text/plain' as an accept header
         // which caused problems matching with the relevant JAXRS resource
         WebClient.client(clientProxy)
-                .header(CMSREST_CREDENTIALS_HEADER, getEncryptedCredentials(subject))
+                .header(HEADER_CMS_CONTEXT_SERVICE_ID, cmsSessionContext.getCmsContextServiceId())
+                .header(HEADER_CMS_SESSION_CONTEXT_ID, cmsSessionContext.getId())
                 .header(CMSREST_CMSHOST_HEADER, getFarthestRequestHost())
                 .accept(MediaType.WILDCARD_TYPE);
 
@@ -214,24 +222,6 @@ public class RestProxyServicePlugin extends Plugin implements IRestProxyService 
             host = request.getServerName() + ":" + serverPort;
         }
         return host;
-    }
-
-    protected String getEncryptedCredentials(Subject subject) throws IllegalArgumentException {
-        if (subject == null) {
-            throw new IllegalArgumentException("Null subject has been passed which is not acceptable as an argument!");
-        }
-
-        Set<Object> credentials = subject.getPrivateCredentials();
-
-        if ( (credentials == null) || (credentials.isEmpty()) ) {
-            throw new IllegalArgumentException("Subject has no credentials attached with it!");
-        }
-
-        Iterator<Object> credentialsIterator = credentials.iterator();
-        SimpleCredentials subjectCredentials = (SimpleCredentials) credentialsIterator.next();
-
-        CredentialCipher credentialCipher = CredentialCipher.getInstance();
-        return credentialCipher.getEncryptedString(CREDENTIAL_CIPHER_KEY, subjectCredentials);
     }
 
     protected List<Object> getProviders(final List<Object> additionalProviders) {
