629e97caa1cf15961e1d96db4d8009707c877d0a
Oscar Scholten <o.scholten@onehippo.com>
2016-09-05 13:48:35 +0200

    HIPPLUG-1388 moving component parameter translations to repository resource bundle

--- seo-support/site/src/main/java/org/onehippo/forge/seo/support/SEOHelperComponentParamsInfo.java
+++ seo-support/site/src/main/java/org/onehippo/forge/seo/support/SEOHelperComponentParamsInfo.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2011-2015 Hippo B.V. (http://www.onehippo.com)
+ * Copyright 2011-2016 Hippo B.V. (http://www.onehippo.com)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -66,64 +66,63 @@ import org.hippoecm.hst.core.parameters.Parameter;
 
 public interface SEOHelperComponentParamsInfo {
 
-    @Parameter(name="document-attribute", defaultValue="document", description="Request scope attribute name for the current request content bean", hideInChannelManager = true)
+    @Parameter(name="document-attribute", defaultValue="document", hideInChannelManager = true)
     String getDocumentAttribute();
 
-    @Parameter(name="menu-name", defaultValue="main", description="Name with which the current site menu for the request is retrieved")
+    @Parameter(name="menu-name", defaultValue="main")
     String getMenuName();
 
-    @Parameter(name="menu-attribute", defaultValue="menu", description="Request scope attribute name for the current request site menu", hideInChannelManager = true)
+    @Parameter(name="menu-attribute", defaultValue="menu", hideInChannelManager = true)
     String getMenuAttribute();
 
-    @Parameter(name="menu-item-allow-expanded", defaultValue = "false",
-            description = "Flag whether the sitemenu item is allowed to be an expanded ancestor. If false, only exact matches are allowed.")
+    @Parameter(name="menu-item-allow-expanded", defaultValue = "false")
     boolean isMenuItemAllowExpanded();
 
-    @Parameter(name="params-info-attribute", defaultValue="paramsInfo", description="Request scope attribute name for the parameters info of the current HST component", hideInChannelManager = true)
+    @Parameter(name="params-info-attribute", defaultValue="paramsInfo", hideInChannelManager = true)
     String getParamsInfoAttribute();
 
-    @Parameter(name="document-title-bean-props", defaultValue="title", description="Candidate bean property names of the current request content bean from which document title can be retrieved")
+    @Parameter(name="document-title-bean-props", defaultValue="title")
     String getDocumentTitleBeanProperties();
 
-    @Parameter(name="document-keywords-bean-props", defaultValue="keywords", description="Candidate bean property names of the current request content bean from which comma separated keywords can be retrieved", hideInChannelManager = true)
+    @Parameter(name="document-keywords-bean-props", defaultValue="keywords", hideInChannelManager = true)
     String getDocumentKeywordsBeanProperties();
 
-    @Parameter(name="document-description-bean-props", defaultValue="description", description="Candidate bean property names of the current request content bean from which document description can be retrieved")
+    @Parameter(name="document-description-bean-props", defaultValue="description")
     String getDocumentDescriptionBeanProperties();
 
-    @Parameter(name="site-title", description="Title of this website")
+    @Parameter(name="site-title")
     String getSiteTitle();
 
-    @Parameter(name="dc-schema-link", defaultValue="http://purl.org/dc/elements/1.1/", description="Dublin Core Schema Link")
+    @Parameter(name="dc-schema-link", defaultValue="http://purl.org/dc/elements/1.1/")
     String getDublinCoreSchemaLink();
 
-    @Parameter(name="dc-terms-link", defaultValue="http://purl.org/dc/terms/", description="Dublin Core Terms Link")
+    @Parameter(name="dc-terms-link", defaultValue="http://purl.org/dc/terms/")
     String getDublinCoreTermsLink();
 
-    @Parameter(name="dc-language", description="Dublin Core Language")
+    @Parameter(name="dc-language")
     String getDublinCoreLanguage();
 
-    @Parameter(name="enable-dc-language", description="Enable Dublin Core Language")
+    @Parameter(name="enable-dc-language")
     String getEnableDublinCoreLanguage();
 
-    @Parameter(name="site-dc-copyright-link", description="SITE's Dublin Core Copyright Link")
+    @Parameter(name="site-dc-copyright-link")
     String getSiteDublinCoreCopyrightLink();
 
-    @Parameter(name="headers-in-template", defaultValue="false", description="Flag whether a render template is used to contribute head elements.", hideInChannelManager = true)
+    @Parameter(name="headers-in-template", defaultValue="false", hideInChannelManager = true)
     boolean isHeadersInTemplate();
 
-    @Parameter(name="keywords-in-document-title", defaultValue="true", description="Flag whether keywords are appended to document title.", hideInChannelManager = true)
+    @Parameter(name="keywords-in-document-title", defaultValue="true", hideInChannelManager = true)
     boolean isKeywordsInDocumentTitle();
 
-    @Parameter(name="default-meta-keywords", defaultValue="", description="If there are no meta-keywords in the document (because the field is empty or not available) these values are used", hideInChannelManager = true)
+    @Parameter(name="default-meta-keywords", defaultValue="", hideInChannelManager = true)
     String getDefaultMetaKeywords();
 
-    @Parameter(name="default-meta-description", defaultValue="", description="If there are no meta-description in the document (because the field is empty or not available) this value is used")
+    @Parameter(name="default-meta-description", defaultValue="")
     String getDefaultMetaDescription();
 
-    @Parameter(name="site-title-in-template", defaultValue="false", description="Flag whether the page title is provided by the template.")
+    @Parameter(name="site-title-in-template", defaultValue="false")
     boolean isSiteTitleInTemplate();
 
-    @Parameter(name="template-page-title", defaultValue="%(siteTitle) - %(menuItem) - %(pageTitle) : %(keywords)", description="Template used to generate the page title")
+    @Parameter(name="template-page-title", defaultValue="%(siteTitle) - %(menuItem) - %(pageTitle) : %(keywords)")
     String getTemplatePageTitle();
 }
