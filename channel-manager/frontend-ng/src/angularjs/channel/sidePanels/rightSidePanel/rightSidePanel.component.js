b7eba653c32e0c8b846d5ce25254de9bd0263c77
Mark <m.lenser@onehippo.com>
2016-09-07 12:19:49 +0200

    CHANNELMGR-842 add close so the panel can close itself

--- channel-manager/frontend-ng/src/angularjs/channel/sidePanels/rightSidePanel/rightSidePanel.component.js
+++ channel-manager/frontend-ng/src/angularjs/channel/sidePanels/rightSidePanel/rightSidePanel.component.js
@@ -56,6 +56,10 @@ export class ChannelRightSidePanelCtrl {
   isActiveSiteMapItem(siteMapItem) {
     return siteMapItem.renderPathInfo === this.HippoIframeService.getCurrentRenderPathInfo();
   }
+
+  close() {
+    this.ChannelSidePanelService.close('right');
+  }
 }
 
 const channelRightSidePanelComponentModule = angular
