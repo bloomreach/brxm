2ada3e1a99c3a592f638acb03f65277604ae847a
Bert Leunis <b.leunis@onehippo.com>
2016-09-15 15:38:50 +0200

    HSTTWO-3617 check for instance of Number instead of Long or Double

--- site-toolkit/content-beans/src/main/java/org/hippoecm/hst/content/beans/query/filter/FilterImpl.java
+++ site-toolkit/content-beans/src/main/java/org/hippoecm/hst/content/beans/query/filter/FilterImpl.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2008-2013 Hippo B.V. (http://www.onehippo.com)
+ *  Copyright 2008-2016 Hippo B.V. (http://www.onehippo.com)
  * 
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -511,7 +511,7 @@ public class FilterImpl implements Filter {
     public String getStringValue(Object value) throws FilterException {
         if (value instanceof String || value instanceof Boolean) {
             return "'" + value.toString() + "'";
-        } else if (value instanceof Long || value instanceof Double) {
+        } else if (value instanceof Number) {
             return value.toString();
         } else if (value instanceof Calendar) {
             return DateTools.createXPathConstraint(session, (Calendar)value);
