0c1f016ecf94f109ec72caa9ad36ae85af4309d5
Joeri de Gooijer <j.degooijer@onehippo.com>
2016-09-16 11:43:36 +0200

    CHANNELMGR-863 Change requires to imports

--- channel-manager/frontend-ng/src/index.js
+++ channel-manager/frontend-ng/src/index.js
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-require('./vendor.scss');
-require('./index.scss');
+import './vendor.scss';
+import './index.scss';
 
 export { hippoCmng } from './angularjs/hippo-cm.js';
