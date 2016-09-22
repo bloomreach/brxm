ac5bf122c04947dfc77b88b11b3478ce2fe0eda7
Arthur Bogaart <a.bogaart@onehippo.com>
2016-09-22 15:07:38 +0200

    CHANNELMGR-863 Stop karma task immediatly on done
    Dont pass a null into done callback as that will cause gulp to wait for 20 or so seconds while the tests already finished

--- channel-manager/frontend-ng/gulpfile.js
+++ channel-manager/frontend-ng/gulpfile.js
@@ -4,19 +4,11 @@ const browserSync = require('browser-sync');
 
 const conf = require('./conf/gulp.conf');
 
-// Load some files into the registry
+// Load tasks into the registry
 const hub = new HubRegistry([conf.path.tasks('*.js')]);
 
 // Tell gulp to use the tasks just loaded
 gulp.registry(hub);
-
-gulp.task('build', gulp.series(gulp.parallel('other', 'webpack:dist')));
-gulp.task('test', gulp.series('karma:single-run'));
-gulp.task('test:auto', gulp.series('karma:auto-run'));
-gulp.task('serve', gulp.series('webpack:watch', 'watch', 'browsersync'));
-gulp.task('serve:dist', gulp.series('default', 'browsersync:dist'));
-gulp.task('default', gulp.series('clean', 'build'));
-gulp.task('watch', watch);
 
 function reloadBrowserSync(cb) {
   browserSync.reload();
@@ -27,3 +19,11 @@ function watch(done) {
   gulp.watch(conf.path.src('app/**/*.html'), reloadBrowserSync);
   done();
 }
+
+gulp.task('build', gulp.series(gulp.parallel('other', 'webpack:dist')));
+gulp.task('test', gulp.series('karma:single-run'));
+gulp.task('test:auto', gulp.series('karma:auto-run'));
+gulp.task('serve', gulp.series('webpack:watch', 'watch', 'browsersync'));
+gulp.task('serve:dist', gulp.series('default', 'browsersync:dist'));
+gulp.task('default', gulp.series('clean', 'build'));
+gulp.task('watch', watch);
