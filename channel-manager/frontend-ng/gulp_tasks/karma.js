42419a267309ecc3b97bb4aece8ed2bc6af691f7
Arthur Bogaart <a.bogaart@onehippo.com>
2016-09-23 12:04:47 +0200

    CHANNELMGR-863 Simplified gulp-karma task

--- channel-manager/frontend-ng/gulp_tasks/karma.js
+++ channel-manager/frontend-ng/gulp_tasks/karma.js
@@ -1,29 +1,19 @@
 const path = require('path');
-
 const gulp = require('gulp');
-const karma = require('karma');
+const { Server } = require('karma');
 
-function karmaFinishHandler(done) {
-  return failCount => {
-    if (failCount) {
-      done(new Error(`Failed ${failCount} tests.`));
-    } else {
-      done();
-    }
+function karmaRun(config) {
+  const configFile = path.join(process.cwd(), 'conf', config);
+  return (done) => {
+    new Server({ configFile }, failCount => {
+      if (failCount === 0) {
+        done();
+      } else {
+        done(new Error(`Failed ${failCount} tests.`));
+      }
+    }).start();
   };
 }
 
-function karmaSingleRun(done) {
-  const configFile = path.join(process.cwd(), 'conf', 'karma.conf.js');
-  const karmaServer = new karma.Server({ configFile }, karmaFinishHandler(done));
-  karmaServer.start();
-}
-
-function karmaAutoRun(done) {
-  const configFile = path.join(process.cwd(), 'conf', 'karma-auto.conf.js');
-  const karmaServer = new karma.Server({ configFile }, karmaFinishHandler(done));
-  karmaServer.start();
-}
-
-gulp.task('karma:single-run', karmaSingleRun);
-gulp.task('karma:auto-run', karmaAutoRun);
+gulp.task('karma:single-run', karmaRun('karma.conf.js'));
+gulp.task('karma:auto-run', karmaRun('karma-auto.conf.js'));
