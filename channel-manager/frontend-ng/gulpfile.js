02e61f7c4d4e0bd275be2b14fbd848df4954f8af
Joeri de Gooijer <j.degooijer@onehippo.com>
2016-09-29 17:37:44 +0200

    CHANNELMGR-863 implement frontend-build with webpack

--- channel-manager/frontend-ng/gulpfile.js
+++ channel-manager/frontend-ng/gulpfile.js
@@ -1,22 +1,4 @@
-const del = require('del');
 const gulp = require('gulp');
 const HubRegistry = require('gulp-hub');
-const conf = require('./conf/gulp.conf');
-
-// Load tasks into the registry
-const hub = new HubRegistry([conf.path.tasks('*.js')]);
-
-// Tell gulp to use the tasks just loaded
+const hub = new HubRegistry(['node_modules/frontend-build/gulpfile.js']);
 gulp.registry(hub);
-
-function clean() {
-  return del([conf.paths.dist, conf.paths.tmp]);
-}
-
-gulp.task('test', gulp.series('karma:single-run'));
-gulp.task('test:auto', gulp.series('karma:auto-run'));
-gulp.task('serve', gulp.series('webpack:serve'));
-gulp.task('serveDist', gulp.series('webpack:distServe'));
-gulp.task('clean', clean);
-gulp.task('build', gulp.series('clean', 'webpack:dist'));
-gulp.task('default', gulp.series('build'));
