5b32b074f3547a009d158c72e06420da84091f56
Bert Leunis <b.leunis@onehippo.com>
2016-10-21 15:08:36 +0200

    HSTTWO-3842 ResourceContainer: Don't log warning when resource path is invalid - just log message at  info level
    (cherry picked from commits 80deb6a86f7958c8eb5934b9160dd3e7b456c09f, 2d1debc40fa04324de92d7a776e1df073c4884a6)

--- site-toolkit/commons/src/main/java/org/hippoecm/hst/core/linking/AbstractResourceContainer.java
+++ site-toolkit/commons/src/main/java/org/hippoecm/hst/core/linking/AbstractResourceContainer.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2010-2014 Hippo B.V. (http://www.onehippo.com)
+ *  Copyright 2010-2016 Hippo B.V. (http://www.onehippo.com)
  * 
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -173,9 +173,8 @@ public abstract class AbstractResourceContainer implements ResourceContainer {
        } catch (PathNotFoundException e) {
            log.debug("Cannot find resource node for path '{}' beloning to pathInfo '{}'", actualPath, pathInfo);
        } catch (RepositoryException e) {
-           log.warn("RepositoryException: '{}'", e.getMessage());
+           log.info("RepositoryException: '{}'", e.getMessage());
        }
-        
         return null;
     }
 
