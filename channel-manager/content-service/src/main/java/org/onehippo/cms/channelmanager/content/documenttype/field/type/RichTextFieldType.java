1497b5ece5efc6bc37debb3b0576dc170d796f00
Michael Metternich <michael.metternich@bloomreach.com>
2017-01-12 14:17:19 +0100

    CHANNELMGR-1041 Read the content of a Rich Text field and provide it to the REST API

--- channel-manager/content-service/src/main/java/org/onehippo/cms/channelmanager/content/documenttype/field/type/RichTextFieldType.java
+++ channel-manager/content-service/src/main/java/org/onehippo/cms/channelmanager/content/documenttype/field/type/RichTextFieldType.java
@@ -16,10 +16,52 @@
 
 package org.onehippo.cms.channelmanager.content.documenttype.field.type;
 
+import org.hippoecm.repository.util.NodeIterable;
+import org.hippoecm.repository.util.PropertyIterable;
+import org.onehippo.cms.channelmanager.content.document.model.FieldValue;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.jcr.Node;
+import javax.jcr.Property;
+import javax.jcr.RepositoryException;
+import java.util.*;
+
 public class RichTextFieldType extends StringFieldType {
+
+    private static final Logger log = LoggerFactory.getLogger(RichTextFieldType.class);
 
     public RichTextFieldType() {
         setType(Type.RICH_TEXT);
+    }
+
+    @Override
+    public Optional<List<FieldValue>> readFrom(final Node node) {
+        final List<FieldValue> values = readValues(node);
+
+        trimToMaxValues(values);
+
+        return values.isEmpty() ? Optional.empty() : Optional.of(values);
+    }
+
+    private List<FieldValue> readValues(final Node node) {
+        final String nodeName = getId();
+        final List<FieldValue> values = new ArrayList<>();
+        try {
+            for (final Node child : new NodeIterable(node.getNodes(nodeName))) {
+                if(child.hasProperty("hippostd:content")) {
+                    values.add(new FieldValue(child.getProperty("hippostd:content").getString()));
+                }
+                for (final Node subNode : new NodeIterable(child.getNodes())) {
+                    if(subNode.hasProperty("hippo:docbase")) {
+                        values.add(new FieldValue(subNode.getProperty("hippo:docbase").getString()));
+                    }
+                }
+            }
+        } catch (final RepositoryException e) {
+            log.warn("Failed to read nodes for compound type '{}'", getId(), e);
+        }
+        return values;
     }
 
 }
