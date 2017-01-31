fcced6186d864b1c1e099e35c6473991772daed6
Mathijs den Burger <mathijs.denburger@bloomreach.com>
2017-01-31 16:52:06 +0100

    CHANNELMGR-1149 Don't fire 'properties changed' event twice
    The setActiveTab() call already fires a properties changed event.

--- channel-manager/frontend/src/main/resources/org/onehippo/cms7/channelmanager/channeleditor/ComponentPropertiesPanel.js
+++ channel-manager/frontend/src/main/resources/org/onehippo/cms7/channelmanager/channeleditor/ComponentPropertiesPanel.js
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2016 Hippo B.V. (http://www.onehippo.com)
+ * Copyright 2010-2017 Hippo B.V. (http://www.onehippo.com)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -463,7 +463,6 @@
         this.setActiveTab(newTabIndex);
         this.syncSize();
         newTab.syncVisibleHeight();
-        newPropertiesForm.firePropertiesChanged();
       } else {
         console.log("Cannot find tab for variant '" + existingVariantId + "', copy to '" + newVariant + "' failed");
       }
