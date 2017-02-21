713d13662221a48d6a95e0ce08cb8e7f644fed23
Mathijs den Burger <mathijs.denburger@bloomreach.com>
2017-02-21 14:02:50 +0100

    CHANNELMGR-1168 Show horizontal scrollbar for constrained viewports
    Removed the use of flexbox in the hippoIframe markup since
    centering a constrained sheet div with flexbox does not work in
    Safari. Instead we center the sheet with margin auto and make the
    canvas as big as needed with absolute positioning.

--- channel-manager/frontend-ng/src/angularjs/channel/hippoIframe/viewport/viewport.service.js
+++ channel-manager/frontend-ng/src/angularjs/channel/hippoIframe/viewport/viewport.service.js
@@ -34,11 +34,17 @@ class ViewportService {
     this.width = width;
 
     if (width === 0) {
-      // Desktop mode - no width constraints
-      this.$sheet.css('max-width', '100%');
+      // 'any device' mode - no width constraints
+      this.$sheet.css({
+        'max-width': '100%',
+        'min-width': 0,
+      });
     } else {
       // viewport is constrained
-      this.$sheet.css('max-width', `${width}px`);
+      this.$sheet.css({
+        'max-width': `${width}px`,
+        'min-width': `${width}px`,
+      });
     }
   }
 
