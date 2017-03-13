7a4d4151ff8b78adcf4c0582ace84fd149864e88
Ate Douma <ate.douma@bloomreach.com>
2017-03-13 17:35:41 +0100

    CMS-10653 Provide JCR Event Collector test harness for verifying specific JCR Events (to be) raised during testing

---
+++ testutils/src/main/java/org/onehippo/testutils/jcr/event/EventUtils.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2017 Hippo B.V. (http://www.onehippo.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.onehippo.testutils.jcr.event;
+
+import static javax.jcr.observation.Event.NODE_ADDED;
+import static javax.jcr.observation.Event.NODE_MOVED;
+import static javax.jcr.observation.Event.NODE_REMOVED;
+import static javax.jcr.observation.Event.PERSIST;
+import static javax.jcr.observation.Event.PROPERTY_ADDED;
+import static javax.jcr.observation.Event.PROPERTY_CHANGED;
+import static javax.jcr.observation.Event.PROPERTY_REMOVED;
+
+public class EventUtils {
+
+    public static final int ALL_EVENTS = NODE_ADDED | NODE_REMOVED | NODE_MOVED
+            | PROPERTY_ADDED | PROPERTY_CHANGED | PROPERTY_REMOVED | PERSIST;
+
+}
