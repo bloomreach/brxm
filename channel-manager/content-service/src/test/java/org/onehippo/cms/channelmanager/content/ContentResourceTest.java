5a43d6791676599ef6fd56deaf2a3c5c39f9488d
Michiel Rop <michiel.rop@bloomreach.com>
2017-04-10 09:49:09 +0200

    CHANNELMGR-1231 Remove duplicated Code
    org.onehippo.cms.channelmanager.content.ContentServiceModule and a enterprise
    addon both use the same functionality: initialization, configuration and
    shutdown of a service that adds a json rest resource. This means a huge part is
    duplicated.
    
    Unit tests are moved to the repository as well. ( See REPO-1650 )

--- channel-manager/content-service/src/test/java/org/onehippo/cms/channelmanager/content/ContentResourceTest.java
+++ channel-manager/content-service/src/test/java/org/onehippo/cms/channelmanager/content/ContentResourceTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 Hippo B.V. (http://www.onehippo.com)
+ * Copyright 2017 Hippo B.V. (http://www.onehippo.com)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -40,6 +40,7 @@ import org.onehippo.cms.channelmanager.content.documenttype.model.DocumentType;
 import org.onehippo.cms.channelmanager.content.documenttype.DocumentTypesService;
 import org.onehippo.cms.channelmanager.content.document.DocumentsService;
 import org.onehippo.jaxrs.cxf.CXFTest;
+import org.onehippo.repository.jaxrs.api.SessionDataProvider;
 import org.powermock.api.easymock.PowerMock;
 import org.powermock.core.classloader.annotations.PowerMockIgnore;
 import org.powermock.core.classloader.annotations.PrepareForTest;
