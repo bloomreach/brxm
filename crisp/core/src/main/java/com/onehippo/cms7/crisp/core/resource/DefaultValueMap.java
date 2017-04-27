268fbbac70e871a54ac5ecef51475d79d5b157d3
Woonsan Ko <woonsan.ko@bloomreach.com>
2017-04-27 18:08:51 -0400

    Adding javadocs

--- crisp/core/src/main/java/com/onehippo/cms7/crisp/core/resource/DefaultValueMap.java
+++ crisp/core/src/main/java/com/onehippo/cms7/crisp/core/resource/DefaultValueMap.java
@@ -13,16 +13,29 @@ import java.util.Set;
 import com.onehippo.cms7.crisp.api.resource.AbstractValueMap;
 import com.onehippo.cms7.crisp.api.resource.ValueMap;
 
+/**
+ * Default {@link ValueMap} implementation.
+ */
 public class DefaultValueMap extends AbstractValueMap {
 
     private static final long serialVersionUID = 1L;
 
+    /**
+     * Delegating map instance.
+     */
     private final Map<String, Object> delegatedMap;
 
+    /**
+     * Default constructor.
+     */
     public DefaultValueMap() {
         this(null);
     }
 
+    /**
+     * Constructs with a map to delegate.
+     * @param delegatedMap a map to delegate
+     */
     public DefaultValueMap(Map<String, Object> delegatedMap) {
         if (delegatedMap == null) {
             delegatedMap = new LinkedHashMap<>();
@@ -31,66 +44,105 @@ public class DefaultValueMap extends AbstractValueMap {
         this.delegatedMap = delegatedMap;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void clear() {
         delegatedMap.clear();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public Object put(String key, Object value) {
         return (delegatedMap.put(key, value));
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void putAll(Map<? extends String, ? extends Object> map) {
         delegatedMap.putAll(map);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public Object remove(Object key) {
         return (delegatedMap.remove(key));
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public int size() {
         return delegatedMap.size();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public boolean isEmpty() {
         return delegatedMap.isEmpty();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public boolean containsKey(Object key) {
         return delegatedMap.containsKey(key);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public boolean containsValue(Object value) {
         return delegatedMap.containsValue(value);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public Object get(Object name) {
         return delegatedMap.get(name);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public Set<String> keySet() {
         return delegatedMap.keySet();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public Collection<Object> values() {
         return delegatedMap.values();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public Set<java.util.Map.Entry<String, Object>> entrySet() {
         return delegatedMap.entrySet();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public boolean equals(Object o) {
         if (o == null || !(o instanceof DefaultValueMap)) {
@@ -100,11 +152,18 @@ public class DefaultValueMap extends AbstractValueMap {
         return Objects.equals(delegatedMap, ((DefaultValueMap) o).delegatedMap);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public int hashCode() {
         return delegatedMap.hashCode();
     }
 
+    /**
+     * Returns an unmodifiable view of this <code>ValueMap</code>
+     * @return an unmodifiable view of this <code>ValueMap</code>
+     */
     public ValueMap toUnmodifiable() {
         return new DefaultValueMap(Collections.unmodifiableMap(delegatedMap));
     }
--- crisp/core/src/main/java/com/onehippo/cms7/crisp/core/resource/SpringResourceDataCache.java
+++ crisp/core/src/main/java/com/onehippo/cms7/crisp/core/resource/SpringResourceDataCache.java
@@ -8,14 +8,27 @@ import org.springframework.cache.Cache.ValueWrapper;
 
 import com.onehippo.cms7.crisp.api.resource.ResourceDataCache;
 
+/**
+ * {@link ResourceDataCache} implementation using Spring Framework's {@link Cache} instance.
+ */
 public class SpringResourceDataCache implements ResourceDataCache {
 
+    /**
+     * The delegate cache representation of Spring Framework's {@link Cache}.
+     */
     private final Cache cache;
 
+    /**
+     * Constructs with a Spring Framework's {@link Cache} instance.
+     * @param cache a Spring Framework's {@link Cache} instance
+     */
     public SpringResourceDataCache(final Cache cache) {
         this.cache = cache;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public Object getData(Object key) {
         Object data = null;
@@ -29,11 +42,17 @@ public class SpringResourceDataCache implements ResourceDataCache {
         return data;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void putData(Object key, Object data) {
         cache.put(key, data);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public Object putDataIfAbsent(Object key, Object data) {
         Object existingData = null;
@@ -47,11 +66,17 @@ public class SpringResourceDataCache implements ResourceDataCache {
         return existingData;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void evictData(Object key) {
         cache.evict(key);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void clear() {
         cache.clear();
