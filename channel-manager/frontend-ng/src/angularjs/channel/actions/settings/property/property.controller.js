d484a5413ec16cf19bfd6f74f3e9085159d2b892
Ard Schrijvers <ard.schrijvers@bloomreach.com>
2017-05-03 11:23:02 +0200

    CHANNELMGR-1239 Make sure the settings fields are read only if the channel node is not editable
    (this is the case when the channel node is not below hst:workspace)

--- channel-manager/frontend-ng/src/angularjs/channel/actions/settings/property/property.controller.js
+++ channel-manager/frontend-ng/src/angularjs/channel/actions/settings/property/property.controller.js
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 Hippo B.V. (http://www.onehippo.com)
+ * Copyright 2016-2017 Hippo B.V. (http://www.onehippo.com)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -47,7 +47,9 @@ class ChannelPropertyCtrl {
     this.annotation = this._getFirstFieldAnnotation();
     this.type = this._getType();
     this.qaClass = this._getQaClass();
-    this.readOnly = this.data.lockedBy && this.data.lockedBy !== ConfigService.cmsUser;
+    if (!this.data.editable || (this.data.lockedBy && this.data.lockedBy !== ConfigService.cmsUser)) {
+      this.readOnly = true;
+    }
     this.required = this.definition && this.definition.isRequired;
 
     if (this._isPickerField()) {
--- channel-manager/frontend-ng/src/angularjs/channel/actions/settings/settings.controller.js
+++ channel-manager/frontend-ng/src/angularjs/channel/actions/settings/settings.controller.js
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 Hippo B.V. (http://www.onehippo.com)
+ * Copyright 2016-2017 Hippo B.V. (http://www.onehippo.com)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -37,9 +37,13 @@ class ChannelSettingsCtrl {
 
     this.ChannelService.getChannelInfoDescription()
       .then((channelInfoDescription) => {
+        console.log(channelInfoDescription);
         this.channelInfoDescription = channelInfoDescription;
         if (this.isLockedByOther()) {
           this.FeedbackService.showErrorOnSubpage('ERROR_CHANNEL_SETTINGS_READONLY', { lockedBy: channelInfoDescription.lockedBy });
+        }
+        if (!this.isEditable()) {
+          this.FeedbackService.showErrorOnSubpage('ERROR_CHANNEL_SETTINGS_NOT_EDITABLE');
         }
       })
       .catch(() => {
@@ -53,6 +57,13 @@ class ChannelSettingsCtrl {
 
   isLockedByOther() {
     return this.channelInfoDescription.lockedBy && this.channelInfoDescription.lockedBy !== this.ConfigService.cmsUser;
+  }
+  isEditable() {
+    return this.channelInfoDescription.editable;
+  }
+
+  isSaveDisabled() {
+    return this.isLockedByOther() || !this.isEditable();
   }
 
   touchRequiredFields() {
