de47d5570f918dac4c93d2bea25075199e489e30
Bert Leunis <bert.leunis@bloomreach.com>
2017-05-18 15:10:16 +0200

    REPO-1661 respect dirlevel setting correctly when checking if next level must be read

--- repository/engine/src/main/java/org/hippoecm/repository/security/group/AbstractGroupManager.java
+++ repository/engine/src/main/java/org/hippoecm/repository/security/group/AbstractGroupManager.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2008-2016 Hippo B.V. (http://www.onehippo.com)
+ *  Copyright 2008-2017 Hippo B.V. (http://www.onehippo.com)
  * 
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -321,7 +321,7 @@ public abstract class AbstractGroupManager implements GroupManager {
                             }
                         }
                     }
-                } else if (groupNode.isNodeType(NT_GROUPFOLDER) && level <= dirLevels) {
+                } else if (groupNode.isNodeType(NT_GROUPFOLDER) && level < dirLevels) {
                     level++;
                     groups.addAll(getMembershipsByPath(userId, providerId, groupNode, level));
                 }
