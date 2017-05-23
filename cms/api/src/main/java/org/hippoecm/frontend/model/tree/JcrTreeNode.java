225f4ae4fc739762fb9421fc5223beb78ec3df46
Jeroen Hoffman <jeroen.hoffman@bloomreach.com>
2017-05-23 11:40:25 +0200

    CMS-10553 non-functional: remove serialVersionUID

--- cms/api/src/main/java/org/hippoecm/frontend/model/tree/JcrTreeNode.java
+++ cms/api/src/main/java/org/hippoecm/frontend/model/tree/JcrTreeNode.java
@@ -38,7 +38,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class JcrTreeNode extends NodeModelWrapper<JcrTreeNode> implements IJcrTreeNode {
-    private static final long serialVersionUID = 1L;
 
     static final Logger log = LoggerFactory.getLogger(JcrTreeNode.class);
 
