e8a92163088da1a158ab0cc04bc63a9c0edd7205
Ate Douma <ate.douma@bloomreach.com>
2017-05-24 00:45:05 +0200

    HCM-111 final packaging refactoring - first only git mv and rm sources

--- configuration-management/engine/esv2yaml.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo mvn test -Dtest=TestEsv2Yaml -Dsrc=$HIPPO_CODE_HOME/$1/$2/master/$3/src/main/resources -Dtarget=target/esv2yaml/$2/$3
-mvn test -Dtest=TestEsv2Yaml -Dsrc=$HIPPO_CODE_HOME/$1/$2/master/$3/src/main/resources -Dtarget=target/esv2yaml/$2/$3
-
