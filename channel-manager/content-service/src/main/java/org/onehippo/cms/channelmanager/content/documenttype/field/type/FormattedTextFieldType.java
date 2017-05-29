930c3dd0ac19637b638c9e888af3186b882f296f
Arthur Bogaart <arthur.bogaart@bloomreach.com>
2017-05-29 15:06:20 +0200

    CHANNELMGR-1308 Update package names

--- channel-manager/content-service/src/main/java/org/onehippo/cms/channelmanager/content/documenttype/field/type/FormattedTextFieldType.java
+++ channel-manager/content-service/src/main/java/org/onehippo/cms/channelmanager/content/documenttype/field/type/FormattedTextFieldType.java
@@ -22,8 +22,6 @@ import java.util.Optional;
 
 import javax.jcr.Node;
 
-import com.fasterxml.jackson.databind.node.ObjectNode;
-
 import org.onehippo.ckeditor.CKEditorConfig;
 import org.onehippo.cms.channelmanager.content.document.model.FieldValue;
 import org.onehippo.cms.channelmanager.content.documenttype.field.FieldTypeContext;
@@ -32,6 +30,8 @@ import org.onehippo.cms7.services.htmlprocessor.model.HtmlProcessorModel;
 import org.onehippo.cms7.services.htmlprocessor.model.Model;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.fasterxml.jackson.databind.node.ObjectNode;
 
 public class FormattedTextFieldType extends StringFieldType {
 
