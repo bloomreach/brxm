492d3b7b3cf81bef47ac61a3e548a50b71a6cdea
Mathijs den Burger <mathijs.denburger@bloomreach.com>
2017-06-13 16:16:33 +0200

    CHANNELMGR-1037 Call backend when drafting a field

--- channel-manager/frontend-ng/src/angularjs/services/content.service.js
+++ channel-manager/frontend-ng/src/angularjs/services/content.service.js
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 Hippo B.V. (http://www.onehippo.com)
+ * Copyright 2016-2017 Hippo B.V. (http://www.onehippo.com)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -43,8 +43,7 @@ class ContentService {
   }
 
   draftField(documentId, fieldName, value) {
-    // TODO: HTTP call
-    angular.noop(`ContentService.draftField (id: ${fieldName}, data: ${value})`);
+    return this._send('PUT', ['documents', documentId, 'draft', fieldName], value);
   }
 
   _send(method, pathElements, data) {
