59788c7f25cf25cd3de0bebe68acd03a41d69d67
Sergey Shepelevich <sergey.shepelevich@bloomreach.com>
2017-06-20 12:47:25 +0200

    CMS-10797 Move Console YAML import/export changes into hippo-cms-console-frontend project

---
+++ cms/console/frontend/src/main/java/org/hippoecm/frontend/plugins/console/menu/content/YamlExportPlugin.java
@@ -0,0 +1,46 @@
+/*
+ *  Copyright 2017 Hippo B.V. (http://www.onehippo.com)
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.hippoecm.frontend.plugins.console.menu.content;
+
+import javax.jcr.Node;
+
+import org.apache.wicket.model.Model;
+import org.hippoecm.frontend.dialog.DialogLink;
+import org.hippoecm.frontend.dialog.IDialogFactory;
+import org.hippoecm.frontend.dialog.IDialogService.Dialog;
+import org.hippoecm.frontend.model.JcrNodeModel;
+import org.hippoecm.frontend.plugin.IPluginContext;
+import org.hippoecm.frontend.plugin.config.IPluginConfig;
+import org.hippoecm.frontend.plugins.console.NodeModelReference;
+import org.hippoecm.frontend.service.render.RenderPlugin;
+
+public class YamlExportPlugin extends RenderPlugin<Node> {
+
+    private static final long serialVersionUID = 1L;
+
+    public YamlExportPlugin(IPluginContext context, IPluginConfig config) {
+        super(context, config);
+        IDialogFactory factory = new IDialogFactory() {
+            private static final long serialVersionUID = 1L;
+            public Dialog createDialog() {
+                return new YamlExportDialog(new NodeModelReference(YamlExportPlugin.this,
+                        (JcrNodeModel) getDefaultModel()));
+            }
+        };
+        add(new DialogLink("link", new Model<>("YAML Export"), factory, getDialogService()));
+    }
+
+}
