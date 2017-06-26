8a8ca62f0f73b061782b623c9c544ac9f959f2b9
Marijan Milicevic <marijan.milicevic@bloomreach.com>
2017-06-26 10:46:53 +0200

    ESSENTIALS-1076 Add personalization plugins to essentials project

---
+++ essentials/plugins/bloomreach-connector/src/main/java/org/onehippo/cms7/essentials/bloomreach/connector/rest/BloomreachConnectorResource.java
@@ -0,0 +1,180 @@
+/*
+ * Copyright 2017 Hippo B.V. (http://www.onehippo.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.onehippo.cms7.essentials.bloomreach.connector.rest;
+
+import java.io.InputStream;
+
+import javax.inject.Inject;
+import javax.jcr.Node;
+import javax.jcr.Session;
+import javax.servlet.ServletContext;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+
+import org.onehippo.cms7.essentials.dashboard.ctx.PluginContext;
+import org.onehippo.cms7.essentials.dashboard.ctx.PluginContextFactory;
+import org.onehippo.cms7.essentials.dashboard.model.DependencyRestful;
+import org.onehippo.cms7.essentials.dashboard.model.TargetPom;
+import org.onehippo.cms7.essentials.dashboard.rest.BaseResource;
+import org.onehippo.cms7.essentials.dashboard.rest.ErrorMessageRestful;
+import org.onehippo.cms7.essentials.dashboard.rest.MessageRestful;
+import org.onehippo.cms7.essentials.dashboard.rest.RestfulList;
+import org.onehippo.cms7.essentials.dashboard.utils.CndUtils;
+import org.onehippo.cms7.essentials.dashboard.utils.DependencyUtils;
+import org.onehippo.cms7.essentials.dashboard.utils.GlobalUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.eventbus.EventBus;
+
+
+@Produces({MediaType.APPLICATION_JSON})
+@Consumes({MediaType.APPLICATION_JSON, MediaType.APPLICATION_FORM_URLENCODED})
+@Path("bloomreachConnector/")
+public class BloomreachConnectorResource extends BaseResource {
+
+    private static final Logger log = LoggerFactory.getLogger(BloomreachConnectorResource.class);
+    @Inject
+    private EventBus eventBus;
+
+
+    @GET
+    public ResourceData index(@Context ServletContext servletContext) throws Exception {
+        foo();
+        // check if we have crisp namespace registered:
+        final PluginContext context = PluginContextFactory.getContext();
+        final boolean exists = CndUtils.nodeTypeExists(context, "crisp:resourceresolvercontainer");
+        final ResourceData resourceData = new ResourceData();
+        final DependencyRestful dependency = new DependencyRestful();
+        dependency.setTargetPom(TargetPom.CMS.getName());
+        dependency.setArtifactId("hippo-addon-crisp-repository");
+        dependency.setGroupId("com.onehippo.cms7");
+        final DependencyRestful dependencyApi = new DependencyRestful();
+        dependencyApi.setTargetPom(TargetPom.CMS.getName());
+        dependencyApi.setArtifactId("hippo-addon-crisp-api");
+        dependencyApi.setGroupId("com.onehippo.cms7");
+        final boolean hasDependency = DependencyUtils.hasDependency(context, dependency)
+                && DependencyUtils.hasDependency(context, dependencyApi);
+        resourceData.setCrispDependencyExists(hasDependency);
+        resourceData.setCrispExists(exists);
+        return resourceData;
+    }
+
+
+    private void foo() {
+        System.out.println("log = " + log);
+    }
+
+    @POST
+    @Path("/install")
+    public RestfulList<MessageRestful> install(final ResourceData data, @Context ServletContext servletContext) throws Exception {
+        // check if we have crisp namespace registered:
+        final PluginContext context = PluginContextFactory.getContext();
+        final RestfulList<MessageRestful> retVal = new RestfulList<>();
+        //TODO read dependency from plugin descriptor
+        final DependencyRestful dependency = new DependencyRestful();
+        dependency.setTargetPom(TargetPom.CMS.getName());
+        dependency.setArtifactId("hippo-addon-crisp-repository");
+        dependency.setGroupId("com.onehippo.cms7");
+        final DependencyRestful dependencyApi = new DependencyRestful();
+        dependencyApi.setTargetPom(TargetPom.CMS.getName());
+        dependencyApi.setArtifactId("hippo-addon-crisp-api");
+        dependencyApi.setGroupId("com.onehippo.cms7");
+        //
+        dependency.setVersion("0.9.0");
+        dependencyApi.setVersion("0.9.0");
+        boolean added = DependencyUtils.addDependency(context, dependency) &&
+                DependencyUtils.addDependency(context, dependencyApi);
+        if (added) {
+            retVal.add(new MessageRestful("Successfully added dependencies"));
+        } else {
+            retVal.add(new ErrorMessageRestful("Failed to add dependencies"));
+        }
+        return retVal;
+    }
+
+    @POST
+    public RestfulList<MessageRestful> run(final ResourceData data, @Context ServletContext servletContext) throws Exception {
+        log.info("data = {}", data);
+        // TODO validate data
+
+        final PluginContext context = PluginContextFactory.getContext();
+        final Session session = context.createSession();
+        final RestfulList<MessageRestful> retVal = new RestfulList<>();
+        try {
+
+            final String basePath = data.getBasePath();
+            final Node root = session.getNode(basePath);
+            final String resourceName = data.getResourceName();
+            if (root.hasNode(resourceName)) {
+                retVal.add(new MessageRestful("Resource with name" + resourceName + " already exists!"));
+                return retVal;
+            }
+            final Node node = root.addNode(resourceName, "crisp:resourceresolver");
+            final InputStream stream = getClass().getResourceAsStream("/bloomreachResourcetemplate.xml");
+            final String xml = GlobalUtils.readStreamAsText(stream);
+            node.setProperty("crisp:beandefinition", xml);
+            // add names
+            node.setProperty("crisp:propnames", new String[]{
+                    "bloomreach.account_id",
+                    "bloomreach.auth_key",
+                    "bloomreach.domain_key",
+                    "bloomreach.realm",
+                    "bloomreach.fl",
+                    "bloomreach.ref_url",
+                    "bloomreach.url",
+                    "bloomreach.request_id",
+                    "bloomreach.baseUrl",
+                    "bloomreach.maxEntriesLocalHeap",
+                    "bloomreach.maxEntriesLocalDisk",
+                    "bloomreach.timeToLiveSeconds",
+                    "bloomreach.timeToIdleSeconds"
+
+            });
+
+            // add values
+            node.setProperty("crisp:propvalues", new String[]{
+                    data.getAccountId(),
+                    data.getAuthKey(),
+                    data.getDomainKey(),
+                    data.getRealm(),
+                    data.getFl(),
+                    data.getRefUrl(),
+                    data.getUrl(),
+                    data.getRequestId(),
+                    data.getBaseUrl(),
+                    String.valueOf(data.getMaxEntriesLocalHeap()),
+                    String.valueOf(data.getMaxEntriesLocalDisk()),
+                    String.valueOf(data.getTimeToLiveSeconds()),
+                    String.valueOf(data.getTimeToIdleSeconds())
+
+            });
+            session.save();
+            retVal.add(new MessageRestful("Successfully created Bloomreach CRSIP resource: " + resourceName));
+        } finally {
+            GlobalUtils.cleanupSession(session);
+        }
+        return retVal;
+    }
+
+
+}
---
+++ essentials/plugins/bloomreach-connector/src/main/resources/META-INF/resources/tool/bloomreachConnector/bloomreachConnector.js
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2017 Hippo B.V. (http://www.onehippo.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+(function () {
+    "use strict";
+    angular.module('hippo.essentials')
+        .controller('bloomreachConnectorCtrl', function ($scope, $sce, $log, $rootScope, $http) {
+            
+            $scope.endpoint = $rootScope.REST.dynamic + 'bloomreachConnector/';
+            $scope.data = {};
+            $scope.crispExists = false;
+            $scope.crispDependencyExists = false;
+
+
+            $scope.run = function () {
+                var payload = $scope.data;
+                $http.post($scope.endpoint, payload).success(function (data) {
+
+                });
+            };
+            $scope.install = function () {
+
+                $http.post($scope.endpoint +"install", {}).success(function (data) {
+                    // do nothing
+                    $scope.crispDependencyExists = true;
+                });
+            };
+            $http.get($scope.endpoint).success(function (data) {
+                console.log(data);
+                data.resourceName = "productsResource";
+                data.realm = "prod";
+                data.refUrl = "refUrl";
+                data.fl = "pid,title,brand,price,sale_price,promotions,thumb_image,sku_thumb_images,sku_swatch_images,sku_color_group,url,price_range,sale_price_range,description,is_live,score";
+                $scope.data = data;
+                $scope.crispExists = data.crispExists;
+                $scope.crispDependencyExists = data.crispDependencyExists;
+            });
+
+        });
+})();
