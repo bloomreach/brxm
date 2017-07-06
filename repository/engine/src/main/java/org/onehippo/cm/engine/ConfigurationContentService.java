9e15b864b7b156d062b3cfbdd1ea16677b77c005
Tobias Jeger <tobias.jeger@bloomreach.com>
2017-07-06 16:46:49 +0200

    REPO-1728 Refactor upgrade logic to get rid of system property

--- repository/engine/src/main/java/org/onehippo/cm/engine/ConfigurationContentService.java
+++ repository/engine/src/main/java/org/onehippo/cm/engine/ConfigurationContentService.java
@@ -27,6 +27,7 @@ import java.util.Set;
 import javax.jcr.Node;
 import javax.jcr.RepositoryException;
 import javax.jcr.Session;
+import javax.jcr.nodetype.NodeType;
 
 import org.apache.commons.lang.StringUtils;
 import org.onehippo.cm.model.ActionItem;
@@ -70,10 +71,20 @@ public class ConfigurationContentService {
      * @param session active {@link Session}
      */
     public void apply(final ConfigurationModelImpl model, final Session session) throws RepositoryException {
+        final boolean isUpgradeTo12 = checkUpgradeTo12(session);
+        boolean allModulesHaveSucceeded = true;
+
         for (ModuleImpl module : model.getModules()) {
             if (isNotEmpty(module.getActionsMap()) || isNotEmpty(module.getContentDefinitions())) {
-                apply(module, model, session);
+                final boolean success = apply(module, model, session, isUpgradeTo12);
+                if (!success) {
+                    allModulesHaveSucceeded = false;
+                }
             }
+        }
+
+        if (isUpgradeTo12 && allModulesHaveSucceeded) {
+            completeUpgradeTo12(session);
         }
     }
 
@@ -105,10 +116,11 @@ public class ConfigurationContentService {
      * @param module target {@link Module}
      * @param session active {@link Session}
      */
-    private void apply(final ModuleImpl module, final ConfigurationModel model, final Session session) throws RepositoryException {
+    private boolean apply(final ModuleImpl module, final ConfigurationModel model,
+                          final Session session, final boolean isUpgradeTo12) throws RepositoryException {
         final double moduleSequenceNumber = module.getSequenceNumber() != null ? module.getSequenceNumber() : Double.MIN_VALUE;
         final List<ActionItem> actionsToProcess = collectNewActions(moduleSequenceNumber, module.getActionsMap());
-        processItemsToDelete(actionsToProcess, model, session);
+        processItemsToDelete(actionsToProcess, model, session, isUpgradeTo12);
         session.save();
 
         sortContentDefinitions(module);
@@ -132,7 +144,7 @@ public class ConfigurationContentService {
                 try {
                     if (ConfigurationModelUtils.getCategoryForNode(baseNodePath, model) == ConfigurationItemCategory.CONTENT) {
                         log.debug("Processing {} action for node: {}", action, baseNodePath);
-                        contentProcessingService.apply(contentNode, action, session);
+                        contentProcessingService.apply(contentNode, action, session, isUpgradeTo12);
 
                         if (!nodeAlreadyProcessed) {
                             // will save all the session changes!
@@ -152,6 +164,7 @@ public class ConfigurationContentService {
         }
 
         configurationBaselineService.updateModuleSequenceNumber(module, session);
+        return failedPaths.isEmpty();
     }
 
     /**
@@ -183,7 +196,7 @@ public class ConfigurationContentService {
      * @param session active {@link Session}
      */
     private void processItemsToDelete(final List<ActionItem> items, final ConfigurationModel model,
-                                      final Session session) throws RepositoryException {
+                                      final Session session, final boolean isUpgradeTo12) throws RepositoryException {
         for (final ActionItem item : items) {
             if (item.getType() == ActionType.DELETE) {
                 final String baseNodePath = item.getPath();
@@ -192,7 +205,7 @@ public class ConfigurationContentService {
 
                     final DefinitionNode deleteNode = new DefinitionNodeImpl(baseNodePath,
                             StringUtils.substringAfterLast(baseNodePath, SEPARATOR), null);
-                    contentProcessingService.apply(deleteNode, ActionType.DELETE, session);
+                    contentProcessingService.apply(deleteNode, ActionType.DELETE, session, isUpgradeTo12);
                 } else {
                     log.warn(String.format("Base node '%s' is not categorized as content, skipping delete action.",
                             baseNodePath));
@@ -213,5 +226,46 @@ public class ConfigurationContentService {
                 .filter(x -> x.getPath().equals(absolutePath) && x.getType() != ActionType.DELETE)
                 .map(ActionItem::getType)
                 .reduce((__, second) -> second);
+    }
+
+    /**
+     * Below upgradeTo12 functionality works as follows:
+     *
+     * Upon entering ConfigurationContentService#apply, we check if the content node (/hcm:hcm/hcm:content) exists.
+     * If it doesn't, we create it, and we also create a "content upgrade to 12 marker" node at the root of the
+     * repository. The content node doesn't exist when bootstrapping the first time against a CMS 11 repository, but
+     * also when bootstrapping a fresh repository. In the latter case, there will be no content in the repository
+     * either, and therefore, the upgrade flag's effect will be nil.
+     *
+     * While that marker node exists, we accept/ignore the situation that a content definition is attempted to be
+     * applied (i.e. it has not yet been marked as applied in the baseline), while the content definition's base node
+     * already exists in the repository. We do, however, in such a case, add the content definition base path to the
+     * baseline's set of applied content paths, such that subsequently, this content definition is ignored (unless it
+     * is associated with a content action).
+     *
+     * The first time the content of all modules is applied successfully, the marker node is removed, and therefore,
+     * the system returns to its normal content bootstrapping behaviour.
+     *
+     * Above-described logic pertains to the upgrade from CMS 11 to CMS 12. In CMS 13, this logic should be cleaned up.
+     */
+    private static final String UPGRADE_TO_12_MARKER_NODE_NAME = "content-upgrade-to-12-marker";
+
+    private boolean checkUpgradeTo12(final Session session) throws RepositoryException {
+        if (!configurationBaselineService.contentNodeExists(session)) {
+            configurationBaselineService.createContentNode(session);
+            session.getRootNode()
+                    .addNode(UPGRADE_TO_12_MARKER_NODE_NAME, NodeType.NT_UNSTRUCTURED)
+                    .setProperty("description", "This node is used by the bootstrapping mechanism " +
+                            "to track the upgrade of repository content to CMS 12. Please don't remove " +
+                            "it manually, it will be removed automatically.");
+            session.save();
+        }
+
+        return session.nodeExists("/" + UPGRADE_TO_12_MARKER_NODE_NAME);
+    }
+
+    private void completeUpgradeTo12(final Session session) throws RepositoryException {
+        session.removeItem("/" + UPGRADE_TO_12_MARKER_NODE_NAME);
+        session.save();
     }
 }
