d3476eb309d8952fe8b975794901300d3e7fa9a7
Mathijs den Burger <mathijs.denburger@bloomreach.com>
2017-07-26 10:59:06 +0200

    CHANNELMGR-1368 Change indentation to 2 spaces

--- channel-manager/frontend/src/main/resources/org/onehippo/cms7/channelmanager/BreadcrumbToolbar.js
+++ channel-manager/frontend/src/main/resources/org/onehippo/cms7/channelmanager/BreadcrumbToolbar.js
@@ -1,5 +1,5 @@
 /**
- * Copyright 2012-2014 Hippo B.V. (http://www.onehippo.com)
+ * Copyright 2012-2017 Hippo B.V. (http://www.onehippo.com)
  *
  * Licensed under the Apache License, Version 2.0 (the  "License");
  * you may not use this file except in compliance with the License.
@@ -13,125 +13,125 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-(function() {
-    "use strict";
+(function () {
+  "use strict";
 
-    Ext.namespace('Hippo.ChannelManager');
+  Ext.namespace('Hippo.ChannelManager');
 
-    /** TODO fix Ext bug
-     * The implementation is using a temporary hack to prevent ext removing the complete toolbar
-     * with hiding / removing toolbar items.
-     * Instead of using default ext implementation to handle the toolbar items we just set the visibility of the
-     * items with pure dom manipulation.
+  /** TODO fix Ext bug
+   * The implementation is using a temporary hack to prevent ext removing the complete toolbar
+   * with hiding / removing toolbar items.
+   * Instead of using default ext implementation to handle the toolbar items we just set the visibility of the
+   * items with pure dom manipulation.
+   */
+  Hippo.ChannelManager.BreadcrumbToolbar = Ext.extend(Ext.Toolbar, {
+
+    constructor: function (config) {
+      this.breadcrumbStackSize = 0;
+      this.breadcrumbIconUrl = config.breadcrumbIconUrl;
+
+      if (config.items === undefined) {
+        config.items = [];
+      }
+
+      Hippo.ChannelManager.BreadcrumbToolbar.superclass.constructor.call(this, config);
+    },
+
+    createBreadcrumbItem: function (card) {
+      var item = this.add({
+        id: 'breadcrumb-item' + this.breadcrumbStackSize,
+        cls: 'breadcrumb-item',
+        icon: this.breadcrumbIconUrl,
+        margins: {
+          top: 0,
+          right: 10,
+          bottom: 0,
+          left: 0
+        },
+        text: card.title,
+        scope: this
+      });
+      item.card = card;
+      card.on('titlechange', this._onTitleChange, item);
+
+      this.doLayout();
+      this.breadcrumbStackSize++;
+      return item;
+    },
+
+    getBreadcrumbItem: function (index) {
+      if (index >= this.breadcrumbStackSize) {
+        return false;
+      }
+      return this.getComponent('breadcrumb-item' + index);
+    },
+
+    /**
+     * listener to be invoked with breadcrumbitem scope
+     * Updates the title of the breadcrumb
      */
-    Hippo.ChannelManager.BreadcrumbToolbar = Ext.extend(Ext.Toolbar, {
+    _onTitleChange: function (panel, title) {
+      this.setText(title);
+    },
 
-        constructor: function(config) {
-            this.breadcrumbStackSize = 0;
-            this.breadcrumbIconUrl = config.breadcrumbIconUrl;
+    // public methods:
 
-            if (config.items === undefined) {
-                config.items = [];
-            }
+    pushItem: function (config) {
+      var index, breadcrumbItem, i;
+      index = this.breadcrumbStackSize;
 
-            Hippo.ChannelManager.BreadcrumbToolbar.superclass.constructor.call(this, config);
-        },
+      breadcrumbItem = this.createBreadcrumbItem(config.card);
+      breadcrumbItem.setDisabled(true);
 
-        createBreadcrumbItem: function(card) {
-            var item = this.add({
-                id: 'breadcrumb-item' + this.breadcrumbStackSize,
-                cls: 'breadcrumb-item',
-                icon: this.breadcrumbIconUrl,
-                margins: {
-                    top: 0,
-                    right: 10,
-                    bottom: 0,
-                    left: 0
-                },
-                text: card.title,
-                scope: this
-            });
-            item.card = card;
-            card.on('titlechange', this._onTitleChange, item);
+      // IE ignores all style attributes when a containing element (table in this case) has been disabled
+      if (breadcrumbItem.rendered) {
+        breadcrumbItem.getEl().dom.disabled = false;
+      }
 
-            this.doLayout();
-            this.breadcrumbStackSize++;
-            return item;
-        },
-
-        getBreadcrumbItem: function(index) {
-            if (index >= this.breadcrumbStackSize) {
-                return false;
-            }
-            return this.getComponent('breadcrumb-item' + index);
-        },
-
-        /**
-         * listener to be invoked with breadcrumbitem scope
-         * Updates the title of the breadcrumb
-         */
-        _onTitleChange: function(panel, title) {
-            this.setText(title);
-        },
-
-        // public methods:
-
-        pushItem: function(config) {
-            var index, breadcrumbItem, i;
-            index = this.breadcrumbStackSize;
-
-            breadcrumbItem = this.createBreadcrumbItem(config.card);
-            breadcrumbItem.setDisabled(true);
-
-            // IE ignores all style attributes when a containing element (table in this case) has been disabled
-            if (breadcrumbItem.rendered) {
-                breadcrumbItem.getEl().dom.disabled = false;
-            }
-
-            breadcrumbItem.on('click', function() {
-                if (index === this.breadcrumbStackSize) {
-                    return;
-                }
-                while (index + 1 < this.breadcrumbStackSize) {
-                    this.popItem();
-                }
-                if (config.scope) {
-                    config.click.apply(config.scope, arguments);
-                } else {
-                    config.click.apply(breadcrumbItem, arguments);
-                }
-            }, this);
-
-            for (i = 0; i < index; i++) {
-                breadcrumbItem = this.getBreadcrumbItem(i);
-                breadcrumbItem.setDisabled(false);
-            }
-        },
-
-        popItem: function() {
-            var lastBreadcrumbItem, card, newLastBreadcrumbItem;
-            if (this.breadcrumbStackSize === 0) {
-                return null;
-            }
-
-            lastBreadcrumbItem = this.getBreadcrumbItem(this.breadcrumbStackSize - 1);
-
-            card = lastBreadcrumbItem.card;
-            card.un('titlechange', this._onTitleChange, lastBreadcrumbItem);
-
-            this.remove(lastBreadcrumbItem);
-            this.breadcrumbStackSize--;
-            if (this.breadcrumbStackSize === 1) {
-                newLastBreadcrumbItem = this.getBreadcrumbItem(this.breadcrumbStackSize - 1);
-                newLastBreadcrumbItem.setDisabled(true);
-            }
-            return lastBreadcrumbItem;
-        },
-
-        getBreadcrumbSize: function() {
-            return this.breadcrumbStackSize;
+      breadcrumbItem.on('click', function () {
+        if (index === this.breadcrumbStackSize) {
+          return;
         }
-    });
-    Ext.reg('Hippo.ChannelManager.BreadcrumbToolbar', Hippo.ChannelManager.BreadcrumbToolbar);
+        while (index + 1 < this.breadcrumbStackSize) {
+          this.popItem();
+        }
+        if (config.scope) {
+          config.click.apply(config.scope, arguments);
+        } else {
+          config.click.apply(breadcrumbItem, arguments);
+        }
+      }, this);
+
+      for (i = 0; i < index; i++) {
+        breadcrumbItem = this.getBreadcrumbItem(i);
+        breadcrumbItem.setDisabled(false);
+      }
+    },
+
+    popItem: function () {
+      var lastBreadcrumbItem, card, newLastBreadcrumbItem;
+      if (this.breadcrumbStackSize === 0) {
+        return null;
+      }
+
+      lastBreadcrumbItem = this.getBreadcrumbItem(this.breadcrumbStackSize - 1);
+
+      card = lastBreadcrumbItem.card;
+      card.un('titlechange', this._onTitleChange, lastBreadcrumbItem);
+
+      this.remove(lastBreadcrumbItem);
+      this.breadcrumbStackSize--;
+      if (this.breadcrumbStackSize === 1) {
+        newLastBreadcrumbItem = this.getBreadcrumbItem(this.breadcrumbStackSize - 1);
+        newLastBreadcrumbItem.setDisabled(true);
+      }
+      return lastBreadcrumbItem;
+    },
+
+    getBreadcrumbSize: function () {
+      return this.breadcrumbStackSize;
+    }
+  });
+  Ext.reg('Hippo.ChannelManager.BreadcrumbToolbar', Hippo.ChannelManager.BreadcrumbToolbar);
 
 }());
