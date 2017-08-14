a3429c2d4c7c3d2932a4e3040d3fd978eb5fbc95
Bert Leunis <bert.leunis@bloomreach.com>
2017-08-14 14:50:50 +0200

    REPO-1772 add polish characters to unit test

--- repository/api/src/test/java/org/hippoecm/repository/api/UriEncodingTest.java
+++ repository/api/src/test/java/org/hippoecm/repository/api/UriEncodingTest.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2011-2013 Hippo B.V. (http://www.onehippo.com)
+ *  Copyright 2011-2017 Hippo B.V. (http://www.onehippo.com)
  * 
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -68,6 +68,7 @@ public class UriEncodingTest {
         // foreign characters
         assertEquals("хиппо-устойчивости", uri.encode("Хиппо устойчивости"));
         assertEquals("hippo-可持续性", uri.encode("Hippo 可持续性"));
+        assertEquals("acelnoszzacelnoszz", uri.encode("ąćęłńóśżźĄĆĘŁŃÓŚŻŹ"));
 
         // trailing dots
         assertEquals("abc", uri.encode("abc."));
