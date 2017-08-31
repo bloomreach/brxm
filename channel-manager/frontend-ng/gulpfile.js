3fdf3158f1203859b36aa97898b5614a76b7e0b6
Ariel Weinberger <ariel.weinberger@bloomreach.com>
2017-08-31 13:26:44 +0200

    CHANNELMGR-1274 Use frontend-build latest version and prefix frontend-build import paths with @bloomreach

--- channel-manager/frontend-ng/gulpfile.js
+++ channel-manager/frontend-ng/gulpfile.js
@@ -16,5 +16,5 @@
 
 const gulp = require('gulp');
 const HubRegistry = require('gulp-hub');
-const hub = new HubRegistry(['node_modules/frontend-build/index.js']);
+const hub = new HubRegistry(['node_modules/@bloomreach/frontend-build/index.js']);
 gulp.registry(hub);
