47b73f47c9db4b68346b0e60d1af41d3e12b2647
Joeri de Gooijer <joeri.degooijer@bloomreach.com>
2017-09-12 14:48:15 +0200

    CHANNELMGR-1274 Enable production when doing dist build

--- channel-manager/frontend-ng/src/index.ts
+++ channel-manager/frontend-ng/src/index.ts
@@ -17,8 +17,13 @@
 import './vendor';
 import './index.scss';
 
+import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
+
+if (process.env.ENV === 'production') {
+  enableProdMode();
+}
 
 platformBrowserDynamic().bootstrapModule(AppModule);
