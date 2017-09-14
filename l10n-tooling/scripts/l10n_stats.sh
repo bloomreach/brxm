8dc37e7e4a194f65e853b80ab346b9ed62350181
Oscar Scholten <oscar.scholten@bloomreach.com>
2017-09-14 10:54:45 +0200

    CMS-10902 Merge scripts, improve parameter naming

--- l10n-tooling/scripts/l10n_stats.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for locale in nl de fr zh es; do
-  echo Translation statistics for $locale \(lines, words, characters\):
-  mvn -N hippo-cms-l10n:export -Dformat=csv -Dlocale=$locale > /dev/null
-  cat export_$locale.csv | tail -n +2 | sed -e 's/^[^,]*,"\(.*\)",.*/\1/' -e t -e 's/^[^,]*,\(.*\),.*/\1/' | wc
-done
