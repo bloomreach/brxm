40ca6c3279537d09bb9696af3c70b1f6395a40e6
Arthur Bogaart <arthur.bogaart@bloomreach.com>
2017-09-14 13:02:53 +0200

    CMS-10846 Prevent javascript wicket.ajax error on hidden minutes field

--- cms/api/src/main/java/org/hippoecm/frontend/plugins/yui/datetime/YuiGMTDateField.java
+++ cms/api/src/main/java/org/hippoecm/frontend/plugins/yui/datetime/YuiGMTDateField.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2016 Hippo B.V. (http://www.onehippo.com)
+ *  Copyright 2016-2017 Hippo B.V. (http://www.onehippo.com)
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -30,6 +30,8 @@ public class YuiGMTDateField extends YuiDateTimeField {
 
         // hiding the "hours" component hides the entire "hours" wicket:enclosure
         get(HOURS).setVisibilityAllowed(false);
+        // hide the minutes field to prevent wicket.ajax javascript errors
+        get(MINUTES).setVisibilityAllowed(false);
     }
 
     @Override
