7885ee0b77963f9c828ec2c8b2a9146a143bb580
Arthur Bogaart <arthur.bogaart@bloomreach.com>
2017-09-19 22:11:50 +0200

    CMS-10913 Add ConfigProvider constructor, use passed arguments in create

--- cms/richtext/frontend/src/main/java/org/hippoecm/frontend/plugins/richtext/dialog/images/ImagePickerBehavior.java
+++ cms/richtext/frontend/src/main/java/org/hippoecm/frontend/plugins/richtext/dialog/images/ImagePickerBehavior.java
@@ -1,12 +1,12 @@
 /*
  *  Copyright 2008-2017 Hippo B.V. (http://www.onehippo.com)
- * 
+ *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
  *  You may obtain a copy of the License at
- * 
+ *
  *       http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  *  Unless required by applicable law or agreed to in writing, software
  *  distributed under the License is distributed on an "AS IS" BASIS,
  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -18,13 +18,14 @@ package org.hippoecm.frontend.plugins.richtext.dialog.images;
 import org.apache.wicket.Component;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
+import org.hippoecm.frontend.dialog.ConfigProvider;
+import org.hippoecm.frontend.dialog.Dialog;
+import org.hippoecm.frontend.dialog.DialogBehavior;
 import org.hippoecm.frontend.plugin.IPluginContext;
 import org.hippoecm.frontend.plugin.config.IPluginConfig;
-import org.hippoecm.frontend.plugins.richtext.dialog.AbstractAjaxDialogBehavior;
-import org.hippoecm.frontend.plugins.richtext.dialog.AbstractRichTextEditorDialog;
 import org.hippoecm.frontend.plugins.richtext.model.RichTextEditorImageLink;
 
-public class ImagePickerBehavior extends AbstractAjaxDialogBehavior<RichTextEditorImageLink> {
+public class ImagePickerBehavior extends DialogBehavior<RichTextEditorImageLink> {
 
     private final RichTextEditorImageService imageService;
 
@@ -34,12 +35,18 @@ public class ImagePickerBehavior extends AbstractAjaxDialogBehavior<RichTextEdit
         super(context, config);
         this.imageService = imageService;
     }
+    public ImagePickerBehavior(final IPluginContext context,
+                               final ConfigProvider configProvider,
+                               final RichTextEditorImageService imageService) {
+        super(context, configProvider);
+        this.imageService = imageService;
+    }
 
     @Override
-    protected AbstractRichTextEditorDialog<RichTextEditorImageLink> createDialog() {
+    protected Dialog<RichTextEditorImageLink> createDialog(final IPluginContext context, final IPluginConfig config) {
         final RichTextEditorImageLink imageLink = imageService.createRichTextEditorImage(getParameters());
         final IModel<RichTextEditorImageLink> model = Model.of(imageLink);
-        return new ImageBrowserDialog(getPluginContext(), getPluginConfig(), model);
+        return new ImageBrowserDialog(context, config, model);
     }
 
     @Override
--- cms/richtext/frontend/src/main/java/org/hippoecm/frontend/plugins/richtext/dialog/links/LinkPickerBehavior.java
+++ cms/richtext/frontend/src/main/java/org/hippoecm/frontend/plugins/richtext/dialog/links/LinkPickerBehavior.java
@@ -1,12 +1,12 @@
 /*
  *  Copyright 2008-2017 Hippo B.V. (http://www.onehippo.com)
- * 
+ *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
  *  You may obtain a copy of the License at
- * 
+ *
  *       http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  *  Unless required by applicable law or agreed to in writing, software
  *  distributed under the License is distributed on an "AS IS" BASIS,
  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -18,14 +18,15 @@ package org.hippoecm.frontend.plugins.richtext.dialog.links;
 import org.apache.wicket.Component;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
+import org.hippoecm.frontend.dialog.ConfigProvider;
+import org.hippoecm.frontend.dialog.Dialog;
+import org.hippoecm.frontend.dialog.DialogBehavior;
 import org.hippoecm.frontend.plugin.IPluginContext;
 import org.hippoecm.frontend.plugin.config.IPluginConfig;
-import org.hippoecm.frontend.plugins.richtext.dialog.AbstractAjaxDialogBehavior;
-import org.hippoecm.frontend.plugins.richtext.dialog.AbstractRichTextEditorDialog;
 import org.hippoecm.frontend.plugins.richtext.model.RichTextEditorDocumentLink;
 import org.hippoecm.frontend.plugins.richtext.model.RichTextEditorInternalLink;
 
-public class LinkPickerBehavior extends AbstractAjaxDialogBehavior<RichTextEditorDocumentLink> {
+public class LinkPickerBehavior extends DialogBehavior<RichTextEditorDocumentLink> {
 
     private final RichTextEditorLinkService linkService;
 
@@ -35,12 +36,18 @@ public class LinkPickerBehavior extends AbstractAjaxDialogBehavior<RichTextEdito
         super(context, config);
         this.linkService = linkService;
     }
+    public LinkPickerBehavior(final IPluginContext context,
+                              final ConfigProvider configProvider,
+                              final RichTextEditorLinkService linkService) {
+        super(context, configProvider);
+        this.linkService = linkService;
+    }
 
     @Override
-    protected AbstractRichTextEditorDialog<RichTextEditorDocumentLink> createDialog() {
+    protected Dialog<RichTextEditorDocumentLink> createDialog(final IPluginContext context, final IPluginConfig config) {
         final RichTextEditorInternalLink internalLink = linkService.create(getParameters());
         final IModel<RichTextEditorDocumentLink> model = Model.of(internalLink);
-        return new DocumentBrowserDialog<>(getPluginContext(), getPluginConfig(), model);
+        return new DocumentBrowserDialog<>(context, config, model);
     }
 
     @Override
