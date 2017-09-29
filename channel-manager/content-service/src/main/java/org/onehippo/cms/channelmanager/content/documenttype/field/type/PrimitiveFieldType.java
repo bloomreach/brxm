ff228910098f435d27da446cf51d0c4e4668ee7f
Michael Metternich <michael.metternich@bloomreach.com>
2017-09-29 15:58:16 +0200

    CHANNELMGR-1464 Ignore invalid values for multiples

--- channel-manager/content-service/src/main/java/org/onehippo/cms/channelmanager/content/documenttype/field/type/PrimitiveFieldType.java
+++ channel-manager/content-service/src/main/java/org/onehippo/cms/channelmanager/content/documenttype/field/type/PrimitiveFieldType.java
@@ -131,16 +131,22 @@ public abstract class PrimitiveFieldType extends AbstractFieldType {
     }
 
     private String convertToSpecificType(final String input) throws ValueFormatException {
-        if(input != null) {
+        if (input != null) {
             return fieldSpecificConversion(input);
         }
         throw new ValueFormatException("Trying to convert null value");
     }
 
-    private String[] convertToSpecificTypeArray(final String[] strings) throws ValueFormatException {
+    private String[] convertToSpecificTypeArray(final String[] strings) {
         final List<String> convertedStrings = new ArrayList<>();
         for (final String element : strings) {
-            convertedStrings.add(convertToSpecificType(element));
+            String specificType;
+            try {
+                specificType = convertToSpecificType(element);
+            } catch (final ValueFormatException ignore) {
+                specificType = null;
+            }
+            convertedStrings.add(specificType);
         }
         return convertedStrings.toArray(new String[0]);
     }
