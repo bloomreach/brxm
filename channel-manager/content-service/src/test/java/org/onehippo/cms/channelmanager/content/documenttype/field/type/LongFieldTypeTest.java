da0dcb407869133261c078789d33e6d1cfd4d957
Arthur Bogaart <arthur.bogaart@bloomreach.com>
2017-10-03 11:39:22 +0200

    CHANNELMGR-1464 Add tests for writing incorrect values
    Validate that writing incorrect values on both long and double fields
    does not overwrite existing values.

--- channel-manager/content-service/src/test/java/org/onehippo/cms/channelmanager/content/documenttype/field/type/LongFieldTypeTest.java
+++ channel-manager/content-service/src/test/java/org/onehippo/cms/channelmanager/content/documenttype/field/type/LongFieldTypeTest.java
@@ -23,6 +23,7 @@ import java.util.Optional;
 
 import javax.jcr.Node;
 import javax.jcr.PropertyType;
+import javax.jcr.Value;
 
 import org.hippoecm.repository.util.JcrUtils;
 import org.junit.Before;
@@ -92,19 +93,41 @@ public class LongFieldTypeTest {
         assertThat(node.getProperty(PROPERTY).getString(), equalTo("" + newValue));
     }
 
-//    @Test
-//    public void writeIncorrectValue() throws Exception {
-//        final Node node = MockNode.root();
-//        final PrimitiveFieldType fieldType = new LongFieldType();
-//        final Long oldValue = 1l;
-//        final String invalidValue = "foo";
-//
-//        fieldType.setId(PROPERTY);
-//        node.setProperty(PROPERTY, new String[]{"" + oldValue}, PropertyType.LONG);
-//
-//        fieldType.writeTo(node, Optional.of(listOf(valueOf(invalidValue))));
-//        assertThat(node.getProperty(PROPERTY).getString(), equalTo("" + oldValue));
-//    }
+    @Test
+    public void writeIncorrectValueDoesNotOverwriteExistingValue() throws Exception {
+        final Node node = MockNode.root();
+        final PrimitiveFieldType fieldType = new LongFieldType();
+        final Long oldValue = 1L;
+        final String invalidValue = "foo";
+
+        fieldType.setId(PROPERTY);
+        node.setProperty(PROPERTY, oldValue);
+
+        fieldType.writeTo(node, Optional.of(listOf(valueOf(invalidValue))));
+        assertThat(node.getProperty(PROPERTY).getLong(), equalTo(oldValue));
+    }
+
+    @Test
+    public void writeIncorrectValuesDoesNotOverwriteExistingValues() throws Exception {
+        final Node node = MockNode.root();
+        final PrimitiveFieldType fieldType = new LongFieldType();
+
+        fieldType.setId(PROPERTY);
+        fieldType.setMultiple(true);
+        fieldType.setMaxValues(2);
+
+        final Long oldValue1 = 1L;
+        final Long oldValue2 = 2L;
+        node.setProperty(PROPERTY, new String[] {oldValue1 + "", oldValue2 + ""}, PropertyType.LONG);
+
+        final String invalidValue1 = "foo";
+        final List<FieldValue> es = Arrays.asList(valueOf(invalidValue1), valueOf(oldValue2 + ""));
+        fieldType.writeTo(node, Optional.of(es));
+
+        final Value[] values = node.getProperty(PROPERTY).getValues();
+        assertThat(values[0].getLong(), equalTo(oldValue1));
+        assertThat(values[1].getLong(), equalTo(oldValue2));
+    }
 
     private static List<FieldValue> listOf(final FieldValue value) {
         return Collections.singletonList(value);
