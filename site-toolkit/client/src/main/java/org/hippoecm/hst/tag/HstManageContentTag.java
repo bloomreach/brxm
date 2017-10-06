c4d0dea90f0eb7c1e542d538065214ebc58ce4dd
Arthur Bogaart <arthur.bogaart@bloomreach.com>
2017-10-07 00:29:22 +0200

    HSTTWO-4147: Return MANAGE_CONTENT_LINK as type for ManageContentTag

--- site-toolkit/client/src/main/java/org/hippoecm/hst/tag/HstManageContentTag.java
+++ site-toolkit/client/src/main/java/org/hippoecm/hst/tag/HstManageContentTag.java
@@ -1,12 +1,12 @@
 /*
  *  Copyright 2017 Hippo B.V. (http://www.onehippo.com)
- * 
+ *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
  *  You may obtain a copy of the License at
- * 
+ *
  *       http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  *  Unless required by applicable law or agreed to in writing, software
  *  distributed under the License is distributed on an "AS IS" BASIS,
  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -117,7 +117,7 @@ public class HstManageContentTag extends TagSupport {
 
     private Map<?, ?> getAttributeMap() {
         final Map<String, Object> result = new HashMap<>();
-        result.put(ChannelManagerConstants.HST_TYPE, "CONTENT_LINK");
+        result.put(ChannelManagerConstants.HST_TYPE, "MANAGE_CONTENT_LINK");
         result.put("templateQuery", templateQuery);
         result.put("rootPath", rootPath);
         result.put("defaultPath", defaultPath);
@@ -151,4 +151,4 @@ public class HstManageContentTag extends TagSupport {
 
 
 
-   
+
