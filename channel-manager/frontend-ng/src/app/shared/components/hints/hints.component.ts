082d5cbf7ce118f6b8e555e26b1aeaca09765568
Ariel Weinberger <ariel.weinberger@bloomreach.com>
2017-10-10 12:32:57 +0200

    CHANNELMGR-1503 Implement .map() interation through children NodeList instead of a for loop

--- channel-manager/frontend-ng/src/app/shared/components/hints/hints.component.ts
+++ channel-manager/frontend-ng/src/app/shared/components/hints/hints.component.ts
@@ -17,18 +17,15 @@ export class HintsComponent implements OnInit {
     this.el = this.elementRef.nativeElement;
     const children: NodeListOf<Element> = this.el.querySelectorAll('*');
 
-    for (let i = 0; i < children.length; i++) {
-      const child: Element = children[i];
-
-      this.hints.push({
+    this.hints = [...children].map((child: Element) => {
+      const hint = {
         key: child.getAttribute('hint'),
         text: child.innerHTML,
         classList: child.className ? child.className.split('/\\s+/') : [],
-      });
+      };
 
-      // Remove original elements from the DOM to prevent an overhead.
-      // The hints will be represented as new elements in the template, with *ngIf.
       child.remove();
-    }
+      return hint;
+    });
   }
 }
