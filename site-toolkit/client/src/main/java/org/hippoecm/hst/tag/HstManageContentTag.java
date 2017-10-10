3586a75ed5020633f73cdaf8344c3d81f185ea71
Michael Metternich <michael.metternich@bloomreach.com>
2017-10-10 13:01:54 +0200

    HSTTWO-4145 Revert "Dont add parameters to manage content tag when ..."
    This reverts commit ff2d399

--- site-toolkit/client/src/main/java/org/hippoecm/hst/tag/HstManageContentTag.java
+++ site-toolkit/client/src/main/java/org/hippoecm/hst/tag/HstManageContentTag.java
@@ -1,12 +1,12 @@
 /*
  *  Copyright 2017 Hippo B.V. (http://www.onehippo.com)
- *
+ * 
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
  *  You may obtain a copy of the License at
- *
+ * 
  *       http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  *  Unless required by applicable law or agreed to in writing, software
  *  distributed under the License is distributed on an "AS IS" BASIS,
  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -117,18 +117,12 @@ public class HstManageContentTag extends TagSupport {
 
     private Map<?, ?> getAttributeMap() {
         final Map<String, Object> result = new HashMap<>();
-        writeToMap(result, ChannelManagerConstants.HST_TYPE, "MANAGE_CONTENT_LINK");
-        writeToMap(result, "templateQuery", templateQuery);
-        writeToMap(result, "rootPath", rootPath);
-        writeToMap(result, "defaultPath", defaultPath);
-        writeToMap(result, "componentParameter", componentParameter);
+        result.put(ChannelManagerConstants.HST_TYPE, "CONTENT_LINK");
+        result.put("templateQuery", templateQuery);
+        result.put("rootPath", rootPath);
+        result.put("defaultPath", defaultPath);
+        result.put("componentParameter", componentParameter);
         return result;
-    }
-
-    private static void writeToMap(final Map<String, Object> result, final String key, final String value) {
-        if (StringUtils.isNotEmpty(value)) {
-            result.put(key, value);
-        }
     }
 
     public void setDocument(final HippoBean document) {
@@ -157,4 +151,4 @@ public class HstManageContentTag extends TagSupport {
 
 
 
-
+   
