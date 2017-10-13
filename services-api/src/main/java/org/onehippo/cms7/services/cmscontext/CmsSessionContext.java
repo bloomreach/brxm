890dd467ccf6c04f111beb23ce2143319bd7ecb5
Michiel Rop <michiel.rop@bloomreach.com>
2017-10-13 13:40:07 +0200

    CMS-10929 Add payload to hints method where applicable.

--- services-api/src/main/java/org/onehippo/cms7/services/cmscontext/CmsSessionContext.java
+++ services-api/src/main/java/org/onehippo/cms7/services/cmscontext/CmsSessionContext.java
@@ -48,7 +48,7 @@ public interface CmsSessionContext {
     /**
      * Key to retrieve the initial payload for the documentworkflow.
      */
-    String CMS_SESSION_CONEXT_PAYLOAD_KEY = CmsSessionContext.class.getName() + ".initialPayload";
+    String CMS_SESSION_CONTEXT_PAYLOAD_KEY = CmsSessionContext.class.getName() + ".initialPayload";
 
     /**
      * Static method to retrieve the CmsSessionContext from a HttpSession
