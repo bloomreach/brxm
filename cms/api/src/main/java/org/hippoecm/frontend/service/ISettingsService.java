cf8d25b08dc29c4346e1738bd6ec34e8121a2ce4
Mathijs den Burger <mathijs.denburger@bloomreach.com>
2017-10-18 16:22:02 +0200

    CMS-10951 Code cleanup: remove redundant 'public' modifier

--- cms/api/src/main/java/org/hippoecm/frontend/service/ISettingsService.java
+++ cms/api/src/main/java/org/hippoecm/frontend/service/ISettingsService.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2010-2013 Hippo B.V. (http://www.onehippo.com)
+ *  Copyright 2010-2017 Hippo B.V. (http://www.onehippo.com)
  * 
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -25,5 +25,5 @@ public interface ISettingsService extends IClusterable {
 
     String SERVICE_ID = "service.settings";
 
-    public StringCodecFactory getStringCodecFactory();
+    StringCodecFactory getStringCodecFactory();
 }
