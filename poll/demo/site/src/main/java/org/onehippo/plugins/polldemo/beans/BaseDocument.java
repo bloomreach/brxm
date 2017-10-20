e4f75b6a2e173400c81263c46558b25695393d8e
Jeroen Hoffman <jeroen.hoffman@bloomreach.com>
2017-10-20 10:19:06 +0200

    HIPPLUG-1481 remove the demo files, except the new README

--- poll/demo/site/src/main/java/org/onehippo/plugins/polldemo/beans/BaseDocument.java
+++
@@ -1,9 +0,0 @@
-package org.onehippo.plugins.polldemo.beans;
-
-import org.hippoecm.hst.content.beans.Node;
-import org.hippoecm.hst.content.beans.standard.HippoDocument;
-
-@Node(jcrType="polldemo:basedocument")
-public class BaseDocument extends HippoDocument {
-
-}
