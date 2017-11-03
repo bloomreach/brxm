3db4285a3bdfd788ac8d92808a3e97eff52c9d51
Ariel Weinberger <ariel.weinberger@bloomreach.com>
2017-11-03 13:21:53 +0100

    CHANNELMGR-1551 Add scss rule to hide material select arrow when documentTypes has only one value

--- channel-manager/frontend-ng/src/app/channel/sidePanels/rightSidePanel/create-content/step-1/step-1.component.ts
+++ channel-manager/frontend-ng/src/app/channel/sidePanels/rightSidePanel/create-content/step-1/step-1.component.ts
@@ -85,6 +85,7 @@ export class CreateContentComponent implements OnInit {
 
   private onLoadDocumentTypes(documentTypes) {
     this.documentTypes = documentTypes;
+    this.documentTypes.push(documentTypes[0]);
 
     if (documentTypes.length === 1) {
       this.documentType = documentTypes[0].id;
