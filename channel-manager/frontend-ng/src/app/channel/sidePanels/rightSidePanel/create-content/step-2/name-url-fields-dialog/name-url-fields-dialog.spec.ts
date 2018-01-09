78e38812d8855a7b09560f36c20bcb8b9c62b51d
Arthur Bogaart <arthur.bogaart@bloomreach.com>
2018-01-09 01:35:40 +0100

    CHANNELMGR-1666 Add missing license and format

--- channel-manager/frontend-ng/src/app/channel/sidePanels/rightSidePanel/create-content/step-2/name-url-fields-dialog/name-url-fields-dialog.spec.ts
+++ channel-manager/frontend-ng/src/app/channel/sidePanels/rightSidePanel/create-content/step-2/name-url-fields-dialog/name-url-fields-dialog.spec.ts
@@ -1,44 +1,58 @@
-import { ComponentFixture } from "@angular/core/testing";
-import { FormsModule } from '@angular/forms';
-import { TestBed } from '@angular/core/testing';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
+/*
+ * Copyright 2017-2018 Hippo B.V. (http://www.onehippo.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-import { NameUrlFieldsDialogComponent } from "./name-url-fields-dialog";
-import { NameUrlFieldsComponent } from './../../name-url-fields/name-url-fields.component';
-import { SharedModule } from '../../../../../../shared/shared.module';
-import { HintsComponent } from '../../../../../../shared/components/hints/hints.component';
-import { CreateContentService } from '../../create-content.service';
-import { CreateContentServiceMock } from "../../create-content.mocks.spec";
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {FormsModule} from '@angular/forms';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material';
+
+import {NameUrlFieldsDialogComponent} from './name-url-fields-dialog';
+import {NameUrlFieldsComponent} from './../../name-url-fields/name-url-fields.component';
+import {SharedModule} from '../../../../../../shared/shared.module';
+import {HintsComponent} from '../../../../../../shared/components/hints/hints.component';
+import {CreateContentService} from '../../create-content.service';
+import {CreateContentServiceMock} from '../../create-content.mocks.spec';
 
 xdescribe('NameUrlFields Component', () => {
-    let fixture: ComponentFixture<NameUrlFieldsDialogComponent>;
-    let component: NameUrlFieldsDialogComponent;
+  let fixture: ComponentFixture<NameUrlFieldsDialogComponent>;
+  let component: NameUrlFieldsDialogComponent;
 
-    beforeEach(() => {
-        TestBed.configureTestingModule({
-            declarations: [
-                HintsComponent,
-                NameUrlFieldsComponent,
-                NameUrlFieldsDialogComponent
-            ],
-            imports: [
-                BrowserAnimationsModule,
-                FormsModule,
-                SharedModule
-            ],
-            providers: [
-                { provide: CreateContentService, useClass: CreateContentServiceMock },
-                { provide: MatDialogRef, useValue: {} },
-                { provide: MAT_DIALOG_DATA, useValue: {} }
-            ]
-        });
-
-        fixture = TestBed.createComponent(NameUrlFieldsDialogComponent);
-        component = fixture.componentInstance;
-
-
-        component.ngOnInit();
-        fixture.detectChanges();
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        HintsComponent,
+        NameUrlFieldsComponent,
+        NameUrlFieldsDialogComponent
+      ],
+      imports: [
+        BrowserAnimationsModule,
+        FormsModule,
+        SharedModule
+      ],
+      providers: [
+        {provide: CreateContentService, useClass: CreateContentServiceMock},
+        {provide: MatDialogRef, useValue: {}},
+        {provide: MAT_DIALOG_DATA, useValue: {}}
+      ]
     });
+
+    fixture = TestBed.createComponent(NameUrlFieldsDialogComponent);
+    component = fixture.componentInstance;
+
+    component.ngOnInit();
+    fixture.detectChanges();
+  });
 });
