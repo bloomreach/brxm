9fc6fa0634875b4af9d4c06c1e414383b5926010
Ariel Weinberger <ariel.weinberger@bloomreach.com>
2018-01-15 14:27:32 +0100

    CHANNELMGR-1676 Update license years and remove fdescribe

--- channel-manager/frontend-ng/src/app/channel/overlayToggle/overlayToggle.controller.spec.js
+++ channel-manager/frontend-ng/src/app/channel/overlayToggle/overlayToggle.controller.spec.js
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015-2017 Hippo B.V. (http://www.onehippo.com)
+ * Copyright 2017-2018 Hippo B.V. (http://www.onehippo.com)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,10 +17,10 @@
 import angular from 'angular';
 import 'angular-mocks';
 
-fdescribe('overlayToggle component', () => {
+describe('overlayToggle component', () => {
   let $ctrl;
   let localStorageService;
-  const testStorageKey = 'channelManager.overlays.testToggle'
+  const testStorageKey = 'channelManager.overlays.testToggle';
 
   beforeEach(() => {
     angular.mock.module('hippo-cm');
