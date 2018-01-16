d0c63c4c36ee790a60183b80b3eaca5506deb7c8
Arthur Bogaart <arthur.bogaart@bloomreach.com>
2018-01-17 00:34:14 +0100

    CHANNELMGR-1673 Remove left-over typescript files

--- channel-manager/frontend-ng/src/app/channel/sidePanels/rightSidePanel/fields/ckeditor/sharedspace-toolbar/sharedspace-toolbar.module.js
+++ channel-manager/frontend-ng/src/app/channel/sidePanels/rightSidePanel/fields/ckeditor/sharedspace-toolbar/sharedspace-toolbar.module.js
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 Hippo B.V. (http://www.onehippo.com)
+ * Copyright 2017-2018 Hippo B.V. (http://www.onehippo.com)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import SharedSpaceToolbarComponent from './sharedspace-toolbar.ng1.component';
+import SharedSpaceToolbarComponent from './sharedspace-toolbar.component';
 import SharedSpaceToolbarService from './sharedspace-toolbar.service';
 
 const SharedSpaceToolbarModule = angular
