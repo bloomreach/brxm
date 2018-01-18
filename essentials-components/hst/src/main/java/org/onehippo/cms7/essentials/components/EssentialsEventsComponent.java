0c71c120ed4ebfc72d1f54d1fb6679346e0fa4bb
Bert Leunis <bert.leunis@bloomreach.com>
2018-01-18 11:40:47 +0100

    ESSCOM-29 components of features that store documents in year/month folders supply attributes to the request
    This supports the manageContent tags to create new documents in the channel manager.

--- essentials-components/hst/src/main/java/org/onehippo/cms7/essentials/components/EssentialsEventsComponent.java
+++ essentials-components/hst/src/main/java/org/onehippo/cms7/essentials/components/EssentialsEventsComponent.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014 Hippo B.V. (http://www.onehippo.com)
+ * Copyright 2014-2018 Hippo B.V. (http://www.onehippo.com)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -28,6 +28,7 @@ import org.hippoecm.hst.core.component.HstResponse;
 import org.hippoecm.hst.core.parameters.ParametersInfo;
 import org.hippoecm.repository.util.DateTools;
 import org.onehippo.cms7.essentials.components.info.EssentialsEventsComponentInfo;
+import org.onehippo.cms7.essentials.components.utils.ComponentsUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -52,6 +53,7 @@ public class EssentialsEventsComponent extends EssentialsListComponent {
             return;
         }
         super.doBeforeRender(request, response);
+        ComponentsUtils.addCurrentDateStrings(request);
     }
 
     @Override
