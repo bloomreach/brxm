90e4451fd9892758dcfab515fd29c6af327036ce
Arthur Bogaart <arthur.bogaart@bloomreach.com>
2018-01-23 23:53:32 +0100

    CHANNELMGR-1684 Finish unit tests for Step1Service

--- channel-manager/frontend-ng/src/app/channel/sidePanels/rightSidePanel/createContent/step1/step1.service.spec.js
+++ channel-manager/frontend-ng/src/app/channel/sidePanels/rightSidePanel/createContent/step1/step1.service.spec.js
@@ -15,9 +15,35 @@
  */
 
 describe('Step1Service', () => {
+  let $q;
+  let $rootScope;
   let ChannelService;
   let ContentService;
+  let FeedbackService;
   let Step1Service;
+
+  function expectError(triggerError, errorData, defaultMessage) {
+    // backend returns an error with passed data
+    ContentService._send.and.returnValue($q.reject(errorData));
+    triggerError();
+    $rootScope.$apply();
+
+    // parse and expect key/default-message and optional params
+    const key = errorData.data ? `ERROR_${errorData.data.reason}` : defaultMessage;
+    const args = [key];
+    if (errorData.data && errorData.data.params) {
+      args.push(errorData.data.params);
+    }
+    expect(FeedbackService.showError).toHaveBeenCalledWith(...args);
+  }
+
+  function expectReset() {
+    expect(Step1Service.name).toBeUndefined();
+    expect(Step1Service.url).toBeUndefined();
+    expect(Step1Service.locale).toBeUndefined();
+    expect(Step1Service.rootPath).toBeUndefined();
+    expect(Step1Service.templateQuery).toBeUndefined();
+  }
 
   beforeEach(() => {
     angular.mock.module('hippo-cm.channel.createContentModule');
@@ -34,50 +60,158 @@ describe('Step1Service', () => {
       $provide.value('ContentService', ContentService);
     });
 
-    inject((_ChannelService_, _ContentService_, _Step1Service_) => {
+    inject((_$q_, _$rootScope_, _ChannelService_, _ContentService_, _FeedbackService_, _Step1Service_) => {
+      $q = _$q_;
+      $rootScope = _$rootScope_;
       ChannelService = _ChannelService_;
       ContentService = _ContentService_;
+      FeedbackService = _FeedbackService_;
       Step1Service = _Step1Service_;
     });
   });
 
-  describe('Step1 service', () => {
-    it('is created with a clean state', () => {
-      expect(Step1Service.name).toBeUndefined();
-      expect(Step1Service.url).toBeUndefined();
-      expect(Step1Service.locale).toBeUndefined();
-      expect(Step1Service.rootPath).toBeUndefined();
-      expect(Step1Service.templateQuery).toBeUndefined();
+  it('is created with a clean state', () => {
+    expectReset();
+  });
+
+  it('is reset when stopped', () => {
+    Step1Service.name = 'name';
+    Step1Service.url = 'url';
+    Step1Service.locale = 'locale';
+    Step1Service.rootPath = 'rootPath';
+    Step1Service.defaultPath = 'defaultPath';
+    Step1Service.templateQuery = 'templateQuery';
+    Step1Service.stop();
+    expectReset();
+  });
+
+  describe('open', () => {
+    it('stores the input values', () => {
+      Step1Service.open('tpl-query', '/root/path', 'default/path');
+      expect(Step1Service.defaultPath).toBe('default/path');
+      expect(Step1Service.rootPath).toBe('/root/path');
+      expect(Step1Service.templateQuery).toBe('tpl-query');
     });
 
-    describe('open', () => {
-      describe('parsing the rootPath', () => {
-        it('defaults to the channel root if not set', () => {
-          Step1Service.open('tpl-query');
-          expect(Step1Service.rootPath).toBe('/channel/content');
+    it('resets the values before storing new values', () => {
+      Step1Service.open('tpl-query', '/root/path', 'default/path');
+      Step1Service.name = 'name';
 
-          Step1Service.open('tpl-query', '');
-          expect(Step1Service.rootPath).toBe('/channel/content');
-        });
+      Step1Service.open('tpl-query2', '/root/path2');
+      expect(Step1Service.templateQuery).toBe('tpl-query2');
+      expect(Step1Service.rootPath).toBe('/root/path2');
+      expect(Step1Service.defaultPath).toBeUndefined();
+      expect(Step1Service.name).toBeUndefined();
+    });
 
-        it('overrides the channel root path if absolute', () => {
-          Step1Service.open('tpl-query', '/root/path');
-          expect(Step1Service.rootPath).toBe('/root/path');
-        });
-
-        it('is concatenated wth the channel\'s root path if relative', () => {
-          Step1Service.open('tpl-query', 'some/path');
-          expect(Step1Service.rootPath).toBe('/channel/content/some/path');
-        });
-
-        it('never ends with a slash', () => {
-          Step1Service.open('tpl-query', '/root/path/');
-          expect(Step1Service.rootPath).toBe('/root/path');
-
-          Step1Service.open('tpl-query', 'some/path/');
-          expect(Step1Service.rootPath).toBe('/channel/content/some/path');
-        });
+    describe('loading document types by template-query', () => {
+      it('executes a backend call to /templatequery/{template-query}', () => {
+        Step1Service.open('tpl-query');
+        expect(ContentService._send).toHaveBeenCalledWith('GET', ['templatequery', 'tpl-query'], null, true);
       });
+
+      it('stores the document types returned by the backend', () => {
+        ContentService._send.and.returnValue($q.resolve({
+          documentTypes: ['a', 'b'],
+        }));
+        Step1Service.open('tpl-query');
+        $rootScope.$apply();
+        expect(Step1Service.documentTypes).toEqual(['a', 'b']);
+        expect(Step1Service.documentType).toEqual('');
+      });
+
+      it('pre-selects the documentType if there is only one returned by the backend', () => {
+        ContentService._send.and.returnValue($q.resolve({
+          documentTypes: [{
+            id: 'a',
+          }],
+        }));
+        Step1Service.open('tpl-query');
+        $rootScope.$apply();
+        expect(Step1Service.documentTypes).toEqual([{
+          id: 'a',
+        }]);
+        expect(Step1Service.documentType).toEqual('a');
+      });
+
+      it('handles template query errors', () => {
+        spyOn(FeedbackService, 'showError');
+        expectError(() => Step1Service.open('tpl-query'), {}, 'Unexpected error loading template query "tpl-query"');
+        expectError(() => Step1Service.open('tpl-query'), { data: { reason: 'the_cause' } }, 'ERROR_the_cause');
+      });
+    });
+
+    describe('parsing the rootPath', () => {
+      it('defaults to the channel root if not set', () => {
+        Step1Service.open('tpl-query');
+        expect(Step1Service.rootPath).toBe('/channel/content');
+
+        Step1Service.open('tpl-query', '');
+        expect(Step1Service.rootPath).toBe('/channel/content');
+      });
+
+      it('overrides the channel root path if absolute', () => {
+        Step1Service.open('tpl-query', '/root/path');
+        expect(Step1Service.rootPath).toBe('/root/path');
+      });
+
+      it('is concatenated wth the channel\'s root path if relative', () => {
+        Step1Service.open('tpl-query', 'some/path');
+        expect(Step1Service.rootPath).toBe('/channel/content/some/path');
+      });
+
+      it('never ends with a slash', () => {
+        Step1Service.open('tpl-query', '/root/path/');
+        expect(Step1Service.rootPath).toBe('/root/path');
+
+        Step1Service.open('tpl-query', 'some/path/');
+        expect(Step1Service.rootPath).toBe('/channel/content/some/path');
+      });
+    });
+  });
+
+  describe('createDraft', () => {
+    it('executes a "documents" backend call to create a draft', (done) => {
+      Step1Service.name = 'test-name';
+      Step1Service.url = 'test-url';
+      Step1Service.templateQuery = 'test-tpl-query';
+      Step1Service.documentType = 'test-doctype';
+      Step1Service.rootPath = 'test-rootpath';
+      Step1Service.defaultPath = 'test-defaultpath';
+
+      Step1Service.createDraft().then(done);
+      expect(ContentService._send).toHaveBeenCalledWith('POST', ['documents'], {
+        name: 'test-name',
+        slug: 'test-url',
+        templateQuery: 'test-tpl-query',
+        documentTypeId: 'test-doctype',
+        rootPath: 'test-rootpath',
+        defaultPath: 'test-defaultpath',
+      });
+    });
+
+    it('handles create-draft backend errors', () => {
+      spyOn(FeedbackService, 'showError');
+      expectError(() => Step1Service.createDraft(), {}, 'Unexpected error creating new draft document');
+      expectError(() => Step1Service.createDraft(), {
+        data: {
+          reason: 'the_cause',
+          params: 'the_params',
+        },
+      });
+    });
+  });
+
+  describe('getFolders', () => {
+    it('executes a "folders" backend call', () => {
+      Step1Service.getFolders('aa/bb');
+      expect(ContentService._send).toHaveBeenCalledWith('GET', ['folders', 'aa/bb'], null, true);
+    });
+
+    it('handles getFolders backend errors', () => {
+      spyOn(FeedbackService, 'showError');
+      expectError(() => Step1Service.getFolders('aa/bb'), {}, 'Unexpected error loading folders for path aa/bb');
+      expectError(() => Step1Service.getFolders('aa/bb'), { data: { reason: 'the_cause' } }, 'ERROR_the_cause');
     });
   });
 });
