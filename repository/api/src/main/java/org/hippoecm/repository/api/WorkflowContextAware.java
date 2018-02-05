f6cfbcc31b761d713b0d8743e547b3f280f763da
Michiel Eggermont <michiel.eggermont@bloomreach.com>
2018-02-05 15:19:42 +0100

    REPO-1933 Suppress logging of WorkflowContextAware method invocations
    These methods aren't actions that perform workflow operations on behalf
    of a user, but are more like utility methods that prepare a workflow
    action.

--- repository/api/src/main/java/org/hippoecm/repository/api/WorkflowContextAware.java
+++ repository/api/src/main/java/org/hippoecm/repository/api/WorkflowContextAware.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2017 Hippo B.V. (http://www.onehippo.com)
+ *  Copyright 2017-2018 Hippo B.V. (http://www.onehippo.com)
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -18,13 +18,19 @@ package org.hippoecm.repository.api;
 import javax.jcr.Node;
 import javax.jcr.RepositoryException;
 
+import org.onehippo.repository.api.annotation.WorkflowAction;
+
 public interface WorkflowContextAware {
 
+    @WorkflowAction(loggable = false, mutates = false)
     WorkflowContext getWorkflowContext();
 
+    @WorkflowAction(loggable = false)
     void setWorkflowContext(final WorkflowContext context);
 
+    @WorkflowAction(loggable = false, mutates = false)
     Node getNode();
 
+    @WorkflowAction(loggable = false)
     void setNode(final Node node) throws RepositoryException;
 }
