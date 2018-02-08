6f844654c31b1899b18712c3bbcce4d6f6b8a1b7
Arthur Bogaart <arthur.bogaart@bloomreach.com>
2018-02-08 11:56:23 +0100

    CHANNELMGR-1707 Remove fdescribe

--- channel-manager/frontend-ng/src/app/services/hstComponent.service.spec.js
+++ channel-manager/frontend-ng/src/app/services/hstComponent.service.spec.js
@@ -17,7 +17,7 @@
 import angular from 'angular';
 import 'angular-mocks';
 
-fdescribe('HstComponentService', () => {
+describe('HstComponentService', () => {
   let $q;
   let $rootScope;
   let ChannelService;
