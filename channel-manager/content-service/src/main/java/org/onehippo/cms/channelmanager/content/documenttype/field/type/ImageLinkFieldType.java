505fc9d365dd737576b42f0bcc247ed4f4e89531
Arthur Bogaart <arthur.bogaart@bloomreach.com>
2018-03-08 23:43:03 +0100

    CHANNELMGR-1431 Return FieldsInformation from #init

--- channel-manager/content-service/src/main/java/org/onehippo/cms/channelmanager/content/documenttype/field/type/ImageLinkFieldType.java
+++ channel-manager/content-service/src/main/java/org/onehippo/cms/channelmanager/content/documenttype/field/type/ImageLinkFieldType.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2017 Hippo B.V. (http://www.onehippo.com)
+ *  Copyright 2017-2018 Hippo B.V. (http://www.onehippo.com)
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -80,9 +80,7 @@ public class ImageLinkFieldType extends PrimitiveFieldType implements NodeFieldT
     }
 
     @Override
-    public void init(final FieldTypeContext fieldContext) {
-        super.init(fieldContext);
-
+    public FieldsInformation init(final FieldTypeContext fieldContext) {
         config = Json.object();
 
         final ObjectNode imagePickerConfig = new FieldTypeConfig(fieldContext)
@@ -92,6 +90,8 @@ public class ImageLinkFieldType extends PrimitiveFieldType implements NodeFieldT
                 .strings(IMAGE_PICKER_PREFIXED_STRING_PROPERTIES)
                 .build();
         config.set("imagepicker", imagePickerConfig);
+
+        return super.init(fieldContext);
     }
 
     void initListBasedChoice(final String choiceId) {
