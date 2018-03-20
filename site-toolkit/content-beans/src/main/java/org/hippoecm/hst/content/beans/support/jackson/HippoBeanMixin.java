9637b48de3793c78e4376017f1a9b36ec92e1cfc
Woonsan Ko <woonsan.ko@bloomreach.com>
2018-03-20 10:23:06 -0400

    HSTTWO-4283: Remove JsonIdentityInfo on HippoBeanMixin as it was introduced to avoid circular references initially, but it was fixed in a different way.

--- site-toolkit/content-beans/src/main/java/org/hippoecm/hst/content/beans/support/jackson/HippoBeanMixin.java
+++ site-toolkit/content-beans/src/main/java/org/hippoecm/hst/content/beans/support/jackson/HippoBeanMixin.java
@@ -23,12 +23,9 @@ import org.hippoecm.hst.content.beans.standard.HippoAvailableTranslationsBean;
 import org.hippoecm.hst.content.beans.standard.HippoBean;
 import org.hippoecm.hst.provider.jcr.JCRValueProvider;
 
-import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 
-@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
 public interface HippoBeanMixin extends HippoBean {
 
     @JsonProperty("id")
