c6f5ee2194bbd1b90335a466b316333fa77d6fd6
Jasper Floor <jasper.floor@bloomreach.com>
2018-04-10 14:58:26 +0200

    ESSCOM-45 throw FilterException in contributeAndFilters

--- essentials-components/hst/src/main/java/org/onehippo/cms7/essentials/components/EssentialsListComponent.java
+++ essentials-components/hst/src/main/java/org/onehippo/cms7/essentials/components/EssentialsListComponent.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014-2017 Hippo B.V. (http://www.onehippo.com)
+ * Copyright 2014-2018 Hippo B.V. (http://www.onehippo.com)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -273,7 +273,7 @@ public class EssentialsListComponent extends CommonComponent {
      * @param request current HST request
      * @param query   query under construction, provider for new filters
      */
-    protected void contributeAndFilters(final List<BaseFilter> filters, final HstRequest request, final HstQuery query) {
+    protected void contributeAndFilters(final List<BaseFilter> filters, final HstRequest request, final HstQuery query) throws FilterException{
         // empty
     }
 
