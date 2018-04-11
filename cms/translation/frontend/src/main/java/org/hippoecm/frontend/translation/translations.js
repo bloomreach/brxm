be69f908e9aca970b07bb12fcc95ce26cc4e4fe9
Mathijs den Burger <mathijs.denburger@bloomreach.com>
2018-04-11 11:31:20 +0200

    CMS-11165 HTML-encode path names in folder translation dialog

--- cms/translation/frontend/src/main/java/org/hippoecm/frontend/translation/translations.js
+++ cms/translation/frontend/src/main/java/org/hippoecm/frontend/translation/translations.js
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2015 Hippo B.V. (http://www.onehippo.com)
+ * Copyright 2010-2018 Hippo B.V. (http://www.onehippo.com)
  *
  * Licensed under the Apache License, Version 2.0 (the  "License");
  * you may not use this file except in compliance with the License.
@@ -58,7 +58,7 @@ Hippo.Translation.PathRenderer = Ext.extend(Ext.util.Observable, {
         + '<span class="hi hi-bottom hi-right ' + countryClass + '">&nbsp;</span>'
         + '</span>';
 
-      text += path[i].name;
+      text += Ext.util.Format.htmlEncode(path[i].name);
     }
     text += '</div>';
     if (locale !== null && locale !== undefined) {
