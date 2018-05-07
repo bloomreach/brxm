fa3062a0fa2812e3137fc8e2e6fd8ad4952a648b
Mathijs den Burger <mathijs.denburger@bloomreach.com>
2018-05-07 12:12:08 +0200

    CHANNELMGR-1843 Add tests for PageInfoService

---
+++ channel-manager/frontend-ng/src/app/channel/sidePanels/rightSidePanel/pageInfo/pageInfo.service.spec.js
@@ -0,0 +1,113 @@
+/*
+ * Copyright 2018 Hippo B.V. (http://www.onehippo.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+describe('PageInfoService', () => {
+  let $rootScope;
+  let $state;
+  let $stateRegistry;
+  let $translate;
+  let PageInfoService;
+  let RightSidePanelService;
+
+  let extension1;
+  let extension2;
+
+  beforeEach(() => {
+    extension1 = {
+      id: 'extension1',
+      displayName: 'Page Extension 1',
+      context: 'page',
+    };
+    extension2 = {
+      id: 'extension2',
+      displayName: 'Page Extension 2',
+      context: 'page',
+    };
+
+    const ExtensionService = jasmine.createSpyObj('ExtensionService', ['getExtensions']);
+    ExtensionService.getExtensions.and.returnValue([extension1, extension2]);
+
+    angular.mock.module('hippo-cm.channel.pageInfo', ($provide) => {
+      $provide.value('ExtensionService', ExtensionService);
+    });
+
+    inject((_$rootScope_, _$state_, _$stateRegistry_, _$translate_, _PageInfoService_, _RightSidePanelService_) => {
+      $rootScope = _$rootScope_;
+      $state = _$state_;
+      $stateRegistry = _$stateRegistry_;
+      $translate = _$translate_;
+      PageInfoService = _PageInfoService_;
+      RightSidePanelService = _RightSidePanelService_;
+    });
+  });
+
+  describe('state per page extension', () => {
+    it('is registered', () => {
+      expect($stateRegistry.states['hippo-cm.channel.page-info.extension1']).toBeDefined();
+      expect($stateRegistry.states['hippo-cm.channel.page-info.extension2']).toBeDefined();
+    });
+
+    it('has certain properties', () => {
+      const state = $stateRegistry.states['hippo-cm.channel.page-info.extension1'];
+      expect(state.name).toEqual('hippo-cm.channel.page-info.extension1');
+      expect(state.parameter('extensionId').value()).toEqual('extension1');
+      expect(state.sticky).toEqual(true);
+      expect(state.views.extension1).toBeDefined();
+      expect(state.resolve.flex()).toEqual('');
+      expect(state.resolve.layout()).toEqual('column');
+    });
+  });
+
+  describe('showPageInfo', () => {
+    it('sets the title of the right side-panel', () => {
+      spyOn($translate, 'instant').and.callThrough();
+      spyOn(RightSidePanelService, 'setTitle');
+
+      PageInfoService.showPageInfo('/pageUrl');
+
+      expect($translate.instant).toHaveBeenCalledWith('PAGE_INFO_TITLE', { pageUrl: '/pageUrl' });
+      expect(RightSidePanelService.setTitle).toHaveBeenCalledWith('PAGE_INFO_TITLE');
+    });
+
+    it('goes to the state of the first page extension', () => {
+      spyOn($state, 'go');
+      PageInfoService.showPageInfo('/pageUrl');
+      expect($state.go).toHaveBeenCalledWith('hippo-cm.channel.page-info.extension1', { pageUrl: '/pageUrl' });
+    });
+  });
+
+  describe('selectedExtensionId', () => {
+    it('returns the extension ID of the current state', () => {
+      $state.go('hippo-cm.channel.page-info.extension2', { pageUrl: '' });
+      $rootScope.$digest();
+      expect(PageInfoService.selectedExtensionId).toEqual('extension2');
+    });
+
+    it('navigates to a page extension state when set', () => {
+      PageInfoService.selectedExtensionId = 'extension2';
+      $rootScope.$digest();
+      expect($state.current.name).toEqual('hippo-cm.channel.page-info.extension2');
+    });
+  });
+
+  describe('closePageInfo', () => {
+    it('goes to the hippo-cm.channel state', () => {
+      spyOn($state, 'go');
+      PageInfoService.closePageInfo();
+      expect($state.go).toHaveBeenCalledWith('hippo-cm.channel');
+    });
+  });
+});
