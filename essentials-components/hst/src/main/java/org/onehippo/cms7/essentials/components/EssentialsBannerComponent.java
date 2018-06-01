2427e9a0376c0dc43d1e0db877ea7564b1171e10
Arthur Bogaart <arthur.bogaart@bloomreach.com>
2018-06-01 15:06:24 +0200

    ESSCOM-47 Add EssentialsBannerComponent & EssentialsBannerComponentInfo
    An EssentialsBannerComponent restricts the picker to documents-only
    meaning both the section and the type of node.

---
+++ essentials-components/hst/src/main/java/org/onehippo/cms7/essentials/components/EssentialsBannerComponent.java
@@ -0,0 +1,29 @@
+/*
+ *  Copyright 2018 Hippo B.V. (http://www.onehippo.com)
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.onehippo.cms7.essentials.components;
+
+import org.hippoecm.hst.core.parameters.ParametersInfo;
+import org.onehippo.cms7.essentials.components.info.EssentialsBannerComponentInfo;
+
+/**
+ * Hippo HST component for simple (document detail) request handling.
+ * If document path is not defined, EssentialsDocumentComponent will try to fetch sitemap mapped bean.
+ * @version "$Id$"
+ * @see EssentialsContentComponent
+ */
+@ParametersInfo(type = EssentialsBannerComponentInfo.class)
+public class EssentialsBannerComponent extends EssentialsDocumentComponent {
+}
