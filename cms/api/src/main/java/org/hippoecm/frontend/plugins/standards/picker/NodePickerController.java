9eb004876f8e4f4fa96ecf1161c08a0cc6314e50
Arthur Bogaart <arthur.bogaart@bloomreach.com>
2018-06-05 17:12:40 +0200

    CMS-11231 Update documentation for #saveLastModelVisited

--- cms/api/src/main/java/org/hippoecm/frontend/plugins/standards/picker/NodePickerController.java
+++ cms/api/src/main/java/org/hippoecm/frontend/plugins/standards/picker/NodePickerController.java
@@ -338,10 +338,9 @@ public abstract class NodePickerController implements IDetachable {
     }
 
     /**
-     * Save the last visited location in the preferences store. By default, only nodes of type hippostd:folder are
-     * allowed, other nodetypes can be specified by configuring a multi-value String property named "last.visited.nodetypes".
-     *
-     * By default, all nodes except hippo document are allowed
+     * Save the last visited location in the preferences store. By default, it will try to use the parent of the nearest
+     * "hippo:handle" node, other node-types can be specified by configuring a multi-value String property named
+     * "last.visited.nodetypes".
      */
     private void saveLastModelVisited() {
         if (lastModelVisited == null) {
