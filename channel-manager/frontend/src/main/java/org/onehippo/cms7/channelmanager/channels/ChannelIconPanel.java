578b89de384b6a1d3d75b8d60862ca0300fb9dbc
Ard Schrijvers <ard.schrijvers@bloomreach.com>
2018-06-22 16:40:19 +0200

    CHANNELMGR-1949 remove accidental added code

--- channel-manager/frontend/src/main/java/org/onehippo/cms7/channelmanager/channels/ChannelIconPanel.java
+++ channel-manager/frontend/src/main/java/org/onehippo/cms7/channelmanager/channels/ChannelIconPanel.java
@@ -37,11 +37,6 @@ public class ChannelIconPanel extends ExtPanel {
     @ExtProperty
     private String userId;
 
-    // TODO is composerRestMountUrl still needed?
-    @ExtProperty
-    @SuppressWarnings("unused")
-    private String composerRestMountUrl;
-
     public ChannelIconPanel(IPluginConfig channelListConfig, ExtStoreFuture storeFuture) {
         this.store = (ChannelStore) storeFuture.getStore();
         this.userId = UserSession.get().getJcrSession().getUserID();
