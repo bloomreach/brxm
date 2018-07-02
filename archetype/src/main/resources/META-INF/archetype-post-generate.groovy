e70dfb0da02081635103de3cb1421e222c1e8565
Sergey Shepelevich <sergey.shepelevich@bloomreach.com>
2018-07-02 16:00:28 +0200

    ARCHE-584 Archetype support for (sub)site project generation

--- archetype/src/main/resources/META-INF/archetype-post-generate.groovy
+++ archetype/src/main/resources/META-INF/archetype-post-generate.groovy
@@ -1,52 +1,66 @@
-final String ANSI_RESET = "\u001B[0m";
-final String ANSI_BLACK = "\u001B[30m";
-final String ANSI_RED = "\u001B[31m";
-final String ANSI_GREEN = "\u001B[32m";
-final String ANSI_YELLOW = "\u001B[33m";
-final String ANSI_BLUE = "\u001B[34m";
-final String ANSI_PURPLE = "\u001B[35m";
-final String ANSI_CYAN = "\u001B[36m";
-final String ANSI_WHITE = "\u001B[37m";
+final String propertiesMarker = '<!-- PROPERTIES_MARKER -->'
+final String cmsDependenciesMarker = '<!-- PARENT_PROJECT_DEPENDENCIES -->'
+final String pomFile = 'pom.xml'
 
+def projectLocation = request.getOutputDirectory() + "/" + request.getArtifactId()
 
-println "---------------------------------------------------------------"
-println GroovySystem.version
-println request.getOutputDirectory() + "/" + request.getArtifactId()
-println "---------------------------------------------------------------"
+println ""
+println "Project location: $projectLocation"
+println ""
 
 def property = System.getProperty("site");
 
+def rootModuleDir = new File(projectLocation)
+def rootPomFile = new File(rootModuleDir, pomFile)
+def cmsDependenciesModuleDir = new File(rootModuleDir, "cms-dependencies")
+def cmsDependenciesPomFile = new File(cmsDependenciesModuleDir, pomFile)
+
 if (property == null) {
-//    Main project creation. No need to do anything extra here (strip markers?)
-    println ANSI_WHITE + "Creating main project$ANSI_RESET"
+//    Main project creation. No need to do anything extra here, just clear markers
+    println "Creating main project"
+    replacePlaceHolder(rootPomFile, propertiesMarker, '')
+    replacePlaceHolder(cmsDependenciesPomFile, cmsDependenciesMarker, '')
 } else if (property != null) {
-    println ANSI_WHITE + "Creating site project$ANSI_RESET"
-    def parentGroupId = System.getProperty("parentGroupId")?.trim() ?
-            System.getProperty("parentGroupId") : System.console().readLine('Parent project groupId: ')
-    def parentArtifactId = System.getProperty("parentArtifactId")?.trim() ?
-            System.getProperty("parentArtifactId") : System.console().readLine('Parent project artifactId: ')
-    def parentVersion = System.getProperty("parentVersion")?.trim() ?
-            System.getProperty("parentVersion") : System.console().readLine ('Parent project version: ')
+    println "Creating site project"
 
-    println "Parent GroupId: $parentGroupId"
-    println "Parent artifactId: $parentArtifactId"
-    println "Parent version: $parentVersion"
+    def parentGroupId = System.getProperty("parentGroupId")?.trim();
+    if (parentGroupId != null) {
+        println "Parent GroupId: $parentGroupId"
+    }
+    def parentArtifactId = System.getProperty("parentArtifactId")?.trim()
+    if (parentArtifactId != null) {
+        println "Parent artifactId: $parentArtifactId"
+    }
 
-    def parentModuleDir = new File(request.getOutputDirectory() + "/" + request.getArtifactId())
-    def propertiesMarker = '<!-- PROPERTIES_MARKER -->'
-    def pomFile = new File(parentModuleDir, 'pom.xml')
-    println "Updating " + pomFile
+    def parentVersion = System.getProperty("parentVersion")?.trim();
+    if (parentVersion != null) {
+        println "Parent version: $parentVersion"
+    }
 
-    def pomContent = pomFile.getText('UTF-8')
+    parentGroupId = parentGroupId != null ? parentGroupId : System.console().readLine('Parent groupId: ')
+    parentArtifactId = parentArtifactId != null ? parentArtifactId : System.console().readLine('Parent artifactId: ')
+    parentVersion = parentVersion ? parentVersion : System.console().readLine ('Parent version: ')
+
+    println "Updating " + rootPomFile
 
     def properties = """<!-- Parent project version -->
     <parent.project.version>$parentVersion</parent.project.version>"""
+    replacePlaceHolder(rootPomFile, propertiesMarker, properties)
 
-    pomContent = pomContent.replace(propertiesMarker, properties)
+    println "Updating " + cmsDependenciesPomFile
 
+    def cmdDepText = """<dependency>
+      <groupId>$parentGroupId</groupId>
+      <artifactId>$parentArtifactId-repository-data-application</artifactId>
+      <version>$parentVersion</version>
+    </dependency>"""
+    replacePlaceHolder(cmsDependenciesPomFile, cmsDependenciesMarker, cmdDepText)
+}
+
+private static void replacePlaceHolder(File pomFile, String marker, String text) {
+    def pomContent = pomFile.getText('UTF-8').replace(marker, text)
 // rewrite pom.xml
     pomFile.newWriter().withWriter { w ->
         w << pomContent
     }
-
 }
