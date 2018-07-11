b065af5002e265c74ceeff5a69158b33437ddd1c
Ate Douma <ate.douma@bloomreach.com>
2018-07-11 17:36:29 +0200

    CMS-11289 adapt to the refactored HippoServiceRegistry and removal of the @Singleton and @WhiteboardService annotations

--- search-service/jcr/src/main/java/org/onehippo/cms7/services/search/jcr/service/SearchServiceModule.java
+++ search-service/jcr/src/main/java/org/onehippo/cms7/services/search/jcr/service/SearchServiceModule.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 Hippo B.V. (http://www.onehippo.com)
+ * Copyright 2016-2018 Hippo B.V. (http://www.onehippo.com)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -68,7 +68,7 @@ public class SearchServiceModule extends AbstractReconfigurableDaemonModule {
             }
         };
 
-        HippoServiceRegistry.registerService(searchServiceFactory, SearchServiceFactory.class, SearchServiceFactory.class.getName());
+        HippoServiceRegistry.register(searchServiceFactory, SearchServiceFactory.class);
     }
 
     @Override
@@ -92,7 +92,7 @@ public class SearchServiceModule extends AbstractReconfigurableDaemonModule {
     private void unregister() {
         log.debug("Unregistering search service factory {}", searchServiceFactory);
         if (searchServiceFactory != null) {
-            HippoServiceRegistry.unregisterService(searchServiceFactory, SearchServiceFactory.class, SearchServiceFactory.class.getName());
+            HippoServiceRegistry.unregister(searchServiceFactory, SearchServiceFactory.class);
         }
     }
 
