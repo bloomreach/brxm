4faff30e6393e5bd6b8dde1a0d71c236d6bf7af5
Ate Douma <ate.douma@bloomreach.com>
2018-07-11 17:41:15 +0200

    CMS-11289 adapt to the refactored HippoServiceRegistry and removal of the @Singleton and @WhiteboardService annotations

--- services-autoreload/src/main/java/org/onehippo/cms7/services/autoreload/AutoReloadServiceModule.java
+++ services-autoreload/src/main/java/org/onehippo/cms7/services/autoreload/AutoReloadServiceModule.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014 Hippo B.V. (http://www.onehippo.com)
+ * Copyright 2014-2018 Hippo B.V. (http://www.onehippo.com)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -47,13 +47,13 @@ public class AutoReloadServiceModule extends AbstractReconfigurableDaemonModule 
     @Override
     protected void doInitialize(final Session session) throws RepositoryException {
         service = new AutoReloadServiceImpl(config, new AutoReloadScriptLoader(), AutoReloadServer.getInstance());
-        HippoServiceRegistry.registerService(service, AutoReloadService.class);
+        HippoServiceRegistry.register(service, AutoReloadService.class);
     }
 
     @Override
     protected void doShutdown() {
         if (service != null) {
-            HippoServiceRegistry.unregisterService(service, AutoReloadService.class);
+            HippoServiceRegistry.unregister(service, AutoReloadService.class);
         }
     }
 }
