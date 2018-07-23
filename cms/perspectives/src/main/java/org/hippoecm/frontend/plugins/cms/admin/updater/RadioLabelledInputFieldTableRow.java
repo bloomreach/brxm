7e78ff8fb2a976b6d45a616747947e3b2a678ad3
Mathijs den Burger <mathijs.denburger@bloomreach.com>
2018-07-23 11:54:58 +0200

    CMS-11342 Use non-deprecated constructor of AttributeModifier

--- cms/perspectives/src/main/java/org/hippoecm/frontend/plugins/cms/admin/updater/RadioLabelledInputFieldTableRow.java
+++ cms/perspectives/src/main/java/org/hippoecm/frontend/plugins/cms/admin/updater/RadioLabelledInputFieldTableRow.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2012-2015 Hippo B.V. (http://www.onehippo.com)
+ * Copyright 2012-2018 Hippo B.V. (http://www.onehippo.com)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -33,7 +33,7 @@ public abstract class RadioLabelledInputFieldTableRow extends LabelledInputField
             }
         });
 
-        label.add(new AttributeModifier("style", true, new AbstractReadOnlyModel<String>() {
+        label.add(new AttributeModifier("style", new AbstractReadOnlyModel<String>() {
             @Override
             public String getObject() {
                 if (isRadioVisible()) {
