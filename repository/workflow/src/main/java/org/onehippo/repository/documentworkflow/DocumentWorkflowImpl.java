56d557a86e8691aa932dd49daf8ee2dfd04a2407
Ard Schrijvers <ard.schrijvers@bloomreach.com>
2018-07-25 22:17:52 +0200

    REPO-2085 Do not throw Repository Exception
    This is unhandy for callers of hints(branchId).
    
    Also don't repeat the api javadoc on the impl class

--- repository/workflow/src/main/java/org/onehippo/repository/documentworkflow/DocumentWorkflowImpl.java
+++ repository/workflow/src/main/java/org/onehippo/repository/documentworkflow/DocumentWorkflowImpl.java
@@ -199,44 +199,19 @@ public class DocumentWorkflowImpl extends WorkflowImpl implements DocumentWorkfl
         return Collections.unmodifiableMap(hints);
     }
 
-    /**
-     * <p>
-     * The DocumentWorkflow hints method provides all the operational hints corresponding with the DocumentWorkflow
-     * operational method names which are available to the current user to be invoked. However, only when the returned
-     * value is Boolean.TRUE are they <em>allowed</em> to be invoked. They might have a value of Boolean.FALSE though
-     * when the current document <em>state</em> does not allow executing the specific operation.
-     * </p>
-     * <p>
-     * The returned hints map may also provide additional information, like the current editable "status", if the
-     * document "isLive", or if currently there is a "previewAvailable".
-     * </p>
-     * <p>
-     * Separately, the hints map also returns status information about existing workflow or scheduled requests.
-     * This request status data is returned under key "requests" with as value a nested 'hints'
-     * Map&lt;String,Map&lt;String, Boolean&gt;&gt; per request node identifier,
-     * defining the allowable request operations ({@link #acceptRequest(String)}, {@link #cancelRequest(String)},
-     * {@link #rejectRequest(String, String)}) for each request node.
-     * </p>
-     *
-     * @param branchId a {@link String} with a branchId.
-     * @return a map containing hints given by the workflow, the data in this map may be considered valid until the
-     * document itself of the branchId changes.
-     * @throws WorkflowException   indicates that the work-flow call failed due work-flow specific conditions
-     * @throws RepositoryException indicates that the work-flow call failed because of storage problems internal to the
-     *                             repository
-     * @throws RemoteException     indicates that the work-flow call failed because of a connection problem with the
-     *                             repository
-     */
     @Override
-    public Map<String, Serializable> hints(final String branchId) throws WorkflowException, RepositoryException {
+    public Map<String, Serializable> hints(final String branchId) throws WorkflowException {
         BranchHandle branchHandle = new BranchHandleImpl(branchId, getNode());
         DocumentHandle documentHandle = workflowExecutor.getData();
-        BranchHints branchHints =  new BranchHints.Builder()
-                .branchHandle(branchHandle)
-                .documentHandle(documentHandle)
-                .hints(hints())
-                .build();
-        return branchHints.getHints();
+        try {
+            return  new BranchHints.Builder()
+                    .branchHandle(branchHandle)
+                    .documentHandle(documentHandle)
+                    .hints(hints())
+                    .build().getHints();
+        } catch (RepositoryException e) {
+            throw new WorkflowException(String.format("Could not build hints for branchId '%s'", branchId), e);
+        }
 
     }
 
