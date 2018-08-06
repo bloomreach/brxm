be995440ab63fa77874d28d0045faaa6c0c0e2c4
Bert Leunis <bert.leunis@bloomreach.com>
2018-08-06 16:14:56 +0200

    CMS-11375 restore annotation: method still overrides another method
    Although it is a different one than before.

--- cms/api/src/main/java/org/hippoecm/frontend/service/render/AbstractRenderService.java
+++ cms/api/src/main/java/org/hippoecm/frontend/service/render/AbstractRenderService.java
@@ -443,6 +443,7 @@ public abstract class AbstractRenderService<T> extends Panel implements IObserve
     /**
      * {@inheritDoc}
      */
+    @Override
     public String getId() {
         return wicketId;
     }
