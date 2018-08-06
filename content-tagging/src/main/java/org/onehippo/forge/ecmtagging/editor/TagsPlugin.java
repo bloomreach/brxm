205510b4d7b9051a1d7af6a82b13ddbf4844607c
Mathijs den Burger <mathijs.denburger@bloomreach.com>
2018-08-06 15:01:46 +0200

    CMS-11375 Remove usage of removed method TextAreaWidget#setCols
    setCols was deprecated and is removed in 13.0.

--- content-tagging/src/main/java/org/onehippo/forge/ecmtagging/editor/TagsPlugin.java
+++ content-tagging/src/main/java/org/onehippo/forge/ecmtagging/editor/TagsPlugin.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2008-2015 Hippo B.V. (http://www.onehippo.com)
+ *  Copyright 2008-2018 Hippo B.V. (http://www.onehippo.com)
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -138,7 +138,6 @@ public class TagsPlugin extends AbstractTagsPlugin {
     public TextAreaWidget createTextArea(TagsModel tagModel) {
         TextAreaWidget widget = new TextAreaWidget("value", tagModel);
         widget.setRows(rows);
-        widget.setCols(cols);
         //widget.addBehaviourOnFormComponent(new TagBehaviour(getPluginContext(), getPluginConfig(), nodeModel, createTagModel(nodeModel)));
         return widget;
     }
