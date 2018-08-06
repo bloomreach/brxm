6e03cd9ec8ae636f2f53686da4c021fdb49dfed1
Mathijs den Burger <mathijs.denburger@bloomreach.com>
2018-08-06 11:18:28 +0200

    CMS-11375 Remove IServiceTracker#updateService

--- cms/engine/src/main/java/org/hippoecm/frontend/plugin/impl/ClusterControl.java
+++ cms/engine/src/main/java/org/hippoecm/frontend/plugin/impl/ClusterControl.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2008-2013 Hippo B.V. (http://www.onehippo.com)
+ *  Copyright 2008-2018 Hippo B.V. (http://www.onehippo.com)
  * 
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -193,9 +193,6 @@ public class ClusterControl implements IClusterControl, IServiceTracker<ICluster
         } else {
             log.error("Unknown extension " + serviceId + " of service " + service);
         }
-    }
-
-    public void updateService(IClusterable service, String name) {
     }
 
     public void detach() {
