95db571920d762c52ac48af870987e2691b07cb9
Michael Metternich <michael.metternich@bloomreach.com>
2018-09-13 15:39:54 +0200

    CMS-11252 Change brand name on tab for CMS and Console

--- cms/console/frontend/src/main/java/org/hippoecm/frontend/plugins/console/RootPlugin.java
+++ cms/console/frontend/src/main/java/org/hippoecm/frontend/plugins/console/RootPlugin.java
@@ -116,7 +116,7 @@ public class RootPlugin extends RenderPlugin {
     }
 
     private String getPageTitle(IPluginConfig config) {
-        StringBuilder pageTitle = new StringBuilder(config.getString("page.title", "Hippo CMS Console"));
+        StringBuilder pageTitle = new StringBuilder(config.getString("page.title", "BloomReach Console"));
         if(config.getAsBoolean("page.title.showservername", false)) {
             pageTitle.append(config.getString("page.title.separator", "@"));
             pageTitle.append(getServerName());
