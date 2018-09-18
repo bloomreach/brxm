7048cc51cf275f0a16d81d9e2c1a2b2844704a64
Bert Leunis <bert.leunis@bloomreach.com>
2018-09-18 15:04:47 +0200

    CMS-11419 remove @Color annotation
    The annotation is not longer supported. Color values must be entered as strings in a regular text field.

--- testsuite/components/src/main/java/org/hippoecm/hst/demo/components/BannerInfo.java
+++ testsuite/components/src/main/java/org/hippoecm/hst/demo/components/BannerInfo.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2010-2017 Hippo B.V. (http://www.onehippo.com)
+ *  Copyright 2010-2018 Hippo B.V. (http://www.onehippo.com)
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -13,19 +13,15 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-
 package org.hippoecm.hst.demo.components;
-
 
 import java.util.Date;
 
-import org.hippoecm.hst.core.parameters.Color;
 import org.hippoecm.hst.core.parameters.DropDownList;
 import org.hippoecm.hst.core.parameters.JcrPath;
 import org.hippoecm.hst.core.parameters.Parameter;
 
 public interface BannerInfo {
-
     @Parameter(name = "bannerWidth", displayName = "Banner Width")
     int getBannerWidth();
 
@@ -36,7 +32,6 @@ public interface BannerInfo {
     Date getDate();
 
     @Parameter(name = "borderColor", displayName = "Border Color", required = true)
-    @Color
     String getBorderColor();
 
     @Parameter(name = "content", displayName = "Content")
@@ -55,5 +50,4 @@ public interface BannerInfo {
     @Parameter(name = "cssDisplay2", displayName = "CSS Display 2")
     @DropDownList(valueListProvider = CssDisplayValueListProvider.class)
     String getCssDisplay2();
-
 }
