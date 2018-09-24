4011c4c547dec21dc2fe3a2446ab0872f945db78
Mathijs den Burger <mathijs.denburger@bloomreach.com>
2018-09-24 15:24:36 +0200

    CHANNELMGR-2093 Remove dirty state from ComponentEditor service
    Dirty state is now maintained and handled by editContentMainCtrl.

--- channel-manager/frontend-ng/src/app/channel/sidePanels/rightSidePanel/editComponent/componentEditor.service.js
+++ channel-manager/frontend-ng/src/app/channel/sidePanels/rightSidePanel/editComponent/componentEditor.service.js
@@ -157,21 +157,12 @@ class ComponentEditorService {
     return undefined;
   }
 
-  get dirty() {
-    return !!this.propertiesDirty;
-  }
-
-  set dirty(dirty) {
-    this.propertiesDirty = dirty;
-  }
-
   valueChanged() {
     this.PageStructureService.renderComponent(this.component.id, this._propertiesAsFormData());
   }
 
   save() {
-    return this.HstComponentService.setParameters(this.component.id, this.component.variant, this._propertiesAsFormData())
-      .then(() => delete this.propertiesDirty);
+    return this.HstComponentService.setParameters(this.component.id, this.component.variant, this._propertiesAsFormData());
   }
 
   _propertiesAsFormData() {
@@ -236,10 +227,6 @@ class ComponentEditorService {
   }
 
   _askSaveOrDiscardChanges() {
-    if (!this.propertiesDirty) {
-      return this.$q.resolve();
-    }
-
     const message = this.$translate.instant('SAVE_CHANGES_TO_COMPONENT', { componentLabel: this.component.label });
     const title = this.$translate.instant('SAVE_CHANGES_TITLE');
 
@@ -263,7 +250,6 @@ class ComponentEditorService {
     delete this.page;
     delete this.properties;
     delete this.propertyGroups;
-    delete this.propertiesDirty;
   }
 }
 
