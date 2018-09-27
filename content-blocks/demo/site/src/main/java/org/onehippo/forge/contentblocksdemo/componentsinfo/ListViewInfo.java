7293ea0d916d2673121d756ab9d6c8bf1593e367
Bert Leunis <bert.leunis@bloomreach.com>
2018-09-27 16:25:31 +0200

    HIPPLUG-1557 remove import

--- content-blocks/demo/site/src/main/java/org/onehippo/forge/contentblocksdemo/componentsinfo/ListViewInfo.java
+++ content-blocks/demo/site/src/main/java/org/onehippo/forge/contentblocksdemo/componentsinfo/ListViewInfo.java
@@ -16,7 +16,6 @@
 package org.onehippo.forge.contentblocksdemo.componentsinfo;
 
 import org.hippoecm.hst.configuration.hosting.Mount;
-import org.hippoecm.hst.core.parameters.Color;
 import org.hippoecm.hst.core.parameters.FieldGroup;
 import org.hippoecm.hst.core.parameters.FieldGroupList;
 import org.hippoecm.hst.core.parameters.Parameter;
