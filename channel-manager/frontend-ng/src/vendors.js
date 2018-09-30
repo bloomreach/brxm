53ffb432c197701ed362f28d8a9b85a1c278bef0
Arthur Bogaart <arthur.bogaart@bloomreach.com>
2018-09-30 23:53:41 +0200

    CHANNELMGR-2120 Add vendor-styles entry
    The content of vendor.scss will generate a bundle separate from
    the bootstrap, app and packages bundles.

--- channel-manager/frontend-ng/src/vendors.js
+++ channel-manager/frontend-ng/src/vendors.js
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015-2018 Hippo B.V. (http://www.onehippo.com)
+ * Copyright 2018 Hippo B.V. (http://www.onehippo.com)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,8 +14,4 @@
  * limitations under the License.
  */
 
-import './index.scss';
-import './app/hippo-cm';
-
-// Uncomment to visualize ui-router state (see https://github.com/ui-router/visualizer)
-// import './visualize-state';
+import './vendor.scss';
