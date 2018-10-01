acc3bff01619a31f9eba8fad25a0c8781373e391
Sergey Shepelevich <sergey.shepelevich@bloomreach.com>
2018-10-01 16:49:26 +0200

    REPO-2142 Read module along with supplied site name if available
    (cherry picked from commit bfa531b175f7628a60ddf31b021d6a321612199f)

--- configuration-management/model/src/main/java/org/onehippo/cm/model/parser/ModuleReader.java
+++ configuration-management/model/src/main/java/org/onehippo/cm/model/parser/ModuleReader.java
@@ -23,8 +23,10 @@ import java.nio.file.Path;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.function.BiPredicate;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import org.apache.commons.lang3.tuple.Pair;
@@ -92,16 +94,17 @@ public class ModuleReader {
         return moduleContext;
     }
 
-    public ModuleContext readReplacement(final Path moduleDescriptorPath, final ConfigurationModelImpl comparisonModel)
-            throws IOException, ParserException {
-        final ModuleImpl module = readDescriptor(moduleDescriptorPath, null);
+    public ModuleContext readReplacement(final Path moduleDescriptorPath, final ConfigurationModelImpl comparisonModel,
+                                         final String siteName) throws IOException, ParserException {
+        final ModuleImpl module = readDescriptor(moduleDescriptorPath, siteName);
 
-        // if there was already a matching module in the comparisonModule, use the extension info from that one
-        comparisonModel.getModulesStream().filter(Predicate.isEqual(module)).findFirst()
-                .ifPresent(comparisonModule -> {
-            module.getProject().getGroup().setSite(new SiteImpl(comparisonModule.getSiteName()));
-            module.setHstRoot(comparisonModule.getHstRoot());
-        });
+        // if there was already a matching module in the comparisonModule, use the hst root info from that one
+        final Optional<ModuleImpl> existingModule = comparisonModel.getModulesStream().filter(Predicate.isEqual(module)).findFirst();
+        if (siteName != null) {
+            existingModule.ifPresent(comparisonModule -> {
+                module.setHstRoot(comparisonModule.getHstRoot());
+            });
+        }
 
         final ModuleContext moduleContext = new ModuleContext(module, moduleDescriptorPath);
         readModule(module, moduleContext, true);
