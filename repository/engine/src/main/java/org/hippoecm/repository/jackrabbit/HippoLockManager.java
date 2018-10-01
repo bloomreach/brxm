579ec38f340489299ce8dce07ea91f64633a786d
Ate Douma <ate.douma@bloomreach.com>
2018-10-01 16:25:43 +0200

    REPO-1900: using default Jackrabbit ScheduledExecutorService for cleaning up timedout locks.
    Also restoring corresponding timedOutLockIsNotLocked() test in LockTest

--- repository/engine/src/main/java/org/hippoecm/repository/jackrabbit/HippoLockManager.java
+++ repository/engine/src/main/java/org/hippoecm/repository/jackrabbit/HippoLockManager.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2012-2013 Hippo B.V. (http://www.onehippo.com)
+ *  Copyright 2012-2018 Hippo B.V. (http://www.onehippo.com)
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -47,7 +47,7 @@ public class HippoLockManager extends LockManagerImpl {
      * @throws javax.jcr.RepositoryException if an error occurs
      */
     public HippoLockManager(org.apache.jackrabbit.core.SessionImpl session, FileSystem fs, ScheduledExecutorService executor) throws RepositoryException {
-        super(session, fs, null);
+        super(session, fs, executor);
     }
 
     @Override
