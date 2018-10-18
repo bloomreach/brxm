21772470eb9ef5452b5e1a80fca05878eb1106ad
Ard Schrijvers <ard.schrijvers@bloomreach.com>
2018-10-18 13:55:13 +0200

    HSTTWO-4472 make test sitemap handler classes everywhere available
    since the sitemap handlers are now initialized eagerly instead of
    lazily, they always need to be present, hence move them to the test jar

--- site-toolkit/toolkit-resources/addon/unittestcontents/src/main/java/org/hippoecm/hst/test/sitemapitemhandler/NoopExampleHandlerItem1.java
+++ site-toolkit/toolkit-resources/addon/unittestcontents/src/main/java/org/hippoecm/hst/test/sitemapitemhandler/NoopExampleHandlerItem1.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2013 Hippo B.V. (http://www.onehippo.com)
+ * Copyright 2010-2018 Hippo B.V. (http://www.onehippo.com)
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,7 +16,7 @@
 package org.hippoecm.hst.test.sitemapitemhandler;
 
 
-public class NoopHandlerItem extends AbstractTestHstSiteItemMapHandler {
+public class NoopExampleHandlerItem1 extends AbstractTestHstSiteItemMapHandler {
 
  
 }
