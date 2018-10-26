7d680ba0bc78cc948234ccf8fec72780a0f4a065
Bert Leunis <bert.leunis@bloomreach.com>
2018-10-26 16:20:37 +0200

    CMS-11529 update path matchers to get the color for hst nodes back

--- cms/console/frontend/src/main/java/org/hippoecm/frontend/plugins/console/icons/JcrNodeIcon.java
+++ cms/console/frontend/src/main/java/org/hippoecm/frontend/plugins/console/icons/JcrNodeIcon.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2015-2017 Hippo B.V. (http://www.onehippo.com)
+ *  Copyright 2015-2018 Hippo B.V. (http://www.onehippo.com)
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -208,8 +208,8 @@ public class JcrNodeIcon {
         // reports
         primaryTypeNameIcons.put("hipporeport:folder", FontAwesomeIcon.PIECHART);
 
-        pathCssMatchers.put("^/hst:hst.*-preview.*$", "hst-preview");
-        pathCssMatchers.put("^/hst:hst.*$", "hst");
+        pathCssMatchers.put("^/hst:.*-preview.*$", "hst-preview");
+        pathCssMatchers.put("^/hst:.*$", "hst");
         pathCssMatchers.put("^/" + CONFIGURATION_PATH + ".*$", "conf");
         pathCssMatchers.put("^/" + HCM_ROOT + ".*$", "system");
         pathCssMatchers.put("^/" + JCR_SYSTEM + ".*$", "system");
