3ca91448a4e2517b8e9c2f78cbf18614a56f28ae
Woonsan Ko <woonsan.ko@bloomreach.com>
2018-11-20 05:06:10 -0500

    CRISP-69: recreating crisp demo

---
+++ crisp/demo/site/components/src/main/java/org/onehippo/cms7/crisp/demo/beans/BaseDocument.java
@@ -0,0 +1,9 @@
+package org.onehippo.cms7.crisp.demo.beans;
+
+import org.hippoecm.hst.content.beans.Node;
+import org.hippoecm.hst.content.beans.standard.HippoDocument;
+
+@Node(jcrType="crispdemo:basedocument")
+public class BaseDocument extends HippoDocument {
+
+}
