5ee7480579706f2485155cc23700d82709c62574
Arthur Bogaart <arthur.bogaart@bloomreach.com>
2018-12-07 17:24:04 +0100

    CMS-11583 Disable form fields when submitting

--- cms/login/src/main/resources/org/hippoecm/frontend/plugins/login/PreventResubmit.js
+++ cms/login/src/main/resources/org/hippoecm/frontend/plugins/login/PreventResubmit.js
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015 Hippo B.V. (http://www.onehippo.com)
+ * Copyright 2015-2018 Hippo B.V. (http://www.onehippo.com)
  *
  * Licensed under the Apache License, Version 2.0 (the  "License");
  * you may not use this file except in compliance with the License.
@@ -31,7 +31,7 @@
           if (el.submitting) {
             e.preventDefault();
           } else {
-            $(':submit', el).prop('disabled', true);
+            $(':submit, :input', el).prop('disabled', true);
             el.submitting = true;
           }
         });
