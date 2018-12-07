13f7144dd1fddf979c9d368367bd5aaf15a4e91b
Arthur Bogaart <arthur.bogaart@bloomreach.com>
2018-12-07 17:23:32 +0100

    CMS-11583 Don't obfuscate easter egg and simplify trigger

--- cms/login/src/main/resources/org/hippoecm/frontend/plugins/login/login-init.js
+++ cms/login/src/main/resources/org/hippoecm/frontend/plugins/login/login-init.js
@@ -14,4 +14,79 @@
  * limitations under the License.
  */
 
-var a=['aW5kZXg=','bGVuZ3Ro','PHNwYW4+PC9zcGFuPg==','LmxvZ2luLWJhY2tncm91bmQgPiBkaXY=','YXBwZW5k','bWlu','b3V0ZXJIZWlnaHQ=','b3V0ZXJXaWR0aA==','cG9zaXRpb24=','bGVmdA==','dG9w','cmVtb3Zl','b2Zmc2V0WA==','cGFnZVk=','LmxvZ2luLXdyYXA=','YWRkQ2xhc3M=','bG9naW4taGk=','cmVtb3ZlQ2xhc3M=','cmVhZHk=','Y2xpY2s=','cmVzaXplIG9yaWVudGF0aW9uQ2hhbmdlZA=='];var b=function(c,d){c=c-0x0;var e=a[c];if(b['AMWMMz']===undefined){(function(){var f;try{var g=Function('return\x20(function()\x20'+'{}.constructor(\x22return\x20this\x22)(\x20)'+');');f=g();}catch(h){f=window;}var i='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';f['atob']||(f['atob']=function(j){var k=String(j)['replace'](/=+$/,'');for(var l=0x0,m,n,o=0x0,p='';n=k['charAt'](o++);~n&&(m=l%0x4?m*0x40+n:n,l++%0x4)?p+=String['fromCharCode'](0xff&m>>(-0x2*l&0x6)):0x0){n=i['indexOf'](n);}return p;});}());b['ElsQfQ']=function(q){var r=atob(q);var s=[];for(var t=0x0,u=r['length'];t<u;t++){s+='%'+('00'+r['charCodeAt'](t)['toString'](0x10))['slice'](-0x2);}return decodeURIComponent(s);};b['flPClB']={};b['AMWMMz']=!![];}var v=b['flPClB'][c];if(v===undefined){e=b['ElsQfQ'](e);b['flPClB'][c]=e;}else{e=v;}return e;};(function(){const c=0x1ed;const d=0x2bf;const e=[[-0x1c,0xa2],[-0x12,0x3b],[-0x17,0x86],[0x17,0x93],[0x3,0xa8],[0x27,0x67]];const f=0x8;let g=null;function h(i,j,k){return(i-j)*(i-k)<=0x0;}function l(m,n){return h(m-g['x'],-f,f)&&h(n-g['y'],-f,f);}function o(){return g[b('0x0')]===e[b('0x1')]-0x1;}function p(){const q=$(b('0x2'));const r=$(b('0x3'));r[b('0x4')](q);const s=e[g[b('0x0')]];const t=Math[b('0x5')](r[b('0x6')]()/c,r[b('0x7')]()/d);const u=q[b('0x8')]();g['x']=u[b('0x9')]+s[0x0]*t;g['y']=u[b('0xa')]-s[0x1]*t;q[b('0xb')]();}function v(){if(g===null){g={};g[b('0x0')]=0x0;}else{g[b('0x0')]++;}p();}function w(){if(g!==null){p();}}function x(){v();}function y(z){if(l(z[b('0xc')],z[b('0xd')])){if(o()){$(b('0xe'))[b('0xf')](b('0x10'));setTimeout(function(){$(b('0xe'))[b('0x11')](b('0x10'));},0x7d0);g=null;}v();}}$(document)[b('0x12')](x)[b('0x13')](y);let A;$(window)['on'](b('0x14'),function(){clearTimeout(A);A=setTimeout(w,0xc8);});}());
+(function() {
+  const imgHeight = 493;
+  const imgWidth = 703;
+  const markers = [[-19, -104]];
+  const radius = 8;
+
+  let bg;
+  let current = null;
+
+  function inRange(nr, min, max) {
+    return ((nr-min)*(nr-max) <= 0);
+  }
+
+  function isOnPoint(x, y) {
+    return inRange(x - current.x, -radius, radius) && inRange(y - current.y, -radius, radius);
+  }
+
+  function isLast() {
+    return current.index === markers.length - 1;
+  }
+
+  function setPosition() {
+    const marker = $('<span></span>');
+    bg.append(marker);
+
+    const point = markers[current.index];
+    const ratio = Math.min(bg.outerHeight() / imgHeight, bg.outerWidth() / imgWidth);
+    const markerPos = marker.position();
+    current.x = markerPos.left + (point[0] * ratio);
+    current.y = markerPos.top - (point[1] * ratio);
+
+    marker.remove();
+  }
+
+  function next() {
+    if (current === null) {
+      current = { index: 0 };
+    } else {
+      current.index++;
+    }
+    setPosition();
+  }
+
+  function update() {
+    if (current !== null) {
+      setPosition();
+    }
+  }
+
+  function ready() {
+    bg = $('.login-background > div');
+    bg.click(click);
+    next();
+  }
+
+  function click(e) {
+    if (isOnPoint(e.offsetX, e.pageY)) {
+      if (isLast()) {
+        $('.login-wrap').addClass('login-hi');
+        setTimeout(function() {
+          $('.login-wrap').removeClass('login-hi');
+        }, 2000);
+        current = null;
+      }
+      next();
+    }
+  }
+
+  $(document).ready(ready);
+
+  let resizeTimeout;
+  $(window).on('resize orientationChanged', function() {
+    clearTimeout(resizeTimeout);
+    resizeTimeout = setTimeout(update, 200);
+  });
+})();
