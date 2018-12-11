47ef5752c5046b565bb7835c5be635ab929c1f7a
Arthur Bogaart <arthur.bogaart@bloomreach.com>
2018-12-11 16:49:56 +0100

    CMS-11121 Do not redraw editor if node.isCheckedOut() did not change
    The console node editor can be displayed in read-only mode for nodes
    that are checked-in. Previously, any change to the nodeModel would
    trigger a redraw of the full editor, resulting in focus loss.
    
    With the current change, the editor is redrawn when the nodeModel
    changes (user select a different node) or when a change is made to the
    node that changes it's checked-out/checked-in state.

--- cms/console/frontend/src/main/java/org/hippoecm/frontend/plugins/console/editor/EditorPlugin.java
+++ cms/console/frontend/src/main/java/org/hippoecm/frontend/plugins/console/editor/EditorPlugin.java
@@ -1,12 +1,12 @@
 /*
- *  Copyright 2008-2017 Hippo B.V. (http://www.onehippo.com)
- * 
+ *  Copyright 2008-2018 Hippo B.V. (http://www.onehippo.com)
+ *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
  *  You may obtain a copy of the License at
- * 
+ *
  *       http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  *  Unless required by applicable law or agreed to in writing, software
  *  distributed under the License is distributed on an "AS IS" BASIS,
  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -18,22 +18,22 @@ package org.hippoecm.frontend.plugins.console.editor;
 import javax.jcr.Node;
 import javax.jcr.RepositoryException;
 
-import org.apache.wicket.AttributeModifier;
 import org.apache.wicket.event.IEvent;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.model.IModel;
+import org.hippoecm.frontend.model.ReadOnlyModel;
 import org.hippoecm.frontend.plugin.IPluginContext;
 import org.hippoecm.frontend.plugin.config.IPluginConfig;
+import org.hippoecm.frontend.plugins.standards.list.resolvers.CssClass;
 import org.hippoecm.frontend.plugins.yui.togglebehavior.ToggleBehavior;
 import org.hippoecm.frontend.service.render.RenderPlugin;
 
 public class EditorPlugin extends RenderPlugin<Node> {
-    private static final long serialVersionUID = 1L;
 
     private final NodeEditor editor;
-    private Label message;
+    private final Label message;
 
-    public EditorPlugin(IPluginContext context, IPluginConfig config) {
+    public EditorPlugin(final IPluginContext context, final IPluginConfig config) {
         super(context, config);
 
         message = new Label("message", getString("node.checked.in.info"));
@@ -42,6 +42,17 @@ public class EditorPlugin extends RenderPlugin<Node> {
         editor = new NodeEditor("editor", getModel());
         add(editor);
         add(new ToggleBehavior());
+
+        add(CssClass.append(ReadOnlyModel.of(() -> {
+            final Node node = getModel().getObject();
+            try {
+                if (node != null && !node.isCheckedOut()) {
+                    return "checked-in";
+                }
+            } catch (final RepositoryException ignore) {
+            }
+            return "";
+        })));
 
         onModelChanged();
     }
@@ -53,23 +64,25 @@ public class EditorPlugin extends RenderPlugin<Node> {
     @Override
     public void onModelChanged() {
         super.onModelChanged();
-        IModel<Node> newModel = getModel();
+        final IModel<Node> newModel = getModel();
         try {
             final Node node = newModel.getObject();
             if (node != null) {
-                if (node.isCheckedOut()) {
-                    add(new AttributeModifier("style", ""));
-                } else {
-                    add(new AttributeModifier("style", "background-color:#ddd;"));
+                final boolean nodeIsEditable = node.isCheckedOut();
+                if (editor.isEnabled() != nodeIsEditable) {
+                    editor.setEnabled(nodeIsEditable);
+                    redraw();
                 }
-                message.setVisible(!node.isCheckedOut());
-                editor.setEnabled(node.isCheckedOut());
-                redraw();
-            }
 
-        } catch (RepositoryException e) {
+                if (message.isVisible() == nodeIsEditable) {
+                    message.setVisible(!nodeIsEditable);
+                    redraw();
+                }
+            }
+        } catch (final RepositoryException e) {
             // ignore
         }
+
         if (!editor.getModel().equals(newModel)) {
             editor.setModel(newModel);
             redraw();
@@ -78,14 +91,14 @@ public class EditorPlugin extends RenderPlugin<Node> {
 
 
     @Override
-    public void onEvent(IEvent<?> event) {
+    public void onEvent(final IEvent<?> event) {
         super.onEvent(event);
 
         if (event.getPayload() instanceof EditorUpdate)
         {
             redraw();
-            
-            EditorUpdate update = (EditorUpdate) event.getPayload();
+
+            final EditorUpdate update = (EditorUpdate) event.getPayload();
             update.getTarget().add(this);
         }
     }
