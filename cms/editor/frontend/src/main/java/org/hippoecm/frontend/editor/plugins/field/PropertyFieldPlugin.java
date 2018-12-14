91490be727ea70ce958e85ebb871b28a3adaaba4
Bert Leunis <bert.leunis@bloomreach.com>
2018-12-14 10:42:07 +0100

    CMS-11612 use constants for validators; use util for checking required
    The ValidatorUtils is moved to the cms api to make it more broadly available.
    
    (cherry picked from commit 14a7eb700474d35e6accc2ce7299cb60ffbc2782)

--- cms/editor/frontend/src/main/java/org/hippoecm/frontend/editor/plugins/field/PropertyFieldPlugin.java
+++ cms/editor/frontend/src/main/java/org/hippoecm/frontend/editor/plugins/field/PropertyFieldPlugin.java
@@ -31,7 +31,7 @@ import org.apache.wicket.markup.html.panel.Fragment;
 import org.apache.wicket.markup.repeater.Item;
 import org.apache.wicket.model.IModel;
 import org.hippoecm.frontend.editor.plugins.fieldhint.FieldHint;
-import org.hippoecm.frontend.editor.validator.ValidatorUtils;
+import org.hippoecm.frontend.validation.ValidatorUtils;
 import org.hippoecm.frontend.model.AbstractProvider;
 import org.hippoecm.frontend.model.JcrNodeModel;
 import org.hippoecm.frontend.model.PropertyValueProvider;
@@ -47,8 +47,6 @@ import org.hippoecm.frontend.service.IRenderService;
 import org.hippoecm.frontend.skin.Icon;
 import org.hippoecm.frontend.types.IFieldDescriptor;
 import org.hippoecm.frontend.types.ITypeDescriptor;
-
-import static org.hippoecm.frontend.editor.validator.ValidatorUtils.REQUIRED_VALIDATOR;
 
 public class PropertyFieldPlugin extends AbstractFieldPlugin<Property, JcrPropertyValueModel> {
 
@@ -80,7 +78,7 @@ public class PropertyFieldPlugin extends AbstractFieldPlugin<Property, JcrProper
         final IFieldDescriptor field = getFieldHelper().getField();
         if (field != null) {
             subscribe(field);
-            if (!field.getValidators().contains(REQUIRED_VALIDATOR)) {
+            if (!ValidatorUtils.hasRequiredValidator(field.getValidators())) {
                 required.setVisible(false);
             }
 
