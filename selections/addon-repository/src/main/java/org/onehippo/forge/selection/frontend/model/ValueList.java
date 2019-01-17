3beadcd317329a0de489ea764250d31fae2c179c
Bert Leunis <bert.leunis@bloomreach.com>
2019-01-17 13:52:27 +0100

    CHANNELMGR-1396 whitespace formatting

--- selections/addon-repository/src/main/java/org/onehippo/forge/selection/frontend/model/ValueList.java
+++ selections/addon-repository/src/main/java/org/onehippo/forge/selection/frontend/model/ValueList.java
@@ -1,18 +1,18 @@
 /*
  * Copyright 2010-2019 Hippo B.V. (http://www.onehippo.com)
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
-*/
+ */
 
 package org.onehippo.forge.selection.frontend.model;
 
@@ -20,8 +20,7 @@ import java.io.Serializable;
 import java.util.ArrayList;
 
 /**
- * Value list object, i.e. an array list with ListItem objects, reflecting a
- * value list document.
+ * Value list object, i.e. an array list with ListItem objects, reflecting a value list document.
  *
  * @author Jeroen Hoffman
  * @author Dennis Dam
@@ -37,9 +36,8 @@ public class ValueList extends ArrayList<ListItem> implements Serializable {
 
     /**
      * The index of the specified object in this list.
-     *
-     * The object can be either a listitem or a string. If it
-     * is a string, then it represents the key of a list item.
+     * <p>
+     * The object can be either a listitem or a string. If it is a string, then it represents the key of a list item.
      *
      * @param element a listitem or a string
      * @return index of a list item in the list
@@ -63,9 +61,8 @@ public class ValueList extends ArrayList<ListItem> implements Serializable {
     }
 
     /**
-     * Returns the key of the list item associated with the provided object.
-     * The object can be either a listitem or a string. If it
-     * is a string, then it represents the key of a list item.
+     * Returns the key of the list item associated with the provided object. The object can be either a listitem or a
+     * string. If it is a string, then it represents the key of a list item.
      *
      * @param object a listitem or a string
      * @return the key associated with the listitem/string
@@ -85,9 +82,8 @@ public class ValueList extends ArrayList<ListItem> implements Serializable {
     }
 
     /**
-     * Returns the label of the list item associated with the provided object.
-     * The object can be either a listitem or a string. If it
-     * is a string, then it represents the key of a list item.
+     * Returns the label of the list item associated with the provided object. The object can be either a listitem or a
+     * string. If it is a string, then it represents the key of a list item.
      *
      * @param object a listitem or a string
      * @return the label associated with the listitem/string
@@ -112,10 +108,11 @@ public class ValueList extends ArrayList<ListItem> implements Serializable {
 
     /**
      * Looks up a listitem by matching the given key.
+     *
      * @param key the key of a listitem
      * @return the listitem of which the key is equal to the given key
      */
-    public ListItem getListItemByKey(final String key){
+    public ListItem getListItemByKey(final String key) {
         for (ListItem valueListItem : this) {
             if (valueListItem.getKey().equals(key)) {
                 return valueListItem;
