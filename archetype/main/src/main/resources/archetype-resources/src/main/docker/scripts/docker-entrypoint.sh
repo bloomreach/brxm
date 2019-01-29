1df3789fac1e3234efc727327cf964c710830f23
Peter Centgraf <peter.centgraf@bloomreach.com>
2019-01-29 14:07:41 +0100

    ARCHE-601 Reduce number of docker image layers; Add more inline comments

--- archetype/main/src/main/resources/archetype-resources/src/main/docker/scripts/docker-entrypoint.sh
+++ archetype/main/src/main/resources/archetype-resources/src/main/docker/scripts/docker-entrypoint.sh
@@ -2,8 +2,11 @@
 
 set -e
 
+# perform text replacements to apply environment vars properly to jackrabbit database configuration
 source /brxm/bin/$profile/setup-db.sh
 
+# use the appropriate database context info for the selected database type
 cp /usr/local/tomcat/conf/context-$profile.xml /usr/local/tomcat/conf/context.xml
 
+# run tomcat
 exec /usr/local/tomcat/bin/catalina.sh run
