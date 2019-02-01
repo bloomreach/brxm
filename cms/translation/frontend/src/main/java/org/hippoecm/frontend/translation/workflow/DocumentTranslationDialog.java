408ac844aed5558154ea32488873ee1d2de99dc4
Bert Leunis <bert.leunis@bloomreach.com>
2019-02-01 16:38:01 +0100

    CMS-11647 fix the button styling of the translations dialog
    The AbstractWorkflowDialog now extends Dialog, which gives the correct styling.

--- cms/translation/frontend/src/main/java/org/hippoecm/frontend/translation/workflow/DocumentTranslationDialog.java
+++ cms/translation/frontend/src/main/java/org/hippoecm/frontend/translation/workflow/DocumentTranslationDialog.java
@@ -1,12 +1,12 @@
 /*
- *  Copyright 2010-2015 Hippo B.V. (http://www.onehippo.com)
- * 
+ *  Copyright 2010-2019 Hippo B.V. (http://www.onehippo.com)
+ *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
  *  You may obtain a copy of the License at
- * 
+ *
  *       http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  *  Unless required by applicable law or agreed to in writing, software
  *  distributed under the License is distributed on an "AS IS" BASIS,
  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -35,18 +35,15 @@ import org.hippoecm.repository.api.StringCodecFactory;
 
 public class DocumentTranslationDialog extends AbstractWorkflowDialog<Void> {
 
-    private static final long serialVersionUID = 1L;
-
-    private IModel<String> title;
     private ISettingsService settingsService;
 
     public DocumentTranslationDialog(ISettingsService settings,
-            IWorkflowInvoker action, IModel<String> title, List<FolderTranslation> folders, IModel<Boolean> autoTranslateContent,
-            String sourceLanguage, final String targetLanguage,
-            ILocaleProvider provider) {
+                                     IWorkflowInvoker action, IModel<String> title, List<FolderTranslation> folders,
+                                     IModel<Boolean> autoTranslateContent, String sourceLanguage, 
+                                     final String targetLanguage, ILocaleProvider provider) {
         super(null, action);
         this.settingsService = settings;
-        this.title = title;
+        setTitle(title);
 
         DocumentTranslationView dtv = new DocumentTranslationView("grid", folders,
                 sourceLanguage, targetLanguage,
@@ -61,15 +58,11 @@ public class DocumentTranslationDialog extends AbstractWorkflowDialog<Void> {
                 }, provider);
         dtv.setFrame(false);
         add(dtv);
-        if(autoTranslateContent != null) {
+        if (autoTranslateContent != null) {
             add(new BooleanFieldWidget("translate", autoTranslateContent));
         } else {
             add(new Label("translate").setVisible(false));
         }
-    }
-
-    public IModel<String> getTitle() {
-        return title;
     }
 
     @Override
