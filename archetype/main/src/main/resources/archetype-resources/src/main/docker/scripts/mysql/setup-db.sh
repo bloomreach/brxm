804854ac6853cd8d536e89ad9fb7869c5d9b170d
Cuneyt Gul <cuneyt.gul@bloomreach.com>
2019-02-20 14:35:15 +0100

    CMS-11704 set jackrabbit cluster node id when running in docker

--- archetype/main/src/main/resources/archetype-resources/src/main/docker/scripts/mysql/setup-db.sh
+++ archetype/main/src/main/resources/archetype-resources/src/main/docker/scripts/mysql/setup-db.sh
@@ -11,6 +11,12 @@ sed --in-place 's/@mysql.driver@/'"$MYSQL_DB_DRIVER"'/' /usr/local/tomcat/conf/c
 
 sed --in-place=.backup 's/@mysql.repo.db@/'"$MYSQL_DB_NAME"'/' /usr/local/tomcat/conf/repository-$profile.xml
 
+# Initialize jackrabbit cluster node id by setting with an external value or with hostname.
+repo_cluster_id=$REPO_CLUSTER_NODE_ID
+if [ -z "$repo_cluster_id" ]; then
+    repo_cluster_id="$(hostname -f)"
+fi
+sed --in-place 's/@cluster.node.id@/'"$repo_cluster_id"'/' /usr/local/tomcat/conf/repository-$profile.xml
 
 #copy db driver to /usr/local/tomcat/common/lib
 cp -R /brxm/db-drivers/$profile/. /usr/local/tomcat/common/lib/
