d52bc7059da6f0deaf6970153b1785e63197b0b5
Bert Leunis <bert.leunis@bloomreach.com>
2019-02-25 14:00:26 +0100

    CHANNELMGR-2290 ignore posted errorInfo from the frontend
    In case of posting information for one field, the errorInfo is stripped from the field info to reduce overhead.

--- channel-manager/frontend-ng/src/app/channel/sidePanels/rightSidePanel/contentEditor/fields/field.service.js
+++ channel-manager/frontend-ng/src/app/channel/sidePanels/rightSidePanel/contentEditor/fields/field.service.js
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017-2018 Hippo B.V. (http://www.onehippo.com)
+ * Copyright 2017-2019 Hippo B.V. (http://www.onehippo.com)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -83,9 +83,12 @@ class FieldService {
     }, this.AUTOSAVE_DELAY);
   }
 
-  saveField(fieldName, fieldValue, documentId = this.getDocumentId()) {
+  saveField(fieldName, fieldValues, documentId = this.getDocumentId()) {
     this._clearFieldTimer(documentId, fieldName);
-    this.ContentService.saveField(documentId, fieldName, fieldValue);
+    const saveFieldValues = angular.copy(fieldValues);
+    // do not send back errorInfo
+    saveFieldValues.forEach(value => delete value.errorInfo);
+    this.ContentService.saveField(documentId, fieldName, saveFieldValues);
     this._cleanupTimers(documentId);
   }
 
