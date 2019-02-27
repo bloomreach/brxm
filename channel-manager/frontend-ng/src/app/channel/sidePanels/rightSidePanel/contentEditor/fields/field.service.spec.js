ad6c7f1d7361cc35d6c862d3edd9dc94f58ef9f0
Mathijs den Burger <mathijs.denburger@bloomreach.com>
2019-02-27 13:43:16 +0100

    CHANNELMGR-2290 Enable all unit tests again

--- channel-manager/frontend-ng/src/app/channel/sidePanels/rightSidePanel/contentEditor/fields/field.service.spec.js
+++ channel-manager/frontend-ng/src/app/channel/sidePanels/rightSidePanel/contentEditor/fields/field.service.spec.js
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-fdescribe('field service', () => {
+describe('field service', () => {
   let $timeout;
   let FieldService;
   let ContentService;
