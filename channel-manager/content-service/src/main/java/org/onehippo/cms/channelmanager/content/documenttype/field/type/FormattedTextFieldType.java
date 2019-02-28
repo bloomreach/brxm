0d482ece3c767b8fb9520bb008bd801241ace235
Bert Leunis <bert.leunis@bloomreach.com>
2019-02-28 16:21:24 +0100

    CHANNELMGR-2300 set correct validation type for FormattedTextField

--- channel-manager/content-service/src/main/java/org/onehippo/cms/channelmanager/content/documenttype/field/type/FormattedTextFieldType.java
+++ channel-manager/content-service/src/main/java/org/onehippo/cms/channelmanager/content/documenttype/field/type/FormattedTextFieldType.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017-2018 Hippo B.V. (http://www.onehippo.com)
+ * Copyright 2017-2019 Hippo B.V. (http://www.onehippo.com)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -77,11 +77,6 @@ public class FormattedTextFieldType extends StringFieldType {
         processorFactory = HtmlProcessorFactory.of(processorId);
 
         return fieldsInfo;
-    }
-
-    @Override
-    protected String getValidationType() {
-        return "Html";
     }
 
     public ObjectNode getConfig() {
