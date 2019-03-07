02e9c796ce021f78aa90fc68bd2f82459b20a4de
Bert Leunis <bert.leunis@bloomreach.com>
2019-03-07 15:42:05 +0100

    CHANNELMGR-2046 show custom error messages from channel event handlers
    The errors are shown in a sticky toast with dismiss button.

--- channel-manager/frontend-ng/src/app/channel/menu/channel/channelMenu.service.js
+++ channel-manager/frontend-ng/src/app/channel/menu/channel/channelMenu.service.js
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017-2018 Hippo B.V. (http://www.onehippo.com)
+ * Copyright 2017-2019 Hippo B.V. (http://www.onehippo.com)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -197,7 +197,7 @@ class ChannelMenuService extends MenuService {
     if (error.message) {
       this.$log.error(error.message);
     }
-    this.FeedbackService.showError(errorKey, error.data);
+    this.FeedbackService.showErrorResponse(error, errorKey);
   }
 
   _reject() {
