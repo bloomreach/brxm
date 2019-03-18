67da77f83365333d7b8f6ae923c32e50953a10fd
Bert Leunis <bert.leunis@bloomreach.com>
2019-03-18 14:03:11 +0100

    CMS-11781 bare bones setup to test communication
    Why is the innerHtml of the uiProperties element not replaced? (or shown). To be continued.

--- cms/editor/frontend/src/main/java/org/hippoecm/frontend/editor/plugins/OpenUiStringPlugin.java
+++ cms/editor/frontend/src/main/java/org/hippoecm/frontend/editor/plugins/OpenUiStringPlugin.java
@@ -18,10 +18,16 @@ package org.hippoecm.frontend.editor.plugins;
 import java.util.Optional;
 
 import org.apache.commons.lang.StringUtils;
+//import org.apache.wicket.ajax.json.JSONObject;
 import org.apache.wicket.behavior.AttributeAppender;
+import org.apache.wicket.markup.head.IHeaderResponse;
+import org.apache.wicket.markup.head.JavaScriptReferenceHeaderItem;
+import org.apache.wicket.markup.head.OnDomReadyHeaderItem;
 import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.model.Model;
+import org.apache.wicket.request.resource.JavaScriptResourceReference;
+import org.apache.wicket.request.resource.ResourceReference;
 import org.hippoecm.frontend.plugin.IPluginContext;
 import org.hippoecm.frontend.plugin.config.IPluginConfig;
 import org.hippoecm.frontend.service.render.RenderPlugin;
@@ -31,10 +37,20 @@ import org.onehippo.cms7.openui.extensions.UiExtension;
 import org.onehippo.cms7.openui.extensions.UiExtensionLoader;
 import org.onehippo.cms7.openui.extensions.UiExtensionPoint;
 
+import net.sf.json.JSONObject;
+
 public class OpenUiStringPlugin extends RenderPlugin<String> {
 
     private static final String UI_EXTENSION = "uiExtension";
     private static final String ERROR_MESSAGE = "Cannot load extension '%s'.";
+    private String extensionUrl = null;
+    private WebMarkupContainer iframe;
+    
+    // penpal.js is fetched by npm
+    private static final ResourceReference PENPAL_JS = new JavaScriptResourceReference(OpenUiStringPlugin.class, 
+            "penpal.js");
+    private static final String JS_TEMPLATE = "Penpal.connectToChild(%s);";
+
 
     public OpenUiStringPlugin(final IPluginContext context, final IPluginConfig config) {
         super(context, config);
@@ -42,8 +58,12 @@ public class OpenUiStringPlugin extends RenderPlugin<String> {
         final String extensionName = config.getString(UI_EXTENSION);
         final Optional<UiExtension> uiExtension = loadUiExtension(extensionName);
 
-        final WebMarkupContainer iframe = new WebMarkupContainer("iframe");
-        uiExtension.ifPresent(extension -> iframe.add(new AttributeAppender("src", Model.of(extension.getUrl()))));
+        iframe = new WebMarkupContainer("iframe");
+        uiExtension.ifPresent(extension -> {
+            iframe.add(new AttributeAppender("src", Model.of(getIframeUrl(extension.getUrl()))));
+            extensionUrl = extension.getUrl();
+        });
+        iframe.setOutputMarkupId(true);
         queue(iframe);
         iframe.setVisible(uiExtension.isPresent());
 
@@ -60,4 +80,31 @@ public class OpenUiStringPlugin extends RenderPlugin<String> {
         final UiExtensionLoader loader = new JcrUiExtensionLoader(UserSession.get().getJcrSession());
         return loader.loadUiExtension(uiExtensionName, UiExtensionPoint.DOCUMENT_FIELD);
     }
+    
+    private String getIframeUrl(final String extensionUrl) {
+        return extensionUrl + "?br.parentOrigin=http%3A%2F%2Flocalhost%3A8080" + "&br.antiCache=bla";
+    }
+
+    @Override
+    public void renderHead(final IHeaderResponse response) {
+        response.render(JavaScriptReferenceHeaderItem.forReference(PENPAL_JS));
+
+        JSONObject options = new JSONObject();
+        options.put("url", getIframeUrl(extensionUrl));
+        options.put("appendTo", "document.getElementById('" + iframe.getMarkupId() + "')");
+        options.put("methods", "{}");
+
+        String options2 = "{" +
+                "url: '" + getIframeUrl(extensionUrl) + "'," +
+                "appendTo: document.getElementById('" + iframe.getMarkupId() + "')," +
+                "methods: {" +
+                " getProperties() { return { baseUrl : 'testUrl', locale: 'testLocale' } }" +
+                "}" +
+                "}";
+        
+        
+        final String script = String.format(JS_TEMPLATE, options2);
+        response.render(OnDomReadyHeaderItem.forScript(script));
+    }
+
 }
