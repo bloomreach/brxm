e2588c9704c63566fc6347f282419895cff7a757
Mathijs den Burger <mathijs.denburger@bloomreach.com>
2019-03-20 16:46:10 +0100

    CMS-11781 Implement all UI properties
    Use a separate header item to load Penpal.

--- cms/editor/frontend/src/main/java/org/hippoecm/frontend/editor/plugins/OpenUiStringPlugin.java
+++ cms/editor/frontend/src/main/java/org/hippoecm/frontend/editor/plugins/OpenUiStringPlugin.java
@@ -19,42 +19,46 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 
+import javax.jcr.RepositoryException;
+import javax.jcr.Session;
+
 import org.apache.commons.lang.StringUtils;
 import org.apache.wicket.markup.head.IHeaderResponse;
-import org.apache.wicket.markup.head.JavaScriptReferenceHeaderItem;
 import org.apache.wicket.markup.head.OnDomReadyHeaderItem;
 import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.model.Model;
-import org.apache.wicket.request.resource.JavaScriptResourceReference;
-import org.apache.wicket.request.resource.ResourceReference;
 import org.apache.wicket.util.template.PackageTextTemplate;
+import org.hippoecm.frontend.model.SystemInfoDataProvider;
 import org.hippoecm.frontend.plugin.IPluginContext;
 import org.hippoecm.frontend.plugin.config.IPluginConfig;
 import org.hippoecm.frontend.service.render.RenderPlugin;
 import org.hippoecm.frontend.session.UserSession;
+import org.hippoecm.repository.api.HippoWorkspace;
+import org.hippoecm.repository.util.UserUtils;
 import org.onehippo.cms7.openui.extensions.JcrUiExtensionLoader;
 import org.onehippo.cms7.openui.extensions.UiExtension;
 import org.onehippo.cms7.openui.extensions.UiExtensionLoader;
 import org.onehippo.cms7.openui.extensions.UiExtensionPoint;
+import org.onehippo.repository.security.SecurityService;
+import org.onehippo.repository.security.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class OpenUiStringPlugin extends RenderPlugin<String> {
 
-    private static final String UI_EXTENSION = "uiExtension";
+    private static final String CONFIG_PROPERTY_UI_EXTENSION = "uiExtension";
     private static final String ERROR_MESSAGE = "Cannot load extension '%s'.";
-    private String extensionUrl = null;
+    private static final String JS_TEMPLATE = "OpenUiStringPlugin.js";
+    private static final Logger log = LoggerFactory.getLogger(OpenUiStringPlugin.class);
+
+    private UiExtension extension;
     private String iframeParentId;
-    
-    // penpal.js is fetched by npm
-    private static final ResourceReference PENPAL_JS = new JavaScriptResourceReference(OpenUiStringPlugin.class, 
-            "penpal.js");
-    private final PackageTextTemplate PENPAL_CONNECT_TEMPLATE = new PackageTextTemplate(OpenUiStringPlugin.class,
-            "penpalConnect.js");
 
     public OpenUiStringPlugin(final IPluginContext context, final IPluginConfig config) {
         super(context, config);
-        
-        final String extensionName = config.getString(UI_EXTENSION);
+
+        final String extensionName = config.getString(CONFIG_PROPERTY_UI_EXTENSION);
         final Optional<UiExtension> uiExtension = loadUiExtension(extensionName);
 
         final WebMarkupContainer iframeParent = new WebMarkupContainer("iframeParent");
@@ -62,7 +66,7 @@ public class OpenUiStringPlugin extends RenderPlugin<String> {
         queue(iframeParent);
         iframeParentId = iframeParent.getMarkupId();
 
-        uiExtension.ifPresent(extension -> extensionUrl = extension.getUrl());
+        extension = uiExtension.orElse(null);
 
         final Label errorMessage = new Label("errorMessage", Model.of(String.format(ERROR_MESSAGE, extensionName)));
         errorMessage.setVisible(!uiExtension.isPresent());
@@ -76,24 +80,54 @@ public class OpenUiStringPlugin extends RenderPlugin<String> {
         final UiExtensionLoader loader = new JcrUiExtensionLoader(UserSession.get().getJcrSession());
         return loader.loadUiExtension(uiExtensionName, UiExtensionPoint.DOCUMENT_FIELD);
     }
-    
+
     @Override
     public void renderHead(final IHeaderResponse response) {
-        if (extensionUrl == null) {
+        if (extension == null) {
             return;
         }
-        
-        response.render(JavaScriptReferenceHeaderItem.forReference(PENPAL_JS));
 
-        final Map<String, String> variables = new HashMap<>();
-        variables.put("extensionUrl", extensionUrl);
-        variables.put("iframeParentId", iframeParentId);
-        variables.put("userLocale", UserSession.get().getLocale().getLanguage());
-        variables.put("userTimezone", UserSession.get().getTimeZone().getID());
-        
-        final String script = PENPAL_CONNECT_TEMPLATE.asString(variables);
-                
-        response.render(OnDomReadyHeaderItem.forScript(script));
+        response.render(new PenpalHeaderItem());
+        response.render(OnDomReadyHeaderItem.forScript(createJavaScript()));
     }
 
+    private String createJavaScript() {
+        final Map<String, String> variables = new HashMap<>();
+        variables.put("extensionConfig", StringUtils.defaultString(extension.getConfig()));
+        variables.put("extensionUrl", StringUtils.defaultString(extension.getUrl()));
+        variables.put("iframeParentId", iframeParentId);
+
+        final UserSession userSession = UserSession.get();
+        addCmsVariables(variables, userSession);
+        addUserVariables(variables, userSession);
+
+        final PackageTextTemplate javaScript = new PackageTextTemplate(OpenUiStringPlugin.class, JS_TEMPLATE);
+        return javaScript.asString(variables);
+    }
+
+    private static void addCmsVariables(final Map<String, String> variables, final UserSession userSession) {
+        variables.put("cmsLocale", userSession.getLocale().getLanguage());
+        variables.put("cmsTimeZone", StringUtils.defaultString(userSession.getTimeZone().getID()));
+        variables.put("cmsVersion", new SystemInfoDataProvider().getReleaseVersion());
+    }
+
+    private static void addUserVariables(final Map<String, String> variables, final UserSession userSession) {
+        final Session jcrSession = userSession.getJcrSession();
+        final String userId = jcrSession.getUserID();
+        variables.put("userId", userId);
+
+        final HippoWorkspace workspace = (HippoWorkspace) jcrSession.getWorkspace();
+        try {
+            final SecurityService securityService = workspace.getSecurityService();
+            final User user = securityService.getUser(userId);
+
+            variables.put("userFirstName", StringUtils.defaultString(user.getFirstName()));
+            variables.put("userLastName", StringUtils.defaultString(user.getLastName()));
+        } catch (RepositoryException e) {
+            log.warn("Unable to retrieve first and last name of user '{}'", userId, e);
+        }
+
+        UserUtils.getUserName(userId, jcrSession)
+                .ifPresent(displayName -> variables.put("userDisplayName", displayName));
+    }
 }
