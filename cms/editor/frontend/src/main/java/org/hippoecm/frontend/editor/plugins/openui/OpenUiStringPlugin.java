6b41f97c655b25988403c5bc31317ebc1e2259a7
Mathijs den Burger <mathijs.denburger@bloomreach.com>
2019-04-09 12:32:49 +0200

    CMS-11787 Move Open UI related classes to separate 'openui' sub-package

--- cms/editor/frontend/src/main/java/org/hippoecm/frontend/editor/plugins/openui/OpenUiStringPlugin.java
+++ cms/editor/frontend/src/main/java/org/hippoecm/frontend/editor/plugins/openui/OpenUiStringPlugin.java
@@ -13,7 +13,7 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-package org.hippoecm.frontend.editor.plugins;
+package org.hippoecm.frontend.editor.plugins.openui;
 
 import java.util.Optional;
 
--- cms/editor/frontend/src/main/java/org/hippoecm/frontend/editor/plugins/openui/PenpalHeaderItem.java
+++ cms/editor/frontend/src/main/java/org/hippoecm/frontend/editor/plugins/openui/PenpalHeaderItem.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package org.hippoecm.frontend.editor.plugins;
+package org.hippoecm.frontend.editor.plugins.openui;
 
 import java.util.Collections;
 
