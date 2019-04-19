3d1b22274805e61e654e49e6f0d3604e5457c401
Bert Leunis <bert.leunis@bloomreach.com>
2019-04-19 11:09:25 +0200

    CMS-11896 restyle the clearable dialog link
    In a multiple orderable docbase field the move and delete controls were hidden behind the link element. The clearable link is also used in the FacetSelectTemplate plugin. This plugin is not used anymore and is now deprecated.
    
    (cherry picked from commit 96bcae0cafc7fde956a5fc230b0a73c313d50de0)
    
    # Conflicts:
    #	api/src/main/java/org/hippoecm/frontend/dialog/ClearableDialogLink.java

--- cms/api/src/main/java/org/hippoecm/frontend/dialog/ClearableDialogLink.java
+++ cms/api/src/main/java/org/hippoecm/frontend/dialog/ClearableDialogLink.java
@@ -1,12 +1,12 @@
 /*
- *  Copyright 2008-2013 Hippo B.V. (http://www.onehippo.com)
- * 
+ *  Copyright 2008-2019 Hippo B.V. (http://www.onehippo.com)
+ *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
  *  You may obtain a copy of the License at
- * 
+ *
  *       http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  *  Unless required by applicable law or agreed to in writing, software
  *  distributed under the License is distributed on an "AS IS" BASIS,
  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,52 +17,37 @@ package org.hippoecm.frontend.dialog;
 
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.ajax.markup.html.AjaxLink;
-import org.apache.wicket.behavior.AttributeAppender;
-import org.apache.wicket.markup.ComponentTag;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.IModel;
-import org.apache.wicket.model.Model;
 
 abstract public class ClearableDialogLink extends Panel {
-    private static final long serialVersionUID = 1L;
-    
-    private AjaxLink dialogLink;
-    private AjaxLink clearLink;
 
-    public ClearableDialogLink(String id, IModel linktext, final IDialogFactory dialogFactory,
-            final IDialogService dialogService) {
+    private final AjaxLink dialogLink;
+    private final AjaxLink clearLink;
+
+    public ClearableDialogLink(final String id, final IModel linktext, final IDialogFactory dialogFactory,
+                               final IDialogService dialogService) {
         this(id, linktext, new DialogAction(dialogFactory, dialogService));
     }
 
-    public ClearableDialogLink(String id, IModel linktext, final DialogAction action) {
+    public ClearableDialogLink(final String id, final IModel linktext, final DialogAction action) {
         super(id, linktext);
 
         dialogLink = new AjaxLink("dialog-link") {
-            private static final long serialVersionUID = 1L;
 
             @Override
-            public void onClick(AjaxRequestTarget target) {
+            public void onClick(final AjaxRequestTarget target) {
                 action.execute();
-            }
-            
-            @Override
-            protected void onComponentTag(ComponentTag tag) {
-                super.onComponentTag(tag);
-                
-                if(!isClearVisible()) {
-                    tag.addBehavior(new AttributeAppender("class", true, new Model("linefill"), " "));
-                }
             }
         };
         add(dialogLink);
         dialogLink.add(new Label("dialog-link-text", linktext));
 
         clearLink = new AjaxLink("clear-link") {
-            private static final long serialVersionUID = 1L;
 
             @Override
-            public void onClick(AjaxRequestTarget target) {
+            public void onClick(final AjaxRequestTarget target) {
                 onClear();
             }
 
@@ -70,7 +55,7 @@ abstract public class ClearableDialogLink extends Panel {
             public boolean isEnabled() {
                 return canClear();
             }
-            
+
             @Override
             public boolean isVisible() {
                 return isClearVisible();
