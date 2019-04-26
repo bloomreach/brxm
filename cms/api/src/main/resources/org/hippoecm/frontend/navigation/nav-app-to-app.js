637fbe68a08250075b984c9f36269497e0d4a714
Michiel Eggermont <michiel.eggermont@bloomreach.com>
2019-04-26 15:59:27 +0200

    CMS-11840 Start implementing Child API methods
    For the 1st sprint at least
    - the navigate method must be implemented. This one is implemented
    - the CSM must connect to the parent. The client side implementation
      is done, but because the navigation communication npm package is
      not ready yet the connect function is stubbed.

--- cms/api/src/main/resources/org/hippoecm/frontend/navigation/nav-app-to-app.js
+++ cms/api/src/main/resources/org/hippoecm/frontend/navigation/nav-app-to-app.js
@@ -14,7 +14,55 @@
  * limitations under the License.
  *
  */
+
+/**
+ * This file contains functions for establishing a connection between the
+ * CMS (the child) and the Navigation Application (the parent).
+ * It provides an implementation of the Child API.
+ * It will register the CMS as a child of the Navigation App and, upon
+ * success stores the Parent API methods in a global object so that
+ * these methods will be available in the CMS iframe.
+ */
 (function () {
-  "use strict";
-  // TODO (meggermont): implement
+  'use strict';
+
+  function connectToParent (cfg) {
+    // TODO (meggermont): replace this stub with implementation from navigation client npm package
+    console.log('Stubbed connection to parent:', cfg);
+    return new Promise(function (resolve, ignore) {
+      resolve({updateLocation: 'NOT IMPLEMENTED'});
+    });
+  }
+
+  // Implementation of the Child API
+  Hippo.NavAppToApp = {
+
+    navigate: function (location) {
+      const perspectiveClassName = location.path;
+      const perspectives = document.getElementsByClassName(perspectiveClassName);
+      if (perspectives && perspectives[0]) {
+        perspectives[0].click();
+        return Promise.resolve();
+      }
+      return Promise.reject(new Error(`${perspectiveClassName} not found`));
+    }
+
+  };
+
+  // Receiver object for the implementation of the Parent API
+  Hippo.AppToNavApp = {};
+
+  // Config object sent to parent when connecting.
+  const parentConnectionConfig = {
+    parentOrigin: '${parentOrigin}',
+    methods: Hippo.NavAppToApp
+  };
+
+  // Establish the connection
+  connectToParent(parentConnectionConfig)
+    .then(parentApi =>
+      Object.assign(Hippo.AppToNavApp, parentApi))
+    .catch(error =>
+      console.error(error));
+
 }());
