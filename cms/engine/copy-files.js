8eb03a2a28d74556b814b219e543a49b985303d7
Michiel Eggermont <michiel.eggermont@bloomreach.com>
2019-05-03 10:28:07 +0200

    CMS-11839 Load the navigation communication library
    We do not deploy our javascript libraries to a CDN yet. So we need to
    include them in the engine jar for now. This means we need to pull them
    in via npm and then copy them to the target directory. Then we can use
    a HeaderItem (NavAppHeaderItem) to generate a script source header in
    the NavAppPage.html for it.
    
    The NavAppHeaderItem will later also generate javascript for the
    NavAppSettings object and generate a script header for the navigation
    application angular app.

---
+++ cms/engine/copy-files.js
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2019 Hippo B.V. (http://www.onehippo.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+'use strict';
+
+const fs = require('file-system');
+
+const sourceDir = 'node_modules/@bloomreach';
+const targetDir = 'target/classes/org/hippoecm/frontend/js';
+
+fs.recurse(sourceDir, ['**/bundles/*.js'], (filepath, relative, filename) => {
+    if (filename) {
+        const destpath = `${targetDir}/${filename}`;
+        fs.copyFile(filepath, destpath,
+          {done: (err) => {
+              if (err) {
+                  console.error(err);
+              } else {
+                  console.log(`[npm] copied ${filename} to ${destpath}`);
+              }
+          }});
+    }
+});
---
+++ cms/engine/src/main/java/org/hippoecm/frontend/NavAppHeaderItem.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2019 Hippo B.V. (http://www.onehippo.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package org.hippoecm.frontend;
+
+import java.util.Collections;
+
+import org.apache.wicket.markup.head.HeaderItem;
+import org.apache.wicket.markup.head.JavaScriptReferenceHeaderItem;
+import org.apache.wicket.request.Response;
+import org.apache.wicket.request.resource.JavaScriptResourceReference;
+import org.apache.wicket.request.resource.ResourceReference;
+
+/**
+ * Contains the javascript needed to start the Navigation Application
+ */
+public class NavAppHeaderItem extends HippoHeaderItem {
+
+    private static final HippoHeaderItem INSTANCE = new NavAppHeaderItem();
+
+    public static HeaderItem get() {
+        return INSTANCE;
+    }
+
+    private NavAppHeaderItem() {
+    }
+
+    @Override
+    public Iterable<?> getRenderTokens() {
+        return Collections.singleton("hippo-nav-app-header-item");
+    }
+
+    @Override
+    public void render(final Response response) {
+        // TODO (meggermont): this is a temporary solution
+        // For now we depend on js files pulled in via npm
+        // Later we will use CDN provided resources instead
+        getJsItem("bloomreach-navigation-communication.umd").render(response);
+    }
+
+    private JavaScriptReferenceHeaderItem getJsItem(String jsResourceName) {
+        final String resourceName = getJsName(jsResourceName);
+        final ResourceReference resourceReference = new JavaScriptResourceReference(NavAppHeaderItem.class, resourceName);
+        return JavaScriptReferenceHeaderItem.forReference(resourceReference);
+    }
+
+    private String getJsName(String jsResourceName) {
+        return String.format("js/%s%s.js", jsResourceName, getProdSuffix());
+    }
+
+    private String getProdSuffix() {
+        return isDevelopmentMode() ? "" : ".min";
+    }
+}
