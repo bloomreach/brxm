46dcbe5ee975c8c14516cfec8b4945faef44e70f
Bert Leunis <bert.leunis@bloomreach.com>
2019-05-06 10:34:26 +0200

    CMS-11884 keep the order of validators as configured
    Not order alphabetically anymore, but keep the order of the configuration property.

--- cms/editor/frontend/src/main/java/org/hippoecm/editor/type/JcrFieldDescriptor.java
+++ cms/editor/frontend/src/main/java/org/hippoecm/editor/type/JcrFieldDescriptor.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2008-2013 Hippo B.V. (http://www.onehippo.com)
+ *  Copyright 2008-2019 Hippo B.V. (http://www.onehippo.com)
  * 
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -17,8 +17,8 @@ package org.hippoecm.editor.type;
 
 import java.util.Collections;
 import java.util.Iterator;
+import java.util.LinkedHashSet;
 import java.util.Set;
-import java.util.TreeSet;
 
 import javax.jcr.Node;
 import javax.jcr.RepositoryException;
@@ -216,7 +216,7 @@ public class JcrFieldDescriptor extends JcrObject implements IFieldDescriptor {
     }
 
     private Set<String> getStringSet(String path) {
-        Set<String> result = new TreeSet<String>();
+        Set<String> result = new LinkedHashSet<>();
         try {
             if (getNode().hasProperty(path)) {
                 Value[] values = getNode().getProperty(path).getValues();
