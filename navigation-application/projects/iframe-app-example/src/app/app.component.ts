731322cb09e1b799b3323a7efe0cc64709f0053e
Joeri de Gooijer <joeri.degooijer@bloomreach.com>
2019-05-08 15:15:04 +0200

    NAVAPP-21 Generate iframe-app-example

---
+++ navigation-application/projects/iframe-app-example/src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <!--The content below is only a placeholder and can be replaced.-->
+    <div style="text-align:center">
+      <h1>
+        Welcome to {{ title }}!
+      </h1>
+    </div>
+    <h2>Here are some links to help you start: </h2>
+    <ul>
+      <li>
+        <h2><a target="_blank" rel="noopener" href="https://angular.io/tutorial">Tour of Heroes</a></h2>
+      </li>
+      <li>
+        <h2><a target="_blank" rel="noopener" href="https://angular.io/cli">CLI Documentation</a></h2>
+      </li>
+      <li>
+        <h2><a target="_blank" rel="noopener" href="https://blog.angular.io/">Angular blog</a></h2>
+      </li>
+    </ul>
+
+  `,
+  styles: [],
+})
+export class AppComponent {
+  title = 'iframe-app-example';
+}
