d5dfc9d4de103ffaf39776f24dfd6affe2935647
Joeri de Gooijer <joeri.degooijer@bloomreach.com>
2019-05-14 14:25:31 +0200

    NAVAPP-21 Report code coverage for all ts files
    By default angular cli only reports unit test coverage of
    any code imported into spec files

--- navigation-application/projects/navapp-communication/src/test.ts
+++ navigation-application/projects/navapp-communication/src/test.ts
@@ -1,6 +1,6 @@
 // This file is required by karma.conf.js and loads recursively all the .spec and framework files
 
 // We find all the tests.
-const context = (require as any).context('./', true, /\.spec\.ts$/);
+const context = (require as any).context('./lib', true, /\.ts$/);
 // And load the modules.
 context.keys().map(context);
