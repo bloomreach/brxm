dc29f5f43559e3e623d462a75c87943f5e6b708d
Joeri de Gooijer <joeri.degooijer@bloomreach.com>
2019-05-24 22:46:23 +0200

    NAVAPP-16 Refactor styling to use css grid and flexbox for layout
    Using flexbox and grid for layout gives more flexibility and readability
    to layouting and elements can stay within the normal layout flow. This
    refactor also allows for more flexible widths will adjust to the naming
    of menu items.

--- navigation-application/projects/navapp/src/app/main-menu/components/expandable-menu-item/expandable-menu-item.component.ts
+++ navigation-application/projects/navapp/src/app/main-menu/components/expandable-menu-item/expandable-menu-item.component.ts
@@ -14,8 +14,14 @@
  * limitations under the License.
  */
 
-import { animate, style, transition, trigger } from '@angular/animations';
-import { Component, HostBinding, Input, OnChanges, SimpleChanges } from '@angular/core';
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
+import { Component, HostBinding, Input, OnChanges } from '@angular/core';
 
 import { QaHelperService } from '../../../services';
 import { MenuItem, MenuItemContainer, MenuItemLink } from '../../models';
@@ -27,13 +33,19 @@ import { MenuStateService } from '../../services';
   styleUrls: ['expandable-menu-item.component.scss'],
   animations: [
     trigger('slideInOut', [
-      transition(':enter', [
-        style({ height: '0' }),
-        animate('300ms ease', style({ height: '*' })),
-      ]),
-      transition(':leave', [
-        animate('300ms ease', style({ height: '0' })),
-      ]),
+      state(
+        'false',
+        style({
+          height: '0',
+        }),
+      ),
+      state(
+        'true',
+        style({
+          height: '*',
+        }),
+      ),
+      transition('false <=> true', animate('300ms ease')),
     ]),
   ],
 })
@@ -56,7 +68,7 @@ export class ExpandableMenuItemComponent implements OnChanges {
     return this.isChildMenuOpened;
   }
 
-  ngOnChanges(changes: SimpleChanges): void {
+  ngOnChanges(): void {
     if (this.active) {
       this.isChildMenuOpened = true;
     }
