a01a25f2211af2aaa226b319dd2374aef763d0af
Erdem Karakus <erdem.karakus@bloomreach.com>
2019-05-31 14:27:26 +0200

    CMS-11954 Set enhanceable feature to false on internal beans

--- relateddocs/hst/src/main/java/org/onehippo/forge/beans/RelatedDocsBean.java
+++ relateddocs/hst/src/main/java/org/onehippo/forge/beans/RelatedDocsBean.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2009-2015 Hippo B.V. (http://www.onehippo.com)
+ * Copyright 2009-2019 Hippo B.V. (http://www.onehippo.com)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -22,8 +22,10 @@ import org.hippoecm.hst.content.beans.Node;
 import org.hippoecm.hst.content.beans.standard.HippoBean;
 import org.hippoecm.hst.content.beans.standard.HippoFacetSelect;
 import org.hippoecm.hst.content.beans.standard.HippoItem;
+import org.onehippo.cms7.essentials.dashboard.annotations.HippoEssentialsGenerated;
 
 @Node(jcrType = "relateddocs:docs")
+@HippoEssentialsGenerated(allowModifications = false)
 public class RelatedDocsBean extends HippoItem {
 
     public List<HippoBean> getDocs() {
