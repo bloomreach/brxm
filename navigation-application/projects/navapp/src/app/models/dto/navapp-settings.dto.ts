1a7aba6af957bd366e60648903e9caebafde487b
Michiel Eggermont <michiel.eggermont@bloomreach.com>
2019-06-07 15:57:33 +0200

    NAVAPP-60 Extract interface and add optional email property

--- navigation-application/projects/navapp/src/app/models/dto/navapp-settings.dto.ts
+++ navigation-application/projects/navapp/src/app/models/dto/navapp-settings.dto.ts
@@ -1,11 +1,15 @@
 import { NavConfigResource } from './nav-config-resource.dto';
 
+export interface UserSettings {
+  userName: string;
+  email?: string;
+  language: string;
+  timeZone: string;
+}
+
 export interface NavAppSettings {
-  userSettings: {
-    userName: string;
-    language: string;
-    timeZone: string;
-  };
+
+  userSettings: UserSettings;
 
   appSettings: {
     navConfigResources: NavConfigResource[];
