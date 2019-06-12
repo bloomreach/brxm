48508e400d9c546c68bfe3efacd2d6686377e92b
Michiel Eggermont <michiel.eggermont@bloomreach.com>
2019-06-12 08:32:23 +0200

    NAVAPP-63 Move assets back to conventional location

--- navigation-application/projects/navapp/src/app/shared/material/material.module.ts
+++ navigation-application/projects/navapp/src/app/shared/material/material.module.ts
@@ -78,7 +78,7 @@ export class MaterialModule {
 
     menuItemIconsList.concat(serviceIconsList).forEach(icon => iconRegistry.addSvgIcon(
       icon,
-      donSanitizer.bypassSecurityTrustResourceUrl(`navapp/assets/menu-icons/${icon}.svg`)),
+      donSanitizer.bypassSecurityTrustResourceUrl(`assets/menu-icons/${icon}.svg`)),
     );
   }
 }
