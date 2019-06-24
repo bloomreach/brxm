b357bb171df4a6564a89c958a42e3063a021f749
Arthur Bogaart <arthur.bogaart@bloomreach.com>
2019-06-24 14:59:20 +0200

    TRIVIAL Suppress sonarqube warning for shadowing a parent field.

--- cms/api/src/main/java/org/hippoecm/frontend/model/tree/ObservableTreeModel.java
+++ cms/api/src/main/java/org/hippoecm/frontend/model/tree/ObservableTreeModel.java
@@ -1,12 +1,12 @@
 /*
- *  Copyright 2008-2015 Hippo B.V. (http://www.onehippo.com)
- * 
+ *  Copyright 2008-2019 Hippo B.V. (http://www.onehippo.com)
+ *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
  *  You may obtain a copy of the License at
- * 
+ *
  *       http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  *  Unless required by applicable law or agreed to in writing, software
  *  distributed under the License is distributed on an "AS IS" BASIS,
  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -392,9 +392,12 @@ public class ObservableTreeModel extends DefaultTreeModel implements IJcrTreeMod
         }
     }
 
-    final static Logger log = LoggerFactory.getLogger(ObservableTreeModel.class);
+    static final Logger log = LoggerFactory.getLogger(ObservableTreeModel.class);
 
     private IObservationContext<ObservableTreeModel> observationContext;
+    // Field {@code root} can not be renamed or scoped private as it is protected and in the API package. After
+    // examining the code we decided to suppress the Sonarqube warning.
+    @SuppressWarnings("squid:S2387")
     protected final IJcrTreeNode root;
     private final String rootPath;
     private final ExpandedNode expandedRoot;
