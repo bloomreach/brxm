b6b2bf5c2021261b68769296b3d91333fda726e6
Arthur Bogaart <arthur.bogaart@bloomreach.com>
2019-06-25 15:47:39 +0200

    TRIVIAL Rename field "limit" to "queryLimit"
    Added feature to log a successful query when the log-level is set to
    debug.

--- cms/api/src/main/java/org/hippoecm/frontend/plugins/standards/search/QueryResultModel.java
+++ cms/api/src/main/java/org/hippoecm/frontend/plugins/standards/search/QueryResultModel.java
@@ -1,12 +1,12 @@
 /*
- *  Copyright 2010-2018 Hippo B.V. (http://www.onehippo.com)
- * 
+ *  Copyright 2010-2019 Hippo B.V. (http://www.onehippo.com)
+ *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
  *  You may obtain a copy of the License at
- * 
+ *
  *       http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  *  Unless required by applicable law or agreed to in writing, software
  *  distributed under the License is distributed on an "AS IS" BASIS,
  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -23,39 +23,44 @@ import javax.jcr.query.QueryResult;
 import org.apache.wicket.model.LoadableDetachableModel;
 import org.hippoecm.frontend.session.UserSession;
 
-public class QueryResultModel extends LoadableDetachableModel<QueryResult> {
+import static org.hippoecm.frontend.plugins.standards.search.GeneralSearchBuilder.TEXT_QUERY_NAME;
 
-    private static final long serialVersionUID = 1L;
+public class QueryResultModel extends LoadableDetachableModel<QueryResult> {
 
     public static final int LIMIT = 100;
 
     private final String query;
-    private int limit;
+    private final int queryLimit;
 
-    public QueryResultModel(String query, int limit) {
+    public QueryResultModel(final String query, final int limit) {
         this.query = query;
-        this.limit = limit;
+        this.queryLimit = limit;
     }
 
     @Override
     protected QueryResult load() {
         QueryResult result = null;
-        javax.jcr.Session session = UserSession.get().getJcrSession();
         try {
-            QueryManager queryManager = UserSession.get().getQueryManager();
-            Query q = queryManager.createQuery(query, "xpath");
-            if (limit > 0) {
-                q.setLimit(limit);
+            final QueryManager queryManager = UserSession.get().getQueryManager();
+            final Query q = queryManager.createQuery(query, Query.XPATH);
+            if (queryLimit > 0) {
+                q.setLimit(queryLimit);
             } else {
                 q.setLimit(LIMIT);
             }
 
-            long start = System.currentTimeMillis();
+            final long start = System.currentTimeMillis();
             result = q.execute();
-            long end = System.currentTimeMillis();
-            TextSearchBuilder.log.info("Executing search query: " + GeneralSearchBuilder.TEXT_QUERY_NAME + " took " + (end - start) + "ms");
+            final long executionTime = System.currentTimeMillis() - start;
+
+            if (TextSearchBuilder.log.isDebugEnabled()) {
+                TextSearchBuilder.log.debug("Executing search query[{}]: {} took {}ms", TEXT_QUERY_NAME, query,
+                        executionTime);
+            } else {
+                TextSearchBuilder.log.info("Executing search query[{}]: took {}ms", TEXT_QUERY_NAME, executionTime);
+            }
         } catch (RepositoryException e) {
-            TextSearchBuilder.log.error("Error executing query[" + GeneralSearchBuilder.TEXT_QUERY_NAME + "]: " + query, e);
+            TextSearchBuilder.log.error("Error executing query[{}]: {}", TEXT_QUERY_NAME, query, e);
         }
         return result;
     }
