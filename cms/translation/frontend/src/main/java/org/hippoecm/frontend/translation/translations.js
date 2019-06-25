e9a31047da051d092dab197f18601c0d4edc5091
Arthur Bogaart <arthur.bogaart@bloomreach.com>
2019-06-25 16:10:04 +0200

    TRIVIAL Initialize global Hippo variable consistently

--- cms/translation/frontend/src/main/java/org/hippoecm/frontend/translation/translations.js
+++ cms/translation/frontend/src/main/java/org/hippoecm/frontend/translation/translations.js
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2018 Hippo B.V. (http://www.onehippo.com)
+ * Copyright 2010-2019 Hippo B.V. (http://www.onehippo.com)
  *
  * Licensed under the Apache License, Version 2.0 (the  "License");
  * you may not use this file except in compliance with the License.
@@ -14,9 +14,7 @@
  * limitations under the License.
  */
 
-if (Hippo === undefined) {
-  Hippo = {};
-}
+window.Hippo = window.Hippo || {};
 
 Hippo.Translation = {};
 
