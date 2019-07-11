d3e200ff5e1e4fc863ac96cdb7c6138709a747ec
Bert Leunis <bert.leunis@bloomreach.com>
2019-07-11 11:52:59 +0200

    CHANNELMGR-1394 add dynamic dropdown field type to the editor

---
+++ channel-manager/frontend-ng/src/app/channel/sidePanels/rightSidePanel/contentEditor/fields/dynamicDropdown/dynamicDropdown.controller.spec.js
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2019 Hippo B.V. (http://www.onehippo.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+describe('dynamicDropdown', () => {
+  let ContentService;
+  let $ctrl;
+  let $q;
+  let $scope;
+
+  beforeEach(() => {
+    angular.mock.module('hippo-cm.channel.rightSidePanel.contentEditor.fields');
+
+    inject(($componentController, _$q_, $rootScope) => {
+      $q = _$q_;
+      ContentService = jasmine.createSpyObj('ContentService', ['getValueList']);
+
+      $scope = $rootScope.$new();
+      $ctrl = $componentController('dynamicDropdown', {
+        $scope,
+        ContentService,
+      });
+    });
+  });
+
+  describe('load options for dynamic dropdown field', () => {
+    it('loads option values correctly', () => {
+      const valueList = [{ key: 's', label: 'Ship' }, { key: 'b', label: 'Boat' }];
+      ContentService.getValueList.and.returnValue($q.resolve(valueList));
+
+      $ctrl.$onInit();
+      $scope.$apply();
+
+      expect($ctrl.optionValues()).toEqual(['s', 'b']);
+    });
+
+    it('loads option display values correctly', () => {
+      const valueList = [{ key: 's', label: 'Ship' }, { key: 'b', label: 'Boat' }];
+      ContentService.getValueList.and.returnValue($q.resolve(valueList));
+
+      $ctrl.$onInit();
+      $scope.$apply();
+
+      expect($ctrl.optionDisplayValues(0)).toEqual('Ship');
+      expect($ctrl.optionDisplayValues(1)).toEqual('Boat');
+    });
+  });
+});
