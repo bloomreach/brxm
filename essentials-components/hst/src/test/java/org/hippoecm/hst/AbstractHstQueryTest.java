3aab8dd4d5d1cbb1ebdb945fce95b80c9a33ad0a
Bert Leunis <bert.leunis@bloomreach.com>
2019-07-11 15:44:21 +0200

    ESSCOM-72 tests are located in child class

--- essentials-components/hst/src/test/java/org/hippoecm/hst/AbstractHstQueryTest.java
+++ essentials-components/hst/src/test/java/org/hippoecm/hst/AbstractHstQueryTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 Hippo B.V. (http://www.onehippo.com)
+ * Copyright 2016-2019 Hippo B.V. (http://www.onehippo.com)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -31,6 +31,7 @@ import javax.jcr.Session;
 import java.util.HashMap;
 import java.util.Map;
 
+@SuppressWarnings("squid:S2187")
 public class AbstractHstQueryTest extends AbstractBeanTestCase {
 
   protected HstQueryManager queryManager;
