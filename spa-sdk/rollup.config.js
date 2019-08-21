55f914405c6324738516e565006b8dd1cadee5d1
Mathijs den Burger <mathijs.denburger@bloomreach.com>
2019-08-21 14:51:17 +0200

    CMS-12133 Add build system boilerplate
    Copied from the ui-extension setup and tailored for the spa-sdk library.
    Notable differences:
    - TypeScript uses "strict": true
    - Rollup does not generate an IIFE module since we don't need to load
      this library directly in the browser
    - tslint does not allow a leading underscore
    - the main code entry point is index.ts

---
+++ spa-sdk/rollup.config.js
@@ -0,0 +1,93 @@
+/*
+ * Copyright 2019 Hippo B.V. (http://www.onehippo.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import babel from 'rollup-plugin-babel';
+import { dts } from 'rollup-plugin-dts';
+import { terser } from 'rollup-plugin-terser';
+import typescript from 'rollup-plugin-typescript2';
+import pkg from './package.json';
+
+export default [
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        exports: 'named',
+        file: 'dist/spa-sdk.js',
+        format: 'umd',
+        name: 'spa',
+      },
+      {
+        file: 'dist/spa-sdk.mjs',
+        format: 'esm',
+      },
+    ],
+    external: [
+      ...Object.keys(pkg.dependencies || {}),
+      ...Object.keys(pkg.peerDependencies || {}),
+    ],
+    plugins: [
+      typescript({ cacheRoot: './node_modules/.cache/rpt2' }),
+      babel({ extensions: ['.ts'] }),
+      terser({
+        ecma: 5,
+        mangle: false,
+        compress: false,
+        output: {
+          beautify: true,
+          comments: false,
+        },
+        sourcemap: false,
+      })
+    ],
+  },
+
+  {
+    input: 'src/index.ts',
+    output: [{
+      file: 'dist/spa-sdk.es6.mjs',
+      format: 'esm',
+    }],
+    external: [
+      ...Object.keys(pkg.dependencies || {}),
+      ...Object.keys(pkg.peerDependencies || {}),
+    ],
+    plugins: [
+      typescript({ cacheRoot: './node_modules/.cache/rpt2' }),
+      terser({
+        ecma: 5,
+        mangle: false,
+        compress: false,
+        output: {
+          beautify: true,
+          comments: false,
+        },
+        sourcemap: false,
+      })
+    ],
+  },
+
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        file: 'dist/spa-sdk.d.ts',
+        format: 'es',
+      },
+    ],
+    plugins: [ dts() ],
+  }
+];
--- spa-sdk/src/docs/index.md
+++ spa-sdk/src/docs/index.md
@@ -1,8 +1,2 @@
-# Bloomreach SPA SDK
-
-JavaScript library to be used in
+This is the TypeScript documentation of the JavaScript library to be used in
 [SPA integrations with Bloomreach Experience Manager](https://documentation.bloomreach.com/library/concepts/spa-plus/introduction.html).
-
-## License
-
-Apache 2.0
