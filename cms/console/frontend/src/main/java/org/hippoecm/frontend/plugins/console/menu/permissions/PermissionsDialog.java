3c7c07babf6d2065c418e45ef78221b8d7beadd3
Ate Douma <ate.douma@bloomreach.com>
2019-08-25 17:31:11 +0200

    REPO-2232 Refactor and harden the check permissions and privileges logic
    A new (internal) PermissionManager singleton is introduced, together with a StandardPermissionNames constants class,
    which standardizes handling, mapping and caching of all the standard JCR permission actions and privileges, as well as custom privileges.
    
    The PermissionManager is used to retrofit all the current logic for parsing/mapping permission actions and/or privileges
    in Session.checkPermission and the HippoAccessManager, including its related methods for the JCR AccessControlManager and
    Jackrabbit AccessManager interfaces.
    And no longer delegating to the default Jackrabbit implementation, removing the need to (always) catch a AccessDeniedException before proceeding.
    
    The (Hippo) AccessManager created FacetAuthPrincipal class is enhanced to also provide the resolved privileges, with jcr:write and jcr:all replaced
    with their aggregate standard privileges, to allow a more efficient and hardened implementation of the actual privileges checks done
    in a new HippoAccessManager.hasPrivileges(Path path, Set<String> privilegeNames).
    
    All usages of inline coded names of standard JCR privileges and default custom privileges are replaced with named constants in th PermissionNames constants class.

--- cms/console/frontend/src/main/java/org/hippoecm/frontend/plugins/console/menu/permissions/PermissionsDialog.java
+++ cms/console/frontend/src/main/java/org/hippoecm/frontend/plugins/console/menu/permissions/PermissionsDialog.java
@@ -15,13 +15,14 @@
  */
 package org.hippoecm.frontend.plugins.console.menu.permissions;
 
-import java.security.AccessControlException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.Arrays;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
 
 import javax.jcr.Node;
 import javax.jcr.RepositoryException;
+import javax.jcr.security.Privilege;
 
 import org.apache.commons.lang.StringUtils;
 import org.apache.wicket.markup.html.basic.Label;
@@ -30,45 +31,13 @@ import org.apache.wicket.model.Model;
 import org.hippoecm.frontend.dialog.Dialog;
 import org.hippoecm.repository.api.HippoSession;
 import org.onehippo.repository.security.Group;
+import org.onehippo.repository.security.StandardPermissionNames;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class PermissionsDialog extends Dialog<Node> {
 
     private static final long serialVersionUID = 1L;
-
-    /**
-     * predefined action constants in checkPermission
-     */
-    public static final String READ_ACTION = "read";
-    public static final String REMOVE_ACTION = "remove";
-    public static final String ADD_NODE_ACTION = "add_node";
-    public static final String MODIFY_PROPERTY_ACTION = "modify_property";
-
-    public static final String[] JCR_ACTIONS = new String[]{READ_ACTION, REMOVE_ACTION, ADD_NODE_ACTION,
-            MODIFY_PROPERTY_ACTION};
-
-    /**
-     * Predefined jcr privileges
-     */
-    public static final String READ_PRIVILEGE = "jcr:read";
-    public static final String WRITE_PRIVILEGE = "jcr:write";
-    public static final String ALL_PRIVILEGE = "jcr:all";
-    public static final String MODIFY_PROPERTIES_PRIVILEGE = "jcr:modifyProperties";
-    public static final String ADD_CHILD_PRIVILEGE = "jcr:addChildNodes";
-    public static final String REMOVE_CHILD_PRIVILEGE = "jcr:removeChildNodes";
-    public static final String REMOVE_NODE = "jcr:removeNode";
-
-    /**
-     * Predefined hippo privileges
-     */
-    public static final String AUTHOR_PRIVILEGE = "hippo:author";
-    public static final String EDITOR_PRIVILEGE = "hippo:editor";
-    public static final String ADMIN_PRIVILEGE = "hippo:admin";
-
-    public static final String[] JCR_PRIVILEGES = new String[]{READ_PRIVILEGE, WRITE_PRIVILEGE, ALL_PRIVILEGE,
-            MODIFY_PROPERTIES_PRIVILEGE, ADD_CHILD_PRIVILEGE, REMOVE_CHILD_PRIVILEGE, REMOVE_NODE, AUTHOR_PRIVILEGE, EDITOR_PRIVILEGE,
-            ADMIN_PRIVILEGE};
 
     private static final Logger log = LoggerFactory.getLogger(PermissionsDialog.class);
 
@@ -91,16 +60,25 @@ public class PermissionsDialog extends Dialog<Node> {
         try {
             Node subject = nodeModel.getObject();
 
-            String[] actions = getAllowedActions(subject, JCR_ACTIONS);
-            String[] roles = getAllowedActions(subject, JCR_PRIVILEGES);
+            // getPrivileges() must be called before getSupportedPrivileges() to ensure they are all 'loaded'
+            Set<String> privileges = getPrivileges(subject);
+            if (privileges.contains(StandardPermissionNames.JCR_WRITE)) {
+                privileges.removeAll(StandardPermissionNames.JCR_WRITE_PRIVILEGES);
+            }
+            if (privileges.contains(StandardPermissionNames.JCR_ALL)) {
+                privileges.removeAll(StandardPermissionNames.JCR_ALL_PRIVILEGES);
+                privileges.remove(StandardPermissionNames.JCR_WRITE);
+            }
+            Set<String> supportedPrivileges = getSupportedPrivileges(subject);
+            Set<String> actions = getAllowedActions(subject, StandardPermissionNames.JCR_ACTIONS);
 
             final HippoSession userSession = (HippoSession) subject.getSession();
             usernameLabel.setDefaultModel(Model.of(userSession.getUserID()));
             membershipsLabel.setDefaultModel(Model.of(StringUtils.join(getMemberships(userSession), ", ")));
-            allActionsLabel.setDefaultModel(Model.of(StringUtils.join(JCR_ACTIONS, ", ")));
-            allPrivilegesLabel.setDefaultModel(Model.of(StringUtils.join(JCR_PRIVILEGES, ", ")));
+            allActionsLabel.setDefaultModel(Model.of(StringUtils.join(StandardPermissionNames.JCR_ACTIONS, ", ")));
+            allPrivilegesLabel.setDefaultModel(Model.of(StringUtils.join(supportedPrivileges, ", ")));
             actionsLabel.setDefaultModel(Model.of(StringUtils.join(actions, ", ")));
-            privilegesLabel.setDefaultModel(Model.of(StringUtils.join(roles, ", ")));
+            privilegesLabel.setDefaultModel(Model.of(StringUtils.join(privileges, ", ")));
 
         } catch (RepositoryException ex) {
             actionsLabel.setDefaultModel(Model.of(ex.getClass().getName() + ": " + ex.getMessage()));
@@ -109,28 +87,30 @@ public class PermissionsDialog extends Dialog<Node> {
         setFocusOnCancel();
     }
 
-    private boolean hasPermission(Node node, String actions) throws RepositoryException {
-        try {
-            node.getSession().checkPermission(node.getPath(), actions);
-            return true;
-        } catch (AccessControlException e) {
-            return false;
-        }
-    }
-
-    private String[] getAllowedActions(Node node, String[] actions) throws RepositoryException {
-        final List<String> list = new ArrayList<>();
+    private Set<String> getAllowedActions(Node node, Set<String> actions) throws RepositoryException {
+        final Set<String> allowedActions = new TreeSet<>();
         for (String action : actions) {
-            if (hasPermission(node, action)) {
-                list.add(action);
+            if (node.getSession().hasPermission(node.getPath(), action)) {
+                allowedActions.add(action);
             }
         }
-        Collections.sort(list);
-        return list.toArray(new String[0]);
+        return allowedActions;
     }
 
-    private List<String> getMemberships(final HippoSession userSession) throws RepositoryException {
-        final List<String> memberships = new ArrayList<>();
+    private Set<String> getPrivileges(Node node) throws RepositoryException {
+        return Arrays.stream(node.getSession().getAccessControlManager().getPrivileges(node.getPath()))
+                .map(Privilege::getName)
+                .collect(Collectors.toCollection(TreeSet::new));
+    }
+
+    private Set<String> getSupportedPrivileges(Node node) throws RepositoryException {
+        return Arrays.stream(node.getSession().getAccessControlManager().getSupportedPrivileges(node.getPath()))
+                .map(Privilege::getName)
+                .collect(Collectors.toCollection(TreeSet::new));
+    }
+
+    private Set<String> getMemberships(final HippoSession userSession) throws RepositoryException {
+        final Set<String> memberships = new TreeSet<>();
         for (Group group : userSession.getUser().getMemberships()) {
             memberships.add(group.getId());
         }
