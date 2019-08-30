a91ea0d2ebee9dd579e62bb4455adbff1b7dc485
Maxim Palenov <maxim.palenov@bloomreach.com>
2019-08-30 11:20:26 +0200

    NAVAPP-209 Add error page component

---
+++ navigation-application/projects/navapp/src/app/error-handling/components/error-page.component.spec.ts
@@ -0,0 +1,60 @@
+/*!
+ * Copyright 2019 BloomReach. All rights reserved. (https://www.bloomreach.com/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { DebugElement, NO_ERRORS_SCHEMA } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+
+import { DeepLinkingService } from '../../routing/deep-linking.service';
+
+import { ErrorPageComponent } from './error-page.component';
+
+describe('ErrorPageComponent', () => {
+  let component: ErrorPageComponent;
+  let fixture: ComponentFixture<ErrorPageComponent>;
+  let de: DebugElement;
+
+  const deepLinkingServiceMock = jasmine.createSpyObj('DeepLinkingService', [
+    'navigateToHome',
+  ]);
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [ErrorPageComponent],
+      schemas: [NO_ERRORS_SCHEMA],
+      providers: [
+        { provide: DeepLinkingService, useValue: deepLinkingServiceMock },
+      ],
+    });
+
+    fixture = TestBed.createComponent(ErrorPageComponent);
+    de = fixture.debugElement;
+
+    component = fixture.componentInstance;
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should call navigateToHome', () => {
+    const goToHomeButton = de.query(By.css('.go-to-home-btn'));
+
+    goToHomeButton.triggerEventHandler('click', {});
+
+    expect(deepLinkingServiceMock.navigateToHome).toHaveBeenCalled();
+  });
+});
