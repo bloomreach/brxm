cc3ffcf602763e2d00c826fc4f63617b3778f2f0
Ate Douma <ate.douma@bloomreach.com>
2019-09-16 10:25:50 +0200

    CMS-12202 Add and use user roles xm-cms-user and xm-console-user for login check
    This replaces (and removes) the current cms/console application login check requiring to
    configure a specific repository privilege which already authenticated users were required to have
    at a specific (but actually arbritrary) configured repository path.
    
    This typically was a cumbersome setup if/when needing to customize it.
    
    Instead, by introducing the user roles xm-console-user and xm-cms-user, this now becomes really
    trivial and no longer needs an arbritary privilege configuration through security domains.
    
    The checkPermission method in the IPluginConfigService, which was *only* used for this one purpose
    no longer is needed and has been dropped from the interface / implementations.

--- cms/api/src/main/java/org/hippoecm/frontend/plugin/config/IPluginConfigService.java
+++ cms/api/src/main/java/org/hippoecm/frontend/plugin/config/IPluginConfigService.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2008-2013 Hippo B.V. (http://www.onehippo.com)
+ *  Copyright 2008-2019 Hippo B.V. (http://www.onehippo.com)
  * 
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -26,8 +26,6 @@ import org.apache.wicket.model.IDetachable;
  * The default cluster will be started when the page is (re)loaded, it can start other clusters as necessary.
  */
 public interface IPluginConfigService extends IDetachable {
-
-    boolean checkPermission(Session session);
 
     IClusterConfig getDefaultCluster();
 
