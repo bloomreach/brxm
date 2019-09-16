11c29e4883731a214c93491a1d1d39dffeb9c864
Arthur Bogaart <arthur.bogaart@bloomreach.com>
2019-09-16 09:36:41 +0200

    CMS-12178 Render sample config from .env in CSR

--- spa-sdk/examples/react-csr/src/App.tsx
+++ spa-sdk/examples/react-csr/src/App.tsx
@@ -16,6 +16,23 @@
 
  import React from 'react';
 
+ const BR_ORIGIN = new URL(process.env.REACT_APP_BR_ORIGIN!);
+ const BR_CONTEXT_PATH = process.env.REACT_APP_BR_CONTEXT_PATH;
+ const BR_CHANNEL_PATH = process.env.REACT_APP_BR_CHANNEL_PATH;
+
+ const urlConfig = {
+   scheme: BR_ORIGIN.protocol.slice(0, -1),
+   hostname: BR_ORIGIN.hostname,
+   port: BR_ORIGIN.port,
+   contextPath: BR_CONTEXT_PATH,
+   channelPath: BR_CHANNEL_PATH
+ };
+
+ const cmsUrls = {
+   preview: urlConfig,
+   live: urlConfig
+ };
+
 const App: React.FC = () => {
   return (
     <div id="br-page">
@@ -32,12 +49,13 @@ const App: React.FC = () => {
             <span className="navbar-toggler-icon" />
           </button>
           <div className="collapse navbar-collapse" id="navbarCollapse">
-            Render Menu
+            Render Menu here
           </div>
         </nav>
       </div>
       <div className="container marketing">
-        Render Container
+        Render Container here<br/>
+        cmsUrls: <pre>{JSON.stringify(cmsUrls, null, 2)}</pre>
       </div>
     </div>
   );
