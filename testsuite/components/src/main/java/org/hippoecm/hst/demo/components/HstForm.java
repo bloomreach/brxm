daee799f3fe1f8bb4b0c15db3f94e5200791e3b7
Ard Schrijvers <ard.schrijvers@bloomreach.com>
2019-09-19 15:18:05 +0200

    TRIVIAL remove double super invocation

--- testsuite/components/src/main/java/org/hippoecm/hst/demo/components/HstForm.java
+++ testsuite/components/src/main/java/org/hippoecm/hst/demo/components/HstForm.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2008-2018 Hippo B.V. (http://www.onehippo.com)
+ *  Copyright 2008-2019 Hippo B.V. (http://www.onehippo.com)
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -34,7 +34,6 @@ public class HstForm extends BaseHstComponent {
     public void doAction(final HstRequest request, final HstResponse response) throws HstComponentException {
         super.doAction(request, response);
 
-        super.doAction(request, response);
         FormMap formMap = new FormMap(request, DEFINED_FORM_FIELDS);
         FormField checkbox = formMap.getField("checkbox");
         if (checkbox == null || checkbox.getValueList().size() == 0) {
