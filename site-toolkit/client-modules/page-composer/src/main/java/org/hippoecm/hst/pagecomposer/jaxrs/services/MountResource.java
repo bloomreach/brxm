1a5b98e4258f6f4fab35fe06996477deb571a454
Ard Schrijvers <ard.schrijvers@bloomreach.com>
2019-09-25 18:46:13 +0200

    HSTTWO-4653 Correct the role for webviewer and allow toolkit for viewer
    Apparently 'channel webviewer's need to be able to get the /toolkit. I
    did not expect this. Since it is just a harmless get, also expose this
    method to 'webviewer's
    
    Next to that, corrected the obvious wrong value for the hipposys:role
    for a 'channel-webviewer'

--- site-toolkit/client-modules/page-composer/src/main/java/org/hippoecm/hst/pagecomposer/jaxrs/services/MountResource.java
+++ site-toolkit/client-modules/page-composer/src/main/java/org/hippoecm/hst/pagecomposer/jaxrs/services/MountResource.java
@@ -78,6 +78,7 @@ import static org.hippoecm.hst.configuration.HstNodeTypes.BRANCH_PROPERTY_BRANCH
 import static org.hippoecm.hst.configuration.HstNodeTypes.MIXINTYPE_HST_BRANCH;
 import static org.hippoecm.hst.pagecomposer.jaxrs.api.SecurityConstants.CHANNEL_ADMIN_PRIVILEGE_NAME;
 import static org.hippoecm.hst.pagecomposer.jaxrs.api.SecurityConstants.CHANNEL_WEBMASTER_PRIVILEGE_NAME;
+import static org.hippoecm.hst.pagecomposer.jaxrs.api.SecurityConstants.CHANNEL_WEB_VIEWER_PRIVILEGE_NAME;
 
 @Path("/hst:mount/")
 public class MountResource extends AbstractConfigResource {
@@ -139,7 +140,7 @@ public class MountResource extends AbstractConfigResource {
     @GET
     @Path("/toolkit/")
     @Produces(MediaType.APPLICATION_JSON)
-    @PrivilegesAllowed(CHANNEL_WEBMASTER_PRIVILEGE_NAME)
+    @PrivilegesAllowed(CHANNEL_WEB_VIEWER_PRIVILEGE_NAME)
     public Response getToolkitRepresentation() {
         final Mount editingMount = getPageComposerContextService().getEditingMount();
         ToolkitRepresentation toolkitRepresentation = new ToolkitRepresentation().represent(editingMount);
