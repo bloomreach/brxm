77a8b30668a104bcc0211feaca4af67ffc6aedd9
Tobias Jeger <tobias.jeger@bloomreach.com>
2019-09-26 23:29:30 +0200

    HIPPLUG-1573 Remove deprecated code from standard plugins

--- poll/hst-client/src/main/java/org/onehippo/forge/poll/contentbean/compound/Poll.java
+++ poll/hst-client/src/main/java/org/onehippo/forge/poll/contentbean/compound/Poll.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2009-2019 Hippo B.V. (http://www.onehippo.com)
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *  http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -23,9 +23,9 @@ import org.onehippo.cms7.essentials.dashboard.annotations.HippoEssentialsGenerat
 
 /**
  * Bean representation of:
- *  
+ *
  *  [poll:poll]
- *    - poll:active (boolean)
+ *    - poll:active (boolean) // deprecated
  *    - poll:text (string)
  *    - poll:introduction (string)
  *    + poll:option (poll:option) multiple
@@ -35,23 +35,14 @@ import org.onehippo.cms7.essentials.dashboard.annotations.HippoEssentialsGenerat
 @HippoEssentialsGenerated(allowModifications = false)
 public class Poll extends HippoItem {
 
-    /**
-     * Deprecated 'active' flag because document (de)publication leads to same functionality
-     *
-     * @deprecated no longer in use
-     */
-    public Boolean getActive() {
-        return this.getSingleProperty("poll:active");
-    }
-    
     public String getText() {
         return this.getSingleProperty("poll:text");
     }
-    
+
     public String getIntroduction() {
         return this.getSingleProperty("poll:introduction");
     }
-    
+
     public List<Option> getOptions() {
         return this.getChildBeans("poll:option");
     }
