98a9d35e527f785ba50d8d607453d61ad404434c
Michael Dokolin <mikhail.dokolin@bloomreach.com>
2019-10-01 10:01:21 +0200

    CMS-12024 Remove mock dependency from MetaComment component unit-tests

--- spa-sdk/packages/react-sdk/src/meta/MetaComment.spec.tsx
+++ spa-sdk/packages/react-sdk/src/meta/MetaComment.spec.tsx
@@ -14,45 +14,45 @@
  * limitations under the License.
  */
 
-jest.mock('@bloomreach/spa-sdk');
-
-import { mount } from 'enzyme';
 import React from 'react';
-import { META_POSITION_BEGIN } from '@bloomreach/spa-sdk';
-
-import { MetaComment } from './MetaComment';
-import { mockMeta } from '../../__mocks__/@bloomreach/spa-sdk';
+import { mount } from 'enzyme';
+import { MetaComment, META_POSITION_BEGIN } from '@bloomreach/spa-sdk';
+import { MetaComment as MetaCommentComponent } from './MetaComment';
 
 describe('MetaComment', () => {
+  const meta = new class implements MetaComment {
+    getData = jest.fn(() => 'comment-data');
+    getPosition = jest.fn(() => META_POSITION_BEGIN as typeof META_POSITION_BEGIN);
+  };
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
   it('should render a comment that contains data', () => {
-    const meta = mockMeta('comment-data', META_POSITION_BEGIN);
-    const wrapper = mount(<div><MetaComment meta={meta}/></div>);
+    const wrapper = mount(<div><MetaCommentComponent meta={meta}/></div>);
 
     expect(wrapper.html()).toBe('<div><!--comment-data--></div>');
   });
 
   it('should update if meta data has changed', () => {
-    const meta = mockMeta('comment-data', META_POSITION_BEGIN);
-    const shouldComponentUpdate = jest.spyOn(MetaComment.prototype, 'shouldComponentUpdate');
-    const wrapper = mount(<MetaComment meta={meta}/>);
+    const shouldComponentUpdate = jest.spyOn(MetaCommentComponent.prototype, 'shouldComponentUpdate');
+    const wrapper = mount(<MetaCommentComponent meta={meta}/>);
     expect(shouldComponentUpdate).not.toHaveBeenCalled();
 
-    const newMeta = {
-      getData: () => 'new-comment-data',
-      getPosition: jest.fn(),
-    };
-    wrapper.setProps({ meta: newMeta });
+    meta.getData.mockReturnValueOnce('comment-data');
+    meta.getData.mockReturnValueOnce('new-comment-data');
+
+    wrapper.setProps({ meta });
 
     expect(shouldComponentUpdate).toHaveBeenCalled();
     expect(shouldComponentUpdate).lastReturnedWith(true);
   });
 
   it('should remove the comment when the component unmounts', () => {
-    const meta = mockMeta('comment-data', META_POSITION_BEGIN);
-    const wrapper = mount(<div><MetaComment meta={meta}/></div>);
+    const wrapper = mount(<div><MetaCommentComponent meta={meta}/></div>);
     wrapper.unmount();
 
-    expect(wrapper.contains(<MetaComment meta={meta}/>)).toBe(false);
+    expect(wrapper.contains(<MetaCommentComponent meta={meta}/>)).toBe(false);
   });
-
 });
