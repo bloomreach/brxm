f2a1592873924a1fb327ea1f0f639090597a0ca4
Michael Dokolin <mikhail.dokolin@bloomreach.com>
2019-10-02 10:50:34 +0200

    CMS-12028 Add BrNode component for the component model rendering

--- spa-sdk/packages/react-sdk/src/component/index.ts
+++ spa-sdk/packages/react-sdk/src/component/index.ts
@@ -16,6 +16,7 @@
 
 export * from './BrComponentContext';
 export * from './BrMappingContext';
+export * from './BrNode';
 export * from './BrNodeComponent';
 export * from './BrNodeContainer';
 export * from './BrNodeContainerItem';
