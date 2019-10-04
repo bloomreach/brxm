9d564f05281fdde8da4ffc85acadf90f9969c671
Ard Schrijvers <ard.schrijvers@bloomreach.com>
2019-10-04 10:27:49 +0200

    HSTTWO-4658 Revert accidental test commit

--- site-toolkit/client-modules/page-composer/src/main/java/org/hippoecm/hst/pagecomposer/jaxrs/services/ContainerComponentResource.java
+++ site-toolkit/client-modules/page-composer/src/main/java/org/hippoecm/hst/pagecomposer/jaxrs/services/ContainerComponentResource.java
@@ -63,7 +63,7 @@ public class ContainerComponentResource extends AbstractConfigResource {
     @Path("/{itemUUID}")
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    @PrivilegesAllowed(value = CHANNEL_WEBMASTER_PRIVILEGE_NAME, absPath = "test")
+    @PrivilegesAllowed(CHANNEL_WEBMASTER_PRIVILEGE_NAME)
     public Response createContainerItem(final @PathParam("itemUUID") String itemUUID,
                                         final @QueryParam("lastModifiedTimestamp") long versionStamp) throws ContainerException {
         if (StringUtils.isEmpty(itemUUID)) {
