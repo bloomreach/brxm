685f9dc80e0db3a4cb6c76647810b53720a29264
Michael Dokolin <mikhail.dokolin@bloomreach.com>
2019-10-07 15:15:29 +0200

    CMS-12253 Add Url builder to generate Page Model API URLs

---
+++ spa-sdk/packages/spa-sdk/src/url/builder.spec.ts
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2019 Hippo B.V. (http://www.onehippo.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { UrlBuilderImpl } from './builder';
+
+describe('UrlBuilderImpl', () => {
+  describe('getApiUrl', () => {
+    const options1 = { cmsBaseUrl: 'http://localhost:8080/site/spa' };
+    const options2 = { ...options1, apiBaseUrl: 'http://127.0.0.1/resourceapi' };
+    const options3 = { ...options1, apiUrlSuffix: '/something?query' };
+    const options4 = { ...options1, spaBaseUrl: '/news' };
+    const options5 = { ...options1, spaBaseUrl: '//example.com/news' };
+    const options6 = { ...options1, spaBaseUrl: 'https://example.com/news' };
+
+    it.each`
+      options     | path                      | expected
+      ${options1} | ${'/'}                    | ${'http://localhost:8080/site/spa/resourceapi/'}
+      ${options1} | ${'/news'}                | ${'http://localhost:8080/site/spa/resourceapi/news'}
+      ${options1} | ${'/news/'}               | ${'http://localhost:8080/site/spa/resourceapi/news/'}
+      ${options1} | ${'/news/2019/foo.html'}  | ${'http://localhost:8080/site/spa/resourceapi/news/2019/foo.html'}
+      ${options1} | ${'/?param=value'}        | ${'http://localhost:8080/site/spa/resourceapi/?param=value'}
+      ${options1} | ${'/news?param=value'}    | ${'http://localhost:8080/site/spa/resourceapi/news?param=value'}
+      ${options2} | ${'/'}                    | ${'http://127.0.0.1/resourceapi/'}
+      ${options2} | ${'/news'}                | ${'http://127.0.0.1/resourceapi/news'}
+      ${options3} | ${'/news'}                | ${'http://localhost:8080/site/spa/resourceapi/news/something?query'}
+      ${options3} | ${'/news?param=value'}    | ${'http://localhost:8080/site/spa/resourceapi/news/something?query&param=value'}
+      ${options4} | ${'/news'}                | ${'http://localhost:8080/site/spa/resourceapi'}
+      ${options4} | ${'/news/'}               | ${'http://localhost:8080/site/spa/resourceapi/'}
+      ${options4} | ${'/news?param=value'}    | ${'http://localhost:8080/site/spa/resourceapi?param=value'}
+      ${options4} | ${'/news/2019/?query'}    | ${'http://localhost:8080/site/spa/resourceapi/2019/?query'}
+      ${options5} | ${'/news/2019/?query'}    | ${'http://localhost:8080/site/spa/resourceapi/2019/?query'}
+      ${options6} | ${'/news/2019/?query'}    | ${'http://localhost:8080/site/spa/resourceapi/2019/?query'}
+    `('should create the Page Model API URL for "$path" using options "$options"', ({ options, path, expected }) => {
+      const builder = new UrlBuilderImpl(options);
+
+      expect(builder.getApiUrl(path)).toBe(expected);
+    });
+
+    it.each`
+      options     | path                      | message
+      ${options4} | ${'/'}                    | ${'The path "/" does not start with the base path "/news".'}
+      ${options6} | ${'/something'}           | ${'The path "/something" does not start with the base path "/news".'}
+    `('should throw an error for the path "$path" with message "$message"', ({ options, path, message }) => {
+      const builder = new UrlBuilderImpl(options);
+
+      expect(() => builder.getApiUrl(path)).toThrow(message);
+    });
+  });
+});
---
+++ spa-sdk/packages/spa-sdk/src/url/builder.ts
@@ -0,0 +1,91 @@
+/*
+ * Copyright 2019 Hippo B.V. (http://www.onehippo.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+const DEFAULT_API_BASE_URL = '/resourceapi';
+const DEFAULT_API_URL_SUFFIX = '';
+const DEFAULT_SPA_BASE_URL = '';
+
+/**
+ * Mapping of the incoming HTTP request path to the URL of the page model API.
+ */
+export interface UrlBuilderOptions {
+  /**
+   * Base URL to fetch the page model from.
+   * The default URL is `cmsBaseUrl` + `/resourceapi`.
+   */
+  apiBaseUrl?: string;
+
+  /**
+   * Optional custom suffix for requests to the page model API.
+   * The default suffix is empty string.
+   */
+  apiUrlSuffix?: string;
+
+  /**
+   * Base URL of the CMS.
+   */
+  cmsBaseUrl: string;
+
+  /**
+   * Base URL of the SPA. Everything after it will be interpreted as a route into the page model.
+   * The default base url is an empty string.
+   */
+  spaBaseUrl?: string;
+}
+
+export interface UrlBuilder {
+  /**
+   * @param path Source path to generate the Page Model API URL.
+   * @returns The Page Model API URL.
+   */
+  getApiUrl(path: string): string;
+}
+
+export class UrlBuilderImpl {
+  private static getUrlPath(url: string) {
+    // URL constructor requires either a valid URL or a base URL.
+    // Since this function returns a pathname, we can safely pass a fake host to be able to resolve relative URLs.
+    return url && (new URL(url, 'http://example.com')).pathname;
+  }
+
+  private static appendQuery(url: string, query: string) {
+    if (!query) {
+      return url;
+    }
+
+    return `${url}${url.includes('?') ? '&' : '?'}${query}`;
+  }
+
+  constructor(private options: UrlBuilderOptions) {}
+
+  getApiUrl(path: string) {
+    const [pathname, query = ''] = path.split('?', 2);
+    const {
+      cmsBaseUrl,
+      apiBaseUrl = `${cmsBaseUrl}${DEFAULT_API_BASE_URL}`,
+      apiUrlSuffix = DEFAULT_API_URL_SUFFIX,
+      spaBaseUrl = DEFAULT_SPA_BASE_URL,
+    } = this.options;
+    const basePath = UrlBuilderImpl.getUrlPath(spaBaseUrl);
+    if (basePath && !pathname.startsWith(basePath)) {
+      throw new Error(`The path "${path}" does not start with the base path "${basePath}".`);
+    }
+
+    const channelPath = pathname.substring(basePath.length);
+
+    return UrlBuilderImpl.appendQuery(`${apiBaseUrl}${channelPath}${apiUrlSuffix}`, query);
+  }
+}
