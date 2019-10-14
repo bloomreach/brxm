9cc9dacedd3188e55b527dc60949fc34df1d0c4c
Arthur Bogaart <arthur.bogaart@bloomreach.com>
2019-10-14 16:50:17 +0200

    CMS-12270 Apply generic top-bar styling
    The top bar of the CMS is now 52px high, has a bottom border and a
    lighter background.
    
    Buttons have a border-radius of 4px.

--- channel-manager/frontend/src/main/resources/org/onehippo/cms7/channelmanager/channels/ChannelOverview.js
+++ channel-manager/frontend/src/main/resources/org/onehippo/cms7/channelmanager/channels/ChannelOverview.js
@@ -1,5 +1,5 @@
 /**
- * Copyright 2011-2018 Hippo B.V. (http://www.onehippo.com)
+ * Copyright 2011-2019 Hippo B.V. (http://www.onehippo.com)
  *
  * Licensed under the Apache License, Version 2.0 (the  "License");
  * you may not use this file except in compliance with the License.
@@ -33,7 +33,7 @@
             this.userPreferences = JSON.parse(localStorage.getItem('channelMgrConf'));
 
             toolbar = new Ext.Toolbar({
-                height: 40,
+                height: 52,
                 cls: 'channel-manager-toolbar',
                 items: [
                     '->',
