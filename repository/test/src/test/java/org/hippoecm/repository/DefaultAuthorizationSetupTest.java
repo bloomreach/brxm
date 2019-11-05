909a1c24b65ecd25baad0cd299067b354bdd5c66
Ard Schrijvers <ard.schrijvers@bloomreach.com>
2019-11-05 15:33:59 +0100

    REPO-2238 Add some more default assertions wrt default setup model

--- repository/test/src/test/java/org/hippoecm/repository/DefaultAuthorizationSetupTest.java
+++ repository/test/src/test/java/org/hippoecm/repository/DefaultAuthorizationSetupTest.java
@@ -23,6 +23,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
+import org.onehippo.repository.security.DomainInfoPrivilege;
 import org.onehippo.repository.testutils.RepositoryTestCase;
 
 import static org.hippoecm.repository.HippoStdNodeType.HIPPOSTD_HOLDER;
@@ -172,5 +173,64 @@ public class DefaultAuthorizationSetupTest extends RepositoryTestCase {
                 user.logout();
             }
         }
+    }
+
+    private final String[] testuserConfig = new String[]{
+            "/hippo:configuration/hippo:users/testuser", "hipposys:user",
+            "hipposys:password", "testuser",
+            "hipposys:securityprovider", "internal"
+    };
+
+    @Test
+    public void any_user_can_read_hippo_derivatives() throws Exception {
+
+        build(testuserConfig, session);
+        session.save();
+
+        final Session author = server.login("author", "author".toCharArray());
+        final Session editor = server.login("author", "author".toCharArray());
+        final Session testuser = server.login("testuser", "testuser".toCharArray());
+
+        for (Session user : new Session[]{author, editor, testuser}) {
+            try {
+                assertTrue(user.hasPermission("/hippo:configuration/hippo:derivatives", Session.ACTION_READ));
+            } finally {
+                user.logout();
+            }
+        }
+
+        session.getNode("/hippo:configuration/hippo:users/testuser").remove();
+        session.save();
+    }
+
+    // /hippo:configuration gets implicit read access due to a jcr:path constraint.
+    @Test
+    public void any_user_can_read_hippo_configuration_node() throws Exception {
+        build(testuserConfig, session);
+        session.save();
+
+        final Session author = server.login("author", "author".toCharArray());
+        final Session editor = server.login("author", "author".toCharArray());
+        final Session testuser = server.login("testuser", "testuser".toCharArray());
+
+        for (Session user : new Session[]{author, editor, testuser}) {
+            try {
+                assertTrue(user.hasPermission("/hippo:configuration", Session.ACTION_READ));
+
+                final DomainInfoPrivilege[] privileges =  (DomainInfoPrivilege[])user.getAccessControlManager().getPrivileges("/hippo:configuration");
+
+                // this is quite a specific use case: Although the user can read "/hippo:configuration", (s)he has no
+                // privileges on that node, not even read...this is because the read is implicit from a read/write
+                // privilege on a descendant node
+
+                assertTrue(privileges.length == 0);
+
+            } finally {
+                user.logout();
+            }
+        }
+
+        session.getNode("/hippo:configuration/hippo:users/testuser").remove();
+        session.save();
     }
 }
