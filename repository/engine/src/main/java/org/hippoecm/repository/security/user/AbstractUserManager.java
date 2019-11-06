8d501e28baf6f5b1270f32205a2b768c2c887a75
Erdem Karakus <erdem.karakus@bloomreach.com>
2019-11-06 11:43:49 +0100

    REPO-2282 Remove compareUserIdBy check in userManager
    With the new SecurityService improvements, forcing users to login by
    using either LOWERCASE or UPPERCASE usernames aren't necessary.
    If users are synchronized from an incasesensitive setup, using
    lowercase usernames is sufficient.

--- repository/engine/src/main/java/org/hippoecm/repository/security/user/AbstractUserManager.java
+++ repository/engine/src/main/java/org/hippoecm/repository/security/user/AbstractUserManager.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2008-2018 Hippo B.V. (http://www.onehippo.com)
+ *  Copyright 2008-2019 Hippo B.V. (http://www.onehippo.com)
  * 
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -256,10 +256,8 @@ public abstract class AbstractUserManager implements HippoUserManager {
         }
         if (isCaseSensitive()) {
             return rawUserId.trim();
-        } else if (isUserIdLowerCase()) {
-            return rawUserId.trim().toLowerCase();
         } else {
-            return rawUserId.trim().toUpperCase();
+            return rawUserId.trim().toLowerCase();
         }
     }
 
