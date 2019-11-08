ffa3f4b625bb1abb869fa163a1504d330444b559
Arthur Bogaart <arthur.bogaart@bloomreach.com>
2019-11-08 09:33:14 +0100

    CMS-12065 Fix license header format

--- cms/perspectives/src/main/java/org/hippoecm/frontend/plugins/cms/admin/AdminPanelPlugin.java
+++ cms/perspectives/src/main/java/org/hippoecm/frontend/plugins/cms/admin/AdminPanelPlugin.java
@@ -1,5 +1,5 @@
-/**
-  * Copyright 2011-2013 Hippo B.V. (http://www.onehippo.com)
+/*
+  * Copyright 2011-2019 Hippo B.V. (http://www.onehippo.com)
   *
   * Licensed under the Apache License, Version 2.0 (the  "License");
   * you may not use this file except in compliance with the License.
@@ -21,7 +21,7 @@ import org.hippoecm.frontend.plugins.standards.panelperspective.PanelPlugin;
 
 public abstract class AdminPanelPlugin extends PanelPlugin {
 
-    public static final String ADMIN_PANEL_SERVICE_ID = "admin.panel";
+    static final String ADMIN_PANEL_SERVICE_ID = "admin.panel";
 
     public AdminPanelPlugin(final IPluginContext context, final IPluginConfig config) {
         super(context, config);
