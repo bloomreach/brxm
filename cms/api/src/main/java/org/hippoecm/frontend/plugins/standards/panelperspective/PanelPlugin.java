8d6c1a3a11a985d3d8d1186078a17eab575fcf39
Arthur Bogaart <arthur.bogaart@bloomreach.com>
2019-11-11 13:09:51 +0100

    CMS-12271 Update panel styling for reports and admin perspective
    Apart from a bigger font (14px) and rounded corners, the default styling
    for the panels is without an icon. For backwards-compatibility,
    rendering with an icon is still supported.
    
    For the admin panel, all icons have been removed.

--- cms/api/src/main/java/org/hippoecm/frontend/plugins/standards/panelperspective/PanelPlugin.java
+++ cms/api/src/main/java/org/hippoecm/frontend/plugins/standards/panelperspective/PanelPlugin.java
@@ -1,5 +1,5 @@
-/**
-  * Copyright 2011-2013 Hippo B.V. (http://www.onehippo.com)
+/*
+  * Copyright 2011-2019 Hippo B.V. (http://www.onehippo.com)
   *
   * Licensed under the Apache License, Version 2.0 (the  "License");
   * you may not use this file except in compliance with the License.
@@ -35,7 +35,9 @@ public abstract class PanelPlugin implements IPlugin, IBreadCrumbPanelFactory {
         this.config = config;
     }
 
-    public abstract ResourceReference getImage();
+    public ResourceReference getImage() {
+        return null;
+    }
 
     public abstract IModel<String> getTitle();
 
