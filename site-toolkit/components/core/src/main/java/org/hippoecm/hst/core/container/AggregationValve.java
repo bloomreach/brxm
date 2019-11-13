c3b411a1edf3118d88c545b7dcf811dca6a261ba
Arent-Jan Banck <arent-jan.banck@bloomreach.com>
2019-11-13 23:24:12 +0100

    CMS-12357 Backport servlet 3.1 support

--- site-toolkit/components/core/src/main/java/org/hippoecm/hst/core/container/AggregationValve.java
+++ site-toolkit/components/core/src/main/java/org/hippoecm/hst/core/container/AggregationValve.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2008-2016 Hippo B.V. (http://www.onehippo.com)
+ *  Copyright 2008-2019 Hippo B.V. (http://www.onehippo.com)
  * 
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -941,6 +941,11 @@ public class AggregationValve extends AbstractBaseOrderableValve {
 
         @Override
         public void setContentLength(int len) {
+        }
+
+        @Override
+        public void setContentLengthLong(long len) {
+
         }
 
         @Override
