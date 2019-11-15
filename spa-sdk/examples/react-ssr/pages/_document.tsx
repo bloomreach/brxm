9ff58b18944c7ae0f7e480a06c7d090039a6e526
Michael Dokolin <mikhail.dokolin@bloomreach.com>
2019-11-15 15:44:39 +0100

    CMS-12203 Add favicons to example apps

--- spa-sdk/examples/react-ssr/pages/_document.tsx
+++ spa-sdk/examples/react-ssr/pages/_document.tsx
@@ -27,6 +27,7 @@ export default class DefaultDocument extends Document {
 
           <title>brXM + Next.js = ♥️</title>
 
+          <link rel="shortcut icon" type="image/png" href={`${process.env.PUBLIC_URL}/static/favicon.png`} sizes="64x64" />
           <link rel="stylesheet" media="screen"
             href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
             integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
