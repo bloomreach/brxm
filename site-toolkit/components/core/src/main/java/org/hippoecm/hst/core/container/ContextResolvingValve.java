6638cf2bfca4e5ecbf0be8ae7b1389684b7aa4bd
Ard Schrijvers <ard.schrijvers@bloomreach.com>
2019-11-15 11:12:22 +0100

    HSTTWO-4673 Make sure partial rendering pages are cached separately
    To avoid that for example the URLs
    
    http://localhost:8080/site/blog./menu
    http://localhost:8080/site/blog./main
    
    end up in the cache with the same cachekey, the partial page indicator
    (main/menu) should be included in the cachekey

--- site-toolkit/components/core/src/main/java/org/hippoecm/hst/core/container/ContextResolvingValve.java
+++ site-toolkit/components/core/src/main/java/org/hippoecm/hst/core/container/ContextResolvingValve.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2008-2018 Hippo B.V. (http://www.onehippo.com)
+ *  Copyright 2008-2019 Hippo B.V. (http://www.onehippo.com)
  * 
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -94,7 +94,8 @@ public class ContextResolvingValve extends AbstractBaseOrderableValve {
                             context.getServletRequest(),
                             (rootComponentWindow.getComponent() != null) ? rootComponentWindow.getComponent().getComponentConfiguration() : null);
                 } else {
-                    log.warn("Cannot find the component by the path, '{}', for partial page rendering."
+                    // info level since user request originated wrong path suffix (which should not result in warning logs)
+                    log.info("Cannot find the component by the path, '{}', for partial page rendering."
                             + "The default root component is rendered as fallback instead.", partialWindowPath);
                 }
             }
