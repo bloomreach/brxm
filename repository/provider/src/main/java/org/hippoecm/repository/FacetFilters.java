16f6825b4ed9644876061d7399b3def55c561d89
Ard Schrijvers <ard.schrijvers@bloomreach.com>
2019-11-18 16:18:28 +0100

    REPO-2284 [Backport 12.6] in case a FacetFilter cannot be created, throw IllegalArgExc
    Only IllegalArgumentExceptions are handled gracefully, however, there
    can be other exception like for example ArrayOutOfBounds in case the
    facet filter is for example:
    
    myproject:color!=
    
    In this case, the fac nav node cannot be loaded and also not fixed any
    more. Instead, we should catch any exception and wrap it in a
    IllegalArgumentException which is handled gracefully.
    
    (cherry picked from commit 0f5ebacf3653aaad26cac35e949979d5a6c4efdd)

--- repository/provider/src/main/java/org/hippoecm/repository/FacetFilters.java
+++ repository/provider/src/main/java/org/hippoecm/repository/FacetFilters.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2010-2013 Hippo B.V. (http://www.onehippo.com)
+ *  Copyright 2010-2019 Hippo B.V. (http://www.onehippo.com)
  * 
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -115,101 +115,112 @@ public class FacetFilters {
         public boolean negated = false;
         
         public FacetFilter(String filter, HippoVirtualProvider provider) throws IllegalArgumentException {
-            String origFilter = filter;
-            filter = filter.trim();
-            String lcaseFilter = filter.toLowerCase(); 
-            if(lcaseFilter.startsWith(NOT_OPERATOR.substring(0, NOT_OPERATOR.length() -1)) && lcaseFilter.endsWith(NOT_OPERATOR.substring(NOT_OPERATOR.length() -1)) ) {
-                // remove not( AND )
-                filter = filter.substring(NOT_OPERATOR.length() -1);
-                filter = filter.substring(0, filter.length() -1);
-                negated = true;
+            if (filter == null) {
+                throw new IllegalArgumentException("filter is not allowed to be null");
             }
-            if(filter.indexOf(IGNORE_NOTEQUAL_OPERATOR) > -1) {
-                filter.replaceAll(IGNORE_NOTEQUAL_OPERATOR, String.valueOf(IGNORE_NOTEQUAL_OPERATOR_DELIM));
-            }
-            if(filter.indexOf(IGNORE_EQUAL_OPERATOR) > -1) {
-                filter.replaceAll(IGNORE_EQUAL_OPERATOR , String.valueOf(IGNORE_EQUAL_OPERATOR_DELIM));
-            }
-            
-            for(Operator op: Operator.SUPPORTED_OPERATORS) {
-                if(lcaseFilter.indexOf(op.operator) > -1) {
-                    if(lcaseFilter.indexOf(op.operator) != lcaseFilter.lastIndexOf(op.operator)) {
-                        throw new IllegalArgumentException("Cannot have 2 operators: '"+op.operator+"' occurs more then once in '"+origFilter+"'");
+
+            final String origFilter = filter;
+
+            try {
+                filter = filter.trim();
+                String lcaseFilter = filter.toLowerCase();
+                if (lcaseFilter.startsWith(NOT_OPERATOR.substring(0, NOT_OPERATOR.length() - 1)) && lcaseFilter.endsWith(NOT_OPERATOR.substring(NOT_OPERATOR.length() - 1))) {
+                    // remove not( AND )
+                    filter = filter.substring(NOT_OPERATOR.length() - 1);
+                    filter = filter.substring(0, filter.length() - 1);
+                    negated = true;
+                }
+                if (filter.indexOf(IGNORE_NOTEQUAL_OPERATOR) > -1) {
+                    filter.replaceAll(IGNORE_NOTEQUAL_OPERATOR, String.valueOf(IGNORE_NOTEQUAL_OPERATOR_DELIM));
+                }
+                if (filter.indexOf(IGNORE_EQUAL_OPERATOR) > -1) {
+                    filter.replaceAll(IGNORE_EQUAL_OPERATOR, String.valueOf(IGNORE_EQUAL_OPERATOR_DELIM));
+                }
+
+                for (Operator op : Operator.SUPPORTED_OPERATORS) {
+                    if (lcaseFilter.indexOf(op.operator) > -1) {
+                        if (lcaseFilter.indexOf(op.operator) != lcaseFilter.lastIndexOf(op.operator)) {
+                            throw new IllegalArgumentException("Cannot have 2 operators: '" + op.operator + "' occurs more then once in '" + origFilter + "'");
+                        }
+                        operator = op;
+                        // found operator, stop
+                        break;
                     }
-                    operator = op;
-                    // found operator, stop
-                    break;
                 }
-            }
-            
-            if(operator == null) {
-                // do nothing: the string will be parsed by the Lucene QueryParser  
-                operator = NOOP_OPERATOR;
-                queryString = filter;
-            } else if (operator == EQUAL_OPERATOR || operator == NOTEQUAL_OPERATOR){
-                String[] split = filter.split(operator.operator);
-                String jcrPropertyName = split[0].trim();
-                queryString = split[1].trim();
-                try {
-                    Name qName = provider.resolveName(jcrPropertyName);
-                    namespacedProperty = qName.toString();
-                } catch (IllegalNameException e) {
-                    throw new IllegalArgumentException("Unsupported property '"+namespacedProperty+"' found in '"+origFilter+"': "+e.getMessage()+"");
-                } catch (NamespaceException e) {
-                    throw new IllegalArgumentException("Unsupported property '"+namespacedProperty+"' found in '"+origFilter+"': "+e.getMessage()+"");
-                }
-            } else if(operator == CONTAINS_OPERATOR) {
-                if(filter.startsWith(CONTAINS_OPERATOR.operator) && filter.endsWith(")")) {
-                    filter = filter.substring(CONTAINS_OPERATOR.operator.length());
-                    filter = filter.substring(0, filter.length() -1);
-                    
-                    // we now should have a format like:  prefix:localname , search terms  if we want to search in some property
-                    // and it will be  . , search terms when we need to search in all fields.
-                    
-                    filter = filter.trim();
-                    if(filter.indexOf(",") > -1 && filter.lastIndexOf(",") == filter.indexOf(",") ){
-                        // we have a single comma ,
-                        String[] split = filter.split(",");
-                        String jcrPropertyName = split[0].trim();
-                        queryString = split[1].trim();
-                        if(jcrPropertyName.equals(".")) {
-                            // we effectively have the same as the NOOP_OPERATOR, and only the right terms
-                            operator = NOOP_OPERATOR;
-                        } else {
-                            if(jcrPropertyName.indexOf("$") > -1) {
-                                // Not yet supported is searching in 'innerpart' properties, like hippo:date$year
-                                throw new IllegalArgumentException("We do not (yet) support contains() in combination with a 'partial' property, like 'hippo:date$year' ");
+
+                if (operator == null) {
+                    // do nothing: the string will be parsed by the Lucene QueryParser
+                    operator = NOOP_OPERATOR;
+                    queryString = filter;
+                } else if (operator == EQUAL_OPERATOR || operator == NOTEQUAL_OPERATOR) {
+                    String[] split = filter.split(operator.operator);
+                    String jcrPropertyName = split[0].trim();
+                    queryString = split[1].trim();
+                    try {
+                        Name qName = provider.resolveName(jcrPropertyName);
+                        namespacedProperty = qName.toString();
+                    } catch (IllegalNameException e) {
+                        throw new IllegalArgumentException("Unsupported property '" + namespacedProperty + "' found in '" + origFilter + "': " + e.getMessage() + "");
+                    } catch (NamespaceException e) {
+                        throw new IllegalArgumentException("Unsupported property '" + namespacedProperty + "' found in '" + origFilter + "': " + e.getMessage() + "");
+                    }
+                } else if (operator == CONTAINS_OPERATOR) {
+                    if (filter.startsWith(CONTAINS_OPERATOR.operator) && filter.endsWith(")")) {
+                        filter = filter.substring(CONTAINS_OPERATOR.operator.length());
+                        filter = filter.substring(0, filter.length() - 1);
+
+                        // we now should have a format like:  prefix:localname , search terms  if we want to search in some property
+                        // and it will be  . , search terms when we need to search in all fields.
+
+                        filter = filter.trim();
+                        if (filter.indexOf(",") > -1 && filter.lastIndexOf(",") == filter.indexOf(",")) {
+                            // we have a single comma ,
+                            String[] split = filter.split(",");
+                            String jcrPropertyName = split[0].trim();
+                            queryString = split[1].trim();
+                            if (jcrPropertyName.equals(".")) {
+                                // we effectively have the same as the NOOP_OPERATOR, and only the right terms
+                                operator = NOOP_OPERATOR;
                             } else {
-                                try {
-                                    Name qName = provider.resolveName(jcrPropertyName);
-                                    namespacedProperty = qName.toString();
-                                } catch (IllegalNameException e) {
-                                    throw new IllegalArgumentException("Unsupported property found in contains() for property '"+namespacedProperty+"' in '"+origFilter+"' : "+e.getMessage()+"");
-                                } catch (NamespaceException e) {
-                                    throw new IllegalArgumentException("Unsupported property found in contains() for property '"+namespacedProperty+"' in '"+origFilter+"' : "+e.getMessage()+"");
+                                if (jcrPropertyName.indexOf("$") > -1) {
+                                    // Not yet supported is searching in 'innerpart' properties, like hippo:date$year
+                                    throw new IllegalArgumentException("We do not (yet) support contains() in combination with a 'partial' property, like 'hippo:date$year' ");
+                                } else {
+                                    try {
+                                        Name qName = provider.resolveName(jcrPropertyName);
+                                        namespacedProperty = qName.toString();
+                                    } catch (IllegalNameException e) {
+                                        throw new IllegalArgumentException("Unsupported property found in contains() for property '" + namespacedProperty + "' in '" + origFilter + "' : " + e.getMessage() + "");
+                                    } catch (NamespaceException e) {
+                                        throw new IllegalArgumentException("Unsupported property found in contains() for property '" + namespacedProperty + "' in '" + origFilter + "' : " + e.getMessage() + "");
+                                    }
                                 }
                             }
-                        }
-                        if(queryString.startsWith("*") || queryString.startsWith("?")) {
-                            throw new IllegalArgumentException("Invalid filter is '"+origFilter+"'  . On purpose we do not support prefix wildcard searches as they blow up in inverted indexes such as lucene");
+                            if (queryString.startsWith("*") || queryString.startsWith("?")) {
+                                throw new IllegalArgumentException("Invalid filter is '" + origFilter + "'  . On purpose we do not support prefix wildcard searches as they blow up in inverted indexes such as lucene");
+                            }
+                        } else {
+                            throw new IllegalArgumentException("Invalid filter is '" + origFilter + "'  .When using the 'contains(my:prop,foo bar)' format, in contains only a single comma ',' is allowed");
                         }
                     } else {
-                        throw new IllegalArgumentException("Invalid filter is '"+origFilter+"'  .When using the 'contains(my:prop,foo bar)' format, in contains only a single comma ',' is allowed");
+                        throw new IllegalArgumentException("Invalid filter is '" + origFilter + "'  .When using 'contains(my:prop,foo bar)', the filter must start with 'contains(' and end with ')'");
                     }
-                } else {
-                    throw new IllegalArgumentException("Invalid filter is '"+origFilter+"'  .When using 'contains(my:prop,foo bar)', the filter must start with 'contains(' and end with ')'");
                 }
-            }   
-            if(operator == null || queryString == null || (operator != NOOP_OPERATOR && namespacedProperty == null) ) {
-                throw new IllegalArgumentException("Could not parse filter '"+origFilter+"'");
-            }
-            
-            // restore possibly escaped operators
-            if(queryString.indexOf(IGNORE_NOTEQUAL_OPERATOR_DELIM) > -1) {
-                queryString.replaceAll(String.valueOf(IGNORE_NOTEQUAL_OPERATOR_DELIM), IGNORE_NOTEQUAL_OPERATOR);
-            }
-            if(queryString.indexOf(IGNORE_EQUAL_OPERATOR_DELIM) > -1) {
-                queryString.replaceAll(String.valueOf(IGNORE_EQUAL_OPERATOR_DELIM), IGNORE_EQUAL_OPERATOR);
+                if (operator == null || queryString == null || (operator != NOOP_OPERATOR && namespacedProperty == null)) {
+                    throw new IllegalArgumentException("Could not parse filter '" + origFilter + "'");
+                }
+
+                // restore possibly escaped operators
+                if (queryString.indexOf(IGNORE_NOTEQUAL_OPERATOR_DELIM) > -1) {
+                    queryString.replaceAll(String.valueOf(IGNORE_NOTEQUAL_OPERATOR_DELIM), IGNORE_NOTEQUAL_OPERATOR);
+                }
+                if (queryString.indexOf(IGNORE_EQUAL_OPERATOR_DELIM) > -1) {
+                    queryString.replaceAll(String.valueOf(IGNORE_EQUAL_OPERATOR_DELIM), IGNORE_EQUAL_OPERATOR);
+                }
+            } catch (IllegalArgumentException e) {
+                throw e;
+            } catch (Exception e) {
+                throw new IllegalArgumentException(String.format("Could not create FacetFilter for '%s'", origFilter), e);
             }
         }
         
