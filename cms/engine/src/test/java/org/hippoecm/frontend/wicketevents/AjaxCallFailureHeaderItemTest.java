dfe964b67a79dda8656d149e61f2ec19bdc6bc9a
GitLab Jira integration <hippo-hosting+gitlab@bloomreach.com>
2019-12-03 11:09:45 +0100

    CMS-12401 Show error on non-response wicket xhr
    Show error page in navigation application when interacting with the cms
    and the server does not respond.
    
    Send an onError message [penpall] with a 500 error code and a message to the
    navigation application when an xhr fails.
    
    Add a custom javascript header item, adds the no-response.js
    
    Add Hippo.onError global method so it can be called from the javascript
    that is included in the header item.
    
    In cms versions before version 14 we did not show any error page if a
    xhr call failed. Now that the navigation application loads the cms in an
    iframe, the navigation application updates the breadcrumb and the menu
    item, but the cms does not respond anymore. This is inconsistent from a
    ux point of view.
    
    Wicket uses jquery to send xhr calls and has a number of callbacks, see
    wicket-core-7.14.0 /org/apache/wicket/ajax/res/js/wicket-ajax-jquery.js:1119.
    
    When the wicket xhr call fails it sends a AJAX_CALL_FAILURE event.

---
+++ cms/engine/src/test/java/org/hippoecm/frontend/wicketevents/AjaxCallFailureHeaderItemTest.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2019 Hippo B.V. (http://www.onehippo.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package org.hippoecm.frontend.wicketevents;
+
+import org.apache.wicket.markup.head.HeaderItem;
+import org.apache.wicket.request.Response;
+import org.apache.wicket.util.tester.WicketTestCase;
+import org.easymock.Capture;
+import org.easymock.CaptureType;
+import org.easymock.EasyMockRunner;
+import org.easymock.Mock;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.easymock.EasyMock.capture;
+import static org.easymock.EasyMock.expectLastCall;
+import static org.easymock.EasyMock.replay;
+import static org.easymock.EasyMock.verify;
+import static org.hamcrest.CoreMatchers.allOf;
+import static org.hamcrest.CoreMatchers.containsString;
+
+@RunWith(EasyMockRunner.class)
+public class AjaxCallFailureHeaderItemTest extends WicketTestCase {
+
+    @SuppressWarnings("unused")
+    @Mock
+    private Response response;
+
+    @Test
+    public void test_script_subscribes_to_ajax_call_failure_events() {
+
+        final Capture<String> data = Capture.newInstance(CaptureType.ALL);
+        response.write(capture(data));
+        expectLastCall().atLeastOnce();
+        replay(response);
+
+        final HeaderItem headerItem = new AjaxCallFailureHeaderItem();
+        headerItem.render(response);
+
+        final String script = String.join("\n", data.getValues());
+        assertThat(script, allOf(
+                containsString("Wicket.Event.subscribe"),
+                containsString("Wicket.Event.Topic.AJAX_CALL_FAILURE")
+        ));
+
+        verify(response);
+    }
+}
