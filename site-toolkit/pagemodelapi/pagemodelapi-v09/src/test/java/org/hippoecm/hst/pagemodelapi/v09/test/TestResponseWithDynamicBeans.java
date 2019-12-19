d49ab48274c161ccc725eba2fb48c075e9746060
Minos Chatzidakis <minos.chatzidakis@bloomreach.com>
2019-12-19 16:32:39 +0100

    HSTTWO-4696 Add PageModelAPI IT for dynamic beans
    Ensure dynamic beans generation works properly for content blocks fields
    when exposed via PageModel API. This test can be used as a template for
    other PageModelAPI tests

---
+++ site-toolkit/pagemodelapi/pagemodelapi-v09/src/test/java/org/hippoecm/hst/pagemodelapi/v09/test/TestResponseWithDynamicBeans.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2019 Hippo B.V. (http://www.onehippo.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.hippoecm.hst.pagemodelapi.v09.test;
+
+import java.util.Iterator;
+
+import javax.jcr.Session;
+
+import org.apache.commons.lang.StringUtils;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.mock.web.MockHttpServletResponse;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class TestResponseWithDynamicBeans extends PageModelAPITest {
+
+    private static final Logger log = LoggerFactory.getLogger(TestResponseWithDynamicBeans.class);
+
+    private static ObjectMapper mapper = new ObjectMapper();
+
+    @Test
+    public void test_pagemodelapi__dynamic_beans__document_with_contentblocks() throws Exception {
+
+        Session session = createLiveUserSession();
+        final RequestResponseMock requestResponse = mockGetRequestResponse(
+                "http", "localhost", "/spa/resourceapi/genericdetail/dynamiccontent", null);
+
+        final MockHttpServletResponse response = render(requestResponse);
+        final String restResponse = response.getContentAsString();
+        assertTrue("PageModelAPI response is empty", StringUtils.isNotEmpty(restResponse));
+
+        JsonNode root = mapper.readTree(restResponse);
+        //Content is referenced in this json response by the property page.models.document.$ref, example format: "/content/uf227192b3c0941a196b1bee660626033"
+        String contentRef = root.path("page").path("models").path("document").path("$ref").asText();
+        String contentUuid = contentRef.substring(contentRef.lastIndexOf('/') + 1);
+
+        JsonNode contentNode = root.path("content").path(contentUuid);
+
+        //Custom contentblocks field without contentblocksWithValidator
+        assertTrue("Content blocks field with name 'contentblocks' not found", contentNode.hasNonNull("contentblocks"));
+        assertEquals("Field 'contentblocks' does not contain expected value", "Welcome Home!",
+                contentNode.path("contentblocks").iterator().next().path("text").path("value").asText());
+
+        //Custom contentblocks field with contentblocksWithValidator
+        assertTrue("Content blocks field with name 'contentblocksWithValidator' not found", contentNode.hasNonNull("contentblocksWithValidator"));
+        assertEquals("Field 'contentblocksWithValidator' does not contain expected value",
+                "Welcome Home with validator!", contentNode.path("contentblocksWithValidator").iterator().next().path("text").path("value").asText());
+
+        //Essentials generated contentblocks fields
+        assertTrue("Content blocks fields with name 'essentialsGeneratedContentblocks' not found", contentNode.hasNonNull("essentialsGeneratedContentblocks"));
+        Iterator<JsonNode> contentBlocks = contentNode.path("essentialsGeneratedContentblocks").iterator();
+        assertEquals("First field named 'essentialsGeneratedContentblocks' does not contain expected value", "Welcome Home with Essentials!", contentBlocks.next().path("text").path("value").asText());
+        assertEquals("Second field named 'essentialsGeneratedContentblocks' does not contain expected value", "Welcome Home with Essentials and lazy loaded types!", contentBlocks.next().path("text").path("value").asText());
+
+        session.logout();
+    }
+}
