1b3a44ad2031626fc4f994c9a91ae3614b0f790f
Ard Schrijvers <ard.schrijvers@bloomreach.com>
2020-01-02 12:39:50 +0100

    TRIVIAL Remove leftover Force-Client-Host: usage has been removed long ago
    This was still a leftover, the HST doesn't do anything with it since
    HSTTWO_4372

--- channel-manager/frontend-api/src/main/resources/org/onehippo/cms7/channelmanager/channeleditor/ComponentVariantAdder.js
+++ channel-manager/frontend-api/src/main/resources/org/onehippo/cms7/channelmanager/channeleditor/ComponentVariantAdder.js
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2016 Hippo B.V. (http://www.onehippo.com)
+ * Copyright 2010-2020 Hippo B.V. (http://www.onehippo.com)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -51,7 +51,7 @@
     saveVariant: function (variant) {
       Ext.Ajax.request({
         method: 'POST',
-        url: this.composerRestMountUrl + '/' + this.componentId + './' + encodeURIComponent(variant) + '?Force-Client-Host=true',
+        url: this.composerRestMountUrl + '/' + this.componentId + './' + encodeURIComponent(variant),
         success: function () {
           this.fireEvent('save', variant);
         },
