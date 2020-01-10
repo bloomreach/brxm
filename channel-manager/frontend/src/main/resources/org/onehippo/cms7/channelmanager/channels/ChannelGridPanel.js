ca0d12772dfa55caeeffa5f216e24513aadc3a7a
Arthur Bogaart <arthur.bogaart@bloomreach.com>
2020-01-10 14:02:37 +0100

    CHANNELMGR-2450 Re-use channel-grid styling for blueprint-grid

--- channel-manager/frontend/src/main/resources/org/onehippo/cms7/channelmanager/channels/ChannelGridPanel.js
+++ channel-manager/frontend/src/main/resources/org/onehippo/cms7/channelmanager/channels/ChannelGridPanel.js
@@ -1,5 +1,5 @@
 /**
- * Copyright 2011-2018 Hippo B.V. (http://www.onehippo.com)
+ * Copyright 2011-2020 Hippo B.V. (http://www.onehippo.com)
  *
  * Licensed under the Apache License, Version 2.0 (the  "License");
  * you may not use this file except in compliance with the License.
@@ -58,6 +58,7 @@
                 stateful: true,
                 stateEvents: ['columnmove', 'columnresize', 'sortchange', 'groupchange'],
                 colModel: columnModel,
+                ctCls: 'br-grid',
 
                 view: new Ext.grid.GroupingView({
                     forceFit: true,
