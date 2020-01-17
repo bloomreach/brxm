84fef358ce4c0c39afd5b35a417b3c8db0b141b9
Joeri de Gooijer <joeri.degooijer@bloomreach.com>
2020-01-17 08:58:04 +0100

    CHANNELMGR-2455 Change layout and styling of site menu editor
    - Move 'back' button to right and label it instead of using an icon
      This also impacts all other fullscreen dialogs
    - Move 'add' button to top bar and label it
    - Select first menu item and auto focus on expand button

--- channel-manager/frontend-ng/src/app/channel/subpage/toolbar/subpageToolbar.component.js
+++ channel-manager/frontend-ng/src/app/channel/subpage/toolbar/subpageToolbar.component.js
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016-2018 Hippo B.V. (http://www.onehippo.com)
+ * Copyright 2016-2020 Hippo B.V. (http://www.onehippo.com)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -25,6 +25,7 @@ const subpageToolbarComponent = {
   },
   controller,
   template,
+  transclude: true,
 };
 
 export default subpageToolbarComponent;
