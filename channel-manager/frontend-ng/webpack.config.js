26f42d351f82e5dca2937e09c37735a1b50ae5e4
Michael Dokolin <mikhail.dokolin@bloomreach.com>
2020-01-20 14:51:53 +0100

    CHANNELMGR-2464 Add entry loader

--- channel-manager/frontend-ng/webpack.config.js
+++ channel-manager/frontend-ng/webpack.config.js
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018-2019 Hippo B.V. (http://www.onehippo.com)
+ * Copyright 2018-2020 Hippo B.V. (http://www.onehippo.com)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -40,6 +40,13 @@ const webpackConfig = merge(config, {
 
   module: {
     rules: [
+      {
+        test: /iframe\/index\.js$/,
+        use: {
+          loader: require.resolve('./entry-loader'),
+          options: { name: 'iframe' },
+        },
+      },
       {
         test: /babel-plugin-transform-async-to-promises/,
         use: {
