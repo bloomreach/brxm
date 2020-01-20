152ca8c939ac98b51e6406beee4097deb754787c
Michiel Eggermont <michiel.eggermont@bloomreach.com>
2020-01-20 15:49:13 +0100

    CMS-12499 Remove captcha from default login plugin
    The CMS login captcha can be bypassed by deleting cookies
    so it is not a proper protection against brute force login
    attacks.
    
    (cherry picked from 6e4fd20f)

--- cms/login/src/main/java/org/hippoecm/frontend/plugins/login/DefaultLoginPlugin.java
+++ cms/login/src/main/java/org/hippoecm/frontend/plugins/login/DefaultLoginPlugin.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2016-2018 Hippo B.V. (http://www.onehippo.com)
+ *  Copyright 2016-2020 Hippo B.V. (http://www.onehippo.com)
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -37,7 +37,7 @@ import org.hippoecm.frontend.plugin.IPluginContext;
 import org.hippoecm.frontend.plugin.config.IPluginConfig;
 import org.hippoecm.frontend.session.UserSession;
 
-public class DefaultLoginPlugin extends SimpleLoginPlugin {
+public class DefaultLoginPlugin extends LoginPlugin {
 
     private static final TextTemplate INIT_JS = new PackageTextTemplate(DefaultLoginPlugin.class, "timezones-init.js");
     // jstz.min.js is fetched by npm
@@ -67,7 +67,7 @@ public class DefaultLoginPlugin extends SimpleLoginPlugin {
         return new TimeZonePanel(id, config, handler);
     }
 
-    protected class TimeZonePanel extends CaptchaPanel {
+    protected class TimeZonePanel extends LoginPanel {
 
         private static final String TIMEZONE_COOKIE = "tzcookie";
         private static final int TIMEZONE_COOKIE_MAX_AGE = 365 * 24 * 3600; // expire one year from now
