21a6e561cd5b4350e75c2a58445c0432b6bf0bd7
Michiel Eggermont <michiel.eggermont@bloomreach.com>
2020-01-27 10:54:25 +0100

    CMS-11043 Replace CollapsedItems with FlagList
    I also added utility methods to the NodeFieldPlugin to
    make the code more readable. I moved the tests from the
    CollapsedItemsTest over to the FlagListTest. They still
    pass, so it seems I refactored correctly.

--- cms/editor/frontend/src/test/java/org/hippoecm/frontend/editor/plugins/field/FlagListTest.java
+++ cms/editor/frontend/src/test/java/org/hippoecm/frontend/editor/plugins/field/FlagListTest.java
@@ -110,4 +110,94 @@ public class FlagListTest {
         assertTrue(flagList.get(1));
         assertFalse(flagList.get(2));
     }
+
+    @Test
+    public void testToggle() {
+        flagList.set(7, true);
+        assertTrue(flagList.get(7));
+        flagList.set(7, false);
+        assertFalse(flagList.get(7));
+    }
+
+    @Test
+    public void testSetTwice() {
+        flagList.set(17, true);
+        flagList.set(17, true);
+        assertTrue(flagList.get(17));
+
+        flagList.set(17, false);
+        flagList.set(17, false);
+        assertFalse(flagList.get(17));
+    }
+
+    @Test
+    public void testRemoveRemovesItemAndUpdatesSubsequentItems() {
+        flagList.set(2, true);
+        flagList.set(7, true);
+        flagList.set(8, true);
+        flagList.set(10, true);
+
+        flagList.remove(7);
+
+        assertTrue(flagList.get(2));
+        assertTrue(flagList.get(7));
+        assertFalse(flagList.get(8));
+        assertTrue(flagList.get(9));
+    }
+
+    @Test
+    public void testUpdateMoveToTop() {
+        flagList.set(0, true);
+        flagList.set(1, true);
+        flagList.set(7, true);
+
+        flagList.moveTo(7, 0);
+
+        assertTrue(flagList.get(0));
+        assertTrue(flagList.get(1));
+        assertTrue(flagList.get(2));
+        assertFalse(flagList.get(7));
+    }
+
+    @Test
+    public void testUpdateMoveUp() {
+        flagList.set(7, true);
+        flagList.set(9, true);
+        flagList.set(17, true);
+
+        flagList.moveUp(9);
+
+        assertTrue(flagList.get(7));
+        assertTrue(flagList.get(8));
+        assertFalse(flagList.get(9));
+        assertTrue(flagList.get(17));
+    }
+
+    @Test
+    public void testUpdateMoveToBottom() {
+        flagList.set(7, true);
+        flagList.set(17, true);
+        flagList.set(19, true);
+
+        flagList.moveTo(7, 19);
+
+        assertFalse(flagList.get(7));
+        assertTrue(flagList.get(16));
+        assertTrue(flagList.get(18));
+        assertTrue(flagList.get(19));
+    }
+
+    @Test
+    public void testUpdateMoveDown() {
+        flagList.set(1, true);
+        flagList.set(7, true);
+        flagList.set(9, true);
+
+        flagList.moveDown(7);
+
+        assertTrue(flagList.get(1));
+        assertFalse(flagList.get(7));
+        assertTrue(flagList.get(8));
+        assertTrue(flagList.get(9));
+    }
 }
