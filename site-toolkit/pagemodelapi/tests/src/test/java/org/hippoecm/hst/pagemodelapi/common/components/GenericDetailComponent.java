31cffb8cdaeccaddee025bb083b365514eb7b1b1
Ard Schrijvers <ard.schrijvers@bloomreach.com>
2020-01-30 14:41:45 +0100

    HSTTWO-4736 Add assertions for HstLink objects and internal document links

--- site-toolkit/pagemodelapi/tests/src/test/java/org/hippoecm/hst/pagemodelapi/common/components/GenericDetailComponent.java
+++ site-toolkit/pagemodelapi/tests/src/test/java/org/hippoecm/hst/pagemodelapi/common/components/GenericDetailComponent.java
@@ -15,16 +15,66 @@
  */
 package org.hippoecm.hst.pagemodelapi.common.components;
 
+import javax.jcr.Node;
+import javax.jcr.RepositoryException;
+
+import org.hippoecm.hst.content.beans.standard.HippoBean;
 import org.hippoecm.hst.core.component.GenericHstComponent;
 import org.hippoecm.hst.core.component.HstRequest;
 import org.hippoecm.hst.core.component.HstResponse;
+import org.hippoecm.hst.core.linking.HstLink;
+import org.hippoecm.hst.core.linking.HstLinkCreator;
+import org.hippoecm.hst.core.request.HstRequestContext;
+import org.hippoecm.hst.platform.linking.DefaultHstLinkCreator;
+import org.hippoecm.hst.platform.linking.HstLinkImpl;
 
 public class GenericDetailComponent extends GenericHstComponent {
 
     @Override
     public void doBeforeRender(final HstRequest request, final HstResponse response) {
         super.doBeforeRender(request, response);
-        request.setModel("document", request.getRequestContext().getContentBean());
+
+        HstRequestContext requestContext = request.getRequestContext();
+
+        request.setModel("document", requestContext.getContentBean());
+
+        // trigger some HstLinks on the model to make sure HstLinkMixin is triggered and that we do not make
+        // BC incompatible changes to the Page Model API
+
+        final HstLinkCreator hstLinkCreator = requestContext.getHstLinkCreator();
+        final HippoBean requestContentBean = requestContext.getContentBean();
+
+        request.setModel("currentLink", hstLinkCreator.create(requestContentBean, requestContext));
+
+        // include link not found
+        HstLink notFoundLink = new HstLinkImpl(DefaultHstLinkCreator.DEFAULT_PAGE_NOT_FOUND_PATH, requestContext.getResolvedMount().getMount());
+        notFoundLink.setNotFound(true);
+
+        // assert that notFoundLink results in something like in PMA (with type = internal) !!
+        //       "notFoundLink":{
+        //        "href":"/site/spa/pagenotfound",
+        //        "type":"internal"
+        //    },
+        request.setModel("notFoundLink", notFoundLink);
+
+        try {
+            final Node sameChannelNews = requestContext.getSession().getNode("/unittestcontent/documents/unittestproject/News/News1");
+            final Node otherChannelNews = requestContext.getSession().getNode("/unittestcontent/documents/unittestsubproject/News/2008/SubNews1");
+
+            final HstLink sameChannelNewsLink = hstLinkCreator.create(sameChannelNews, requestContext);
+
+            request.setModel("sameChannelNewsLink", sameChannelNewsLink);
+
+            final HstLink otherChannelNewsLink = hstLinkCreator.create(otherChannelNews, requestContext);
+
+            request.setModel("otherChannelNewsLink", otherChannelNewsLink);
+
+
+        } catch (RepositoryException e) {
+            e.printStackTrace();
+        }
+
+
     }
 
 }
