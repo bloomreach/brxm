f4070fb81f50ddf5beda7dbd9db288985be5f607
Michael Dokolin <mikhail.dokolin@bloomreach.com>
2020-02-12 17:15:19 +0100

    CHANNELMGR-2467 Add host communication service

---
+++ channel-manager/frontend-ng/src/app/channel/hippoIframe/spa/communication.service.js
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2020 Hippo B.V. (http://www.onehippo.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+export default class CommunicationService {
+  constructor($rootScope, Penpal) {
+    'ngInject';
+
+    this.$rootScope = $rootScope;
+    this.Penpal = Penpal;
+  }
+
+  async connect({ target, origin }) {
+    this._connection = this.Penpal.connectToChild({
+      childOrigin: origin,
+      iframe: target,
+      methods: {
+        emit: this._emit.bind(this),
+      },
+    });
+
+    this._child = await this._connection.promise;
+  }
+
+  disconnect() {
+    if (!this._connection) {
+      return;
+    }
+
+    this._connection.destroy();
+    delete this._child;
+    delete this._connection;
+  }
+
+  _emit(event, data) {
+    this.$rootScope.$emit(`iframe:${event}`, data);
+  }
+
+  emit(event, data) {
+    if (!this._child) {
+      return;
+    }
+
+    this._child.emit(event, data);
+  }
+}
