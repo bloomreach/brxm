48d6153235a8d6ad5edb3fbd45d35c749d948fe8
Michael Dokolin <mikhail.dokolin@bloomreach.com>
2020-02-21 14:32:19 +0100

    CMS-12646 Rename page structure elements

--- channel-manager/frontend-ng/src/app/channel/hippoIframe/entities/manage-content-link.js
+++ channel-manager/frontend-ng/src/app/channel/hippoIframe/entities/manage-content-link.js
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017-2018 Hippo B.V. (http://www.onehippo.com)
+ * Copyright 2017-2020 Hippo B.V. (http://www.onehippo.com)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-import EmbeddedLink from './embeddedLink';
+import EmbeddedLink from './link-entity';
 
 class ManageContentLink extends EmbeddedLink {
   constructor(commentElement, metaData) {
