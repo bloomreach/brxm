1ae9454cda2f8b0d5640c1e4e25efbea7982966e
Michael Dokolin <mikhail.dokolin@bloomreach.com>
2020-02-21 12:24:25 +0100

    CMS-12646 Add page meta-data entities

---
+++ channel-manager/frontend-ng/src/app/model/entities/component.js
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2020 Hippo B.V. (http://www.onehippo.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import * as HstConstants from '../constants';
+import { ComponentEntity } from './component-entity';
+
+export class Component extends ComponentEntity {
+  getContainer() {
+    return this.container;
+  }
+
+  setContainer(container) {
+    this.container = container;
+  }
+
+  getType() {
+    return 'component';
+  }
+
+  getRenderVariant() {
+    return this._meta[HstConstants.RENDER_VARIANT] || HstConstants.DEFAULT_RENDER_VARIANT;
+  }
+
+  getReferenceNamespace() {
+    return this._meta[HstConstants.REFERENCE_NAMESPACE];
+  }
+}
