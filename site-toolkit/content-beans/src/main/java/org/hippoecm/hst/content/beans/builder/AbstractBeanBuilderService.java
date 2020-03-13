a18dfa14f831796132638eeba0211cf50969484c
Erdem Karakus <erdem.karakus@bloomreach.com>
2020-03-13 12:48:25 +0100

    CMS-13119 Change enum DocumentType to CmsFieldType
    (cherry picked from commit 28a31622b34f57d8d29b71459cf3d47ed8056a47)

--- site-toolkit/content-beans/src/main/java/org/hippoecm/hst/content/beans/builder/AbstractBeanBuilderService.java
+++ site-toolkit/content-beans/src/main/java/org/hippoecm/hst/content/beans/builder/AbstractBeanBuilderService.java
@@ -51,9 +51,9 @@ public abstract class AbstractBeanBuilderService {
                 continue;
             }
 
-            final DocumentType documentType = getPropertyDocumentType(property.getType(), property.getCmsType());
+            final CmsFieldType cmsFieldType = getCmsFieldType(property.getType(), property.getCmsType());
 
-            switch (documentType) {
+            switch (cmsFieldType) {
             case STRING:
             case HTML:
             case PASSWORD:
@@ -106,9 +106,10 @@ public abstract class AbstractBeanBuilderService {
                 continue;
             }
 
-            final DocumentType documentType = getChildNodeDocumentType(childNode.getType(), childNode.hasContentBlocks());
+            final CmsFieldType cmsFieldType = childNode.hasContentBlocks() ?
+                    CmsFieldType.CONTENT_BLOCKS : CmsFieldType.getCmsFieldType(childNode.getType());
 
-            switch (documentType) {
+            switch (cmsFieldType) {
             case HIPPO_HTML:
                 addBeanMethodHippoHtml(propertyName, methodName, multiple, builder);
                 break;
@@ -138,46 +139,22 @@ public abstract class AbstractBeanBuilderService {
     }
 
     /**
-     * Gets the corresponding document type enum value of a given document type by checking
-     * a possible docbase definition
+     * Gets the corresponding cms field type enum value of the given document type
      * 
      * @param type of the document
      * @param cmsType itemType of the {@link org.onehippo.cms7.services.contenttype.ContentTypeProperty}
      * @return the corresponding document type
      */
-    private DocumentType getPropertyDocumentType(final String type, final String cmsType) {
-        DocumentType documentType = DocumentType.getDocumentType(type);
+    private CmsFieldType getCmsFieldType(final String type, final String cmsType) {
+        CmsFieldType cmsFieldType = CmsFieldType.getCmsFieldType(type);
 
-        // if a document type is a string type, then the cms/item type check
+        // if a cms field type is a string type, then the cms/item type check
         // should be made to figure out the actual type of the document
-        if (documentType == DocumentType.STRING) {
-            documentType = DocumentType.getDocumentType(cmsType);
+        if (cmsFieldType == CmsFieldType.STRING) {
+            cmsFieldType = CmsFieldType.getCmsFieldType(cmsType);
         }
 
-        return documentType;
-    }
-
-    /**
-     * Gets the corresponding document type enum value of a given document type by checking
-     * a possible content block definition
-     * 
-     * @param type of the document
-     * @param hasContentBlocks whether a contentType has any content blocks or not
-     * @return the corresponding document type
-     */
-    private DocumentType getChildNodeDocumentType(final String type, final boolean hasContentBlocks) {
-        final DocumentType documentType = DocumentType.getDocumentType(type);
-
-        // if a document type doesn't match with any predefined document type, then
-        // a content block definition check must be made to figure out whether the
-        // document type is a content block
-        if (DocumentType.UNKNOWN == documentType) {
-            if (hasContentBlocks) {
-                return DocumentType.CONTENT_BLOCKS;
-            }
-        }
-
-        return documentType;
+        return cmsFieldType;
     }
 
     /**
