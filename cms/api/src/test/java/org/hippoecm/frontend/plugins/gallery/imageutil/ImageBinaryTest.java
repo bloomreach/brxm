21c9f33a3c5b844a460127029f1aa57c2930b1d5
Ate Douma <ate.douma@bloomreach.com>
2020-04-30 22:43:09 +0200

    CMS-13333 remove usage and dependency on metadata-extractor
    - backport of CMS-7459 removal of deprecated ImageMetaData classes (moved to ImageBinary class)
    
      this (only) reverts the earlier revert commit 535ce4d3f947a9d973337fb03d29d5cccc795dba
    
    - backport of CMS-12520 Update image library from Sanselan to commons-imaging
    
      (cherry picked from commit a6d57983670e35f07dd6d98b2e1548cc30c814b0)
    
    - backport of CMS-13333 remove usage and dependency on metadata-extractor
    
        (cherry picked from commit 14c15e498b5060ddfe2a723eb34bd7d3f0d55785)
        (cherry picked from commit daf526d63d7c6753d33c1aadbfbfaf23098181ab)

--- cms/api/src/test/java/org/hippoecm/frontend/plugins/gallery/imageutil/ImageBinaryTest.java
+++ cms/api/src/test/java/org/hippoecm/frontend/plugins/gallery/imageutil/ImageBinaryTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013-2015 Hippo B.V. (http://www.onehippo.com)
+ * Copyright 2013-2020 Hippo B.V. (http://www.onehippo.com)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -23,7 +23,7 @@ import javax.jcr.Binary;
 import javax.jcr.Node;
 import javax.jcr.RepositoryException;
 
-import org.apache.sanselan.ImageReadException;
+import org.apache.commons.imaging.ImageReadException;
 import org.apache.wicket.util.io.IOUtils;
 import org.hippoecm.frontend.editor.plugins.resource.MimeTypeHelper;
 import org.hippoecm.frontend.plugins.gallery.model.GalleryException;
