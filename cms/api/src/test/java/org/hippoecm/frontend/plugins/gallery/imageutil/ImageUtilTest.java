fea0bd24e21e79d004a774696c35a2fa4c53bc09
Ate Douma <ate.douma@bloomreach.com>
2020-04-30 22:43:09 +0200

    CMS-13333 remove usage and dependency on metadata-extractor
    - backport of CMS-7459 removal of deprecated ImageMetaData classes (moved to ImageBinary class)
    
      this (only) reverts the earlier revert commit 535ce4d3f947a9d973337fb03d29d5cccc795dba
    
    - backport of CMS-12520 Update image library from Sanselan to commons-imaging
    
      (cherry picked from commit 7ad6c39a7c5b91bfb1fe4b085196c07838b063bf)
    
    - backport of CMS-13333 remove usage and dependency on metadata-extractor
    
        (cherry picked from commit 14c15e498b5060ddfe2a723eb34bd7d3f0d55785)
        (cherry picked from commit daf526d63d7c6753d33c1aadbfbfaf23098181ab)

--- cms/api/src/test/java/org/hippoecm/frontend/plugins/gallery/imageutil/ImageUtilTest.java
+++ cms/api/src/test/java/org/hippoecm/frontend/plugins/gallery/imageutil/ImageUtilTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2013 Hippo B.V. (http://www.onehippo.com)
+ * Copyright 2012-2020 Hippo B.V. (http://www.onehippo.com)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,12 +19,12 @@ package org.hippoecm.frontend.plugins.gallery.imageutil;
 import java.io.IOException;
 import java.io.InputStream;
 
-import org.apache.sanselan.ImageInfo;
-import org.apache.sanselan.ImageReadException;
-import org.apache.sanselan.Sanselan;
+import org.apache.commons.imaging.ImageInfo;
+import org.apache.commons.imaging.ImageReadException;
 import org.apache.wicket.util.io.IOUtils;
 import org.junit.Test;
 
+import static org.apache.commons.imaging.Imaging.getImageInfo;
 import static org.junit.Assert.assertTrue;
 
 public class ImageUtilTest {
@@ -45,32 +45,12 @@ public class ImageUtilTest {
         assertTrue(isRGB(rgb, "test-YCCK.jpg"));
     }
 
-    @Test
-    @Deprecated
-    public void convertYCCKtoRGBUsingDeprecatedMethod() throws Exception {
-        InputStream ycck = getClass().getResourceAsStream("/test-YCCK.jpg");
-        InputStream rgb = ImageUtils.convertToRGB(ycck, ImageMetaData.ColorModel.YCCK);
-
-        assertTrue(isRGB(rgb, "test-YCCK.jpg"));
-    }
-
-    @Test
-    @Deprecated
-    public void convertCMYKToRGBUsingDeprecatedMethod() throws Exception {
-        InputStream cmyk = getClass().getResourceAsStream("/test-CMYK.jpg");
-        InputStream rgb = ImageUtils.convertToRGB(cmyk, ImageMetaData.ColorModel.CMYK);
-
-        assertTrue(isRGB(rgb, "test-CMYK.jpg"));
-    }
-
     public static boolean isRGB(final InputStream stream, final String fileName) throws IOException, ImageReadException {
         try {
-            ImageInfo info = Sanselan.getImageInfo(stream, fileName);
-            return ImageInfo.COLOR_TYPE_RGB == info.getColorType();
+            ImageInfo info = getImageInfo(stream, fileName);
+            return ImageInfo.ColorType.RGB == info.getColorType() || ImageInfo.ColorType.YCbCr == info.getColorType();
         } finally {
             IOUtils.closeQuietly(stream);
         }
     }
-
-
 }
