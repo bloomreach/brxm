bc06b4c96ae80e3cc9fb3c9de21a64988b561f1b
Erdem Karakus <erdem.karakus@bloomreach.com>
2020-05-01 12:27:16 +0200

    CMS-12875 [Backport 14.2] Harden fetching and removing properties
    Query limit is set to static value 1000 to prevent any performance
    issues. Migrator will not throw any exception, caller class will handle
    exceptions instead.
    
    (cherry picked from commit 640849da2af9b9203a6d4e138c6e23b0f89fb8d5)

--- repository/engine/src/main/java/org/onehippo/cm/engine/migrator/components/ContainerComponentPropertyMigrator.java
+++ repository/engine/src/main/java/org/onehippo/cm/engine/migrator/components/ContainerComponentPropertyMigrator.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2019 Hippo B.V. (http://www.onehippo.com)
+ *  Copyright 2019-2020 Hippo B.V. (http://www.onehippo.com)
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -16,6 +16,7 @@
 package org.onehippo.cm.engine.migrator.components;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -28,9 +29,8 @@ import javax.jcr.query.Query;
 import javax.jcr.query.QueryManager;
 import javax.jcr.query.QueryResult;
 
-import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.lang3.ArrayUtils;
 import org.onehippo.cm.engine.migrator.ConfigurationMigrator;
-import org.onehippo.cm.engine.migrator.MigrationException;
 import org.onehippo.cm.engine.migrator.PostMigrator;
 import org.onehippo.cm.model.ConfigurationModel;
 import org.slf4j.Logger;
@@ -40,41 +40,64 @@ import org.slf4j.LoggerFactory;
 public class ContainerComponentPropertyMigrator implements ConfigurationMigrator {
 
     private static final Logger log = LoggerFactory.getLogger(ContainerComponentPropertyMigrator.class);
+    private static final String HST_NAMESPACE = "hst";
     private static final String PARENT_NODE_TYPE = "hst:containercomponent";
     private static final String STANDARD_CONTAINER_COMPONENT_PROPERTY_NAME = "hst:componentclassname";
     private static final String OLD_STANDARD_CONTAINER_COMPONENT_PATH = "org.hippoecm.hst.pagecomposer.builtin.components.StandardContainerComponent";
     private static final String NEW_STANDARD_CONTAINER_COMPONENT_PATH = "org.hippoecm.hst.builtin.components.StandardContainerComponent";
+    private static final int QUERY_LIMIT = 1000;
 
     @Override
-    public boolean migrate(final Session session, final ConfigurationModel configurationModel, final boolean autoExportEnabled) throws RepositoryException {
-        try {
-            return doMigrate(session, configurationModel, autoExportEnabled);
-        } catch (final RepositoryException e) {
-            throw new MigrationException("ContainerComponentPropertyMigrator failed.", e);
-        }
-    }
+    public boolean migrate(final Session session, final ConfigurationModel configurationModel, final boolean autoExportEnabled)
+            throws RepositoryException {
 
-    private boolean doMigrate(final Session session, final ConfigurationModel configurationModel, final boolean autoExportEnabled) throws RepositoryException {
-
-        final List<Property> containerComponentPropertyNodes = getContainerComponentPropertyNodes(
-                session.getWorkspace().getQueryManager());
-        if (CollectionUtils.isEmpty(containerComponentPropertyNodes)) {
-            log.info("No container component property found with the default values.");
+        final String[] registeredNamespaces = session.getWorkspace().getNamespaceRegistry().getPrefixes();
+        if (ArrayUtils.isEmpty(registeredNamespaces) || !Arrays.asList(registeredNamespaces).contains(HST_NAMESPACE)) {
             return false;
         }
 
-        log.info("{} unnecessary default container component values have been found in repository.", containerComponentPropertyNodes.size());
-        final boolean success = cleanComponentsConfiguration(containerComponentPropertyNodes);
-        if (!success) {
-            throw new MigrationException("Could not clean unnecessary configurations for migrator ContainerComponentPropertyMigrator");
+        return migrateContainerComponentProperties(session);
+    }
+
+    private boolean migrateContainerComponentProperties(final Session session) throws RepositoryException {
+
+        final QueryManager queryManager = session.getWorkspace().getQueryManager();
+
+        boolean anyPropertyMigrated = false;
+        int migratedPropertyCount = 0;
+        while (true) {
+
+            final List<Property> containerComponentProperties = fetchContainerComponentProperties(queryManager);
+            if (containerComponentProperties.size() == 0) {
+                // finish the migration when all records are migrated
+                break;
+            }
+
+            migratedPropertyCount = migratedPropertyCount + containerComponentProperties.size();
+
+            final boolean propertiesMigrated = removeContainerComponentProperties(containerComponentProperties);
+            if (!propertiesMigrated) {
+                // An error occured, do not continue
+                log.error("Could not clean unnecessary configurations for migrator ContainerComponentPropertyMigrator.");
+                return false;
+            }
+
+            session.save();
+
+            anyPropertyMigrated = true;
         }
 
-        session.save();
+        if (!anyPropertyMigrated) {
+            return false;
+        }
+
+        log.info("{} unnecessary default container component values have been deleted.", migratedPropertyCount);
+
         return true;
     }
 
-    private boolean cleanComponentsConfiguration(final List<Property> componentComponentPropertyNodes) {
-        for (Property property : componentComponentPropertyNodes) {
+    private boolean removeContainerComponentProperties(final List<Property> containerComponentProperties) {
+        for (Property property : containerComponentProperties) {
             try {
                 property.remove();
             } catch (final RepositoryException e) {
@@ -86,14 +109,16 @@ public class ContainerComponentPropertyMigrator implements ConfigurationMigrator
     }
 
     @SuppressWarnings("deprecation")
-    private List<Property> getContainerComponentPropertyNodes(final QueryManager queryManager) throws RepositoryException {
+    private List<Property> fetchContainerComponentProperties(final QueryManager queryManager) throws RepositoryException {
 
-        final Query query = queryManager.createQuery("//element(*, " + PARENT_NODE_TYPE + ")"
-                + "[@" + STANDARD_CONTAINER_COMPONENT_PROPERTY_NAME + "='" + OLD_STANDARD_CONTAINER_COMPONENT_PATH + "' or "
-                + "@" + STANDARD_CONTAINER_COMPONENT_PROPERTY_NAME + "='" + NEW_STANDARD_CONTAINER_COMPONENT_PATH + "']", Query.XPATH);
+        final Query query = queryManager.createQuery("//element(*, " + PARENT_NODE_TYPE + ")" + "[@"
+                + STANDARD_CONTAINER_COMPONENT_PROPERTY_NAME + "='" + OLD_STANDARD_CONTAINER_COMPONENT_PATH + "' or " + "@"
+                + STANDARD_CONTAINER_COMPONENT_PROPERTY_NAME + "='" + NEW_STANDARD_CONTAINER_COMPONENT_PATH + "']", Query.XPATH);
+        query.setLimit(QUERY_LIMIT);
+
         final QueryResult queryResult = query.execute();
-
         final NodeIterator nodes = queryResult.getNodes();
+
         if (nodes.getSize() == 0) {
             return Collections.emptyList();
         }
