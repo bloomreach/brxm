8e5902021283d916f64ea180f2fdd690cfed892e
Arthur Bogaart <arthur.bogaart@bloomreach.com>
2020-05-19 10:48:22 +0200

    CMS-12494 Only schedule "collapse" call if any menu visible

--- cms/engine/src/main/java/org/hippoecm/frontend/behaviors/contextmenu.js
+++ cms/engine/src/main/java/org/hippoecm/frontend/behaviors/contextmenu.js
@@ -1,5 +1,5 @@
 /*
- * Copyright 2008-2019 Hippo B.V. (http://www.onehippo.com)
+ * Copyright 2008-2020 Hippo B.V. (http://www.onehippo.com)
  *
  * Licensed under the Apache License, Version 2.0 (the  "License");
  * you may not use this file except in compliance with the License.
@@ -62,6 +62,10 @@
 
   Hippo.ContextMenu.isShown = function (id) {
     return menus.has(id);
+  };
+
+  Hippo.ContextMenu.isVisible = function () {
+    return menus.size > 0;
   };
 
   Hippo.ContextMenu.renderInTree = function (id) {
