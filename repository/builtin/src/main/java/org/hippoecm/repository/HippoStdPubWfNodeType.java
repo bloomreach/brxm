a2299b2d1c2aff587c73c83536f202b266a74703
Michiel Eggermont <michiel.eggermont@bloomreach.com>
2020-05-20 09:00:19 +0200

    CMS-13359 Audit trace changes without scxml changes
    We can't make backwards incompatible changes to scxml in
    a minor release. So we should implement audit tracing in
    the code. This is done as follows:
    If auditTracing mixin is present then call version action
    in DocumentWorkflowImpl#commitEditableInstance().

--- repository/builtin/src/main/java/org/hippoecm/repository/HippoStdPubWfNodeType.java
+++ repository/builtin/src/main/java/org/hippoecm/repository/HippoStdPubWfNodeType.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2010-2014 Hippo B.V. (http://www.onehippo.com)
+ *  Copyright 2010-2020 Hippo B.V. (http://www.onehippo.com)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -29,6 +29,7 @@ public interface HippoStdPubWfNodeType {
     String HIPPOSTDPUBWF_USERNAME = "hippostdpubwf:username";
     String HIPPOSTDPUBWF_REQDATE = "hippostdpubwf:reqdate";
     String HIPPOSTDPUBWF_REASON = "hippostdpubwf:reason";
+    String MIXIN_HIPPOSTDPUBWF_AUDIT_TRACE = "hippostdpubwf:auditTrace";
 
     String REJECTED = "rejected";
     String PUBLISH = "publish";
