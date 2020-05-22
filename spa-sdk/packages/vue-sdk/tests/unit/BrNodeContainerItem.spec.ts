8080a7ad56a3dafb78ec43df5fb38756020949ee
Michael Dokolin <mikhail.dokolin@bloomreach.com>
2020-05-22 13:09:47 +0200

    CMS-13165 Add tests for BrNodeContainerItem component

---
+++ spa-sdk/packages/vue-sdk/tests/unit/BrNodeContainerItem.spec.ts
@@ -0,0 +1,132 @@
+/*
+ * Copyright 2020 Hippo B.V. (http://www.onehippo.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { Wrapper, shallowMount } from '@vue/test-utils';
+import { Component, Prop, ProvideReactive, Provide, Vue } from 'vue-property-decorator';
+import { ContainerItem, Page } from '@bloomreach/spa-sdk';
+import BrNodeContainerItem from '@/BrNodeContainerItem.vue';
+
+@Component({ template: '<div />' })
+class BrPage extends Vue {
+  @Prop() component!: ContainerItem;
+
+  @Provide() component$() {
+    return this.component;
+  }
+
+  @ProvideReactive() @Prop() mapping!: Record<string, Vue.Component>;
+
+  @ProvideReactive() @Prop() page!: Page;
+}
+
+@Component
+class SomeContainerItem extends Vue {
+  @Prop() component!: ContainerItem;
+
+  @Prop() page!: Page;
+}
+
+describe('BrNodeContainerItem', () => {
+  let component: jest.Mocked<ContainerItem>;
+  let mapping: Record<string, Vue.Component>;
+  let page: jest.Mocked<Page>;
+  let provide: Function;
+  let shallowMountComponent: () => Wrapper<Vue>;
+  let parent: Wrapper<Vue>;
+
+  beforeAll(() => {
+    component = ({
+      getType: jest.fn(),
+      on: jest.fn(),
+    } as unknown) as typeof component;
+    mapping = { something: SomeContainerItem };
+    page = ({ sync: jest.fn() } as unknown) as typeof page;
+
+    parent = shallowMount(BrPage, { propsData: { component, mapping, page } });
+    provide = (parent.vm.$options.provide as Function).bind(parent.vm);
+
+    shallowMountComponent = () => shallowMount(BrNodeContainerItem, { provide });
+  });
+
+  describe('render', () => {
+    it('should render a mapped container item', async () => {
+      component.getType.mockReturnValue('something');
+
+      const wrapper = shallowMountComponent();
+      await wrapper.vm.$nextTick();
+
+      const props = wrapper.findComponent(SomeContainerItem).props();
+
+      expect(wrapper.html()).toMatchSnapshot();
+      expect(props.component).toBe(component);
+      expect(props.page).toEqual(page);
+    });
+
+    it('should render an undefined container item', async () => {
+      component.getType.mockReturnValue('undefined');
+
+      const wrapper = shallowMountComponent();
+      await wrapper.vm.$nextTick();
+
+      expect(wrapper.html()).toMatchSnapshot();
+    });
+  });
+
+  describe('destroyed', () => {
+    it('should unsubscribe from update event on component destroy', async () => {
+      const unsubscribe = jest.fn();
+      component.on.mockReturnValue(unsubscribe);
+
+      const wrapper = shallowMountComponent();
+      await wrapper.vm.$nextTick();
+      wrapper.destroy();
+
+      expect(unsubscribe).toBeCalled();
+    });
+  });
+
+  describe('update', () => {
+    it('should subscribe for update event', async () => {
+      const wrapper = shallowMountComponent();
+      await wrapper.vm.$nextTick();
+
+      expect(component.on).toBeCalledWith('update', expect.any(Function));
+    });
+
+    it('should unsubscribe from update event on component change', async () => {
+      const unsubscribe = jest.fn();
+      component.on.mockReturnValue(unsubscribe);
+
+      const wrapper = shallowMountComponent();
+      await wrapper.vm.$nextTick();
+
+      parent.setProps({ component: { ...component } });
+      await wrapper.vm.$nextTick();
+
+      expect(unsubscribe).toBeCalled();
+    });
+
+    it('should sync page on update event', async () => {
+      const wrapper = shallowMountComponent();
+      await wrapper.vm.$nextTick();
+
+      const [[, listener]] = component.on.mock.calls;
+      listener({});
+
+      expect(page.sync).toBeCalled();
+    });
+  });
+});
