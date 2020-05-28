2a4060b242888eaea24f0d6638cffcc40e41c71b
Arthur Bogaart <arthur.bogaart@bloomreach.com>
2020-05-28 11:33:04 +0200

    CMS-13373 [Backport 14.x] Container is disabled if it is not editable

--- channel-manager/frontend-ng/src/app/model/entities/container.spec.js
+++ channel-manager/frontend-ng/src/app/model/entities/container.spec.js
@@ -62,6 +62,7 @@ describe('Container', () => {
     beforeEach(() => {
       container = new Container();
       spyOn(container, 'isInherited');
+      spyOn(container, 'isEditable');
       spyOn(container, 'isLocked');
       spyOn(container, 'isLockedByCurrentUser');
     });
@@ -72,8 +73,16 @@ describe('Container', () => {
       expect(container.isDisabled()).toBe(true);
     });
 
+    it('should return true when the container is not editable', () => {
+      container.isInherited.and.returnValue(false);
+      container.isEditable.and.returnValue(false);
+
+      expect(container.isDisabled()).toBe(true);
+    });
+
     it('should return true when the container is locked', () => {
       container.isInherited.and.returnValue(false);
+      container.isEditable.and.returnValue(true);
       container.isLocked.and.returnValue(true);
       container.isLockedByCurrentUser.and.returnValue(false);
 
@@ -82,13 +91,15 @@ describe('Container', () => {
 
     it('should return false when the container is not locked', () => {
       container.isInherited.and.returnValue(false);
+      container.isEditable.and.returnValue(true);
       container.isLocked.and.returnValue(false);
 
       expect(container.isDisabled()).toBe(false);
     });
 
-    it('should return true when the container is locked by the current user', () => {
+    it('should return false when the container is locked by the current user', () => {
       container.isInherited.and.returnValue(false);
+      container.isEditable.and.returnValue(true);
       container.isLocked.and.returnValue(true);
       container.isLockedByCurrentUser.and.returnValue(true);
 
