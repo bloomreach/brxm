e3d386b5f4de34f383ff4a130d9ff41bb0df09d9
Eduardo Perez <eduardo.perez@bloomreach.com>
2020-06-08 10:03:25 +0200

    CMS-13463 Remove solr

--- testsuite/components/src/main/java/org/hippoecm/hst/demo/servlet/DateTimeServlet.java
+++ testsuite/components/src/main/java/org/hippoecm/hst/demo/servlet/DateTimeServlet.java
@@ -44,17 +44,21 @@ public class DateTimeServlet  extends HttpServlet {
     
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        PrintWriter out = response.getWriter();
-        String line = null;
-        Date d = new Date();
-        
-        if (formatPattern == null) {
-            line = d.toString();
-        } else {
-            line = new SimpleDateFormat(formatPattern).format(d);
+        try {
+            PrintWriter out = response.getWriter();
+            String line;
+            Date d = new Date();
+
+            if (formatPattern == null) {
+                line = d.toString();
+            } else {
+                line = new SimpleDateFormat(formatPattern).format(d);
+            }
+
+            out.print(line);
+            out.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        
-        out.print(line);
-        out.flush();
     }
 }
