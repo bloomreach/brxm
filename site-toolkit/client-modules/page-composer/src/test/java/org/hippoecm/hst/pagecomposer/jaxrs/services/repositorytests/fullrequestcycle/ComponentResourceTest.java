e16517cc7f6687757682c9eef9e7d81806d9d5ac
Michiel Eggermont <michiel.eggermont@bloomreach.com>
2020-06-24 15:06:31 +0200

    CMS-13416 Improve integration test
    (cherry picked from commit 3eef723893bb88793d67a0cd4421a08b6a194e28)

--- site-toolkit/client-modules/page-composer/src/test/java/org/hippoecm/hst/pagecomposer/jaxrs/services/repositorytests/fullrequestcycle/ComponentResourceTest.java
+++ site-toolkit/client-modules/page-composer/src/test/java/org/hippoecm/hst/pagecomposer/jaxrs/services/repositorytests/fullrequestcycle/ComponentResourceTest.java
@@ -18,36 +18,70 @@ package org.hippoecm.hst.pagecomposer.jaxrs.services.repositorytests.fullrequest
 
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.util.Map;
 
 import javax.jcr.RepositoryException;
 import javax.servlet.ServletException;
 import javax.ws.rs.core.Response;
 
+import org.apache.commons.lang3.tuple.Pair;
 import org.assertj.core.api.Assertions;
 import org.hippoecm.hst.pagecomposer.jaxrs.model.ActionsRepresentation;
+import org.hippoecm.hst.pagecomposer.jaxrs.services.action.Category;
+import org.hippoecm.hst.pagecomposer.jaxrs.services.action.HstAction;
 import org.junit.Test;
 import org.springframework.mock.web.MockHttpServletResponse;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.google.common.collect.ImmutableMap;
 
+import static java.util.stream.Collectors.toMap;
+import static org.hippoecm.hst.pagecomposer.jaxrs.services.action.HstAction.CHANNEL_CLOSE;
+import static org.hippoecm.hst.pagecomposer.jaxrs.services.action.HstAction.CHANNEL_CONFIRM;
+import static org.hippoecm.hst.pagecomposer.jaxrs.services.action.HstAction.CHANNEL_DELETE;
+import static org.hippoecm.hst.pagecomposer.jaxrs.services.action.HstAction.CHANNEL_DISCARD_CHANGES;
+import static org.hippoecm.hst.pagecomposer.jaxrs.services.action.HstAction.CHANNEL_MANAGE_CHANGED;
+import static org.hippoecm.hst.pagecomposer.jaxrs.services.action.HstAction.CHANNEL_PUBLISH;
+import static org.hippoecm.hst.pagecomposer.jaxrs.services.action.HstAction.CHANNEL_SETTINGS;
+import static org.hippoecm.hst.pagecomposer.jaxrs.services.action.HstAction.PAGE_COPY;
+import static org.hippoecm.hst.pagecomposer.jaxrs.services.action.HstAction.PAGE_DELETE;
+import static org.hippoecm.hst.pagecomposer.jaxrs.services.action.HstAction.PAGE_MOVE;
+import static org.hippoecm.hst.pagecomposer.jaxrs.services.action.HstAction.PAGE_NEW;
+import static org.hippoecm.hst.pagecomposer.jaxrs.services.action.HstAction.PAGE_PROPERTIES;
+import static org.hippoecm.hst.pagecomposer.jaxrs.services.action.HstAction.PAGE_TOOLS;
 import static org.hippoecm.hst.pagecomposer.jaxrs.services.action.HstCategories.channel;
 import static org.hippoecm.hst.pagecomposer.jaxrs.services.action.HstCategories.page;
-import static org.hippoecm.hst.pagecomposer.jaxrs.services.action.HstCategories.xpage;
 
 public class ComponentResourceTest extends AbstractComponentResourceTest {
 
     @Test
     public void test_actions_for_unlocked_page_component() throws RepositoryException, IOException, ServletException {
+
         final String containerTestPageId = getNodeId("/hst:hst/hst:configurations/unittestproject/hst:workspace/hst:pages/containertestpage");
 
         final MockHttpServletResponse response = getActionsRequest(containerTestPageId);
         Assertions.assertThat(response.getStatus()).isEqualTo(Response.Status.OK.getStatusCode());
 
-        final ActionsRepresentation actions = getActions(response);
-        Assertions.assertThat(actions.getActions())
-                .isNotEmpty()
-                .containsKeys(channel().getName(), page().getName())
-                .doesNotContainKey(xpage().getName());
+        final Map<String, Boolean> actions = flatten(getActions(response));
+
+        final Map<String, Boolean> expectedActionItems = ImmutableMap.<String, Boolean>builder()
+                .put(key(channel(), CHANNEL_CLOSE), true)
+                .put(key(channel(), CHANNEL_CONFIRM), true)
+                .put(key(channel(), CHANNEL_DELETE), true)
+                .put(key(channel(), CHANNEL_DISCARD_CHANGES), true)
+                .put(key(channel(), CHANNEL_MANAGE_CHANGED), true)
+                .put(key(channel(), CHANNEL_PUBLISH), true)
+                .put(key(channel(), CHANNEL_SETTINGS), true)
+                .put(key(page(), PAGE_COPY), true)
+                .put(key(page(), PAGE_DELETE), true)
+                .put(key(page(), PAGE_MOVE), true)
+                .put(key(page(), PAGE_NEW), true)
+                .put(key(page(), PAGE_PROPERTIES), true)
+                .put(key(page(), PAGE_TOOLS), true)
+                .build();
+        Assertions.assertThat(actions)
+                .describedAs("A page component request contains all channel and page components")
+                .isEqualTo(expectedActionItems);
     }
 
     private MockHttpServletResponse getActionsRequest(String containerId) throws RepositoryException, IOException, ServletException {
@@ -63,6 +97,16 @@ public class ComponentResourceTest extends AbstractComponentResourceTest {
 
     private ActionsRepresentation getActions(MockHttpServletResponse response) throws UnsupportedEncodingException, JsonProcessingException {
         return mapper.readValue(response.getContentAsString(), ActionsRepresentation.class);
+    }
+
+    private Map<String, Boolean> flatten(ActionsRepresentation actionsRepresentation) {
+        return actionsRepresentation.getActions().entrySet().stream()
+                .flatMap(ec -> ec.getValue().getItems().entrySet().stream().map(ea -> Pair.of(ec.getKey() + "." + ea.getKey(), ea.getValue().isEnabled())))
+                .collect(toMap(Pair::getLeft, Pair::getRight));
+    }
+
+    private static String key(Category category, HstAction action) {
+        return category.getName() + "." + action.getName();
     }
 
 }
