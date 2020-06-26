14005b6353de9ab612a673524d70110ae41ae21b
Michiel Rop <michiel.rop@bloomreach.com>
2020-06-26 14:02:34 +0200

    CMS-13433 Improve maintainability
    Improve maintainability based on comments from Arthur Bogaart.
    
    Call xpageLayoutProvider.getXPageLayouts in loadableDetachableModel, so
    the provider will only be called if the folder is actually a xpage
    folder.
    
    (cherry picked from commit f1b5d457f000e6881ba79be8800f57599e905d10)

--- cms/builtin/src/main/java/org/hippoecm/frontend/plugins/standardworkflow/xpagelayout/XPageLayoutContainer.java
+++ cms/builtin/src/main/java/org/hippoecm/frontend/plugins/standardworkflow/xpagelayout/XPageLayoutContainer.java
@@ -16,37 +16,36 @@
 package org.hippoecm.frontend.plugins.standardworkflow.xpagelayout;
 
 import java.util.List;
-import java.util.Optional;
+
 
 import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.DropDownChoice;
 import org.apache.wicket.markup.html.form.IChoiceRenderer;
 import org.apache.wicket.model.IModel;
-import org.apache.wicket.model.StringResourceModel;
-import org.hippoecm.frontend.model.SerializableSupplier;
+import org.hippoecm.frontend.l10n.ResourceBundleModel;
 import org.onehippo.cms7.services.hst.IXPageLayout;
 
 public class XPageLayoutContainer extends WebMarkupContainer {
 
+    public static final String HIPPO_CMS_XPAGES = "hippo:cms.xpage";
+
     public XPageLayoutContainer(final String id, final IModel<IXPageLayout> xPageLayoutModel
-            , final SerializableSupplier<List<IXPageLayout>> xPageLayoutProvider) {
+            , final IModel<List<IXPageLayout>> xPageLayoutListModel) {
         super(id, xPageLayoutModel);
-        final IModel<String> xPageLayoutLabelModel = new StringResourceModel("xpage-layout",
-                this);
+        final IModel<String> xPageLayoutLabelModel = ResourceBundleModel.of(HIPPO_CMS_XPAGES,"x-page-layout");
         final Label xPageLayoutLabel = new Label("xpage-layout-label", xPageLayoutLabelModel);
         add(xPageLayoutLabel);
-        final XPageLayoutListModel XPageLayoutListModel = new XPageLayoutListModel(xPageLayoutProvider);
-        final DropDownChoice<IXPageLayout> pageLayoutDropDownChoice = new DropDownChoice<>(
+        final DropDownChoice<IXPageLayout> pageLayoutDropDownChoice = new DropDownChoice<IXPageLayout>(
                 "xpage-layout-drop-down-choice",
                 xPageLayoutModel,
-                XPageLayoutListModel,
+                xPageLayoutListModel,
                 choiceRenderer);
         pageLayoutDropDownChoice.setRequired(true);
         pageLayoutDropDownChoice.setNullValid(false);
         pageLayoutDropDownChoice.setLabel(xPageLayoutLabelModel);
         add(pageLayoutDropDownChoice);
-        setVisible(!xPageLayoutProvider.get().isEmpty());
+        setVisible(!xPageLayoutListModel.getObject().isEmpty());
     }
 
     IChoiceRenderer<IXPageLayout> choiceRenderer =
@@ -63,12 +62,10 @@ public class XPageLayoutContainer extends WebMarkupContainer {
 
                 @Override
                 public IXPageLayout getObject(final String id, final IModel<? extends List<? extends IXPageLayout>> choices) {
-                    final Optional<? extends IXPageLayout> first =
-                            choices.getObject().stream().filter(iPageLayout -> iPageLayout.getKey().equals(id)).findFirst();
-                    if (first.isPresent()) {
-                        return first.get();
-                    }
-                    return null;
+                    return choices.getObject().
+                            stream().
+                            filter(ixPageLayout -> ixPageLayout.getKey().equals(id)).findFirst().
+                            orElse(null);
                 }
             };
 
