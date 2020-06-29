01aab82f49c89205839895d9e9429a8989ef2a9b
Michiel Eggermont <michiel.eggermont@bloomreach.com>
2020-06-29 10:00:05 +0200

    CMS-13416 Disable page category for xpage request
    A request cannot be for a page and an xpage at the
    same time. Note that the CXFJaxrsHstConfigService
    modified the path by prefixing it with experiencepage
    if the request is for an experience page. However,
    the prefix should not be added for requests for the
    ComponentResource.
    
    (cherry picked from commit 15e208f033fe36201b5c67283f1e9812bab10259)

--- site-toolkit/client-modules/page-composer/src/test/java/org/hippoecm/hst/pagecomposer/jaxrs/services/repositorytests/fullrequestcycle/ComponentResourceTest.java
+++ site-toolkit/client-modules/page-composer/src/test/java/org/hippoecm/hst/pagecomposer/jaxrs/services/repositorytests/fullrequestcycle/ComponentResourceTest.java
@@ -20,16 +20,25 @@ import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.util.Map;
 
+import javax.jcr.Node;
 import javax.jcr.RepositoryException;
+import javax.jcr.Session;
 import javax.servlet.ServletException;
 import javax.ws.rs.core.Response;
 
 import org.apache.commons.lang3.tuple.Pair;
 import org.assertj.core.api.Assertions;
+import org.hippoecm.hst.configuration.HstNodeTypes;
 import org.hippoecm.hst.pagecomposer.jaxrs.model.ActionsRepresentation;
 import org.hippoecm.hst.pagecomposer.jaxrs.services.action.Category;
 import org.hippoecm.hst.pagecomposer.jaxrs.services.action.HstAction;
+import org.hippoecm.repository.api.HippoSession;
+import org.hippoecm.repository.api.WorkflowException;
+import org.hippoecm.repository.api.WorkflowManager;
+import org.hippoecm.repository.util.JcrUtils;
+import org.hippoecm.repository.util.NodeIterable;
 import org.junit.Test;
+import org.onehippo.repository.documentworkflow.DocumentWorkflow;
 import org.springframework.mock.web.MockHttpServletResponse;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -49,8 +58,14 @@ import static org.hippoecm.hst.pagecomposer.jaxrs.services.action.HstAction.PAGE
 import static org.hippoecm.hst.pagecomposer.jaxrs.services.action.HstAction.PAGE_NEW;
 import static org.hippoecm.hst.pagecomposer.jaxrs.services.action.HstAction.PAGE_PROPERTIES;
 import static org.hippoecm.hst.pagecomposer.jaxrs.services.action.HstAction.PAGE_TOOLS;
+import static org.hippoecm.hst.pagecomposer.jaxrs.services.action.HstAction.XPAGE_DELETE;
+import static org.hippoecm.hst.pagecomposer.jaxrs.services.action.HstAction.XPAGE_MOVE;
+import static org.hippoecm.hst.pagecomposer.jaxrs.services.action.HstAction.XPAGE_NEW;
 import static org.hippoecm.hst.pagecomposer.jaxrs.services.action.HstCategories.channel;
 import static org.hippoecm.hst.pagecomposer.jaxrs.services.action.HstCategories.page;
+import static org.hippoecm.hst.pagecomposer.jaxrs.services.action.HstCategories.xpage;
+import static org.hippoecm.repository.HippoStdNodeType.HIPPOSTD_STATE;
+import static org.hippoecm.repository.HippoStdNodeType.UNPUBLISHED;
 
 public class ComponentResourceTest extends AbstractComponentResourceTest {
 
@@ -84,6 +99,70 @@ public class ComponentResourceTest extends AbstractComponentResourceTest {
                 .isEqualTo(expectedActionItems);
     }
 
+    @Test
+    public void test_actions_for_locked_page_component() throws RepositoryException, IOException, ServletException {
+
+        final String containerTestPageId = getNodeId("/hst:hst/hst:configurations/unittestproject/hst:workspace/hst:pages/containertestpage");
+        final Session session = createSession(ADMIN_CREDENTIALS);
+        session.getNodeByIdentifier(getNodeId("/hst:hst/hst:configurations/unittestproject")).setProperty(HstNodeTypes.CONFIGURATION_PROPERTY_LOCKED, true);
+        session.save();
+        session.logout();
+
+        final MockHttpServletResponse response = getActionsRequest(containerTestPageId);
+        Assertions.assertThat(response.getStatus()).isEqualTo(Response.Status.OK.getStatusCode());
+
+        final Map<String, Boolean> actions = flatten(getActions(response));
+
+        final Map<String, Boolean> expectedActionItems = ImmutableMap.<String, Boolean>builder()
+                .put(key(channel(), CHANNEL_CLOSE), true)
+                .put(key(channel(), CHANNEL_CONFIRM), true)
+                .put(key(channel(), CHANNEL_DELETE), true)
+                .put(key(channel(), CHANNEL_DISCARD_CHANGES), true)
+                .put(key(channel(), CHANNEL_MANAGE_CHANGED), true)
+                .put(key(channel(), CHANNEL_PUBLISH), true)
+                .put(key(channel(), CHANNEL_SETTINGS), true)
+                .build();
+        Assertions.assertThat(actions)
+                .describedAs("A page component request contains all channel and page components")
+                .isEqualTo(expectedActionItems);
+    }
+
+    @Test
+    public void test_actions_for_xpage_component() throws RepositoryException, IOException, ServletException, WorkflowException {
+
+        final String xpagePath = "/unittestcontent/documents/unittestproject/experiences/expPage1";
+        final String handleId = getNodeId(xpagePath);
+        final HippoSession hippoSession = (HippoSession) createSession(ADMIN_CREDENTIALS);
+        final WorkflowManager workflowManager = hippoSession.getWorkspace().getWorkflowManager();
+        final DocumentWorkflow documentWorkflow = (DocumentWorkflow) workflowManager.getWorkflow("default", hippoSession.getNodeByIdentifier(handleId));
+        documentWorkflow.depublish();
+        documentWorkflow.publish();
+        final String containerId = getVariant(documentWorkflow.getNode(), UNPUBLISHED).getNode("hst:page/body").getIdentifier();
+        hippoSession.save();
+        hippoSession.logout();
+
+        final MockHttpServletResponse response = getActionsRequest(containerId);
+        Assertions.assertThat(response.getStatus()).isEqualTo(Response.Status.OK.getStatusCode());
+
+        final Map<String, Boolean> actions = flatten(getActions(response));
+
+        final Map<String, Boolean> expectedActionItems = ImmutableMap.<String, Boolean>builder()
+                .put(key(channel(), CHANNEL_CLOSE), true)
+                .put(key(channel(), CHANNEL_CONFIRM), true)
+                .put(key(channel(), CHANNEL_DELETE), true)
+                .put(key(channel(), CHANNEL_DISCARD_CHANGES), true)
+                .put(key(channel(), CHANNEL_MANAGE_CHANGED), true)
+                .put(key(channel(), CHANNEL_PUBLISH), true)
+                .put(key(channel(), CHANNEL_SETTINGS), true)
+                .put(key(xpage(), XPAGE_DELETE), true)
+                .put(key(xpage(), XPAGE_MOVE), true)
+                .put(key(xpage(), XPAGE_NEW), true)
+                .build();
+        Assertions.assertThat(actions)
+                .describedAs("A page component request contains all channel and page components")
+                .isEqualTo(expectedActionItems);
+    }
+
     private MockHttpServletResponse getActionsRequest(String containerId) throws RepositoryException, IOException, ServletException {
 
         final String pathInfo = "/_rp/" + containerId;
@@ -107,6 +186,15 @@ public class ComponentResourceTest extends AbstractComponentResourceTest {
 
     private static String key(Category category, HstAction action) {
         return category.getName() + "." + action.getName();
+    }
+
+    private Node getVariant(final Node handle, final String state) throws RepositoryException, WorkflowException {
+        for (Node variant : new NodeIterable(handle.getNodes(handle.getName()))) {
+            if (state.equals(JcrUtils.getStringProperty(variant, HIPPOSTD_STATE, null))) {
+                return variant;
+            }
+        }
+        return null;
     }
 
 }
