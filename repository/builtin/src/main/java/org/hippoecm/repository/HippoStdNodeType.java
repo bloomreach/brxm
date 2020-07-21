135d4b7866ac6d5a0ffa8873cf01948a9ac9489e
Ard Schrijvers <ard.schrijvers@bloomreach.com>
2020-07-21 14:31:00 +0200

    CMS-13590 [Backoprt 14.3] 'hippostd:channelId' is not a mandatory property
    since hippostd:channelId is not a mandatory property for
    hippostd:xpagefolder it can be missing which would result in a
    path not found exception. Therefore default to null. Next to that
    introduced nodetype/property constants and  hippostd:xpagefolder
    
    (cherry picked from commit cdebb3729a9dd9227ef60617d90343041a69d003)

--- repository/builtin/src/main/java/org/hippoecm/repository/HippoStdNodeType.java
+++ repository/builtin/src/main/java/org/hippoecm/repository/HippoStdNodeType.java
@@ -254,4 +254,14 @@ public interface HippoStdNodeType {
      * <p>The property should only be present on a draft variant.</p>
      */
     String HIPPOSTD_RETAINABLE = "hippostd:retainable";
+
+    /**
+     * mixin to mark a folder to be an XPage folder (containing XPage documents)
+     */
+    String NT_XPAGE_FOLDER = "hippostd:xpagefolder";
+
+    /**
+     * A property for an {@link #NT_XPAGE_FOLDER} to indicate for which channel the xpage folder is
+     */
+    String HIPPOSTD_CHANNEL_ID = "hippostd:channelid";
 }
