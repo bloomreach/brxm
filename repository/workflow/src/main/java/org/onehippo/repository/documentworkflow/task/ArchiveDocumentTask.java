09538498711d216f942401f71496dd2e33d2f143
Michiel Rop <michiel.rop@bloomreach.com>
2020-08-03 09:18:04 +0200

    CMS-13647 Skip archive if no unpublished variant
    Skip archive if there is no unpublished variant.
    
    Delete is now enabled for a transferable draft (A draft that is saved as
    draft).
    When a document is deleted it's archived (See delete transition in scxml).
    The ArchiveDocumentTask archives the unpublished variant.
    In the case that there is no unpublished variant a NullPointer is
    thrown.
    So a NullPointer is throws when deleting a transferable document.
    
    When the archivation is skipped if there is no unpublished variant a
    NullPointerException will not be thrown.
    
    (cherry picked from commit 9327ce10b38fda96e0f4e772515ae43205c1619d)

--- repository/workflow/src/main/java/org/onehippo/repository/documentworkflow/task/ArchiveDocumentTask.java
+++ repository/workflow/src/main/java/org/onehippo/repository/documentworkflow/task/ArchiveDocumentTask.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2013 Hippo B.V. (http://www.onehippo.com)
+ * Copyright 2013-2020 Hippo B.V. (http://www.onehippo.com)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -78,8 +78,10 @@ public class ArchiveDocumentTask extends AbstractDocumentTask {
 
         try {
             variant = dh.getDocuments().get(HippoStdNodeType.UNPUBLISHED);
-            DefaultWorkflow defaultWorkflow = (DefaultWorkflow) getWorkflowContext().getWorkflow("core", variant);
-            defaultWorkflow.archive();
+            if (variant == null) {
+                DefaultWorkflow defaultWorkflow = (DefaultWorkflow) getWorkflowContext().getWorkflow("core", variant);
+                defaultWorkflow.archive();
+            }
         } catch (MappingException e) {
             log.warn("Cannot archive document: no default workflow", e);
         }
