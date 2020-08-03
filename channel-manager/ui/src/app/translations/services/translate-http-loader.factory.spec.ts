e10e11ddf2a1a77fb9bd685b9199312d91c5eb56
Maxim Palenov <maxim.palenov@bloomreach.com>
2020-08-03 15:58:15 +0200

    CMS-13699 Set up translations module
    (cherry picked from commit 5ed71050a27d1aa1c8f55ba37a0492747548d88d)

---
+++ channel-manager/ui/src/app/translations/services/translate-http-loader.factory.spec.ts
@@ -0,0 +1,47 @@
+/*!
+ * Copyright 2020 Bloomreach. All rights reserved. (https://www.bloomreach.com/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { Location } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { mocked } from 'ts-jest/utils';
+
+import * as getAntiCacheModule from './get-anti-cache-query-param';
+import { translateHttpLoaderFactory } from './translate-http-loader.factory';
+
+jest.mock('./get-anti-cache-query-param');
+
+describe('translateHttpLoaderFactory', () => {
+  const httpClientMock: HttpClient = {} as any;
+
+  let locationMock: Location;
+
+  beforeEach(() => {
+    mocked(getAntiCacheModule.getAntiCacheQueryParam).mockImplementation(() => 'antiCache=some-unique-hash');
+
+    locationMock = {
+      path: jest.fn(() => 'some-location'),
+    } as unknown as typeof locationMock;
+  });
+
+  it('should create TranslateHttpLoader', () => {
+    const expected = new TranslateHttpLoader(httpClientMock, 'assets/i18n/', '.json?antiCache=some-unique-hash');
+
+    const actual = translateHttpLoaderFactory(httpClientMock, locationMock);
+
+    expect(actual).toEqual(expected);
+  });
+});
---
+++ channel-manager/ui/src/app/translations/translations.module.spec.ts
@@ -0,0 +1,61 @@
+/*!
+ * Copyright 2020 Bloomreach. All rights reserved. (https://www.bloomreach.com/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { TranslateService } from '@ngx-translate/core';
+import 'jest-extended';
+
+import enTranslations from '../../assets/i18n/en.json';
+
+import { TranslationsModule } from './translations.module';
+
+describe('TranslationsModule', () => {
+  let module: TranslationsModule;
+  let translateServiceMock: { [key: string]: jest.Mock };
+
+  beforeEach(() => {
+    translateServiceMock = {
+      setTranslation: jest.fn(),
+      setDefaultLang: jest.fn(),
+      addLangs: jest.fn(),
+      use: jest.fn(),
+    };
+
+    module = new TranslationsModule(translateServiceMock as unknown as TranslateService);
+  });
+
+  it('should set a default language', () => {
+    expect(translateServiceMock.setDefaultLang).toHaveBeenCalledWith('en');
+  });
+
+  it('should set default translations', () => {
+    expect(translateServiceMock.setTranslation).toHaveBeenCalledWith('en', enTranslations);
+  });
+
+  it('should set default translations before the default language', () => {
+    expect(translateServiceMock.setTranslation).toHaveBeenCalledBefore(translateServiceMock.setDefaultLang);
+  });
+
+  it('should add available languages', () => {
+    expect(translateServiceMock.addLangs).toHaveBeenCalledWith([
+      'en',
+      'nl',
+      'fr',
+      'de',
+      'es',
+      'zh',
+    ]);
+  });
+});
