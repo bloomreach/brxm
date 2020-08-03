ca310e43720aa86f148eb93c8abb96649ce569bf
Maxim Palenov <maxim.palenov@bloomreach.com>
2020-08-03 15:58:15 +0200

    CMS-13699 Set up translations module
    (cherry picked from commit 75fe13fd4902dc583f0bc61f34fb0722bca5c572)

---
+++ channel-manager/ui/src/app/translations/translations.module.ts
@@ -0,0 +1,68 @@
+/*!
+ * Copyright 2020 Bloomreach. All rights reserved. (https://www.bloomreach.com/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { Location } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Inject, NgModule } from '@angular/core';
+import { TranslateLoader, TranslateModule, TranslateService } from '@ngx-translate/core';
+
+import enTranslations from '../../assets/i18n/en.json';
+import { Ng1ConfigService, NG1_CONFIG_SERVICE } from '../services/ng1/config.service';
+
+import { Translations } from './models/translations.model';
+import { translateHttpLoaderFactory } from './services/translate-http-loader.factory';
+
+@NgModule({
+  imports: [
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: translateHttpLoaderFactory,
+        deps: [HttpClient, Location],
+      },
+    }),
+  ],
+  exports: [TranslateModule],
+})
+export class TranslationsModule {
+  constructor(
+    private readonly translateService: TranslateService,
+    @Inject(NG1_CONFIG_SERVICE) private readonly ng1ConfigService: Ng1ConfigService,
+  ) {
+    this.setUpTranslations('en', 'en', enTranslations);
+    console.log(ng1ConfigService);
+  }
+
+  private setUpTranslations(language: string, defaultLanguage: string, defaultTranslations: Translations): void {
+    this.setDefaultLanguage(defaultLanguage, defaultTranslations);
+
+    this.translateService.addLangs([
+      'en',
+      'nl',
+      'fr',
+      'de',
+      'es',
+      'zh',
+    ]);
+
+    this.translateService.use(language || defaultLanguage);
+  }
+
+  private setDefaultLanguage(defaultLang: string, defaultTranslations: Translations): void {
+    this.translateService.setTranslation(defaultLang, defaultTranslations);
+    this.translateService.setDefaultLang(defaultLang);
+  }
+}
