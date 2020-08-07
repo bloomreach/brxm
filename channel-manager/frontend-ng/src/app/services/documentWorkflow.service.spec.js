db20c9b36327ae9ae764488f5ec9eb2a7d1ccce3
Arthur Bogaart <arthur.bogaart@bloomreach.com>
2020-08-07 16:11:10 +0200

    CMS-13666 Add DocumentWorkflowService that invokes CMS workflow calls
    (cherry picked from commit 0c583c14209e7bdeee8bf7a22e23fc3235536603)

---
+++ channel-manager/frontend-ng/src/app/services/documentWorkflow.service.spec.js
@@ -0,0 +1,91 @@
+/*
+ * Copyright 2020 Bloomreach
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import angular from 'angular';
+import 'angular-mocks';
+
+describe('DocumentWorkflowService', () => {
+  let $window;
+  let DocumentWorkflowService;
+  let workflow;
+
+  beforeEach(() => {
+    angular.mock.module('hippo-cm');
+
+    inject((_$window_, _DocumentWorkflowService_) => {
+      $window = _$window_;
+      DocumentWorkflowService = _DocumentWorkflowService_;
+    });
+
+    workflow = $window.parent.Hippo.Workflow;
+    spyOn(workflow, 'invoke');
+  });
+
+  it('should invoke the "copy" workflow', () => {
+    DocumentWorkflowService.copy('doc1');
+    expect(workflow.invoke).toHaveBeenCalledWith('doc1', 'document', 'copy');
+  });
+
+  it('should invoke the "move" workflow', () => {
+    DocumentWorkflowService.move('doc1');
+    expect(workflow.invoke).toHaveBeenCalledWith('doc1', 'document', 'move');
+  });
+
+  it('should invoke the "delete" workflow', () => {
+    DocumentWorkflowService.delete('doc1');
+    expect(workflow.invoke).toHaveBeenCalledWith('doc1', 'document', 'delete');
+  });
+
+  it('should invoke the "publish" workflow', () => {
+    DocumentWorkflowService.publish('doc1');
+    expect(workflow.invoke).toHaveBeenCalledWith('doc1', 'publication', 'PUB');
+  });
+
+  it('should invoke the "schedule publication" workflow', () => {
+    DocumentWorkflowService.schedulePublication('doc1');
+    expect(workflow.invoke).toHaveBeenCalledWith('doc1', 'publication', 'SCHED_PUB');
+  });
+
+  it('should invoke the "request publication" workflow', () => {
+    DocumentWorkflowService.requestPublication('doc1');
+    expect(workflow.invoke).toHaveBeenCalledWith('doc1', 'publication', 'REQ_PUB');
+  });
+
+  it('should invoke the "request schedule publication" workflow', () => {
+    DocumentWorkflowService.requestSchedulePublication('doc1');
+    expect(workflow.invoke).toHaveBeenCalledWith('doc1', 'publication', 'REQ_SCHED_PUB');
+  });
+
+  it('should invoke the "unpublish" workflow', () => {
+    DocumentWorkflowService.unpublish('doc1');
+    expect(workflow.invoke).toHaveBeenCalledWith('doc1', 'publication', 'DEPUB');
+  });
+
+  it('should invoke the "schedule unpublication" workflow', () => {
+    DocumentWorkflowService.scheduleUnpublication('doc1');
+    expect(workflow.invoke).toHaveBeenCalledWith('doc1', 'publication', 'SCHED_DEPUB');
+  });
+
+  it('should invoke the "request unpublication" workflow', () => {
+    DocumentWorkflowService.requestUnpublication('doc1');
+    expect(workflow.invoke).toHaveBeenCalledWith('doc1', 'publication', 'REQ_DEPUB');
+  });
+
+  it('should invoke the "request schedule unpublication" workflow', () => {
+    DocumentWorkflowService.requestScheduleUnpublication('doc1');
+    expect(workflow.invoke).toHaveBeenCalledWith('doc1', 'publication', 'REQ_SCHED_DEPUB');
+  });
+});
