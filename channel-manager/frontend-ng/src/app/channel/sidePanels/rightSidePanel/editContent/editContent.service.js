10d47e9d842f0de1302828a0977367f6b113461b
Arthur Bogaart <arthur.bogaart@bloomreach.com>
2020-08-14 23:03:16 +0200

    CMS-13728 [Backport 14.3] Reload menu when closing xpage content editor
    (cherry picked from commit d100b83655534fc88c9368769d8af5c1ff9a62c8)

--- channel-manager/frontend-ng/src/app/channel/sidePanels/rightSidePanel/editContent/editContent.service.js
+++ channel-manager/frontend-ng/src/app/channel/sidePanels/rightSidePanel/editContent/editContent.service.js
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018 Hippo B.V. (http://www.onehippo.com)
+ * Copyright 2018-2020 Hippo B.V. (http://www.onehippo.com)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,6 +17,7 @@
 class EditContentService {
   constructor(
     $q,
+    $rootScope,
     $state,
     $transitions,
     $translate,
@@ -55,6 +56,11 @@ class EditContentService {
     $transitions.onBefore(
       { from: '**.edit-page', to: 'hippo-cm' },
       () => this._onCloseChannel(),
+    );
+
+    $transitions.onExit(
+      { exiting: '**.edit-page' },
+      () => $rootScope.$emit('page:check-changes'),
     );
 
     CmsService.subscribe('kill-editor', (documentId) => {
