5925f7ca90bb0f4f57138c0d85d83693e5f6a924
Joeri de Gooijer <joeri.degooijer@bloomreach.com>
2020-08-14 20:57:58 +0200

    CMS-13204 Update unit tests for versions info component
    (cherry picked from commit 363c8c28b7b7f0e90c517f8707503971af8f01c6)

--- channel-manager/ui/src/app/versions/components/versions-info/versions-info.component.spec.ts
+++ channel-manager/ui/src/app/versions/components/versions-info/versions-info.component.spec.ts
@@ -32,28 +32,30 @@ describe('VersionsInfoComponent', () => {
   let fixture: ComponentFixture<VersionsInfoComponent>;
   let iframeService: IframeService;
   let channelService: ChannelService;
+  let workflowService: WorkflowService;
 
   const date = Date.parse('11/08/2020 16:03');
   const path = '/some/test/path';
   const renderPath = `${path}?withParam=test`;
   const selectedVersionUUID = 'testVariantId';
-  const selectEvent = {
-    option: {
-      value: selectedVersionUUID,
-    },
-  } as MatSelectionListChange;
+  const mockVersionsInfo = {
+    versions: [
+      {
+        jcrUUID: 'testId',
+        userName: 'testUserName',
+        timestamp: date,
+      },
+      {
+        jcrUUID: selectedVersionUUID,
+        userName: 'testUserName2',
+        timestamp: date,
+      },
+    ],
+  } as VersionsInfo;
 
   beforeEach(() => {
     const contentServiceMock = {
-      getDocumentVersionsInfo: jest.fn(() => Promise.resolve({
-        versions: [
-          {
-            jcrUUID: 'testId',
-            userName: 'testUserName',
-            timestamp: date,
-          },
-        ],
-      } as VersionsInfo)),
+      getDocumentVersionsInfo: jest.fn(() => Promise.resolve(mockVersionsInfo)),
     };
 
     const channelServiceMock = {
@@ -65,7 +67,9 @@ describe('VersionsInfoComponent', () => {
       load: jest.fn(() => Promise.resolve()),
     };
 
-    const workflowServiceMock = {};
+    const workflowServiceMock = {
+      createWorkflowAction: jest.fn(() => { }),
+    };
 
     TestBed.configureTestingModule({
       declarations: [VersionsInfoComponent],
@@ -83,6 +87,7 @@ describe('VersionsInfoComponent', () => {
 
     iframeService = TestBed.inject(IframeService);
     channelService = TestBed.inject(ChannelService);
+    workflowService = TestBed.inject(WorkflowService);
   });
 
   beforeEach(() => {
@@ -92,20 +97,18 @@ describe('VersionsInfoComponent', () => {
 
     component.documentId = 'testDocumentId';
     component.branchId = 'projectId';
-    component.unpublishedVariantId = 'unpublishedVariantId';
+    component.unpublishedVariantId = 'testId';
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(componentEl).toMatchSnapshot();
-  });
-
   describe('showing versions', () => {
+    it('should show header indicating no versions are available', () => {
+      expect(componentEl).toMatchSnapshot();
+    });
+
     it('should show list of versions', fakeAsync(() => {
       component.ngOnInit();
-
       tick();
-
       fixture.detectChanges();
 
       expect(componentEl).toMatchSnapshot();
@@ -113,19 +116,92 @@ describe('VersionsInfoComponent', () => {
   });
 
   describe('selecting version', () => {
-    it('should add the version param to the url and load that url', async () => {
+    beforeEach(fakeAsync(() => {
+      component.ngOnInit();
+      tick();
+      fixture.detectChanges();
+    }));
+
+    it('should add the version param to the url and load that url', () => {
       jest.spyOn(iframeService, 'load');
-      await component.selectVersion(selectEvent);
+
+      const versionItem = componentEl.querySelector<HTMLElement>(`.qa-version-${selectedVersionUUID}`);
+      versionItem?.click();
 
       expect(iframeService.load).toHaveBeenCalledWith(`${path}?br_version_uuid=${selectedVersionUUID}`);
     });
 
-    it('should append the version param to the url if params are already present and load that url', async () => {
+    it('should append the version param to the url if params are already present and load that url', () => {
       jest.spyOn(iframeService, 'load');
       jest.spyOn(channelService, 'makeRenderPath').mockReturnValueOnce(renderPath);
-      await component.selectVersion(selectEvent);
+
+      const versionItem = componentEl.querySelector<HTMLElement>(`.qa-version-${selectedVersionUUID}`);
+      versionItem?.click();
 
       expect(iframeService.load).toHaveBeenCalledWith(`${renderPath}&br_version_uuid=${selectedVersionUUID}`);
+    });
+
+    it('should show the selected version', () => {
+      jest.spyOn(iframeService, 'load').mockImplementationOnce(() => {
+        component.unpublishedVariantId = selectedVersionUUID;
+        return Promise.resolve();
+      });
+
+      const versionItem = componentEl.querySelector<HTMLElement>(`.qa-version-${selectedVersionUUID}`);
+      versionItem?.click();
+      fixture.detectChanges();
+
+      const versionList = componentEl.querySelector<HTMLElement>('.qa-version-list');
+      expect(versionList).toMatchSnapshot();
+    });
+  });
+
+  describe('restoring a version', () => {
+    beforeEach(fakeAsync(() => {
+      component.ngOnInit();
+      tick();
+      fixture.detectChanges();
+    }));
+
+    it('should show restore button for selected version', async () => {
+      jest.spyOn(iframeService, 'load').mockImplementationOnce(() => {
+        component.unpublishedVariantId = selectedVersionUUID;
+        return Promise.resolve();
+      });
+
+      await component.selectVersion(selectedVersionUUID);
+      fixture.detectChanges();
+
+      expect(componentEl).toMatchSnapshot();
+    });
+
+    it('should not show restore button for first version in the list', async () => {
+      const firstVersionId = mockVersionsInfo.versions[0].jcrUUID;
+
+      jest.spyOn(iframeService, 'load').mockImplementationOnce(() => {
+        component.unpublishedVariantId =  firstVersionId;
+        return Promise.resolve();
+      });
+
+      await component.selectVersion(firstVersionId);
+      fixture.detectChanges();
+
+      expect(componentEl).toMatchSnapshot();
+    });
+
+    it('should call to restore', async () => {
+      jest.spyOn(iframeService, 'load').mockImplementationOnce(() => {
+        component.unpublishedVariantId = selectedVersionUUID;
+        return Promise.resolve();
+      });
+
+      await component.selectVersion(selectedVersionUUID);
+      fixture.detectChanges();
+
+      const restoreButton = componentEl.querySelector<HTMLButtonElement>('.qa-restore-version-action');
+      restoreButton?.click();
+
+      expect(workflowService.createWorkflowAction).toHaveBeenCalledWith(component.documentId, 'restore', selectedVersionUUID);
     });
   });
 });
