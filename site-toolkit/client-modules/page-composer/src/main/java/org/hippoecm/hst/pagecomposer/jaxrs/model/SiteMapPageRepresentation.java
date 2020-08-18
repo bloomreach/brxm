6d7a39b85bd5e9da435618cb2b493545800920ac
Arthur Bogaart <arthur.bogaart@bloomreach.com>
2020-08-18 16:05:15 +0200

    CMS-13770 [Backport 14.3] Add endpoint for exposing the pahtInfo of a handle
    When creating a new xpage document, we want to navigate to that document
    after it has been created. To do this, we add a new REST endpoint that
    returns a SiteMapPageRepresentation for said handle.
    
    (cherry picked from commit da156af58a2139d70a9bd7db8f658faf3691ffb9)

--- site-toolkit/client-modules/page-composer/src/main/java/org/hippoecm/hst/pagecomposer/jaxrs/model/SiteMapPageRepresentation.java
+++ site-toolkit/client-modules/page-composer/src/main/java/org/hippoecm/hst/pagecomposer/jaxrs/model/SiteMapPageRepresentation.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014-2015 Hippo B.V. (http://www.onehippo.com)
+ * Copyright 2014-2020 Hippo B.V. (http://www.onehippo.com)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,11 +18,11 @@ package org.hippoecm.hst.pagecomposer.jaxrs.model;
 import javax.jcr.Node;
 import javax.jcr.RepositoryException;
 
-import org.apache.commons.lang3.StringUtils;
 import org.hippoecm.hst.configuration.hosting.Mount;
 import org.hippoecm.hst.configuration.internal.CanonicalInfo;
 import org.hippoecm.hst.configuration.sitemap.HstSiteMapItem;
 import org.hippoecm.hst.core.linking.HstLink;
+import org.hippoecm.hst.pagecomposer.jaxrs.services.experiencepage.XPageUtils;
 import org.hippoecm.hst.util.HstSiteMapUtils;
 import org.hippoecm.repository.api.HippoNode;
 import org.hippoecm.repository.api.NodeNameCodec;
@@ -85,7 +85,7 @@ public class SiteMapPageRepresentation {
         final Mount mount = hstLink.getMount();
         pathInfo = "/" + hstLink.getPath();
         renderPathInfo = mount == null ? pathInfo : mount.getMountPath() + pathInfo;
-        experiencePage = true;
+        experiencePage = XPageUtils.isXPageDocument(handleNode);
         return this;
     }
 
