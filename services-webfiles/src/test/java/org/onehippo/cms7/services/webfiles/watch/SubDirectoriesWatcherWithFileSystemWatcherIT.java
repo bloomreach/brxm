561a6b40893bb5c70ec9e4af829c35f7bba66142
Arent-Jan Banck <arent-jan.banck@bloomreach.com>
2020-08-18 13:35:21 +0200

    CMS-13783 Disable FileSystemWatcherIT and SubDirectoriesWatcherWithFileSystemWatcherIT as the tests ocassionally fail with 'User limit of inotify instances reached or too many open files'

--- services-webfiles/src/test/java/org/onehippo/cms7/services/webfiles/watch/SubDirectoriesWatcherWithFileSystemWatcherIT.java
+++ services-webfiles/src/test/java/org/onehippo/cms7/services/webfiles/watch/SubDirectoriesWatcherWithFileSystemWatcherIT.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014-2015 Hippo B.V. (http://www.onehippo.com)
+ * Copyright 2014-2020 Hippo B.V. (http://www.onehippo.com)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,7 +20,9 @@ import java.io.IOException;
 import org.apache.commons.lang3.SystemUtils;
 import org.junit.Assume;
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 
+@Ignore
 public class SubDirectoriesWatcherWithFileSystemWatcherIT extends AbstractSubDirectoriesWatcherWithFileSystemObserverIT {
 
     @BeforeClass
