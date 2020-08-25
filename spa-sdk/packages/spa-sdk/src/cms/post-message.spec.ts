aaeb447b4996d70da13a93166422bd5bf543c383
Michael Dokolin <mikhail.dokolin@bloomreach.com>
2020-08-25 12:44:15 +0200

    CMS-13821 Add support of the wildcard origin in the Channel Manager integration

--- spa-sdk/packages/spa-sdk/src/cms/post-message.spec.ts
+++ spa-sdk/packages/spa-sdk/src/cms/post-message.spec.ts
@@ -22,15 +22,28 @@ jest.mock('../url');
 
 describe('PostMessage', () => {
   let postMessage: PostMessage;
+  let processSpy: jasmine.Spy;
 
-  beforeEach(() => {
+  beforeAll(() => {
     postMessage = new PostMessage();
+    processSpy = spyOn(postMessage, 'process');
+  });
+
+  afterEach(() => {
+    processSpy.calls.reset();
   });
 
   describe('initialize', () => {
+    it('should not process a message if not initialized', async () => {
+      window.postMessage({ some: 'data' }, '*');
+
+      await new Promise(resolve => setTimeout(resolve, 0));
+
+      expect(processSpy).not.toHaveBeenCalled();
+    });
+
     it('should process a message', async () => {
       postMessage.initialize({ window, origin: 'http://example.com' });
-      const processSpy = spyOn(postMessage, 'process');
       mocked(isMatched).mockReturnValueOnce(true);
       window.postMessage({ some: 'data' }, '*');
 
@@ -39,9 +52,19 @@ describe('PostMessage', () => {
       expect(processSpy).toHaveBeenCalledWith({ some: 'data' });
     });
 
+    it('should process a message in case of wildcard origin', async () => {
+      postMessage.initialize({ window, origin: '*' });
+      mocked(isMatched).mockReturnValueOnce(true);
+      window.postMessage({ some: 'data' }, '*');
+
+      await new Promise(resolve => setTimeout(resolve, 0));
+
+      expect(isMatched).toHaveBeenCalledWith(expect.anything(), '');
+      expect(processSpy).toHaveBeenCalledWith({ some: 'data' });
+    });
+
     it('should not process a message if the origin is not matching', async () => {
       postMessage.initialize({ window, origin: 'http://example.com' });
-      const processSpy = spyOn(postMessage, 'process');
       mocked(isMatched).mockReturnValueOnce(false);
       window.postMessage({ some: 'data' }, '*');
 
@@ -52,19 +75,8 @@ describe('PostMessage', () => {
 
     it('should not process a message without data', async () => {
       postMessage.initialize({ window, origin: 'http://example.com' });
-      const processSpy = spyOn(postMessage, 'process');
       mocked(isMatched).mockReturnValueOnce(true);
       window.postMessage(undefined, '*');
-
-      await new Promise(resolve => setTimeout(resolve, 0));
-
-      expect(processSpy).not.toHaveBeenCalled();
-    });
-
-    it('should not process a message if not initialized', async () => {
-      const processSpy = spyOn(postMessage, 'process');
-      window.postMessage({ some: 'data' }, '*');
-      mocked(isMatched).mockReturnValueOnce(true);
 
       await new Promise(resolve => setTimeout(resolve, 0));
 
