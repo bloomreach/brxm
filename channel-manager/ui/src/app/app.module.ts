3962539aa6e6196a6d6db825dd6465f4287f38ae
Maxim Palenov <maxim.palenov@bloomreach.com>
2020-08-31 19:21:17 +0200

    CMS-13809 Display previous version info in the notification bar

--- channel-manager/ui/src/app/app.module.ts
+++ channel-manager/ui/src/app/app.module.ts
@@ -21,9 +21,10 @@ import { NG1_CHANNEL_SERVICE } from './services/ng1/channel.ng1service';
 import { NG1_CONFIG_SERVICE } from './services/ng1/config.ng1.service';
 import { NG1_CONTENT_SERVICE } from './services/ng1/content.ng1.service';
 import { NG1_IFRAME_SERVICE } from './services/ng1/iframe.ng1service';
-import { NG1_PAGESTRUCTURE_SERVICE } from './services/ng1/page-structure.ng1.service';
+import { NG1_PAGE_STRUCTURE_SERVICE } from './services/ng1/page-structure.ng1.service';
 import { NG1_PAGE_SERVICE } from './services/ng1/page.ng1.service';
 import { NG1_PROJECT_SERVICE } from './services/ng1/project.ng1.service';
+import { NG1_ROOT_SCOPE } from './services/ng1/root-scope.service';
 import { NG1_WORKFLOW_SERVICE } from './services/ng1/workflow.ng1.service';
 import { SharedModule } from './shared/shared.module';
 import { VersionsModule } from './versions/versions.module';
@@ -41,8 +42,9 @@ import { VersionsModule } from './versions/versions.module';
     { provide: NG1_CONTENT_SERVICE, useValue: window.angular.element(document.body).injector().get('ContentService') },
     { provide: NG1_WORKFLOW_SERVICE, useValue: window.angular.element(document.body).injector().get('WorkflowService') },
     { provide: NG1_PROJECT_SERVICE, useValue: window.angular.element(document.body).injector().get('ProjectService') },
-    { provide: NG1_PAGESTRUCTURE_SERVICE, useValue: window.angular.element(document.body).injector().get('PageStructureService') },
+    { provide: NG1_PAGE_STRUCTURE_SERVICE, useValue: window.angular.element(document.body).injector().get('PageStructureService') },
     { provide: NG1_PAGE_SERVICE, useValue: window.angular.element(document.body).injector().get('PageService') },
+    { provide: NG1_ROOT_SCOPE, useValue: window.angular.element(document.body).injector().get('$rootScope') },
   ],
 })
 export class AppModule {
