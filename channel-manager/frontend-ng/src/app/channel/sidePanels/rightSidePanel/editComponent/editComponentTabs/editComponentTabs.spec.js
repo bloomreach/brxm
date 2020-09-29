22a2e41ef576f63f3e29f2d8194973385212066f
Maxim Palenov <maxim.palenov@bloomreach.com>
2020-09-29 14:25:13 +0200

    CMS-13909 Set up routing for the edit component module
    Routing allows easily extend functionality by adding more tabs.

---
+++ channel-manager/frontend-ng/src/app/channel/sidePanels/rightSidePanel/editComponent/editComponentTabs/editComponentTabs.spec.js
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2020 Hippo B.V. (http://www.onehippo.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+describe('EditComponentTabsCtrl', () => {
+  let $ctrl;
+  let $state;
+  let $rootScope;
+
+  const navItem = 'test';
+  const mockState = `hippo-cm.channel.edit-component.${navItem}`;
+
+  beforeEach(() => {
+    angular.mock.module('hippo-cm');
+
+    inject(($componentController, _$rootScope_, _$state_, $uiRouterGlobals, $transitions) => {
+      $rootScope = _$rootScope_;
+      $state = _$state_;
+
+      $uiRouterGlobals.current = {
+        name: mockState,
+      };
+
+      $ctrl = $componentController('editComponentTabs', {
+        $uiRouterGlobals,
+        $transitions,
+      });
+    });
+  });
+
+  describe('onInit', () => {
+    it('should set the initial selected nav item', () => {
+      $ctrl.$onInit();
+
+      expect($ctrl.selectedNavItem).toEqual(navItem);
+    });
+
+    it('should select the properties tab on transition to content state', () => {
+      const newNavItem = 'properties';
+      $ctrl.$onInit();
+      $state.go(`hippo-cm.channel.edit-component.${newNavItem}`, { componentId: 'c1', variantId: 'v1' });
+      $rootScope.$digest();
+      expect($ctrl.selectedNavItem).toEqual(newNavItem);
+    });
+  });
+});
