1cafe293a7b5de313b4fd6b3be8a469d29301d13
Maxim Palenov <maxim.palenov@bloomreach.com>
2020-10-08 18:44:43 +0200

    CMS-13934 Add notification service

---
+++ channel-manager/ui/src/app/services/notification.service.spec.ts
@@ -0,0 +1,67 @@
+/*!
+ * Copyright 2020 Bloomreach. All rights reserved. (https://www.bloomreach.com/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { TestBed } from '@angular/core/testing';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+import { NotificationService } from './notification.service';
+
+describe('NotificationService', () => {
+  let service: NotificationService;
+  let matSnackBarMock: MatSnackBar;
+
+  beforeEach(() => {
+    matSnackBarMock = {
+      open: jest.fn(),
+    } as unknown as typeof matSnackBarMock;
+
+    TestBed.configureTestingModule({
+      providers: [
+        NotificationService,
+        { provide: MatSnackBar, useValue: matSnackBarMock },
+      ],
+    });
+
+    service = TestBed.inject(NotificationService);
+  });
+
+  describe('simple notification', () => {
+    beforeEach(() => {
+      service.showNotification('Some message');
+    });
+
+    it('should show a snack bar', () => {
+      expect(matSnackBarMock.open).toHaveBeenCalledWith('Some message', undefined, {
+        duration: 5000,
+        horizontalPosition: 'end',
+        verticalPosition: 'top',
+      });
+    });
+  });
+
+  describe('error notification', () => {
+    beforeEach(() => {
+      service.showErrorNotification('Some error message');
+    });
+
+    it('should show a snack bar', () => {
+      expect(matSnackBarMock.open).toHaveBeenCalledWith('Some error message', 'Dismiss', {
+        horizontalPosition: 'end',
+        verticalPosition: 'top',
+      });
+    });
+  });
+});
