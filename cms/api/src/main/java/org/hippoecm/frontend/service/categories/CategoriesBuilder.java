8055369b7bc106312f9dc7b5b1ad6dfc4b142d23
Arthur Bogaart <arthur.bogaart@bloomreach.com>
2020-11-12 14:59:00 +0100

    CMS-14083 [Backport 14.4] Hide "translations" workflow category for xpages
    (cherry picked from commit 53da5d6a49b159fcfa2e04775c356dab563a06a5)

--- cms/api/src/main/java/org/hippoecm/frontend/service/categories/CategoriesBuilder.java
+++ cms/api/src/main/java/org/hippoecm/frontend/service/categories/CategoriesBuilder.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018 Hippo B.V. (http://www.onehippo.com)
+ * Copyright 2018-2020 Hippo B.V. (http://www.onehippo.com)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -10,9 +10,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
  */
-
 package org.hippoecm.frontend.service.categories;
 
 import javax.jcr.Node;
@@ -30,4 +28,6 @@ public interface CategoriesBuilder {
     CategoriesBuilder versionCategories(String[] versionCategories);
 
     CategoriesBuilder workflowCategories(String[] workflowCategories);
+
+    CategoriesBuilder xpageCategories(String[] xpageCategories);
 }
