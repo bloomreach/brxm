a6b440c706b468d709e86ec59ee808dc91ee43a2
Michiel Eggermont <michiel.eggermont@bloomreach.com>
2020-11-16 20:07:41 +0100

    ENT-2139 [Backport 14.4] Use maps instead of sets
    The implementations for Action and State implemented equals and
    hasCode because they were collected and merged in a set. These
    implementations were a little awkward because they did not include
    all fields. Then I realized that Action and State are only needed
    for the JSON representation, so internally I could use a different
    structure. I also realized is that the fields category and name are
    actually the unique keys for Action and State. So internally it is
    easier to use maps. The keys of these maps are implementations of
    interface NamedCategory. By letting the existing enums implement
    NamedCategory we get equals and hashCode for free.
    
    (cherry picked from commit e0a66b5de74ae30970a9763b455fd7501736524a)

--- site-toolkit/client-modules/page-composer/src/main/java/org/hippoecm/hst/pagecomposer/jaxrs/services/component/ActionState.java
+++ site-toolkit/client-modules/page-composer/src/main/java/org/hippoecm/hst/pagecomposer/jaxrs/services/component/ActionState.java
@@ -16,19 +16,20 @@
  */
 package org.hippoecm.hst.pagecomposer.jaxrs.services.component;
 
-import java.util.Collections;
-import java.util.HashSet;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
-import java.util.Set;
+
+import static java.util.Collections.emptyMap;
 
 public final class ActionState {
 
-    private final Set<Action> actions;
-    private final Set<State> states;
+    private final Map<NamedCategory, Boolean> actions;
+    private final Map<NamedCategory, Object> states;
 
-    private static final ActionState EMPTY = new ActionState(Collections.emptySet(), Collections.emptySet());
+    private static final ActionState EMPTY = new ActionState(emptyMap(), emptyMap());
 
-    public ActionState(final Set<Action> actions, final Set<State> states) {
+    public ActionState(final Map<NamedCategory, Boolean> actions, final Map<NamedCategory, Object> states) {
         Objects.requireNonNull(actions);
         Objects.requireNonNull(states);
         this.actions = actions;
@@ -39,34 +40,30 @@ public final class ActionState {
         return EMPTY;
     }
 
-    static ActionState merge(final ActionState si1, final ActionState si2) {
-        final Set<Action> actions = new HashSet<>(si1.actions);
-        si2.actions.forEach(a2 -> {
-            if (actions.contains(a2)) {
-                actions.remove(a2);
-                if (a2.isEnabled() != null) {
-                    actions.add(a2);
-                }
-            }
-        });
-        final Set<State> states = new HashSet<>(si1.states);
-        si2.states.forEach(s2 -> {
-            if (states.contains(s2)) {
-                states.remove(s2);
-                if (s2.getValue() != null) {
-                    states.add(s2);
-                }
-            }
-        });
+    static ActionState merge(final ActionState as1, final ActionState as2) {
+        final Map<NamedCategory, Boolean> actions = mergeMaps(as1.actions, as2.actions);
+        final Map<NamedCategory, Object> states = mergeMaps(as1.states, as2.states);
         return new ActionState(actions, states);
     }
 
-    public Set<Action> getActions() {
+    public Map<NamedCategory, Boolean> getActions() {
         return actions;
     }
 
-    public Set<State> getStates() {
+    public Map<NamedCategory, Object> getStates() {
         return states;
     }
 
+    private static  <T> Map<NamedCategory, T> mergeMaps(final Map<NamedCategory, T> m1, final Map<NamedCategory, T> m2) {
+        final Map<NamedCategory, T> merged = new HashMap<>(m1);
+        m2.forEach( (s, v) -> {
+            if (merged.containsKey(s)) {
+                merged.remove(s);
+                if (v != null) {
+                    merged.put(s, v);
+                }
+            }
+        });
+        return merged;
+    }
 }
