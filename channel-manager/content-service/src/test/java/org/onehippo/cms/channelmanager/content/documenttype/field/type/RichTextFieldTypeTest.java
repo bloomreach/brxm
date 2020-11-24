67fb0bd25e225b666310c255b1eed8b61bc9295a
Ard Schrijvers <ard.schrijvers@bloomreach.com>
2020-11-24 15:18:34 +0100

    ENT-2191 [Backport 14.4] Make sure PowerMock ignores logging for bytecode manipulation
    If during tests there is any class that uses PowerMock, then during that
    test cycle, the tests after PowerMock has been injecting bytecode can
    have broken logging, and as a result, tests which rely on Log4jInterceptor
    using to assert against certain log messages can fail. Whether it really
    happens relies on the order in which the tests happen to be executed.
    
    After many trial and errors and finally some simple dummy tests to
    reproduce the issue, the issue seems to get solved by adding
    
    org.apache.logging.log4j.*
    to the @PowerMockIgnore : this seems to avoid PowerMock manipulating the log classes which resulted in the flaky tests to get fixed
    
    (cherry picked from commit 9fe553020dcd80c8bd7303f2e11cabe1b68e198f)

--- channel-manager/content-service/src/test/java/org/onehippo/cms/channelmanager/content/documenttype/field/type/RichTextFieldTypeTest.java
+++ channel-manager/content-service/src/test/java/org/onehippo/cms/channelmanager/content/documenttype/field/type/RichTextFieldTypeTest.java
@@ -59,7 +59,7 @@ import static org.powermock.api.easymock.PowerMock.mockStatic;
 import static org.powermock.api.easymock.PowerMock.replayAll;
 
 @RunWith(PowerMockRunner.class)
-@PowerMockIgnore({"javax.management.*", "com.sun.org.apache.xerces.*", "javax.xml.*", "org.xml.*", "org.w3c.dom.*", "com.sun.org.apache.xalan.*", "javax.activation.*", "javax.net.ssl.*"})
+@PowerMockIgnore({"org.apache.logging.log4j.*", "javax.management.*", "com.sun.org.apache.xerces.*", "javax.xml.*", "org.xml.*", "org.w3c.dom.*", "com.sun.org.apache.xalan.*", "javax.activation.*", "javax.net.ssl.*"})
 @PrepareForTest({HtmlProcessorFactory.class})
 public class RichTextFieldTypeTest {
 
