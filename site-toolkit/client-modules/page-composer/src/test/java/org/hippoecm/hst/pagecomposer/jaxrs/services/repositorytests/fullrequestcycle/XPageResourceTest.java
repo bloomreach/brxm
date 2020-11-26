e150f1630bbd687f53ad881e03427efe096bb211
Michiel Eggermont <michiel.eggermont@bloomreach.com>
2020-11-26 08:53:00 +0100

    CMS-14112 Add integration test

--- site-toolkit/client-modules/page-composer/src/test/java/org/hippoecm/hst/pagecomposer/jaxrs/services/repositorytests/fullrequestcycle/XPageResourceTest.java
+++ site-toolkit/client-modules/page-composer/src/test/java/org/hippoecm/hst/pagecomposer/jaxrs/services/repositorytests/fullrequestcycle/XPageResourceTest.java
@@ -44,6 +44,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import static java.lang.Boolean.FALSE;
 import static javax.ws.rs.core.Response.Status.CREATED;
+import static org.hippoecm.repository.HippoStdNodeType.NT_ROOT_XPAGE_FOLDER;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
@@ -293,6 +294,22 @@ public class XPageResourceTest extends AbstractXPageComponentResourceTest {
         assertions(ADMIN_CREDENTIALS, "action_and_state_for_channel_foo_and_EXISTING_unpublished_xpage_for_branch_foo.json", "foo");
         assertions(AUTHOR_CREDENTIALS, "action_and_state_for_channel_foo_and_EXISTING_unpublished_xpage_for_branch_foo.json", "foo");
     }
+
+    @Test
+    public void action_and_state_for_channel_master_rootxpagefolder() throws Exception {
+
+        admin.getNode("/unittestcontent/documents/unittestproject/experiences/experiences-subfolder").addMixin(NT_ROOT_XPAGE_FOLDER);
+        admin.save();
+
+        try {
+            assertions(ADMIN_CREDENTIALS, "action_and_state_for_master_channel_and_master_xpage_admin_rootxpagefolder.json", "master");
+            assertions(AUTHOR_CREDENTIALS, "action_and_state_for_master_channel_and_master_xpage_author_rootxpagefolder.json", "master");
+        } finally {
+            admin.getNode("/unittestcontent/documents/unittestproject/experiences/experiences-subfolder").removeMixin(NT_ROOT_XPAGE_FOLDER);
+            admin.save();
+        }
+    }
+
 
     private void assertions(final SimpleCredentials creds, String fixtureFileName, final String branchId) throws Exception {
         final RequestResponseMock createRequestResponse = mockGetRequestResponse(
