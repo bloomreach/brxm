b795545e9bdc76fdbe01187f78e211a67d6e6edf
Michiel Eggermont <michiel.eggermont@bloomreach.com>
2020-12-07 11:02:49 +0100

    CMS-14196 Enable cancelling scheduled (de)publication
    (cherry picked from commit 80574010142d7ce7fa122f3e090931feb1a94c71)

--- site-toolkit/client-modules/page-composer/src/main/java/org/hippoecm/hst/pagecomposer/jaxrs/services/component/XPageContextFactory.java
+++ site-toolkit/client-modules/page-composer/src/main/java/org/hippoecm/hst/pagecomposer/jaxrs/services/component/XPageContextFactory.java
@@ -80,7 +80,7 @@ final class XPageContextFactory {
         if (branches.contains(selectedBranchId)) {
             // xpage doc branch exists for currently selectedBranchId (in Channel mgr)
             useXPageDocBranch = selectedBranchId;
-        } else if (branches.contains(MASTER_BRANCH_ID)){
+        } else if (branches.contains(MASTER_BRANCH_ID)) {
             // xpage doc branch does not exist for selectedBranchId, use master
             useXPageDocBranch = MASTER_BRANCH_ID;
         } else {
@@ -132,19 +132,20 @@ final class XPageContextFactory {
             return xPageContext;
         }
 
-        final boolean pageIsUnlocked = StringUtils.isBlank(xPageContext.getLockedBy());
-        if (hints.containsKey("publishBranch")) {
-            xPageContext.setPublishable(TRUE.equals(hints.get("publishBranch")) && pageIsUnlocked);
-        } else if (hints.containsKey("requestPublication")) {
-            xPageContext.setRequestPublication(TRUE.equals(hints.get("requestPublication")));
-        }
+        if (scheduledRequest == null) {
+            final boolean pageIsUnlocked = StringUtils.isBlank(xPageContext.getLockedBy());
+            if (hints.containsKey("publishBranch")) {
+                xPageContext.setPublishable(TRUE.equals(hints.get("publishBranch")) && pageIsUnlocked);
+            } else if (hints.containsKey("requestPublication")) {
+                xPageContext.setRequestPublication(TRUE.equals(hints.get("requestPublication")));
+            }
 
-        if (hints.containsKey("depublishBranch")) {
-            xPageContext.setUnpublishable(TRUE.equals(hints.get("depublishBranch")) && pageIsUnlocked);
-        } else if (hints.containsKey("requestDepublication")) {
-            xPageContext.setRequestDepublication(TRUE.equals(hints.get("requestDepublication")));
+            if (hints.containsKey("depublishBranch")) {
+                xPageContext.setUnpublishable(TRUE.equals(hints.get("depublishBranch")) && pageIsUnlocked);
+            } else if (hints.containsKey("requestDepublication")) {
+                xPageContext.setRequestDepublication(TRUE.equals(hints.get("requestDepublication")));
+            }
         }
-
         return xPageContext;
     }
 
