eb493aa98d4eadeff984a9d8c7fc4beb0a68e77f
Michael Dokolin <mikhail.dokolin@bloomreach.com>
2020-12-30 14:25:20 +0100

    TRIVIAL Move shared styles in React example

--- spa-sdk/examples/react/src/index.tsx
+++ spa-sdk/examples/react/src/index.tsx
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019 Hippo B.V. (http://www.onehippo.com)
+ * Copyright 2019-2020 Hippo B.V. (http://www.onehippo.com)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -23,6 +23,7 @@ import {
   Switch,
 } from 'react-router-dom';
 
+import './index.css';
 import App from './App';
 
 ReactDOM.render(
