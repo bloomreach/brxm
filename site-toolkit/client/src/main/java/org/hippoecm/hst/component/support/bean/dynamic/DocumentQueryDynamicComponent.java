11352368cbc7a797b4903a3a7df12038c3440eac
Tobias Jeger <tobias.jeger@bloomreach.com>
2020-12-31 14:40:18 +0100

    CMS-14363 Fix limit parameter of DocumentQueryDynamicComponent
    (cherry picked from commit 2be0df51ee51af15aff7bf6cbb76a304b39231ed)

--- site-toolkit/client/src/main/java/org/hippoecm/hst/component/support/bean/dynamic/DocumentQueryDynamicComponent.java
+++ site-toolkit/client/src/main/java/org/hippoecm/hst/component/support/bean/dynamic/DocumentQueryDynamicComponent.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2020 Hippo B.V. (http://www.onehippo.com)
+ *  Copyright 2021 Hippo B.V. (http://www.onehippo.com)
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -251,7 +251,7 @@ public class DocumentQueryDynamicComponent extends BaseHstDynamicComponent {
      * @return the page limit of the query
      */
     protected int getPageLimit(final HstRequest request) {
-        String value = request.getParameter(REQUEST_PARAM_PAGE);
+        String value = request.getParameter(REQUEST_PARAM_LIMIT);
         if (!Strings.isNullOrEmpty(value)) {
             try {
                 return Integer.parseInt(value);
