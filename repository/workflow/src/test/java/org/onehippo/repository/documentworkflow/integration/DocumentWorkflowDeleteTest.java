afd81878a10850bccf832b1f43a8cb74d57d9b14
Michiel Rop <michiel.rop@bloomreach.com>
2021-02-01 15:44:58 +0100

    CMS-14035 Add a failing unit test
    If a document has been saved as draft, the only variant is the draft
    variant.
    If that document is deleted, it's archived and moved to the attic.
    Jeroen Hoffman pointed out that a handle with a draft variant in the
    attic does not contain any useful information. Carolien de Kort
    mentioned that deleting a document that is saved as draft is funcionally
    equal to the cancel action. The cancel action deletes the draft and the
    handle and does not move it to the attic.
    
    Add a (failing) unit test that asserts the document should be deleted in
    case there is only a draft variant.
    
    (cherry picked from commit b5659f9ffc620effab621e55d583b2a34e90ceb6)

--- repository/workflow/src/test/java/org/onehippo/repository/documentworkflow/integration/DocumentWorkflowDeleteTest.java
+++ repository/workflow/src/test/java/org/onehippo/repository/documentworkflow/integration/DocumentWorkflowDeleteTest.java
@@ -17,28 +17,24 @@ package org.onehippo.repository.documentworkflow.integration;
 
 import java.util.Calendar;
 
+import javax.jcr.InvalidItemStateException;
 import javax.jcr.Node;
-import javax.jcr.NodeIterator;
 import javax.jcr.RepositoryException;
 
 import org.hippoecm.repository.HippoStdNodeType;
 import org.hippoecm.repository.api.HippoNodeType;
 import org.junit.Test;
 import org.onehippo.repository.documentworkflow.DocumentWorkflow;
-import org.onehippo.repository.mock.MockNode;
 
 import static org.hippoecm.repository.HippoStdNodeType.HIPPOSTD_STATE;
-import static org.hippoecm.repository.HippoStdNodeType.NT_DOCUMENT;
 import static org.hippoecm.repository.HippoStdNodeType.NT_RELAXED;
-import static org.hippoecm.repository.HippoStdNodeType.UNPUBLISHED;
 import static org.hippoecm.repository.HippoStdPubWfNodeType.HIPPOSTDPUBWF_CREATED_BY;
 import static org.hippoecm.repository.HippoStdPubWfNodeType.HIPPOSTDPUBWF_CREATION_DATE;
 import static org.hippoecm.repository.HippoStdPubWfNodeType.HIPPOSTDPUBWF_DOCUMENT;
 import static org.hippoecm.repository.HippoStdPubWfNodeType.HIPPOSTDPUBWF_LAST_MODIFIED_BY;
 import static org.hippoecm.repository.HippoStdPubWfNodeType.HIPPOSTDPUBWF_LAST_MODIFIED_DATE;
 import static org.hippoecm.repository.api.HippoNodeType.HIPPO_AVAILABILITY;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.onehippo.repository.util.JcrConstants.MIX_VERSIONABLE;
 
@@ -64,14 +60,29 @@ public class DocumentWorkflowDeleteTest extends AbstractDocumentWorkflowIntegrat
     }
 
     @Test
+    public void testDeleteDocument_unpublishedAndDraft() throws Exception {
+        final DocumentWorkflow workflow = getDocumentWorkflow(handle);
+        createVariant(HippoNodeType.NT_DOCUMENT, HippoStdNodeType.DRAFT);
+        session.save();
+        workflow.delete();
+        final boolean isInAttic = workflow.getNode().getPath().contains("attic");
+        assertTrue("Document has not been archived", isInAttic);
+    }
+
+    @Test
     public void testDeleteDocument_draftOnly() throws Exception {
         handle.getNode("document").remove();
         createVariant(HippoNodeType.NT_DOCUMENT, HippoStdNodeType.DRAFT);
         session.save();
         final DocumentWorkflow workflow = getDocumentWorkflow(handle);
         workflow.delete();
-        final boolean isInAttic = workflow.getNode().getPath().contains("attic");
-        assertTrue("Document has not been archived", isInAttic);
+        try {
+            final boolean isInAttic = workflow.getNode().getPath().contains("attic");
+            assertFalse("Document should not have been archived", isInAttic);
+        }
+        catch (InvalidItemStateException ise){
+            // This is what is expected;
+        }
     }
 
     private Node createVariant(String primaryType, String state) throws RepositoryException {
