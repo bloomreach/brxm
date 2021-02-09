7e37838682b3058406cd18b157c44cacc1ed630a
Michael Dokolin <mikhail.dokolin@bloomreach.com>
2021-02-09 17:23:16 +0100

    CMS-13692 Add undefined container item type constant

--- spa-sdk/packages/spa-sdk/src/page/index.ts
+++ spa-sdk/packages/spa-sdk/src/page/index.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019-2020 Hippo B.V. (http://www.onehippo.com)
+ * Copyright 2019-2021 Hippo B.V. (http://www.onehippo.com)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -81,7 +81,7 @@ export {
   TYPE_COMPONENT_CONTAINER as TYPE_COMPONENT_CONTAINER_09,
   TYPE_COMPONENT_CONTAINER_ITEM as TYPE_COMPONENT_CONTAINER_ITEM_09,
 } from './component09';
-export { ContainerItem } from './container-item';
+export { ContainerItem, TYPE_CONTAINER_ITEM_UNDEFINED } from './container-item';
 export {
   Container,
   TYPE_CONTAINER_BOX,
