762b49189045e799d4e4a4e238d33d6f36cfe688
Ard Schrijvers <ard.schrijvers@bloomreach.com>
2021-02-18 18:54:34 +0100

    CMS-14482 For one and the same Credentials instance, sync only once
    When there is a custom security provider, for example LDAP, after every
    JCR login a new sync with the external system is done and to the user
    node the 'last login' field is updated
    
    However, for a CMS user this only makes sense during the logging in into
    the CMS, not many times when working within the same (http session) in
    the CMS : Currently, every new JCR login (which happens for example
    during Channel Mgr interactions or /ws/ invocations) results in a sync,
    regardless whether it is a 'real' CMS login
    
    Technical Details
    Logging in new JCR Sessions for a logged in user happens all the time.
    This happens via the same Credentials instance (with nullified pwd)
    stored on the CmsSessionContext.
    
    Technical Solution
    After the first sync for a Credentials instance, do not sync again any
    more for a new login with the exact same instance

--- repository/engine/src/main/java/org/hippoecm/repository/security/AbstractSecurityProvider.java
+++ repository/engine/src/main/java/org/hippoecm/repository/security/AbstractSecurityProvider.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2008-2019 Hippo B.V. (http://www.onehippo.com)
+ *  Copyright 2008-2021 Hippo B.V. (http://www.onehippo.com)
  * 
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -24,8 +24,16 @@ import org.hippoecm.repository.security.group.DummyGroupManager;
 import org.hippoecm.repository.security.group.GroupManager;
 import org.hippoecm.repository.security.user.DummyUserManager;
 import org.hippoecm.repository.security.user.HippoUserManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static java.lang.Boolean.TRUE;
 
 public abstract class AbstractSecurityProvider implements SecurityProvider {
+
+    private static Logger log = LoggerFactory.getLogger(AbstractSecurityProvider.class.getName());
+
+    public static final String SYNCED_ATTR_NAME = AbstractSecurityProvider.class.getName() + ".synced";
 
     protected UserManager userManager = new DummyUserManager();
     protected GroupManager groupManager = new DummyGroupManager();
@@ -49,7 +57,18 @@ public abstract class AbstractSecurityProvider implements SecurityProvider {
         return groupManager;
     }
 
+    /**
+     * Synchronizes based on the {@code creds} object, however, the exact same {@code creds} instance
+     * will result only *ONCE* in an actual synchronize, to avoid potentially many sync for one and the
+     * same Credentials object when being reused to log in new {@link Session} instances
+     * @param creds
+     * @throws RepositoryException
+     */
     public void synchronizeOnLogin(SimpleCredentials creds) throws RepositoryException {
+        if (TRUE.equals(creds.getAttribute(SYNCED_ATTR_NAME))) {
+            log.info("Sync for user '{}' already done", creds.getUserID());
+        }
+        log.info("Sync user '{}'", creds.getUserID());
         // The sync blocks are synchronized because the underlying
         // methods can share the same jcr session and the jcr session is
         // not thread safe. This is a "best effort" solution as the usrMgr
@@ -61,6 +80,8 @@ public abstract class AbstractSecurityProvider implements SecurityProvider {
 
         final GroupManager groupMgr = getGroupManager();
         syncGroup(creds, userMgr, groupMgr);
+
+        creds.setAttribute(SYNCED_ATTR_NAME, TRUE);
     }
 
     protected void syncUser(final SimpleCredentials creds, final HippoUserManager userMgr) throws RepositoryException {
