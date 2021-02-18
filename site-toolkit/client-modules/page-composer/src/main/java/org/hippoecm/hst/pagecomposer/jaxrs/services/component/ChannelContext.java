765418c873aaca43c2643c447586d2890fa739f5
Jeroen Hoffman <jeroen.hoffman@bloomreach.com>
2021-02-18 18:06:29 +0100

    CMS-14299 improve the WARN with mount path instead of uuid, bit more accurate wording
    - also bump copyright year

--- site-toolkit/client-modules/page-composer/src/main/java/org/hippoecm/hst/pagecomposer/jaxrs/services/component/ChannelContext.java
+++ site-toolkit/client-modules/page-composer/src/main/java/org/hippoecm/hst/pagecomposer/jaxrs/services/component/ChannelContext.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 Bloomreach
+ * Copyright 2020-2021 Bloomreach
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -35,8 +35,8 @@ public class ChannelContext {
     private boolean crossChannelPageCopySupported;
     private boolean workspaceExists;
     private boolean hasPrototypes;
-    private Set<String> changeBySet =  new HashSet<>();
-    private Set<XPageLayout> xPageLayouts =  new HashSet<>();
+    private Set<String> changeBySet = new HashSet<>();
+    private Set<XPageLayout> xPageLayouts = new HashSet<>();
     private Map<String, String> xPageTemplateQueries;
 
     public String getChannelId() {
