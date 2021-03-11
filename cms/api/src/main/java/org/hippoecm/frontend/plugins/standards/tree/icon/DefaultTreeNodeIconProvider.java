0d72e1ca20b806048742fb7b3056423288741f27
Michiel Rop <michiel.rop@bloomreach.com>
2021-03-11 15:33:06 +0100

    CMS-14317 Add xpage icons in content perspective
    Add folder icons for closed and open xpage folder document.
    
    I left the hst:xpagemixin as a String literal, because adding a
    dependency on the hst api seems like an overkill.
    
    Resolved merge conflict in IconRenderUtil.java. The major difference
    between 14.x and release/saas here is that 14.x still uses the "template
    type" and release/saas "content type" for document types.
    
    (cherry picked from commit 144c31386788460364711dfa799b46a6b209f836)

--- cms/api/src/main/java/org/hippoecm/frontend/plugins/standards/tree/icon/DefaultTreeNodeIconProvider.java
+++ cms/api/src/main/java/org/hippoecm/frontend/plugins/standards/tree/icon/DefaultTreeNodeIconProvider.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2010-2014 Hippo B.V. (http://www.onehippo.com)
+ *  Copyright 2010-2021 Hippo B.V. (http://www.onehippo.com)
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -15,22 +15,45 @@
  */
 package org.hippoecm.frontend.plugins.standards.tree.icon;
 
+import javax.jcr.Node;
+import javax.jcr.RepositoryException;
 import javax.swing.tree.TreeNode;
 
 import org.apache.wicket.extensions.markup.html.tree.ITreeState;
 import org.hippoecm.frontend.plugins.standards.icon.HippoIcon;
+import org.hippoecm.frontend.plugins.standards.tree.FolderTreeNode;
 import org.hippoecm.frontend.skin.Icon;
+import org.hippoecm.repository.HippoStdNodeType;
+import org.hippoecm.repository.util.JcrUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class DefaultTreeNodeIconProvider extends AbstractJcrTreeNodeIconProvider {
 
-    private static final long serialVersionUID = 1L;
+    private static final Logger log = LoggerFactory.getLogger(DefaultTreeNodeIconProvider.class);
 
     public HippoIcon getNodeIcon(final String id, final TreeNode treeNode, final ITreeState state) {
         if (state.isNodeExpanded(treeNode)) {
-            return HippoIcon.fromSprite(id, Icon.FOLDER_OPEN);
-        } else {
-            return HippoIcon.fromSprite(id, Icon.FOLDER);
+            return isXPageFolder(treeNode)
+                    ? HippoIcon.fromSprite(id, Icon.XPAGE_FOLDER_OPEN)
+                    : HippoIcon.fromSprite(id, Icon.FOLDER_OPEN);
         }
+        return isXPageFolder(treeNode)
+                ? HippoIcon.fromSprite(id, Icon.XPAGE_FOLDER)
+                : HippoIcon.fromSprite(id, Icon.FOLDER);
+    }
+
+    private static boolean isXPageFolder(TreeNode treeNode){
+        if (treeNode instanceof FolderTreeNode){
+            FolderTreeNode folderTreeNode = ((FolderTreeNode) treeNode);
+            final Node node = folderTreeNode.getNodeModel().getObject();
+            try {
+                return node.isNodeType(HippoStdNodeType.NT_XPAGE_FOLDER);
+            } catch (RepositoryException exception) {
+                log.warn("Could not determine node type for node: { path: {} }", JcrUtils.getNodePathQuietly(node));
+            }
+        }
+        return false;
     }
 
 }
