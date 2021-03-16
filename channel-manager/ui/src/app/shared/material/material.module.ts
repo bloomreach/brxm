b9e4d0d226cbcf90c9c976f500ccf8aed0433e3a
Joeri de Gooijer <joeri.degooijer@bloomreach.com>
2021-03-16 08:00:18 +0100

    ENT-2493 Use mat-toolbar to wrap actions in experiments footer

--- channel-manager/ui/src/app/shared/material/material.module.ts
+++ channel-manager/ui/src/app/shared/material/material.module.ts
@@ -27,6 +27,7 @@ import { MatMenuModule } from '@angular/material/menu';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatSelectModule } from '@angular/material/select';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatTreeModule } from '@angular/material/tree';
 import { DomSanitizer } from '@angular/platform-browser';
@@ -42,13 +43,14 @@ import { registerSvgIcons } from './register-svg-icons';
     MatIconModule,
     MatInputModule,
     MatListModule,
+    MatMenuModule,
     MatProgressBarModule,
     MatRippleModule,
     MatSelectModule,
     MatSnackBarModule,
+    MatToolbarModule,
     MatTooltipModule,
     MatTreeModule,
-    MatMenuModule,
   ],
 })
 export class MaterialModule {
