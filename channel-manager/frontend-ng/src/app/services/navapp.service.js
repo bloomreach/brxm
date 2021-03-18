1035a1da102d8b570c78fa041e56b65d0410533b
Joeri de Gooijer <joeri.degooijer@bloomreach.com>
2021-03-18 16:42:22 +0100

    CMS-14607 Create navapp service for navapp communications
    (cherry picked from commit 56e9a1fa081d2d0294f03234c599a319aaf2112e)

---
+++ channel-manager/frontend-ng/src/app/services/navapp.service.js
@@ -0,0 +1,140 @@
+// Copyright 2021 Bloomreach B.V. (https://www.bloomreach.com/)
+
+import {
+  connectToParent,
+  getVersion,
+} from '@bloomreach/navapp-communication';
+
+class NavappService {
+  constructor(
+    $log,
+  ) {
+    'ngInject';
+
+    this.$log = $log;
+
+    this.childConfig = {
+      apiVersion: getVersion(),
+    };
+
+    this._childApi = {
+      getConfig: async () => this.childConfig,
+      beforeNavigation: async () => this._resolveChildApiCall('beforeNavigation'),
+      navigate: async (...args) => this._resolveChildApiCall('navigate', ...args),
+      beforeLogout: async () => this._resolveChildApiCall('beforeLogout'),
+      logout: async () => this._resolveChildApiCall('logout'),
+    };
+
+    // Map<string, Set<() => Promise>>
+    this._apiSubscriptions = new Map();
+  }
+
+  subscribe(api, callback) {
+    if (!(api in this._childApi)) {
+      throw Error(`Attempted to subscribe to unknown navapp ChildApi api: ${api}`);
+    }
+
+    if (!this._apiSubscriptions.has(api)) {
+      this._apiSubscriptions.set(api, new Set());
+    }
+
+    const subscriptions = this._apiSubscriptions.get(api);
+    subscriptions.add(callback);
+  }
+
+  unsubscribe(api, callback) {
+    if (!(api in this._childApi)) {
+      throw Error(`Attempted to unsubscribe to unknown navapp ChildApi api: ${api}`);
+    }
+
+    const subscriptions = this._apiSubscriptions.get(api);
+    subscriptions.delete(callback);
+  }
+
+  async connect() {
+    if (this.connectionPromise) {
+      await this.connectionPromise;
+      return;
+    }
+
+    this.connectionPromise = this._establishConnectionToNavapp();
+    this.api = await this.connectionPromise;
+  }
+
+  async getConfig() {
+    await this.connect();
+    return this.api.getConfig();
+  }
+
+  async getUserSettings() {
+    await this.connect();
+    const settings = await this.getConfig();
+    return settings.userSettings;
+  }
+
+  async updateNavLocation(location) {
+    await this.connect();
+    return this.api.updateNavLocation(location);
+  }
+
+  async navigate(location) {
+    await this.connect();
+    return this.api.navigate(location);
+  }
+
+  async showMask() {
+    await this.connect();
+    return this.api.showMask();
+  }
+
+  async hideMask() {
+    await this.connect();
+    return this.api.hideMask();
+  }
+
+  async showBusyIndicator() {
+    await this.connect();
+    return this.api.showBusyIndicator();
+  }
+
+  async hideBusyIndicator() {
+    await this.connect();
+    return this.api.hideBusyIndicator();
+  }
+
+  async onUserActivity() {
+    await this.connect();
+    return this.api.onUserActivity();
+  }
+
+  async onSessionExpired() {
+    await this.connect();
+    return this.api.onSessionExpired();
+  }
+
+  async onError(error) {
+    await this.connect();
+    return this.api.onError(error);
+  }
+
+  async _establishConnectionToNavapp() {
+    return connectToParent({
+      parentOrigin: window.location.origin,
+      methods: this._childApi,
+    });
+  }
+
+  async _resolveChildApiCall(api, ...args) {
+    if (!this._apiSubscriptions.has(api)) {
+      this.$log.info(`Called ${api} on Experience Manager childApi but it does not have any subscribed actions`);
+      return Promise.resolve();
+    }
+
+    const subscriptions = this._apiSubscriptions.get(api);
+    const promises = [];
+    subscriptions.forEach(cb => promises.push(cb(...args)));
+    return Promise.all(promises);
+  }
+}
+
+export default NavappService;
