20d7a5e4eaaa84ffde43e949466a0728b71cb7e5
Joeri de Gooijer <joeri.degooijer@bloomreach.com>
2021-04-14 17:35:45 +0200

    CMS-14604 Split version info component template into multiple components
    (cherry picked from commit 8002ce24c2c883e82d21b20b142c19a3d2f09552)

---
+++ channel-manager/ui/src/app/versions/components/latest-version/latest-version.component.ts
@@ -0,0 +1,47 @@
+/*!
+ * Copyright 2021 Bloomreach. All rights reserved. (https://www.bloomreach.com/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+import { Version } from '../../models/version.model';
+
+@Component({
+  selector: 'em-latest-version',
+  templateUrl: './latest-version.component.html',
+  styleUrls: ['./latest-version.component.scss'],
+})
+export class LatestVersionComponent implements OnInit {
+  @Input()
+  version?: Version;
+
+  @Input()
+  isSelected?: boolean;
+
+  @Input()
+  createEnabled?: boolean;
+
+  @Input()
+  actionInProgress?: boolean;
+
+  @Output()
+  createVersion = new EventEmitter<void>();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
