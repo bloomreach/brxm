46e01db2e0c3d2da9fcb21c862a856632bc62079
Joeri de Gooijer <joeri.degooijer@bloomreach.com>
2021-04-15 17:12:53 +0200

    CMS-14604 Use to observable based versions state
    (cherry picked from commit 0296f4c750379fbff71f6eeb1ff79310326d1ee9)

--- channel-manager/ui/src/app/versions/components/latest-version/latest-version.component.spec.ts
+++ channel-manager/ui/src/app/versions/components/latest-version/latest-version.component.spec.ts
@@ -1,5 +1,5 @@
 /*!
- * Copyright 2020 Bloomreach. All rights reserved. (https://www.bloomreach.com/)
+ * Copyright 2021 Bloomreach. All rights reserved. (https://www.bloomreach.com/)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,33 +15,89 @@
  */
 
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatIconTestingModule } from '@angular/material/icon/testing';
 import { TranslateModule } from '@ngx-translate/core';
+
+import { NG1_UI_ROUTER_GLOBALS } from '../../../services/ng1/ui-router-globals.ng1.service';
+import { Ng1WorkflowService, NG1_WORKFLOW_SERVICE } from '../../../services/ng1/workflow.ng1.service';
+import { Version } from '../../models/version.model';
+import { VersionsInfo } from '../../models/versions-info.model';
+import { VersionsService } from '../../services/versions.service';
 
 import { LatestVersionComponent } from './latest-version.component';
 
 describe('LatestVersionComponent', () => {
   let component: LatestVersionComponent;
   let fixture: ComponentFixture<LatestVersionComponent>;
+  let componentEl: HTMLElement;
+
+  let ng1WorkflowService: Ng1WorkflowService;
+  let versionsService: VersionsService;
+
+  const date = Date.parse('11/08/2020 16:03');
+  const documentId = 'testDocumentId';
+  const firstVersionUUID = 'testId';
+  const mockVersion = {
+    jcrUUID: firstVersionUUID,
+    branchId: 'master',
+    userName: 'testUserName',
+    timestamp: date,
+  } as Version;
+
+  const mockVersionsInfo = {
+    versions: [ mockVersion ],
+    restoreEnabled: true,
+    createEnabled: true,
+    labelEnabled: true,
+    campaignEnabled: true,
+  } as VersionsInfo;
 
   beforeEach(async(() => {
+    const workflowServiceMock = {
+      createWorkflowAction: jest.fn(() => { }),
+    };
+
+    const versionsServiceMock = {
+      getVersionsInfo: jest.fn(() => Promise.resolve()),
+    };
+
+    const uiRouterGlobalsMock = {
+      params: {
+        documentId,
+      },
+    };
+
     TestBed.configureTestingModule({
+      declarations: [
+        LatestVersionComponent,
+      ],
       imports: [
-        MatButtonModule,
         MatIconModule,
         MatIconTestingModule,
         TranslateModule.forRoot(),
       ],
-      declarations: [ LatestVersionComponent ],
+      providers: [
+       { provide: NG1_WORKFLOW_SERVICE, useValue: workflowServiceMock },
+       { provide: NG1_UI_ROUTER_GLOBALS, useValue: uiRouterGlobalsMock },
+       { provide: VersionsService, useValue: versionsServiceMock },
+       ],
     })
-    .compileComponents();
+      .compileComponents();
+
+    ng1WorkflowService = TestBed.inject(NG1_WORKFLOW_SERVICE);
+    versionsService = TestBed.inject(VersionsService);
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(LatestVersionComponent);
     component = fixture.componentInstance;
+    componentEl = fixture.nativeElement;
+
+    component.versionsInfo = mockVersionsInfo;
+    component.version = mockVersion;
+    component.isSelected = false;
+
     fixture.detectChanges();
   });
 
--- channel-manager/ui/src/app/versions/components/version/version.component.ts
+++ channel-manager/ui/src/app/versions/components/version/version.component.ts
@@ -14,34 +14,45 @@
  * limitations under the License.
  */
 
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Inject, Input, OnInit, Output } from '@angular/core';
+import { UIRouterGlobals } from '@uirouter/core';
 
+import { Ng1IframeService, NG1_IFRAME_SERVICE } from '../../../services/ng1/iframe.ng1service';
+import { NG1_UI_ROUTER_GLOBALS } from '../../../services/ng1/ui-router-globals.ng1.service';
+import { Ng1WorkflowService, NG1_WORKFLOW_SERVICE } from '../../../services/ng1/workflow.ng1.service';
 import { Version } from '../../models/version.model';
+import { VersionsInfo } from '../../models/versions-info.model';
+import { VersionsService } from '../../services/versions.service';
 
 @Component({
   selector: 'em-version',
   templateUrl: './version.component.html',
   styleUrls: ['./version.component.scss'],
 })
-export class VersionComponent implements OnInit {
+export class VersionComponent {
   @Input()
-  version?: Version;
-
-  @Input()
-  isSelected?: boolean;
+  version!: Version;
 
   @Input()
-  actionInProgress?: boolean;
+  versionsInfo!: VersionsInfo;
 
   @Input()
-  restoreEnabled?: boolean;
+  isSelected!: boolean;
 
-  @Output()
-  restoreVersion = new EventEmitter<string>();
+  @Input()
+  renderPath!: string;
 
-  constructor() { }
+  constructor(
+    @Inject(NG1_IFRAME_SERVICE) private readonly ng1IframeService: Ng1IframeService,
+    @Inject(NG1_WORKFLOW_SERVICE) private readonly ng1WorkflowService: Ng1WorkflowService,
+    @Inject(NG1_UI_ROUTER_GLOBALS) private readonly ng1UiRouterGlobals: UIRouterGlobals,
+    private readonly versionsService: VersionsService,
+  ) { }
 
-  ngOnInit(): void {
+  async restoreVersion(versionUUID: string): Promise<void> {
+    const documentId = this.ng1UiRouterGlobals.params.documentId;
+    await this.ng1WorkflowService.createWorkflowAction(documentId, {}, 'restore', versionUUID);
+    await this.ng1IframeService.load(this.renderPath);
+    await this.versionsService.getVersionsInfo(documentId);
   }
-
 }
