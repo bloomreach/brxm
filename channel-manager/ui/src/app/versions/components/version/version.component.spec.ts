631ebb2ae17e74b0ee1b004e7ca1d0ee98841607
Joeri de Gooijer <joeri.degooijer@bloomreach.com>
2021-04-15 17:12:53 +0200

    CMS-14604 Use to observable based versions state
    (cherry picked from commit 7331f13b3f19b0110bc64359583996d8b6c166b7)

--- channel-manager/ui/src/app/versions/components/version/version.component.spec.ts
+++ channel-manager/ui/src/app/versions/components/version/version.component.spec.ts
@@ -20,6 +20,13 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatIconTestingModule } from '@angular/material/icon/testing';
 import { TranslateModule } from '@ngx-translate/core';
 
+import { Ng1IframeService, NG1_IFRAME_SERVICE } from '../../../services/ng1/iframe.ng1service';
+import { NG1_UI_ROUTER_GLOBALS } from '../../../services/ng1/ui-router-globals.ng1.service';
+import { Ng1WorkflowService, NG1_WORKFLOW_SERVICE } from '../../../services/ng1/workflow.ng1.service';
+import { Version } from '../../models/version.model';
+import { VersionsInfo } from '../../models/versions-info.model';
+import { VersionsService } from '../../services/versions.service';
+
 import { VersionComponent } from './version.component';
 
 @Pipe({ name: 'moment' })
@@ -34,7 +41,49 @@ describe('VersionComponent', () => {
   let fixture: ComponentFixture<VersionComponent>;
   let componentEl: HTMLElement;
 
+  let ng1IframeService: Ng1IframeService;
+  let versionsService: VersionsService;
+  let ng1WorkflowService: Ng1WorkflowService;
+
+  const date = Date.parse('11/08/2020 16:03');
+  const path = '/some/test/path';
+  const renderPath = `${path}?withParam=test`;
+  const documentId = 'testDocumentId';
+  const firstVersionUUID = 'testId';
+  const mockVersion = {
+    jcrUUID: firstVersionUUID,
+    branchId: 'master',
+    userName: 'testUserName',
+    timestamp: date,
+  } as Version;
+
+  const mockVersionsInfo = {
+    versions: [ mockVersion ],
+    restoreEnabled: true,
+    createEnabled: true,
+    labelEnabled: true,
+    campaignEnabled: true,
+  } as VersionsInfo;
+
   beforeEach(async(() => {
+    const iframeServiceMock = {
+      load: jest.fn(() => Promise.resolve()),
+    };
+
+    const workflowServiceMock = {
+      createWorkflowAction: jest.fn(() => Promise.resolve()),
+    };
+
+    const versionsServiceMock = {
+      getVersionsInfo: () => Promise.resolve(),
+    };
+
+    const uiRouterGlobalsMock = {
+      params: {
+        documentId,
+      },
+    };
+
     TestBed.configureTestingModule({
       declarations: [
         VersionComponent,
@@ -45,15 +94,29 @@ describe('VersionComponent', () => {
         MatIconTestingModule,
         TranslateModule.forRoot(),
       ],
-      providers: [ ],
+      providers: [
+       { provide: NG1_IFRAME_SERVICE, useValue: iframeServiceMock },
+       { provide: NG1_WORKFLOW_SERVICE, useValue: workflowServiceMock },
+       { provide: NG1_UI_ROUTER_GLOBALS, useValue: uiRouterGlobalsMock },
+       { provide: VersionsService, useValue: versionsServiceMock },
+       ],
     })
       .compileComponents();
+
+    ng1WorkflowService = TestBed.inject(NG1_WORKFLOW_SERVICE);
+    ng1IframeService = TestBed.inject(NG1_IFRAME_SERVICE);
+    versionsService = TestBed.inject(VersionsService);
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(VersionComponent);
     component = fixture.componentInstance;
     componentEl = fixture.nativeElement;
+
+    component.versionsInfo = mockVersionsInfo;
+    component.version = mockVersion;
+    component.renderPath = renderPath;
+    component.isSelected = false;
 
     fixture.detectChanges();
   });
