0a4d7cf6ac23589e6a303654ab94aa90e267d2da
Joeri de Gooijer <joeri.degooijer@bloomreach.com>
2021-04-26 12:52:55 +0200

    CMS-14609 Add ability to set a label to the version creation flow
    (cherry picked from commit 4f20724f3c4ef502dcac8ccdd74e814179aa708b)

--- channel-manager/ui/src/app/versions/components/latest-version/latest-version.component.ts
+++ channel-manager/ui/src/app/versions/components/latest-version/latest-version.component.ts
@@ -14,10 +14,15 @@
  * limitations under the License.
  */
 
-import { Component, EventEmitter, HostBinding, Inject, Input, Output } from '@angular/core';
+import { Component, EventEmitter, HostBinding, Inject, Input, OnChanges, OnDestroy, OnInit, Output } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 import { UIRouterGlobals } from '@uirouter/core';
+import { interval, Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
+import { DateService } from '../../../services/date.service';
 import { DocumentWorkflowService } from '../../../services/document-workflow.service';
+import { Ng1NavappService, NG1_NAVAPP_SERVICE } from '../../../services/ng1/navapp.ng1.service';
 import { NG1_UI_ROUTER_GLOBALS } from '../../../services/ng1/ui-router-globals.ng1.service';
 import { Version } from '../../models/version.model';
 import { VersionsInfo } from '../../models/versions-info.model';
@@ -28,7 +33,9 @@ import { VersionsService } from '../../services/versions.service';
   templateUrl: './latest-version.component.html',
   styleUrls: ['./latest-version.component.scss'],
 })
-export class LatestVersionComponent {
+export class LatestVersionComponent implements OnChanges, OnInit, OnDestroy {
+  private readonly unsubscribe = new Subject();
+
   @Input()
   version!: Version;
 
@@ -49,16 +56,52 @@ export class LatestVersionComponent {
 
   private readonly documentId = this.ng1UiRouterGlobals.params.documentId;
 
+  isCreateFormVisible = false;
+
+  createForm = this.fb.group({
+    label: [''],
+  });
+
+  currentDate = this.dateService.getCurrentDate();
+
+  currentUserName?: string;
+
   constructor(
     @Inject(NG1_UI_ROUTER_GLOBALS) private readonly ng1UiRouterGlobals: UIRouterGlobals,
     private readonly versionsService: VersionsService,
     private readonly documentWorkflowService: DocumentWorkflowService,
+    private readonly fb: FormBuilder,
+    private readonly dateService: DateService,
+    @Inject(NG1_NAVAPP_SERVICE) private readonly navappService: Ng1NavappService,
   ) { }
+
+  async ngOnInit(): Promise<void> {
+    const { userName } = await this.navappService.getUserSettings();
+    this.currentUserName = userName;
+
+    interval(1000)
+      .pipe(takeUntil(this.unsubscribe))
+      .subscribe(() => {
+      this.currentDate = this.dateService.getCurrentDate();
+    });
+  }
+
+  ngOnChanges(): void {
+    this.isCreateFormVisible = false;
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribe.next();
+    this.unsubscribe.complete();
+  }
 
   async createVersion(): Promise<void> {
     this.actionInProgressChange.emit(true);
-    await this.documentWorkflowService.postAction(this.documentId, ['version']);
+
+    const label = this.createForm.get('label')?.value;
+    await this.documentWorkflowService.postAction(this.documentId, ['version'], { label });
     this.versionsService.getVersionsInfo(this.ng1UiRouterGlobals.params.documentId);
+
     this.actionInProgressChange.emit(false);
   }
 }
