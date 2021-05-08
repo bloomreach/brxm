a292e7e7d666002af46254dcfd3e6ceefe0c93bd
Arent-Jan Banck <arent-jan.banck@bloomreach.com>
2021-05-08 09:04:08 +0200

    CMS-14634 Use try-with-resources or close classes that implement Closeable in a "finally" clause

--- configuration-management/model/src/main/java/org/onehippo/cm/model/parser/ConfigurationVerifier.java
+++ configuration-management/model/src/main/java/org/onehippo/cm/model/parser/ConfigurationVerifier.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2017 Hippo B.V. (http://www.onehippo.com)
+ *  Copyright 2021 Hippo B.V. (http://www.onehippo.com)
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -33,8 +33,9 @@ public class ConfigurationVerifier {
         final Path moduleDescriptorPath = Paths.get(args[0]);
         final ModuleContext result = new ModuleReader().read(moduleDescriptorPath, true);
 
-        new ConfigurationModelImpl()
-            .addModule(result.getModule())
-            .build();
+        try (ConfigurationModelImpl configurationModelImpl = new ConfigurationModelImpl()) {
+            configurationModelImpl.addModule(result.getModule())
+                    .build();
+        }
     }
 }
