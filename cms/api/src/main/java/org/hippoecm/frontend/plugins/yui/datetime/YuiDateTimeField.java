8c62fba03aa29eb4480a2fd181e949942051e776
Arent-Jan Banck <arent-jan.banck@bloomreach.com>
2021-08-29 14:31:03 +0200

    CMS-14959 Bump Wicket and Wicket datetime to 8.12
    Move Wicket datetime from org.apache.wicket to org.wicketstuff and use
    the same version as Wicket.
    
    (cherry picked from commit 4ba879db401bee41c3740323af44ea24fe03b733)

--- cms/api/src/main/java/org/hippoecm/frontend/plugins/yui/datetime/YuiDateTimeField.java
+++ cms/api/src/main/java/org/hippoecm/frontend/plugins/yui/datetime/YuiDateTimeField.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2008-2020 Hippo B.V. (http://www.onehippo.com)
+ *  Copyright 2008-2021 Hippo B.V. (http://www.onehippo.com)
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -23,11 +23,11 @@ import org.apache.wicket.AttributeModifier;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.ajax.form.AjaxFormComponentUpdatingBehavior;
 import org.apache.wicket.ajax.markup.html.AjaxLink;
-import org.apache.wicket.datetime.DateConverter;
-import org.apache.wicket.datetime.markup.html.form.DateTextField;
+import org.wicketstuff.datetime.DateConverter;
+import org.wicketstuff.datetime.markup.html.form.DateTextField;
 import org.apache.wicket.event.Broadcast;
-import org.apache.wicket.extensions.yui.calendar.DatePicker;
-import org.apache.wicket.extensions.yui.calendar.DateTimeField;
+import org.wicketstuff.datetime.extensions.yui.calendar.DatePicker;
+import org.wicketstuff.datetime.extensions.yui.calendar.DateTimeField;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.model.IModel;
@@ -213,7 +213,7 @@ public class YuiDateTimeField extends DateTimeField {
     }
 
     @Override
-    protected DateTextField newDateTextField(final String id, final PropertyModel<Date> dateFieldModel) {
+    protected DateTextField newDateTextField(final String id, final IModel<Date> dateFieldModel) {
         return new DateTextField(id, dateFieldModel, new DateConverter(false) {
             @Override
             public String getDatePattern(final Locale locale) {
