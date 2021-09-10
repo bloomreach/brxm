d6ec85918107bcd9ff61a6ff834f233e4480e796
Vasili Kauryha <vasili.kauryha@bloomreach.com>
2021-09-10 17:12:49 +0200

    ENT-3541 Highlight sub menu items with a error color
    (cherry picked from commit b5c071bb14738669fe4b3729c2c83a8528976c34)

--- navigation-application/projects/navapp/src/app/main-menu/components/expandable-sub-menu-item/expandable-sub-menu-item.component.spec.ts
+++ navigation-application/projects/navapp/src/app/main-menu/components/expandable-sub-menu-item/expandable-sub-menu-item.component.spec.ts
@@ -48,6 +48,7 @@ describe('ExpandableSubMenuItemComponent', () => {
   beforeEach(waitForAsync(() => {
     menuStateServiceMock = jasmine.createSpyObj('MenuStateService', [
       'isMenuItemHighlighted',
+      'isMenuItemFailed',
     ]);
 
     qaHelperServiceMock = jasmine.createSpyObj('QaHelperService', [
