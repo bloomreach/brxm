c8f0932ea92ff627d8f130889984aa7546df7ad0
Michiel Rop <michiel.rop@bloomreach.com>
2021-10-14 15:39:33 +0200

    CMS-14854 Prevent non-whitelisted nodes
    Throw an exception if a non-whitelisted element or attribute is found in
    the xml.
    
    Use the SAX parser, because the scaling of svg's also uses the SAX
    parser. If we would be lenient towards malformed xml during the upload
    of the gallery item, the scaling would still fail.
    
    The owasp library is non present in any of our used maven repositories,
    the SAX parser is part of the java core. So the SAX parser has preference.
    
    (cherry picked from commit 31532bc5109f8b2296020ec7fade609c9f0c7a69)
    (cherry picked from commit b8c38610dd2394d4a9e451806e63fa9cc454c166)

--- cms/gallery/frontend/src/main/java/org/hippoecm/frontend/plugins/gallery/GalleryWorkflowPlugin.java
+++ cms/gallery/frontend/src/main/java/org/hippoecm/frontend/plugins/gallery/GalleryWorkflowPlugin.java
@@ -25,6 +25,7 @@ import java.util.Objects;
 
 import javax.jcr.Node;
 import javax.jcr.RepositoryException;
+import javax.xml.parsers.ParserConfigurationException;
 
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang.StringUtils;
@@ -54,6 +55,7 @@ import org.hippoecm.frontend.plugin.config.IPluginConfig;
 import org.hippoecm.frontend.plugins.gallery.model.DefaultGalleryProcessor;
 import org.hippoecm.frontend.plugins.gallery.model.GalleryException;
 import org.hippoecm.frontend.plugins.gallery.model.GalleryProcessor;
+import org.hippoecm.frontend.plugins.gallery.model.SvgGalleryException;
 import org.hippoecm.frontend.plugins.jquery.upload.multiple.JQueryFileUploadDialog;
 import org.hippoecm.frontend.plugins.standards.icon.HippoIconStack;
 import org.hippoecm.frontend.plugins.standards.icon.HippoIconStack.Position;
@@ -64,7 +66,7 @@ import org.hippoecm.frontend.skin.CmsIcon;
 import org.hippoecm.frontend.skin.Icon;
 import org.hippoecm.frontend.translation.ILocaleProvider;
 import org.hippoecm.frontend.util.CodecUtils;
-import org.hippoecm.frontend.validation.SvgValidationException;
+import org.hippoecm.frontend.validation.SvgValidationResult;
 import org.hippoecm.frontend.validation.SvgValidator;
 import org.hippoecm.frontend.widgets.AbstractView;
 import org.hippoecm.repository.api.Document;
@@ -77,6 +79,7 @@ import org.hippoecm.repository.gallery.GalleryWorkflow;
 import org.hippoecm.repository.standardworkflow.DefaultWorkflow;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.xml.sax.SAXException;
 
 public class GalleryWorkflowPlugin extends CompatibilityWorkflowPlugin<GalleryWorkflow> {
     private static final long serialVersionUID = 1L;
@@ -147,12 +150,18 @@ public class GalleryWorkflowPlugin extends CompatibilityWorkflowPlugin<GalleryWo
                 final boolean svgScriptsEnabled = GalleryWorkflowPlugin.this.getPluginConfig()
                         .getAsBoolean(SVG_SCRIPTS_ENABLED, false);
                 if (!svgScriptsEnabled && Objects.equals(mimeType, SVG_MIME_TYPE)) {
+                    final SvgValidationResult svgValidationResult;
                     try {
-                        SvgValidator.validate(new String(upload.getBytes()));
+                        svgValidationResult = SvgValidator.validate(is);
+                        if (!(svgValidationResult.getOffendingAttributes()
+                                .isEmpty() && svgValidationResult.getOffendingElements().isEmpty())){
+                            throw new SvgGalleryException("Validation did not pass", svgValidationResult);
+                        }
+                    } catch (ParserConfigurationException | SAXException e) {
+                        log.error("Something went wrong during the upload of the svg", e);
+                        throw new GalleryException("Something went wrong during the validation", e);
                     }
-                    catch ( SvgValidationException svgValidationException){
-                        throw svgValidationException.getGalleryException();
-                    }
+
                 }
 
                 WorkflowDescriptorModel workflowDescriptorModel = (WorkflowDescriptorModel) GalleryWorkflowPlugin.this
