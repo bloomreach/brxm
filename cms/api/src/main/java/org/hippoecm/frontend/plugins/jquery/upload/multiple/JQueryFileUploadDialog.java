0f70d7d4f12f1de76df915a4eb6de9b6128859d4
Michiel Rop <michiel.rop@bloomreach.com>
2021-10-14 15:39:33 +0200

    CMS-14855 Prevent non-whitelisted nodes
    Throw an exception if a non-whitelisted element or attribute is found in
    the xml.
    
    Use the SAX parser, because the scaling of svg's also uses the SAX
    parser. If we would be lenient towards malformed xml during the upload
    of the gallery item, the scaling would still fail.
    
    The owasp library is non present in any of our used maven repositories,
    the SAX parser is part of the java core. So the SAX parser has preference.
    
    (cherry picked from commit bd739373f5c8cd11206dd7491559f0f180437e69)

--- cms/api/src/main/java/org/hippoecm/frontend/plugins/jquery/upload/multiple/JQueryFileUploadDialog.java
+++ cms/api/src/main/java/org/hippoecm/frontend/plugins/jquery/upload/multiple/JQueryFileUploadDialog.java
@@ -18,12 +18,13 @@ package org.hippoecm.frontend.plugins.jquery.upload.multiple;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.apache.commons.lang.StringUtils;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.ajax.markup.html.form.AjaxButton;
 import org.apache.wicket.markup.html.form.Button;
-import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.form.upload.FileUpload;
 import org.apache.wicket.model.ResourceModel;
 import org.apache.commons.fileupload.FileUploadException;
@@ -32,9 +33,11 @@ import org.hippoecm.frontend.dialog.Dialog;
 import org.hippoecm.frontend.dialog.DialogConstants;
 import org.hippoecm.frontend.plugin.IPluginContext;
 import org.hippoecm.frontend.plugin.config.IPluginConfig;
+import org.hippoecm.frontend.plugins.gallery.model.SvgGalleryException;
 import org.hippoecm.frontend.plugins.jquery.upload.FileUploadViolationException;
 import org.hippoecm.frontend.plugins.yui.upload.validation.FileUploadValidationService;
 import org.hippoecm.frontend.plugins.yui.upload.validation.ImageUploadValidationService;
+import org.hippoecm.frontend.validation.SvgValidationResult;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -140,7 +143,15 @@ public abstract class JQueryFileUploadDialog extends Dialog {
             List<String> errors = new ArrayList<>();
             Throwable t = e;
             while(t != null) {
-                final String translatedMessage = (String) getExceptionTranslation(t, file.getClientFileName()).getObject();
+                Object[] parameters;
+                if (t instanceof SvgGalleryException) {
+                    SvgGalleryException svgGalleryException = (SvgGalleryException) t;
+                    final SvgValidationResult validationResult = svgGalleryException.getValidationResult();
+                    parameters = new Object[]{getCommaSeparatedList(validationResult)};
+                } else {
+                    parameters = new Object[]{file.getClientFileName()};
+                }
+                final String translatedMessage = (String) getExceptionTranslation(t, parameters).getObject();
                 if (translatedMessage != null && !errors.contains(translatedMessage)) {
                     errors.add(translatedMessage);
                 }
@@ -153,6 +164,11 @@ public abstract class JQueryFileUploadDialog extends Dialog {
             }
             throw new FileUploadViolationException(errors);
         }
+    }
+
+    private String getCommaSeparatedList(final SvgValidationResult validationResult) {
+        return Stream.concat(validationResult.getOffendingElements().stream(),
+                validationResult.getOffendingAttributes().stream()).collect(Collectors.joining(","));
     }
 
     /**
