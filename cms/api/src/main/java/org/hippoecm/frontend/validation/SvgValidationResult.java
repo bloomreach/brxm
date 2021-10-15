e722cbde3f12b849ce2775a3341df80a521d79ce
Michiel Rop <michiel.rop@bloomreach.com>
2021-10-15 15:18:45 +0200

    CMS-14855 Improve feedback message
    Improve feedback message and maintainability.
    
    (cherry picked from commit 3cb5ecad32a85a595ac7c171c17dc3da8d01a360)

--- cms/api/src/main/java/org/hippoecm/frontend/validation/SvgValidationResult.java
+++ cms/api/src/main/java/org/hippoecm/frontend/validation/SvgValidationResult.java
@@ -1,5 +1,19 @@
+/*
+ * Copyright 2021 Bloomreach Inc. (www.bloomreach.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.hippoecm.frontend.validation;
-
 
 import java.util.Set;
 
@@ -17,5 +31,9 @@ public class SvgValidationResult {
     Set<String> offendingElements;
     @Singular
     Set<String> offendingAttributes;
+
+    public boolean isValid(){
+        return offendingAttributes.isEmpty() && offendingElements.isEmpty();
+    }
 
 }
