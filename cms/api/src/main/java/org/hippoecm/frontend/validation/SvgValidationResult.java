c7c3ba195b3ac25e5d98be02f3e65bad91ca06e1
Michiel Rop <michiel.rop@bloomreach.com>
2021-10-28 09:16:50 +0200

    CMS-14855 Remove lombok dependency
    For release/14.x we don't have a lombok dependency, so remove the
    dependency and use plain old java for the builder and slf4j.
    
    (cherry picked from commit ad56a2aeec81b09864f8b1b989082ac2fdfaca57)

--- cms/api/src/main/java/org/hippoecm/frontend/validation/SvgValidationResult.java
+++ cms/api/src/main/java/org/hippoecm/frontend/validation/SvgValidationResult.java
@@ -15,25 +15,125 @@
  */
 package org.hippoecm.frontend.validation;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Set;
 
-import lombok.Builder;
-import lombok.Singular;
-import lombok.ToString;
-import lombok.Value;
+public final class SvgValidationResult {
 
-@Value
-@Builder
-@ToString
-public class SvgValidationResult {
+    private final Set<String> offendingElements;
+    private final Set<String> offendingAttributes;
 
-    @Singular
-    Set<String> offendingElements;
-    @Singular
-    Set<String> offendingAttributes;
+    SvgValidationResult(Set<String> offendingElements, Set<String> offendingAttributes) {
+        this.offendingElements = offendingElements;
+        this.offendingAttributes = offendingAttributes;
+    }
+
+    public static SvgValidationResultBuilder builder() {
+        return new SvgValidationResultBuilder();
+    }
 
     public boolean isValid(){
         return offendingAttributes.isEmpty() && offendingElements.isEmpty();
     }
 
+    public Set<String> getOffendingElements() {
+        return this.offendingElements;
+    }
+
+    public Set<String> getOffendingAttributes() {
+        return this.offendingAttributes;
+    }
+
+    public static class SvgValidationResultBuilder {
+        private ArrayList<String> offendingElements;
+        private ArrayList<String> offendingAttributes;
+
+        SvgValidationResultBuilder() {
+        }
+
+        public org.hippoecm.frontend.validation.SvgValidationResult.SvgValidationResultBuilder offendingElement(
+                String offendingElement) {
+            if (this.offendingElements == null) {
+                this.offendingElements = new ArrayList<String>();
+            }
+            this.offendingElements.add(offendingElement);
+            return this;
+        }
+
+        public org.hippoecm.frontend.validation.SvgValidationResult.SvgValidationResultBuilder offendingElements(
+                Collection<? extends String> offendingElements) {
+            if (this.offendingElements == null) {
+                this.offendingElements = new ArrayList<String>();
+            }
+            this.offendingElements.addAll(offendingElements);
+            return this;
+        }
+
+        public org.hippoecm.frontend.validation.SvgValidationResult.SvgValidationResultBuilder clearOffendingElements() {
+            if (this.offendingElements != null) {
+                this.offendingElements.clear();
+            }
+            return this;
+        }
+
+        public org.hippoecm.frontend.validation.SvgValidationResult.SvgValidationResultBuilder offendingAttribute(
+                String offendingAttribute) {
+            if (this.offendingAttributes == null) {
+                this.offendingAttributes = new ArrayList<String>();
+            }
+            this.offendingAttributes.add(offendingAttribute);
+            return this;
+        }
+
+        public org.hippoecm.frontend.validation.SvgValidationResult.SvgValidationResultBuilder offendingAttributes(
+                Collection<? extends String> offendingAttributes) {
+            if (this.offendingAttributes == null) {
+                this.offendingAttributes = new ArrayList<String>();
+            }
+            this.offendingAttributes.addAll(offendingAttributes);
+            return this;
+        }
+
+        public org.hippoecm.frontend.validation.SvgValidationResult.SvgValidationResultBuilder clearOffendingAttributes() {
+            if (this.offendingAttributes != null) {
+                this.offendingAttributes.clear();
+            }
+            return this;
+        }
+
+        public org.hippoecm.frontend.validation.SvgValidationResult build() {
+            Set<String> offendingElements;
+            switch (this.offendingElements == null ? 0 : this.offendingElements.size()) {
+                case 0:
+                    offendingElements = java.util.Collections.emptySet();
+                    break;
+                case 1:
+                    offendingElements = java.util.Collections.singleton(this.offendingElements.get(0));
+                    break;
+                default:
+                    offendingElements = new java.util.LinkedHashSet<String>(
+                            this.offendingElements.size() < 1073741824 ? 1 + this.offendingElements.size() + (this.offendingElements.size() - 3) / 3 : Integer.MAX_VALUE);
+                    offendingElements.addAll(this.offendingElements);
+                    offendingElements = java.util.Collections.unmodifiableSet(offendingElements);
+            }
+            Set<String> offendingAttributes;
+            switch (this.offendingAttributes == null ? 0 : this.offendingAttributes.size()) {
+                case 0:
+                    offendingAttributes = java.util.Collections.emptySet();
+                    break;
+                case 1:
+                    offendingAttributes = java.util.Collections.singleton(this.offendingAttributes.get(0));
+                    break;
+                default:
+                    offendingAttributes = new java.util.LinkedHashSet<String>(
+                            this.offendingAttributes.size() < 1073741824 ? 1 + this.offendingAttributes.size() + (this.offendingAttributes.size() - 3) / 3 : Integer.MAX_VALUE);
+                    offendingAttributes.addAll(this.offendingAttributes);
+                    offendingAttributes = java.util.Collections.unmodifiableSet(offendingAttributes);
+            }
+
+            return new SvgValidationResult(offendingElements, offendingAttributes);
+        }
+
+    }
 }
