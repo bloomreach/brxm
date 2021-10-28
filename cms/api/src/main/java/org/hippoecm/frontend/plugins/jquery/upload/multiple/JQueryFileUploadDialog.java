dc5431c70cf7fb671af12bd88be50c4206ff9a85
Michiel Rop <michiel.rop@bloomreach.com>
2021-10-28 09:16:50 +0200

    CMS-14842 Remove lombok dependency
    For release/14.x we don't have a lombok dependency, so remove the
    dependency and use plain old java for the builder and slf4j.

--- cms/api/src/main/java/org/hippoecm/frontend/plugins/jquery/upload/multiple/JQueryFileUploadDialog.java
+++ cms/api/src/main/java/org/hippoecm/frontend/plugins/jquery/upload/multiple/JQueryFileUploadDialog.java
@@ -25,6 +25,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.ajax.markup.html.form.AjaxButton;
 import org.apache.wicket.markup.html.form.Button;
+import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.form.upload.FileUpload;
 import org.apache.wicket.model.ResourceModel;
 import org.apache.wicket.util.value.IValueMap;
