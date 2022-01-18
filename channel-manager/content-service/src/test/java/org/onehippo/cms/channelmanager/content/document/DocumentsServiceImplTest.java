def06538ee4ca4550595381899d1e0e8e633d1fe
Ard Schrijvers <ard.schrijvers@bloomreach.com>
2022-01-18 16:19:25 +0100

    CMS-14946 workaround for PowerMock initialization conflicting log4j
    [Backport 14.7]
    
    See CMS-14946 description for the explanation of this workaround
    
    (cherry picked from commit 0a55d95ab702c3d52dc0bc9d6eb4d1cdab65fabe)

--- channel-manager/content-service/src/test/java/org/onehippo/cms/channelmanager/content/document/DocumentsServiceImplTest.java
+++ channel-manager/content-service/src/test/java/org/onehippo/cms/channelmanager/content/document/DocumentsServiceImplTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016-2021 Hippo B.V. (http://www.onehippo.com)
+ * Copyright 2016-2022 Hippo B.V. (http://www.onehippo.com)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -82,6 +82,8 @@ import org.powermock.api.easymock.PowerMock;
 import org.powermock.core.classloader.annotations.PowerMockIgnore;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import static java.util.Collections.emptyMap;
 import static org.easymock.EasyMock.anyObject;
@@ -128,6 +130,10 @@ import static org.powermock.api.easymock.PowerMock.verifyAll;
         WorkflowUtils.class,
 })
 public class DocumentsServiceImplTest {
+
+    // WORKAROUND CMS-14946 :trigger log4j initialization to avoid power mock triggered deadlock in log4j.
+    // After CMS-14948 has been done this workaround can be removed again
+    private static final Logger ignore = LoggerFactory.getLogger(Object.class);
 
     private Session session;
     private UserContext userContext;
