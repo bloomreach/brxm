d4b452de7410ba65d37adb23a634c728d9292c78
Ard Schrijvers <ard.schrijvers@bloomreach.com>
2022-01-20 13:07:36 +0100

    CMS-14930 only logout (return to pool) pooled sessions
    [Backport 14.7]
    
    Make sure that only a pooled session is logged out early if a new
    session (or null) is set on the request context and the request
    context already had a session : if we'd log out any jcr session when
    a new session or null is set, it would also logout a jcr session which
    is tied to a CMS user which of course should never happen at this place
    
    Hence in the HstRequestContextImpl check whether the session is a
    pooled session and only log it out (return to pool) if the previously
    set session is a pooled session. This way a cms jcr session won't be
    logged out prematurely
    
    Also note that in the CleanupValve, setting the session to null really
    isn't needed as in the HstDelegateeFilterBean in the finally
    ResourceLifecycleManagement is taking care of logging out any sessions
    which require a logout (return to pool)
    
    (cherry picked from commit 6c13a1ee190357bc6a095fd25f29bf61611abaf8)

--- site-toolkit/components/core/src/main/java/org/hippoecm/hst/core/container/CleanupValve.java
+++ site-toolkit/components/core/src/main/java/org/hippoecm/hst/core/container/CleanupValve.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2008-2020 Hippo B.V. (http://www.onehippo.com)
+ *  Copyright 2008-2022 Hippo B.V. (http://www.onehippo.com)
  * 
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -47,9 +47,6 @@ public class CleanupValve extends AbstractBaseOrderableValve {
         if (subjectBasedSession) {
             clearSubjectSession(context, requestContext, sessionStateful);
         }
-
-        // ensure Session isn't tried to reuse again (it has been returned to the pool anyway if it was a pooled session)
-        ((HstMutableRequestContext) requestContext).setSession(null);
 
         if (sessionSecurityDelegation.sessionSecurityDelegationEnabled()) {
             sessionSecurityDelegation.cleanupSessionDelegates(requestContext);
