d4e81d6818af865fb1bd90341ac668410717c6f3
Michiel Rop <michiel.rop@bloomreach.com>
2022-01-28 14:33:43 +0100

    CMS-14951 Add command for node addition
    The branch information and the construction of the ContentTypeHandle are
    not needed for on-premise, because the content type are not publishable.
    
    Lombok is not a dependency in the on-premise branch, so the
    AddNodeFieldCommand is a delomboked version of the version on
    release/saas.
    
    (Compare with release/saas for a clear view of the differences)
    
    Loosely based on ENT-3403

---
+++ channel-manager/content-service/src/main/java/org/onehippo/cms/channelmanager/content/command/AddNodeFieldCommand.java
@@ -0,0 +1,103 @@
+/*
+ * Copyright 2021-2022 Hippo B.V. (http://www.onehippo.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.onehippo.cms.channelmanager.content.command;
+
+import java.util.List;
+
+import javax.jcr.Session;
+
+import org.onehippo.cms.channelmanager.content.document.DocumentsServiceImpl;
+import org.onehippo.cms.channelmanager.content.document.NodeFieldAddition;
+import org.onehippo.cms.channelmanager.content.document.util.FieldPath;
+import org.onehippo.cms.channelmanager.content.documenttype.field.type.FieldType;
+import org.onehippo.cms7.services.channelmanager.ChannelManagerCommand;
+import org.slf4j.Logger;
+
+import static org.onehippo.cms.channelmanager.content.document.DocumentsServiceImpl.getAbsolutePath;
+import static org.onehippo.cms.channelmanager.content.document.util.DocumentHandleUtils.getHandle;
+
+public class AddNodeFieldCommand implements ChannelManagerCommand {
+
+    private static final Logger log = org.slf4j.LoggerFactory.getLogger(AddNodeFieldCommand.class);
+    private final String uuid;
+    private final FieldPath fieldPath;
+    private final String type;
+    private final List<FieldType> fieldTypes;
+
+    AddNodeFieldCommand(String uuid, FieldPath fieldPath, String type, List<FieldType> fieldTypes) {
+        this.uuid = uuid;
+        this.fieldPath = fieldPath;
+        this.type = type;
+        this.fieldTypes = fieldTypes;
+    }
+
+    public static AddNodeFieldCommandBuilder builder() {
+        return new AddNodeFieldCommandBuilder();
+    }
+
+    @Override
+    public void execute(final Session session) {
+        log.debug("Execute {} on session: { {}, userId: {} }", this, session, session.getUserID());
+        final String variantAbsolutePath = getAbsolutePath(
+                DocumentsServiceImpl.getUnpublished(getHandle(uuid, session)));
+        final NodeFieldAddition nodeFieldAddition = new NodeFieldAddition(variantAbsolutePath,
+                fieldPath, fieldTypes, type, session);
+        nodeFieldAddition.add();
+    }
+
+    public String toString() {
+        return "AddNodeFieldCommand(uuid=" + this.uuid + ", fieldPath=" + this.fieldPath + ", type=" + this.type;
+    }
+
+    public static class AddNodeFieldCommandBuilder {
+        private String uuid;
+        private FieldPath fieldPath;
+        private String type;
+        private List<FieldType> fieldTypes;
+
+        AddNodeFieldCommandBuilder() {
+        }
+
+        public AddNodeFieldCommandBuilder uuid(String uuid) {
+            this.uuid = uuid;
+            return this;
+        }
+
+        public AddNodeFieldCommandBuilder fieldPath(FieldPath fieldPath) {
+            this.fieldPath = fieldPath;
+            return this;
+        }
+
+        public AddNodeFieldCommandBuilder type(String type) {
+            this.type = type;
+            return this;
+        }
+
+        public AddNodeFieldCommandBuilder fieldTypes(List<FieldType> fieldTypes) {
+            this.fieldTypes = fieldTypes;
+            return this;
+        }
+
+        public AddNodeFieldCommand build() {
+            return new AddNodeFieldCommand(uuid, fieldPath, type, fieldTypes);
+        }
+
+        public String toString() {
+            return "AddNodeFieldCommand.AddNodeFieldCommandBuilder(uuid=" + this.uuid + ", fieldPath=" + this.fieldPath
+                    + ", type=" + this.type + ", fieldTypes=" + this.fieldTypes + ")";
+        }
+    }
+}
