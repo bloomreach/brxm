62d9086490a861f01665e04153186aad7d58c6cf
Michiel Rop <michiel.rop@bloomreach.com>
2022-01-28 10:53:28 +0100

    CMS-14950 Align test with release/saas
    Align tests with instant preview feature on release/saas.
    
    See ENT-3371, ENT-3295, ENT-3283

--- channel-manager/frontend-ng/src/app/channel/sidePanels/rightSidePanel/contentEditor/contentEditor.service.spec.js
+++ channel-manager/frontend-ng/src/app/channel/sidePanels/rightSidePanel/contentEditor/contentEditor.service.spec.js
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018-2020 Hippo B.V. (http://www.onehippo.com)
+ * Copyright 2018-2022 Hippo B.V. (http://www.onehippo.com)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -103,10 +103,10 @@ describe('ContentEditorService', () => {
     };
 
     ContentService = jasmine.createSpyObj('ContentService', [
-      'getEditableDocument', 'getDocumentType', 'saveDocument', 'discardChanges', 'deleteDocument',
+      'getEditableDocument', 'getDocumentType', 'saveDocument', 'discardChanges', 'deleteDocument', 'saveDraft',
     ]);
     FeedbackService = jasmine.createSpyObj('FeedbackService', ['showError', 'showNotification']);
-    FieldService = jasmine.createSpyObj('FieldService', ['setDocumentId']);
+    FieldService = jasmine.createSpyObj('FieldService', ['setup']);
     DocumentWorkflowService = jasmine.createSpyObj('DocumentWorkflowService', [
       'cancelRequest',
       'publish',
