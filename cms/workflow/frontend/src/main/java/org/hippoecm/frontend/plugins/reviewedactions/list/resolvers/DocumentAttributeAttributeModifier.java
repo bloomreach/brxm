8ef58cb1d99c9e344b7e93dd14a35e015ecb7a25
Arthur Bogaart <arthur.bogaart@bloomreach.com>
2022-02-01 01:08:36 +0100

    CMS-14959 Replace deprecated AbstractReadOnlyModel
    The AbstractReadOnlyModel is deprecated and can be replaced by a
    lambda or a simple impl of IModel + "getObject" (the IModel interface
    has a default impl for "setObject").
    
    (cherry picked from commit 0dfbaf9350e3570c4c962c45331dc639a30af00f)

--- cms/workflow/frontend/src/main/java/org/hippoecm/frontend/plugins/reviewedactions/list/resolvers/DocumentAttributeAttributeModifier.java
+++ cms/workflow/frontend/src/main/java/org/hippoecm/frontend/plugins/reviewedactions/list/resolvers/DocumentAttributeAttributeModifier.java
@@ -1,33 +1,30 @@
 /*
- *  Copyright 2010-2018 Hippo B.V. (http://www.onehippo.com)
- * 
+ *  Copyright 2010-2022 Hippo B.V. (http://www.onehippo.com)
+ *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
  *  You may obtain a copy of the License at
- * 
+ *
  *       http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  *  Unless required by applicable law or agreed to in writing, software
  *  distributed under the License is distributed on an "AS IS" BASIS,
  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-
 package org.hippoecm.frontend.plugins.reviewedactions.list.resolvers;
 
 import javax.jcr.Node;
 
 import org.apache.wicket.AttributeModifier;
-import org.apache.wicket.model.AbstractReadOnlyModel;
+import org.apache.wicket.model.IModel;
 import org.hippoecm.frontend.model.JcrNodeModel;
 import org.hippoecm.frontend.plugins.standards.list.resolvers.AbstractNodeAttributeModifier;
 
 public abstract class DocumentAttributeAttributeModifier extends AbstractNodeAttributeModifier {
 
-    private static final long serialVersionUID = -1694566516119253553L;
-
-    private String attribute;
+    private final String attribute;
 
     public DocumentAttributeAttributeModifier(String attribute) {
         this.attribute = attribute;
@@ -38,7 +35,7 @@ public abstract class DocumentAttributeAttributeModifier extends AbstractNodeAtt
         return new AttributeModifier(attribute, new MyModel(node));
     }
 
-    class MyModel extends AbstractReadOnlyModel<String> {
+    class MyModel implements IModel<String> {
 
         StateIconAttributes atts;
 
