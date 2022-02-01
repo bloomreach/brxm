4d9798d426bf055f6339fe1f4fa054cca7761c05
Arthur Bogaart <arthur.bogaart@bloomreach.com>
2022-02-01 13:17:33 +0100

    CMS-14959 Remove deprecated "abstract" listener classes
    Replaced them with a direct impl of the interface.
    
    (cherry picked from commit 35313ab99c5a0904bde65f147311a29021282ea5)

--- cms/api/src/main/java/org/hippoecm/frontend/plugins/yui/layout/UnitBehavior.java
+++ cms/api/src/main/java/org/hippoecm/frontend/plugins/yui/layout/UnitBehavior.java
@@ -1,12 +1,12 @@
 /*
  *  Copyright 2008-2022 Hippo B.V. (http://www.onehippo.com)
- * 
+ *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
  *  You may obtain a copy of the License at
- * 
+ *
  *       http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  *  Unless required by applicable law or agreed to in writing, software
  *  distributed under the License is distributed on an "AS IS" BASIS,
  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -23,7 +23,6 @@ import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.apache.wicket.Component;
 import org.apache.wicket.Page;
 import org.apache.wicket.ajax.AjaxRequestTarget;
-import org.apache.wicket.ajax.AjaxRequestTarget.AbstractListener;
 import org.apache.wicket.behavior.Behavior;
 import org.apache.wicket.request.cycle.RequestCycle;
 import org.slf4j.Logger;
@@ -32,15 +31,13 @@ import org.slf4j.LoggerFactory;
 /**
  * This behavior stores the component id as it's markupId in the {@link UnitSettings} and if the request rendering this
  * behavior is an Ajax-request, it will force the whole wireframe to re-render (since this unit might be added to an
- * already existing wireframe).  
+ * already existing wireframe).
  */
 public class UnitBehavior extends Behavior {
 
-    private static final long serialVersionUID = 1L;
-
     static final Logger log = LoggerFactory.getLogger(UnitBehavior.class);
 
-    private String position;
+    private final String position;
 
     public UnitBehavior(String position) {
         this.position = position;
@@ -52,20 +49,19 @@ public class UnitBehavior extends Behavior {
 
         // re-render complete wireframe during the render phase
         Optional<AjaxRequestTarget> target = RequestCycle.get().find(AjaxRequestTarget.class);
-        target.ifPresent(ajaxRequestTarget -> ajaxRequestTarget.addListener(new AbstractListener() {
+        target.ifPresent(ajaxRequestTarget -> ajaxRequestTarget.addListener(new AjaxRequestTarget.IListener() {
+            @Override
             public void onBeforeRespond(Map map, AjaxRequestTarget target) {
                 if (target.getPage() != component.findParent(Page.class)) {
                     return;
                 }
+
                 IWireframe wireframe = WireframeUtils.getParentWireframe(component);
                 if (wireframe != null) {
                     wireframe.render(target);
                 } else {
                     log.warn("Unable to find parent wireframe-behavior");
                 }
-            }
-
-            public void onAfterRespond(Map map, AjaxRequestTarget.IJavaScriptResponse response) {
             }
         }));
     }
