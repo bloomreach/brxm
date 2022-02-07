71fd8e8f74ae9b45fe75243a1adafcb5b4104d75
Arthur Bogaart <arthur.bogaart@bloomreach.com>
2022-02-07 15:21:35 +0100

    CMS-14959 Bump license headers

--- cms/workflow/frontend/src/main/java/org/hippoecm/frontend/plugins/standardworkflow/PermissionsFolderWorkflowPlugin.java
+++ cms/workflow/frontend/src/main/java/org/hippoecm/frontend/plugins/standardworkflow/PermissionsFolderWorkflowPlugin.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013-2020 Hippo B.V. (http://www.onehippo.com)
+ * Copyright 2013-2022 Hippo B.V. (http://www.onehippo.com)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
