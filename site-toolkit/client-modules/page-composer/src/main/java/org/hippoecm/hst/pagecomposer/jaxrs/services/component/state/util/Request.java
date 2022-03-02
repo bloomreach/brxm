4d90c0d4077e5b85a8e5b50acdbcd36efe49e964
Dmytro Hachok <dmytro.hachok@bloomreach.com>
2022-03-02 11:44:07 +0000

    CMS-14312 Return workflow and scheduled requests in one array
    [Backport 15.0]
    
    As the FE must know the most recent one, return them both in one array
    instead of two separate ones
    
    The FE should be adjusted to deal with this BE change
    
    (cherry picked from commit cece2590872b0b535ed45a8bc1bcf32d73f5234d)

---
+++ site-toolkit/client-modules/page-composer/src/main/java/org/hippoecm/hst/pagecomposer/jaxrs/services/component/state/util/Request.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright 2022 Bloomreach
+ *
+ *
+ */
+package org.hippoecm.hst.pagecomposer.jaxrs.services.component.state.util;
+
+public interface Request {
+
+    String getRequestType();
+}
