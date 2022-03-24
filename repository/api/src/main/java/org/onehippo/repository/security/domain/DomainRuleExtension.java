37a42042d566a59b5369456009f8458f5d7fa92d
Michiel Rop <michiel.rop@bloomreach.com>
2022-03-24 13:08:12 +0100

    CMS-14973 Update preview session with added node
    Give the preview session access to prototypes so the AddNodeFieldCommand
    can access the prototype so the node field can be added and the user
    sees the newly added node field in the preview.

--- repository/api/src/main/java/org/onehippo/repository/security/domain/DomainRuleExtension.java
+++ repository/api/src/main/java/org/onehippo/repository/security/domain/DomainRuleExtension.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2013 Hippo B.V. (http://www.onehippo.com)
+ *  Copyright 2013-2022 Bloomreach (https://bloomreach.com)
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -18,17 +18,66 @@ package org.onehippo.repository.security.domain;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashSet;
+import java.util.UUID;
+
+import javax.jcr.PropertyType;
+
+import static org.hippoecm.repository.api.HippoNodeType.HIPPO_AVAILABILITY;
 
 public final class DomainRuleExtension {
+
+    public enum Type {
+        RELAX,
+        CONSTRAINT
+    }
 
     private final String domainName;
     private final String domainRuleName;
     private final Collection<FacetRule> facetRules;
+    private final Type type;
 
-    public DomainRuleExtension(final String domainName, final String domainRuleName, final Collection<FacetRule> facetRules) {
+    public final static FacetRule HIPPO_AVAILABILITY_PREVIEW_RULE = new FacetRule(HIPPO_AVAILABILITY, "preview", true, true, PropertyType.STRING);
+
+
+    public DomainRuleExtension(final String domainName,
+                               final String domainRuleName,
+                               final Collection<FacetRule> facetRules,
+                               final Type type) {
         this.domainName = domainName;
         this.domainRuleName = domainRuleName;
         this.facetRules = new HashSet<FacetRule>(facetRules);
+        this.type = type;
+    }
+
+    public DomainRuleExtension(final String domainName,
+                               final String domainRuleName,
+                               final Collection<FacetRule> facetRules) {
+        this(domainName, domainRuleName, facetRules, Type.CONSTRAINT);
+    }
+
+    /**
+     * To 'relax' the domain {@code domainName} this method can be used. There will be added a new domain rule below
+     * {@code domainName} with {@code facetRules} as constraint.
+     * @param domainName
+     * @param facetRules
+     * @return
+     */
+    public static DomainRuleExtension relaxDomain(final String domainName, final Collection<FacetRule> facetRules) {
+        return new DomainRuleExtension(domainName, UUID.randomUUID().toString(), facetRules, Type.RELAX);
+    }
+
+    /**
+     * To 'constraint' the domain rule {@code domainRuleName} for {@code domainName} this method can be used.
+     * There will be added extra constraints (facetRules)  to the domain rule. If the {@code domainRuleName} does not exist
+     * for the {@code domainName}, it will be skipped. Note that the {@code domainName} and/or {@code domainRuleName}
+     * can be '*'
+     * @param domainName
+     * @param domainRuleName
+     * @param facetRules
+     * @return
+     */
+    public static DomainRuleExtension constraintDomainRule(final String domainName, final String domainRuleName, final Collection<FacetRule> facetRules) {
+        return new DomainRuleExtension(domainName, domainRuleName, facetRules, Type.CONSTRAINT);
     }
 
     public String getDomainName() {
