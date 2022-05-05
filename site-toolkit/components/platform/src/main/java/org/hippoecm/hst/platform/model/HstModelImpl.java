7fa95564872aa65f3d2beb05f6311c5372c97651
Ard Schrijvers <ard.schrijvers@bloomreach.com>
2022-05-05 16:38:43 +0200

    CMS-15077 Fix HST Model invalidation / loading
    [Backport 15.1]
    
    Next to the Asynchronous HstConfigurationEventListener add a Synchronous
    listener for changes below /hst:hst. This Synchronous listener does not
    synchronize on the HstModel loading of the VirtualHostsService as this
    resulted in live/deadlocks. Instead it only increments an atomic long
    value to keep track of the number of onEvent invocations.
    
    When the VirtualHostsService buildNumber which is now stored does not
    match the Synchronous onEventCounter while the VirtualHostsService
    object in HstModelImpl has not yet been set to null, aka not yet
    invalidated, it means we have to wait until the asynchronous
    HstConfigurationEventListener has caught up. We cannot do this in a
    method synchronized on HstModelImpl instance as this would easily result
    in deadlocks. Hence the added InvalidationMonitor#awaitEventsConsistency
    which waits until the async listener is at least as far as the
    synchronous event listener. Only after that happens, we know all the
    events for invalidating parts of the HstNode tree and Hst Configuration
    objects have been processed by the HstConfigurationEventListener after
    which we can continue with building the model.
    
    Of course during building the model from the HstNode tree, the backing
    JCR Nodes can already change again, making the HstModel UUIDs out of
    sync with the actual JCR Nodes (in case of replacing nodes modifying
    the UUIDs). However this is nothing different than always can happen
    when a CMS user looks at a page in the CM : the VirtualHostsService
    used to render the page can always be stale and outdated, a fact which
    is just part of the interaction design and which the FE should handle
    gracefully.
    
    (cherry picked from commit 667e340b3a216d5d25d4817e748701c5aca1ac00)

--- site-toolkit/components/platform/src/main/java/org/hippoecm/hst/platform/model/HstModelImpl.java
+++ site-toolkit/components/platform/src/main/java/org/hippoecm/hst/platform/model/HstModelImpl.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2018-2020 Hippo B.V. (http://www.onehippo.com)
+ *  Copyright 2018-2022 Hippo B.V. (http://www.onehippo.com)
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -92,7 +92,7 @@ public class HstModelImpl implements InternalHstModel {
     private final HstCache pageCache;
     private final boolean clearPageCacheAfterModelLoad;
 
-    private volatile VirtualHosts virtualHosts;
+    private volatile VirtualHostsService virtualHosts;
     private BiPredicate<Session, Channel> channelFilter;
 
     private final String[] hstFilterPrefixExclusions;
@@ -160,7 +160,6 @@ public class HstModelImpl implements InternalHstModel {
     }
 
     public synchronized void invalidate() {
-
         if (virtualHosts == null) {
             return;
         }
@@ -177,17 +176,31 @@ public class HstModelImpl implements InternalHstModel {
 
     @Override
     public VirtualHosts getVirtualHosts() {
-        VirtualHosts vhosts = virtualHosts;
-        if (vhosts != null) {
+
+        VirtualHostsService vhosts = virtualHosts;
+        if (vhosts != null && vhosts.getBuildNumber() == invalidationMonitor.getSynchronousOnEventsCounter()) {
             return vhosts;
         }
+        if (vhosts != null) {
+            log.info("VirtualHostsService was not yet invalidated however the synchronous event listener already " +
+                    "received an event: trigger rebuild");
+        }
+
+        log.info("Building VirtualHostsService Model");
+
+
+        // this await is NOT allowed to be done in the synchronized (this) block below as it will result in LIVE/DEADLOCKS
+        // since invalidationMonitor.awaitEventsConsistency waits for the async hst model listener to receive events
+        // but that also synchronized on this
+        invalidationMonitor.awaitEventsConsistency();
 
         synchronized (this) {
+            // use the async counter as build number as this is how 'far' the events have been processed
+            long buildNumber = invalidationMonitor.getAsynchronousOnEventsCounter();
             vhosts = virtualHosts;
-            if (vhosts != null) {
+            if (vhosts != null && vhosts.getBuildNumber() == buildNumber) {
                 return vhosts;
             }
-
 
             final PlatformRequestContextProvider platformRequestContextProvider = new PlatformRequestContextProvider();
             final HstRequestContext requestContext = RequestContextProvider.get();
@@ -208,7 +221,7 @@ public class HstModelImpl implements InternalHstModel {
                 if (platformClassloader != currentClassloader) {
                     Thread.currentThread().setContextClassLoader(platformClassloader);
                 }
-                final VirtualHostsService virtualHosts = new VirtualHostsService(websiteServletContext.getContextPath(),
+                final VirtualHostsService virtualHosts = new VirtualHostsService(buildNumber, websiteServletContext.getContextPath(),
                         websiteContainerConfiguration,
                         hstNodeLoadingCache,
                         hstConfigurationLoadingCache);
