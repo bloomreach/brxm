b5248339977100f5e7e189c309f9e2eb22c81ede
Vasili Kauryha <vasili.kauryha@bloomreach.com>
2022-05-13 12:17:33 +0200

    CMS-15080 Populate all fields used in channel grid with type equal to string
    Explicitly set the data type for automatic conversion from received data to the stored value.
    During sorting in the grid the sort function is selected based on the defined type.
    If type equal to `string` the sorting function will be used as `asUCString` to make a case insensitive sorting.
    By default if type is absent it will be defined as `auto`, see Ext.data.Field.
    
    (cherry picked from commit 55bb98623c2fe66dd4668e9adcdd3cc186ca70a5)

--- channel-manager/frontend/src/main/resources/org/onehippo/cms7/channelmanager/channels/ChannelGridPanel.js
+++ channel-manager/frontend/src/main/resources/org/onehippo/cms7/channelmanager/channels/ChannelGridPanel.js
@@ -1,5 +1,5 @@
 /**
- * Copyright 2011-2020 Hippo B.V. (http://www.onehippo.com)
+ * Copyright 2011-2022 Hippo B.V. (http://www.onehippo.com)
  *
  * Licensed under the Apache License, Version 2.0 (the  "License");
  * you may not use this file except in compliance with the License.
@@ -188,6 +188,14 @@
      */
     Hippo.ChannelManager.ChannelStore = Ext.extend(Ext.data.GroupingStore, {
         constructor: function(config) {
+            // Explicitly set the data type for automatic conversion from received data to the stored value.
+            // The 'string' type will be use case insensitive 'asUCString' sorting, see Ext.data.SortTypes.
+            config.fields = config.fields.map(function(field) {
+                field.type = 'string';
+
+                return field;
+            });
+
             Hippo.ChannelManager.ChannelStore.superclass.constructor.call(this, Ext.apply(config, {
                 reader: new Ext.data.JsonReader(config)
             }));
