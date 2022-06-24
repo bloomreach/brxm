cef2c5a4de5804fa210fca33e253a74d17840282
Eduardo Perez <eduardo.perez@bloomreach.com>
2022-06-24 23:17:04 +0200

    CMS-13449 Fix unresolved references
    On some cases by fixing the class, and in some other cases by removing it:
    we can't reference a class that belongs to a downstream module.

--- site-toolkit/api/src/main/java/org/hippoecm/hst/configuration/sitemapitemhandlers/HstSiteMapItemHandlersConfiguration.java
+++ site-toolkit/api/src/main/java/org/hippoecm/hst/configuration/sitemapitemhandlers/HstSiteMapItemHandlersConfiguration.java
@@ -1,21 +1,10 @@
 /*
- *  Copyright 2008-2013 Hippo B.V. (http://www.onehippo.com)
- * 
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- * 
- *       http://www.apache.org/licenses/LICENSE-2.0
- * 
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
+ * Copyright 2008-2022 Bloomreach
  */
 package org.hippoecm.hst.configuration.sitemapitemhandlers;
 
 import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 
 public interface HstSiteMapItemHandlersConfiguration {
@@ -35,7 +24,7 @@ public interface HstSiteMapItemHandlersConfiguration {
     /**
      * Return the map of all <code>HstSiteMapItemHandlerConfiguration</code>'s where the keys are the the <code>HstSiteMapItemHandlerConfiguration</code>'s 
      * ({@link HstSiteMapItemHandlerConfiguration#getId()}).
-     * Implementations should return an unmodifiable map, for example {@link java.util.Collections$UnmodifiableMap} to avoid 
+     * Implementations should return an unmodifiable map, for example the one returned by {@link java.util.Collections#unmodifiableList(List)}, to avoid
      * client code changing configuration
      * @return the map of all <code>HstSiteMapItemHandlerConfiguration</code>'s and an empty map if there are no <code>HstSiteMapItemHandlerConfiguration</code>'s
      */
