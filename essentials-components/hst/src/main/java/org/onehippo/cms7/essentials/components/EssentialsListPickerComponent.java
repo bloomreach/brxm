fe415e1e9b6a74ae75e0fc9993a54685ee296605
Arthur Bogaart <arthur.bogaart@bloomreach.com>
2022-11-08 15:18:42 +0100

    CMS-15141 Remove deprecated classes and un-deprecate EssentialsListPickerComponent
    The EssentialsListPickerComponent class is still in use by customers.

--- essentials-components/hst/src/main/java/org/onehippo/cms7/essentials/components/EssentialsListPickerComponent.java
+++ essentials-components/hst/src/main/java/org/onehippo/cms7/essentials/components/EssentialsListPickerComponent.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014-2018 Hippo B.V. (http://www.onehippo.com)
+ * Copyright 2014-2022 Bloomreach (https://www.bloomreach.com)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -26,10 +26,6 @@ import org.hippoecm.hst.core.parameters.ParametersInfo;
 import org.onehippo.cms7.essentials.components.info.EssentialsListPickerComponentInfo;
 import org.onehippo.cms7.essentials.components.paging.Pageable;
 
-/**
- * @deprecated Not used by any supported plugin. Will be removed in 14.0.0. 
- */
-@Deprecated
 @ParametersInfo(type = EssentialsListPickerComponentInfo.class)
 public class EssentialsListPickerComponent extends EssentialsListComponent {
 
