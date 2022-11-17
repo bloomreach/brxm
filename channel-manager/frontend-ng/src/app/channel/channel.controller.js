c6e23a420945e214b92f4bd6c81c69814874afd5
Cuneyt Gul <cuneyt.gul@bloomreach.com>
2022-11-17 13:25:34 +0100

    CMS-15199 Allow managing external preview/urls on channel settings page
    External preview can be enabled over the channel settings page. Also the
    token can be regenerated on the same page. This can be managed on core
    and the projects separately. Also a preview button is added to the
    toolbar on the channel manager view.
    
    The site application can also be a ftl view site so besides spa sites,
    this type of site is also supported while preparing the preview urls
    which is different than SaaS.
    
    There is another difference on spa url configuration between PaaS and
    SaaS. In PaaS, spa url may also contain the endpoint url parameter which
    is not expected in SaaS. This distinction is also handled properly.

--- channel-manager/frontend-ng/src/app/channel/channel.controller.js
+++ channel-manager/frontend-ng/src/app/channel/channel.controller.js
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015-2020 Hippo B.V. (http://www.onehippo.com)
+ * Copyright 2015-2022 Hippo B.V. (http://www.onehippo.com)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -106,6 +106,15 @@ class ChannelCtrl {
 
   isPageLoaded() {
     return this.HippoIframeService.isPageLoaded();
+  }
+
+  openProjectChannelPreviewUrl(){
+    const token = this.ChannelService.getChannel().externalPreviewToken;
+    window.open(this.ChannelService.getProjectChannelPreviewURL(token),'_blank');
+  }
+
+  isExternalPreviewEnabled(){
+    return this.ChannelService.getChannel().externalPreviewEnabled;
   }
 
   editMenu(menuUuid) {
