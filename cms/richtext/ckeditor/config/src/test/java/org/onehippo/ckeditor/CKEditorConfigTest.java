52952ba617d7063da369ca885955c23e29daf3b6
Jeroen Hoffman <jeroen.hoffman@bloomreach.com>
2023-01-18 20:52:18 +0000

    CMS-15289 Make 15.x code base compilable on Windows

--- cms/richtext/ckeditor/config/src/test/java/org/onehippo/ckeditor/CKEditorConfigTest.java
+++ cms/richtext/ckeditor/config/src/test/java/org/onehippo/ckeditor/CKEditorConfigTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 Hippo B.V. (http://www.onehippo.com)
+ * Copyright 2017-2023 Hippo B.V. (http://www.onehippo.com)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -73,13 +73,14 @@ public class CKEditorConfigTest {
                 + "  plugins: 'myplugin'"
                 + "}";
         final ObjectNode combined = CKEditorConfig.combineConfig(defaultJson, overlayedJson, appendedJson);
-        assertEquals("{\n"
-                + "  codemirror : {\n"
-                + "    autoFormatOnStart : false\n"
-                + "  },\n"
-                + "  ignoreEmptyParagraph : true,\n"
-                + "  keystrokes : [ [ 77, \"maximize\" ], [ 88, \"showblocks\" ] ],\n"
-                + "  plugins : \"foo,bar,myplugin\"\n"
+        final String ls = System.lineSeparator();
+        assertEquals("{" + ls
+                + "  codemirror : {" + ls
+                + "    autoFormatOnStart : false" + ls
+                + "  }," + ls
+                + "  ignoreEmptyParagraph : true," + ls
+                + "  keystrokes : [ [ 77, \"maximize\" ], [ 88, \"showblocks\" ] ]," + ls
+                + "  plugins : \"foo,bar,myplugin\"" + ls
                 + "}",
                 Json.prettyString(combined));
     }
